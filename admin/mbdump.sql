f030b78b8d27b9628eb6343720b7ddc9ffe9984d
Robert Kaye <rob@eorbit.net>
2000-12-20 18:33:27 +0000

    Fixed some capiltalization issues

--- admin/mbdump.sql
+++ admin/mbdump.sql
@@ -1,8 +1,8 @@
 select * from Artist into outfile '/tmp/mbdump/Artist';
 select * from Album into outfile '/tmp/mbdump/Album';
 select * from Diskid into outfile '/tmp/mbdump/Diskid';
-select * from GUIDJoin into outfile '/tmp/mbdump/Guidjoin';
-select * from GUID into outfile '/tmp/mbdump/Guid';
-select * from AlbumJoin into outfile '/tmp/mbdump/Albumjoin';
+select * from GUIDJoin into outfile '/tmp/mbdump/GUIDJoin';
+select * from GUID into outfile '/tmp/mbdump/GUID';
+select * from AlbumJoin into outfile '/tmp/mbdump/AlbumJoin';
 select * from Track into outfile '/tmp/mbdump/Track';
 select * from TOC into outfile '/tmp/mbdump/TOC';
