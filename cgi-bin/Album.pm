f106350c4bf11d9d3e53e3b7fdd0b3a72ec38c69
Robert Kaye <rob@eorbit.net>
2001-01-11 23:16:57 +0000

    Ooops.

--- cgi-bin/Album.pm
+++ cgi-bin/Album.pm
@@ -70,7 +70,7 @@ sub GetTrackCountFromAlbum
    my ($this, $albumid) = @_;
    my ($sth, $count);
 
-   return -1 if (!defined $albumid)
+   return -1 if (!defined $albumid);
    $sth = $this->{DBH}->prepare(qq/select count(*) from AlbumJoin where 
                                    album=$albumid/);
    $sth->execute;
