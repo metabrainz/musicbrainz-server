7bb49cc26bb590bb5f3d0c12f0489d258b544334
Robert Kaye <rob@eorbit.net>
2001-07-19 23:26:50 +0000

    Added more checks

--- admin/cleanup/SanityCheck.pl
+++ admin/cleanup/SanityCheck.pl
@@ -39,6 +39,7 @@ sub Cleanup
     my ($dbh, $fix, $quiet, $arg1, $arg2) = @_;
     my $count = 0;
 
+    print "Missing artists:\n";
     $sth = $dbh->prepare(qq|select Album.id, Album.Artist 
                             from   Album left join Artist 
                             on     Album.artist = Artist.id 
@@ -49,13 +50,16 @@ sub Cleanup
 
         while(@row = $sth->fetchrow_array())
         {
-            print "Album $row[0] references non-existing artist $row[1].\n";
+            print "  Album $row[0] references non-existing artist $row[1].\n";
             $count++;
         }
     }
     $sth->finish;
-    print "Found $count missing artists.\n";
+    print "Found $count missing artists.\n\n";
 
+    # --------------------------------------------------------------------
+
+    print "Empty track names:\n";
     $count = 0;
     $sth = $dbh->prepare(qq|select Track.id, Track.artist
                             from   Track
@@ -66,12 +70,73 @@ sub Cleanup
 
         while(@row = $sth->fetchrow_array())
         {
-            print "Track $row[0] by artist $row[1] has an empty track name.\n";
+            print "  Track $row[0] by artist $row[1] has an " .
+                  " empty track name.\n";
             $count++;
+
+            $dbh->do("delete from Track where id = $row[0]") if ($fix);
         }
     }
     $sth->finish;
-    print "Found $count bad tracks.\n";
+    print "Found $count bad tracks.\n\n";
+
+    # --------------------------------------------------------------------
+
+    print "Empty albums:\n";
+    $count = 0;
+    $sth = $dbh->prepare(qq|select Album.id
+                            from   Album|);
+    if ($sth->execute() && $sth->rows())
+    {
+        my (@row, @row2, $sth2);
+
+        while(@row = $sth->fetchrow_array())
+        {
+            $sth2 = $dbh->prepare(qq|select count(*)
+                                     from   AlbumJoin
+                                     where  Album = $row[0]|);
+            if ($sth2->execute() && $sth2->rows())
+            {
+                @row2 = $sth2->fetchrow_array();
+                if ($row2[0] == 0)
+                {
+                    print "  Album $row[0] has no tracks.\n";
+                    $count++;
+
+                    if ($fix)
+                    {
+                        $dbh->do("delete from Album where id = $row[0]");
+                        $dbh->do("delete from Diskid where album = $row[0]");
+                    }
+                }
+            }
+        }
+    }
+    $sth->finish;
+    print "Found $count empty albums.\n\n";
+
+    # --------------------------------------------------------------------
+    
+    print "Orphaned diskids:\n";
+    $count = 0;
+    $sth = $dbh->prepare(qq|select Diskid.id, Diskid.Disk, Diskid.Album
+                            from   Diskid left join Album 
+                            on     Diskid.album = Album.id 
+                            WHERE  Album.id IS NULL|);
+    if ($sth->execute() && $sth->rows())
+    {
+        my @row;
+
+        while(@row = $sth->fetchrow_array())
+        {
+            print "  Diskid $row[1] references non-existing album $row[2].\n";
+            $count++;
+
+            $dbh->do("delete from Diskid where id = $row[0]") if ($fix);
+        }
+    }
+    $sth->finish;
+    print "Found $count missing artists.\n\n";
 }
 
 # Call main with the number of arguments that you are expecting
