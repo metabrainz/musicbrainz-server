c61e6fd821757d30957c7aac821715decabd6085
Myers W Carpenter <myers@maski.org>
2002-04-28 18:47:18 +0000

    Updated freedb import script.  Not that anyone uses it anymore, but here it is.  It's better because it computes track Lengths, and trys to extract the Issue year.

--- admin/freedb.pl
+++ admin/freedb.pl
@@ -6,10 +6,14 @@ use DBI;
 use DBDefs;
 use MusicBrainz;
 use Insert;
+use File::Copy;
+use File::Basename;
 
 my $line;
 my $tracks;
 my $artist;
+my $year;
+my $genre;
 my $title;
 my @ttitles;
 my $arg;
@@ -18,29 +22,41 @@ my $mb;
 my $crc;
 my $toc;
 
+my $error_dir;
+
 sub ReadOffsets
 {
    my $i = 0;
    my $line;
    my $crc;
    my $str;
+   my $total;
 
    while(defined($line = <FILE>))
    {
+       if ($line =~ /Disc length:/)
+       {
+           $line =~ s/^# Disc length: //;
+           $line =~ s/ seconds$//;
+           chop($line);
+           $total .= $line;
+           last;  
+       }
        $line =~ tr/0-9//cd;
-       
        if ($line eq '')
        {
-           last;
+           next;
        }
+       
        $str .= $line . " ";
        $i++
    }
+
    chop($str);
 
    $str = "1 $i 0 $str";
 
-   return ($i, $str);
+   return ($i, $str, $total);
 }
 
 sub ReadTitleAndArtist
@@ -89,6 +105,55 @@ sub ReadTitleAndArtist
    return ($title, $artist, $line);
 }
 
+sub ReadGenreAndYear
+{
+   my $line = $_[0];
+   my $genre;
+   my $year;
+   my $saved;
+
+   while(defined($line))
+   {
+       if (!($line =~ m/^DYEAR/))
+       {
+          last;
+       }
+
+       $line =~ s/DYEAR=//;
+       chop($line);
+       $saved .= $line;
+       $line = <FILE>;
+   }
+
+   if($saved eq '') 
+   {
+       $year = 0;
+   }
+   else
+   {
+       $year = $saved;
+   }
+
+   $saved = '';
+   
+   while(defined($line))
+   {
+       if (!($line =~ m/^DGENRE/))
+       {
+          last;
+       }
+
+       $line =~ s/DGENRE=//;
+       chop($line);
+       $saved .= $line;
+       $line = <FILE>;
+   }
+
+   $genre = $saved;
+
+   return ($genre, $year, $line);
+}
+
 sub ReadTitles
 {
    my $i = 0;
@@ -104,14 +169,15 @@ sub ReadTitles
            return [];
        }
        @parts = split /=/, $line, 2;
-       chop($parts[1]);
 
        $line = $parts[0];
+       chop($parts[1]);
+
        $line =~ tr/0-9//cd;
      
        if ($line eq '')
        {
-           printf("Something weird in '$title'. Skipping...\n");
+           printf("Something weird in '$title'. Skipping... (line eq '')\n");
            return @dummy
        }
        if ($line == $i - 1)
@@ -126,7 +192,7 @@ sub ReadTitles
       
        if ($line != $i)
        {
-           printf("Something weird in '$title'. Skipping...\n");
+           printf("Something weird in '$title'. Skipping... (line ($line) != i ($i)) \n",);
            return @dummy;
        }
 
@@ -142,7 +208,11 @@ sub ProcessFile
    my $line;
    my %info;
    my $in;
-
+   my @track_offsets;
+   my $disk_info;
+   my $total;
+   my $error = 0;
+   
    $in = Insert->new($mb->{DBH});
 
    $file = $_[0];
@@ -157,36 +227,67 @@ sub ProcessFile
          next;
       }
 
-      ($tracks, $toc) = ReadOffsets;
+      ($tracks, $toc, $total) = ReadOffsets;
       if (defined($tracks))
       {
           ($title, $artist, $line) = ReadTitleAndArtist;
           if (defined($artist) && defined($title) && defined($line))
           {
+              ($genre, $year, $line) = ReadGenreAndYear($line);
+              
               my (@data, $tit, @tarray);
 
               $info{artist} = $artist;
               $info{sortname} = $artist;
               $info{album} = $title;
 
+              $disk_info  = sprintf("artist: \"$info{artist}\"\n");
+              $disk_info .= sprintf(" album: \"$info{album}\"\n");
+              $disk_info .= sprintf("  year: \"$year\"\n");
+              $disk_info .= sprintf("tracks: \n");
+
+              @track_offsets = split(/ /, $toc);
+              shift @track_offsets;
+              shift @track_offsets;
+              shift @track_offsets;
+              
               @ttitles = ReadTitles($tracks, $line);
               if (scalar(@ttitles) > 0)
               {   
                   my $tracknum = 1;
                   foreach $tit (@ttitles)
                   {
+                     my $duration;
+                     if($tracknum < $tracks) 
+                     {
+                         $duration = int (($track_offsets[$tracknum] - $track_offsets[$tracknum - 1]) / 75);
+                     }
+                     else 
+                     {
+                         $duration = $total - (int (($track_offsets[$tracknum - 1]) / 75)) ;
+                     }
+                     #$disk_info .= sprintf("\t%02d - $tit (%d:%02d)\n", $tracknum, int($duration/60), $duration%60);
+                     $disk_info .= sprintf("\t%02d - $tit (%d)\n", $tracknum, $duration);
                      push @tarray, 
                         {
                            track => $tit,
-                           tracknum => $tracknum
+                           tracknum => $tracknum,
+                           duration => $duration,
+                           year => $year
                         };
                      $tracknum++;
                   }
                   $info{tracks} = \@tarray;
-
+                  
+                  #use Data::Dumper;
+                  #print Dumper(\%info);
+                   
                   if (!defined $in->Insert(\%info))
                   {
-                     print $in->GetError();
+                     $error = 1;
+                     print STDERR "Error with \"$file\":\n";
+                     print STDERR $in->GetError();
+                     print STDERR $disk_info;
                   }
                   $i++;
               }
@@ -196,6 +297,14 @@ sub ProcessFile
 
    close FILE;
    
+   if($error) 
+   {
+       my $filename = basename($file);
+       my $new_fullname = $error_dir."/".$filename;
+       move($file, $new_fullname);
+       print STDERR "Moved \"$file\" -> \"$new_fullname\"\n";
+   }         
+
    return $i;
 }
 
@@ -258,6 +367,7 @@ sub Recurse
     my ($dir) = @_;
     my $count = 0;
     my (@files, $path);
+    my $ii = 0;
 
     opendir(DIR, $dir) or die "Can't open $dir";
     @files = readdir(DIR);
@@ -285,9 +395,11 @@ my $count = 0;
 $mb = MusicBrainz->new;
 if (!$mb->Login)
 {
-    printf("Cannot log into database.\n");
+    print("Cannot log into database.\n");
     exit(0);
 }
+
+$error_dir = shift;
 
 while($arg = shift)
 {
