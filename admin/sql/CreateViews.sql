153fff2f29ca971630047206f0351a84e35eb44c
Dave Evans <dave@rudolf.org.uk>
2003-03-20 00:29:39 +0000

    Views no longer required

--- admin/sql/CreateViews.sql
+++ admin/sql/CreateViews.sql
@@ -1,30 +1,4 @@
 \set ON_ERROR_STOP 1
 begin;
 
-create view open_moderations as
-     select Moderation.id as moderation_id, tab, col, rowid, 
-            Moderation.artist, type, prevvalue, newvalue, 
-            ExpireTime, yesvotes, novotes, status, automod,
-            Moderator.id as moderator_id, 
-            Moderator.name as moderator_name, 
-            Artist.name as artist_name
-       from Moderation, Moderator, Artist
-      where Moderation.Artist = Artist.id and 
-            Moderator.id = Moderation.moderator and 
-            Moderation.moderator != 2 and 
-            status = 1;
-
-create view open_moderations_freedb as
-     select Moderation.id as moderation_id, tab, col, rowid, 
-            Moderation.artist, type, prevvalue, newvalue, 
-            ExpireTime, yesvotes, novotes, status, automod,
-            Moderator.id as moderator_id, 
-            Moderator.name as moderator_name, 
-            Artist.name as artist_name
-       from Moderation, Moderator, Artist
-      where Moderation.Artist = Artist.id and 
-            Moderator.id = Moderation.moderator and 
-            Moderation.moderator = 2 and 
-            status = 1;
-
 commit;
