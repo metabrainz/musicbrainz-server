43b4f92e4c9c55ae2b6bb7b9ddc03f11de9f32d6
Dave Evans <dave@rudolf.org.uk>
2003-04-15 23:02:02 +0000

    Redefined all settings as non-constant subs, so they can be changed at run time. Added WEB_SESSION_SECONDS_TO_LIVE and RDF_SESSION_SECONDS_TO_LIVE (and removed SESSION_SECONDS_TO_LIVE from UserStuff.pm). Added a few other settings (including MASON_DIR, APACHE_USER, APACHE_GROUP) so that virtually all changeable settings are in this one file. Re-organised the file into directories, database, other. Changed some default values.

--- cgi-bin/DBDefs.pm
+++ cgi-bin/DBDefs.pm
@@ -27,61 +27,103 @@ use vars qw(@ISA @EXPORT);
 @ISA    = @ISA    = '';
 @EXPORT = @EXPORT = '';
 
-# Make sure these settings are right. If you are using Postgres with a
-# database called 'musicbrainz' then you shouldn't need to change the next line
-use constant DSN 	=>	'dbi:Pg:dbname=musicbrainz';
 
-# Insert the name of the database here
-use constant DB_NAME 	=>	'musicbrainz';
-# Insert the user name that has access to the database listed above
-use constant DB_USER 	=>	'postgres';
-# Insert the password of the user from above
-use constant DB_PASSWD 	=>	'';
-# Insert other options to pass to Postgres programs here, e.g. "-h otherhost"
-use constant DB_PGOPTS	=>	'';
 
-# Set this value if you want to have a read-only server.
-use constant DB_READ_ONLY => 0;
+################################################################################
+# Directories
+################################################################################
+
+# The Server Root, i.e. the parent directory of admin, cgi-bin and htdocs
+sub MB_SERVER_ROOT	{ "/home/httpd/musicbrainz/mb_server" }
+# The htdocs directory
+sub HTDOCS_ROOT		{ MB_SERVER_ROOT() . "/htdocs" }
+
+# These two settings specify where to create the Apache::Session files
+# that are needed for HTTP session persistence
+sub LOCK_DIR		{ "/home/httpd/musicbrainz/locks" }
+sub SESSION_DIR		{ "/home/httpd/musicbrainz/sessions" }
+
+# Mason's data_dir and its cache directory
+sub MASON_DIR		{ "/home/httpd/musicbrainz/mason" }
+sub CACHE_DIR		{ MASON_DIR() . "/cache" }
+
+
+
+################################################################################
+# The Database
+################################################################################
+
+# The DBI connection string for your database
+sub DSN		{ 'dbi:Pg:dbname=musicbrainz_db' }
+
+# The name of the database
+sub DB_NAME	{ 'musicbrainz_db' }
+# The database user that has access to the database listed above
+sub DB_USER	{ 'musicbrainz_user' }
+# The password for the above user
+sub DB_PASSWD	{ '' }
+# Other command-line options to pass to Postgres programs, e.g. "-h otherhost"
+sub DB_PGOPTS	{ '' }
+
+
+
+################################################################################
+# HTTP Server Names
+################################################################################
+
+# The host names of the HTML / RDF parts of the server
+# To use a port number other than 80, add it like so: "myhost:8000"
+sub WEB_SERVER	{ "www.musicbrainz.example.com" }
+sub RDF_SERVER	{ "rdf.musicbrainz.example.com" }
+
+
+
+################################################################################
+# Other Settings
+################################################################################
+
+# Set this value to something true (e.g. 1) to set the server to read-only
+sub DB_READ_ONLY { 0 }
 
 # Set this value to a message that you'd like to display to users when
-# they attempt to log or otherwise modify the database.
-use constant DB_READ_ONLY_MESSAGE => qq/This server is temporarily in 
-read-only mode for database maintainance./;
+# they attempt to write to your read-only database (not used if DB_READ_ONLY
+# is false)
+sub DB_READ_ONLY_MESSAGE { <<EOF }
+This server is temporarily in read-only mode
+for database maintainance.
+EOF
 
-# Set this value if this is a staging server
-use constant DB_STAGING_SERVER => 1;
+# Set this to true if this is a staging server (i.e. if you want the page banner
+# and the front page to declare that this is the staging server.  It doesn't
+# affect much else).
+sub DB_STAGING_SERVER { 1 }
 
-# This defines the version of the server
-use constant VERSION => "1.0.0-preX";
+# This defines the version of the server.  Only used by things which display
+# the server version, e.g. at the foot of each web page.  Basically it can be
+# whatever you want.
+sub VERSION { "TRUNK" }
 
-# Defines the number of seconds before the votes on a 
-# modification are evaluated
-#use constant MOD_PERIOD => 604800;   # 1 week
-#use constant MOD_PERIOD => '2 days';   # 2 days
-use constant MOD_PERIOD => '1 second';   
+# Defines the expiry period of new moderations; i.e. age of the moderation
+# after which a simple majority will carry the vote.  This has to be in a
+# format understood by Postgres as an "interval".
+sub MOD_PERIOD { '1 week' }
 
-# Defines the number of unanimous votes required to pass a mod early
-use constant NUM_UNANIMOUS_VOTES => 5;
+# Defines the number of unanimous votes required to pass a moderation early
+sub NUM_UNANIMOUS_VOTES { 5 }
 
-# Do not set this unless you are running on a test server. This setting
-# Allows a person to vote on ther own modifications. It should be used
-# for testing purposes only.
-use constant ALLOW_SELF_VOTE => 0;
-
-# These two defines specify where to create the Apache::Session files
-# that are needed for HTTP session persistence.
-use constant LOCK_DIR => "/home/httpd/musicbrainz/locks";
-use constant SESSION_DIR => "/home/httpd/musicbrainz/sessions";
-use constant CACHE_DIR => "/home/httpd/musicbrainz/mason/cache";
-
-use constant DEBUG_LOG => "/tmp/musicbrainz-debug.log";
-
-# The host names of the HTML / RDF parts of the server.
-# To use a port number other than 80, add it like so: "myhost:8000"
-use constant WEB_SERVER => "www.musicbrainz.org";
-use constant RDF_SERVER => "mm.musicbrainz.org";
+# If this file exists (and is writeable by the web server), debugging
+# information is logged here.
+sub DEBUG_LOG	{ "/tmp/musicbrainz-debug.log" }
 
 # This log file is used to record updates to perform later.
-use constant DEFERRED_UPDATE_LOG => "/tmp/musicbrainz-deferred-update.log";
+sub DEFERRED_UPDATE_LOG { "/tmp/musicbrainz-deferred-update.log" }
+
+# How long (in seconds) a web/rdf session can go "idle" before being timed out
+sub WEB_SESSION_SECONDS_TO_LIVE { 3600 * 3 }
+sub RDF_SESSION_SECONDS_TO_LIVE { 3600 * 1 }
+
+# The user/group which Apache runs as after starting up
+sub APACHE_USER  { "nobody" }
+sub APACHE_GROUP { "nobody" }
 
 1;
