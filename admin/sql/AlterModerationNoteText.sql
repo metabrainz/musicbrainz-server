1c632a49b0c96148a44d93472e4465a54430ad05
Dave Evans <dave@rudolf.org.uk>
2003-08-09 09:59:30 +0000

    Change moderationnote.text from varchar(255) to TEXT (nice and long :-)

---
+++ admin/sql/AlterModerationNoteText.sql
@@ -0,0 +1,12 @@
+\set ON_ERROR_STOP 1
+
+BEGIN;
+
+ALTER TABLE moderationnote ADD COLUMN text2 TEXT;
+UPDATE moderationnote SET text2 = text;
+ALTER TABLE moderationnote ALTER COLUMN text2 SET NOT NULL;
+ALTER TABLE moderationnote DROP COLUMN text;
+ALTER TABLE moderationnote RENAME COLUMN text2 TO text;
+
+COMMIT;
+
