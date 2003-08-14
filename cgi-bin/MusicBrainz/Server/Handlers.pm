134fbbb4e86ef86a0d618d416dd80d71c4d4c161
Dave Evans <dave@rudolf.org.uk>
2003-08-14 20:06:03 +0000

    When a URL like "/artist/GUID" or "/track/GUID" (etc) is requested, do content negotiation: currently we supply two variants, namely "showartist.html" (etc) or the RDF MM query.  Switch by default on the "Accept" header, but allow "?content-type=..." to force a particular variant to be selected.

--- cgi-bin/MusicBrainz/Server/Handlers.pm
+++ cgi-bin/MusicBrainz/Server/Handlers.pm
@@ -5,7 +5,14 @@ use strict;
 
 package MusicBrainz::Server::Handlers;
 
-use Apache::Constants qw( DECLINED );
+use Apache::Constants qw( DECLINED HTTP_NOT_ACCEPTABLE );
+use HTTP::Headers;
+use HTTP::Negotiate;
+
+my $hex = "[0-9A-Fa-f]";
+my $GUID = join "-", map { scalar($hex x $_) } qw( 8 4 4 4 12 );
+my $discid_char = "[ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789._]";
+my $discid = "$discid_char+-{0,2}";
 
 sub TransHandler
 {
@@ -26,7 +33,136 @@ sub TransHandler
 		}
 	}
 
+	return negotiate_artist($r, $1, $2, $3)
+		if $uri =~ m[^/artist/($GUID)(?:/(.*?))?(?:\?(.*))?$]; #]
+
+	return negotiate_album($r, $1, $2, $3)
+		if $uri =~ m[^/album/($GUID)(?:/(.*?))?(?:\?(.*))?$]; #]
+
+	return negotiate_track($r, $1, $2, $3)
+		if $uri =~ m[^/track/($GUID)(?:/(.*?))?(?:\?(.*))?$]; #]
+
+	return negotiate_trm($r, $1, $2, $3)
+		if $uri =~ m[^/(?:trm|trmid)/($GUID)(?:/(.*?))?(?:\?(.*))?$]; #]
+
+	return negotiate_discid($r, $1, $2, $3)
+		if $uri =~ m[^/(?:cdindex|discid)/($discid)(?:/(.*?))?(?:\?(.*))?$]; #]
+
 	DECLINED;
+}
+
+sub negotiate_artist
+{
+	my ($r, $guid, $path, $query) = @_;
+
+	negotiate($r, {
+		"text/xml+rdf"	=> "/cgi-bin/rdf.pl?query=artist&id=$guid"
+			. (defined($path) ? "&depth=$path" : ""),
+		"text/html"		=> "/showartist.html?mbid=$guid",
+		"text/css"		=> "/css/main.css",
+	});
+}
+
+sub negotiate_album
+{
+	my ($r, $guid, $path, $query) = @_;
+
+	negotiate($r, {
+		"text/xml+rdf"	=> "/cgi-bin/rdf.pl?query=album&id=$guid"
+			. (defined($path) ? "&depth=$path" : ""),
+		"text/html"		=> "/showalbum.html?mbid=$guid",
+	});
+}
+
+sub negotiate_track
+{
+	my ($r, $guid, $path, $query) = @_;
+
+	negotiate($r, {
+		"text/xml+rdf"	=> "/cgi-bin/rdf.pl?query=track&id=$guid"
+			. (defined($path) ? "&depth=$path" : ""),
+		"text/html"		=> "/showtrack.html?mbid=$guid",
+	});
+}
+
+sub negotiate_trm
+{
+	my ($r, $guid, $path, $query) = @_;
+
+	negotiate($r, {
+		"text/xml+rdf"	=> "/cgi-bin/rdf.pl?query=trmid&id=$guid"
+			. (defined($path) ? "&depth=$path" : ""),
+		"text/html"		=> "/showtrm.html?trm=$guid",
+	});
+}
+
+sub negotiate_discid
+{
+	my ($r, $guid, $path, $query) = @_;
+
+	negotiate($r, {
+		# Hmm, that's odd.  It's in vh_httpd.conf, but rdf.pl doesn't support
+		# this query.
+		# "text/xml+rdf"	=> "/cgi-bin/rdf.pl?query=cdindexid&id=$guid"
+		# 	. (defined($path) ? "&depth=$path" : ""),
+		"text/html"		=> "/showalbum.html?discid=$guid",
+	});
+}
+
+sub negotiate
+{
+	my ($r, $map) = @_;
+
+	# Not perfect; $r->args in list context doesn't grok key/value pairs, so
+	# for example ?foo&bar=baz would come out as { foo => 'bar', baz => undef }
+	my $args = do { no warnings; +{ map { lc $_ } $r->args } };
+
+	# If the requested content-type is available, use that.
+	my $requested_type = $args->{"content-type"} || "";
+	if (my $uri = $map->{$requested_type})
+	{
+		return use_new_uri($r, $uri);
+	}
+
+	# Otherwise, negotiate the best fit based on the "Accept" header.
+	my $use_type = best_type($r, keys %$map) || "";
+	if (my $uri = $map->{$use_type})
+	{
+		return use_new_uri($r, $uri);
+	}
+	
+	# Otherwise, fail.
+	HTTP_NOT_ACCEPTABLE;
+}
+
+sub use_new_uri
+{
+	my ($r, $uri) = @_;
+
+	my $subr = $r->lookup_uri($uri);
+	$r->uri($subr->uri);
+	$r->filename($subr->filename);
+	$r->path_info($subr->path_info);
+	$r->args(scalar $subr->args);
+
+	DECLINED;
+}
+
+sub best_type
+{
+	my ($r, @types) = @_;
+
+	my @variants = map {
+		[ $_, 1, $_, undef, undef, undef, undef ]
+	} @types;
+
+	my $headers = new HTTP::Headers;
+	for (qw( Accept Accept-Charset Accept-Encoding Accept-Language ))
+	{
+		$headers->header($_, $r->header_in($_));
+	}
+
+	scalar HTTP::Negotiate::choose(\@variants, $headers);
 }
 
 1;
