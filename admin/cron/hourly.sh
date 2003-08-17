a5ae07d6e7072ccd26f7e8b53d0dfea9092e0a7f
Dave Evans <dave@rudolf.org.uk>
2003-08-17 17:44:51 +0000

    Simplify the modbot mailing strategy; now we e-mail if and only if "CheckVotes.pl" ended with an error code, for example if an exception was thrown whilst processing a moderation.

--- admin/cron/hourly.sh
+++ admin/cron/hourly.sh
@@ -5,8 +5,8 @@ cd ..
 # Hmmm, ugly.  Alternatively, just add a crontab entry like so:
 # 0 * * * * MAILTO="rob@eorbit.net,djce@musicbrainz.org" ./CheckVotes.pl
 ADMINS="rob@eorbit.net djce@musicbrainz.org"
-
-OUTPUT=`./CheckVotes.pl 2>&1 | grep -v ^DELETE`
-[ "$OUTPUT" == "" ] || ( echo "$OUTPUT" | mail -s "ModBot output" $ADMINS )
+OUTPUT=`
+	./CheckVotes.pl --verbose --summary 2>&1
+` || ( echo "$OUTPUT" | mail -s "ModBot output" $ADMINS )
 
 ./RemoveOldSessions > /dev/null
