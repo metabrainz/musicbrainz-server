b7bd9e38efe314ce0dac248473ed1441a0b1f399
Dave Evans <dave@rudolf.org.uk>
2003-08-17 15:35:28 +0000

    Filter out the "DELETE:" messages printed by Album.pm et al. (It may end up being more wise to just allow anything to be printed, but only mail if the "CheckVotes" command failed.)

--- admin/cron/hourly.sh
+++ admin/cron/hourly.sh
@@ -6,7 +6,7 @@ cd ..
 # 0 * * * * MAILTO="rob@eorbit.net,djce@musicbrainz.org" ./CheckVotes.pl
 ADMINS="rob@eorbit.net djce@musicbrainz.org"
 
-OUTPUT=`./CheckVotes.pl 2>&1`
+OUTPUT=`./CheckVotes.pl 2>&1 | grep -v ^DELETE`
 [ "$OUTPUT" == "" ] || ( echo "$OUTPUT" | mail -s "ModBot output" $ADMINS )
 
 ./RemoveOldSessions > /dev/null
