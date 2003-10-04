4312fe1dca3efbc3afa8de194abd22369bd31689
Dave Evans <dave@rudolf.org.uk>
2003-10-04 09:39:24 +0000

    Cosmetic change only: removed trailing fluff from comment lines

--- admin/sql/CreateTables.sql
+++ admin/sql/CreateTables.sql
@@ -11,7 +11,7 @@ create table Artist (
 
 create table ArtistAlias (
    Id serial primary key,
-   Ref int not null, -- references Artist, 
+   Ref int not null, -- references Artist
    Name varchar(255) not null, 
    TimesUsed int default 0,
    ModPending int default 0,
@@ -20,7 +20,7 @@ create table ArtistAlias (
 
 create table Album (
    Id serial primary key,
-   Artist int not null, -- references Artist,
+   Artist int not null, -- references Artist
    Name varchar(255) not null,
    GID char(36) not null, 
    ModPending int default 0,
@@ -35,7 +35,7 @@ create table AlbumMeta (
 
 create table Track (
    Id serial primary key,
-   Artist int not null, -- references Artist,
+   Artist int not null, -- references Artist
    Name varchar(255) not null,
    GID char(36) not null, 
    Length int default 0,
@@ -44,8 +44,8 @@ create table Track (
 
 create table AlbumJoin (
    Id serial primary key,
-   Album int not null, -- references Album,
-   Track int not null, -- references Track,
+   Album int not null, -- references Album
+   Track int not null, -- references Track
    Sequence int not null,
    ModPending int default 0);
 
@@ -57,24 +57,24 @@ create table TRM (
    Id serial primary key,
    TRM char(36) not null,
    LookupCount int default 0,
-   Version int not null); -- references ClientVersion);
+   Version int not null); -- references ClientVersion
 
 create table TRMJoin (
    Id serial primary key,
-   TRM int not null, -- references TRM, 
-   Track int not null); -- references Track);
+   TRM int not null, -- references TRM
+   Track int not null); -- references Track
 
 create table Discid (
    Id serial primary key,
-   Album int not null, -- references Album,
+   Album int not null, -- references Album
    Disc char(28) not null unique,
    Toc text not null, 
    ModPending int default 0);
 
 create table TOC (
    Id serial primary key,
-   Album int not null, -- references Album,
-   Discid char(28), -- references Discid (Disc),
+   Album int not null, -- references Album
+   Discid char(28), -- references Discid (Disc)
    Tracks int,
    Leadout int, Track1 int, Track2 int, Track3 int, Track4 int, Track5 int, Track6 int, Track7 int, 
    Track8 int, Track9 int, Track10 int, Track11 int, Track12 int, Track13 int, Track14 int, 
@@ -109,8 +109,8 @@ create table Moderator (
 
 create table Moderation (
    Id serial primary key,
-   Artist int not null, -- references Artist, 
-   Moderator int not null, -- references Moderator, 
+   Artist int not null, -- references Artist
+   Moderator int not null, -- references Moderator
    Tab varchar(32) not null,
    Col varchar(64) not null, 
    Type smallint not null, 
@@ -135,8 +135,8 @@ create table ModerationNote (
 
 create table Votes (
    Id serial primary key,
-   Uid int not null, -- references Moderator, 
-   Rowid int not null, -- references Moderation, 
+   Uid int not null, -- references Moderator
+   Rowid int not null, -- references Moderation
    vote smallint not null,
    votetime timestamp with time zone default now()
    );
@@ -175,8 +175,8 @@ create table Stats (
 
 create table artist_relation (
    Id serial primary key,
-   artist int not null, -- references Artist, 
-   ref int not null, -- references Artist, 
+   artist int not null, -- references Artist
+   ref int not null, -- references Artist
    weight integer not null);
 
 CREATE TABLE currentstat
