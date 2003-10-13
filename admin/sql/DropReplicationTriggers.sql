0a00fa4d9d586802d1a36fec8474945841966949
Robert Kaye <rob@eorbit.net>
2003-10-13 09:01:56 +0000

    utility script for dealing with replication triggers

---
+++ admin/sql/DropReplicationTriggers.sql
@@ -0,0 +1,18 @@
+SET autocommit TO 'on';
+
+DROP TRIGGER "a_idu_album" ON "album";
+DROP TRIGGER "a_idu_albumjoin" ON "albumjoin";
+DROP TRIGGER "a_idu_albummeta" ON "albummeta";
+DROP TRIGGER "a_idu_albumwords" ON "albumwords";
+DROP TRIGGER "a_idu_artist" ON "artist";
+DROP TRIGGER "a_idu_artistwords" ON "artistwords";
+DROP TRIGGER "a_idu_artist_relation" ON "artist_relation";
+DROP TRIGGER "a_idu_artistalias" ON "artistalias";
+DROP TRIGGER "a_idu_clientversion" ON "clientversion";
+DROP TRIGGER "a_idu_discid" ON "discid";
+DROP TRIGGER "a_idu_toc" ON "toc";
+DROP TRIGGER "a_idu_track" ON "track";
+DROP TRIGGER "a_idu_trackwords" ON "trackwords";
+DROP TRIGGER "a_idu_trm" ON "trm";
+DROP TRIGGER "a_idu_trmjoin" ON "trmjoin";
+DROP TRIGGER "a_idu_wordlist" ON "wordlist";
