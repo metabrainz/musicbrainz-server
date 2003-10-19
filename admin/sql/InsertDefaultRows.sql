0a1f24f8fd38c8419fa1742f2d37902b114178f6
Dave Evans <dave@rudolf.org.uk>
2003-10-19 22:24:05 +0000

    Always reset moderator sequence to 1 before inserting default moderators.

--- admin/sql/InsertDefaultRows.sql
+++ admin/sql/InsertDefaultRows.sql
@@ -8,20 +8,15 @@ insert into Artist (Name, SortName, GID, ModPending, Page)
       values ('Deleted Artist', 'Deleted Artist', 
               'c06aa285-520e-40c0-b776-83d2c9e8a6d1', 0, 0); 
 
-insert into Moderator (Name, Password, Privs, 
-      ModsAccepted, ModsRejected, MemberSince) values 
-      ('Anonymous', '', 0, 0, 0, now());
-insert into Moderator (Name, Password, Privs, 
-      ModsAccepted, ModsRejected, MemberSince) 
-      values ('FreeDB', '', 0, 0, 0, now());
-insert into Moderator (Name, Password, Privs, 
-      ModsAccepted, ModsRejected, MemberSince) 
-      values ('rob', '', 1, 0, 0, now());
-insert into Moderator (Name, Password, Privs, 
-      ModsAccepted, ModsRejected, MemberSince) 
-      values ('ModBot', '', 0, 0, 0, now());
+-- Three of these four moderators need fixed IDs, so we must set the sequence
+-- first to make sure we get the right ones.
+SELECT SETVAL('moderator_id_seq', 1);
 
-insert into ClientVersion (Id, Version) 
-      values (1, 'unknown');
+INSERT INTO moderator (name, password) VALUES ('Anonymous', '');
+INSERT INTO moderator (name, password) VALUES ('FreeDB', '');
+INSERT INTO moderator (name, password, privs) VALUES ('rob', '', 1);
+INSERT INTO moderator (name, password) VALUES ('ModBot');
+
+INSERT INTO clientversion (id, version) VALUES (1, 'unknown');
 
 Commit;
