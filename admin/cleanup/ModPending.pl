5e3e55d98cd55a00625a12f327d1de80b38eb644
Dave Evans <dave@rudolf.org.uk>
2003-11-13 13:28:54 +0000

    Made the switches actually do something, and implemented three "lockmodes".

--- admin/cleanup/ModPending.pl
+++ admin/cleanup/ModPending.pl
@@ -31,27 +31,18 @@ use lib "$FindBin::Bin/../../cgi-bin";
 
 use Getopt::Long;
 
-use DBDefs;
-use MusicBrainz;
-use Sql;
-use Moderation;
-use ModDefs qw( STATUS_OPEN );
-
-# TODO make these options do something
 my $verbose = -t;
-my $summary = 1;
-
-# TODO add a "locking strategy" option.  Possible strategies:
-# "full" - all tables exclusively locked, all in one transaction.
-#          (The current behaviour.)
-# "none" - don't lock anything much at all.  Separate transactions for
-#          resetting each table, finding open mods, and adjusting each mod.
-# "blank"- lock whilst blanking modpending and finding open mods.  Then
-#          unlock, and adjust for each mod in its own transaction.
+my $eachmod = -t;
+my $lockmode = "full";
 
 GetOptions(
 	"verbose!"		=> \$verbose,
-	"summary!"		=> \$summary,
+	"eachmod!"		=> \$eachmod,
+	"lockmode=s"	=> sub {
+		my $mode = $_[1];
+		$mode =~ /^(full|none|blank)$/ or die "Unknown lockmode\n";
+		$lockmode = $mode;
+	},
 	"help|h|?"		=> sub { usage(); exit },
 ) or exit 2;
 
@@ -63,61 +54,93 @@ sub usage
 Usage: ModPending.pl [OPTIONS]
 
 Allowed options are:
-        --[no]verbose     [don't] be verbose
-        --[no]summary     [don't] show summary information at the end
-                          (default is --summary)
+        --[no]verbose     [don't] describe what's being done
+        --[no]eachmod     [don't] show each moderation being processed
+        --lockmode=MODE   select locking strategy (see below)
     -h, --help            show this help (also "-?")
+
+The "locking strategy" option:
+"full" - all tables exclusively locked, all in one transaction.
+         (The default behaviour.)
+"none" - don't lock anything much at all.  Separate transactions for
+         resetting each table, finding open mods, and adjusting each mod.
+"blank"- lock whilst blanking modpending and finding open mods.  Then
+         unlock, and adjust for each mod in its own transaction.
 
 EOF
 }
+
+require DBDefs;
+require MusicBrainz;
+require Sql;
+require Moderation;
+
+use ModDefs qw( STATUS_OPEN );
 
 my $mb = MusicBrainz->new;
 $mb->Login;
 my $sql = Sql->new($mb->{DBH});
 
-print localtime() . " : Beginning transaction, locking tables\n";
-$sql->Begin;
-$sql->Do("LOCK TABLE moderation, artist, artistalias, album, discid, albumjoin, track IN EXCLUSIVE MODE");
+$verbose
+	? open(LOG, ">&STDOUT")
+	: open(LOG, ">/dev/null");
+
+################################################################################
+
+print LOG localtime() . " : Beginning transaction, locking tables\n";
+
+if ($lockmode eq "full" or $lockmode eq "blank")
+{
+	$sql->Begin;
+	my @t = qw(
+		moderation
+		artist
+		artistalias
+		album
+		discid
+		albumjoin
+		track
+	);
+	$sql->Do("LOCK TABLE ".join(", ", @t)." IN EXCLUSIVE MODE");
+}
 
 # Reset modpending to zero
 
-print localtime() . " : Blanking non-zero modpending counts\n";
+print LOG localtime() . " : Blanking non-zero modpending counts\n";
 
-$a = $sql->Do("UPDATE artist SET modpending = 0 WHERE modpending < 0");
-$b = $sql->Do("UPDATE artist SET modpending = 0 WHERE modpending > 0");
-print localtime() . " :   artist - $a negative, $b positive\n";
-
-$a = $sql->Do("UPDATE artistalias SET modpending = 0 WHERE modpending < 0");
-$b = $sql->Do("UPDATE artistalias SET modpending = 0 WHERE modpending > 0");
-print localtime() . " :   artistalias - $a negative, $b positive\n";
-
-$a = $sql->Do("UPDATE album SET modpending = 0 WHERE modpending < 0");
-$b = $sql->Do("UPDATE album SET modpending = 0 WHERE modpending > 0");
-print localtime() . " :   album - $a negative, $b positive\n";
-
-$a = $sql->Do("UPDATE album SET attributes[1] = 0 WHERE attributes[1] < 0");
-$b = $sql->Do("UPDATE album SET attributes[1] = 0 WHERE attributes[1] > 0");
-print localtime() . " :   album.attributes - $a negative, $b positive\n";
-
-$a = $sql->Do("UPDATE discid SET modpending = 0 WHERE modpending < 0");
-$b = $sql->Do("UPDATE discid SET modpending = 0 WHERE modpending > 0");
-print localtime() . " :   discid - $a negative, $b positive\n";
-
-$a = $sql->Do("UPDATE albumjoin SET modpending = 0 WHERE modpending < 0");
-$b = $sql->Do("UPDATE albumjoin SET modpending = 0 WHERE modpending > 0");
-print localtime() . " :   albumjoin - $a negative, $b positive\n";
-
-$a = $sql->Do("UPDATE track SET modpending = 0 WHERE modpending < 0");
-$b = $sql->Do("UPDATE track SET modpending = 0 WHERE modpending > 0");
-print localtime() . " :   track - $a negative, $b positive\n";
+for (qw(
+	artist
+	artistalias
+	album
+	album.attributes[1]
+	discid
+	albumjoin
+	track
+)) {
+	my ($table, $expr) = split /\./, $_;
+	$expr ||= "modpending";
+	$sql->AutoCommit if $lockmode eq "none";
+	$a = $sql->Do("UPDATE $table SET $expr = 0 WHERE $expr < 0");
+	$sql->AutoCommit if $lockmode eq "none";
+	$b = $sql->Do("UPDATE $table SET $expr = 0 WHERE $expr > 0");
+	printf LOG "%s : %-24.24s - %4d negative, %4d positive\n",
+		scalar(localtime),
+		"$table.$expr", $a, $b;
+}
 
 # Find all open moderations
 
-print localtime() . " : Finding open moderations\n";
+print LOG localtime() . " : Finding open moderations\n";
 my $ids = $sql->SelectSingleColumnArray(
 	"SELECT id FROM moderation WHERE status = " . STATUS_OPEN
 );
-print localtime() . " :   ".@$ids." mods open\n";
+print LOG localtime() . " :   ".@$ids." mods open\n";
+
+if ($lockmode eq "blank")
+{
+	print LOG localtime() . " : Committing transaction\n";
+	$sql->Commit;
+}
 
 # For each open moderation, construct the handler object and call its
 # "AdjustModPending" method
@@ -125,38 +148,54 @@ print localtime() . " :   ".@$ids." mods open\n";
 my $modclass = Moderation->new($mb->{DBH});
 my $n = 0;
 
+$eachmod
+	? open(EACHMOD, ">&STDOUT")
+	: open(EACHMOD, ">/dev/null");
+
 for my $modid (@$ids)
 {
 	++$n;
-	printf "%6d of %d - mod #%d",
+	printf EACHMOD "%6d of %d - mod #%d",
 		$n, scalar(@$ids), $modid;
+
+	$sql->Begin
+		if $lockmode eq "blank" or $lockmode eq "none";
 
 	my $mod = $modclass->CreateFromId($modid);
 
 	if (not ref($mod))
 	{
-		print " - load failed\n";
+		print EACHMOD " - load failed\n";
 		warn "Could not load moderation #$modid\n";
+		$sql->Commit
+			if $lockmode eq "blank" or $lockmode eq "none";
 		next;
 	}
 
-	printf " - %s (%s #%d)",
+	printf EACHMOD " - %s (%s #%d)",
 		$mod->Name, $mod->GetTable, $mod->GetRowId;
 
 	if (eval { $mod->AdjustModPending(+1); 1 })
 	{
-		print " - ok\n";
+		print EACHMOD " - ok\n";
+		$sql->Commit
+			if $lockmode eq "blank" or $lockmode eq "none";
 		next;
 	}
 
-	print " - AdjustModPending failed\n";
+	print EACHMOD " - AdjustModPending failed\n";
 	warn "Error encountered for moderation #$modid 'AdjustModPending': $@\n";
+	$sql->Rollback
+		if $lockmode eq "blank" or $lockmode eq "none";
 }
 
 # Commit!
 
-print localtime() . " : Committing transaction\n";
-$sql->Commit;
-print localtime() . " : Done!\n";
+if ($lockmode eq "full")
+{
+	print LOG localtime() . " : Committing transaction\n";
+	$sql->Commit;
+}
+print LOG localtime() . " : Done!\n";
 
 # eof ModPending.pl
