48bfbaa4fa4f7b1086a08ff74e29d2aa84779929
Dave Evans <dave@rudolf.org.uk>
2004-06-05 22:07:59 +0000

    Case and whitespace normalisation

--- admin/sql/CreateTables.sql
+++ admin/sql/CreateTables.sql
@@ -1,99 +1,99 @@
 \set ON_ERROR_STOP 1
-begin;
+BEGIN;
 
-create table Artist (
-   Id serial,
-   Name varchar(255) not null,
-   GID char(36) not null,
-   ModPending int default 0,
-   SortName varchar(255) not null,
-   Page int not null);
+CREATE TABLE artist (
+    id                  SERIAL,
+    name                VARCHAR(255) NOT NULL,
+    gid                 CHAR(36) NOT NULL,
+    modpending          INT DEFAULT 0,
+    sortname            VARCHAR(255) NOT NULL,
+    page                INT NOT NULL);
 
-create table ArtistAlias (
-   Id serial,
-   Ref int not null, -- references Artist
-   Name varchar(255) not null, 
-   TimesUsed int default 0,
-   ModPending int default 0,
-   lastused timestamp with time zone
+CREATE TABLE artistalias (
+    id                  SERIAL,
+    ref                 INT NOT NULL, -- references artist
+    name                VARCHAR(255) NOT NULL, 
+    timesused           INT DEFAULT 0,
+    modpending          INT DEFAULT 0,
+    lastused            TIMESTAMP WITH TIME ZONE
    );
 
-create table Album (
-   Id serial,
-   Artist int not null, -- references Artist
-   Name varchar(255) not null,
-   GID char(36) not null, 
-   ModPending int default 0,
-   Attributes int[] default '{0}',
-   Page int not null);
+CREATE TABLE album (
+    id                  SERIAL,
+    artist              INT NOT NULL, -- references artist
+    name                VARCHAR(255) NOT NULL,
+    gid                 CHAR(36) NOT NULL, 
+    modpending          INT DEFAULT 0,
+    attributes          INT[] DEFAULT '{0}',
+    page                INT NOT NULL);
 
-create table AlbumMeta (
-   Id integer not null,
-   tracks int default 0,
-   discids int default 0,
-   trmids int default 0,
-   firstreleasedate char(10),
-   asin char(10),
-   coverarturl varchar(255));
+CREATE TABLE albummeta (
+    Id                  INTEGER NOT NULL,
+    tracks              INT DEFAULT 0,
+    discids             INT DEFAULT 0,
+    trmids              INT DEFAULT 0,
+    firstreleasedate    CHAR(10),
+    asin                CHAR(10),
+    coverarturl         VARCHAR(255));
 
-create table Track (
-   Id serial,
-   Artist int not null, -- references Artist
-   Name varchar(255) not null,
-   GID char(36) not null, 
-   Length int default 0,
-   Year int default 0,
-   ModPending int default 0);
+CREATE TABLE track (
+    id                  SERIAL,
+    artist              INT NOT NULL, -- references artist
+    name                VARCHAR(255) NOT NULL,
+    gid                 CHAR(36) NOT NULL, 
+    length              INT DEFAULT 0,
+    year                INT DEFAULT 0,
+    modpending          INT DEFAULT 0);
 
-create table AlbumJoin (
-   Id serial,
-   Album int not null, -- references Album
-   Track int not null, -- references Track
-   Sequence int not null,
-   ModPending int default 0);
+CREATE TABLE albumjoin (
+    id                  SERIAL,
+    album               INT NOT NULL, -- references album
+    track               INT NOT NULL, -- references track
+    sequence            INT NOT NULL,
+    modpending          INT DEFAULT 0);
 
-create table ClientVersion (
-   Id serial,
-   Version varchar(64) not null);
+CREATE TABLE clientversion (
+    id                  SERIAL,
+    version             VARCHAR(64) NOT NULL);
 
-create table TRM (
-   Id serial,
-   TRM char(36) not null,
-   LookupCount int default 0,
-   Version int not null); -- references ClientVersion
+CREATE TABLE trm (
+    id                  SERIAL,
+    trm                 CHAR(36) NOT NULL,
+    lookupcount         INT DEFAULT 0,
+    version             INT NOT NULL); -- references clientversion
 
-create table TRMJoin (
-   Id serial,
-   TRM int not null, -- references TRM
-   Track int not null); -- references Track
+CREATE TABLE trmjoin (
+    id                  SERIAL,
+    trm                 INT NOT NULL, -- references trm
+    track               INT NOT NULL); -- references track
 
-create table Discid (
-   Id serial,
-   Album int not null, -- references Album
-   Disc char(28) not null,
-   Toc text not null, 
-   ModPending int default 0);
+CREATE TABLE discid (
+    id                  SERIAL,
+    album               INT NOT NULL, -- references album
+    disc                CHAR(28) NOT NULL,
+    toc                 TEXT NOT NULL, 
+    modpending          INT DEFAULT 0);
 
-create table TOC (
-   Id serial,
-   Album int not null, -- references Album
-   Discid char(28), -- references Discid (Disc)
-   Tracks int,
-   Leadout int, Track1 int, Track2 int, Track3 int, Track4 int, Track5 int, Track6 int, Track7 int, 
-   Track8 int, Track9 int, Track10 int, Track11 int, Track12 int, Track13 int, Track14 int, 
-   Track15 int, Track16 int, Track17 int, Track18 int, Track19 int, Track20 int, Track21 int, 
-   Track22 int, Track23 int, Track24 int, Track25 int, Track26 int, Track27 int, Track28 int, 
-   Track29 int, Track30 int, Track31 int, Track32 int, Track33 int, Track34 int, Track35 int, 
-   Track36 int, Track37 int, Track38 int, Track39 int, Track40 int, Track41 int, Track42 int, 
-   Track43 int, Track44 int, Track45 int, Track46 int, Track47 int, Track48 int, Track49 int, 
-   Track50 int, Track51 int, Track52 int, Track53 int, Track54 int, Track55 int, Track56 int, 
-   Track57 int, Track58 int, Track59 int, Track60 int, Track61 int, Track62 int, Track63 int, 
-   Track64 int, Track65 int, Track66 int, Track67 int, Track68 int, Track69 int, Track70 int, 
-   Track71 int, Track72 int, Track73 int, Track74 int, Track75 int, Track76 int, Track77 int, 
-   Track78 int, Track79 int, Track80 int, Track81 int, Track82 int, Track83 int, Track84 int, 
-   Track85 int, Track86 int, Track87 int, Track88 int, Track89 int, Track90 int, Track91 int, 
-   Track92 int, Track93 int, Track94 int, Track95 int, Track96 int, Track97 int, Track98 int, 
-   Track99 int);
+CREATE TABLE toc (
+    id                  SERIAL,
+    album               INT NOT NULL, -- references album
+    discid              CHAR(28), -- references discid (disc)
+    tracks              INT,
+    leadout int, track1 int, track2 int, track3 int, track4 int, track5 int, track6 int, track7 int, 
+    track8 int, track9 int, track10 int, track11 int, track12 int, track13 int, track14 int, 
+    track15 int, track16 int, track17 int, track18 int, track19 int, track20 int, track21 int, 
+    track22 int, track23 int, track24 int, track25 int, track26 int, track27 int, track28 int, 
+    track29 int, track30 int, track31 int, track32 int, track33 int, track34 int, track35 int, 
+    track36 int, track37 int, track38 int, track39 int, track40 int, track41 int, track42 int, 
+    track43 int, track44 int, track45 int, track46 int, track47 int, track48 int, track49 int, 
+    track50 int, track51 int, track52 int, track53 int, track54 int, track55 int, track56 int, 
+    track57 int, track58 int, track59 int, track60 int, track61 int, track62 int, track63 int, 
+    track64 int, track65 int, track66 int, track67 int, track68 int, track69 int, track70 int, 
+    track71 int, track72 int, track73 int, track74 int, track75 int, track76 int, track77 int, 
+    track78 int, track79 int, track80 int, track81 int, track82 int, track83 int, track84 int, 
+    track85 int, track86 int, track87 int, track88 int, track89 int, track90 int, track91 int, 
+    track92 int, track93 int, track94 int, track95 int, track96 int, track97 int, track98 int, 
+    track99 int);
 
 CREATE TABLE moderator (
     id                  SERIAL,
@@ -112,179 +112,179 @@ CREATE TABLE moderator (
     modsfailed          INTEGER DEFAULT 0
 );
 
-create table moderation_open (
-   id serial not null,
-   artist int not null, -- references Artist
-   moderator int not null, -- references Moderator
-   tab varchar(32) not null,
-   col varchar(64) not null, 
-   type smallint not null, 
-   status smallint not null, 
-   rowid int not null, 
-   prevvalue varchar(255) not null, 
-   newvalue text not null, 
-   yesvotes int default 0, 
-   novotes int default 0,
-   depmod int default 0,
-   automod smallint default 0,
-   opentime timestamp with time zone default now(),
-   closetime timestamp with time zone,
-   expiretime timestamp with time zone not null
+CREATE TABLE moderation_open (
+    id                  SERIAL NOT NULL,
+    artist              INT NOT NULL, -- references artist
+    moderator           INT NOT NULL, -- references moderator
+    tab                 VARCHAR(32) NOT NULL,
+    col                 VARCHAR(64) NOT NULL, 
+    type                SMALLINT NOT NULL, 
+    status              SMALLINT NOT NULL, 
+    rowid               INT NOT NULL, 
+    prevvalue           VARCHAR(255) NOT NULL, 
+    newvalue            TEXT NOT NULL, 
+    yesvotes            INT DEFAULT 0, 
+    novotes             INT DEFAULT 0,
+    depmod              INT DEFAULT 0,
+    automod             SMALLINT DEFAULT 0,
+    opentime            TIMESTAMP WITH TIME ZONE DEFAULT NOW(),
+    closetime           TIMESTAMP WITH TIME ZONE,
+    expiretime          TIMESTAMP WITH TIME ZONE NOT NULL
    );
 
-create table moderation_note_open (
-   id serial not null,
-   moderation int not null, 
-   moderator int not null, 
-   text TEXT not null);
+CREATE TABLE moderation_note_open (
+    id                  SERIAL NOT NULL,
+    moderation          INT NOT NULL, 
+    moderator           INT NOT NULL, 
+    text                TEXT NOT NULL);
 
-create table vote_open (
-   id serial not null,
-   moderator int not null, -- references Moderator
-   moderation int not null, -- references Moderation
-   vote smallint not null,
-   votetime timestamp with time zone default now(),
-   superseded BOOLEAN NOT NULL DEFAULT FALSE
+CREATE TABLE vote_open (
+    id                  SERIAL NOT NULL,
+    moderator           INT NOT NULL, -- references moderator
+    moderation          INT NOT NULL, -- references moderation
+    vote                SMALLINT NOT NULL,
+    votetime            TIMESTAMP WITH TIME ZONE DEFAULT NOW(),
+    superseded          BOOLEAN NOT NULL DEFAULT FALSE
    );
 
-create table moderation_closed (
-   id int not null,
-   artist int not null, -- references Artist
-   moderator int not null, -- references Moderator
-   tab varchar(32) not null,
-   col varchar(64) not null, 
-   type smallint not null, 
-   status smallint not null, 
-   rowid int not null, 
-   prevvalue varchar(255) not null, 
-   newvalue text not null, 
-   yesvotes int default 0, 
-   novotes int default 0,
-   depmod int default 0,
-   automod smallint default 0,
-   opentime timestamp with time zone default now(),
-   closetime timestamp with time zone,
-   expiretime timestamp with time zone not null
+CREATE TABLE moderation_closed (
+    id                  INT NOT NULL,
+    artist              INT NOT NULL, -- references artist
+    moderator           INT NOT NULL, -- references moderator
+    tab                 VARCHAR(32) NOT NULL,
+    col                 VARCHAR(64) NOT NULL, 
+    type                SMALLINT NOT NULL, 
+    status              SMALLINT NOT NULL, 
+    rowid               INT NOT NULL, 
+    prevvalue           VARCHAR(255) NOT NULL, 
+    newvalue            TEXT NOT NULL, 
+    yesvotes            INT DEFAULT 0, 
+    novotes             INT DEFAULT 0,
+    depmod              INT DEFAULT 0,
+    automod             SMALLINT DEFAULT 0,
+    opentime            TIMESTAMP WITH TIME ZONE DEFAULT NOW(),
+    closetime           TIMESTAMP WITH TIME ZONE,
+    expiretime          TIMESTAMP WITH TIME ZONE NOT NULL
    );
 
-create table moderation_note_closed (
-   id int not null,
-   moderation int not null, 
-   moderator int not null, 
-   text TEXT not null);
+CREATE TABLE moderation_note_closed (
+    id                  INT NOT NULL,
+    moderation          INT NOT NULL, 
+    moderator           INT NOT NULL, 
+    text                TEXT NOT NULL);
 
-create table vote_closed (
-   id int not null,
-   moderator int not null, -- references Moderator
-   moderation int not null, -- references Moderation
-   vote smallint not null,
-   votetime timestamp with time zone default now(),
-   superseded BOOLEAN NOT NULL DEFAULT FALSE
+CREATE TABLE vote_closed (
+    id                  INT NOT NULL,
+    moderator           INT NOT NULL, -- references moderator
+    moderation          INT NOT NULL, -- references moderation
+    vote                SMALLINT NOT NULL,
+    votetime            TIMESTAMP WITH TIME ZONE DEFAULT NOW(),
+    superseded          BOOLEAN NOT NULL DEFAULT FALSE
    );
 
-create table wordlist(
-   id serial,
-   word varchar(255) not null,
-   artistusecount SMALLINT NOT NULL DEFAULT 0,
-   albumusecount SMALLINT NOT NULL DEFAULT 0,
-   trackusecount SMALLINT NOT NULL DEFAULT 0
+CREATE TABLE wordlist(
+    id                  SERIAL,
+    word                VARCHAR(255) NOT NULL,
+    artistusecount      SMALLINT NOT NULL DEFAULT 0,
+    albumusecount       SMALLINT NOT NULL DEFAULT 0,
+    trackusecount       SMALLINT NOT NULL DEFAULT 0
 );
 
-create table ArtistWords(
-   Wordid int not null,
-   Artistid int not null);
+CREATE TABLE artistwords(
+    wordid              INT NOT NULL,
+    artistid            INT NOT NULL);
 
-create table AlbumWords(
-   Wordid int not null,
-   Albumid int not null);
+CREATE TABLE albumwords(
+    wordid              INT NOT NULL,
+    albumid             INT NOT NULL);
 
-create table TrackWords (
-   Wordid int not null,
-   Trackid int not null);
+CREATE TABLE trackwords (
+    wordid              INT NOT NULL,
+    trackid             INT NOT NULL);
 
-create table Stats (
-   Id serial,
-   artists int not null, 
-   albums int not null, 
-   tracks int not null, 
-   discids int not null, 
-   trmids int not null, 
-   moderations int not null, 
-   votes int not null, 
-   moderators int not null, 
-   timestamp date not null);
+CREATE TABLE stats (
+    id                  SERIAL,
+    artists             INT NOT NULL, 
+    albums              INT NOT NULL, 
+    tracks              INT NOT NULL, 
+    discids             INT NOT NULL, 
+    trmids              INT NOT NULL, 
+    moderations         INT NOT NULL, 
+    votes               INT NOT NULL, 
+    moderators          INT NOT NULL, 
+    timestamp           DATE NOT NULL);
 
-create table artist_relation (
-   Id serial,
-   artist int not null, -- references Artist
-   ref int not null, -- references Artist
-   weight integer not null);
+CREATE TABLE artist_relation (
+    id                  SERIAL,
+    artist              INT NOT NULL, -- references artist
+    ref                 INT NOT NULL, -- references artist
+    weight              INTEGER NOT NULL);
 
 CREATE TABLE currentstat
 (
-        id              SERIAL,
-        name            VARCHAR(100) NOT NULL,
-        value           INTEGER NOT NULL,
-        lastupdated     TIMESTAMP WITH TIME ZONE
+    id                  SERIAL,
+    name                VARCHAR(100) NOT NULL,
+    value               INTEGER NOT NULL,
+    lastupdated         TIMESTAMP WITH TIME ZONE
 );
 
 CREATE TABLE historicalstat
 (
-        id              SERIAL,
-        name            VARCHAR(100) NOT NULL,
-        value           INTEGER NOT NULL,
-        snapshotdate    DATE NOT NULL
+    id                  SERIAL,
+    name                VARCHAR(100) NOT NULL,
+    value               INTEGER NOT NULL,
+    snapshotdate        DATE NOT NULL
 );
 
 CREATE TABLE moderator_preference
 (
-        id              SERIAL,
-        moderator       INTEGER NOT NULL, -- references moderator
-        name            VARCHAR(50) NOT NULL,
-        value           VARCHAR(100) NOT NULL
+    id                  SERIAL,
+    moderator           INTEGER NOT NULL, -- references moderator
+    name                VARCHAR(50) NOT NULL,
+    value               VARCHAR(100) NOT NULL
 );
 
 CREATE TABLE moderator_subscribe_artist
 (
-        id              SERIAL,
-        moderator       INTEGER NOT NULL, -- references moderator
-        artist          INTEGER NOT NULL, -- weakly references artist
-        lastmodsent     INTEGER NOT NULL, -- weakly references moderation
-        deletedbymod    INTEGER NOT NULL DEFAULT 0, -- weakly references moderation
-        mergedbymod     INTEGER NOT NULL DEFAULT 0 -- weakly references moderation
+    id                  SERIAL,
+    moderator           INTEGER NOT NULL, -- references moderator
+    artist              INTEGER NOT NULL, -- weakly references artist
+    lastmodsent         INTEGER NOT NULL, -- weakly references moderation
+    deletedbymod        INTEGER NOT NULL DEFAULT 0, -- weakly references moderation
+    mergedbymod         INTEGER NOT NULL DEFAULT 0 -- weakly references moderation
 );
 
 CREATE TABLE country
 (
-        id              SERIAL,
-        isocode         VARCHAR(2) NOT NULL,
-        name            VARCHAR(100) NOT NULL
+    id                  SERIAL,
+    isocode             VARCHAR(2) NOT NULL,
+    name                VARCHAR(100) NOT NULL
 );
 
 CREATE TABLE release
 (
-        id              SERIAL,
-        album           INTEGER NOT NULL, -- references album
-        country         INTEGER NOT NULL, -- references country
-        releasedate     CHAR(10) NOT NULL,
-        modpending      INTEGER DEFAULT 0
+    id                  SERIAL,
+    album               INTEGER NOT NULL, -- references album
+    country             INTEGER NOT NULL, -- references country
+    releasedate         CHAR(10) NOT NULL,
+    modpending          INTEGER DEFAULT 0
 );
 
-create table album_amazon_asin (
-        album           INTEGER NOT NULL, -- references Album
-        asin            CHAR(10),
-        coverarturl     VARCHAR(255),
-        lastupdate      timestamp with time zone default now()
+CREATE TABLE album_amazon_asin (
+    album               INTEGER NOT NULL, -- references album
+    asin                CHAR(10),
+    coverarturl         VARCHAR(255),
+    lastupdate          TIMESTAMP WITH TIME ZONE DEFAULT NOW()
 );
 
 CREATE TABLE replication_control
 (
-        id                              SERIAL,
-        current_schema_sequence         INTEGER NOT NULL,
-        current_replication_sequence    INTEGER,
-        last_replication_date           TIMESTAMP WITH TIME ZONE
+    id                              SERIAL,
+    current_schema_sequence         INTEGER NOT NULL,
+    current_replication_sequence    INTEGER,
+    last_replication_date           TIMESTAMP WITH TIME ZONE
 );
 
-commit;
+COMMIT;
 
 -- vi: set ts=4 sw=4 et :
