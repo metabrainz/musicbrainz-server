21e7a26ace84ccfd0d83849b8a7294722e004739
Dave Evans <dave@rudolf.org.uk>
2005-01-08 12:50:00 +0000

    Alphabetical order by table, then constraint

--- admin/sql/CreateFKConstraints.sql
+++ admin/sql/CreateFKConstraints.sql
@@ -1,55 +1,16 @@
 \set ON_ERROR_STOP 1
 
-ALTER TABLE artistalias
-    ADD CONSTRAINT artistalias_fk_ref
-    FOREIGN KEY (ref)
-    REFERENCES artist(id);
+-- Alphabetical order by table, then constraint
 
 ALTER TABLE album
     ADD CONSTRAINT album_fk_artist
     FOREIGN KEY (artist)
     REFERENCES artist(id);
 
-ALTER TABLE track
-    ADD CONSTRAINT track_fk_artist
-    FOREIGN KEY (artist)
-    REFERENCES artist(id);
-
-ALTER TABLE albumjoin
-    ADD CONSTRAINT albumjoin_fk_album
+ALTER TABLE album_amazon_asin
+    ADD CONSTRAINT album_amazon_asin_fk_album
     FOREIGN KEY (album)
-    REFERENCES album(id);
-
-ALTER TABLE albumjoin
-    ADD CONSTRAINT albumjoin_fk_track
-    FOREIGN KEY (track)
-    REFERENCES track(id);
-
-ALTER TABLE trm
-    ADD CONSTRAINT trm_fk_clientversion
-    FOREIGN KEY (version)
-    REFERENCES clientversion(id);
-
-ALTER TABLE trm_stat
-    ADD CONSTRAINT trm_stat_fk_trm
-    FOREIGN KEY (trm_id)
-    REFERENCES trm(id)
-    ON DELETE CASCADE;
-
-ALTER TABLE trmjoin
-    ADD CONSTRAINT trmjoin_fk_trm
-    FOREIGN KEY (trm)
-    REFERENCES trm(id);
-
-ALTER TABLE trmjoin
-    ADD CONSTRAINT trmjoin_fk_track
-    FOREIGN KEY (track)
-    REFERENCES track(id);
-
-ALTER TABLE trmjoin_stat
-    ADD CONSTRAINT trmjoin_stat_fk_trmjoin
-    FOREIGN KEY (trmjoin_id)
-    REFERENCES trmjoin(id)
+    REFERENCES album(id)
     ON DELETE CASCADE;
 
 ALTER TABLE album_cdtoc
@@ -62,65 +23,20 @@ ALTER TABLE album_cdtoc
     FOREIGN KEY (cdtoc)
     REFERENCES cdtoc(id);
 
-ALTER TABLE moderation_open
-    ADD CONSTRAINT moderation_open_fk_artist
-    FOREIGN KEY (artist)
+ALTER TABLE albumjoin
+    ADD CONSTRAINT albumjoin_fk_album
+    FOREIGN KEY (album)
+    REFERENCES album(id);
+
+ALTER TABLE albumjoin
+    ADD CONSTRAINT albumjoin_fk_track
+    FOREIGN KEY (track)
+    REFERENCES track(id);
+
+ALTER TABLE artistalias
+    ADD CONSTRAINT artistalias_fk_ref
+    FOREIGN KEY (ref)
     REFERENCES artist(id);
-
-ALTER TABLE moderation_open
-    ADD CONSTRAINT moderation_open_fk_moderator
-    FOREIGN KEY (moderator)
-    REFERENCES moderator(id);
-
-ALTER TABLE moderation_note_open
-    ADD CONSTRAINT moderation_note_open_fk_moderation
-    FOREIGN KEY (moderation)
-    REFERENCES moderation_open(id);
-
-ALTER TABLE moderation_note_open
-    ADD CONSTRAINT moderation_note_open_fk_moderator
-    FOREIGN KEY (moderator)
-    REFERENCES moderator(id);
-
-ALTER TABLE vote_open
-    ADD CONSTRAINT vote_open_fk_moderator
-    FOREIGN KEY (moderator)
-    REFERENCES moderator(id);
-
-ALTER TABLE vote_open
-    ADD CONSTRAINT vote_open_fk_moderation
-    FOREIGN KEY (moderation)
-    REFERENCES moderation_open(id);
-
-ALTER TABLE moderation_closed
-    ADD CONSTRAINT moderation_closed_fk_artist
-    FOREIGN KEY (artist)
-    REFERENCES artist(id);
-
-ALTER TABLE moderation_closed
-    ADD CONSTRAINT moderation_closed_fk_moderator
-    FOREIGN KEY (moderator)
-    REFERENCES moderator(id);
-
-ALTER TABLE moderation_note_closed
-    ADD CONSTRAINT moderation_note_closed_fk_moderation
-    FOREIGN KEY (moderation)
-    REFERENCES moderation_closed(id);
-
-ALTER TABLE moderation_note_closed
-    ADD CONSTRAINT moderation_note_closed_fk_moderator
-    FOREIGN KEY (moderator)
-    REFERENCES moderator(id);
-
-ALTER TABLE vote_closed
-    ADD CONSTRAINT vote_closed_fk_moderator
-    FOREIGN KEY (moderator)
-    REFERENCES moderator(id);
-
-ALTER TABLE vote_closed
-    ADD CONSTRAINT vote_closed_fk_moderation
-    FOREIGN KEY (moderation)
-    REFERENCES moderation_closed(id);
 
 ALTER TABLE artist_relation
     ADD CONSTRAINT artist_relation_fk_artist1
@@ -131,34 +47,6 @@ ALTER TABLE artist_relation
     ADD CONSTRAINT artist_relation_fk_artist2
     FOREIGN KEY (ref)
     REFERENCES artist(id);
-
-ALTER TABLE moderator_preference
-    ADD CONSTRAINT moderator_preference_fk_moderator
-    FOREIGN KEY (moderator)
-    REFERENCES moderator(id);
-
-ALTER TABLE moderator_subscribe_artist
-    ADD CONSTRAINT modsubartist_fk_moderator
-    FOREIGN KEY (moderator)
-    REFERENCES moderator(id);
-
--- albummeta ?
-
-ALTER TABLE release
-    ADD CONSTRAINT release_fk_album
-    FOREIGN KEY (album)
-    REFERENCES album(id);
-
-ALTER TABLE release
-    ADD CONSTRAINT release_fk_country
-    FOREIGN KEY (country)
-    REFERENCES country(id);
-
-ALTER TABLE album_amazon_asin
-    ADD CONSTRAINT album_amazon_asin_fk_album
-    FOREIGN KEY (album)
-    REFERENCES album(id)
-    ON DELETE CASCADE;
 
 ALTER TABLE automod_election
     ADD CONSTRAINT automod_election_fk_candidate
@@ -188,6 +76,118 @@ ALTER TABLE automod_election_vote
 ALTER TABLE automod_election_vote
     ADD CONSTRAINT automod_election_vote_fk_voter
     FOREIGN KEY (voter)
+    REFERENCES moderator(id);
+
+ALTER TABLE moderation_closed
+    ADD CONSTRAINT moderation_closed_fk_artist
+    FOREIGN KEY (artist)
+    REFERENCES artist(id);
+
+ALTER TABLE moderation_closed
+    ADD CONSTRAINT moderation_closed_fk_moderator
+    FOREIGN KEY (moderator)
+    REFERENCES moderator(id);
+
+ALTER TABLE moderation_note_closed
+    ADD CONSTRAINT moderation_note_closed_fk_moderation
+    FOREIGN KEY (moderation)
+    REFERENCES moderation_closed(id);
+
+ALTER TABLE moderation_note_closed
+    ADD CONSTRAINT moderation_note_closed_fk_moderator
+    FOREIGN KEY (moderator)
+    REFERENCES moderator(id);
+
+ALTER TABLE moderation_open
+    ADD CONSTRAINT moderation_open_fk_artist
+    FOREIGN KEY (artist)
+    REFERENCES artist(id);
+
+ALTER TABLE moderation_open
+    ADD CONSTRAINT moderation_open_fk_moderator
+    FOREIGN KEY (moderator)
+    REFERENCES moderator(id);
+
+ALTER TABLE moderation_note_open
+    ADD CONSTRAINT moderation_note_open_fk_moderation
+    FOREIGN KEY (moderation)
+    REFERENCES moderation_open(id);
+
+ALTER TABLE moderation_note_open
+    ADD CONSTRAINT moderation_note_open_fk_moderator
+    FOREIGN KEY (moderator)
+    REFERENCES moderator(id);
+
+ALTER TABLE moderator_preference
+    ADD CONSTRAINT moderator_preference_fk_moderator
+    FOREIGN KEY (moderator)
+    REFERENCES moderator(id);
+
+ALTER TABLE moderator_subscribe_artist
+    ADD CONSTRAINT modsubartist_fk_moderator
+    FOREIGN KEY (moderator)
+    REFERENCES moderator(id);
+
+ALTER TABLE release
+    ADD CONSTRAINT release_fk_album
+    FOREIGN KEY (album)
+    REFERENCES album(id);
+
+ALTER TABLE release
+    ADD CONSTRAINT release_fk_country
+    FOREIGN KEY (country)
+    REFERENCES country(id);
+
+ALTER TABLE track
+    ADD CONSTRAINT track_fk_artist
+    FOREIGN KEY (artist)
+    REFERENCES artist(id);
+
+ALTER TABLE trm
+    ADD CONSTRAINT trm_fk_clientversion
+    FOREIGN KEY (version)
+    REFERENCES clientversion(id);
+
+ALTER TABLE trmjoin
+    ADD CONSTRAINT trmjoin_fk_track
+    FOREIGN KEY (track)
+    REFERENCES track(id);
+
+ALTER TABLE trmjoin
+    ADD CONSTRAINT trmjoin_fk_trm
+    FOREIGN KEY (trm)
+    REFERENCES trm(id);
+
+ALTER TABLE trmjoin_stat
+    ADD CONSTRAINT trmjoin_stat_fk_trmjoin
+    FOREIGN KEY (trmjoin_id)
+    REFERENCES trmjoin(id)
+    ON DELETE CASCADE;
+
+ALTER TABLE trm_stat
+    ADD CONSTRAINT trm_stat_fk_trm
+    FOREIGN KEY (trm_id)
+    REFERENCES trm(id)
+    ON DELETE CASCADE;
+
+ALTER TABLE vote_closed
+    ADD CONSTRAINT vote_closed_fk_moderation
+    FOREIGN KEY (moderation)
+    REFERENCES moderation_closed(id);
+
+ALTER TABLE vote_closed
+    ADD CONSTRAINT vote_closed_fk_moderator
+    FOREIGN KEY (moderator)
+    REFERENCES moderator(id);
+
+ALTER TABLE vote_open
+    ADD CONSTRAINT vote_open_fk_moderation
+    FOREIGN KEY (moderation)
+    REFERENCES moderation_open(id);
+
+ALTER TABLE vote_open
+    ADD CONSTRAINT vote_open_fk_moderator
+    FOREIGN KEY (moderator)
     REFERENCES moderator(id);
 
 -- vi: set ts=4 sw=4 et :
