46aedf5c70585c980006317e55a3b03d9745ebd1
Stefan Kestenholz <keschte@gmail.com>
2006-04-27 13:07:10 +0000

    * added "please help" text and "wiki link" for non-wikidocs pages * release collapse/expand script for releasebatchop * tinkered with more extensive help links from the interface using wikidocs. * re-added the checkbox for releasebatch selections

--- htdocs/scripts/collapsereleases.js
+++ htdocs/scripts/collapsereleases.js
@@ -1,11 +1,18 @@
-function CollapseRelease() {
+function CollapseReleases() {
 	
 	// ----------------------------------------------------------------------------
 	// register class/global id
 	// ---------------------------------------------------------------------------
-	this.CN = "CollapseRelease";
-	this.GID = "collapserelease";
+	this.CN = "CollapseReleases";
+	this.GID = "collapsereleases";
 	mb.log.enter(this.CN, "__constructor");
+
+
+	this.imgplus = new Image(); 
+	this.imgplus.src = "/images/es/maximize.gif";
+	this.imgminus = new Image(); 
+	this.imgminus.src = "/images/es/minimize.gif"; 
+
 
 	/**
 	 *
@@ -17,6 +24,7 @@ function CollapseRelease() {
 			var t = list[i];
 			var id = (t.id || "");
 			if (id.match(/tracks::\d+/i)) {
+				
 				// go through all the TR's of the table
 				var rows = mb.ui.getByTag("tr", t);
 				for (var j=0;j<rows.length; j++) {
@@ -40,8 +48,10 @@ function CollapseRelease() {
 					a.id = id.replace("tracks", "expand");
 					a.className = "toggle";
 					a.onfocus = function onfocus(event) { this.blur(); };
-					a.onclick = function onclick(event) { collapserelease.toggle(this); };
-					
+					a.onclick = function onclick(event) { 
+						var id = this.id.replace("expand", "tracks");
+						collapsereleases.showRelease(id); 
+					};
 					var img = document.createElement("img");
 					img.src = "/images/es/maximize.gif";
 					img.className = "toggle";
@@ -57,35 +67,48 @@ function CollapseRelease() {
 		}
 		mb.log.exit();
 	};
-
+	
+	
+	/**
+	 *
+	 */
+	this.toggleAll = function(flag) {
+		mb.log.enter(this.CN, "setupReleases");
+		var list = mb.ui.getByTag("table");
+		for (var i=0;i<list.length; i++) {
+			var t = list[i];
+			var id = (t.id || "");
+			if (id.match(/tracks::\d+/i)) {	
+				this.showRelease(id, flag);
+			}
+		}
+	};
+	
+	
 	/** 
 	 *
 	 */
-	this.toggle = function(el) {
-		mb.log.enter(this.CN, "toggle");
-		if (el) {
-			var imgplus = new Image(); 
-			imgplus.src = "/images/es/maximize.gif";
-			var imgminus = new Image(); 
-			imgminus.src = "/images/es/minimize.gif"; 
-
-			var obj, img = el.firstChild;
-			var display = "";
-			if (img.src.match("maximize")) {
-				img.src = imgminus.src;
-				display = "";
-			} else {
-				img.src = imgplus.src;
-				display = "none";
+	this.showRelease = function(id, flag) {
+		mb.log.enter(this.CN, "showRelease");
+		var obj, img, t;
+		
+		// get reference to image object.
+		if ((obj = mb.ui.get(id.replace("tracks", "expand"))) != null) {
+			img = obj.firstChild;
+		
+			if (flag == null) {
+				flag = img.src.match("maximize");
 			}
-			if ((obj = mb.ui.get(el.id.replace("expand", "releaselinks"))) != null) {
+			img.src = flag ? this.imgminus.src : this.imgplus.src;
+			var display = flag ? "" : "none";
+			
+			if ((obj = mb.ui.get(id.replace("tracks", "releaselinks"))) != null) {
 				obj.style.display = display;
 			}
-			if ((obj = mb.ui.get(el.id.replace("expand", "releaseevents"))) != null) {
+			if ((obj = mb.ui.get(id.replace("tracks", "releaseevents"))) != null) {
 				obj.style.display = display;
 			}				
-			var t,tid = el.id.replace("expand", "tracks");
-			if ((t = mb.ui.get(tid)) != null) {
+			if ((t = mb.ui.get(id)) != null) {
 				var rows = mb.ui.getByTag("tr", t);
 				for (var j=0;j<rows.length; j++) {
 					if (rows[j].className.match(/track|discid/i)) {
@@ -100,6 +123,62 @@ function CollapseRelease() {
 		}
 		mb.log.exit();
 	};
+	
+	
+	/**
+	 * This function replaces the "remove" in the release title
+	 * with a checkbox for the BatchOp form.
+	 *
+	 */
+	this.setupReleaseBatch = function() {
+		mb.log.enter(this.CN, "showRelease");
+		var obj,list = mb.ui.getByTag("table");
+		for (var i=0;i<list.length; i++) {
+			var t = list[i];
+			var id = (t.id || "");
+			if (id.match(/tracks::\d+/i)) {	
+				
+				var tagchecked = false;
+				if ((obj = mb.ui.get(id.replace("tracks", "tagchecked"))) != null) {
+					tagchecked = obj.value;
+				}
+				if ((obj = mb.ui.get(id.replace("tracks", "batchop"))) != null) {
+					var releaseid = id.replace("tracks::", "");
+					
+					var input = document.createElement("input");
+					input.id = id.replace("tracks", "batchcheckbox");
+					input.type = "checkbox";
+					input.onchange = function onclick(event) { 
+						var batchOpForm = document.forms.BatchOp;
+						var releaseid = this.id.replace("batchcheckbox::", "");
+						var fieldName = "AlbumId"+releaseid;
+						if (batchOpForm) {
+							var value = batchOpForm[fieldName].value;
+							batchOpForm[fieldName].value = (value == "off" ? "on" : "off");
+						}
+					};			
+					obj.innerHTML = "";
+					obj.appendChild(input);
+					input.checked = tagchecked;
+					input.title = tagchecked ?
+						"Deactivate this checkbox and click Update to unselect this release from the Batch Operations," :
+						"Activate this checkbox and click Update to select this release for Batch Operations.";
+				}				
+			}
+		}
+
+		// get BatchOp form, then container element (div)
+		// inside it, to add the Update button
+		if ((obj = mb.ui.get("BatchOp")) != null) {
+			if ((obj = obj.firstChild) != null) {
+				var input = document.createElement("input");
+				input.type = "submit";
+				input.name = "submit";
+				input.value = "Update";
+				obj.appendChild(input);
+			}
+		}
+	};
 
 	// exit constructor
 	mb.log.exit();
@@ -107,10 +186,15 @@ function CollapseRelease() {
 
 
 // register class...
-var collapserelease = new CollapseRelease();
+var collapsereleases = new CollapseReleases();
 mb.registerDOMReadyAction(
-	new MbEventAction(collapserelease.GID, "setupReleases", "Initialising CollapseRelease")
+	new MbEventAction(collapsereleases.GID, "setupReleases", "Initialising CollapseReleases")
 );
+
+mb.registerDOMReadyAction(
+	new MbEventAction(collapsereleases.GID, "setupReleaseBatch", "Initialising CollapseReleases")
+);
+
 
 //toggleRelease(mb.ui.get("expand::82274"));
 
