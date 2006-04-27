9f57bafa96ab97c7a048fef998c3293c95d833bd
Stefan Kestenholz <keschte@gmail.com>
2006-04-27 21:18:48 +0000

    * added comments to new javascript files * made numerous changes to the annotation feature * simplified display login in link<entity> components * css goes along with all the changes as usual * added annotation pages to the topmenu structure

--- htdocs/edit/annotation/wikitoolbar.js
+++ htdocs/edit/annotation/wikitoolbar.js
@@ -1,3 +1,25 @@
+/*----------------------------------------------------------------------------\
+|                              Musicbrainz.org                                |
+|                     Copyright (c) 2005 Stefan Kestenholz                    |
+|-----------------------------------------------------------------------------|
+| This software is provided "as is", without warranty of any kind, express or |
+| implied, including  but not limited  to the warranties of  merchantability, |
+| fitness for a particular purpose and noninfringement. In no event shall the |
+| authors or  copyright  holders be  liable for any claim,  damages or  other |
+| liability, whether  in an  action of  contract, tort  or otherwise, arising |
+| from,  out of  or in  connection with  the software or  the  use  or  other |
+| dealings in the software.                                                   |
+| - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - |
+| GPL - The GNU General Public License    http://www.gnu.org/licenses/gpl.txt |
+| Permits anyone the right to use and modify the software without limitations |
+| as long as proper  credits are given  and the original  and modified source |
+| code are included. Requires  that the final product, software derivate from |
+| the original  source or any  software  utilizing a GPL  component, such  as |
+| this, is also licensed under the GPL license.                               |
+|-----------------------------------------------------------------------------|
+| This script was adapted from trac (http://www.edgewall.com/trac/)           |
+\----------------------------------------------------------------------------*/
+
 function addWikiFormattingToolbar(textarea) {
 	if ((typeof(document["selection"]) == "undefined") && 
 		(typeof(textarea["setSelectionRange"]) == "undefined")) {
@@ -7,6 +29,9 @@ function addWikiFormattingToolbar(textarea) {
 	var toolbar = document.createElement("div");
 	toolbar.className = "wikitoolbar";
 
+	/**
+	 * Adds a button to the toolbar
+	 */
 	function addButton(id, title, fn) {
 		var a = document.createElement("a");
 		a.href = "#";
@@ -20,6 +45,9 @@ function addWikiFormattingToolbar(textarea) {
 		a.style.backgroundImage.src = i.src;
 	}
 
+	/**
+	 * Performs an action with the text of the textarea
+	 */
 	function encloseSelection(prefix, suffix) {
 		textarea.focus();
 		var start, end, sel, scrollPos, subst;
@@ -52,6 +80,7 @@ function addWikiFormattingToolbar(textarea) {
 		}
 	}
 
+	// register the actions on the toolbar
 	addButton("strong", "Bold text: '''Example'''", function() {
 		encloseSelection("'''", "'''");
 	});
@@ -64,23 +93,49 @@ function addWikiFormattingToolbar(textarea) {
 	addButton("link", "Link: [http://www.example.com/ Example]", function() {
 		encloseSelection("[", "]");
 	});
-//	not supported	
-//	addButton("code", "Code block: {{{ example }}}", function() {
-//		encloseSelection("\n{{{\n", "\n}}}\n");
-//	});
 	addButton("hr", "Horizontal rule: ----", function() {
 		encloseSelection("\n----\n", "");
 	});
 	textarea.parentNode.insertBefore(toolbar, textarea);
 }
 
-// Add the toolbar to all <textarea> elements on the page with the class
-// 'wikitext'.
-var re = /\bwikitext\b/;
-var textareas = document.getElementsByTagName("textarea");
-for (var i = 0; i < textareas.length; i++) {
-	var textarea = textareas[i];
-	if (textarea.className && re.test(textarea.className)) {
-		addWikiFormattingToolbar(textarea);
-	}
+
+/** 
+ * Wrapper class for the setup function
+ * which helps setting up the wiki toolbar
+ * in the normal musicbrainz onDOMReady
+ * chain of handlers. 
+ */
+function WikiToolBar() {
+
+	// ----------------------------------------------------------------------------
+	// register class/global id
+	// ---------------------------------------------------------------------------
+	this.CN = "WikiToolBar";
+	this.GID = "wikitoolbar";
+	mb.log.enter(this.CN, "__constructor");
+
+	/**
+	 * Add the toolbar to all <textarea> elements on the page 
+	 * with the class "wikitext"
+	 */
+	this.setup = function() {
+		var re = /\bwikitext\b/;
+		var textareas = document.getElementsByTagName("textarea");
+		for (var i = 0; i < textareas.length; i++) {
+			var textarea = textareas[i];
+			if (textarea.className && re.test(textarea.className)) {
+				addWikiFormattingToolbar(textarea);
+			}
+		}	
+	};
+
+	// exit constructor
+	mb.log.exit();
 }
+
+// instantiate, and setup the form.
+var wikitoolbar = new WikiToolBar();
+mb.registerDOMReadyAction(
+	new MbEventAction(wikitoolbar.GID, 'setup', "Setup Wiki TOolbar")
+);
