164444f3311f61c660ab5db26e2212231abe911f
Stefan Kestenholz <keschte@gmail.com>
2006-05-29 20:53:01 +0000

    tweaky.

--- htdocs/dev/selenium/mb_server/nice.js
+++ htdocs/dev/selenium/mb_server/nice.js
@@ -4,16 +4,16 @@ if ((obj = document.getElementById("suiteTable")) != null) {
 	var tr = obj.firstChild.children;
 	var j=0;
 	for (var i=0; i<tr.length;i++) {
-		if (tr[i].className == "") {
+		if (tr[i].tagName == "TR" && tr[i].className == "")  {
 			tr[i].className = (j++ % 2 == 0 ? "even" : "odd");
 		}
 	}
 }
 if ((obj = document.getElementById("testTable")) != null) {
-	var tr = obj.children[1].children;
+	var tr = obj.firstChild.children;
 	var j=0;
 	for (var i=0; i<tr.length;i++) {
-		if (tr[i].className == "") {
+		if (tr[i].tagName == "TR"  && tr[i].className == "")  {
 			tr[i].className = (j++ % 2 == 0 ? "even" : "odd");
 		}
 	}
