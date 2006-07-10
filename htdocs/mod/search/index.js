dd2131e0fe6ff6e33125c5fc687487a71c21e87c
Stefan Kestenholz <keschte@gmail.com>
2006-07-10 21:31:14 +0000

    improved version of the advanced edit search.

--- htdocs/mod/search/index.js
+++ htdocs/mod/search/index.js
@@ -26,107 +26,128 @@ function AdvancedEditSearch() {
 	this.CN = "AdvancedEditSearch";
 	this.GID = "advancedEditSearch";
 	mb.log.enter(this.CN, "__constructor");
-	
+
 	this.formref = null;
-	
-	this.imgplus = new Image(); 
+
+	this.imgplus = new Image();
 	this.imgplus.src = "/images/es/maximize.gif";
-	this.imgminus = new Image(); 
-	this.imgminus.src = "/images/es/minimize.gif"; 	
-	
+	this.imgminus = new Image();
+	this.imgminus.src = "/images/es/minimize.gif";
+
 	/**
 	 * Hide all filter types, where no selection has been made.
-	 * 
-	 */	
+	 */
 	this.setupForm = function() {
 		var field, container, s, div;
-		if ((this.formref = mb.ui.get("AdvancedEditSearch")) != null) 
-		{
+		if ((this.formref = mb.ui.get("AdvancedEditSearch")) != null) {
 			var isreset = this.formref["isreset"].value == "1";
-			var fields = ["mod_status", "automod", "mod_type", "moderator_type", 
+			var fields = ["mod_status", "automod", "mod_type", "moderator_type",
 						  "voter_type", "vote_cast", "artist_type", "orderby", "object_id",
 						  "mod_language", "minid" ];
-		
-			for (var i=0; i<fields.length; i++) 
-			{
+
+			for (var i=0; i<fields.length; i++) {
 				var fieldname = fields[i];
 				if ((field = this.formref[fieldname]) != null &&
-					(container = mb.ui.get("id_"+fieldname)) != null)
-				{
-					
+					(container = mb.ui.get("id_"+fieldname)) != null) {
+
+					var type = "";
+
 					// get content of label TD
-					var type = container.parentNode.parentNode.firstChild.innerHTML;
-					type = type.replace(/<[^>]*>/ig, "");
-					type = type.replace(/[^a-z]/ig, ""); // only accept chars a-z
-
-					div = document.createElement("div");
-					div.style.marginBottom = "5px";
-					div.style.paddingTop = "2px";
-					div.style.fontSize = "11px";
-				
-					s = [];
-					s.push('<div id="selected_');
-					s.push(fieldname);
-					s.push('">No filters set.</div>');
-					div.innerHTML = s.join("");
-					
-					container.parentNode.insertBefore(div, container);
-					container.style.marginBottom = "5px";
-
-					s = [];
-					s.push('<a href="#"  id="toggle_'+fieldname+'" onclick="');
-					s.push('advancedEditSearch.toggleField(');
-					s.push("'id_"); 
-					s.push(fieldname);
-					s.push("'");
-					s.push('); return false;" title="Show '+type+' filter">');
-					s.push('<img style="margin-left: 4px; margin-top: 4px" src="/images/es/maximize.gif" alt="" /></a>');
-
-					td = document.createElement("td");
-					td.innerHTML = s.join("");
-					td.style.padding = "0px";
-					
 					var tr = container.parentNode.parentNode;
-					tr.insertBefore(td, tr.firstChild.nextSibling);
-					
-					if (field)
-					{
-						var cv = mb.cookie.get("advsearch::"+fieldname);
-						var show = (cv == null || isreset ? false : cv == "1");
-												
-						if (field.options) {
-							if (!show) {	
-								var length = field.options.length;
-								field.size = (length > 25 ? 25 : length);
-							}
-							this.updateFilterDesc(field);
-							field.onchange = function onchange(event) {
-								advancedEditSearch.updateFilterDesc(this);
-							};
-							field.ondblclick = function ondblclick(event) {
-								this.form.submit();
-							};
-							
-						} else if (field.length) {
-							for (var j=0; j<field.length; j++) {
-								var fieldj = field[j];
-								fieldj.onclick = function onchange(event) {
-									advancedEditSearch.updateFilterDesc(this.form[this.name]);
+					var labeltd = null;
+					for (var j=0; j<tr.childNodes.length; j++) {
+						if ((tr.childNodes[j].tagName || "").toLowerCase() == "td") {
+							labeltd = tr.childNodes[j];
+							type = labeltd.innerHTML;
+							type = type.replace(/<[^>]*>/ig, "");
+							type = type.replace(/[^a-z]/ig, ""); // only accept chars a-z
+							break;
+						}
+					}
+
+					if (type != "") {
+
+						// create element which shows the selected items in the toggled state.
+						div = document.createElement("div");
+						div.style.marginBottom = "5px";
+						div.style.paddingTop = "2px";
+						div.style.fontSize = "12px";
+						s = [];
+						s.push('<div id="selected_');
+						s.push(fieldname);
+						s.push('">');
+						s.push(this.getNoFilterText(fieldname));
+						s.push('.</div>');
+						div.innerHTML = s.join("");
+						container.parentNode.insertBefore(div, container);
+						container.style.marginBottom = "5px";
+
+						// create element which shows the toggle icon
+						var toggletd = document.createElement("td");
+						s = [];
+						s.push('<a href="#"  id="toggle_');
+						s.push(fieldname);
+						s.push('" onclick="');
+						s.push('advancedEditSearch.toggleField(');
+						s.push("'id_");
+						s.push(fieldname);
+						s.push("'");
+						s.push('); return false;" title="Show ');
+						s.push(type);
+						s.push(' filter">');
+						s.push('<img style="margin-left: 4px; margin-top: 4px" src="/images/es/maximize.gif" alt="" /></a>');
+						toggletd.innerHTML = s.join("");
+						toggletd.style.padding = "0px";
+						tr.insertBefore(toggletd, labeltd.nextSibling);
+
+						// create element which allows to reset the filter
+						var cleartd = document.createElement("td");
+						s = [];
+						s.push('<a href="javascript: advancedEditSearch.clearFilter(\'');
+						s.push(fieldname);
+						s.push('\');">Clear&nbsp;filter&nbsp;&raquo;</a>');
+						cleartd.innerHTML = s.join("");
+						cleartd.style.padding = "0px";
+						tr.appendChild(cleartd);
+
+						if (field) {
+							var cv = mb.cookie.get("advsearch::"+fieldname);
+							var show = (cv == null || isreset ? false : cv == "1");
+
+							if (field.options) {
+								if (!show) {
+									var length = field.options.length;
+									field.size = (length > 25 ? 25 : length);
+								}
+								this.updateFilterDesc(field);
+								field.onchange = function onchange(event) {
+									advancedEditSearch.updateFilterDesc(this);
 								};
-								fieldj.ondblclick = function ondblclick(event) {
+								field.ondblclick = function ondblclick(event) {
 									this.form.submit();
-								};								
+								};
+
+							} else if (field.length) {
+								for (var j=0; j<field.length; j++) {
+									var fieldj = field[j];
+									fieldj.onclick = function onchange(event) {
+										advancedEditSearch.updateFilterDesc(this.form[this.name]);
+									};
+									fieldj.ondblclick = function ondblclick(event) {
+										this.form.submit();
+									};
+								}
+								this.updateFilterDesc(field);
 							}
-							this.updateFilterDesc(field);
+							if (!show) {
+								container.style.display = "none";
+								mb.ui.get("toggle_"+fieldname).checked = false;
+							} else {
+								container.style.display = "";
+								mb.ui.get("toggle_"+fieldname).checked = true;
+							}
+							this.toggleField("id_"+fieldname, show ? 1 : 0);
 						}
-						if (!show) {
-							container.style.display = "none"; 
-							mb.ui.get("toggle_"+fieldname).checked = false;
-						} else {
-							container.style.display = ""; 
-							mb.ui.get("toggle_"+fieldname).checked = true;
-						}
-						this.toggleField("id_"+fieldname, show ? 1 : 0);
 					}
 				}
 			}
@@ -134,8 +155,25 @@ function AdvancedEditSearch() {
 	};
 
 	/**
+	 * Returns the "no filter set" toggle link.
+	 *
+	 * @param 	field	the filter type
+	 */
+	this.getNoFilterText = function(fieldName) {
+		var s = [];
+		s.push('<a href="#" id="toggle_'+fieldName+'" onclick="');
+		s.push('advancedEditSearch.toggleField(');
+		s.push("'id_");
+		s.push(fieldName);
+		s.push("'");
+		s.push('); return false;">');
+		s.push('No filters set</a>');
+		return s.join("");
+	};
+
+	/**
 	 * Clears the filter given by name.
-	 * 
+	 *
 	 * @param 	field	the filter type
 	 */
 	this.clearFilter = function(fieldName) {
@@ -149,7 +187,7 @@ function AdvancedEditSearch() {
 					var fieldj = field[j];
 					var label = fieldj.nextSibling;
 					var text = (label.innerHTML || "Any");
-					fieldj.checked = (text == "Any" ||	
+					fieldj.checked = (text == "Any" ||
 									  text == "Oldest first");
 				}
 			}
@@ -157,11 +195,11 @@ function AdvancedEditSearch() {
 			this.toggleField("id_"+fieldName, false);
 		}
 	};
-	
-	
+
+
 	/**
 	 * Lists the selected value(s) of the given input element
-	 * 
+	 *
 	 * @param 	field	the filter type
 	 */
 	this.updateFilterDesc = function(field) {
@@ -171,7 +209,7 @@ function AdvancedEditSearch() {
 			if (field.options) {
 				fieldname = field.name
 				var opt = field.options;
-				for (var i=0; i<opt.length; i++) {				
+				for (var i=0; i<opt.length; i++) {
 					if (opt[i].selected) {
 						s.push(opt[i].text);
 					}
@@ -185,7 +223,7 @@ function AdvancedEditSearch() {
 					fieldj.style.marginRight = "5px";
 					if (text != "Any") {
 						if (fieldj.checked) {
-							s.push(text);	 
+							s.push(text);
 						}
 					}
 
@@ -193,50 +231,54 @@ function AdvancedEditSearch() {
 					fieldname = fieldname || fieldj.name;
 				}
 			}
-							
+
 			var obj, tmp;
 			if ((obj = mb.ui.get("selected_"+fieldname)) != null) {
 				var filter = [];
-				var td = '<td style="font-size: 12px; padding: 0px">';
-				var ws0 = '<tr>'+td;
-				var ws = '<tr><td style="padding: 0px; padding-top: 2px; padding-right: 2px">'
-					   + '<img src="/images/tdicon.gif" alt="" /></td>' + td;
-				var we = '</td></tr>'; 
+				var td = '<td style="font-size: 12px; padding: 0px 20px 0px 0px">';
+				var ws0 = '<tr>' + td;
+				var ws = '<tr class="trclass">' +
+						 '<td style="padding: 0px 3px 0px 0px;">' +
+					     '<img src="/images/tdicon.gif" alt="" style="vertical-align: top; margin-top: 2px" />' +
+					     '</td>' +
+					     td;
+				var we = '</td></tr>';
 
-				filter.push('<table style="border-collapse: collapse;">');
+				filter.push('<table style="border-collapse: collapse" class="listing">');
 				if (s.length > 0) {
 					for (var i=0; i<s.length; i++) {
-						tmp = ws.replace("trclass", (i % 2 == 0 ? "even" : "odd"));
+						tmp = ws.replace("trclass", (i % 2 == 0 ? "odd" : "even"));
 						tmp = tmp.replace("tdicon", "misc/bullet");
 						filter.push(tmp);
-						filter.push(s[i]); 
+						filter.push(s[i]);
 						filter.push(we);
 					}
-					
+
 				} else {
 					filter.push(ws0);
-					filter.push("No filters set.");
+					filter.push(this.getNoFilterText(fieldname));
+					filter.push('.');
 				}
 				filter.push(we);
 				filter.push('</table>');
-				
-				var out = [];
-				out.push('<table style="border-collapse: collapse"><tr valign="top" style="padding: 0px;"><td style="padding: 0px; width: 300px">');
-				out.push(filter.join(""));
-				out.push('</td><td style="padding: 0px;">');
-				out.push('<a href="javascript: advancedEditSearch.clearFilter(\''+fieldname+'\');">Clear&nbsp;filter&nbsp;&raquo;</a>');
-				out.push('</td></tr></table>');				
-				
-				// alert(out.join("\n"));
-				obj.innerHTML = out.join("");
+
+				var s = [];
+				s.push('<table style="border-collapse: collapse">');
+				s.push('<tr valign="top" style="padding: 0px;">');
+				s.push('<td style="padding: 0px; width: 300px">');
+				s.push(filter.join(""));
+				s.push('</td>');
+				s.push('</tr>');
+				s.push('</table>');
+				obj.innerHTML = s.join("");
 			}
-		}	
+		}
 	};
 
 	/**
 	 * Show/Hide the div containing the input fields for
 	 * the filter type given by field.
-	 * 
+	 *
 	 * @param 	field	the filter type
 	 *
 	 */
@@ -246,20 +288,24 @@ function AdvancedEditSearch() {
 			if (flag == null) {
 				flag = (obj.style.display == "none");
 			}
-			obj.style.display = (flag ? "" : "none"); 
-		
+			obj.style.display = (flag ? "" : "none");
+
 			mb.cookie.set("advsearch::"+field.replace("id_", ""), flag ? 1 : 0);
 			this.updateToggleIcon(field.replace("id_", "toggle_"), flag ? 1 : 0);
 		}
-	}; 
-	
+
+		if ((obj = mb.ui.get(field.replace("id_", "selected_"))) != null) {
+			obj.style.display = (!flag ? "" : "none");
+		}
+	};
+
 	/**
 	 * Lists the selected value(s) of the given input element
-	 * 
+	 *
 	 * @param 	field	the filter type
 	 */
 	this.updateToggleIcon = function(id, flag) {
-		var a, img;	
+		var a, img;
 		if ((a = mb.ui.get(id)) != null) {
 			a.title = a.title.replace(/$Show|Hide/g, flag ? "Show" : "Hide");
 			img = a.firstChild
