9244b0a9f669d0163f5656e2a5a2885369347de0
Oliver Charles <oliver.g.charles@googlemail.com>
2008-05-13 10:14:58 +0000

    Added support for the blog on the landing page, and moved away from using the Main.pm controller (didn't understand the purpores of the Root controller when I started)

--- catalyst/lib/musicbrainz/Controller/Root.pm
+++ catalyst/lib/musicbrainz/Controller/Root.pm
@@ -7,6 +7,7 @@ use parent 'Catalyst::Controller';
 # We need this to specify the MusicBrainz perl codebase
 use lib "/home/musicbrainz/blah/TemplateToolkit/cgi-bin";
 
+# Import MusicBrainz libraries
 use DBDefs;
 
 #
@@ -16,59 +17,85 @@ use DBDefs;
 __PACKAGE__->config->{namespace} = '';
 
 =head1 NAME
-
 musicbrainz::Controller::Root - Root Controller for musicbrainz
 
 =head1 DESCRIPTION
-
-[enter your description here]
+This controller handles application wide logic for the MusicBrainz website.
 
 =head1 METHODS
-
 =cut
 
-=head2 default
-
+=head2 index
+Render the standard MusicBrainz welcome page, which is mainly static, other than the blog feed.
 =cut
 
-sub index : Path Args(0) {
-    my ( $self, $c ) = @_;
+sub index : Path Args(0)
+{
+    use MusicBrainz::Server::Replication ':replication_type';
+    require MusicBrainz::Server::NewsFeed;
 
-    # Hello World
-    $c->response->body( $c->welcome_message );
+    my ($self, $c) = @_;
+
+    $c->stash->{server_details} = {
+	is_slave_db => &DBDefs::REPLICATION_TYPE == RT_SLAVE,
+	staging_server => &DBDefs::DB_STAGING_SERVER,
+    };
+
+    # Load the blog for the sidebar
+    # 
+    my $feed = MusicBrainz::Server::NewsFeed->new(
+	url => 'http://blog.musicbrainz.org/index-excerpt.xml',
+	update_interval => 5 * 60,
+	max_items => 3);
+    
+    $feed->Load() if defined $feed;
+
+    # Process the items to a template friendly data structure
+    #
+    my $item = undef;    
+    $c->stash->{blog} = [];
+
+    foreach $item ($feed->GetItems()) {
+	push @{ $c->stash->{blog} }, {
+	    title => $item->GetTitle(),
+	    description => $item->GetDescription(),
+	    date_time => $item->GetDateTimeString(),
+	    link => $item->GetLink(),
+	};
+    }
+
+    $c->stash->{template} = 'main/index.tt';
 }
 
-sub default : Path {
-    my ( $self, $c ) = @_;
-    $c->response->body( 'Page not found' );
-    $c->response->status(404);
+=head2 default
+Handle any pages not matched by a specific controller path. In our case, this means serving a 404 error page.
+=cut
+
+sub default : Path
+{
+    my ($self, $c) = @_;
     
+    $c->response->body( 'Page not found' );
+    $c->response->status(404);    
 }
 
 =head2 end
-
 Attempt to render a view, if needed. This will also set up some global variables in the 
 context containing important information about the server used on the majority of templates.
-
 =cut 
 
 sub end : ActionClass('RenderView') {
     my ($self, $c) = @_;
 
-    $c->stash->{server_details} = {
-	version => &DBDefs::VERSION,
-    };
+    $c->stash->{server_details}->{version} = &DBDefs::VERSION;
 }
 
 =head1 AUTHOR
-
-Catalyst developer
+Oliver Charles <oliver.g.charles@googlemail.com>
 
 =head1 LICENSE
-
 This library is free software, you can redistribute it and/or modify
 it under the same terms as Perl itself.
 
 =cut
-
 1;
