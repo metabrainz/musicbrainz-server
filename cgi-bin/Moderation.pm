6a1324006af4e07e82dcc56cc222fb4b9b809474
Aurélien Mino <aurelien.mino@gmail.com>
2008-07-03 09:08:30 +0000

    No more popup for adding note, Part 2.

--- cgi-bin/Moderation.pm
+++ cgi-bin/Moderation.pm
@@ -1516,7 +1516,7 @@ sub InsertNote
 	my $self = shift;
 	require MusicBrainz::Server::ModerationNote;
 	my $notes = MusicBrainz::Server::ModerationNote->new($self->{DBH});
-	$notes->Insert($self, @_);
+	$notes->Insert($self->GetId, @_);
 }
 
 # Links to the Vote class
