77b3befbca911431afea62898a3a9be87b36f165
Niklas Berglund <niklas@asdasd.net>
2008-07-10 21:59:02 +0000

    updated vh_httpd.conf.default

--- admin/sql/updates/discography.sql
+++ admin/sql/updates/discography.sql
@@ -2,33 +2,7 @@
 
 BEGIN;
 
-CREATE TABLE artist_tag_raw
-(
-    artist              INTEGER NOT NULL,
-    tag                 INTEGER NOT NULL,
-    moderator           INTEGER NOT NULL
-);
 
-CREATE TABLE release_tag_raw
-(
-    release             INTEGER NOT NULL,
-    tag                 INTEGER NOT NULL,
-    moderator           INTEGER NOT NULL
-);
-
-CREATE TABLE track_tag_raw
-(
-    track               INTEGER NOT NULL,
-    tag                 INTEGER NOT NULL,
-    moderator           INTEGER NOT NULL
-);
-
-CREATE TABLE label_tag_raw
-(
-    label               INTEGER NOT NULL,
-    tag                 INTEGER NOT NULL,
-    moderator           INTEGER NOT NULL
-);
 
 CREATE TABLE collection_info
 (
@@ -83,32 +57,17 @@ CREATE TABLE collection_has_release_join
 
 
 
-CREATE INDEX artist_tag_raw_idx_artist ON artist_tag_raw (artist);
-CREATE INDEX artist_tag_raw_idx_tag ON artist_tag_raw (tag);
-CREATE INDEX artist_tag_raw_idx_moderator ON artist_tag_raw (moderator);
-
-CREATE INDEX release_tag_raw_idx_release ON release_tag_raw (release);
-CREATE INDEX release_tag_raw_idx_tag ON release_tag_raw (tag);
-CREATE INDEX release_tag_raw_idx_moderator ON release_tag_raw (moderator);
-
-CREATE INDEX track_tag_raw_idx_track ON track_tag_raw (track);
-CREATE INDEX track_tag_raw_idx_tag ON track_tag_raw (tag);
-CREATE INDEX track_tag_raw_idx_moderator ON track_tag_raw (moderator);
-
-CREATE INDEX label_tag_raw_idx_label ON label_tag_raw (label);
-CREATE INDEX label_tag_raw_idx_tag ON label_tag_raw (tag);
-CREATE INDEX label_tag_raw_idx_moderator ON label_tag_raw (moderator);
 
 -- an unique index made out of all the fields in the collection_has_release_join table. used to not allow duplicates of tuples
 CREATE UNIQUE INDEX collection_has_release_join_combined_index ON collection_has_release_join (id, collection_info, album);
+CREATE UNIQUE INDEX collection_ignore_release_join_combined_index ON collection_ignore_release_join (id, collection_info, album);
+CREATE UNIQUE INDEX collection_discography_artist_join_combined_index ON collection_discography_artist_join (id, collection_info, artist);
+CREATE UNIQUE INDEX collection_watch_artist_join_index ON collection_watch_artist_join (id, collection_info, artist);
 
 
 
 
-ALTER TABLE artist_tag_raw ADD CONSTRAINT artist_tag_raw_pkey PRIMARY KEY (artist, tag, moderator);
-ALTER TABLE release_tag_raw ADD CONSTRAINT release_tag_raw_pkey PRIMARY KEY (release, tag, moderator);
-ALTER TABLE track_tag_raw ADD CONSTRAINT track_tag_raw_pkey PRIMARY KEY (track, tag, moderator);
-ALTER TABLE label_tag_raw ADD CONSTRAINT label_tag_raw_pkey PRIMARY KEY (label, tag, moderator);
+
 ALTER TABLE collection_info ADD CONSTRAINT collection_info_pkey PRIMARY KEY (id);
 ALTER TABLE collection_ignore_time_range ADD CONSTRAINT collection_ignore_time_range_pkey PRIMARY KEY (id);
 ALTER TABLE collection_watch_artist_join ADD CONSTRAINT collection_watch_artist_join_pkey PRIMARY KEY (id);
