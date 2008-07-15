2e84c4968f2b14e9bbac3b9f3354535d46b10b95
Aurélien Mino <aurelien.mino@gmail.com>
2008-07-15 15:34:18 +0000

    Started integration of color-coded votes, from Jugdish greasemonkey script: http://userscripts.org/scripts/show/9838.

--- htdocs/scripts/jsvoting.js
+++ htdocs/scripts/jsvoting.js
@@ -47,3 +47,54 @@ function toggleNoteText(editid)
 		}
 	}
 }
+
+
+function captureVoteChange(input) {
+	var results;
+	if (input.name && (results = input.name.match(/^rowid([0-9]+)$/))) {
+		var n = input.id.match(/approve/) ? document.getElementById(input.id.replace(/approve/,"cancel")) : (input.id.match(/cancel/) ? document.getElementById(input.id.replace(/cancel/,"approve")) : null);
+		if (n) n.checked = false;
+		updateVoteColor(input);
+	}
+}
+
+function updateVoteColor(input) {
+	var ancestor = input;
+	while (ancestor.className != "vote" && ancestor.parentNode)
+		ancestor = ancestor.parentNode;
+	if (ancestor.className == "vote")
+		setVoteColor(ancestor);
+}
+
+function setVoteColor(cell) {
+	var inputs = cell.getElementsByTagName("input");
+	var bgColor = "white";
+	for (var i = 0; i < inputs.length; i++) {
+		if (inputs[i].checked) {
+			switch (inputs[i].value) {
+				case "approve":
+					bgColor = "#00a650";
+					inputs[i].parentNode.style["borderColor"] = "white";
+					break;
+				case "yes":
+					bgColor = "#eeffee";
+					inputs[i].parentNode.style["borderColor"] = "#88ff88";
+					break;
+				case "no":
+					bgColor = "#ffeeee";
+					inputs[i].parentNode.style["borderColor"] = "#ff8888";
+					break;
+				case "abs":
+					bgColor = "#ffffcc";
+					inputs[i].parentNode.style["borderColor"] = "#eeee66";
+					break;
+				case "cancel":
+					bgColor = "#ff0000";
+					inputs[i].parentNode.style["borderColor"] = "white";
+					break;
+			}
+		} else
+			inputs[i].parentNode.style["borderColor"] = "transparent";
+	}
+	cell.style["backgroundColor"] = bgColor;
+}
