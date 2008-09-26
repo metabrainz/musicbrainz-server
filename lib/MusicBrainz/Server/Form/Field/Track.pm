1d3f8445cf368d1ddfff0f89f66deda453a00f02
Oliver Charles <oliver.g.charles@googlemail.com>
2008-09-26 16:05:30 +0000

    Ability to get the data back out of the compound track field

--- lib/MusicBrainz/Server/Form/Field/Track.pm
+++ lib/MusicBrainz/Server/Form/Field/Track.pm
@@ -62,10 +62,31 @@ sub validate
 {
     my $self = shift;
 
-    $self->sub_form->validate(scalar $self->form->params)
-        or return undef;
+    my $sub_form_validated = $self->sub_form->validate(scalar $self->form->params);
+
+    return $sub_form_validated
+        unless $sub_form_validated;
 
     return 1;
+}
+
+=head2
+
+Construct a value (that will be accessed by controllers when the form
+is valid) from the user input.
+
+For this field, we simply return a hash containing the keys: number,
+title and duration.
+
+=cut
+
+sub input_to_value
+{
+    my $self  = shift;
+
+    my %values = map { $_->name => $_->value } @{ $self->sub_form->fields };
+
+    $self->value(\%values);
 }
 
 1;
