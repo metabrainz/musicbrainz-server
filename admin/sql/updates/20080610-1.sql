f470f2065f449a61ee332c42d406d811eb39622d
Lukáš Lalinský <lalinsky@gmail.com>
2008-10-02 10:05:42 +0000

    Switch the order, it's much faster to insert data without indexes.

--- admin/sql/updates/20080610-1.sql
+++ admin/sql/updates/20080610-1.sql
@@ -22,15 +22,14 @@ CREATE TABLE track_meta
 
 ALTER TABLE albummeta  ADD COLUMN lastupdate          TIMESTAMP WITH TIME ZONE DEFAULT NOW();
 
+-- Initial population
+INSERT INTO artist_meta (id) SELECT id FROM artist;
+INSERT INTO label_meta (id) SELECT id FROM label;
+INSERT INTO track_meta (id) SELECT id FROM track;
+
 -- primary keys
 ALTER TABLE artist_meta ADD CONSTRAINT artist_meta_pkey PRIMARY KEY(id);
 ALTER TABLE label_meta ADD CONSTRAINT label_meta_pkey PRIMARY KEY(id);
 ALTER TABLE track_meta ADD CONSTRAINT track_meta_pkey PRIMARY KEY(id);
-
--- Initial population
-
-INSERT INTO artist_meta (id) SELECT id FROM artist;
-INSERT INTO label_meta (id) SELECT id FROM label;
-INSERT INTO track_meta (id) SELECT id FROM track;
 
 COMMIT;
