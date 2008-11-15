19c35e2964561aabd69c802d883603b73d4d977a
Oliver Charles <oliver.g.charles@googlemail.com>
2008-11-15 18:36:38 +0000

    Fix feed model to load correctly through a proxy server

--- lib/MusicBrainz/Server/Model/Feeds.pm
+++ lib/MusicBrainz/Server/Model/Feeds.pm
@@ -5,6 +5,7 @@ use warnings;
 
 use URI;
 use XML::Feed;
+use LWP::UserAgent;
 
 sub get_cached
 {
@@ -14,20 +15,30 @@ sub get_cached
     my $feed = MusicBrainz::Server::Cache->get("feed-id-${feed_id}");
     if ($feed)
     {
-        # HACK Force XML::Feed::RSS to load. 
+        # HACK Force XML::Feed::RSS to load.
         require XML::Feed;
         my $fake = _fake_feed();
         XML::Feed->parse(\$fake);
-
-        return $feed;
     }
     else
     {
-        $feed = XML::Feed->parse(URI->new($uri));
-        MusicBrainz::Server::Cache->set("feed-id-${feed_id}", $feed);
+	# Loading is a bit complicated, but we have to ensure we fetch the
+	# feed using any user defined proxies...
+	my $ua = LWP::UserAgent->new;
+	$ua->env_proxy;
 
-        return $feed;
+	my $res = $ua->get($uri);
+	if ($res->is_success)
+	{
+	    my $content = $res->content;
+	    $feed = XML::Feed->parse(\$content)
+		or die XML::Feed->errstr;
+
+	    MusicBrainz::Server::Cache->set("feed-id-${feed_id}", $feed);
+	}
     }
+
+    return $feed;
 }
 
 sub _fake_feed
