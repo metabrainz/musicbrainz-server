8486d9416306ec5430cec08043381bd6bdb7901c
Oliver Charles <oliver.g.charles@googlemail.com>
2008-11-19 14:19:44 +0000

    Fleshed out an interface for creating relationships

--- lib/MusicBrainz/Server/Controller/Relate.pm
+++ lib/MusicBrainz/Server/Controller/Relate.pm
@@ -68,6 +68,37 @@ sub create : Chained('entity') PathPart('to') Args(2)
 
     die "$dest_type is not a valid entity type"
         unless MusicBrainz::Server::LinkEntity->IsValidType($dest_type eq 'release' ? 'album' : $dest_type);
+
+    $c->stash->{source} = $c->stash->{entity};
+    $c->stash->{dest  } = $c->model(ucfirst $dest_type)->load($dest_id);
+
+    my $source = $c->stash->{source};
+    my $dest   = $c->stash->{dest};
+
+    die "Cannot relate an entity to itself"
+	if $source->id eq $dest->id;
+
+    my $form = MusicBrainz::Server::Form->new(
+	profile => {
+	    required => {
+		begin => '+MusicBrainz::Server::Form::Field::Date',
+		end   => '+MusicBrainz::Server::Form::Field::Date',
+		type  => 'Select',
+	    },
+	    optional => {
+		edit_note  => 'TextArea',
+		additional => 'Checkbox',
+		co         => 'Checkbox',
+		executive  => 'Checkbox',
+		guest      => 'Checkbox',
+		orchestra  => 'Select',
+		instrument => 'Select',
+		vocals     => 'Select',
+	    },
+	},
+    );
+
+    $c->stash->{form} = $form;
 }
 
 1;
