75e5cfa1a74e844380164951144fdfcce8311157
Aurélien Mino <aurelien.mino@gmail.com>
2008-11-21 17:34:01 +0000

    Uncomment code.

--- admin/sql/PopulateMetaTables.sql
+++ admin/sql/PopulateMetaTables.sql
@@ -4,14 +4,14 @@ BEGIN;
 
 select fill_album_meta();
 
---TRUNCATE TABLE artist_meta;
---INSERT INTO artist_meta (id, lastupdate) SELECT id, NULL FROM artist;
+TRUNCATE TABLE artist_meta;
+INSERT INTO artist_meta (id, lastupdate) SELECT id, NULL FROM artist;
 
---TRUNCATE TABLE label_meta;
---INSERT INTO label_meta (id, lastupdate) SELECT id, NULL FROM label;
+TRUNCATE TABLE label_meta;
+INSERT INTO label_meta (id, lastupdate) SELECT id, NULL FROM label;
 
---TRUNCATE table track_meta;
---INSERT INTO track_meta (id) SELECT id FROM track;
+TRUNCATE table track_meta;
+INSERT INTO track_meta (id) SELECT id FROM track;
 
 COMMIT;
 
