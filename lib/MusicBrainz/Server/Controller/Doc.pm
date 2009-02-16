a75583bbae574dc183330ac11597ea970a913e17
Oliver Charles <oliver.g.charles@googlemail.com>
2009-02-16 00:43:03 +0000

    Add support for fetching doc pages without the full layout

--- lib/MusicBrainz/Server/Controller/Doc.pm
+++ lib/MusicBrainz/Server/Controller/Doc.pm
@@ -5,15 +5,28 @@ use warnings;
 
 use base 'MusicBrainz::Server::Controller';
 
-sub show : Path('')
+sub load : Chained('/') PathPart('doc') CaptureArgs(1)
 {
     my ($self, $c, $page_id) = @_;
+    $c->stash->{page} = $c->model('Documentation')->fetch_page($page_id);
+}
 
-    my $page          = $c->model('Documentation')->fetch_page($page_id);
-    $c->stash->{page} = $page;
+sub show : Chained('load') PathPart('')
+{
+    my ($self, $c) = @_;
 
+    my $page = $c->stash->{page};
     $c->stash->{template} = $page->{success} ? 'doc/page.tt'
-                          :                    'doc/error.tt';
+                          : 'doc/error.tt';
+}
+
+sub bare : Chained('load') PathPart
+{
+    my ($self, $c) = @_;
+
+    my $page = $c->stash->{page};
+    $c->stash->{template} = $page->{success} ? 'doc/bare.tt'
+                          : 'doc/bare_error.tt';
 }
 
 1;
