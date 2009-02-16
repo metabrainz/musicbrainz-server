4f4150358fe7d5a83e1d945b2a559bec8a59dd67
Oliver Charles <oliver.g.charles@googlemail.com>
2009-02-16 00:41:13 +0000

    Merge Brian's work on client-side table sorting

--- root/static/scripts/release.js
+++ root/static/scripts/release.js
@@ -1,6 +1,7 @@
 /* Small helpers for viewing releases */
 
 $(document).ready(function() {
+    /* Toggle Disc IDs via the show/hide Disc IDs link. */
     $('#toggle_discids').click(function() {
         $('#release_discids').slideToggle("normal", function() {
             $('#toggle_discids').text(this.style.display == 'none' ?
@@ -9,9 +10,28 @@ $(document).ready(function() {
         });
         return false;
     });
-
+    /* Toggle artists via the show/hide artists link. */
     $('#toggle_artists').click(function() {
-        $('.release_tracks .artist').toggle();
-        return false;
+        var showHideLink = $('#toggle_artists');
+        showHideLink.unbind("click");
+        showHideLink.text("Hide Artists");
+        showHideLink.removeAttr("href");
+        showHideLink.css("cursor","pointer"); 
+        showHideLink.bind("click", function() {
+            if(showHideLink.text() == "Show Artists") {
+                showHideLink.text("Hide Artists");
+            }
+            else {
+               showHideLink.text("Show Artists");
+            }
+            $('#col-artist, .release_tracks .artist' ).toggle();
+        });
+        showHideLink.trigger("click");
+    });
+    /* Make the table sortable. */
+    $("thead").toggle();
+    $(".release_tracks").tablesorter({
+        textExtraction: "complex",
+        headers: { 4: { sorter: false}}
     });
 });
