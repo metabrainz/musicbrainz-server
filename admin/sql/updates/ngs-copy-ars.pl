31515254dbcefb47e461847dac0c0e3c9523b429
Robert Kaye <rob@eorbit.net>
2009-06-27 04:29:00 +0000

    Check in an intermediary version of these scripts.

--- admin/sql/updates/ngs-copy-ars.pl
+++ admin/sql/updates/ngs-copy-ars.pl
@@ -11,15 +11,28 @@ use Sql;
 use Getopt::Long;
 
 my $move;
-my $result = GetOptions("move", \$move);
+my $remove;
+my $result = GetOptions("move", \$move,
+                        "remove", \$remove);
 
-my $src_type = shift;     # release_group-url
-my $dest_type = shift;    # release-url
+if (($remove && scalar(@ARGV) < 2) || (!$remove && scalar(@ARGV) < 3))
+{
+    print "Usage: ngs-copy-ars.pl [-m | -r] <src_entity_0-src_entity_1> [dest_entity_0-dest_entity_1]\n\n";
+    print "Options\n";
+    print "    -m     Move the ARs (Copy then remove)\n";
+    print "    -r     Remove the ARs (No copy, just remove)\n";
+    exit(-1);
+}
+
 my $link_type_name = shift; # "cover art link"
+my $src_type = shift;       # "release_group-release_group"
+my $dest_type;              # "release-release"
+$dest_type = shift if (!$remove);    
 
 my ($src_entity0, $src_entity1) = split /-/, $src_type;
-my ($dest_entity0, $dest_entity1) = split /-/, $dest_type;
+my ($dest_entity0, $dest_entity1) = split /-/, $dest_type if (!$remove);
 
+# TODO: Make this script ambidextrous
 die if ($src_entity0 ne 'release_group');
 
 my $mb = MusicBrainz->new;
@@ -29,125 +42,132 @@ my $sql = Sql->new($mb->dbh);
 $sql->Begin;
 eval {
 
-    print localtime() . " Load release groups into ram\n";
-    # Load the release_group => release map into memory
-    my %rg_release_map;
-    if ($sql->Select("SELECT id, release_group FROM release"))
-    {
-        while (1) 
-        {
-            my $row = $sql->NextRowHashRef or last;
-            $rg_release_map{$row->{release_group}} = [] if (!defined $rg_release_map{$row->{release_group}});
-            push @{$rg_release_map{$row->{release_group}}}, $row->{id};
-        }
-        $sql->Finish;
-    }
-
-    print localtime() . " create new link_type\n";
- 
     # Find the current link type id
     my $link_type_id = $sql->SelectSingleValue("SELECT id 
                                                   FROM link_type 
                                                  WHERE name = ?
                                                    AND entitytype0 = ?
                                                    AND entitytype1 = ?", $link_type_name, $src_entity0, $src_entity1);
+    die "Cannot find link type $link_type_name!\n" if (!defined $link_type_id);
 
-    # Create a new link_type and link_type_attribute_type
-    my $new_link_type_id = $sql->SelectSingleValue("INSERT INTO link_type (parent, childorder, gid, entitytype0, entitytype1, 
-                                                         name, description, linkphrase, rlinkphrase, 
-                                                         shortlinkphrase, priority) 
-                                      SELECT parent, childorder, generate_uuid_v4(), ?, ?, 
-                                             name, description, linkphrase, rlinkphrase, 
-                                             shortlinkphrase, priority from link_type 
-                                       WHERE name = ? 
-                                         AND entitytype0 = ? 
-                                         AND entitytype1 = ? RETURNING id", $dest_entity0, $dest_entity1, $link_type_name, 
-                                                               $src_entity0, $src_entity1);
-    $sql->Do("INSERT INTO link_type_attribute_type (link_type, attribute_type, min, max)
-                      SELECT ?, attribute_type, min, max 
-                        FROM link_type_attribute_type
-                       WHERE link_type = ?", $new_link_type_id, $link_type_id);
-
-    print localtime() . " copy ARs\n";
-    my $total = $sql->SelectSingleValue("SELECT count(*) FROM l_".$src_entity0."_".$src_entity1 . " AS l, link 
-                                          WHERE link.link_type = ? 
-                                            AND link.id = l.link", $link_type_id);
-    my $count;
-    my %links;
-    if ($sql->Select("SELECT * 
-                        FROM l_".$src_entity0."_".$src_entity1." AS t, link 
-                       WHERE t.link = link.id
-                         AND link.link_type = ?", $link_type_id))
+    if (!$remove)
     {
-        my $link_id;
-        while (1) 
+        print localtime() . " Load release groups into ram\n";
+        # Load the release_group => release map into memory
+        my %rg_release_map;
+        if ($sql->Select("SELECT id, release_group FROM release"))
         {
-            my $row = $sql->NextRowHashRef or last;
-
-            # create a unique key that represents an link type
-            my $key = sprintf "%d-%d-%d-%d-%d-%d-%d-%d", $new_link_type_id, 
-                                                         $row->{begindate_year} || 0, 
-                                                         $row->{begindate_month} || 0,
-                                                         $row->{begindate_day} || 0,
-                                                         $row->{enddate_year} || 0,
-                                                         $row->{enddate_month} || 0,
-                                                         $row->{enddate_day} || 0,
-                                                         $row->{attributecount} || 0;
-
-            # Now append attribute ids 
-            my $attrs = $sql->SelectSingleColumnArray("SELECT attribute_type 
-                                                         FROM link_type_attribute_type 
-                                                        WHERE link_type = ?
-                                                     ORDER BY attribute_type", $link_type_id);
-            foreach my $attr (@{$attrs})
+            while (1) 
             {
-                $key .= sprintf "-%d", $attr;
+                my $row = $sql->NextRowHashRef or last;
+                $rg_release_map{$row->{release_group}} = [] if (!defined $rg_release_map{$row->{release_group}});
+                push @{$rg_release_map{$row->{release_group}}}, $row->{id};
             }
-
-            if (exists $links{$key})
-            {
-                $link_id = $links{$key};
-            }
-            else
-            {
-                # Insert the proper link row
-                $link_id = $sql->SelectSingleValue("INSERT INTO link (link_type, begindate_year, begindate_month, begindate_day,
-                                                                enddate_year, enddate_month, enddate_day, attributecount) 
-                                                         VALUES (?, ?, ?, ?, ?, ?, ?, ?) 
-                                                      RETURNING id",
-                                                    $new_link_type_id,
-                                                    $row->{begindate_year},
-                                                    $row->{begindate_month},
-                                                    $row->{begindate_day},
-                                                    $row->{enddate_year},
-                                                    $row->{enddate_month},
-                                                    $row->{enddate_day},
-                                                    $row->{attributecount});
-                $links{$key} = $link_id;
-                $sql->Do("INSERT INTO link_attribute (link, attribute_type) 
-                             SELECT ?, attribute_type 
-                               FROM link_attribute
-                              WHERE link = ?", $link_id, $row->{link});
-            }
-
-            my $releases = $rg_release_map{$row->{entity0}};
-            foreach my $release (@{$releases})
-            {
-                $sql->Do("INSERT INTO l_" . $dest_entity0 . "_" . $dest_entity1. 
-                         " (link, entity0, entity1, editpending) VALUES (?, ?, ?, ?)",
-                                           $link_id, 
-                                           $release,
-                                           $row->{entity1},
-                                           $row->{editpending});
-            }
-            $count++;
-            printf localtime() . " %d%% complete ($count of $total)\n", ($count * 100 / $total) if ($count % 1000 == 0);
+            $sql->Finish;
         }
-        $sql->Finish;
-    }
-    print localtime() . " Copied $count ARs.\n";
 
-    if ($move)
+        print localtime() . " create new link_type\n";
+     
+
+        # Create a new link_type and link_type_attribute_type
+        my $new_link_type_id = $sql->SelectSingleValue("INSERT INTO link_type (parent, childorder, gid, entitytype0, entitytype1, 
+                                                             name, description, linkphrase, rlinkphrase, 
+                                                             shortlinkphrase, priority) 
+                                          SELECT parent, childorder, generate_uuid_v4(), ?, ?, 
+                                                 name, description, linkphrase, rlinkphrase, 
+                                                 shortlinkphrase, priority from link_type 
+                                           WHERE name = ? 
+                                             AND entitytype0 = ? 
+                                             AND entitytype1 = ? RETURNING id", $dest_entity0, $dest_entity1, $link_type_name, 
+                                                                   $src_entity0, $src_entity1);
+        $sql->Do("INSERT INTO link_type_attribute_type (link_type, attribute_type, min, max)
+                          SELECT ?, attribute_type, min, max 
+                            FROM link_type_attribute_type
+                           WHERE link_type = ?", $new_link_type_id, $link_type_id);
+
+        print localtime() . " copy ARs\n";
+        my $total = $sql->SelectSingleValue("SELECT count(*) FROM l_".$src_entity0."_".$src_entity1 . " AS l, link 
+                                              WHERE link.link_type = ? 
+                                                AND link.id = l.link", $link_type_id);
+
+        die "There are no ARs to copy/move.\n" if ($total == 0);
+        my $count = 0;
+        my %links;
+        if ($sql->Select("SELECT * 
+                            FROM l_".$src_entity0."_".$src_entity1." AS t, link 
+                           WHERE t.link = link.id
+                             AND link.link_type = ?", $link_type_id))
+        {
+            my $link_id;
+            while (1) 
+            {
+                my $row = $sql->NextRowHashRef or last;
+
+                # create a unique key that represents an link type
+                my $key = sprintf "%d-%d-%d-%d-%d-%d-%d-%d", $new_link_type_id, 
+                                                             $row->{begindate_year} || 0, 
+                                                             $row->{begindate_month} || 0,
+                                                             $row->{begindate_day} || 0,
+                                                             $row->{enddate_year} || 0,
+                                                             $row->{enddate_month} || 0,
+                                                             $row->{enddate_day} || 0,
+                                                             $row->{attributecount} || 0;
+
+                # Now append attribute ids 
+                my $attrs = $sql->SelectSingleColumnArray("SELECT attribute_type 
+                                                             FROM link_type_attribute_type 
+                                                            WHERE link_type = ?
+                                                         ORDER BY attribute_type", $link_type_id);
+                foreach my $attr (@{$attrs})
+                {
+                    $key .= sprintf "-%d", $attr;
+                }
+
+                if (exists $links{$key})
+                {
+                    $link_id = $links{$key};
+                }
+                else
+                {
+                    # Insert the proper link row
+                    $link_id = $sql->SelectSingleValue("INSERT INTO link (link_type, begindate_year, begindate_month, begindate_day,
+                                                                    enddate_year, enddate_month, enddate_day, attributecount) 
+                                                             VALUES (?, ?, ?, ?, ?, ?, ?, ?) 
+                                                          RETURNING id",
+                                                        $new_link_type_id,
+                                                        $row->{begindate_year},
+                                                        $row->{begindate_month},
+                                                        $row->{begindate_day},
+                                                        $row->{enddate_year},
+                                                        $row->{enddate_month},
+                                                        $row->{enddate_day},
+                                                        $row->{attributecount});
+                    $links{$key} = $link_id;
+                    $sql->Do("INSERT INTO link_attribute (link, attribute_type) 
+                                 SELECT ?, attribute_type 
+                                   FROM link_attribute
+                                  WHERE link = ?", $link_id, $row->{link});
+                }
+
+                my $releases = $rg_release_map{$row->{entity0}};
+                foreach my $release (@{$releases})
+                {
+                    $sql->Do("INSERT INTO l_" . $dest_entity0 . "_" . $dest_entity1. 
+                             " (link, entity0, entity1, editpending) VALUES (?, ?, ?, ?)",
+                                               $link_id, 
+                                               $release,
+                                               $row->{entity1},
+                                               $row->{editpending});
+                }
+                $count++;
+                printf localtime() . " %d%% complete ($count of $total)\n", ($count * 100 / $total) if ($count % 1000 == 0);
+            }
+            $sql->Finish;
+        }
+        print localtime() . " Copied $count ARs.\n";
+    }
+
+    if ($move || $remove)
     {
         print localtime() . " Removing old ARs and AR type\n";
         # Save a list of rows in the link table will become unused once we delete the rows for a given type
@@ -180,7 +200,8 @@ eval {
 
         print localtime() . " Done.\n";
     }
-    $sql->Commit;
+#$sql->Commit;
+    $sql->Rollback;
 };
 if ($@) {
     my $err = $@;
--- admin/sql/updates/ngs-deconflate-ars.sh
+++ admin/sql/updates/ngs-deconflate-ars.sh
@@ -1,26 +1,22 @@
 #!/bin/sh
 
-# TODO
-# - make copy/move ambidextrous
-# - create separate copy/move/remove scripts
-
 # The AR conversion script move all ARs to release_group-release_group. If that is the final destination
-# for a given AR type, use the ngs-noop-ars.pl script just so we can track what has been taken care of and what hasn't.
+# for a given AR type, use the # no action required script just so we can track what has been taken care of and what hasn't.
 
 # album-album -----------------------------------------------
 # command               Name                                   From                           To
-ngs-noop-ars.pl         'covers and versions'     
-ngs-rem-ars.pl          'first album release'     
-ngs-move-ars.pl         'remaster'                             release_group-release_group    release-release 
-ngs-noop-ars.pl         'remixes' 
-ngs-noop-ars.pl         'mashes up'
-ngs-noop-ars.pl         'remix'
+# no action required    'covers and versions'     
+ngs-copy-ars.pl -r      'first album release'     
+ngs-copy-ars.pl -m      'remaster'                             release_group-release_group    release-release 
+# no action required    'remixes' 
+# no action required    'mashes up'
+# no action required    'remix'
                         'compilations'
                         'DJ-mix'                               release_group-release-group    release-release         
                         'live performance'                     release_group-release-group    release-release         
-ngs-noop-ars.pl         'cover'
+# no action required    'cover'
 <custom>                'transl-tracklisting'
-ngs-rem-ars.pl          'part of set'
+ngs-copy-ars.pl -r      'part of set'
 
 # album-artist -----------------------------------------------
                         'performance '
@@ -45,15 +41,15 @@ ngs-copy-ars.pl         'live sound'                           artist-release_gr
 ngs-copy-ars.pl         'mix'                                  artist-release_group           artist-release
 ngs-copy-ars.pl         'recording'                            artist-release_group           artist-release
                         'misc'
-ngs-noop-ars.pl         'legal representation'
-ngs-noop-ars.pl         'booking'
+# no action required    'legal representation'
+# no action required    'booking'
                         'artists and repertoire'
                         'creative direction'
 ngs-copy-ars.pl         'art direction'                        artist-release_group           artist-release
 ngs-copy-ars.pl         'design/illustration'                  artist-release_group           artist-release
 ngs-copy-ars.pl         'graphic design'                       artist-release_group           artist-release
 ngs-copy-ars.pl         'photography'                          artist-release_group           artist-release
-ngs-noop-ars.pl         'travel'
+# no action required    'travel'
                         'publishing'
                         'merchandise'
 ngs-copy-ars.pl         'mix-DJ'                               artist-release_group           artist-release
@@ -77,7 +73,7 @@ ngs-copy-ars.pl         'chorus master'                        artist-release_gr
 
 # album-url   ------------------------------------------------
                         'discography'
-ngs-noop-ars.pl         'review'
+# no action required         'review'
                         'get the music'
 ngs-copy-ars.pl         'purchase for mail-order'              release_group-url      release-url
 ngs-copy-ars.pl         'purchase for download'                release_group-url      release-url
@@ -86,10 +82,10 @@ ngs-copy-ars.pl         'download for free'                    release_group-url
                         'wikipedia'
                         'discogs'
                         'musicmoz'
-ngs-noop-ars.pl         'IMDb'
+# no action required         'IMDb'
                         'Affiliate links'
 <custom>                'amazon asin'                          release_group-url      release-url
 ngs-copy-ars.pl         'creative commons licensed download'   release_group-url      release-url
 ngs-copy-ars.pl         'cover art link'                       release_group-url      release-url
-ngs-noop-ars.pl         'ibdb'
-ngs-noop-ars.pl         'iobdb'
+# no action required    'ibdb'
+# no action required    'iobdb'
