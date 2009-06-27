903e105bfc9e0bddb7dffa4d89c9230ae056a30b
Lukáš Lalinský <lalinsky@gmail.com>
2009-06-27 11:53:04 +0000

    Re-run ./admin/GenerateSQLScripts.pl

--- admin/sql/DropIndexes.sql
+++ admin/sql/DropIndexes.sql
@@ -47,7 +47,6 @@ DROP INDEX track_idx_artist_credit;
 DROP INDEX track_idx_name;
 DROP INDEX track_idx_recording;
 DROP INDEX track_idx_tracklist;
-DROP INDEX track_idx_tracklist;
 DROP INDEX track_name_idx_name;
 DROP INDEX track_name_idx_page;
 DROP INDEX tracklist_idx_trackcount;
