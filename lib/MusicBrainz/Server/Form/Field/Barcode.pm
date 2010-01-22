58205f244adc4029eed953d48d742b1175beb8f8
Oliver Charles <oliver.g.charles@googlemail.com>
2010-01-22 22:58:41 +0000

    Enable validation for barcode fields.
    Fixes MBS-251

--- lib/MusicBrainz/Server/Form/Field/Barcode.pm
+++ lib/MusicBrainz/Server/Form/Field/Barcode.pm
@@ -5,17 +5,10 @@ use MusicBrainz::Server::Validation;
 
 extends 'HTML::FormHandler::Field::Text';
 
-=pod
-
-This validation routine is currently too strict, so it has been disabled for
-now. FIXME!
-
 apply ([
    { check => sub { MusicBrainz::Server::Validation::IsValidEAN(shift) },
      message => 'This is not a valid barcode',
    }
 ]);
-
-=cut
 
 1;
