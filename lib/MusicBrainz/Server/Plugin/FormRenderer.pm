19f981548002a3319934dc36c2e7c3635041949e
Kuno Woudt <warp@frob.nl>
2010-02-02 19:13:19 +0000

    See review 518

--- lib/MusicBrainz/Server/Plugin/FormRenderer.pm
+++ lib/MusicBrainz/Server/Plugin/FormRenderer.pm
@@ -108,7 +108,7 @@ sub label
     {
         return $self->h->label({
             id => 'label-' . $field->id,
-            for => $field->id,
+            for => 'id-' . $field->id,
             class => $class || undef
             %$attrs
         }, $label);
@@ -202,3 +202,23 @@ sub artist_credit_editor
 }
 
 1;
+
+=head1 COPYRIGHT
+
+Copyright (C) 2009-2010  MetaBrainz Foundation
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+=cut
