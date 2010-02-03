ab714b922cac169fc7543c251361a473262ab30b
Kuno Woudt <warp@frob.nl>
2010-02-03 15:33:05 +0000

    MBS-350, review 523, view delete artist edit type

--- lib/MusicBrainz/Server/Edit/Artist/Delete.pm
+++ lib/MusicBrainz/Server/Edit/Artist/Delete.pm
@@ -6,7 +6,7 @@ use MusicBrainz::Server::Constants qw( $EDIT_ARTIST_DELETE );
 extends 'MusicBrainz::Server::Edit::Generic::Delete';
 
 sub edit_type { $EDIT_ARTIST_DELETE }
-sub edit_name { "Delete artist" }
+sub edit_name { "Remove artist" }
 sub _delete_model { 'Artist' }
 
 
