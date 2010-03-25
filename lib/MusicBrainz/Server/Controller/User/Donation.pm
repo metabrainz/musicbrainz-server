5e1bf5c8b70ca301c2f13c0f6aa982c93c9371f1
warp <kuno@frob.nl>
2010-03-25 09:49:25 +0100

    MBS-76, add donation check page.

---
+++ lib/MusicBrainz/Server/Controller/User/Donation.pm
@@ -0,0 +1,59 @@
+package MusicBrainz::Server::Controller::User::Donation;
+use Moose;
+
+BEGIN { extends 'MusicBrainz::Server::Controller' };
+
+sub view : Chained('/user/base') PathPart('donation') RequireAuth
+{
+    my ($self, $c) = @_;
+
+    my $user = $c->stash->{user};
+
+    $c->detach('/error_403')
+        unless (defined $c->user && $c->user->id == $user->id);
+
+    my $nag = 1;
+    $nag = 0 if ($user->is_nag_free || $user->is_auto_editor || $user->is_bot || 
+                 $user->is_relationship_editor || $user->is_wiki_transcluder);
+
+    my $days = 0.0;
+    if ($nag)
+    {
+        use LWP::Simple;
+        use URI::Escape;
+        $days = get('http://metabrainz.org/cgi-bin/nagcheck_days?moderator=' . uri_escape($self->GetName));
+        if ($days =~ /\s*([-01]+),([-0-9.]+)\s*/) 
+        {
+            $nag = $1;
+            $days = $2;
+        }
+    }
+
+    $c->stash(
+        nag => $nag,
+        days => $days,
+        template => 'user/donation.tt',
+    );
+}
+
+1;
+
+=head1 COPYRIGHT
+
+Copyright (C) 2010 MetaBrainz Foundation
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+=cut
