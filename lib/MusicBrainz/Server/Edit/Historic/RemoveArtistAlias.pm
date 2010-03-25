ad8bc6cb0d7c6270996b16f996212700eed57330
Oliver Charles <oliver.g.charles@googlemail.com>
2010-03-25 19:36:49 +0000

    Migrate MOD_REMOVE_ARTISTALIAS

---
+++ lib/MusicBrainz/Server/Edit/Historic/RemoveArtistAlias.pm
@@ -0,0 +1,24 @@
+package MusicBrainz::Server::Edit::Historic::RemoveArtistAlias;
+use Moose;
+
+extends 'MusicBrainz::Server::Edit::Historic::NGSMigration';
+
+sub ngs_class { 'MusicBrainz::Server::Edit::Artist::DeleteAlias' }
+sub edit_type { 14 }
+sub edit_name { 'Remove artist alias' }
+
+augment 'upgrade' => sub {
+    my $self = shift;
+    return {
+        entity_id => $self->artist_id,
+        alias_id  => $self->row_id,
+        name      => $self->previous_value
+    }
+};
+
+sub deserialize_previous_value { my $self = shift; return shift; }
+sub deserialize_new_value      { my $self = shift; return shift; }
+
+no Moose;
+__PACKAGE__->meta->make_immutable;
+1;
