61af562935e66da3da16c51aced11b92332ec725
Oliver Charles <oliver.g.charles@googlemail.com>
2010-03-29 15:20:50 +0100

    Migrate EDIT_TRACK_NAME

--- lib/MusicBrainz/Server/Constants.pm
+++ lib/MusicBrainz/Server/Constants.pm
@@ -105,6 +105,7 @@ Readonly our $EDIT_URL_EDIT => 101;
 Readonly our $EDIT_PUID_DELETE => 113;
 
 Readonly our $EDIT_HISTORIC_EDIT_RELEASE_NAME => 201;
+Readonly our $EDIT_HISTORIC_EDIT_TRACKNAME    => 204;
 
 =head1 NAME
 
