03971e6f7af1f01e796eb699a9436befc8e23315
Pavan Chander <pchander@gmail.com>
2010-03-29 22:08:48 -0400

    WIP: Implementing edit countdown

--- lib/MusicBrainz/Server/Plugin/UserDate.pm
+++ lib/MusicBrainz/Server/Plugin/UserDate.pm
@@ -2,6 +2,7 @@ package MusicBrainz::Server::Plugin::UserDate;
 
 use strict;
 use warnings;
+use DateTime;
 
 use base 'Template::Plugin';
 
@@ -30,6 +31,25 @@ sub format {
     }
 
     return $dt->strftime($format);
+}
+
+sub countdown
+{
+    my ($self, $future) = @_;
+
+    my $now = DateTime->now;
+    return 'N/A' unless $future > $now;
+
+    my $diff = $future->subtract_datetime($now);
+    my $delta = $now->delta_days($future)->delta_days;
+
+    my $dfn = $self->format($future);
+
+    if ($delta > 0) {
+        return 'Expires in <dfn title="' . $dfn . '">' . $delta . '</dfn> days.';
+    } else {
+        return 'Expires in ' . $diff->hours . ' hours, and ' . $diff->minutes . ' minutes.';
+    }
 }
 
 1;
