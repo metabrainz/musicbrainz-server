6041dadbe645019b748dc8de0199152c6343f479
Oliver Charles <oliver.g.charles@googlemail.com>
2010-05-25 13:28:35 +0100

    Upgrade MOD_REMOVE_LINK_ATTR to NGS

---
+++ lib/MusicBrainz/Server/Edit/Relationship/RemoveLinkAttribute.pm
@@ -0,0 +1,21 @@
+package MusicBrainz::Server::Edit::Relationship::RemoveLinkAttribute;
+use Moose;
+use MooseX::Types::Structured qw( Dict );
+use MooseX::Types::Moose qw( Str Int );
+use MusicBrainz::Server::Constants qw( $EDIT_RELATIONSHIP_REMOVE_LINK_ATTRIBUTE );
+
+extends 'MusicBrainz::Server::Edit';
+
+sub edit_name { 'Remove relationship attribute' }
+sub edit_type { $EDIT_RELATIONSHIP_REMOVE_LINK_ATTRIBUTE }
+
+has '+data' => (
+    isa => Dict[
+        name        => Str,
+        description => Str,
+        id          => Int
+    ]
+);
+
+no Moose;
+__PACKAGE__->meta->make_immutable;
