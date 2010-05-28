ef9e06ce439a2adde06c2e395ab3a902eff9cad8
Oliver Charles <oliver.g.charles@googlemail.com>
2010-05-28 16:07:40 +0100

    Upgrade MOD_EDIT_RELEASE_GROUP_NAME to NGS

---
+++ lib/MusicBrainz/Server/Edit/Historic/EditReleaseGroupName.pm
@@ -0,0 +1,27 @@
+package MusicBrainz::Server::Edit::Historic::EditReleaseGroupName;
+use Moose;
+
+extends 'MusicBrainz::Server::Edit::Historic::NGSMigration';
+with 'MusicBrainz::Server::Edit::Historic::NoSerialization';
+
+sub edit_name { 'Edit release group name' }
+sub edit_type { 65 }
+sub ngs_class { 'MusicBrainz::Server::Edit::ReleaseGroup::Edit' }
+
+augment 'upgrade' => sub
+{
+    my ($self) = @_;
+
+    return {
+        entity_id => $self->row_id,
+        old => {
+            name => $self->previous_value,
+        },
+        new => {
+            name => $self->new_value
+        }
+    }
+};
+
+no Moose;
+__PACKAGE__->meta->make_immutable;
