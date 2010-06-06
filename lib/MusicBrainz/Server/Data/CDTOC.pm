e10287abecbf0c0bbfb3915f73c34009f1ec4f9b
Oliver Charles <oliver.g.charles@googlemail.com>
2010-06-06 22:40:18 +0100

    Fey conversion of Data::CDTOC

--- lib/MusicBrainz/Server/Data/CDTOC.pm
+++ lib/MusicBrainz/Server/Data/CDTOC.pm
@@ -1,43 +1,29 @@
 package MusicBrainz::Server::Data::CDTOC;
-
 use Moose;
+use Method::Signatures::Simple;
+
 use MusicBrainz::Schema qw( schema );
 
 extends 'MusicBrainz::Server::Data::FeyEntity';
 with 'MusicBrainz::Server::Data::Role::Subobject';
 
-sub _build_table { schema->table('cdtoc') }
+method _build_table  { schema->table('cdtoc') }
+method _entity_class { 'MusicBrainz::Server::Entity::CDTOC' }
 
-sub _table
-{
-    return 'cdtoc';
-}
-
-sub _columns
-{
-    return 'id, discid, freedbid, trackcount, leadoutoffset, trackoffset';
-}
-
-sub _column_mapping
+method _column_mapping
 {
     return {
-        id => 'id',
-        discid => 'discid',
-        freedbid => 'freedbid',
-        track_count => 'trackcount',
+        id             => 'id',
+        discid         => 'discid',
+        freedbid       => 'freedbid',
+        track_count    => 'trackcount',
         leadout_offset => 'leadoutoffset',
-        track_offset => 'trackoffset',
+        track_offset   => 'trackoffset',
     };
 }
 
-sub _entity_class
+method get_by_discid ($discid)
 {
-    return 'MusicBrainz::Server::Entity::CDTOC';
-}
-
-sub get_by_discid
-{
-    my ($self, $discid) = @_;
     my @result = values %{$self->_get_by_keys($self->table->column("discid"),
                                               $discid)};
     return $result[0];
