5a642673e2699886b63befec2afadd3a40d819a7
Oliver Charles <oliver.g.charles@googlemail.com>
2010-08-11 20:41:48 +0100

    Minor fix to the collections-to-lists upgrade

--- admin/sql/updates/ngs-20100802-collections-to-lists.sql
+++ admin/sql/updates/ngs-20100802-collections-to-lists.sql
@@ -8,6 +8,6 @@ ALTER TABLE list_release RENAME COLUMN collection TO list;
 ALTER INDEX collection_idx_gid RENAME TO list_idx_gid;
 ALTER INDEX collection_idx_name RENAME TO list_idx_name;
 
-ALTER INDEX collection_idx_editor RENAME TO list_idx_editor;
+CREATE INDEX list_idx_editor ON list (editor);
 
 COMMIT;
