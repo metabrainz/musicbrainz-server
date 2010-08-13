8e02474cd0b72921fb7fa447021e6369d934d053
Oliver Charles <oliver.g.charles@googlemail.com>
2010-08-13 11:57:44 +0000

    Yet more test data

--- script/webservice_test_data.pl
+++ script/webservice_test_data.pl
@@ -19,6 +19,11 @@ my $releasegroups = [
     '961c672c-7a3f-30c1-9e44-4e3ab02affd5', # Make it hot, sampled on Music:Response - Surrender
     '9b5006e5-b276-3a05-bcdd-8d986842320b', # House of the Holy on Atlantic
     '3e38adc0-bb7d-39d1-a811-7ce6db5d869d', # Recipe for hate, published by Atlantic
+    'd3cad1a9-9173-3f9f-b853-bd0852ceae1a', # Future sound of Budapest, has SKC & Bratwa for artist/artist AR
+    'a4d2a86c-bbd6-352b-b9fa-f9da86df842c', # Something on MCA to get a recording/label AR
+    'a8c946f5-8d26-3534-bab7-37c57eebbc1c', # Should have recording url ARs
+    'cde61708-3be3-31ad-ba61-06af6af59565',
+    '04ba5879-e3ac-3cba-8a69-ee7da8ad8c4c', # These provide recording/release ARs
 ];
 
 my $cmd = "$root/release-group-sql-dump.pl $root/../t/sql/webservice.sql";
