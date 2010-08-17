d43d059e1f55a226f255d6e79b9e07c6fcffad47
Oliver Charles <oliver.g.charles@googlemail.com>
2010-08-17 18:24:54 +0100

    Added the /ws/1/rating controller
    Has support for single and multiple rating submissions, and
    fetching ratings

---
+++ lib/MusicBrainz/Server/Controller/WS/1/Rating.pm
@@ -0,0 +1,83 @@
+package MusicBrainz::Server::Controller::WS::1::Rating;
+use Moose;
+BEGIN { extends 'MusicBrainz::Server::Controller' }
+
+use aliased 'MusicBrainz::Server::WebService::XMLSerializerV1';
+use aliased 'MusicBrainz::Server::WebService::Serializer::XML::1::List';
+
+use MusicBrainz::Server::Data::Utils qw( type_to_model );
+
+use MusicBrainz::XML::Generator;
+
+has 'serializer' => (
+    is => 'ro',
+    default => sub { XMLSerializerV1->new }
+);
+
+has 'gen' => (
+    is => 'ro',
+    default => sub { MusicBrainz::XML::Generator->new }
+);
+
+sub rating : Path('/ws/1/rating')
+{
+    my ($self, $c) = @_;
+    $c->authenticate({}, 'webservice');
+
+    if ($c->req->method eq 'POST') {
+        if (exists $c->req->params->{entity}) {
+            # Looks like a single entity rating submission
+            my ($id, $type, $rating) = (
+                map {
+                    $c->req->params->{$_}
+                } qw( id entity rating )
+            );
+
+            my $model = $c->model( type_to_model($type) );
+            my $entity = $model->get_by_gid($id);
+
+            $model->ratings->update($c->user->id, $entity->id, $rating * 20);
+        }
+        else {
+            my @batch;
+
+            for(my $count = 0;; $count++) {
+		my $entity = $c->req->params->{"entity.$count"};
+		my $id = $c->req->params->{"id.$count"};
+		my $rating = $c->req->params->{"rating.$count"};
+
+		last if (!$entity || !$id || !$rating) || @batch >= 20;
+
+		push @batch, { entity => $entity, id => $id, rating => $rating * 20 };
+            }
+
+            for my $submission (@batch) {
+                my $model = $c->model(type_to_model($submission->{entity}));
+                my $entity = $model->get_by_gid($submission->{id});
+                $model->rating->update($c->user->id, $entity->id, $submission->{rating});
+            }
+        }
+
+        $c->res->content_type($self->serializer->mime_type . '; charset=utf-8');
+        $c->res->body($self->serializer->xml( '' ));
+    }
+    else {
+        my ($id, $type) = ($c->req->query_params->{id}, $c->req->query_params->{entity});
+
+        my $model = $c->model( type_to_model($type) );
+        my $entity = $model->get_by_gid($id);
+        $model->rating->load_user_ratings($c->user->id, $entity);
+
+        $c->res->content_type($self->serializer->mime_type . '; charset=utf-8');
+        $c->res->body(
+            $self->serializer->xml(
+                $entity->user_rating ? $self->gen->user_rating(int($entity->user_rating / 20)) : ''
+            )
+        );
+    }
+}
+
+__PACKAGE__->meta->make_immutable;
+1;
+
+
