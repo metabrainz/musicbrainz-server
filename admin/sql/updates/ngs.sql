c86d8e2c80b302e5efaf9cac32cff45309a5f920
Oliver Charles <oliver.g.charles@googlemail.com>
2010-09-21 17:29:23 +0100

    Add the 'other' gender

--- admin/sql/updates/ngs.sql
+++ admin/sql/updates/ngs.sql
@@ -11,7 +11,8 @@ INSERT INTO script_language SELECT * FROM public.script_language;
 
 INSERT INTO gender (id, name) VALUES
     (1, 'Male'),
-    (2, 'Female');
+    (2, 'Female'),
+    (3, 'Other');
 
 INSERT INTO artist_type (id, name) VALUES
     (1, 'Person'),
