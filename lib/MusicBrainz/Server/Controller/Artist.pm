6cfa8742ad1a1a323980344bf68cc4e21fe120e6
Oliver Charles <oliver.g.charles@googlemail.com>
2010-09-29 12:07:50 +0100

    Display ISRCs on the recordings list

--- lib/MusicBrainz/Server/Controller/Artist.pm
+++ lib/MusicBrainz/Server/Controller/Artist.pm
@@ -293,7 +293,9 @@ sub recordings : Chained('load')
         $c->stash( template => 'artist/recordings.tt' );
     }
 
+    $c->model('ISRC')->load_for_recordings(@$recordings);
     $c->model('ArtistCredit')->load(@$recordings);
+
     $c->stash(
         recordings => $recordings,
         show_artists => scalar grep {
