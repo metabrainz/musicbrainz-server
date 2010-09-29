b26bb3ce6ae1ebf749aa2ba9434ca0e53754388e
warp <kuno@frob.nl>
2010-09-29 07:53:13 +0200

    Add an empty label to the release when it doesn't have any labels, so that label inputs are rendered in the release editor.

--- lib/MusicBrainz/Server/ControllerBase/ReleaseEditor.pm
+++ lib/MusicBrainz/Server/ControllerBase/ReleaseEditor.pm
@@ -245,7 +245,6 @@ sub _load_release
     $c->model('ReleaseLabel')->load($release);
     $c->model('Label')->load(@{ $release->labels });
     $c->model('ReleaseGroupType')->load($release->release_group);
-
     $c->model('MediumFormat')->load($release->all_mediums);
 }
 
@@ -401,7 +400,7 @@ sub _edit_release_labels
                     );
             }
         }
-        else
+        elsif ($new_label->{label_id} && $new_label->{catalog_number})
         {
             # Add ReleaseLabel
             $self->$edit($c,
@@ -629,6 +628,24 @@ sub create_edits
     );
 
     return $release;
+}
+
+sub load
+{
+    my ($self, $c, $wizard, $release) = @_;
+
+    $release = inner();
+
+    if (!$release->label_count)
+    {
+        $release->add_label(
+            MusicBrainz::Server::Entity::ReleaseLabel->new(
+                label => MusicBrainz::Server::Entity::Label->new
+            )
+        );
+    }
+
+    $wizard->initialize($release);
 }
 
 sub associate_recordings
