b359925108fdc2a5a96745e68ef0c0319aa8dded
Oliver Charles <oliver.g.charles@googlemail.com>
2010-10-08 15:37:17 +0100

    Toggle between artist and VA releases and release groups

--- lib/MusicBrainz/Server/Data/Release.pm
+++ lib/MusicBrainz/Server/Data/Release.pm
@@ -179,6 +179,8 @@ sub find_by_track_artist
     my ($self, $artist_id, $limit, $offset) = @_;
     my $query = "SELECT " . $self->_columns . "
                  FROM " . $self->_table . "
+                 JOIN artist_credit_name release_acn
+                   ON release_acn.artist_credit = release.artist_credit
                  WHERE release.id IN (
                      SELECT release FROM medium
                          JOIN track tr
@@ -186,11 +188,12 @@ sub find_by_track_artist
                          JOIN artist_credit_name acn
                          ON acn.artist_credit = tr.artist_credit
                      WHERE acn.artist = ?)
+                  AND release_acn.artist != ?
                  ORDER BY date_year, date_month, date_day, musicbrainz_collate(name.name)
                  OFFSET ?";
     return query_to_list_limited(
         $self->c->dbh, $offset, $limit, sub { $self->_new_from_row(@_) },
-        $query, $artist_id, $offset || 0);
+        $query, $artist_id, $artist_id, $offset || 0);
 }
 
 sub find_for_various_artists
