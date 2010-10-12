e71ad18772245375a6141135df5a71a038d30abc
Oliver Charles <oliver.g.charles@googlemail.com>
2010-10-12 10:42:26 +0100

    Updated constraints for aliases to allow 1 per locale, and unique aliases per entity (over all locales)

--- admin/sql/CreateIndexes.sql
+++ admin/sql/CreateIndexes.sql
@@ -6,7 +6,8 @@ CREATE INDEX artist_idx_name ON artist (name);
 CREATE INDEX artist_idx_sortname ON artist (sortname);
 
 CREATE INDEX artist_alias_idx_artist ON artist_alias (artist);
-CREATE UNIQUE INDEX artist_alias_idx_name_artist ON artist_alias (name, artist, locale);
+CREATE UNIQUE INDEX artist_alias_idx_locale_artist ON artist_alias (artist, locale);
+CREATE UNIQUE INDEX artist_alias_idx_name ON artist_alias (artist, name);
 
 CREATE INDEX artist_credit_name_idx_artist ON artist_credit_name (artist);
 
@@ -111,7 +112,8 @@ CREATE INDEX label_idx_name ON label (name);
 CREATE INDEX label_idx_sortname ON label (sortname);
 
 CREATE INDEX label_alias_idx_label ON label_alias (label);
-CREATE UNIQUE INDEX label_alias_idx_name_label ON label_alias (name, label, locale);
+CREATE UNIQUE INDEX label_alias_idx_locale_label ON label_alias (label, locale);
+CREATE UNIQUE INDEX label_alias_idx_name ON label_alias (label, name);
 
 CREATE UNIQUE INDEX label_name_idx_name ON label_name (name);
 CREATE INDEX label_name_idx_page ON label_name (page_index(name));
@@ -187,7 +189,8 @@ CREATE INDEX work_idx_name ON work (name);
 CREATE INDEX work_idx_artist_credit ON work (artist_credit);
 
 CREATE INDEX work_alias_idx_work ON work_alias (work);
-CREATE UNIQUE INDEX work_alias_idx_name_work ON work_alias (name, work, locale);
+CREATE UNIQUE INDEX work_alias_idx_locale_work ON work_alias (work, locale);
+CREATE UNIQUE INDEX work_alias_idx_name ON work_alias (work, name);
 
 CREATE UNIQUE INDEX work_name_idx_name ON work_name (name);
 CREATE INDEX work_name_idx_page ON work_name (page_index(name));
