7e2b8784c7d56346e9f03631e06dec94788d6283
Oliver Charles <oliver.g.charles@googlemail.com>
2010-10-13 12:10:34 +0100

    Move the dynamic JavaScript to a controller that doesn't use Root's begin/end

---
+++ lib/MusicBrainz/Server/Controller/JS.pm
@@ -0,0 +1,19 @@
+package MusicBrainz::Server::Controller::JS;
+use Moose;
+BEGIN { extends 'Catalyst::Controller' }
+
+sub begin : Private {}
+sub end : ActionClass('RenderView') { }
+
+sub js_text_strings : Path('/text.js') {
+    my ($self, $c) = @_;
+    $c->res->content_type('text/javascript');
+    $c->stash->{template} = 'scripts/text_strings.tt';
+}
+
+sub js_unit_tests : Path('/unit_tests') {
+    my ($self, $c) = @_;
+    $c->stash->{template} = 'scripts/unit_tests.tt';
+}
+
+1;
