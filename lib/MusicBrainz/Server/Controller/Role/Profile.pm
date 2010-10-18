6900ed875e7033735496ffb945ee2ee7e34df741
Oliver Charles <oliver.g.charles@googlemail.com>
2010-10-18 13:18:29 +0100

    Log displays seconds, not milliseconds

--- lib/MusicBrainz/Server/Controller/Role/Profile.pm
+++ lib/MusicBrainz/Server/Controller/Role/Profile.pm
@@ -27,7 +27,7 @@ role {
                 my ($depth, $name, $duration) = @$stat;
                 if ($name eq 'request' && $duration > $threshold) {
                     $c->log->warn(
-                        sprintf("Slow request (%.3fms): %s", $duration, $c->req->uri)
+                        sprintf("Slow request (%.3fs): %s", $duration, $c->req->uri)
                     );
                 }
             }
