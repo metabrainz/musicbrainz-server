469b2530c5aa5522ad6f0c4f3c1f16f084ee9e4f
Aurélien Mino <aurelien.mino@gmail.com>
2010-10-18 21:20:39 +0200

    Ran GenerateSQLScripts.pl and updated ngs-20101015-fix-coverart.sql so the FK is recreated with the ON CASCADE clause.

--- admin/sql/updates/ngs-20101015-fix-coverart.sql
+++ admin/sql/updates/ngs-20101015-fix-coverart.sql
@@ -3,6 +3,14 @@ BEGIN;
 
 ALTER TABLE release_coverart ALTER COLUMN coverfetched DROP DEFAULT;
 
+ALTER TABLE release_coverart DROP CONSTRAINT release_coverart_fk_id;
+
+ALTER TABLE release_coverart
+   ADD CONSTRAINT release_coverart_fk_id
+   FOREIGN KEY (id)
+   REFERENCES release(id)
+   ON DELETE CASCADE;
+
 INSERT INTO release_coverart (id)
     SELECT r.id 
     FROM release r
