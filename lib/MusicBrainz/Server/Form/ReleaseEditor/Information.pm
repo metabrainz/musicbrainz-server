84936cb937d9626853e3c2e255342a3289f9d6d7
warp <kuno@frob.nl>
2010-11-08 16:17:36 +0100

    MBS-1024, replace "Show full lists" button for languages and scripts with option groups.

--- lib/MusicBrainz/Server/Form/ReleaseEditor/Information.pm
+++ lib/MusicBrainz/Server/Form/ReleaseEditor/Information.pm
@@ -40,12 +40,20 @@ sub options_country_id        { shift->_select_all('Country') }
 sub options_language_id {
     my ($self) = @_;
 
+    # group list of languages in <optgroups>.
+    # most frequently used languages have hardcoded value 2.
+
+    # FIXME: optgroups need to go through gettext. --warp.
+
+    my $frequent = 2;
+
     my @sorted = sort { $a->{label} cmp $b->{label} } map {
         {
             'value' => $_->id,
             'label' => $_->{name},
             'class' => 'language',
-            'data-frequency' => $_->{frequency},
+            'optgroup' => $_->{frequency} eq $frequent ? 'Frequently used' : 'Other',
+            'optgroup_order' => $_->{frequency} eq $frequent ? 1 : 2,
         }
     } $self->ctx->model('Language')->get_all;
 
@@ -55,12 +63,20 @@ sub options_language_id {
 sub options_script_id {
     my ($self) = @_;
 
+    # group list of scripts in <optgroups>.
+    # most frequently used scripts have hardcoded value 4.
+
+    # FIXME: optgroups need to go through gettext. --warp.
+
+    my $frequent = 4;
+
     return [ map {
         {
             'value' => $_->id,
             'label' => $_->{name},
             'class' => 'script',
-            'data-frequency' => $_->{frequency},
+            'optgroup' => $_->{frequency} eq $frequent ? 'Frequently used' : 'Other',
+            'optgroup_order' => $_->{frequency} eq $frequent ? 1 : 2,
         }
     } $self->ctx->model('Script')->get_all ];
 }
