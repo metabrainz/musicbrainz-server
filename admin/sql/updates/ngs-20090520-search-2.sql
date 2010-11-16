fa58891abca077d7a355d22a86594ae0a25d0bb0
Aurélien Mino <aurelien.mino@gmail.com>
2010-11-16 01:20:39 +0100

    Remove obsolete NGS updates scripts.

--- admin/sql/updates/ngs-20090520-search-2.sql
+++
@@ -1,5 +0,0 @@
-CREATE INDEX artist_name_idx_name_txt ON artist_name USING gin(to_tsvector('mb_simple', name));
-CREATE INDEX label_name_idx_name_txt ON label_name USING gin(to_tsvector('mb_simple', name));
-CREATE INDEX release_name_idx_name_txt ON release_name USING gin(to_tsvector('mb_simple', name));
-CREATE INDEX track_name_idx_name_txt ON track_name USING gin(to_tsvector('mb_simple', name));
-CREATE INDEX work_name_idx_name_txt ON work_name USING gin(to_tsvector('mb_simple', name));
--- admin/sql/updates/ngs-20100606-edit_url.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE edit_url
-(
-    edit                INTEGER NOT NULL, -- PK
-    url                 INTEGER NOT NULL -- PK
-);
