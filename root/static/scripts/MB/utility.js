c0cdc74f5e01d11e21ccaf50f2acb0236ab348a8
warp <kuno@frob.nl>
2010-11-22 14:08:36 +0100

    MBS-246, missing functions required for GuessCase re-introduced to MB/utility.js. MBS-246, initialize GuessCase/Flags.js properly.

--- root/static/scripts/MB/utility.js
+++ root/static/scripts/MB/utility.js
@@ -57,6 +57,9 @@ MB.utility.fullWidthConverter = function (inputString) {
     return newString.reverse ().join("");
 };
 
+MB.utility.isArray  = function(o) { return (o instanceof Array    || typeof o == "array"); };
+MB.utility.isString = function(o) { return (o instanceof String   || typeof o == "string"); };
+MB.utility.isNullOrEmpty = function(o) { return (!o || o == ""); };
 
 MB.utility.template = function(str) {
     var self = MB.Object();
