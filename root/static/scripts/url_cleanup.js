49796c1dd65bf314b18da8a5ef935d4e8b1bdcc2
Oliver Charles <oliver.g.charles@googlemail.com>
2010-11-29 14:22:15 +0000

    Add the remaining URL type regular expressions

--- root/static/scripts/url_cleanup.js
+++ root/static/scripts/url_cleanup.js
@@ -1,14 +1,3 @@
-/*
- *
-// amazon:		new RegExp("^(http://)?([^/]+\.)?amazon\.(com|ca|co\.uk|fr|at|de|co\.jp|jp)","i"),
-// archive:	new RegExp("^(http://)?([^/]+\.)?archive\.org/.*\.(jpg|jpeg|png|gif)$","i"),
-// cdbaby:		new RegExp("^(http://)?([^/]+\.)?cdbaby\.(com|name)","i"),
-// jamendo:	new RegExp("^(http://)?([^/]+\.)?jamendo\.com","i"),
-// encyclopedisque:	new RegExp("^(http://)?([^/]+\.)?encyclopedisque\.fr/images/.*\.jpg","i"),
-// manjdisc:	new RegExp("^(http://)?([^/]+\.)?mange-disque\.tv/(fs/md_|fstb/tn_md_|info_disque\.php3\\?dis_code=)[0-9]+","i"),
-// lyricwiki:      new RegExp("^(http://)?([^/]+\.)?lyrics\.wikia\.com", "i"),
-*/
-
 var cleanups = {
     wikipedia: {
         match: new RegExp("^(http://)?([^/]+\.)?wikipedia\.","i"),
@@ -33,6 +22,34 @@ var cleanups = {
     purevolume: {
         match: new RegExp("^(http://)?([^/]+\.)?purevolume\.com","i"),
         type: 175
+    },
+    amazon: {
+        match: new RegExp("^(http://)?([^/]+\.)?amazon\.(com|ca|co\.uk|fr|at|de|co\.jp|jp)","i"),
+        type: 76
+    },
+    archive: {
+        match: new RegExp("^(http://)?([^/]+\.)?archive\.org/.*\.(jpg|jpeg|png|gif)$","i"),
+        type: 77
+    },
+    cdbaby: {
+        match: new RegExp("^(http://)?([^/]+\.)?cdbaby\.(com|name)","i"),
+        type: 77
+    },
+    jamendo: {
+        match: new RegExp("^(http://)?([^/]+\.)?jamendo\.com","i"),
+        type: 77
+    },
+    encyclopedisque: {
+        match: new RegExp("^(http://)?([^/]+\.)?encyclopedisque\.fr/images/.*\.jpg","i"),
+        type: 77
+    },
+    manjdisc: {
+        match: new RegExp("^(http://)?([^/]+\.)?mange-disque\.tv/(fs/md_|fstb/tn_md_|info_disque\.php3\\?dis_code=)[0-9]+","i"),
+        type: 77
+    },
+    lyricwiki: {
+        match: new RegExp("^(http://)?([^/]+\.)?lyrics\.wikia\.com", "i"),
+        type: 74
     }
 };
 
