cb60d9afedced83d7ab734ab65c4d76012f6612d
Oliver Charles <oliver.g.charles@googlemail.com>
2010-12-17 15:55:48 +0000

    Move the whole release editor into the Wizard::ReleaseEditor namespace

--- lib/MusicBrainz/Server/Controller/ReleaseEditor/Add.pm
+++ lib/MusicBrainz/Server/Controller/ReleaseEditor/Add.pm
@@ -2,15 +2,22 @@ package MusicBrainz::Server::Controller::ReleaseEditor::Add;
 use Moose;
 BEGIN { extends 'MusicBrainz::Server::ControllerBase::ReleaseEditor' };
 
-use MusicBrainz::Server::Constants qw(
-    $EDIT_RELEASE_CREATE
-    $EDIT_RELEASEGROUP_CREATE
-);
+use aliased 'MusicBrainz::Server::Wizard::ReleaseEditor::Add' => 'Wizard';
 
 sub add : Path('/release/add') Edit RequireAuth
 {
     my ($self, $c) = @_;
-    $self->run($c);
+    my $wizard = Wizard->new(
+        c => $c,
+        on_submit => sub {
+            my $wizard = shift;
+            $c->response->redirect(
+                $c->uri_for_action('/release/show', [ $wizard->release->gid ])
+            );
+            $c->detach
+        }
+    );
+    $wizard->run;
 }
 
 sub cancelled {
@@ -38,111 +45,12 @@ sub cancelled {
     }
 }
 
-augment 'create_edits' => sub
-{
-    my ($self, %args) = @_;
-    my ($c, $data, $create_edit, $editnote, $release, $previewing)
-        = @args{qw( c data create_edit edit_note release previewing )};
-
-    # add release (and release group if necessary)
-    # ----------------------------------------
-
-    my @fields = qw( name comment packaging_id status_id script_id language_id
-                     country_id barcode artist_credit date as_auto_editor );
-    my %add_release_args = map { $_ => $data->{$_} } grep { defined $data->{$_} } @fields;
-
-    if ($data->{release_group_id}){
-        $add_release_args{release_group_id} = $data->{release_group_id};
-    }
-    else {
-        my @fields = qw( name artist_credit type_id as_auto_editor );
-        my %args = map { $_ => $data->{$_} } grep { defined $data->{$_} } @fields;
-
-        my $edit = $create_edit->($EDIT_RELEASEGROUP_CREATE, $editnote, %args);
-
-        # Previewing a release doesn't care about having the release group id
-        $add_release_args{release_group_id} = $edit->entity->id
-            unless $previewing;
-    }
-
-    # Add the release edit
-    my $add_release_edit = $create_edit->(
-        $EDIT_RELEASE_CREATE, $editnote, %add_release_args);
-    $release = $add_release_edit->entity;
-
-    return $release;
-};
-
-augment 'load' => sub
-{
-    my ($self, $c, $wizard) = @_;
-
-    # There was no existing wizard, provide the wizard with
-    # the $release to initialize the forms.
-
-    my $rg_gid = $c->req->query_params->{'release-group'};
-    my $label_gid = $c->req->query_params->{'label'};
-    my $artist_gid = $c->req->query_params->{'artist'};
-
-    my $release = MusicBrainz::Server::Entity::Release->new(
-        mediums => [
-            MusicBrainz::Server::Entity::Medium->new( position => 1 )
-        ]
-    );
-
-    if ($rg_gid)
-    {
-        $c->detach () unless MusicBrainz::Server::Validation::IsGUID($rg_gid);
-        my $rg = $c->model('ReleaseGroup')->get_by_gid($rg_gid);
-        $c->detach () unless $rg;
-
-        $release->release_group_id ($rg->id);
-        $release->release_group ($rg);
-        $release->name ($rg->name);
-
-        $c->model('ArtistCredit')->load ($rg);
-
-        $release->artist_credit ($rg->artist_credit);
-    }
-    elsif ($label_gid)
-    {
-        $c->detach () unless MusicBrainz::Server::Validation::IsGUID($label_gid);
-        my $label = $c->model('Label')->get_by_gid($label_gid);
-
-        $release->add_label(
-            MusicBrainz::Server::Entity::ReleaseLabel->new(
-                label => $label,
-                label_id => $label->id
-           ));
-    }
-    elsif ($artist_gid)
-    {
-        $c->detach () unless MusicBrainz::Server::Validation::IsGUID($artist_gid);
-        my $artist = $c->model('Artist')->get_by_gid($artist_gid);
-        $c->detach () unless $artist;
-
-        $release->artist_credit (
-            MusicBrainz::Server::Entity::ArtistCredit->from_artist ($artist));
-    }
-
-    unless(defined $release->artist_credit) {
-        $release->artist_credit (MusicBrainz::Server::Entity::ArtistCredit->new);
-        $release->artist_credit->add_name (MusicBrainz::Server::Entity::ArtistCreditName->new);
-        $release->artist_credit->names->[0]->artist (MusicBrainz::Server::Entity::Artist->new);
-    }
-
-    return $release;
-};
-
 sub submitted
 {
     my ($self, $c, $release) = @_;
 
     # Not previewing, we've added a release.
-    $c->response->redirect(
-        $c->uri_for_action('/release/show', [ $release->gid ])
-    );
-    $c->detach;
+
 }
 
 1;
--- lib/MusicBrainz/Server/Controller/ReleaseEditor/Edit.pm
+++ lib/MusicBrainz/Server/Controller/ReleaseEditor/Edit.pm
@@ -2,16 +2,19 @@ package MusicBrainz::Server::Controller::ReleaseEditor::Edit;
 use Moose;
 BEGIN { extends 'MusicBrainz::Server::ControllerBase::ReleaseEditor' }
 
-use MusicBrainz::Server::Constants qw(
-    $EDIT_RELEASE_EDIT
-);
+use aliased 'MusicBrainz::Server::Wizard::ReleaseEditor::Edit' => 'Wizard';
 
 sub edit : Chained('/release/load') Edit RequireAuth
 {
     my ($self, $c) = @_;
     my $release = $c->stash->{release};
-
-    $self->run($c, $release);
+    my $wizard = Wizard->new(
+        release => $release,
+        c => $c,
+        on_cancel => sub { $self->cancelled($c) },
+        on_submit => sub { $self->submitted($c, $release) }
+    );
+    $wizard->run;
 }
 
 sub cancelled
@@ -23,63 +26,10 @@ sub cancelled
     $c->response->redirect($c->uri_for_action('/release/show', [ $release->gid ]))
 }
 
-augment 'create_edits' => sub
-{
-    my ($self, %opts) = @_;
-    my ($c, $data, $create_edit, $editnote, $release, $previewing)
-        = @opts{qw( c data create_edit edit_note release previewing )};
-
-    $self->_load_release ($c, $release);
-    $c->stash( medium_formats => [ $c->model('MediumFormat')->get_all ] );
-
-    # FIXME Do we need this? -- acid
-    # we're on the changes preview page, load recordings so that the user can
-    # confirm track <-> recording associations.
-    my @tracks = $release->all_tracks;
-    $c->model('Recording')->load (@tracks);
-
-    # release edit
-    # ----------------------------------------
-
-    my @fields = qw( name comment packaging_id status_id script_id language_id
-                     country_id barcode artist_credit date as_auto_editor );
-    my %args = map { $_ => $data->{$_} } grep { defined $data->{$_} } @fields;
-
-    $args{'to_edit'} = $release;
-    $c->stash->{changes} = 0;
-
-    $create_edit->($EDIT_RELEASE_EDIT, $editnote, %args);
-
-    return $release;
-};
-
-augment 'load' => sub
-{
-    my ($self, $c, $wizard, $release) = @_;
-
-    $self->_load_release ($c, $release);
-    $c->model('Medium')->load_for_releases($release);
-
-    $c->stash( medium_formats => [ $c->model('MediumFormat')->get_all ] );
-
-    return $release;
-};
-
 sub submitted {
     my ($self, $c, $release) = @_;
     $c->response->redirect($c->uri_for_action('/release/show', [ $release->gid ]));
     $c->detach;
-}
-
-# this just loads the remaining bits of a release, not yet loaded by 'load'
-sub _load_release
-{
-    my ($self, $c, $release) = @_;
-
-    $c->model('ReleaseLabel')->load($release);
-    $c->model('Label')->load(@{ $release->labels });
-    $c->model('ReleaseGroupType')->load($release->release_group);
-    $c->model('Release')->annotation->load_latest ($release);
 }
 
 1;
