fd89b3d8c3c95927ca40bed47bf7c9f3cf8aa5ef
warp <kuno@frob.nl>
2010-12-19 12:21:22 +0100

    MBS-1003, forgot to override focus in the autocomplete code, this was causing the menu to close in WebKit when switching search result pages.

--- root/static/scripts/MB/Control/Autocomplete.js
+++ root/static/scripts/MB/Control/Autocomplete.js
@@ -231,6 +231,9 @@ MB.Control.Autocomplete = function (options) {
            to the associated input, which blur would then reset back to it's
            original value (We need to prevent blur from doing that). */
         self.autocomplete.menu.options.blur = function (event, ui) { };
+
+        /* focus, idem. */
+        self.autocomplete.menu.options.focus = function (event, ui) { };
     };
 
     self.input = options.input;
