50ef656bf4d39d39108d4f69392050d93127ea6f
Oliver Charles <oliver.g.charles@googlemail.com>
2011-01-11 14:12:21 +0000

    Make the attributes <select> box required, as every where we currently use one, at least one value is required

--- lib/MusicBrainz/Server/Form/Relationship.pm
+++ lib/MusicBrainz/Server/Form/Relationship.pm
@@ -39,6 +39,7 @@ sub field_list
             push @fields, 'attrs.' . $attr->name . '.contains', {
                 type => 'Select',
                 options => \@opts,
+                required => 1
             };
         }
         else {
