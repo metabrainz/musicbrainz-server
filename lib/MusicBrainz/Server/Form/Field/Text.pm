8f5c7006fa803f170b7894c7a45bc5e2c5e75330
Oliver Charles <oliver.g.charles@googlemail.com>
2011-01-14 14:20:31 +0000

    Strip whitespace from all text fields when creating edits

---
+++ lib/MusicBrainz/Server/Form/Field/Text.pm
@@ -0,0 +1,15 @@
+package MusicBrainz::Server::Form::Field::Text;
+use HTML::FormHandler::Moose;
+extends 'HTML::FormHandler::Field';
+
+apply ([
+    {
+        transform => sub {
+            my $text = shift;
+            $text =~ s/\s+/ /g;
+            return $text;
+        }
+    }
+]);
+
+1;
