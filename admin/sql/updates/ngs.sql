e1d3c6bf6770e3c49eed7dd06e5b8e3142a7bbf3
Robert Kaye <rob@musicbrainz.org>
2011-01-17 17:13:03 -0800

    Removed naive stats migration

--- admin/sql/updates/ngs.sql
+++ admin/sql/updates/ngs.sql
@@ -106,8 +106,6 @@ INSERT INTO url
     FROM public.url;
 
 INSERT INTO replication_control SELECT * FROM public.replication_control;
-INSERT INTO statistic SELECT id, name, value, lastupdated AS last_updated FROM public.currentstat; 
-INSERT INTO statistic SELECT id, name, value FROM public.historicalstat;
 
 ------------------------
 -- Tags
