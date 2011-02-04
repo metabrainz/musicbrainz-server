a1977429cfcad891b431049c0f6167d168522332
Oliver Charles <oliver.g.charles@googlemail.com>
2011-02-04 18:01:27 +0000

    Fix work tests

--- t/lib/t/MusicBrainz/Server/Controller/Work/AddAnnotation.pm
+++ t/lib/t/MusicBrainz/Server/Controller/Work/AddAnnotation.pm
@@ -11,7 +11,7 @@ my $test = shift;
 my $mech = $test->mech;
 my $c    = $test->c;
 
-MusicBrainz::Server::Test->prepare_test_database($c, '+controller_work');
+MusicBrainz::Server::Test->prepare_test_database($c);
 
 $mech->get_ok('/login');
 $mech->submit_form( with_fields => { username => 'new_editor', password => 'password' } );
--- t/lib/t/MusicBrainz/Server/Controller/Work/Tags.pm
+++ t/lib/t/MusicBrainz/Server/Controller/Work/Tags.pm
@@ -11,7 +11,7 @@ my $test = shift;
 my $mech = $test->mech;
 my $c    = $test->c;
 
-MusicBrainz::Server::Test->prepare_test_database($c, '');
+MusicBrainz::Server::Test->prepare_test_database($c);
 
 $mech->get_ok("/work/745c079d-374e-4436-9448-da92dedef3ce/tags");
 html_ok($mech->content);
