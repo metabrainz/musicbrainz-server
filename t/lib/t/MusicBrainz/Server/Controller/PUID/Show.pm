06a6b583bd7679dfe34888e42d65da55efe65d14
Oliver Charles <oliver.g.charles@googlemail.com>
2011-02-04 17:35:58 +0000

    Fix PUID tests

--- t/lib/t/MusicBrainz/Server/Controller/PUID/Show.pm
+++ t/lib/t/MusicBrainz/Server/Controller/PUID/Show.pm
@@ -11,6 +11,8 @@ my $test = shift;
 my $mech = $test->mech;
 my $c    = $test->c;
 
+MusicBrainz::Server::Test->prepare_test_database($c);
+
 $mech->get_ok('/puid/b9c8f51f-cc9a-48fa-a415-4c91fcca80f0');
 html_ok($mech->content);
 $mech->content_contains('Dancing Queen');
