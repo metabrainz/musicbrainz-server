a7798da4b0f4cd46ef4633b1232c656ee34fe7ed
Nikki <nikki@miaubuntu.(none)>
2011-02-11 01:34:05 +0000

    better display for relationship types

---
+++ root/static/scripts/MB/reltypeslist.js
@@ -0,0 +1,25 @@
+$(".reldetails").hide();
+
+$(".toggle").click(function() {
+
+    $(this).parent().next(".reldetails").toggle();
+
+    if ( $(this).parent().next(".reldetails").is(':hidden') ) {
+        $(this).text("more");
+    } else {
+        $(this).text("less");
+    }
+
+
+});
+
+$("#showAll,").click(function () {
+    $(".reldetails, #hideAll").show();
+    $("#showAll").hide();
+});
+
+$("#hideAll,").click(function () {
+    $(".reldetails, #hideAll").hide();
+    $("#showAll").show();
+});
+
