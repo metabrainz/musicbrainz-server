95ae220dd775417b04e7eece2dbe03d82c3a3f5c
Oliver Charles <oliver.g.charles@googlemail.com>
2011-02-17 11:03:25 +0000

    Add /user/votes to view all votes.
    The /user/votes endpoint displays all edits a given user has voted on, and
    displays what their vote was.

--- lib/MusicBrainz/Server/Controller/User/Edits.pm
+++ lib/MusicBrainz/Server/Controller/User/Edits.pm
@@ -10,12 +10,9 @@ __PACKAGE__->config(
 );
 
 sub _edits {
-    my ($self, $c, %args) = @_;
+    my ($self, $c, $loader) = @_;
 
-    $args{editor} = $c->stash->{user}->id;
-    my $edits = $self->_load_paged($c, sub {
-        return $c->model('Edit')->find(\%args, shift, shift);
-    });
+    my $edits = $self->_load_paged($c, $loader);
 
     $c->model('Edit')->load_all(@$edits);
     $c->model('Vote')->load_for_edits(@$edits);
@@ -27,37 +24,76 @@ sub _edits {
         template => 'edit/search_results.tt',
         search => 0
     );
+
+    return $edits;
 }
 
 sub open : Chained('/user/load') PathPart('edits/open') RequireAuth HiddenOnSlaves {
     my ($self, $c) = @_;
-    $self->_edits($c, status => $STATUS_OPEN);
+    $self->_edits($c, sub {
+        return $c->model('Edit')->find({
+            editor => $c->stash->{user}->id,
+            status => $STATUS_OPEN
+        }, shift, shift);
+    });
 }
 
 sub accepted : Chained('/user/load') PathPart('edits/accepted') RequireAuth HiddenOnSlaves {
     my ($self, $c) = @_;
-    $self->_edits($c, status => $STATUS_APPLIED);
+    $self->_edits($c, sub {
+        return $c->model('Edit')->find({
+            editor => $c->stash->{user}->id,
+            status => $STATUS_APPLIED
+        }, shift, shift);
+    });
 }
 
 sub failed : Chained('/user/load') PathPart('edits/failed') RequireAuth HiddenOnSlaves {
     my ($self, $c) = @_;
-    $self->_edits($c, status => [ $STATUS_FAILEDDEP, $STATUS_FAILEDPREREQ,
-                                  $STATUS_ERROR, $STATUS_NOVOTES ]);
+    $self->_edits($c, sub {
+        return $c->model('Edit')->find({
+            editor => $c->stash->{user}->id,
+            status => [ $STATUS_FAILEDDEP, $STATUS_FAILEDPREREQ,
+                        $STATUS_ERROR, $STATUS_NOVOTES ]
+        }, shift, shift);
+    });
 }
 
 sub rejected : Chained('/user/load') PathPart('edits/rejecte') RequireAuth HiddenOnSlaves {
     my ($self, $c) = @_;
-    $self->_edits($c, status => [ $STATUS_FAILEDVOTE ]);
+    $self->_edits($c, sub {
+        return $c->model('Edit')->find({
+            editor => $c->stash->{user}->id,
+            status => [ $STATUS_FAILEDVOTE ]
+        }, shift, shift);
+    });
 }
 
 sub autoedits : Chained('/user/load') PathPart('edits/autoedits') RequireAuth HiddenOnSlaves {
     my ($self, $c) = @_;
-    $self->_edits($c, autoedit => 1);
+    $self->_edits($c, sub {
+        return $c->model('Edit')->find({
+            editor => $c->stash->{user}->id,
+            autoedit => 1
+        }, shift, shift);
+    });
 }
 
 sub all : Chained('/user/load') PathPart('edits') RequireAuth HiddenOnSlaves {
     my ($self, $c) = @_;
-    $self->_edits($c);
+    $self->_edits($c, sub {
+        return $c->model('Edit')->find({
+            editor => $c->stash->{user}->id
+        }, shift, shift);
+    });
+}
+
+sub votes : Chained('/user/load') PathPart('votes') RequireAuth HiddenOnSlaves {
+    my ($self, $c) = @_;
+    my $edits = $self->_edits($c, sub {
+        return $c->model('Edit')->find_by_voter($c->stash->{user}->id, shift, shift);
+    });
+    $c->stash( voter => $c->stash->{user} );
 }
 
 no Moose;
