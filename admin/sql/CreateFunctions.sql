e881803235b2191ea7ab0cc704856aa030d259ac
Oliver Charles <oliver.g.charles@googlemail.com>
2011-03-10 17:30:47 +0000

    Add the delete_ratings function to the RAWDATA function list

--- admin/sql/CreateFunctions.sql
+++ admin/sql/CreateFunctions.sql
@@ -470,19 +470,5 @@ BEGIN
 END;
 $$ LANGUAGE 'plpgsql';
 
-CREATE OR REPLACE FUNCTION delete_ratings(enttype TEXT, ids INTEGER[])
-RETURNS TABLE(editor INT, rating SMALLINT) AS $$
-DECLARE
-    tablename TEXT;
-BEGIN
-    tablename = enttype || '_rating_raw';
-    RETURN QUERY
-       EXECUTE 'DELETE FROM ' || tablename || ' WHERE ' || enttype || ' = any($1)
-                RETURNING editor, rating'
-         USING ids;
-    RETURN;
-END;
-$$ LANGUAGE 'plpgsql';
-
 COMMIT;
 -- vi: set ts=4 sw=4 et :
