f9c07a83bcc02b9fcb80e1e6b30f4c79fc58fe18
Oliver Charles <oliver.g.charles@googlemail.com>
2011-03-23 13:27:39 +0000

    Fix Entity::URL tests

--- t/lib/t/MusicBrainz/Server/Entity/URL.pm
+++ t/lib/t/MusicBrainz/Server/Entity/URL.pm
@@ -13,7 +13,7 @@ my $url = MusicBrainz::Server::Entity::URL->new(
     url => 'http://www.discogs.com/artist/%D0%97%D0%B5%D0%BC%D1%84%D0%B8%D1%80%D0%B0',
 );
 
-is ($url->pretty_name => 'http://www.discogs.com/artist/Руки+Вверх');
+is ($url->pretty_name => 'http://www.discogs.com/artist/Земфира');
 
 };
 
