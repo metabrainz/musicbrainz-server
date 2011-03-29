fa2d7cd779f48a43f7f0e735aa7a981af3faddc4
Oliver Charles <oliver.g.charles@googlemail.com>
2011-03-29 15:42:03 +0100

    Change remove artist conditions to allow ModBot to enter auto edits

--- lib/MusicBrainz/Server/Edit/Artist/Delete.pm
+++ lib/MusicBrainz/Server/Edit/Artist/Delete.pm
@@ -14,6 +14,32 @@ sub edit_type { $EDIT_ARTIST_DELETE }
 sub _delete_model { 'Artist' }
 sub subscription_model { shift->c->model('Artist')->subscription }
 
+# We do allow auto edits for this (as ModBot needs to insert them)
+sub edit_conditions
+{
+    return {
+        $QUALITY_LOW => {
+            duration      => 4,
+            votes         => 1,
+            expire_action => $EXPIRE_ACCEPT,
+            auto_edit     => 1,
+        },
+        $QUALITY_NORMAL => {
+            duration      => 14,
+            votes         => 3,
+            expire_action => $EXPIRE_ACCEPT,
+            auto_edit     => 1,
+        },
+        $QUALITY_HIGH => {
+            duration      => 14,
+            votes         => 4,
+            expire_action => $EXPIRE_REJECT,
+            auto_edit     => 0,
+        },
+    };
+}
+
+
 __PACKAGE__->meta->make_immutable;
 no Moose;
 
