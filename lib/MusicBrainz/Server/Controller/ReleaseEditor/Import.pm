4176a98254be27af86e2960899a0b301e57d53b9
Oliver Charles <oliver.g.charles@googlemail.com>
2011-04-01 16:28:09 +0100

    Change /freedb/import links to /freedb/show

--- lib/MusicBrainz/Server/Controller/ReleaseEditor/Import.pm
+++ lib/MusicBrainz/Server/Controller/ReleaseEditor/Import.pm
@@ -15,7 +15,7 @@ sub freedb : Path('/release/import/freedb') RequireAuth {
 
     if ($import_form->submitted_and_valid($c->req->query_params)) {
         $c->response->redirect(
-            $c->uri_for_action('/freedb/import', [
+            $c->uri_for_action('/freedb/show', [
                 $import_form->field('category')->value,
                 $import_form->field('discid')->value
             ])
