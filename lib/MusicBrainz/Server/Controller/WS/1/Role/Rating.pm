aaa563bcf14a40c12f531fef037f399e81966a26
Oliver Charles <oliver.g.charles@googlemail.com>
2011-04-19 14:11:16 +0100

    Do not authenticate more than once in ws/1
    Specifying inc=user-tags+user-ratings tries to authenticate twice, which
    results in a blank response (as it assumes authentication has failed).

--- lib/MusicBrainz/Server/Controller/WS/1/Role/Rating.pm
+++ lib/MusicBrainz/Server/Controller/WS/1/Role/Rating.pm
@@ -5,7 +5,7 @@ before 'lookup' => sub {
     my ($self, $c) = @_;
 
     return unless $c->stash->{inc}->ratings || $c->stash->{inc}->user_ratings;
-    $c->authenticate({}, 'musicbrainz.org') if $c->stash->{inc}->user_ratings;
+    $c->authenticate({}, 'musicbrainz.org') if !$c->user_exists && $c->stash->{inc}->user_ratings;
 
     my $entity = $c->stash->{entity};
     my $model = $self->model;
