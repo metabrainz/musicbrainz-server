bfaef31e8639e733544d26b8faa511bb2db3ffab
Oliver Charles <oliver.g.charles@googlemail.com>
2011-05-16 11:59:09 +0100

    Rewrite ngs-fix-artist-links.pl to use an iterative approach

--- admin/sql/updates/ngs-fix-artist-links.pl
+++ admin/sql/updates/ngs-fix-artist-links.pl
@@ -15,40 +15,46 @@ $c->sql->begin;
 $c->raw_sql->begin;
 
 try {
-
+    printf STDERR "Selecting artists to update\n";
     my @non_existant = @{
         $c->sql->select_single_column_array('SELECT old_ac FROM tmp_artist_credit_repl')
     } or exit(0); # nothing to do
 
-    $c->raw_sql->select(
-        'DELETE FROM edit_artist
-          WHERE artist IN (' . join(',', ("?") x @non_existant) . ')
-      RETURNING edit, artist',
-        @non_existant
-    );
-
     my $i = 0;
-    while (my $expand = $c->raw_sql->next_row_hash_ref) {
+    printf "Updating edit_artist\n";
+    for my $artist_id (@non_existant) {
+        my @edit_ids = @{ $c->raw_sql->select_single_column_array(
+            'DELETE FROM edit_artist
+              WHERE artist = ?
+          RETURNING edit',
+            $artist_id
+        ) } or next;
+
         my @expand_to = @{
             $c->sql->select_single_column_array(
-                'SELECT artist FROM artist_credit_name
+                'SELECT DISTINCT artist FROM artist_credit_name
                   WHERE artist_credit = (
                         SELECT new_ac
                           FROM tmp_artist_credit_repl
                          WHERE old_ac = ?
                         )',
-                $expand->{artist}
+                $artist_id
             )
         } or next;
 
         $c->raw_sql->do(
             'INSERT INTO edit_artist (edit, artist)
-         VALUES ' . join(',', ('(?, ?)') x @expand_to),
-            map { $expand->{edit}, $_ } @expand_to
+             VALUES ' . join(',', (('(?, ?)') x @expand_to) x @edit_ids),
+            map { my $new_artist = $_; map { $_, $new_artist } @edit_ids } @expand_to
         );
 
-        printf STDERR "\r%d/%d", $i, scalar(@non_existant);
+        printf STDERR "\r%d/%d", $i++, scalar(@non_existant);
     }
+
+    printf STDERR "Removing duplicates\n";
+    $c->raw_sql->do('SELECT DISTINCT edit, artist INTO TEMPORARY edit_artist_distinct FROM edit_artist');
+    $c->raw_sql->do('TRUNCATE edit_artist');
+    $c->raw_sql->do('INSERT INTO edit_artist (edit, artist) SELECT edit, artist FROM edit_artist_distinct');
 
     $c->sql->commit;
     $c->raw_sql->commit;
