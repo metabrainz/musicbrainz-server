5013e6c0181e1c1802b305140cb66c8dd6ca64b2
Oliver Charles <oliver.g.charles@googlemail.com>
2011-05-16 01:31:25 +0100

    Run fix-artist-links in a transaction that can be rolled back

--- admin/sql/updates/ngs-fix-artist-links.pl
+++ admin/sql/updates/ngs-fix-artist-links.pl
@@ -5,37 +5,52 @@ use warnings;
 use FindBin '$Bin';
 use lib "$Bin/../../../lib";
 
+use Try::Tiny;
+
 use MusicBrainz::Server::Context;
 
 my $c = MusicBrainz::Server::Context->create_script_context;
 
-my @non_existant = @{
-    $c->sql->select_single_column_array('SELECT old_ac FROM tmp_artist_credit_repl')
-} or exit(0); # nothing to do
+$c->sql->begin;
+$c->raw_sql->begin;
 
-$c->raw_sql->select(
-    'DELETE FROM edit_artist
-      WHERE artist IN (' . join(',', ("?") x @non_existant) . ')
-  RETURNING edit, artist',
-    @non_existant
-);
+try {
 
-while (my $expand = $c->raw_sql->next_row_hash_ref) {
-    my @expand_to = @{
-        $c->sql->select_single_column_array(
-            'SELECT artist FROM artist_credit_name
-              WHERE artist_credit = (
-                    SELECT new_ac
-                      FROM tmp_artist_credit_repl
-                     WHERE old_ac = ?
-                    )',
-            $expand->{artist}
-        )
-    } or next;
+    my @non_existant = @{
+        $c->sql->select_single_column_array('SELECT old_ac FROM tmp_artist_credit_repl')
+    } or exit(0); # nothing to do
 
-    $c->raw_sql->do(
-        'INSERT INTO edit_artist (edit, artist)
+    $c->raw_sql->select(
+        'DELETE FROM edit_artist
+          WHERE artist IN (' . join(',', ("?") x @non_existant) . ')
+      RETURNING edit, artist',
+        @non_existant
+    );
+
+    while (my $expand = $c->raw_sql->next_row_hash_ref) {
+        my @expand_to = @{
+            $c->sql->select_single_column_array(
+                'SELECT artist FROM artist_credit_name
+                  WHERE artist_credit = (
+                        SELECT new_ac
+                          FROM tmp_artist_credit_repl
+                         WHERE old_ac = ?
+                        )',
+                $expand->{artist}
+            )
+        } or next;
+
+        $c->raw_sql->do(
+            'INSERT INTO edit_artist (edit, artist)
          VALUES ' . join(',', ('(?, ?)') x @expand_to),
-        map { $expand->{edit}, $_ } @expand_to
-    )
+            map { $expand->{edit}, $_ } @expand_to
+        )
+    }
+
+    $c->sql->commit;
+    $c->raw_sql->begin;
+}
+catch {
+    $c->sql->rollback;
+    $c->raw_sql->rollback;
 }
