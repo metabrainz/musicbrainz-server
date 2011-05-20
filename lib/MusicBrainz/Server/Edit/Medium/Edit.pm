d5b78662f6903274d949a34d58e4f8158fed3e12
Oliver Charles <oliver.g.charles@googlemail.com>
2011-05-20 17:57:42 +0100

    First attempt at a diff display for tracklists
    Diffs each track row, and if they are "changed" then attempts to diff each
    property of the track

--- lib/MusicBrainz/Server/Edit/Medium/Edit.pm
+++ lib/MusicBrainz/Server/Edit/Medium/Edit.pm
@@ -1,6 +1,7 @@
 package MusicBrainz::Server::Edit::Medium::Edit;
 use Carp;
 use Clone 'clone';
+use Algorithm::Diff qw( sdiff );
 use Data::Compare;
 use Moose;
 use MooseX::Types::Moose qw( ArrayRef Bool Str Int );
@@ -16,6 +17,7 @@ use MusicBrainz::Server::Edit::Types qw(
 use MusicBrainz::Server::Edit::Utils qw( verify_artist_credits );
 use MusicBrainz::Server::Validation 'normalise_strings';
 use MusicBrainz::Server::Translation qw( l ln );
+use MusicBrainz::Server::Track qw ( format_track_length );
 
 extends 'MusicBrainz::Server::Edit::WithDifferences';
 with 'MusicBrainz::Server::Edit::Role::Preview';
@@ -169,6 +171,23 @@ sub build_display_data
 
     $data->{new}{tracklist} = display_tracklist($loaded, $self->data->{new}{tracklist});
     $data->{old}{tracklist} = display_tracklist($loaded, $self->data->{old}{tracklist});
+
+    $data->{tracklist_changes} =
+        sdiff(
+            [ $data->{old}{tracklist}->all_tracks ],
+            [ $data->{new}{tracklist}->all_tracks ],
+            sub {
+                my $track = shift;
+                return join(
+                    '',
+                    $track->name,
+                    format_track_length($track->length),
+                    $track->artist_credit->name,
+                    $track->position,
+                    $track->recording->id
+                );
+            }
+        );
 
     if ($self->data->{release})
     {
