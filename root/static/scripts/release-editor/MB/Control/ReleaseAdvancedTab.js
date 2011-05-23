33f282a8a9bed4fd52b1e67f75f0a5b3a405e233
warp <kuno@frob.nl>
2011-05-23 10:42:31 +0200

    MBS-2152, do not prevent the last disc from being deleted with javascript.  Instead, validate atleast 1 disc is present in Form::ReleaseEditor::Tracklist.

--- root/static/scripts/release-editor/MB/Control/ReleaseAdvancedTab.js
+++ root/static/scripts/release-editor/MB/Control/ReleaseAdvancedTab.js
@@ -375,9 +375,6 @@ MB.Control.ReleaseDisc = function (parent, $disc) {
     };
 
     self.removeDisc = function (chained) {
-        if (!chained && self.isLastDisc ())
-            return;
-
         self.edits.clearEdits ();
         self.tracklist = null;
         self.removeTracks (-1);
@@ -387,15 +384,6 @@ MB.Control.ReleaseDisc = function (parent, $disc) {
 
         self.parent.removeDisc (self);
         self.position (0);
-
-        if (!chained)
-        {
-            self.basic.removeDisc (true);
-        }
-    };
-
-    self.isLastDisc = function () {
-        return self.parent.isLastDisc (self);
     };
 
     self.isDeleted = function () {
@@ -769,10 +757,6 @@ MB.Control.ReleaseAdvancedTab = function () {
         }
 
         return true;
-    };
-
-    self.isLastDisc = function (disc) {
-        return (self.discs.length == 1);
     };
 
     self.removeDisc = function (disc) {
