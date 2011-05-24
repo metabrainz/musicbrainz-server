21398d5e31dc7f838d6bf675f510d7f5f4dc3c4f
Oliver Charles <oliver.g.charles@googlemail.com>
2011-05-24 15:20:42 +0100

    Upgrade old edit types to have short_link_phrase serialized
    If we can't actually load the link type though, we'll have to stick with what we have

--- upgrade.sh
+++ upgrade.sh
@@ -8,6 +8,8 @@ eval `./admin/ShowDBDefs`
 echo `date` : Rewriting short link phrases for relationships
 ./admin/psql READWRITE < ./admin/sql/updates/20110524-short-link-phrases.sql
 
+echo `date` : Upgrading relationship edit types to include short link phrases
+./admin/sql/updates/20110524-short-link-phrase-edits.pl
 
 echo `date` : Done
 
