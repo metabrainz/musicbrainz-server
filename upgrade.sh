e18ce9e344bb0ca38f4dec2226263dfe4910ef57
Oliver Charles <oliver.g.charles@googlemail.com>
2011-05-24 17:48:23 +0100

    Fix all existing edit relationship edits

--- upgrade.sh
+++ upgrade.sh
@@ -5,14 +5,8 @@ cd `dirname $0`
 
 eval `./admin/ShowDBDefs`
 
-echo `date` : Unlinked PUID edits
-./admin/psql RAWDATA < ./admin/sql/updates/20110521.sql
-
-echo `date` : Making sure ISRC recording pairs are unique
-./admin/psql READWRITE < ./admin/sql/updates/hotfixes2-20110523-unique-isrcs.sql
-
-echo `date` : Cleaning up edit relationship edits
-./admin/sql/updates/hotfix2-20110523-edit-relationship.pl
+echo `date` : Fixing edit relationship edits
+./admin/sql/updates/20110524-fix-broken-relationship-edits.pl
 
 echo `date` : Done
 
