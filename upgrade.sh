e514c5c42e2ce9817d4714149e39bcbdde9fb92e
Oliver Charles <oliver.g.charles@googlemail.com>
2011-05-24 17:12:16 +0100

    Relink all existing relationships so they have artist credit links as well

--- upgrade.sh
+++ upgrade.sh
@@ -6,13 +6,7 @@ cd `dirname $0`
 eval `./admin/ShowDBDefs`
 
 echo `date` : Unlinked PUID edits
-./admin/psql RAWDATA < ./admin/sql/updates/20110521.sql
-
-echo `date` : Making sure ISRC recording pairs are unique
-./admin/psql READWRITE < ./admin/sql/updates/hotfixes2-20110523-unique-isrcs.sql
-
-echo `date` : Cleaning up edit relationship edits
-./admin/sql/updates/hotfix2-20110523-edit-relationship.pl
+./admin/sql/updates/20110524-relink-relationships.pl
 
 echo `date` : Done
 
