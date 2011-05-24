def7bb7603b7d2cc228c3488f87f51e3e13769bb
Oliver Charles <oliver.g.charles@googlemail.com>
2011-05-24 15:06:49 +0100

    Populate the short_link_phrase column with a 'verbose' phrase of the relationship in forward direction

--- upgrade.sh
+++ upgrade.sh
@@ -5,14 +5,9 @@ cd `dirname $0`
 
 eval `./admin/ShowDBDefs`
 
-echo `date` : Unlinked PUID edits
-./admin/psql RAWDATA < ./admin/sql/updates/20110521.sql
+echo `date` : Rewriting short link phrases for relationships
+./admin/psql READWRITE < ./admin/sql/updates/20110524-short-link-phrases.sql
 
-echo `date` : Making sure ISRC recording pairs are unique
-./admin/psql READWRITE < ./admin/sql/updates/hotfixes2-20110523-unique-isrcs.sql
-
-echo `date` : Cleaning up edit relationship edits
-./admin/sql/updates/hotfix2-20110523-edit-relationship.pl
 
 echo `date` : Done
 
