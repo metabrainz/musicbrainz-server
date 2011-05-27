b73b8e13c52ad73cbdcfa37955811a2225d1af0d
Oliver Charles <oliver.g.charles@googlemail.com>
2011-05-27 13:07:11 +0100

    Derive a sortname from artists for WS/1

--- lib/MusicBrainz/Server/Controller/WS/1/Role/ArtistCredit.pm
+++ lib/MusicBrainz/Server/Controller/WS/1/Role/ArtistCredit.pm
@@ -9,8 +9,7 @@ before 'lookup' => sub
 
     if ($c->stash->{inc}->artist) {
         $c->model('ArtistCredit')->load($release);
-        $c->model('Artist')->load($release->artist_credit->names->[0])
-            if (@{ $release->artist_credit->names } == 1);
+        $c->model('Artist')->load($release->artist_credit->all_names);
     }
 };
 
