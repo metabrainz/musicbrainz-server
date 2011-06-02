9671fea430988d91b6b119e381919cecb10e9e13
Oliver Charles <oliver.g.charles@googlemail.com>
2011-06-02 15:11:28 +0100

    Merge duplicate tracklists
    For some reason or another, we have duplicate tracklists in the system, which is
    not by intention. This adds an upgrade script that is capable of merging
    duplicate tracklists.
    
    We create a row for every tracklist, which contains an array of track names,
    artist credits, recording ids and lengths, all ordered by position. This set of
    arrays defines a unique tracklist. Then we find duplicate occurances of these
    tracklists, and merge them and remove the duplicate tracklists. The destination
    tracklist is selected deterministically as retaining the tracklist with the
    lowest ID.

--- upgrade.sh
+++ upgrade.sh
@@ -5,14 +5,8 @@ cd `dirname $0`
 
 eval `./admin/ShowDBDefs`
 
-echo `date` : Backing up data
-./admin/psql RAWDATA < ./admin/sql/updates/20110527-RAWDATA-backup.sql
-
-echo `date` : "Rewrite empty_artists()"
-./admin/psql READWRITE < ./admin/sql/updates/20110530-empty-artists.sql
-
-echo `date` : Upgrade historic artist credits
-./admin/sql/updates/20110527-historic-artist-credits.pl
+echo `date` : Merging duplicate tracklists
+./admin/psql READWRITE < ./admin/sql/updates/20110602-merge-duplicate-tracklists.sql
 
 echo `date` : Done
 
