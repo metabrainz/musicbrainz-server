b68776df1cc603b6f69583d8873e0cdacd35f0bc
Oliver Charles <oliver.g.charles@googlemail.com>
2011-06-07 15:45:02 +0100

    Rewrite cleanup jobs to handle the case where last_updated is NULL

--- upgrade.sh
+++ upgrade.sh
@@ -8,6 +8,9 @@ eval `./admin/ShowDBDefs`
 echo `date` : Backing up data
 ./admin/psql RAWDATA < admin/sql/updates/RAWDATA-backup.sql
 
+echo `date` : Rewriting empty_artists()
+./admin/psql READWRITE < admin/sql/updates/20110607-empty-artists.sql
+
 echo `date` : Fixing old edit label edits
 ./admin/sql/updates/20110606-fix-historic-edit-label.pl
 
