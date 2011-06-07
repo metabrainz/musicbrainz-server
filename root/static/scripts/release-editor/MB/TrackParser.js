71d595265f884e26382738bdb8f87af61e6351c4
warp <kuno@frob.nl>
2011-06-07 14:11:26 +0200

    MBS-2055, if there is only a tracknumber without a trailing space, still consider it as a track number.

--- root/static/scripts/release-editor/MB/TrackParser.js
+++ root/static/scripts/release-editor/MB/TrackParser.js
@@ -199,11 +199,11 @@ MB.TrackParser.Track = function (position, line, parent) {
     self.removeTrackNumbers = function () {
         if (self.parent.vinylNumbers ())
         {
-            self.line = self.line.replace(/^\s*[-\.０-９0-9a-z]+\.?\s+/i, "");
+            self.line = self.line.replace(/^\s*[-\.０-９0-9a-z]+\.?(\s+|$)/i, "");
         }
         else if (self.parent.trackNumbers ())
         {
-            self.line = self.line.replace(/^\s*([-\.０-９0-9\.]+(-[０-９0-9]+)?)\.?\s+/, "");
+            self.line = self.line.replace(/^\s*([-\.０-９0-9\.]+(-[０-９0-9]+)?)\.?(\s+|$)/, "");
         }
     };
 
