b04e2a2ad5997690a6032af799e5d49771c9f785
Oliver Charles <oliver.g.charles@googlemail.com>
2011-06-09 14:30:59 +0100

    Added the 'ID' predicate for edit searching
    Starting to sketch out the edit search system. We have a collection of predicate
    classes, which represent certain types of search conditions (date searching,
    entity searching, enumeration searches, etc). Predicates are collected into a
    Query object, which combines all these predicates in a certain way (ie, AND or
    OR, though this work will be done in a later commit).

---
+++ lib/MusicBrainz/Server/EditSearch/Predicate.pm
@@ -0,0 +1,69 @@
+package MusicBrainz::Server::EditSearch::Predicate;
+use Moose::Role;
+
+use MooseX::Types::Moose qw( Any ArrayRef Str );
+use MusicBrainz::Server::EditSearch::Exceptions;
+use MusicBrainz::Server::Translation;
+
+requires qw(
+    operator_cardinality
+    combine_with_query
+);
+
+sub supports_operator {
+    my ($class, $operator) = @_;
+    my %operator_cardinality = $class->operator_cardinality;
+    return exists $operator_cardinality{$operator};
+}
+
+has operator => (
+    isa => Str,
+    is => 'ro',
+    required => 1
+);
+
+has arguments => (
+    isa => ArrayRef[Any],
+    is => 'bare',
+    required => 1,
+    init_arg => 'args',
+    traits => [ 'Array' ],
+    handles => {
+        arguments => 'elements',
+    }
+);
+
+has field_name => (
+    isa => Str,
+    is => 'ro',
+    required => 1
+);
+
+sub transform_user_input {
+    my ($self, $user_input) = @_;
+    return $user_input;
+}
+
+sub cross_validate { }
+
+sub new_from_input {
+    my ($class, $field_name, $input) = @_;
+
+    MusicBrainz::Server::EditSearch::Exceptions::UnsupportOperator->throw(
+        l('This operator is not supported')
+    ) unless $class->supports_operator($input->{operator});
+
+    return $class->new(
+        field_name => $field_name,
+        %{$input},
+    );
+}
+
+sub sql_arguments {
+    my $self = shift;
+    return [
+        map { $self->transform_user_input($_) } $self->arguments
+    ]
+}
+
+1;
