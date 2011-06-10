26053f6e10caa3675d3fabb7383bc931d457f171
Oliver Charles <oliver.g.charles@googlemail.com>
2011-06-10 17:18:54 +0100

    Fix edit release label edits and avoid autovivification

--- upgrade.sh
+++ upgrade.sh
@@ -8,17 +8,8 @@ eval `./admin/ShowDBDefs`
 echo `date` : Backing up data
 ./admin/psql RAWDATA < admin/sql/updates/RAWDATA-backup.sql
 
-echo `date` : Adding special purpose row constraints
-./admin/psql READWRITE < admin/sql/updates/20110808-special-purpose-triggers.sql
-
-echo `date` : Rewriting empty_artists()
-./admin/psql READWRITE < admin/sql/updates/20110607-empty-artists.sql
-
-echo `date` : Fixing old edit label edits
-./admin/sql/updates/20110606-fix-historic-edit-label.pl
-
-echo `date` : Fix add release label edits
-./admin/sql/updates/20110607-add-release-label.pl
+echo `date` : Fix edit release label edits
+./admin/sql/updates/20110610-edit-release-label.pl
 
 echo `date` : Done
 
