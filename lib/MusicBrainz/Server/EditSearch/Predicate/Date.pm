d77ac3e9426a9caf5111b525f2f2db7dbf13d698
Oliver Charles <oliver.g.charles@googlemail.com>
2011-06-13 12:32:50 +0100

    Basic validation to prevent invalid data from being ran

--- lib/MusicBrainz/Server/EditSearch/Predicate/Date.pm
+++ lib/MusicBrainz/Server/EditSearch/Predicate/Date.pm
@@ -16,6 +16,17 @@ sub transform_user_input {
     my $parser = DateTime::Format::Natural->new;
     DateTime::Format::Pg->format_datetime($parser->parse_datetime($argument));
 }
+
+sub valid {
+    my $self = shift;
+    my $parser = DateTime::Format::Natural->new;
+    for my $arg_index (1.. $self->operator_cardinality($self->operator)) {
+        my $arg = $self->argument($arg_index - 1) or return;
+        $parser->parse_datetime($arg);
+        $parser->success or return;
+    }
+
+    return 1;
 }
 
 1;
