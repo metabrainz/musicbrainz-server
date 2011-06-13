ee5c77e79d0f71fbffd9e42bbe3e9e23c0974b92
Oliver Charles <oliver.g.charles@googlemail.com>
2011-06-13 23:53:04 +0100

    Merge RAWDATA into READWRITE in the musicbrainz_raw schema
    Conflicts:
    
    	upgrade.sh

--- upgrade.sh
+++ upgrade.sh
@@ -4,7 +4,29 @@ set -o errexit
 cd `dirname $0`
 
 eval `./admin/ShowDBDefs`
-./admin/psql READWRITE < ./admin/sql/updates/20110624-cdtoc-indexes.sql
+
+echo `date` : Renaming RAWDATA musicbrainz schema
+echo 'ALTER SCHEMA musicbrainz RENAME TO musicbrainz_raw' | ./admin/psql RAWDATA
+echo 'CREATE SCHEMA musicbrainz_raw' | ./admin/psql READWRITE
+
+echo `date` : Dumping RAWDATA
+eval $(perl -Ilib -MString::ShellQuote -MDBDefs -MMusicBrainz::Server::DatabaseConnectionFactory -e '
+    my $db = MusicBrainz::Server::DatabaseConnectionFactory->get("RAWDATA");
+    my $rw = MusicBrainz::Server::DatabaseConnectionFactory->get("READWRITE");
+    printf "export MB_RAWDATA_%s=%s\n", uc($_), shell_quote($db->$_),
+        for qw( username password schema port database host );
+    printf "export MB_READWRITE_%s=%s\n", uc($_), shell_quote($rw->$_),
+        for qw( username password schema port database host );
+')
+pg_dump --format=c --schema=musicbrainz_raw --host=$MB_RAWDATA_HOST --username=$MB_RAWDATA_USERNAME -v \
+    $MB_RAWDATA_DATABASE > rawdata.dump
+
+echo `date` : Loading RAWDATA into READWRITE
+pg_restore --format=c --schema=musicbrainz_raw --host=$MB_READWRITE_HOST --username=$MB_READWRITE_USERNAME -v \
+    -d $MB_READWRITE_DATABASE rawdata.dump
+
+echo `date` : RAWDATA is now part of the READWRITE database.
+echo `date` : Please update your DBDefs and feel free to drop the RAWDATA database
 
 echo `date` : Done
 
