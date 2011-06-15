046f1be30cb67e8bef4684413f1464654c1d4156
Ian McEwen <ianmcorvidae@ianmcorvidae.net>
2011-06-15 10:09:12 -0700

    change to where type = 1, rather than != 2, since I'm using .notype as well

--- lib/MusicBrainz/Server/Data/Statistics.pm
+++ lib/MusicBrainz/Server/Data/Statistics.pm
@@ -103,7 +103,7 @@ my %stats = (
             my $data = $sql->select_list_of_lists(
                 "SELECT COALESCE(gender::text, 'null'), COUNT(*) AS count
                 FROM artist
-		WHERE type != 2
+                WHERE type = 1
                 GROUP BY gender
                 ",
             );
