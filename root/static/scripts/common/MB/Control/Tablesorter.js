a6789d023c644b072301299ba17acfc951d17cca
Ian McEwen <ianmcorvidae@ianmcorvidae.net>
2011-06-28 11:59:00 -0700

    jquery-ize

--- root/static/scripts/common/MB/Control/Tablesorter.js
+++ root/static/scripts/common/MB/Control/Tablesorter.js
@@ -2,22 +2,22 @@ $(document).ready(function() {
     $.tablesorter.addWidget({
         id: "indexFirstColumn",
         format: function(table) {                               
-            for(var i=0; i < table.tBodies[0].rows.length; i++) {
-                $("tbody tr:eq(" + (i - 1) + ") td:first",table).html(i + '.');
-            }                                                               
+            $('tbody tr', table).each(function (index) {
+                $(this).find('td:first').html((index + 1) + '.');
+            });
         }
     });
 
     $.tablesorter.addWidget({
         id: "evenRowClasses",
         format: function(table) {
-            for(var i=0; i < table.tBodies[0].rows.length; i++) {
-                if (i % 2 == 0) {
-                    $("tbody tr:eq(" + (i-1) + ")", table).addClass("ev");
+            $('tbody tr', table).each(function (index) {
+                if ((index + 1) % 2 == 0) {
+                    $(this).addClass("ev");
                 } else {
-                    $("tbody tr:eq(" + (i-1) + ")", table).removeClass("ev");
+                    $(this).removeClass("ev");
                 }
-            }
+            });
         }
     });
 
