1a4cc4d0ed58439a15701712f2d5f72108a78188
Oliver Charles <oliver.g.charles@googlemail.com>
2011-07-06 11:49:18 +0100

    Update replicated tables
    Adds the following tables to now be replicated:
    
    - url_gid_redirect
    - work_alias

--- admin/sql/CreateReplicationTriggers.sql
+++ admin/sql/CreateReplicationTriggers.sql
@@ -384,8 +384,16 @@ CREATE TRIGGER "reptg_url"
 AFTER INSERT OR DELETE OR UPDATE ON "url"
 FOR EACH ROW EXECUTE PROCEDURE "recordchange" ();
 
+CREATE TRIGGER "reptg_url_gid_redirect"
+AFTER INSERT OR DELETE OR UPDATE ON "url_gid_redirect"
+FOR EACH ROW EXECUTE PROCEDURE "recordchange" ();
+
 CREATE TRIGGER "reptg_work"
 AFTER INSERT OR DELETE OR UPDATE ON "work"
+FOR EACH ROW EXECUTE PROCEDURE "recordchange" ('verbose');
+
+CREATE TRIGGER "reptg_work_alias"
+AFTER INSERT OR DELETE OR UPDATE ON "work_alias"
 FOR EACH ROW EXECUTE PROCEDURE "recordchange" ('verbose');
 
 CREATE TRIGGER "reptg_work_annotation"
