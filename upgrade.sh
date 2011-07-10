7b8f8cd29ee97093cc3de0149bda1b4a0135423f
Oliver Charles <oliver.g.charles@googlemail.com>
2011-07-10 21:30:38 +0100

    Use a DROP/CREATE proces for slaves too

--- upgrade.sh
+++ upgrade.sh
@@ -8,7 +8,7 @@ eval `./admin/ShowDBDefs`
 if [ $REPLICATION_TYPE == $RT_MASTER ]
 then
     echo `date` : Recreating tracklist_index
-    ./admin/psql READWRITE < admin/sql/updates/201107010-tracklist-index.sql
+    ./admin/psql READWRITE < admin/sql/updates/20110710-tracklist-index.sql
 
     echo `date` : Dumping now-replicated tables
     ./admin/ExportAllTables --table=url_gid_redirect --table=work_alias --table=tracklist_index
@@ -22,10 +22,12 @@ then
 elif [ $REPLICATION_TYPE == $RT_SLAVE ]
 then
     echo `date` : Importing new non-replicated data
+    ./admin/psql READWRITE < admin/sql/updates/20110710-tracklist-index-slave-before.sql
     echo 'TRUNCATE tracklist_index' | ./admin/psql READWRITE
     curl -o new_data.tar.bz2 "ftp://data.musicbrainz.org/pub/musicbrainz/data/20110711-update.tar.bz2"
     ./admin/MBImport.pl 20110711-update.tar.bz2
-    rm new_data.tar.bz2
+    ./admin/psql READWRITE < admin/sql/updates/20110710-tracklist-index-slave-after.sql
+    rm 20110711-update.tar.bz
 fi
 
 echo `date` : Done
