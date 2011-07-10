4174a8671095062095547dafebc2daf9439a7c67
Oliver Charles <oliver.g.charles@googlemail.com>
2011-07-10 19:33:10 +0100

    Recreate tracklist_index

--- upgrade.sh
+++ upgrade.sh
@@ -7,8 +7,11 @@ eval `./admin/ShowDBDefs`
 
 if [ $REPLICATION_TYPE == $RT_MASTER ]
 then
+    echo `date` : Recreating tracklist_index
+    ./admin/psql READWRITE < admin/sql/updates/201107010-tracklist-index.sql
+
     echo `date` : Dumping now-replicated tables
-    ./admin/ExportAllTables --table=url_gid_redirect --table=work_alias
+    ./admin/ExportAllTables --table=url_gid_redirect --table=work_alias --table=tracklist_index
     mv mbdump.tar.bz2 /var/ftp/pub/musicbrainz/data/20110711-update.tar.bz2
     rm mbdump*.tar.bz2
 
@@ -19,6 +22,7 @@ then
 elif [ $REPLICATION_TYPE == $RT_SLAVE ]
 then
     echo `date` : Importing new non-replicated data
+    echo 'TRUNCATE tracklist_index' | ./admin/psql READWRITE
     curl -o new_data.tar.bz2 "ftp://data.musicbrainz.org/pub/musicbrainz/data/20110711-update.tar.bz2"
     ./admin/MBImport.pl 20110711-update.tar.bz2
     rm new_data.tar.bz2
