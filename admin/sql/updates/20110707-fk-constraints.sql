36603e128541034369c0529d38e5b63f12225317
Oliver Charles <oliver.g.charles@googlemail.com>
2011-07-11 11:45:38 +0100

    Fix potential FK violations in raw rating and tag tables

--- admin/sql/updates/20110707-fk-constraints.sql
+++ admin/sql/updates/20110707-fk-constraints.sql
@@ -1,5 +1,18 @@
 BEGIN;
 
+DELETE FROM artist_rating_raw WHERE (editor, artist) IN (SELECT editor, artist FROM artist_rating_Raw LEFT JOIN artist ON artist.id = artist WHERE artist.id IS NULL);
+DELETE FROM label_rating_raw WHERE (editor, label) IN (SELECT editor, label FROM label_rating_raw LEFT JOIN label ON label.id = label WHERE label.id IS NULL);
+DELETE FROM release_group_rating_raw WHERE (editor, release_group) IN (SELECT editor, release_group FROM release_group_rating_Raw LEFT JOIN release_group ON release_group.id = release_group WHERE release_group.id IS NULL);
+DELETE FROM recording_rating_raw WHERE (editor, recording) IN (SELECT editor, recording FROM recording_rating_raw LEFT JOIN recording ON recording.id = recording WHERE recording.id IS NULL);
+DELETE FROM work_rating_raw WHERE (editor, work) IN (SELECT editor, work FROM work_rating_raw LEFT JOIN work ON work.id = work WHERE work.id IS NULL);
+
+DELETE FROM artist_tag_raw WHERE (editor, artist) IN (SELECT editor, artist FROM artist_tag_Raw LEFT JOIN artist ON artist.id = artist WHERE artist.id IS NULL);
+DELETE FROM label_tag_raw WHERE (editor, label) IN (SELECT editor, label FROM label_tag_raw LEFT JOIN label ON label.id = label WHERE label.id IS NULL);
+DELETE FROM release_group_tag_raw WHERE (editor, release_group) IN (SELECT editor, release_group FROM release_group_tag_Raw LEFT JOIN release_group ON release_group.id = release_group WHERE release_group.id IS NULL);
+DELETE FROM release_tag_raw WHERE (editor, release) IN (SELECT editor, release FROM release_tag_raw LEFT JOIN release ON release.id = release WHERE release.id IS NULL);
+DELETE FROM recording_tag_raw WHERE (editor, recording) IN (SELECT editor, recording FROM recording_tag_raw LEFT JOIN recording ON recording.id = recording WHERE recording.id IS NULL);
+DELETE FROM work_tag_raw WHERE (editor, work) IN (SELECT editor, work FROM work_tag_raw LEFT JOIN work ON work.id = work WHERE work.id IS NULL);
+
 DELETE FROM edit_artist WHERE (edit, artist) IN (SELECT edit, edit_artist.artist FROM edit_artist LEFT JOIN artist ON artist.id = edit_artist.artist WHERE artist.id IS NULL);
 DELETE FROM edit_label WHERE (edit, label) IN (SELECT edit, edit_label.label FROM edit_label LEFT JOIN label ON label.id = edit_label.label WHERE label.id IS NULL);
 DELETE FROM edit_recording WHERE (edit, recording) IN (SELECT edit, edit_recording.recording FROM edit_recording LEFT JOIN recording ON recording.id = edit_recording.recording WHERE recording.id IS NULL);
