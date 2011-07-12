b40944fe8776e5a395f4c0ba1a3301e7f405843a
Oliver Charles <oliver.g.charles@googlemail.com>
2011-07-12 12:48:47 +0100

    Do not create a replication trigger for tracklist_index on slaves

--- admin/sql/updates/20110710-tracklist-index-slave-after.sql
+++ admin/sql/updates/20110710-tracklist-index-slave-after.sql
@@ -3,8 +3,4 @@ BEGIN;
 ALTER TABLE tracklist_index ADD CONSTRAINT tracklist_index_pkey PRIMARY KEY (tracklist);
 CREATE INDEX tracklist_index_idx ON tracklist_index USING gist (toc);
 
-CREATE TRIGGER "reptg_tracklist_index"
-AFTER INSERT OR DELETE OR UPDATE ON "tracklist_index"
-FOR EACH ROW EXECUTE PROCEDURE "recordchange" ();
-
 COMMIT;
