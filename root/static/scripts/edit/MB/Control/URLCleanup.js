84116a27fdcb30a2f4e1cdf16bdfb9148853380c
Nikki <nikki@miaubuntu.(none)>
2011-07-18 00:08:05 +0100

    Only allow domains on the cover art whitelist

--- root/static/scripts/edit/MB/Control/URLCleanup.js
+++ root/static/scripts/edit/MB/Control/URLCleanup.js
@@ -278,7 +278,11 @@ MB.Control.URLCleanup = function (sourceType, typeControl, urlControl) {
     validationRules[ MB.constants.LINK_TYPES.discogs.release ] = function() {
         return $('#id-ar\\.url').val().match(/\/(release|mp3)\//) != null;
     }
-
+    // only allow domains on the cover art whitelist
+    validationRules[ MB.constants.LINK_TYPES.coverart.release ] = function() {
+        var sites = new RegExp("^(https?://)?([^/]+\.)?(archive\.org|magnatune\.com|jamendo\.com|cdbaby.(com|name)|ozon\.ru|mange-disque\.tv|encyclopedisque\.fr|thastrom\.se|universalpoplab\.com|alpinechic\.net|angelika-express\.de|fixtstore\.com|phantasma13\.com|primordialmusic\.com|transistorsounds\.com|alter-x\.net|zorchfactoryrecords\.com)/");
+        return sites.test($('#id-ar\\.url').val())
+    };
 
     self.guessType = function (sourceType, currentURL) {
         for (var group in cleanups) {
