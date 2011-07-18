46f30394b7bd57182f37323ce71d0e6fb1bef7d8
Oliver Charles <oliver.g.charles@googlemail.com>
2011-07-18 17:03:45 +0100

    Only allow valid ISRCs and ISWCs to be stored in the database

---
+++ admin/sql/updates/20110718-isrc-validation.sql
@@ -0,0 +1,7 @@
+BEGIN;
+
+DELETE FROM isrc WHERE isrc !~ E'^[A-Z]{2}[A-Z0-9]{3}[0-9]{7}$';
+ALTER TABLE isrc ADD CONSTRAINT isrc_check_isrc CHECK (isrc ~ E'^[A-Z]{2}[A-Z0-9]{3}[0-9]{7}$');
+ALTER TABLE work ADD CONSTRAINT work_check_iswc CHECK (iswc ~ E'^T-?\\d{3}\.?\\d{3}\.?\\d{3}[-.]?\\d$');
+
+COMMIT;
