2205934b61abd9d81537085fb0d74201b0c8ba41
Oliver Charles <oliver.g.charles@googlemail.com>
2011-08-02 10:31:13 +0100

    Explain some magic constants in the upgrade process

--- admin/sql/updates/20110725-rebuild-editor-stats.sql
+++ admin/sql/updates/20110725-rebuild-editor-stats.sql
@@ -4,7 +4,7 @@ UPDATE editor SET edits_failed = s.failed
 FROM (
     SELECT editor, count(id) AS failed
     FROM edit
-    WHERE status NOT IN (4, 5, 6, 7)
+    WHERE status NOT IN (4, 5, 6, 7) -- FAILEDDEP, ERROR, FAILEDPREREQ, NOVOTES
     GROUP BY editor
 ) s
 WHERE editor.id = s.editor;
