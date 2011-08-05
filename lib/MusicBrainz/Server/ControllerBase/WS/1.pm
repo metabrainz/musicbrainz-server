0919bd1cf4ec0a870ee4f5d23a1fea19e0535dc2
Oliver Charles <oliver.g.charles@googlemail.com>
2011-08-05 18:39:30 +0100

    Fix /ws/1/ limit and offset parsing if they are not present

--- lib/MusicBrainz/Server/ControllerBase/WS/1.pm
+++ lib/MusicBrainz/Server/ControllerBase/WS/1.pm
@@ -73,10 +73,10 @@ sub search : Chained('root') PathPart('')
 {
     my ($self, $c) = @_;
 
-    my $limit = 0 + (int($c->req->query_params->{limit}) || 25);
+    my $limit = $c->req->query_params->{limit} ? int($c->req->query_params->{limit}) : 25;
     $limit = 25 if $limit < 1 || $limit > 100;
 
-    my $offset = 0 + (int($c->req->query_params->{offset}) || 0);
+    my $offset = $c->req->query_params->{offset} ? int($c->req->query_params->{offset}) : 0;
     $offset = 0 if $offset < 0;
 
     try {
