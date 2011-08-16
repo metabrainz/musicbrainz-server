8bbaaa693e5f53b9780f35ed8a7ed48927daa1ab
Oliver Charles <oliver.g.charles@googlemail.com>
2011-08-16 15:15:39 +0100

    Clarify about unlinked artists having to exist.

--- lib/MusicBrainz/Server/Form/EditArtistCredit.pm
+++ lib/MusicBrainz/Server/Form/EditArtistCredit.pm
@@ -7,7 +7,9 @@ with 'MusicBrainz::Server::Form::Role::Edit';
 has '+name' => ( default => 'split-artist' );
 
 has_field 'artist_credit' => (
-    type => '+MusicBrainz::Server::Form::Field::ArtistCredit'
+    type => '+MusicBrainz::Server::Form::Field::ArtistCredit',
+    required => 1,
+    allow_unlinked => 0
 );
 
 sub edit_field_names { qw( artist_credit )}
