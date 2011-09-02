metabrainz/musicbrainz-server

commit 45df1308da007f71f5c7202df45b24f2dbe57504

MBS-3355, Use recording duration to set track duration if no duration specified for track.

lib/MusicBrainz/Server/Wizard/ReleaseEditor.pm
@@ -1247,7 +1247,7 @@ sub _expand_track
     }
 
     my $entity = Track->new(
-        length => unformat_track_length ($trk->{length}),
+        length => unformat_track_length ($trk->{length}) // $assoc->length,
         name => $trk->{name},
         position => trim ($trk->{position}),
         artist_credit => ArtistCredit->from_array ([

commit c40ab5cb6f932c0c63cb0e09bff28c9ecfc96cb0

MBS-3355, fix crash introduced by commit 45df1308da007f71f5c7202df45b24f2dbe57504.

lib/MusicBrainz/Server/Wizard/ReleaseEditor.pm
@@ -1247,7 +1247,7 @@ sub _expand_track
     }
 
     my $entity = Track->new(
-        length => unformat_track_length ($trk->{length}) // $assoc->length,
+        length => unformat_track_length ($trk->{length}) // ($assoc ? $assoc->length : undef),
         name => $trk->{name},
         position => trim ($trk->{position}),
         artist_credit => ArtistCredit->from_array ([