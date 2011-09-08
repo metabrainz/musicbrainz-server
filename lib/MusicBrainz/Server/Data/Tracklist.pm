f9f176c993daca9a81ad9f8a5a7fc7868a34cbfb
Oliver Charles <oliver.g.charles@googlemail.com>
2011-09-08 12:44:37 +0100

    Replacing tracklists should check for orphaned recordings

--- lib/MusicBrainz/Server/Data/Tracklist.pm
+++ lib/MusicBrainz/Server/Data/Tracklist.pm
@@ -45,8 +45,17 @@ sub delete
 sub replace
 {
     my ($self, $tracklist_id, $tracks) = @_;
-    $self->sql->do('DELETE FROM track WHERE tracklist = ?', $tracklist_id);
+    my @possibly_orphaned_recordings = @{
+        $self->sql->select_single_column_array(
+            'DELETE FROM track WHERE tracklist = ? RETURNING recording', $tracklist_id
+        )
+    };
+
     $self->_add_tracks($tracklist_id, $tracks);
+
+    # Check if any recordings are orphaned. This is done after adding tracks, as
+    # we may simply be re-ordering the tracklist.
+    $self->c->model('Recording')->garbage_collect_orphans(@possibly_orphaned_recordings);
 }
 
 sub _add_tracks {
