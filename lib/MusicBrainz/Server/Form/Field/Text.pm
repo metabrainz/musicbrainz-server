2e08b1f97b30a705f66c111298ce4961f63660a3
Oliver Charles <oliver.g.charles@googlemail.com>
2011-09-12 11:14:44 +0100

    Use Text::Trim to trim text input in form fields

--- lib/MusicBrainz/Server/Form/Field/Text.pm
+++ lib/MusicBrainz/Server/Form/Field/Text.pm
@@ -2,14 +2,15 @@ package MusicBrainz::Server::Form::Field::Text;
 use HTML::FormHandler::Moose;
 extends 'HTML::FormHandler::Field::Text';
 
+use Text::Trim qw( trim );
+
 apply ([
     {
         transform => sub {
             my $text = shift;
             $text =~ s/[^[:print:]]//g;
             $text =~ s/\s+/ /g;
-            $text =~ s/\s*(.*)\s*/$1/g;
-            return $text;
+            return trim($text);
         }
     }
 ]);
