197816ad9f83f71167b17e260df4efb665d9d4c5
Oliver Charles <oliver.g.charles@googlemail.com>
2011-09-20 15:24:12 +0100

    Superfluous data tracks report is only valid for mediums that can have a disc ID

--- lib/MusicBrainz/Server/Report/SuperfluousDataTracks.pm
+++ lib/MusicBrainz/Server/Report/SuperfluousDataTracks.pm
@@ -15,10 +15,12 @@ SELECT DISTINCT
 FROM release
 JOIN release_name ON release_name.id = release.name
 JOIN medium ON medium.release = release.id
+LEFT JOIN medium_format ON medium.format = medium_format.id
 JOIN tracklist ON medium.tracklist = tracklist.id
 JOIN track ON tracklist.id = track.tracklist
 JOIN track_name ON track_name.id = track.name
-WHERE track.position = tracklist.track_count
+WHERE (medium_format.has_discids = TRUE OR medium_format.has_discids IS NULL)
+AND track.position = tracklist.track_count
 AND track_name.name ~* '([[:<:]](dat(a|en)|cccd|gegevens|video)[[:>:]]|\\u30C7\\u30FC\\u30BF)'
 AND NOT EXISTS (
    SELECT TRUE FROM medium_cdtoc WHERE medium_cdtoc.medium = medium.id
