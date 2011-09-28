06c96680e89e88b789848105c11dfc6e816eb4d2
Oliver Charles <oliver.g.charles@googlemail.com>
2011-09-28 13:10:11 +0100

    Add an upgrade script to remove redundant aliases
    An alias is redundant if the alias name is the same as the name as the entity
    that it is attached to.

---
+++ admin/sql/updates/20110928-remove-redundant-aliases.sql
@@ -0,0 +1,21 @@
+BEGIN;
+
+DELETE FROM artist_alias WHERE id IN (
+    SELECT artist_alias.id FROM artist_alias
+    JOIN artist ON artist.id = artist_alias.artist
+    WHERE artist_alias.name = artist.name
+);
+
+DELETE FROM label_alias WHERE id IN (
+    SELECT label_alias.id FROM label_alias
+    JOIN label ON label.id = label_alias.label
+    WHERE label_alias.name = label.name
+);
+
+DELETE FROM work_alias WHERE id IN (
+    SELECT work_alias.id FROM work_alias
+    JOIN work ON work.id = work_alias.work
+    WHERE work_alias.name = work.name
+);
+
+COMMIT;
