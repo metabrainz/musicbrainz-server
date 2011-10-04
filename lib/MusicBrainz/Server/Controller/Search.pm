metabrainz/musicbrainz-server

commit 613ec42153cd131a903b436245cdcf9bc8e465ea

MBS-3545: Make the tag lookup page able to search again
My "improvements" meant it redirected to an external search, using the special
"alias" query, but this makes it really difficult for users to search
again. Now, it forwards to the external search logic, but displays the results
itself and display the same form as before. This allows a user to change their
search query, but still have the power of an external search to do the
searching.

lib/MusicBrainz/Server/Controller/Search.pm
@@ -2,10 +2,12 @@ package MusicBrainz::Server::Controller::Search;
 use Moose;
 BEGIN { extends 'MusicBrainz::Server::Controller' }
 
+use List::Util qw( min max );
 use LWP::UserAgent;
 use MusicBrainz::Server::Data::Utils qw( model_to_type type_to_model );
 use MusicBrainz::Server::Form::Search::Query;
 use MusicBrainz::Server::Form::Search::Search;
+use Scalar::Util qw( looks_like_number );
 use feature 'switch';
 
 sub search : Path('')
@@ -133,12 +135,37 @@ sub external : Private
 
     $c->detach('/search/editor') if $type eq 'editor';
 
-    my $limit  = $form->field('limit')->value;
-    my $page   = $c->request->query_params->{page} || 1;
-    my $adv    = $form->field('advanced') ? $form->field('advanced')->value : 0;
+    $self->do_external_search($c,
+                              query    => $query,
+                              type     => $type,
+                              limit    => $form->field('limit')->value,
+                              page     => $c->request->query_params->{page},
+                              advanced => $form->field('advanced'));
+
+    $c->stash->{template} ="search/results-$type.tt";
+}
+
+sub do_external_search {
+    my ($self, $c, %opts) = @_;
+
+    my $page  = looks_like_number($c->request->query_params->{page})
+                    ? $c->request->query_params->{page} : 1;
+    $page = max(1, $page);
+
+    my $limit = looks_like_number($opts{limit}) ? $opts{limit} : 25;
+    $limit = min(max(1, $limit), 100);
+
+    my $advanced = $opts{advanced} ? 1 : 0;
+
+    my $query = $opts{query};
+    my $type  = $opts{type};
 
     my $search = $c->model('Search');
-    my $ret = $search->external_search($type, $query, $limit, $page, $adv);
+    my $ret = $search->external_search($type,
+                                       $query,
+                                       $limit,
+                                       $page,
+                                       $advanced);
 
     if (exists $ret->{error})
     {
@@ -169,7 +196,6 @@ sub external : Private
         $c->stash->{pager}    = $ret->{pager};
         $c->stash->{offset}   = $ret->{offset};
         $c->stash->{results}  = $ret->{results};
-        $c->stash->{template} ="search/results-$type.tt";
     }
 }
 

lib/MusicBrainz/Server/Controller/TagLookup.pm
@@ -120,7 +120,11 @@ sub puid : Private
 
     my @results = map { { entity => $_ } } @releases;
 
-    $c->stash->{results} = \@results;
+    $c->stash(
+        # A PUID search displays releases as results
+        type    => 'release',
+        results => \@results
+    );
 }
 
 sub external : Private
@@ -176,14 +180,11 @@ sub external : Private
         $c->detach('not_found');
     }
 
-    $c->response->redirect(
-        $c->uri_for_action(
-            '/search/search', {
-                query => join(' ', @search_modifiers),
-                type => $type,
-                advanced => 1
-            }));
-    $c->detach;
+    $c->stash( type => $type );
+    $c->controller('Search')->do_external_search($c,
+                                                 query    => join(' ', @search_modifiers),
+                                                 type     => $type,
+                                                 advanced => 1);
 }
 
 
@@ -203,14 +204,14 @@ sub index : Path('')
 
     return unless $form->submitted_and_valid( $mapped_params );
 
-    $c->stash( template => 'taglookup/results-release.tt' );
-
     if ($form->field('puid')->value()) {
         $self->puid($c, $form);
     }
     else {
         $self->external($c, $form);
     }
+
+    $c->stash( template => 'taglookup/results.tt' );
 }
 
 1;

root/components/results-release.tt
@@ -1,42 +1,0 @@
-[% WRAPPER 'components/with-pager.tt' search=1 %]
-<table class="tbl">
-    <thead>
-        <tr>
-            [%- IF results.0.score -%]
-            <th>[% l('Score') %]</th>
-            [%- END -%]
-            <th>[% l('Name') %]</th>
-            <th>[% l('Artist') %]</th>
-            <th>[% l('Tracks') %]</th>
-            <th>[% l('Country') %]</th>
-            <th>[% l('Language') %]</th>
-            [%- IF c.session.tport -%]
-                <th>[% l('Tagger') %]</th>
-            [%- END -%]
-        </tr>
-    </thead>
-    <tbody>
-        [%- FOR result=results -%]
-        <tr[% ' class="ev"' IF loop.count % 2 == 0 %]>
-            [%- IF results.0.score -%]
-            <td>[% result.score %]</td>
-            [%- END -%]
-            <td>[% link_entity(result.entity) %]</td>
-            <td>[% artist_credit(result.entity.artist_credit) %]</td>
-            <td>[% result.entity.combined_track_count %]</td>
-            <td>
-                <abbr title="[% result.entity.country.name %]">[% result.entity.country.iso_code_for_display %]</abbr>
-            </td>
-            <td>
-                <abbr title="[% result.entity.language.name %]">[% result.entity.language.iso_code_3t %]</abbr>
-                [%= '/' IF result.entity.language && result.entity.script =%]
-                <abbr title="[% result.entity.script.name %]">[% result.entity.script.iso_code %]</abbr>
-            </td>
-            [%- IF c.session.tport -%]
-                <td>[% tagger_icon(result.entity) %]</td>
-            [%- END -%]
-        </tr>
-        [%- END -%]
-    </tbody>
-</table>
-[% END %]

root/search/lib/inline-results-artist.tt
@@ -1,0 +1,32 @@
+[%- IF results.size -%]
+  [% WRAPPER 'components/with-pager.tt' search=1 %]
+  <table class="tbl">
+    <thead>
+      <tr>
+        <th>[% l('Score') %]</th>
+        <th>[% l('Name') %]</th>
+        <th>[% l('Sort Name') %]</th>
+        <th>[% l('Type') %]</th>
+        <th>[% l('Begin') %]</th>
+        <th>[% l('End') %]</th>
+      </tr>
+    </thead>
+    <tbody>
+      [%- FOR result=results -%]
+      <tr[% ' class="ev"' IF loop.count % 2 == 0 %]>
+        <td>[% result.score %]</td>
+        <td>
+          [% link_entity(result.entity) %]
+        </td>
+        <td>[% result.entity.sort_name %]</td>
+        <td>[% result.entity.type_name  %]</td>
+        <td>[% result.entity.begin_date.format %]</td>
+        <td>[% result.entity.end_date.format %]</td>
+      </tr>
+      [%- END -%]
+    </tbody>
+  </table>
+  [% END %]
+[%- ELSE -%]
+  <p>[% l('No results found. Try refining your search query.') %]</p>
+[%- END -%]

root/search/lib/inline-results-recording.tt
@@ -1,0 +1,58 @@
+[%- IF results.size -%]
+  [% WRAPPER 'components/with-pager.tt' search=1 %]
+  <table class="tbl">
+    <thead>
+      <tr>
+        <th>[% l('Score') %]</th>
+        <th>[% l('Name') %]</th>
+        <th class="treleases">[% l('Length') %]</th>
+        <th>[% l('Artist') %]</th>
+        <th>[% l('Release') %]</th>
+        [%- IF c.session.tport -%]
+          <th>[% l('Tagger') %]</th>
+        [%- END %]
+        <th class="t pos">[% l('Track') %]</th>
+        <th>[% l('Medium') %]</th>
+        <th>[% l('Type') %]</th>
+      </tr>
+    </thead>
+    <tbody>
+      [%- linenum = 0 -%]
+      [%- FOR result=results -%]
+        [% IF result.extra.size %]
+          [%- FOR release=result.extra -%]
+            <tr[% ' class="ev"' IF linenum % 2 == 0 %]>
+              [%- IF loop.count == 1 -%]
+                <td>[% result.score %]</td>
+                <td>[% link_entity(result.entity) %]</td>
+                <td>[% result.entity.length | format_length %]</td>
+                <td>[% artist_credit(result.entity.artist_credit) %]</td>
+              [%- ELSE -%]
+                <td colspan="4">&nbsp;</td>
+              [%- END -%]
+              <td>[%- link_entity(release) -%]</td>
+              [%- IF c.session.tport -%]
+                <td>[% tagger_icon(result.entity) %]</td>
+              [%- END -%]
+              <td>[%- release.find_track_for_recording(result.entity).position -%]/[%- release.combined_track_count -%]</td>
+              <td>[%- release.find_medium_for_recording(result.entity).position -%]</td>
+              <td>[%- release.release_group.type.name -%]</td>
+              [%- linenum = linenum + 1 -%]
+            </tr>
+          [%- END -%]
+        [% ELSE %]
+          <tr[% ' class="ev"' IF linenum % 2 == 0 %]>
+            <td>[% result.score %]</td>
+            <td>[% link_entity(result.entity) %]</td>
+            <td>[% result.entity.length | format_length %]</td>
+            <td>[% artist_credit(result.entity.artist_credit) %]</td>
+            <td colspan="4" style="text-align: center">[% l('(standalone recording)') %]</td>
+          </tr>
+        [% END %]
+      [%- END -%]
+    </tbody>
+  </table>
+  [% END %]
+[%- ELSE -%]
+  <p>[% l('No results found') %]</p>
+[%- END -%]

root/search/lib/inline-results-release.tt
@@ -1,0 +1,46 @@
+[%- IF results.size -%]
+  [% WRAPPER 'components/with-pager.tt' search=1 %]
+  <table class="tbl">
+    <thead>
+      <tr>
+        [%- IF results.0.score -%]
+          <th>[% l('Score') %]</th>
+        [%- END -%]
+        <th>[% l('Name') %]</th>
+        <th>[% l('Artist') %]</th>
+        <th>[% l('Tracks') %]</th>
+        <th>[% l('Country') %]</th>
+        <th>[% l('Language') %]</th>
+        [%- IF c.session.tport -%]
+          <th>[% l('Tagger') %]</th>
+        [%- END -%]
+      </tr>
+    </thead>
+    <tbody>
+      [%- FOR result=results -%]
+        <tr[% ' class="ev"' IF loop.count % 2 == 0 %]>
+          [%- IF results.0.score -%]
+            <td>[% result.score %]</td>
+          [%- END -%]
+          <td>[% link_entity(result.entity) %]</td>
+          <td>[% artist_credit(result.entity.artist_credit) %]</td>
+          <td>[% result.entity.combined_track_count %]</td>
+          <td>
+            <abbr title="[% result.entity.country.name %]">[% result.entity.country.iso_code_for_display %]</abbr>
+          </td>
+          <td>
+            <abbr title="[% result.entity.language.name %]">[% result.entity.language.iso_code_3t %]</abbr>
+            [%= '/' IF result.entity.language && result.entity.script =%]
+            <abbr title="[% result.entity.script.name %]">[% result.entity.script.iso_code %]</abbr>
+          </td>
+          [%- IF c.session.tport -%]
+            <td>[% tagger_icon(result.entity) %]</td>
+          [%- END -%]
+        </tr>
+      [%- END -%]
+    </tbody>
+  </table>
+  [% END %]
+[% ELSE %]
+  <p>[% l('No results found') %]</p>
+[% END %]

root/search/results-artist.tt
@@ -1,38 +1,8 @@
 [% WRAPPER "layout.tt" title=l("Search Results") full_width=1 %]
     <div id="content">
         <h1>[% l("Search Results") %]</h1>
-        [%- IF results.size -%]
-            [% WRAPPER 'components/with-pager.tt' search=1 %]
-                <table class="tbl">
-                    <thead>
-                        <tr>
-                            <th>[% l('Score') %]</th>
-                            <th>[% l('Name') %]</th>
-                            <th>[% l('Sort Name') %]</th>
-                            <th>[% l('Type') %]</th>
-                            <th>[% l('Begin') %]</th>
-                            <th>[% l('End') %]</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        [%- FOR result=results -%]
-                        <tr[% ' class="ev"' IF loop.count % 2 == 0 %]>
-                            <td>[% result.score %]</td>
-                            <td>
-                                [% link_entity(result.entity) %]
-                            </td>
-                            <td>[% result.entity.sort_name %]</td>
-                            <td>[% result.entity.type_name  %]</td>
-                            <td>[% result.entity.begin_date.format %]</td>
-                            <td>[% result.entity.end_date.format %]</td>
-                        </tr>
-                        [%- END -%]
-                    </tbody>
-                </table>
-            [% END %]
-        [%- ELSE -%]
-            <p>[% l('No results found. Try refining your search query.') %]</p>
-        [%- END -%]
+        [% INCLUDE 'search/lib-inline-results-artist.tt' %]
+
         <p>
           [% l('Alternatively, you may {uri|add a new artist}.', {
                 uri => c.uri_for_action('/artist/create', {

root/search/results-recording.tt
@@ -1,64 +1,8 @@
 [% WRAPPER "layout.tt" title=l("Search Results") full_width=1 %]
     <div id="content">
         <h1>[% l("Search Results") %]</h1>
-        [%- IF results.size -%]
-            [% WRAPPER 'components/with-pager.tt' search=1 %]
-                <table class="tbl">
-                    <thead>
-                        <tr>
-                            <th>[% l('Score') %]</th>
-                            <th>[% l('Name') %]</th>
-                            <th class="treleases">[% l('Length') %]</th>
-                            <th>[% l('Artist') %]</th>
-                            <th>[% l('Release') %]</th>
-                            [%- IF c.session.tport -%]
-                              <th>[% l('Tagger') %]</th>
-                            [%- END %]
-                            <th class="t pos">[% l('Track') %]</th>
-                            <th>[% l('Medium') %]</th>
-                            <th>[% l('Type') %]</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        [%- linenum = 0 -%]
-                        [%- FOR result=results -%]
-                            [% IF result.extra.size %]
-                              [%- FOR release=result.extra -%]
-                                <tr[% ' class="ev"' IF linenum % 2 == 0 %]>
-                                    [%- IF loop.count == 1 -%]
-                                        <td>[% result.score %]</td>
-                                        <td>[% link_entity(result.entity) %]</td>
-                                        <td>[% result.entity.length | format_length %]</td>
-                                        <td>[% artist_credit(result.entity.artist_credit) %]</td>
-                                    [%- ELSE -%]
-                                        <td colspan="4">&nbsp;</td>
-                                    [%- END -%]
-                                    <td>[%- link_entity(release) -%]</td>
-                                    [%- IF c.session.tport -%]
-                                        <td>[% tagger_icon(result.entity) %]</td>
-                                    [%- END -%]
-                                    <td>[%- release.find_track_for_recording(result.entity).position -%]/[%- release.combined_track_count -%]</td>
-                                    <td>[%- release.find_medium_for_recording(result.entity).position -%]</td>
-                                    <td>[%- release.release_group.type.name -%]</td>
-                                    [%- linenum = linenum + 1 -%]
-                                </tr>
-                              [%- END -%]
-                            [% ELSE %]
-                              <tr[% ' class="ev"' IF linenum % 2 == 0 %]>
-                                <td>[% result.score %]</td>
-                                <td>[% link_entity(result.entity) %]</td>
-                                <td>[% result.entity.length | format_length %]</td>
-                                <td>[% artist_credit(result.entity.artist_credit) %]</td>
-                                <td colspan="4" style="text-align: center">[% l('(standalone recording)') %]</td>
-                              </tr>
-                            [% END %]
-                        [%- END -%]
-                    </tbody>
-                </table>
-            [% END %]
-        [%- ELSE -%]
-            <p>[% l('No results found') %]</p>
-        [%- END -%]
+        [% INCLUDE 'search/lib/inline-results-recording.tt' %]
+
         [%- INCLUDE "search/form.tt" -%]
     </div>
 [% END %]

commit f3a14500c8fc0d46cee17dbe06fb95f71d86e30e

Fix indexed search queries to obey the value of the 'advanced' checkbox
613ec42153cd131 introduced a regression where by this box was thought to be always checked.

lib/MusicBrainz/Server/Controller/Search.pm
@@ -140,7 +140,7 @@ sub external : Private
                               type     => $type,
                               limit    => $form->field('limit')->value,
                               page     => $c->request->query_params->{page},
-                              advanced => $form->field('advanced'));
+                              advanced => $form->field('advanced')->value);
 
     $c->stash->{template} ="search/results-$type.tt";
 }