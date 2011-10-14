66fafd124a89a2f73bc5f43a098276b3f02f71d9
Oliver Charles <oliver.g.charles@googlemail.com>
2011-10-14 12:46:48 +0000

    Fix the TracksNamedWithSequence report to only show with releases where at least 3 tracks have problems
    This matches the classic report behavior

--- lib/MusicBrainz/Server/Report/TracksNamedWithSequence.pm
+++ lib/MusicBrainz/Server/Report/TracksNamedWithSequence.pm
@@ -8,17 +8,24 @@ sub gather_data {
     my ($self, $writer) = @_;
 
     $self->gather_data_from_query($writer, <<'EOSQL');
-SELECT DISTINCT release_name.name, release.id, release.gid,
-                release.artist_credit AS artist_credit_id,
-                release.edits_pending,
-                musicbrainz_collate(release_name.name)
-FROM track
-JOIN track_name tname ON tname.id = track.name
-JOIN medium ON track.tracklist = medium.tracklist
-JOIN release ON medium.release = release.id
-JOIN release_name ON release.name = release_name.id
-WHERE tname.name ~ '^[0-9]'
-AND   tname.name ~ ('^0*' || track.position || '[^0-9]')
+SELECT release.id, release.gid,
+       release.artist_credit AS artist_credit_id,
+       release.edits_pending,
+       release_name.name
+FROM (
+  SELECT release.id
+  FROM track
+  JOIN track_name tname ON tname.id = track.name
+  JOIN medium ON track.tracklist = medium.tracklist
+  JOIN release ON medium.release = release.id
+  JOIN release_name ON release.name = release_name.id
+  WHERE tname.name ~ '^[0-9]'
+  AND   tname.name ~ ('^0*' || track.position || '[^0-9]')
+  GROUP BY release.id
+  HAVING count(*) > 2
+) s
+JOIN release ON s.id = release.id
+JOIN release_name ON release_name.id = release.name
 ORDER BY musicbrainz_collate(release_name.name)
 EOSQL
 }
