59b352f07578a60a08a593ed1602b1ab565da771
warp <kuno@frob.nl>
2011-10-14 10:46:58 -0500

    PostgreSQL 9.1 compatibility fixes (work in progress)

--- lib/MusicBrainz/Server/Test/Connector.pm
+++ lib/MusicBrainz/Server/Test/Connector.pm
@@ -3,7 +3,7 @@ use Moose;
 
 extends 'MusicBrainz::Server::Connector';
 
-sub _schema { 'musicbrainz_test' }
+sub _schema { 'musicbrainz_test,musicbrainz' }
 
 __PACKAGE__->meta->make_immutable;
 no Moose;
