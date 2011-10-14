68a073b58a213d16e816bdc00c312e69f5240c17
warp <kuno@frob.nl>
2011-10-14 10:46:58 -0500

    PostgreSQL 9.1 compatibility fixes (work in progress)

--- script/create_test_db.sh
+++ script/create_test_db.sh
@@ -3,12 +3,19 @@
 set -o errexit
 cd `dirname $0`/..
 
-echo 'DROP SCHEMA musicbrainz_test CASCADE;' | ./admin/psql --profile=test READWRITE
-echo 'CREATE SCHEMA musicbrainz_test;' | ./admin/psql --profile=test READWRITE
+echo 'DROP SCHEMA musicbrainz_test CASCADE;' | ./admin/psql READWRITE
+echo 'CREATE SCHEMA musicbrainz_test;' | ./admin/psql READWRITE
 
-echo `date` : Installing cube extension
-./admin/InitDb.pl --install-extension=cube.sql --extension-schema=musicbrainz_test
-./admin/InitDb.pl --install-extension=musicbrainz_collate.sql  --extension-schema=musicbrainz_test
+PG_VERSION=$( echo "SELECT version()" | ./admin/psql READWRITE | egrep 'PostgreSQL 9.[^0].' )
+
+echo `date` : Installing extensions
+if [ -n "$PG_VERSION" ]; then
+#    ./admin/psql --profile=test READWRITE < ./admin/sql/Extensions.sql
+    true
+else
+    ./admin/InitDb.pl --install-extension=cube.sql --extension-schema=musicbrainz_test
+    ./admin/InitDb.pl --install-extension=musicbrainz_collate.sql  --extension-schema=musicbrainz_test
+fi
 
 ./admin/psql --profile=test READWRITE <./admin/sql/CreateTables.sql
 ./admin/psql --profile=test READWRITE <./admin/sql/CreateFunctions.sql
