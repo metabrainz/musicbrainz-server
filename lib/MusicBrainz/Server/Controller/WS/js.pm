8912c6e84495a42279ff20965720819cf8175fb8
warp <kuno@frob.nl>
2011-10-20 13:13:53 +0200

    MBS-211, clean up content-type negotiation code.
    Conflicts:
    
    	lib/MusicBrainz/Server/Controller/WS/js.pm

--- lib/MusicBrainz/Server/Controller/WS/js.pm
+++ lib/MusicBrainz/Server/Controller/WS/js.pm
@@ -6,6 +6,7 @@ BEGIN { extends 'MusicBrainz::Server::ControllerBase::WS::js'; }
 use Data::OptList;
 use Encode qw( decode encode );
 use List::UtilsBy qw( uniq_by );
+use MusicBrainz::Server::WebService::AcceptHeader;
 use MusicBrainz::Server::WebService::JSONSerializer;
 use MusicBrainz::Server::WebService::Validator;
 use MusicBrainz::Server::Filters;
@@ -42,11 +43,15 @@ my $ws_defs = Data::OptList::mkopt([
     }
 ]);
 
+with 'MusicBrainz::Server::WebService::AcceptHeader' =>
+{
+    serializers => [ 'MusicBrainz::Server::WebService::JSONSerializer' ]
+};
+
 with 'MusicBrainz::Server::WebService::Validator' =>
 {
      defs => $ws_defs,
      version => 'js',
-     default_serialization_type => 'json',
 };
 
 sub entities {
@@ -354,7 +359,7 @@ sub default : Path
 {
     my ($self, $c, $resource) = @_;
 
-    $c->stash->{serializer} = $self->serializers->{$self->get_default_serialization_type}->new();
+    $c->stash->{serializer} = $self->get_serialization ($c);
     $c->stash->{error} = "Invalid resource: $resource";
     $c->detach('bad_req');
 }
