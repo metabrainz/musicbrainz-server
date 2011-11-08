7d7deb77522dd9feafee06240645cb48f7feee21
Nicolás Tamargo <reosarevok@gmail.com>
2011-11-09 00:29:33 +0200

    Trying to fix YouTube cleanup

--- root/static/scripts/edit/MB/Control/URLCleanup.js
+++ root/static/scripts/edit/MB/Control/URLCleanup.js
@@ -273,11 +273,11 @@ MB.constants.CLEANUPS = {
         clean: function(url) { 
             url = url.replace(/^(https?:\/\/)?([^\/]+\.)?youtube\.com/, "http://www.youtube.com");
             //YouTube URL shortener
-            url = url.replace(/^(https?:\/\/)?([^\/]+\.)?youtu\.be\/([a-zA-Z0_9_-]+)/, "http://www.youtube.com/watch?v=$1");
+            url = url.replace(/^(https?:\/\/)?([^\/]+\.)?youtu\.be\/([a-zA-Z0_9_-]+)/, "http://www.youtube.com/watch?v=$2");
             //YouTube standard watch URL
-            url = url.replace(/^http:\/\/www\.youtube\.com\/(?:[^?]*)(v=[a-zA-Z0-9_-]+)(?:[^?]*)/, "http://www.youtube.com/watch?$1");
+            url = url.replace(/^http:\/\/www\.youtube\.com\/(?:[^?]*)(v=[a-zA-Z0-9_-]+)(?:[^?]*)/, "http://www.youtube.com/watch?$2");
             //YouTube embeds
-            url = url.replace(/^(https?:\/\/)?([^\/]+\.)?youtube\.com\/(?:embed|v)\/([a-zA-Z0_9_-]+)([^?]+)/, "http://www.youtube.com/watch?v=$1");
+            url = url.replace(/^(https?:\/\/)?([^\/]+\.)?youtube\.com\/(?:embed|v)\/([a-zA-Z0_9_-]+)([^?]+)/, "http://www.youtube.com/watch?v=$2");
             url = url.replace(/^(https?:\/\/)?([^\/]+\.)?vimeo\.com/, "http://vimeo.com");
 	    return url;
         }
