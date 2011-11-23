2960334986ef25f9fd32c3d93a258a32c0708580
Oliver Charles <oliver.g.charles@googlemail.com>
2011-11-23 15:58:03 +0000

    Fix URL controller tests
    The url.sql test data was incorrect as it didn't specify entity types for the
    end points of the test relationship. For data tests this was fine, as nothing
    tried to load relationships, but the URL controller tests do load relationships.
    
    I've made these columns NOT NULL and added a die statement to type_to_url if you
    try to lookup the model for a type where there is no mapping.

--- t/sql/url.sql
+++ t/sql/url.sql
@@ -11,8 +11,9 @@ INSERT INTO url (id, gid, url, description, ref_count)
     VALUES (3, '25d6b63a-12dc-41c9-858a-2f42ae610a7d', 'http://zh-yue.wikipedia.org/wiki/王菲',
         'Cantonese wikipedia page of Faye Wong', 1);
 
-INSERT INTO link_type (id, name, gid, link_phrase, short_link_phrase, reverse_link_phrase)
-  VALUES (1, 'wikipedia', 'fcd58926-4243-40bb-a2e5-c7464b3ce577', 'wikipedia', 'wikipedia', 'wikipedia');
+INSERT INTO link_type (id, name, gid, link_phrase, short_link_phrase, reverse_link_phrase, entity_type0, entity_type1)
+  VALUES (1, 'wikipedia', 'fcd58926-4243-40bb-a2e5-c7464b3ce577', 'wikipedia', 'wikipedia', 'wikipedia',
+          'artist', 'url');
 INSERT INTO artist_name (id, name) VALUES (1, 'Faye Wong');
 INSERT INTO artist (id, gid, name, sort_name) VALUES (1, 'acd58926-4243-40bb-a2e5-c7464b3ce577', 1, 1);
 INSERT INTO link (id, link_type) VALUES (1, 1);
