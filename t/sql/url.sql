df69952de593aff883387c9dd890cddb5b71e8cb
Oliver Charles <oliver.g.charles@googlemail.com>
2011-11-23 16:30:18 +0000

    Change url.sql test data to create artist 100
    Artist 1 gets created by editor.sql in controller tests

--- t/sql/url.sql
+++ t/sql/url.sql
@@ -14,7 +14,7 @@ INSERT INTO url (id, gid, url, description, ref_count)
 INSERT INTO link_type (id, name, gid, link_phrase, short_link_phrase, reverse_link_phrase, entity_type0, entity_type1)
   VALUES (1, 'wikipedia', 'fcd58926-4243-40bb-a2e5-c7464b3ce577', 'wikipedia', 'wikipedia', 'wikipedia',
           'artist', 'url');
-INSERT INTO artist_name (id, name) VALUES (1, 'Faye Wong');
-INSERT INTO artist (id, gid, name, sort_name) VALUES (1, 'acd58926-4243-40bb-a2e5-c7464b3ce577', 1, 1);
+INSERT INTO artist_name (id, name) VALUES (100, 'Faye Wong');
+INSERT INTO artist (id, gid, name, sort_name) VALUES (100, 'acd58926-4243-40bb-a2e5-c7464b3ce577', 100, 100);
 INSERT INTO link (id, link_type) VALUES (1, 1);
-INSERT INTO l_artist_url (id, link, entity0, entity1) VALUES (1, 1, 1, 3);
+INSERT INTO l_artist_url (id, link, entity0, entity1) VALUES (1, 1, 100, 3);
