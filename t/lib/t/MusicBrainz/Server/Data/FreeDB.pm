6f03f4a9c92a5821c578141da1f77dafc6fab731
Oliver Charles <oliver.g.charles@googlemail.com>
2011-11-23 16:11:14 +0000

    Correct the LWP mock playback for the FreeDB data tests

--- t/lib/t/MusicBrainz/Server/Data/FreeDB.pm
+++ t/lib/t/MusicBrainz/Server/Data/FreeDB.pm
@@ -3,14 +3,15 @@ use Test::Routine;
 use Test::More;
 use LWP::UserAgent::Mockable;
 
+use FindBin '$Bin';
+
 with 't::Context';
 
 test "Correctly handle the 200 OK 'failed to process the response' error" => sub {
     my $test = shift;
     my $c    = $test->c;
 
-    $ENV{LWP_UA_MOCK} = 'playback';
-    $ENV{LWP_UA_MOCK_FILE} = 't/lwp-sessions/mbs-3622-freedb-import.lwp';
+    LWP::UserAgent::Mockable->reset(playback => $Bin . '/lwp-sessions/mbs-3622-freedb-import.lwp');
 
     my $entry = $c->model('FreeDB')->lookup('rock', '24116e15');
     is(scalar(@{ $entry->tracks }), 21);
