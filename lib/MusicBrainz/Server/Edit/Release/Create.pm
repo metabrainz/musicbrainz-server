metabrainz/musicbrainz-server

commit 67d682d53218e49695e8626ae82e0e0f0e5f1b5d

Fix undefined value in hash reference warnings in release editor

lib/MusicBrainz/Server/Edit/Release/Create.pm
@@ -58,16 +58,22 @@ after 'initialize' => sub {
 sub foreign_keys
 {
     my $self = shift;
-    return {
+    my $fks = {
         Artist           => { load_artist_credit_definitions($self->data->{artist_credit}) },
         Country          => [ $self->data->{country_id} ],
-        Release          => { $self->entity_id => [ 'ArtistCredit' ] },
         ReleaseStatus    => [ $self->data->{status_id} ],
-        ReleaseGroup     => { $self->data->{release_group_id} => [ 'ArtistCredit' ] },
         ReleasePackaging => [ $self->data->{packaging_id} ],
         Script           => [ $self->data->{script_id} ],
         Language         => [ $self->data->{language_id} ],
     };
+
+    unless ($self->preview) {
+        # May be undef if previewing and creating a new release group
+        $fks->{ReleaseGroup} = { $self->data->{release_group_id} => [ 'ArtistCredit' ] };
+        $fks->{Release} = { $self->entity_id => [ 'ArtistCredit' ] };
+    }
+
+    return $fks;
 }
 
 sub build_display_data
@@ -82,15 +88,23 @@ sub build_display_data
         artist_credit => artist_credit_preview ($loaded, $self->data->{artist_credit}),
         name          => $self->data->{name} || '',
         comment       => $self->data->{comment} || '',
-        country       => $loaded->{Country}{ $self->data->{country_id} },
-        packaging     => $loaded->{ReleasePackaging}{ $self->data->{packaging_id} },
-        status        => $status ? $loaded->{ReleaseStatus}->{ $status } : '',
-        script        => $script ? $loaded->{Script}{ $script } : '',
-        language      => $lang ? $loaded->{Language}{ $lang } : '',
+        country       => defined($self->data->{country_id}) &&
+                           $loaded->{Country}{ $self->data->{country_id} },
+        packaging     => defined($self->data->{packaging_id}) &&
+                           $loaded->{ReleasePackaging}{ $self->data->{packaging_id} },
+        status        => defined($status) &&
+                           $loaded->{ReleaseStatus}->{ $status },
+        script        => defined($script) &&
+                           $loaded->{Script}{ $script },
+        language      => defined($lang) &&
+                           $loaded->{Language}{ $lang },
         barcode       => $self->data->{barcode} || '',
-        release_group => $loaded->{ReleaseGroup}{ $self->data->{release_group_id} } || ReleaseGroup->new( name => '[removed]' ),
-        release       => $loaded->{Release}{ $self->entity_id } ||
-            Release->new( name => $self->data->{name} ),
+        release_group => (defined($self->data->{release_group_id}) &&
+                           $loaded->{ReleaseGroup}{ $self->data->{release_group_id} }) ||
+                               ReleaseGroup->new( name => '[removed]' ),
+        release       => (defined($self->entity_id) &&
+                              $loaded->{Release}{ $self->entity_id }) ||
+                                  Release->new( name => $self->data->{name} ),
         date          => PartialDate->new({
             map { $_ => $self->data->{date}{$_} } qw( year month day )
         })

lib/MusicBrainz/Server/Edit/ReleaseGroup/Create.pm
@@ -54,8 +54,9 @@ sub build_display_data
         name          => $self->data->{name} || '',
         comment       => $self->data->{comment} || '',
         type          => $type ? $loaded->{ReleaseGroupType}->{ $type } : '',
-        release_group => $loaded->{ReleaseGroup}{ $self->entity_id }
-            || ReleaseGroup->new( name => $self->data->{name} )
+        release_group => (defined($self->entity_id) &&
+                              $loaded->{ReleaseGroup}{ $self->entity_id }) ||
+                                  ReleaseGroup->new( name => $self->data->{name} )
     };
 }
 

commit b7384777fa852a19296b3c8031513453c270ee47

MBS-3471: Display the release group that a release is being added to
When adding a release to an existing release group, make sure there is a link to
this release group. 67d682d53218 introduced a regression meaning it would never
load the release group in the release editor, but this should only happen if
it's a new release group.

lib/MusicBrainz/Server/Edit/Release/Create.pm
@@ -67,9 +67,10 @@ sub foreign_keys
         Language         => [ $self->data->{language_id} ],
     };
 
+    $fks->{ReleaseGroup} = { $self->data->{release_group_id} => [ 'ArtistCredit' ] }
+        if $self->data->{release_group_id};
+
     unless ($self->preview) {
-        # May be undef if previewing and creating a new release group
-        $fks->{ReleaseGroup} = { $self->data->{release_group_id} => [ 'ArtistCredit' ] };
         $fks->{Release} = { $self->entity_id => [ 'ArtistCredit' ] };
     }
 