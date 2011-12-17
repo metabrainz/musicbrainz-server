12068322886ea52e6649796236cadff91dab9da8
Lukáš Lalinský <lalinsky@gmail.com>
2011-12-17 21:18:08 +0100

    Allow updating artist credits when renaming an artist
    http://tickets.musicbrainz.org/browse/MBS-3042

--- lib/MusicBrainz/Server/Controller/Artist.pm
+++ lib/MusicBrainz/Server/Controller/Artist.pm
@@ -411,10 +411,58 @@ into the MusicBrainz database.
 
 =cut
 
-with 'MusicBrainz::Server::Controller::Role::Edit' => {
-    form           => 'Artist',
-    edit_type      => $EDIT_ARTIST_EDIT,
-};
+sub edit : Chained('load') RequireAuth Edit {
+    my ($self, $c) = @_;
+    my $artist = $c->stash->{artist};
+
+    my $artist_credits =$c->model('ArtistCredit')->find_by_artist_id($artist->id);
+    $c->stash( artist_credits => $artist_credits );
+
+    my @default_artist_credits =
+        grep { $_ } map {
+            my @found = grep { $_->name eq $artist->name } $_->all_names;
+            scalar(@found) ? $_->id : undef;
+        } @$artist_credits;
+
+    $self->edit_action(
+        $c,
+        form        => 'ArtistEdit',
+        form_args   => { artist_credits => $artist_credits,
+                         default_artist_credits => \@default_artist_credits },
+        type        => $EDIT_ARTIST_EDIT,
+        item        => $artist,
+        edit_args   => { to_edit => $artist },
+        on_creation => sub {
+            my ($edit, $form) = @_;
+
+            my $name = $form->field('name')->value;
+            if ($name ne $artist->name) {
+                my %rename = %{ $form->rename_artist_credit_set };
+                for my $old_ac (@$artist_credits) {
+                    next unless $rename{$old_ac->id};
+                    my $ac = $old_ac->change_artist_name($artist, $name);
+                    my $ac_edit = $c->model('Edit')->create(
+                        edit_type     => $EDIT_ARTIST_EDITCREDIT,
+                        editor_id     => $c->user->id,
+                        to_edit       => $old_ac,
+                        artist_credit => $ac,
+                    );
+                    $c->model('EditNote')->add_note(
+                        $ac_edit->id,
+                        {
+                            text => l('The artist name has been changed in edit #{id}.',
+                                      { id => $edit->id }),
+                            editor_id => $EDITOR_MODBOT
+                        }
+                    );
+                }
+            }
+
+            $c->res->redirect(
+                $c->uri_for_action('/artist/show', [ $artist->gid ]));
+        }
+    );
+}
 
 =head2 add_release
 
@@ -590,7 +638,7 @@ sub credit : Chained('load') PathPart('credit') CaptureArgs(1) {
     $c->stash( ac => $ac );
 }
 
-sub edit_credit : Chained('credit') PathPart('edit') Edit {
+sub edit_credit : Chained('credit') PathPart('edit') RequireAuth Edit {
     my ($self, $c) = @_;
     my $artist = $c->stash->{artist};
     my $ac = $c->stash->{ac};
