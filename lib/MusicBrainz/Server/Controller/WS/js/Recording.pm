metabrainz/musicbrainz-server

commit d8ad3c5c02cbde669a9077de89c695e1e945fb0b

Display artist credits for release groups when autocompleting release groups
I've ripped apart /ws/js quite drastically in order to allow for artist credits
to be displayed in autocompletion results. All of the core entities (artist,
label, etc) now have their own /ws/js controller, and they all do some form of
the Autocompletion role. The reason for this separation was that the release
group search duplicates bits of the recording search, and I wasnt happy simply
duplicating that code.

The Autocompletion role handles determining which search to dispatch to (indexed
or direct), and provides numerous methods that can be overriden to tweak
behaviour per class. Recording searches need to load ISRCs and work out which
release they appear on, while release group searches simply need to have the
artist credit.

lib/MusicBrainz/Server/Controller/WS/js/Recording.pm
@@ -1,0 +1,71 @@
+package MusicBrainz::Server::Controller::WS::js::Recording;
+use Moose;
+BEGIN { extends 'MusicBrainz::Server::ControllerBase::WS::js' }
+
+with 'MusicBrainz::Server::Controller::WS::js::Role::Autocompletion::WithArtistCredits';
+
+my $ws_defs = Data::OptList::mkopt([
+    "recording" => {
+        method   => 'GET',
+        required => [ qw(q) ],
+        optional => [ qw(a r direct limit page timestamp) ]
+    }
+]);
+
+with 'MusicBrainz::Server::WebService::Validator' =>
+{
+     defs => $ws_defs,
+     version => 'js',
+     default_serialization_type => 'json',
+};
+
+sub type { 'recording' }
+
+sub search : Path('/ws/js/work') {
+    my ($self, $c) = @_;
+    $self->dispatch_search($c);
+}
+
+sub _do_direct_search {
+    my ($self, $c, $query, $offset, $limit) = @_;
+
+    my $where = {};
+    if (my $artist = $c->req->query_params->{artist}) {
+        $where->{artist} = $artist;
+    }
+
+    return $c->model ('Search')->search ('recording', $query, $limit, $offset, $where);
+}
+
+after _load_entities => sub {
+    my ($self, $c, @recordings) = @_;
+    $c->model('ISRC')->load_for_recordings (@recordings);
+};
+
+sub _format_output {
+    my ($self, $c, @entities) = @_;
+    my %appears_on = $c->model('Recording')->appears_on (\@entities, 3);
+
+    return map {
+        {
+            recording => $_,
+            appears_on => $appears_on{$_->id}
+        }
+    } @entities;
+}
+
+around _form_indexed_query => sub {
+    my ($orig, $self) = splice(@_, 0, 2);
+    my ($query, $c) = @_;
+    $query = $self->$orig(@_);
+
+    my $lucene_query = "recording:($query*)";
+    if (my $artist = $c->req->query_params->{a}) {
+        $lucene_query .= " AND artist:($artist)";
+    }
+
+    return $lucene_query;
+};
+
+1;
+

lib/MusicBrainz/Server/Controller/WS/js.pm
@@ -10,47 +10,15 @@ use MusicBrainz::Server::Data::Search qw( escape_query alias_query );
 use MusicBrainz::Server::Data::Utils qw(
     artist_credit_to_alternative_ref
     hash_structure
-    type_to_model
 );
 use MusicBrainz::Server::Track qw( format_track_length );
 use Readonly;
 use Text::Trim;
-use Text::Unaccent qw( unac_string_utf16 );
 use Data::OptList;
 use Encode qw( decode encode );
 
 # This defines what options are acceptable for WS calls
 my $ws_defs = Data::OptList::mkopt([
-    "artist" => {
-        method   => 'GET',
-        required => [ qw(q) ],
-        optional => [ qw(direct limit page timestamp) ]
-    },
-    "label" => {
-        method   => 'GET',
-        required => [ qw(q) ],
-        optional => [ qw(direct limit page timestamp) ]
-    },
-    "recording" => {
-        method   => 'GET',
-        required => [ qw(q) ],
-        optional => [ qw(a r direct limit page timestamp) ]
-    },
-    "release-group" => {
-        method   => 'GET',
-        required => [ qw(q) ],
-        optional => [ qw(r direct limit page timestamp) ]
-    },
-    "release" => {
-        method   => 'GET',
-        required => [ qw(q) ],
-        optional => [ qw(r direct limit page timestamp) ]
-    },
-    "work" => {
-        method   => 'GET',
-        required => [ qw(q) ],
-        optional => [ qw(r direct limit page timestamp) ]
-    },
     "tracklist" => {
         method => 'GET',
         optional => [ qw(q artist tracks limit page timestamp) ]
@@ -91,290 +59,6 @@ sub root : Chained('/') PathPart("ws/js") CaptureArgs(0)
 {
     my ($self, $c) = @_;
     $self->validate($c, \%serializers) or $c->detach('bad_req');
-}
-
-sub _autocomplete_entity {
-    my ($self, $c, $type) = @_;
-
-    my $query = trim $c->stash->{args}->{q};
-    my $limit = $c->stash->{args}->{limit} || 10;
-    my $page = $c->stash->{args}->{page} || 1;
-    my $direct = $c->stash->{args}->{direct};
-
-    unless ($query) {
-        $c->detach('bad_req');
-    }
-
-    if ($direct eq 'true')
-    {
-        $self->_autocomplete_direct ($c, $type, $query, $page, $limit);
-    }
-    else
-    {
-        $self->_autocomplete_indexed($c, $type, $query, $page, $limit);
-    }
-}
-
-sub _autocomplete_direct {
-    my ($self, $c, $type, $query, $page, $limit) = @_;
-
-    my $offset = ($page - 1) * $limit;  # page is not zero based.
-
-    my ($search_results, $hits) = $c->model ('Search')->search (
-        $type, $query, $limit, $offset);
-
-    my @output;
-
-    for (@$search_results)
-    {
-        my $entity = $_->entity;
-
-        my $item = {
-            name => $entity->name,
-            id => $entity->id,
-            gid => $entity->gid,
-            comment => $entity->comment,
-        };
-
-        if ($entity->meta->has_attribute ('sort_name'))
-        {
-            $item->{sortname} = $entity->sort_name;
-        }
-
-        push @output, $item;
-    }
-
-    my $pager = Data::Page->new ();
-    $pager->entries_per_page ($limit);
-    $pager->current_page ($page);
-    $pager->total_entries ($hits);
-
-    push @output, {
-        pages => $pager->last_page,
-        current => $pager->current_page
-    };
-
-    $c->res->content_type($c->stash->{serializer}->mime_type . '; charset=utf-8');
-    $c->res->body($c->stash->{serializer}->serialize('generic', \@output));
-}
-
-sub _autocomplete_indexed {
-    my ($self, $c, $type, $query, $page, $limit) = @_;
-
-    $query = decode ("utf-16", unac_string_utf16 (encode ("utf-16", $query)));
-    $query = escape_query ($query);
-    $query = $query.'*';
-
-    if (grep ($type eq $_, 'artist', 'label', 'work'))
-    {
-        $query = alias_query ($type, $query);
-    }
-
-    my $model = type_to_model ($type);
-
-    my $no_redirect = 1;
-    my $response = $c->model ('Search')->external_search (
-        $c, $type, $query, $limit, $page, 1, undef, $no_redirect);
-
-    my @output;
-
-    if ($response->{pager})
-    {
-        my $pager = $response->{pager};
-
-        for my $result (@{ $response->{results} })
-        {
-            my $entity = $c->model($model)->get_by_gid ($result->{entity}->gid);
-
-            next unless $entity;
-
-            my $item = {
-                name => $result->{entity}->name,
-                id => $entity->id,
-                gid => $result->{entity}->gid,
-                comment => $result->{entity}->comment,
-            };
-
-            if ($entity->meta->has_attribute ('sort_name'))
-            {
-                $item->{sortname} = $entity->sort_name;
-            }
-
-            push @output, $item;
-        }
-
-        push @output, {
-            pages => $pager->last_page,
-            current => $pager->current_page
-        };
-    }
-    else
-    {
-        # If an error occurred just ignore it for now and return an
-        # empty list.  The javascript code for autocomplete doesn't
-        # have any way to gracefully report or deal with
-        # errors. --warp.
-
-        push @output, { pages => 1, current => 1 };
-    }
-
-    $c->res->content_type($c->stash->{serializer}->mime_type . '; charset=utf-8');
-    $c->res->body($c->stash->{serializer}->serialize('generic', \@output));
-}
-
-sub artist : Chained('root') PathPart('artist') Args(0)
-{
-    my ($self, $c) = @_;
-
-    $self->_autocomplete_entity($c, 'artist');
-}
-
-sub label : Chained('root') PathPart('label') Args(0)
-{
-    my ($self, $c) = @_;
-
-    $self->_autocomplete_entity($c, 'label');
-}
-
-sub release_group : Chained('root') PathPart('release-group') Args(0)
-{
-    my ($self, $c) = @_;
-
-    $self->_autocomplete_entity($c, 'release_group');
-}
-
-sub release : Chained('root') PathPart('release') Args(0)
-{
-    my ($self, $c) = @_;
-
-    $self->_autocomplete_entity($c, 'release');
-}
-
-sub work : Chained('root') PathPart('work') Args(0)
-{
-    my ($self, $c) = @_;
-
-    $self->_autocomplete_entity($c, 'work');
-}
-
-sub recording : Chained('root') PathPart('recording') Args(0)
-{
-    my ($self, $c) = @_;
-
-    my $query = trim $c->stash->{args}->{q};
-    my $artist = $c->stash->{args}->{a} || '';
-    my $limit = $c->stash->{args}->{limit} || 10;
-    my $page = $c->stash->{args}->{page} || 1;
-    my $direct = $c->stash->{args}->{direct};
-
-    unless ($query) {
-        $c->detach('bad_req');
-    }
-
-    if ($direct eq 'true')
-    {
-        $self->_recording_direct($c, $query, $artist, $page, $limit);
-    }
-    else
-    {
-        $self->_recording_indexed($c, $query, $artist, $page, $limit);
-    }
-}
-
-sub _recording_direct {
-    my ($self, $c, $query, $artist, $page, $limit) = @_;
-
-    my $offset = ($page - 1) * $limit;  # page is not zero based.
-
-    my $where = {};
-    $where->{artist} = $artist if $artist;
-
-    my ($search_results, $hits) = $c->model ('Search')->search (
-        'recording', $query, $limit, $offset, $where);
-
-    my @entities = map { $_->entity } @$search_results;
-
-    $c->model ('ArtistCredit')->load (@entities);
-    $c->model('ISRC')->load_for_recordings (@entities);
-
-    my %appears_on = $c->model('Recording')->appears_on (\@entities, 3);
-
-    my @output = map {
-        {
-            recording => $_,
-            appears_on => $appears_on{$_->id}
-        }
-    } @entities;
-
-    my $pager = Data::Page->new ();
-    $pager->entries_per_page ($limit);
-    $pager->current_page ($page);
-    $pager->total_entries ($hits);
-
-    $c->res->content_type($c->stash->{serializer}->mime_type . '; charset=utf-8');
-    $c->res->body($c->stash->{serializer}->serialize('autocomplete_recording', \@output, $pager));
-}
-
-sub _recording_indexed {
-    my ($self, $c, $query, $artist, $page, $limit) = @_;
-
-    $query = decode ("utf-16", unac_string_utf16 (encode ("utf-16", $query)));
-    $query = escape_query ($query);
-    $artist = escape_query ($artist);
-
-    my $lucene_query = "recording:($query*)";
-    $lucene_query .= " AND artist:($artist)" if $artist;
-
-    my $no_redirect = 1;
-    my $response = $c->model ('Search')->external_search (
-        $c, 'recording', $lucene_query, $limit, $page, 1, undef, $no_redirect);
-
-    my @output;
-    my $pager;
-
-    if ($response->{pager})
-    {
-        $pager = $response->{pager};
-
-        my @entities;
-
-        for my $result (@{ $response->{results} })
-        {
-            my $entity = $c->model('Recording')->get_by_gid ($result->{entity}->gid);
-
-            next unless $entity;
-
-            $c->model('ISRC')->load_for_recordings ($entity);
-
-            $entity->artist_credit ($result->{entity}->artist_credit);
-
-            push @entities, $entity;
-        }
-
-        my %appears_on = $c->model('Recording')->appears_on (\@entities, 3);
-
-        @output = map {
-            {
-                recording => $_,
-                appears_on => $appears_on{$_->id}
-            }
-        } @entities;
-    }
-    else
-    {
-        # If an error occurred just ignore it for now and return an
-        # empty list.  The javascript code for autocomplete doesn't
-        # have any way to gracefully report or deal with
-        # errors. --warp.
-
-        $pager = Data::Page->new ();
-        $pager->entries_per_page ($limit);
-        $pager->current_page ($page);
-        $pager->total_entries (0);
-    }
-
-    $c->res->content_type($c->stash->{serializer}->mime_type . '; charset=utf-8');
-    $c->res->body($c->stash->{serializer}->serialize('autocomplete_recording', \@output, $pager));
 }
 
 sub tracklist : Chained('root') PathPart Args(1) {

lib/MusicBrainz/Server/Controller/WS/js/Artist.pm
@@ -1,0 +1,30 @@
+package MusicBrainz::Server::Controller::WS::js::Artist;
+use Moose;
+BEGIN { extends 'MusicBrainz::Server::ControllerBase::WS::js' }
+
+with 'MusicBrainz::Server::Controller::WS::js::Role::AliasAutocompletion';
+
+my $ws_defs = Data::OptList::mkopt([
+    "artist" => {
+        method   => 'GET',
+        required => [ qw(q) ],
+        optional => [ qw(direct limit page timestamp) ]
+    }
+]);
+
+with 'MusicBrainz::Server::WebService::Validator' =>
+{
+     defs => $ws_defs,
+     version => 'js',
+     default_serialization_type => 'json',
+};
+
+sub type { 'artist' }
+
+sub search : Path('/ws/js/artist') {
+    my ($self, $c) = @_;
+    $self->dispatch_search($c);
+}
+
+1;
+

lib/MusicBrainz/Server/Controller/WS/js/Label.pm
@@ -1,0 +1,30 @@
+package MusicBrainz::Server::Controller::WS::js::Label;
+use Moose;
+BEGIN { extends 'MusicBrainz::Server::ControllerBase::WS::js' }
+
+with 'MusicBrainz::Server::Controller::WS::js::Role::AliasAutocompletion';
+
+my $ws_defs = Data::OptList::mkopt([
+    "label" => {
+        method   => 'GET',
+        required => [ qw(q) ],
+        optional => [ qw(direct limit page timestamp) ]
+    }
+]);
+
+with 'MusicBrainz::Server::WebService::Validator' =>
+{
+     defs => $ws_defs,
+     version => 'js',
+     default_serialization_type => 'json',
+};
+
+sub type { 'label' }
+
+sub search : Path('/ws/js/label') {
+    my ($self, $c) = @_;
+    $self->dispatch_search($c);
+}
+
+1;
+

lib/MusicBrainz/Server/Controller/WS/js/Release.pm
@@ -1,0 +1,30 @@
+package MusicBrainz::Server::Controller::WS::js::Release;
+use Moose;
+BEGIN { extends 'MusicBrainz::Server::ControllerBase::WS::js' }
+
+with 'MusicBrainz::Server::Controller::WS::js::Role::Autocompletion';
+
+my $ws_defs = Data::OptList::mkopt([
+    "release" => {
+        method   => 'GET',
+        required => [ qw(q) ],
+        optional => [ qw(direct limit page timestamp) ]
+    }
+]);
+
+with 'MusicBrainz::Server::WebService::Validator' =>
+{
+     defs => $ws_defs,
+     version => 'js',
+     default_serialization_type => 'json',
+};
+
+sub type { 'release' }
+
+sub search : Path('/ws/js/release') {
+    my ($self, $c) = @_;
+    $self->dispatch_search($c);
+}
+
+1;
+

lib/MusicBrainz/Server/Controller/WS/js/ReleaseGroup.pm
@@ -1,0 +1,32 @@
+package MusicBrainz::Server::Controller::WS::js::ReleaseGroup;
+use Moose;
+BEGIN { extends 'MusicBrainz::Server::ControllerBase::WS::js' }
+
+with 'MusicBrainz::Server::Controller::WS::js::Role::Autocompletion::WithArtistCredits';
+
+my $ws_defs = Data::OptList::mkopt([
+    "release-group" => {
+        method   => 'GET',
+        required => [ qw(q) ],
+        optional => [ qw(direct limit page timestamp) ]
+    }
+]);
+
+with 'MusicBrainz::Server::WebService::Validator' =>
+{
+     defs => $ws_defs,
+     version => 'js',
+     default_serialization_type => 'json',
+};
+
+sub type { 'release_group' }
+
+sub serialization_routine { 'autocomplete_release_group' }
+
+sub search : Path('/ws/js/release-group') {
+    my ($self, $c) = @_;
+    $self->dispatch_search($c);
+}
+
+1;
+

lib/MusicBrainz/Server/Controller/WS/js/Role/AliasAutocompletion.pm
@@ -1,0 +1,14 @@
+package MusicBrainz::Server::Controller::WS::js::Role::AliasAutocompletion;
+use Moose::Role;
+
+use MusicBrainz::Server::Data::Search qw( alias_query );
+
+with 'MusicBrainz::Server::Controller::WS::js::Role::Autocompletion';
+
+around _form_indexed_query => sub {
+    my ($orig, $self) = splice(@_, 0, 2);
+    my $query = $self->$orig(@_);
+    return alias_query ($self->type, $query);
+};
+
+1;

lib/MusicBrainz/Server/Controller/WS/js/Role/Autocompletion.pm
@@ -1,0 +1,122 @@
+package MusicBrainz::Server::Controller::WS::js::Role::Autocompletion;
+use Moose::Role;
+use namespace::autoclean;
+
+use Encode;
+use MusicBrainz::Server::Data::Utils qw( type_to_model );
+use MusicBrainz::Server::Data::Search qw( escape_query );
+use Text::Trim;
+use Text::Unaccent qw( unac_string_utf16 );
+
+requires 'type';
+
+sub serialization_routine { 'autocomplete_generic' }
+
+sub model {
+    my ($self, $c) = @_;
+    return $c->model(type_to_model($self->type));
+}
+
+sub dispatch_search {
+    my ($self, $c) = @_;
+
+    my $query = trim $c->stash->{args}->{q};
+    my $limit = $c->stash->{args}->{limit} || 10;
+    my $page = $c->stash->{args}->{page} || 1;
+    my $direct = $c->stash->{args}->{direct} || '';
+
+    unless ($query) {
+        $c->detach('bad_req');
+    }
+
+    my ($output, $pager) =
+        $direct eq 'true' ? $self->_direct_search($c, $query, $page, $limit)
+                          : $self->_indexed_search($c, $query, $page, $limit);
+
+    $c->res->content_type($c->stash->{serializer}->mime_type . '; charset=utf-8');
+    $c->res->body($c->stash->{serializer}->serialize($self->serialization_routine, $output, $pager));
+}
+
+sub _load_entities { }
+
+sub _do_direct_search {
+    my ($self, $c, $query, $offset, $limit) = @_;
+    return $c->model ('Search')->search (
+        $self->type, $query, $limit, $offset);
+}
+
+sub _direct_search {
+    my ($self, $c, $query, $page, $limit) = @_;
+
+    my $offset = ($page - 1) * $limit;  # page is not zero based.
+    my ($search_results, $hits) = $self->_do_direct_search($c, $query, $offset, $limit);
+
+    my @entities = map { $_->entity } @$search_results;
+    $self->_load_entities($c, @entities);
+
+    my @output = $self->_format_output($c, @entities);
+
+    my $pager = Data::Page->new ();
+    $pager->entries_per_page ($limit);
+    $pager->current_page ($page);
+    $pager->total_entries ($hits);
+
+    return (\@output, $pager);
+}
+
+sub _format_output {
+    my ($self, $c, @entities) = @_;
+    return @entities;
+}
+
+sub _form_indexed_query {
+    my ($self, $query) = @_;
+    $query = decode ("utf-16", unac_string_utf16 (encode ("utf-16", $query)));
+    $query = escape_query ($query);
+    $query = $query.'*';
+    return $query;
+}
+
+sub _indexed_search {
+    my ($self, $c, $query, $page, $limit) = @_;
+
+    $query = $self->_form_indexed_query($query, $c);
+
+    my $model = $self->model($c);
+
+    my $no_redirect = 1;
+    my $response = $c->model ('Search')->external_search (
+        $c, $self->type, $query, $limit, $page, 1, undef, $no_redirect);
+
+    my (@output, $pager);
+
+    if ($response->{pager})
+    {
+        $pager = $response->{pager};
+
+        for my $result (@{ $response->{results} })
+        {
+            my $entity = $model->get_by_gid ($result->{entity}->gid);
+            next unless $entity;
+            push @output, $entity;
+        }
+
+        $self->_load_entities($c, @output);
+    }
+    else
+    {
+        # If an error occurred just ignore it for now and return an
+        # empty list.  The javascript code for autocomplete doesn't
+        # have any way to gracefully report or deal with
+        # errors. --warp.
+
+        $pager = Data::Page->new;
+        $pager->entries_per_page ($limit);
+        $pager->current_page ($page);
+        $pager->total_entries (0);
+    }
+
+    return ([ $self->_format_output($c, @output) ], $pager);
+}
+
+1;

commit 3d54426bf2b6e87d9af7e409ecc13bda58ecaa46

MBS-3557: Correctly handle the 'a' paremeter for /ws/js/recording
This allows you to limit a search to a specific artist. The refactoring in
d8ad3c5c02cb broke this, by making direct search use an 'artist' parameter, and
indexed search use the (correct) 'a' parameter.

lib/MusicBrainz/Server/Controller/WS/js/Recording.pm
@@ -32,7 +32,7 @@ sub _do_direct_search {
     my ($self, $c, $query, $offset, $limit) = @_;
 
     my $where = {};
-    if (my $artist = $c->req->query_params->{artist}) {
+    if (my $artist = $c->req->query_params->{a}) {
         $where->{artist} = $artist;
     }
 