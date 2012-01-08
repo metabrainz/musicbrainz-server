metabrainz/musicbrainz-server

commit 74eaf5fffb471e7b5d6f0d5647718e00bea98457

MBS-3352: Allow "57" to be parsed as "0:57" in the release editor
Also fixes MBS-4012 by allowing a string of integers to be entered as a track
duration, and split apart into m:ss form

root/static/scripts/release-editor/MB/Control/ReleaseTracklist.js
@@ -75,6 +75,7 @@ MB.Control.ReleaseTrack = function (parent, $track, $artistcredit) {
      */
     self.blurLength = function (event) {
         var length = self.$length.val ();
+        length = length.replace (/([0-9]*)([0-9][0-9])/, "$1:$2");
 
         self.setDuration (MB.utility.unformatTrackLength (length));
     };

root/static/scripts/common/MB/utility.js
@@ -208,5 +208,8 @@ MB.utility.unformatTrackLength = function (duration)
         return null;
     }
 
+    // MBS-3352: Handle the case of ":57"
+    parts[0] = parts[0] || 0;
+
     return parseInt (parts[0], 10) * 60000 + parseInt (parts[1], 10) * 1000;
 };

commit ee719ddb1131de3bdfcb389223fda0eaaaaaff74

MBS-4146: Fix release editor tracklist duration parsing
74eaf5fffb471e7 introduced a regression that meant tabbing out of the length
field when it had a value of the form m:ss would clear it (resetting it to
?:??).

This was caused by an unbounded regular expression replacement, which should
only run over the entire input (start and end markers), which this commit fixes.

root/static/scripts/release-editor/MB/Control/ReleaseTracklist.js
@@ -75,7 +75,7 @@ MB.Control.ReleaseTrack = function (parent, $track, $artistcredit) {
      */
     self.blurLength = function (event) {
         var length = self.$length.val ();
-        length = length.replace (/([0-9]*)([0-9][0-9])/, "$1:$2");
+        length = length.replace (/^([0-9]*)([0-9][0-9])$/, "$1:$2");
 
         self.setDuration (MB.utility.unformatTrackLength (length));
     };