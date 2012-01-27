4aa16e7b15ad39ccc36abacb81e97d3ee8800ddd
Oliver Charles <oliver.g.charles@googlemail.com>
2012-01-27 11:04:19 +0000

    Remove old scripts

--- script/musicbrainz_server.pl
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env perl
-
-use strict;
-use warnings;
-use FindBin '$Bin';
-
-exec('plackup', '-r', "-I$Bin/../lib", "--access-log" => "/dev/null", "$Bin/../musicbrainz_server.psgi", @ARGV);
