dc9f7e6d95fc363baf2e12c77b47d6c8c8fb1692
Oliver Charles <oliver.g.charles@googlemail.com>
2012-02-03 13:40:26 +0000

    Fix a warning filtering label/cat-no pairs
    If a pair didn't have a label at all, it would emit a warning when filtering the
    list.

--- lib/MusicBrainz/Server/Entity/Release.pm
+++ lib/MusicBrainz/Server/Entity/Release.pm
@@ -255,7 +255,7 @@ sub filter_labels
     my ($self, $label) = @_;
     my @labels = $self->all_labels
         or return ();
-    return grep { $_->label_id eq $label->id } @labels;
+    return grep { $_->label_id && $_->label_id == $label->id } @labels;
 }
 
 __PACKAGE__->meta->make_immutable;
