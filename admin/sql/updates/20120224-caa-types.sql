694f7400842cd912a6c82924a3b29f68e107c4ba
Oliver Charles <oliver.g.charles@googlemail.com>
2012-02-28 14:05:41 +0000

    Sort 'other' at the end of the cover art type list

--- admin/sql/updates/20120224-caa-types.sql
+++ admin/sql/updates/20120224-caa-types.sql
@@ -2,7 +2,6 @@ BEGIN;
 
 SET search_path = 'cover_art_archive';
 
-INSERT INTO art_type (name) VALUES ('Other');
 INSERT INTO art_type (name) VALUES ('Front');
 INSERT INTO art_type (name) VALUES ('Back');
 INSERT INTO art_type (name) VALUES ('Booklet');
@@ -10,5 +9,6 @@ INSERT INTO art_type (name) VALUES ('Medium');
 INSERT INTO art_type (name) VALUES ('Obi');
 INSERT INTO art_type (name) VALUES ('Spine');
 INSERT INTO art_type (name) VALUES ('Track');
+INSERT INTO art_type (name) VALUES ('Other');
 
 COMMIT;
