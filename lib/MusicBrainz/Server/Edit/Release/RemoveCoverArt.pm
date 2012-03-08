103d476dd9bca52611f7ad1098c1d1dbbcce5b20
Oliver Charles <oliver.g.charles@googlemail.com>
2012-03-08 10:00:23 +0000

    MBS-4377: Removing cover art should not be an auto-edit

--- lib/MusicBrainz/Server/Edit/Release/RemoveCoverArt.pm
+++ lib/MusicBrainz/Server/Edit/Release/RemoveCoverArt.pm
@@ -5,6 +5,7 @@ use List::MoreUtils qw( any );
 use MooseX::Types::Moose qw( Str Int );
 use MooseX::Types::Structured qw( Dict );
 use MusicBrainz::Server::Constants qw( $EDIT_RELEASE_REMOVE_COVER_ART );
+use MusicBrainz::Server::Constants qw( :expire_action :quality );
 use MusicBrainz::Server::Edit::Exceptions;
 use Net::CoverArtArchive;
 
@@ -40,6 +41,30 @@ has bucket_name => (
         return 'mbid-' . $self->data->{entity}{mbid};
     }
 );
+
+sub edit_conditions
+{
+    return {
+        $QUALITY_LOW => {
+            duration      => 4,
+            votes         => 1,
+            expire_action => $EXPIRE_ACCEPT,
+            auto_edit     => 0,
+        },
+        $QUALITY_NORMAL => {
+            duration      => 14,
+            votes         => 3,
+            expire_action => $EXPIRE_ACCEPT,
+            auto_edit     => 0,
+        },
+        $QUALITY_HIGH => {
+            duration      => 14,
+            votes         => 4,
+            expire_action => $EXPIRE_REJECT,
+            auto_edit     => 0,
+        },
+    };
+}
 
 sub initialize {
     my ($self, %opts) = @_;
