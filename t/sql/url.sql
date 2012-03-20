b4a17e2429084ce9420c6b61e2f7e312776967ee
Oliver Charles <oliver.g.charles@googlemail.com>
2012-03-20 12:13:34 +0000

    Remove ref_count from the tests

--- t/sql/url.sql
+++ t/sql/url.sql
@@ -1,15 +1,15 @@
 SET client_min_messages TO 'warning';
 
 
-INSERT INTO url (id, gid, url, description, ref_count)
+INSERT INTO url (id, gid, url, description)
     VALUES (1, '9201840b-d810-4e0f-bb75-c791205f5b24', 'http://musicbrainz.org/',
-        'MusicBrainz', 2),
+        'MusicBrainz'),
            (2, '9b3c5c67-572a-4822-82a3-bdd3f35cf152', 'http://microsoft.com',
-           'EVIL', 1);
+           'EVIL');
 
-INSERT INTO url (id, gid, url, description, ref_count)
+INSERT INTO url (id, gid, url, description)
     VALUES (3, '25d6b63a-12dc-41c9-858a-2f42ae610a7d', 'http://zh-yue.wikipedia.org/wiki/王菲',
-        'Cantonese wikipedia page of Faye Wong', 1);
+        'Cantonese wikipedia page of Faye Wong');
 
 INSERT INTO link_type (id, name, gid, link_phrase, short_link_phrase, reverse_link_phrase, entity_type0, entity_type1)
   VALUES (1, 'wikipedia', 'fcd58926-4243-40bb-a2e5-c7464b3ce577', 'wikipedia', 'wikipedia', 'wikipedia',
