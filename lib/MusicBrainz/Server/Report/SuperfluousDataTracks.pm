4badb0cb5640eeae875c40a4657326ea9f93336a
nikki <nikki@rika.musicbrainz.org>
2012-04-19 12:38:13 +0000

    Fix a postgresql warning

--- lib/MusicBrainz/Server/Report/SuperfluousDataTracks.pm
+++ lib/MusicBrainz/Server/Report/SuperfluousDataTracks.pm
@@ -21,7 +21,7 @@ JOIN track ON tracklist.id = track.tracklist
 JOIN track_name ON track_name.id = track.name
 WHERE (medium_format.has_discids = TRUE OR medium_format.has_discids IS NULL)
 AND track.position = tracklist.track_count
-AND track_name.name ~* '([[:<:]](dat(a|en)|cccd|gegevens|video)[[:>:]]|\\u30C7\\u30FC\\u30BF)'
+AND track_name.name ~* E'([[:<:]](dat(a|en)|cccd|gegevens|video)[[:>:]]|\\u30C7\\u30FC\\u30BF)'
 AND NOT EXISTS (
    SELECT TRUE FROM medium_cdtoc WHERE medium_cdtoc.medium = medium.id
    LIMIT 1
