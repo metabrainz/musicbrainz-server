6f230b369c2ad39fe52e6b1c713e74a444ec7f9f
Oliver Charles <oliver.g.charles@googlemail.com>
2012-04-23 16:23:37 +0100

    Add a schema migration for secondary release group types

---
+++ admin/sql/updates/20120423-release-group-types.sql
@@ -0,0 +1,16 @@
+BEGIN;
+
+CREATE TABLE release_group_secondary_type (
+    id SERIAL NOT NULL PRIMARY KEY,
+    name TEXT NOT NULL
+);
+
+CREATE TABLE release_group_secondary_type_join (
+    release_group INTEGER NOT NULL REFERENCES release_group (id),
+    secondary_type INTEGER NOT NULL REFERENCES release_group_secondary_type (id),
+    PRIMARY KEY (release_group, content_type)
+);
+
+ALTER TABLE release_group_type RENAME to release_group_primary_type
+
+COMMIT;
