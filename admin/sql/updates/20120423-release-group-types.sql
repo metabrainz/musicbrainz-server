c60cfbb1d139185e174cbc2ac4d3d8130b5bf7e8
Oliver Charles <oliver.g.charles@googlemail.com>
2012-04-23 16:27:11 +0100

    Use the term 'secondary_type' instead of 'release_group_type'

--- admin/sql/updates/20120423-release-group-types.sql
+++ admin/sql/updates/20120423-release-group-types.sql
@@ -8,9 +8,9 @@ CREATE TABLE release_group_secondary_type (
 CREATE TABLE release_group_secondary_type_join (
     release_group INTEGER NOT NULL REFERENCES release_group (id),
     secondary_type INTEGER NOT NULL REFERENCES release_group_secondary_type (id),
-    PRIMARY KEY (release_group, content_type)
+    PRIMARY KEY (release_group, secondary_type)
 );
 
-ALTER TABLE release_group_type RENAME to release_group_primary_type
+ALTER TABLE release_group_type RENAME to release_group_primary_type;
 
 COMMIT;
