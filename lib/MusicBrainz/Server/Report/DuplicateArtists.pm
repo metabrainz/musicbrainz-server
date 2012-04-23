b0d345a227a189088655a22e7be4b9823c1a0517
nikki <nikki@rika.musicbrainz.org>
2012-04-23 18:09:58 +0000

    Use what Ollie suggested in the duplicate artists report

--- lib/MusicBrainz/Server/Report/DuplicateArtists.pm
+++ lib/MusicBrainz/Server/Report/DuplicateArtists.pm
@@ -76,9 +76,12 @@ sub post_load
 {
     my ($self, $dupe_sets) = @_;
     my @artists;
+
+    my $artists = $self->c->model('Artist')->get_by_gids(map { $_->{gid} } map { @$_ } @$dupe_sets);
+
     for my $dupes (@$dupe_sets) {
         for my $dupe (@$dupes) {
-            $dupe->{artist} = $self->c->model('Artist')->get_by_gid($dupe->{gid});
+            $dupe->{artist} = $artists->{$dupe->{gid}};
             push @artists, $dupe->{artist};
         }
     }
