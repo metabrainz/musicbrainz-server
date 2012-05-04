8cd8843825821d7273127810a195a71f95fa2ca1
Oliver Charles <oliver.g.charles@googlemail.com>
2012-05-04 12:13:24 +0100

    Add the 20120420-editor-improvements.sql to upgrade.sh

--- upgrade.sh
+++ upgrade.sh
@@ -19,6 +19,9 @@ then
     ./admin/psql READWRITE < ./admin/sql/DropReplicationTriggers.sql
 fi
 
+echo `date` : Applying 20120420-editor-improvements.sql
+./admin/psql < admin/sql/updates/20120420-editor-improvements.sql
+
 if [ "$REPLICATION_TYPE" = "$RT_MASTER" ]
 then
     echo `date` : Create replication triggers
