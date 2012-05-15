2758b7f2d68855ecba12debf6769c1b64328024c
Oliver Charles <oliver.g.charles@googlemail.com>
2012-05-15 11:24:30 +0100

    Factor out upgraed constraints to a separate file, so this doesn't run on slaves

--- admin/sql/updates/20120508-unknown-end-dates.sql
+++ admin/sql/updates/20120508-unknown-end-dates.sql
@@ -13,28 +13,6 @@ WHERE end_date_year IS NOT NULL OR end_date_month IS NOT NULL OR end_date_day IS
 UPDATE link SET ended = TRUE
 WHERE end_date_year IS NOT NULL OR end_date_month IS NOT NULL OR end_date_day IS NOT NULL;
 
-CREATE OR REPLACE FUNCTION end_date_implies_ended()
-RETURNS trigger AS $$
-BEGIN
-    IF NEW.end_date_year IS NOT NULL OR
-       NEW.end_date_month IS NOT NULL OR
-       NEW.end_date_day IS NOT NULL
-    THEN
-        NEW.ended = TRUE;
-    END IF;
-    RETURN NEW;
-END;
-$$ LANGUAGE 'plpgsql';
-
-CREATE TRIGGER end_date_implies_ended BEFORE UPDATE OR INSERT ON artist
-FOR EACH ROW EXECUTE PROCEDURE end_date_implies_ended();
-
-CREATE TRIGGER end_date_implies_ended BEFORE UPDATE OR INSERT ON label
-FOR EACH ROW EXECUTE PROCEDURE end_date_implies_ended();
-
-CREATE TRIGGER end_date_implies_ended BEFORE UPDATE OR INSERT ON link
-FOR EACH ROW EXECUTE PROCEDURE end_date_implies_ended();
-
 ALTER TABLE artist ADD CONSTRAINT artist_ended_check
 CHECK (
   (
