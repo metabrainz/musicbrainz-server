366e70d04dad33857a106631682fcd624ecd06e5
Oliver Charles <oliver.g.charles@googlemail.com>
2012-05-15 11:04:00 +0100

    Factor out upgraed constraints to a separate file, so this doesn't run on slaves

--- admin/sql/updates/20120420-editor-improvements.sql
+++ admin/sql/updates/20120420-editor-improvements.sql
@@ -4,14 +4,11 @@ ALTER TABLE editor ADD COLUMN birth_date DATE;
 ALTER TABLE editor ADD COLUMN gender INTEGER;
 ALTER TABLE editor ADD COLUMN country INTEGER;
 
-ALTER TABLE editor ADD CONSTRAINT editor_fk_gender FOREIGN KEY (gender) REFERENCES gender (id);
-ALTER TABLE editor ADD CONSTRAINT editor_fk_country FOREIGN KEY (country) REFERENCES country (id);
-
 CREATE TYPE FLUENCY AS ENUM ('basic', 'intermediate', 'advanced', 'native');
 
 CREATE TABLE editor_language (
-    editor INTEGER NOT NULL REFERENCES editor (id),
-    language INTEGER NOT NULL REFERENCES language (id),
+    editor INTEGER NOT NULL,
+    language INTEGER NOT NULL,
     fluency FLUENCY NOT NULL,
     PRIMARY KEY (editor, language)
 );
