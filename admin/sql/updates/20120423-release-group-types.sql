cc600032a6f5dc8c0009b49b357a0bdd3dce7fe2
Oliver Charles <oliver.g.charles@googlemail.com>
2012-05-15 11:21:35 +0100

    Factor out upgraed constraints to a separate file, so this doesn't run on slaves

--- admin/sql/updates/20120423-release-group-types.sql
+++ admin/sql/updates/20120423-release-group-types.sql
@@ -6,8 +6,8 @@ CREATE TABLE release_group_secondary_type (
 );
 
 CREATE TABLE release_group_secondary_type_join (
-    release_group INTEGER NOT NULL REFERENCES release_group (id),
-    secondary_type INTEGER NOT NULL REFERENCES release_group_secondary_type (id),
+    release_group INTEGER NOT NULL,
+    secondary_type INTEGER NOT NULL,
     PRIMARY KEY (release_group, secondary_type)
 );
 
