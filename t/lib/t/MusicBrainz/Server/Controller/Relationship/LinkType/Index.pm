fc67f920193f5daf487249d8b26de541d0390a25
Oliver Charles <oliver.g.charles@googlemail.com>
2012-05-17 10:30:08 +0100

    MBS-4687: Rewrite the Relationship::LinkType controller to use Role::Load and not have overlapping URLs
    Like the move for LinkAttributeTypes, LinkType has now been simplified to use
    Role::Load. I've added a separate chained action for sypecific types of
    relationships - such as artist-artist or label-work. This chained action handles
    validation, to disallow people trying to create moose-pony relationships, for
    example.
    
    The form has been changed to not directly depend on values in $c->stash, that's
    very ugly. Instead, the link type tree is now a required attribute on the form
    and must be explicitly passed in.
    
    Tests have been added to make sure all links work correctly, along with a test
    for the broken 'create' link.

---
+++ t/lib/t/MusicBrainz/Server/Controller/Relationship/LinkType/Index.pm
@@ -0,0 +1,54 @@
+package t::MusicBrainz::Server::Controller::Relationship::LinkType::Index;
+use Test::Routine;
+use Test::More;
+use Test::XPath;
+use MusicBrainz::Server::Test qw( html_ok );
+
+around run_test => sub {
+    my ($orig, $test, @args) = @_;
+    $test->c->sql->do(<<'EOSQL');
+INSERT INTO editor (id, name, password, email, privs)
+  VALUES (1, 'editor1', 'pass', 'editor1@example.com', 255)
+EOSQL
+
+    $test->mech->get('/login');
+    $test->mech->submit_form( with_fields => { username => 'editor1', password => 'pass' } );
+
+    $test->$orig(@args);
+};
+
+with 't::Mechanize', 't::Context';
+
+test 'Viewing /relationship/artist-artist as admin' => sub {
+    my $test = shift;
+    my $mech = $test->mech;
+
+    $test->c->sql->do(<<'EOSQL');
+INSERT INTO link_type (id, gid, entity_type0, entity_type1, name,
+    link_phrase, reverse_link_phrase, short_link_phrase)
+  VALUES (1, '77a0f1d3-f9ec-4055-a6e7-24d7258c21f7', 'artist', 'artist', 'member of band', '', '', '');
+EOSQL
+
+    $mech->get_ok('/relationships/artist-artist');
+    my $tx = Test::XPath->new( xml => $mech->content, is_html => 1 );
+    $tx->ok('//a[contains(@href,"/relationship/77a0f1d3-f9ec-4055-a6e7-24d7258c21f7/edit")]',
+            'has a link to edit the relationship type');
+    $tx->ok('//a[contains(@href,"/relationship/77a0f1d3-f9ec-4055-a6e7-24d7258c21f7/delete")]',
+            'has a link to delete the relationship type');
+    $tx->ok('//a[contains(@href,"/relationships/artist-artist/create")]',
+            'has a link to create new relationship types');
+};
+
+test 'Viewing /relationships shows a full tree' => sub {
+    my $test = shift;
+    my $mech = $test->mech;
+
+    $mech->get_ok('/relationships');
+    my $tx = Test::XPath->new( xml => $mech->content, is_html => 1 );
+    $tx->ok('//a[contains(@href,"/relationships/artist-artist")]',
+            'has a link to artist-artist relationships');
+    $tx->ok('//a[contains(@href,"/relationships/work-work")]',
+            'has a link to work-work relationships');
+};
+
+1;
