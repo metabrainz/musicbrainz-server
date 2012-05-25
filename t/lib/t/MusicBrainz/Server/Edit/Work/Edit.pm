635fa650164149f8942856ded313be247cbedd2a
Oliver Charles <oliver.g.charles@googlemail.com>
2012-05-25 14:55:29 +0100

    MBS-4772: Fix ModBot applying old edit work edits that change ISWCs

--- t/lib/t/MusicBrainz/Server/Edit/Work/Edit.pm
+++ t/lib/t/MusicBrainz/Server/Edit/Work/Edit.pm
@@ -2,6 +2,7 @@ package t::MusicBrainz::Server::Edit::Work::Edit;
 use Test::Routine;
 use Test::More;
 use Test::Fatal;
+use Test::Deep qw( cmp_set );
 
 with 't::Edit';
 with 't::Context';
@@ -11,10 +12,84 @@ BEGIN { use MusicBrainz::Server::Edit::Work::Edit };
 use MusicBrainz::Server::Constants qw( $EDIT_WORK_EDIT );
 use MusicBrainz::Server::Test qw( accept_edit reject_edit );
 
+test 'Old edit work edits to add ISWCs still pass (insert)' => sub {
+    my $test = shift;
+    my $c = $test->c;
+    $c->sql->do(<<'EOSQL');
+INSERT INTO work_name (id, name) VALUES (1, 'Work');
+INSERT INTO work (id, gid, name) VALUES (1, '51546e7c-b11d-410e-a0ff-6c88aa91f5ac', 1);
+INSERT INTO edit (expire_time, id, editor, type, status, data)
+    VALUES (now(), 1, 1, 42, 1, '{"entity":{"name":"Work","id":1},"new":{"iswc":"T-910.986.678-6"},"old":{"iswc":null}}')
+EOSQL
+
+    $c->model('Edit')->accept($c->model('Edit')->get_by_id(1));
+
+    my @iswcs = $c->model('ISWC')->find_by_works(1);
+    cmp_set([ map { $_->iswc } @iswcs ], [ 'T-910.986.678-6' ]);
+};
+
+test 'Old edit work edits to add ISWCs still pass (update)' => sub {
+    my $test = shift;
+    my $c = $test->c;
+    $c->sql->do(<<'EOSQL');
+INSERT INTO work_name (id, name) VALUES (1, 'Work');
+INSERT INTO work (id, gid, name) VALUES (1, '51546e7c-b11d-410e-a0ff-6c88aa91f5ac', 1);
+INSERT INTO iswc (id, work, iswc) VALUES (1, 1, 'T-110.986.678-6');
+INSERT INTO edit (expire_time, id, editor, type, status, data)
+    VALUES (now(), 1, 1, 42, 1, '{"entity":{"name":"Work","id":1},"new":{"iswc":"T-910.986.678-6"},"old":{"iswc":"T-110.986.678-6"}}')
+EOSQL
+
+    $c->model('Edit')->accept($c->model('Edit')->get_by_id(1));
+
+    my @iswcs = $c->model('ISWC')->find_by_works(1);
+    cmp_set([ map { $_->iswc } @iswcs ], [ 'T-910.986.678-6' ]);
+};
+
+test 'Old edit work edits to add ISWCs still pass (delete)' => sub {
+    my $test = shift;
+    my $c = $test->c;
+    $c->sql->do(<<'EOSQL');
+INSERT INTO work_name (id, name) VALUES (1, 'Work');
+INSERT INTO work (id, gid, name) VALUES (1, '51546e7c-b11d-410e-a0ff-6c88aa91f5ac', 1);
+INSERT INTO iswc (id, work, iswc) VALUES (1, 1, 'T-110.986.678-6');
+INSERT INTO edit (expire_time, id, editor, type, status, data)
+    VALUES (now(), 1, 1, 42, 1, '{"entity":{"name":"Work","id":1},"new":{"iswc":null},"old":{"iswc":"T-110.986.678-6"}}')
+EOSQL
+
+    $c->model('Edit')->accept($c->model('Edit')->get_by_id(1));
+
+    my @iswcs = $c->model('ISWC')->find_by_works(1);
+    cmp_set([ map { $_->iswc } @iswcs ], [ ]);
+};
+
+test 'Old edit work edits to add ISWCs still pass (conflict)' => sub {
+    my $test = shift;
+    my $c = $test->c;
+    $c->sql->do(<<'EOSQL');
+INSERT INTO work_name (id, name) VALUES (1, 'Work');
+INSERT INTO work (id, gid, name) VALUES (1, '51546e7c-b11d-410e-a0ff-6c88aa91f5ac', 1);
+INSERT INTO iswc (id, work, iswc) VALUES (1, 1, 'T-110.986.678-6');
+INSERT INTO edit (expire_time, id, editor, type, status, data)
+    VALUES (now(), 1, 1, 42, 1, '{"entity":{"name":"Work","id":1},"new":{"iswc":"T-910.986.678-6"},"old":{"iswc":null}}')
+EOSQL
+
+    $c->model('Edit')->accept($c->model('Edit')->get_by_id(1));
+
+    my @iswcs = $c->model('ISWC')->find_by_works(1);
+    cmp_set([ map { $_->iswc } @iswcs ], [ 'T-110.986.678-6' ]);
+};
+
 test all => sub {
 
 my $test = shift;
 my $c = $test->c;
+
+my %args = (
+    name => 'Edited name',
+    comment => 'Edited comment',
+    type_id => 1,
+    language_id => 1,
+);
 
 MusicBrainz::Server::Test->prepare_test_database($c, '+edit_work');
 MusicBrainz::Server::Test->prepare_test_database($c, '+language');
@@ -23,7 +98,7 @@ my $work = $c->model('Work')->get_by_id(1);
 is_unchanged($work);
 is($work->edits_pending, 0);
 
-my $edit = create_edit($c, $work);
+my $edit = create_edit($c, $work, %args);
 isa_ok($edit, 'MusicBrainz::Server::Edit::Work::Edit');
 
 my ($edits) = $c->model('Edit')->find({ work => 1 }, 10, 0);
@@ -40,7 +115,7 @@ is_unchanged($work);
 is($work->edits_pending, 0);
 
 $work = $c->model('Work')->get_by_id(1);
-$edit = create_edit($c, $work);
+$edit = create_edit($c, $work, %args);
 accept_edit($c, $edit);
 
 $work = $c->model('Work')->get_by_id(1);
@@ -114,10 +189,7 @@ sub create_edit {
         edit_type => $EDIT_WORK_EDIT,
         editor_id => 1,
         to_edit => $work,
-        name => 'Edited name',
-        comment => 'Edited comment',
-        type_id => 1,
-        language_id => 1,
+        @_
     );
 }
 
