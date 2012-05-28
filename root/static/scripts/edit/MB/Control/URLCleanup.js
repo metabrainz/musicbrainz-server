14afe1f3ce6c22276a2bdc1a3c5a634143709974
reosarevok <reosarevok@rika.musicbrainz.org>
2012-05-28 21:51:05 +0000

    Fixing the crap I fixed wrong

--- root/static/scripts/edit/MB/Control/URLCleanup.js
+++ root/static/scripts/edit/MB/Control/URLCleanup.js
@@ -136,7 +136,7 @@ MB.constants.CLEANUPS = {
             url =  url.replace(/\.wikipedia\.org\/w\/index\.php\?title=([^&]+).*/, ".wikipedia.org/wiki/$1");
             url =  url.replace(/\.wikipedia\.org\/[a-z-]+\/([^?]+)$/, ".wikipedia.org/wiki/$1");
             if ((m = url.match(/^(.*\.wikipedia\.org\/wiki\/)([^?#]+)(.*)$/)) != null)
-            url = m[1] + encodeURIComponent(decodeURIComponent(m[2])).replace(/%20/g, "_").replace(/%24/g, "$").replace(/%2C/g, ",").replace(/%2F/g, "/").replace(/%3A/g, ":").replace(/%3B/g, ";").replace(/%40/g, "@") + m[3];
+                url = m[1] + encodeURIComponent(decodeURIComponent(m[2])).replace(/%20/g, "_").replace(/%24/g, "$").replace(/%2C/g, ",").replace(/%2F/g, "/").replace(/%3A/g, ":").replace(/%3B/g, ";").replace(/%40/g, "@") + m[3];
             return url;
         }
     },
