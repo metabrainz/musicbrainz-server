b5f8cc3ece8281bbe40a2f93eb2784b3f1352dd4
reosarevok <reosarevok@rika.musicbrainz.org>
2012-05-28 21:49:19 +0000

    Also cleanup www. myspace URLs

--- root/static/scripts/edit/MB/Control/URLCleanup.js
+++ root/static/scripts/edit/MB/Control/URLCleanup.js
@@ -136,7 +136,7 @@ MB.constants.CLEANUPS = {
             url =  url.replace(/\.wikipedia\.org\/w\/index\.php\?title=([^&]+).*/, ".wikipedia.org/wiki/$1");
             url =  url.replace(/\.wikipedia\.org\/[a-z-]+\/([^?]+)$/, ".wikipedia.org/wiki/$1");
             if ((m = url.match(/^(.*\.wikipedia\.org\/wiki\/)([^?#]+)(.*)$/)) != null)
-                url = m[1] + encodeURIComponent(decodeURIComponent(m[2])).replace(/%20/g, "_").replace(/%24/g, "$").replace(/%2C/g, ",").replace(/%2F/g, "/").replace(/%3A/g, ":").replace(/%3B/g, ";").replace(/%40/g, "@") + m[3];
+                ºurl = m[1] + encodeURIComponent(decodeURIComponent(m[2])).replace(/%20/g, "_").replace(/%24/g, "$").replace(/%2C/g, ",").replace(/%2F/g, "/").replace(/%3A/g, ":").replace(/%3B/g, ";").replace(/%40/g, "@") + m[3];
             return url;
         }
     },
@@ -166,7 +166,7 @@ MB.constants.CLEANUPS = {
         match: new RegExp("^(https?://)?([^/]+\\.)?myspace\\.com","i"),
         type: MB.constants.LINK_TYPES.myspace,
         clean: function(url) {
-            return url.replace(/^(https?:\/\/)?myspace\.com/, "http://www.myspace.com");
+            return url.replace(/^(https?:\/\/)?(www\.)?myspace\.com/, "http://www.myspace.com");
         }
     },
     purevolume: {
