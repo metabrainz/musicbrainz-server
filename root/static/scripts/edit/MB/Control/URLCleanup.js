7300e84b5e6b092652b91b4f3d0deb35f2efc402
reosarevok <reosarevok@rika.musicbrainz.org>
2012-06-06 16:30:14 +0000

    Updating to new Jamendo URLs

--- root/static/scripts/edit/MB/Control/URLCleanup.js
+++ root/static/scripts/edit/MB/Control/URLCleanup.js
@@ -227,7 +227,7 @@ MB.constants.CLEANUPS = {
         match: new RegExp("^(https?://)?([^/]+\\.)?jamendo\\.com","i"),
         type: MB.constants.LINK_TYPES.downloadfree,
         clean: function(url) {
-            url =  url.replace(/jamendo\.com\/\w\w\/album\//, "jamendo.com/album/");
+            url =  url.replace(/jamendo\.com\/\w\w\/(album|list)\/([^\/]+)\/(.*)?/, "jamendo.com/$1/$2");
             url =  url.replace(/img\.jamendo\.com\/albums\/(\d+)\/covers\/\d+\.\d+\.jpg/, "www.jamendo.com/album/$1/");
             return url.replace(/jamendo\.com\/\w\w\/artist\//, "jamendo.com/artist/");
         }
