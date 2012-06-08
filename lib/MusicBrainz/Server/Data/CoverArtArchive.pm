cb7504533d359ec4ea5732e7a243db5d6388219c
Oliver Charles <oliver.g.charles@googlemail.com>
2012-06-08 17:15:22 +0100

    Fix exception when trying to accept edit cover art work edits for cover art that no longer exists

--- lib/MusicBrainz/Server/Data/CoverArtArchive.pm
+++ lib/MusicBrainz/Server/Data/CoverArtArchive.pm
@@ -158,6 +158,13 @@ sub merge_releases {
     }
 }
 
+sub exists {
+    my ($self, $id) = @_;
+    my $row = $self->c->sql->select_single_value(
+        'SELECT TRUE FROM cover_art_archive.cover_art WHERE id = ?', $id
+    ) or return undef;
+}
+
 1;
 
 =head1 COPYRIGHT
