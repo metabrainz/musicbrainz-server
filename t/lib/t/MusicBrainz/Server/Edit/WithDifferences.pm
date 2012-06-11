5e38b9d9b1dea9f136fa847d684dad9a8eda73d5
Oliver Charles <oliver.g.charles@googlemail.com>
2012-06-11 15:15:59 +0100

    Move z_edit_differences.t to a t/tests.t compatible test

---
+++ t/lib/t/MusicBrainz/Server/Edit/WithDifferences.pm
@@ -0,0 +1,41 @@
+package t::MusicBrainz::Server::Edit::WithDifferences;
+use Test::Routine;
+use Test::More;
+
+{
+    package t::MusicBrainz::Server::Edit::WithDifferences::Entity;
+    use Moose;
+
+    has 'foo_id' => (
+        is => 'ro',
+        isa => 'Int',
+    );
+
+    sub code { 'potatoes' }
+};
+
+{
+    package t::MusicBrainz::Server::Edit::WithDifferences::TestEdit;
+    use Moose;
+    extends 'MusicBrainz::Server::Edit::WithDifferences';
+
+    sub edit_name { 'Test Edit' }
+    sub edit_type { 12345 }
+
+    sub _mapping
+    {
+        return (
+            foo => 'foo_id',
+            bar => sub { shift->code }
+        )
+    }
+};
+
+test 'Check _change_hash' => sub {
+    my $edit = t::MusicBrainz::Server::Edit::WithDifferences::TestEdit->new;
+    my $instance = t::MusicBrainz::Server::Edit::WithDifferences::Entity->new(foo_id => 5);
+    is_deeply($edit->_change_hash($instance, qw( foo bar )), { foo => 5, bar => 'potatoes' });
+
+};
+
+1;
