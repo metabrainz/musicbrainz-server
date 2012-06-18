23a5c17292cab0567c49af5ef6234b2156d45207
Oliver Charles <oliver.g.charles@googlemail.com>
2012-06-18 15:42:09 +0100

    Fix deploy scripts now they are in admin/

--- admin/production-deploy.sh
+++ admin/production-deploy.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
-cd `dirname $0`
+mb_server=`dirname $0`/..
+cd $mb_server
 
 echo `date` : Updating from Git
 git pull --ff-only
 
 # Clear the Perl environment from anything now, and use the Carton environment
 eval $(perl -Mlocal::lib)
-export PERL_CARTON_PATH=`dirname $0`/local
+export PERL_CARTON_PATH=$mb_server/local
 
 echo `date` : "Checking dependencies (if this fails on libintl-perl, don't worry)"
 [ -f .carton.lock.md5 ] && (grep $(md5sum carton.lock) .carton.lock.md5 >/dev/null || carton install --deployment)
