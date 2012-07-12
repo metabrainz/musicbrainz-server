035bdfc2a2e2db3435158120364a0de3b296275f
Ian McEwen <ianmcorvidae@ianmcorvidae.net>
2012-07-11 21:33:03 -0700

    Change test re: ocharles' suggestions  * Instead of going through the login/verify/reverify rigmarole, simply    update the editor table  * Move some of the extra checking I added over to the Register test.

--- t/lib/t/MusicBrainz/Server/Controller/User/Reverify.pm
+++ t/lib/t/MusicBrainz/Server/Controller/User/Reverify.pm
@@ -1,8 +1,7 @@
 package t::MusicBrainz::Server::Controller::User::Reverify;
 use Test::Routine;
 use Test::More;
-use Test::MockTime qw( :all );
-use MusicBrainz::Server::Test qw( html_ok );
+use DateTime;
 
 with 't::Mechanize', 't::Context';
 
@@ -14,42 +13,18 @@ my $c    = $test->c;
 
 MusicBrainz::Server::Test->prepare_test_database($c, '+editor');
 
+ok($c->sql->do('UPDATE editor SET email = ?, email_confirm_date = ? WHERE name = ?', 'new_email@example.com', '2003-01-01T00:00:00+0:00', 'new_editor'),
+   'successfully inserted 2003-01-01 as old verification date');
+ok(DateTime->now->year > 2003, 'current year is bigger than 2003');
+
 $mech->get('/login');
 $mech->submit_form( with_fields => { username => 'new_editor', password => 'password' } );
 
-$mech->get_ok('/account/edit');
-html_ok($mech->content);
-$mech->submit_form( with_fields => {
-    'profile.website' => 'http://example.com/~new_editor/',
-    'profile.biography' => 'hello world!',
-    'profile.email' => 'new_email@example.com',
-} );
-$mech->content_contains('Your profile has been updated');
-$mech->content_contains('We have sent you a verification email');
-
-my $email_transport = MusicBrainz::Server::Email->get_test_transport;
-my $email = $email_transport->deliveries->[-1]->{email};
-is($email->get_header('To'), 'new_email@example.com', 'email sent to right place');
-is($email->get_header('Subject'), 'Please verify your email address', 'email subject is correct');
-like($email->get_body, qr{http://localhost/verify-email.*}, 'email contains verify-email link');
-
-$email->get_body =~ qr{http://localhost(/verify-email.*)};
-my $verify_email_path = $1;
-$mech->get_ok($verify_email_path);
-$mech->content_contains("Thank you, your email address has now been verified!");
-my $orig_now = $c->sql->select_single_value('select now()');
-
-$mech->get('/user/new_editor');
-$mech->content_like(qr{\(verified at (.*)\)});
-$mech->content =~ qr{\(verified at (.*)\)};
-my $original_verification = $1;
-like($original_verification, qr{\d+.\d+.\d+ \d+.\d+}, "Verification $original_verification looks like a date");
 $mech->content_contains('/account/resend-verification');
-
-set_relative_time(1); #ensure the timestamp will be different
 
 $mech->follow_link_ok({ url_regex => qr%/account/resend-verification% }, "User page contains a reverification link");
 
+my $email_transport = MusicBrainz::Server::Email->get_test_transport;
 my $reverify_email = $email_transport->deliveries->[-1]->{email};
 is($reverify_email->get_header('To'), 'new_email@example.com', 'email sent to right place');
 is($reverify_email->get_header('Subject'), 'Please verify your email address', 'email subject is correct');
@@ -57,7 +32,6 @@ like($reverify_email->get_body, qr{http://localhost/verify-email.*}, 'email cont
 
 $reverify_email->get_body =~ qr{http://localhost(/verify-email.*)};
 my $reverify_email_path = $1;
-isnt($reverify_email_path, $verify_email_path, "Email verification paths differ");
 $mech->get_ok($reverify_email_path);
 $mech->content_contains("Thank you, your email address has now been verified!");
 
@@ -66,8 +40,9 @@ $mech->content_like(qr{\(verified at (.*)\)});
 $mech->content =~ qr{\(verified at (.*)\)};
 my $reverification = $1;
 like($reverification, qr{\d+.\d+.\d+ \d+.\d+}, "Reverification $reverification looks like a date");
-
-restore_time();
+$reverification =~ qr{\d+.\d+.(\d+) \d+.\d+}; # yes, this depends on the format in t/sql/editor.sql not changing
+my $reverification_year = $1;
+ok($reverification_year > 2003, "reverification is after original verification");
 
 };
 
