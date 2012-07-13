81f112eb49cbced222d8e610bb145648902c317a
Ian McEwen <ianmcorvidae@ianmcorvidae.net>
2012-07-13 15:19:14 -0700

    Cosmetic fixes:  * put country names in parens  * put bare codes in square brackets  * sort the list

--- lib/MusicBrainz/Server/Controller/Root.pm
+++ lib/MusicBrainz/Server/Controller/Root.pm
@@ -11,6 +11,7 @@ use MusicBrainz::Server::Log qw( log_debug );
 use MusicBrainz::Server::Replication ':replication_type';
 
 use DateTime::Locale;
+use List::UtilsBy qw( sort_by );
 
 #
 # Sets the actions in this controller to be registered with no prefix
@@ -161,11 +162,13 @@ sub begin : Private
     # if no javascript cookie is set we don't know if javascript is enabled or not.
     my $jscookie = $c->request->cookie('javascript');
     my $js = $jscookie ? $jscookie->value : "unknown";
-    my @lang_with_locale = map { [ $_ => DateTime::Locale->load($_) ] } 
+    my @lang_with_locale = sort_by { $_->[1]->native_language } 
+                           map { [ $_ => DateTime::Locale->load($_) ] } 
                            grep { my $l = $_; 
                                   grep { $l eq $_ } DateTime::Locale->ids() } 
                            map { s/-([a-z]{2})/_\U$1/; $_; } DBDefs::MB_LANGUAGES();
-    my @lang_without_locale = map { [ $_ => {'native_name' => $_} ] } 
+    my @lang_without_locale = sort_by { $_->[1]->{id} } 
+                              map { [ $_ => {'id' => $_, 'native_language' => ''} ] } 
                               grep { my $l = $_; 
                                      !(grep { $l eq $_ } DateTime::Locale->ids()) } 
                               map { s/-([a-z]{2})/_\U$1/; $_; } DBDefs::MB_LANGUAGES();
