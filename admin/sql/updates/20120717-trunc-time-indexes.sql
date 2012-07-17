831207a345e8b16597154776ad86444c96db796a
Oliver Charles <oliver.g.charles@googlemail.com>
2012-07-17 15:45:38 +0100

    MBS-4439: Do not create indexes in a transaction as this is not possible

--- admin/sql/updates/20120717-trunc-time-indexes.sql
+++ admin/sql/updates/20120717-trunc-time-indexes.sql
@@ -1,7 +1,3 @@
-BEGIN;
-
 CREATE INDEX CONCURRENTLY ON edit (date_trunc('day', open_time AT TIME ZONE 'UTC'));
 CREATE INDEX CONCURRENTLY ON edit (date_trunc('day', close_time AT TIME ZONE 'UTC'));
 CREATE INDEX CONCURRENTLY ON edit (date_trunc('day', expire_time AT TIME ZONE 'UTC'));
-
-COMMIT;
