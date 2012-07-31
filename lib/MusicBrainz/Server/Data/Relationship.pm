019be26269c5658dcc48f1acd10407692fda479b
Oliver Charles <ollie@ocharles.org.uk>
2012-07-31 18:13:32 +0100

    Fix setting edits_pending to relationships since counters were temporarily disabled

--- lib/MusicBrainz/Server/Data/Relationship.pm
+++ lib/MusicBrainz/Server/Data/Relationship.pm
@@ -402,7 +402,7 @@ sub adjust_edit_pending
     $self->_check_types($type0, $type1);
 
     my $query = "UPDATE l_${type0}_${type1}
-                 SET edits_pending = edits_pending + ?
+                 SET edits_pending = numeric_larger(0, edits_pending + ?)
                  WHERE id IN (" . placeholders(@ids) . ")";
     $self->sql->do($query, $adjust, @ids);
 }
