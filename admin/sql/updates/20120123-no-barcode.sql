8648058c7acfa35b2b42038b132b2e4117f4bf2e
Oliver Charles <ollie@ocharles.org.uk>
2012-08-13 16:42:43 +0100

    MBS-5140: Remove unused update scripts
    The remaining update scripts may be useful for running again in the future, or
    are related to on-going problems. I have removed all scripts that I don't
    imagine we will need to run again. We can always fetch them back again from
    version control if need be.

--- admin/sql/updates/20120123-no-barcode.sql
+++
@@ -1,8 +0,0 @@
--- MBS-2799, convert releases with barcode set to all zeroes to the
--- empty string, to indicate the release has no barcode.
-
-BEGIN;
-
-UPDATE release SET barcode = '' WHERE barcode SIMILAR TO '00+';
-
-COMMIT;
