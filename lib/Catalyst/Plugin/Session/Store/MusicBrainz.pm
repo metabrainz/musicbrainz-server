730f6c4446b1f0bb24e43ddd6cd6deeb668e9514
warp <kuno@frob.nl>
2012-08-15 16:39:24 +0200

    MBS-5086, Catch the save session data exception and log it instead of failing to render the page.

---
+++ lib/Catalyst/Plugin/Session/Store/MusicBrainz.pm
@@ -0,0 +1,41 @@
+package Catalyst::Plugin::Session::Store::MusicBrainz;
+
+use Moose;
+use Try::Tiny;
+
+extends 'Catalyst::Plugin::Session::Store::Memcached';
+
+around 'store_session_data' => sub {
+    my $orig = shift;
+    my $c = shift;
+
+    try {
+        $c->$orig(@_);
+    } catch {
+        $c->log->error ("Cannot save session to memcached, kick some servers!");
+    };
+};
+
+=head1 LICENSE
+
+Copyright (C) 2012 MetaBrainz Foundation
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+=cut
+
+1;
+
+
