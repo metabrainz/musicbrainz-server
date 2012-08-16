89d6e089246d145dc100ffe872a0d75f5d87bf64
Michael Wiencek <mwtuea@gmail.com>
2012-08-16 08:26:13 +0000

    fix tests again

--- root/static/scripts/tests/RelationshipEditor.js
+++ root/static/scripts/tests/RelationshipEditor.js
@@ -1,35 +1,7 @@
 MB.tests.RelationshipEditor = (MB.tests.RelationshipEditor) ? MB.tests.RelationshipEditor : {};
 
-var typeInfo = {
-    "69":{"attrs":{"1":[0,1],"14":[0,null]},"types":["recording","release"],"link_phrase":"{additional} {instrument} samples from","reverse_link_phrase":"{additional:additionally} {instrument} sampled by","id":69,"descr":"Indicates that a track contains samples from an album. (<a href=\"http://musicbrainz.org/doc/Samples_Relationship_Type\">Details</a>)","child_order":0},
-    "122":{"types":["artist","recording"],"link_phrase":"performance","children":[156,150,151,152],"reverse_link_phrase":"performance","id":122,"child_order":0},
-    "141":{"parent":160,"attrs":{"1":[0,1],"527":[0,1],"526":[0,1],"424":[0,1],"425":[0,1]},"types":["artist","recording"],"descr":"Indicates the producer, co-producer, executive producer or co-executive producer for this work. (<a href=\"http://musicbrainz.org/doc/Producer_Relationship_Type\">Details</a>)","link_phrase":"{additional:additionally} {assistant} {associate} {co:co-}{executive:executive }produced","id":141,"reverse_link_phrase":"{additional} {assistant} {associate} {co:co-}{executive:executive }producer","child_order":0},
-    "148":{"parent":156,"attrs":{"596":[0,1],"1":[0,1],"194":[0,1],"14":[1,null]},"types":["artist","recording"],"descr":"Indicates an artist that performed a particular instrument on this work. (<a href=\"http://musicbrainz.org/doc/Performer_Relationship_Type\">Details</a>)","link_phrase":"{additional} {guest} {solo} {instrument}","id":148,"reverse_link_phrase":"{additional} {guest} {solo} {instrument}","child_order":0},
-    "154":{"parent":157,"attrs":{"1":[0,1],"14":[0,null]},"types":["artist","recording"],"descr":"Indicates that the track contains samples from material that was originally performed by another artist. Use this only if you really cannot figure out the particular track that has been sampled. (<a href=\"http://musicbrainz.org/doc/Samples_Artist_Relationship_Type\">Details</a>)","link_phrase":"produced {instrument} material that was {additional:additionally} sampled in","id":154,"reverse_link_phrase":"contains {additional} {instrument} samples by","child_order":1},
-    "156":{"parent":122,"attrs":{"596":[0,1],"1":[0,1],"194":[0,1]},"types":["artist","recording"],"children":[148,149],"descr":"Indicates an artist that performed on this work. (<a href=\"http://musicbrainz.org/doc/Performer_Relationship_Type\">Details</a>)","link_phrase":"{additional:additionally} {guest} {solo} performed","id":156,"reverse_link_phrase":"{additional} {guest} {solo} performer","child_order":0},
-    "245":{"types":["recording","work"],"link_phrase":"covers or other versions","children":[278],"reverse_link_phrase":"covers or other versions","id":245,"child_order":0},
-    "278":{"parent":245,"attrs":{"578":[0,1],"567":[0,1],"580":[0,1],"579":[0,1]},"types":["recording","work"],"descr":"This is used to link works to their recordings. (<a href=\"http://musicbrainz.org/doc/Performance_Relationship_Type\">Details</a>)","link_phrase":"{partial} {live} {instrumental} {cover} recording of","id":278,"reverse_link_phrase":"{partial} {live} {instrumental} {cover} recordings","child_order":0},
-};
-
-var attrMap = {
-    "1":{"name":"additional","id":1,"descr":"This attribute describes if a particular performance role was considered normal or additional.","child_order":0},
-    "14":{"name":"instrument","children":["15","69","124","159","185"],"id":14,"descr":"This attribute describes the possible instruments that can be captured as part of a performance. <br/> Can't find an instrument? <a href=\"http://wiki.musicbrainz.org/Advanced_Instrument_Tree\">Request it!</a>","child_order":3},
-    "18":{"parent":17,"name":"bagpipe","children":["514","515"],"id":18,"child_order":0},
-    "22":{"parent":493,"name":"oboe","children":["581","604","21","261","380"],"id":22,"child_order":0},
-    "194":{"name":"guest","id":194,"descr":"This attribute indicates a 'guest' performance where the performer is not usually part of the band.","child_order":0},
-    "229":{"parent":75,"name":"guitar","children":["77","522","79","80","377","529","399","400"],"id":229,"child_order":0},
-    "424":{"name":"co","id":424,"descr":"co-[role]","child_order":0},
-    "425":{"name":"executive","id":425,"descr":"This attribute is to be used if the role was fulfilled in an executive capacity.","child_order":0},
-    "427":{"parent":17,"name":"crumhorn","id":427,"child_order":5},
-    "526":{"name":"assistant","id":526,"descr":"This typically indicates someone who is either a first-timer, or less experienced, and who is working under the direction of someone who is more experienced.","child_order":0},
-    "527":{"name":"associate","id":527,"descr":"This typically indicates someone who is less experienced and who is working under the direction of someone who is more experienced.","child_order":0},
-    "567":{"name":"cover","id":567,"descr":"Indicates that one entity is a cover of another entity","child_order":0},
-    "578":{"name":"live","id":578,"descr":"This indicates that the recording is of a live performance.","child_order":0},
-    "579":{"name":"partial","id":579,"descr":"This indicates that the recording is not of the entire work, e.g. excerpts from, conclusion of, etc.","child_order":0},
-    "580":{"name":"instrumental","id":580,"descr":"For works that have lyrics, this indicates that those lyrics are not relevant to this recording. Examples include instrumental arrangements, or \"beats\" from hip-hop songs which may be reused with different lyrics.","child_order":0},
-    "595":{"parent":283,"name":"haegeum","id":595,"child_order":0},
-    "596":{"name":"solo","id":596,"descr":"This should be used when an artist is credited in liner notes or a similar source as performing a solo part.","child_order":0},
-};
+var typeInfo = {"recording-release":[{"attrs":{"1":[0,1],"14":[0,null]},"reverse_phrase":"{additional:additionally} {instrument} sampled by","id":69,"phrase":"{additional} {instrument} samples from","descr":"Indicates that a track contains samples from an album. (<a href=\"http://musicbrainz.org/doc/Samples_Relationship_Type\">Details</a>)"}],"artist-recording":[{"reverse_phrase":"performance","children":[{"attrs":{"1":[0,1],"194":[0,1],"596":[0,1]},"reverse_phrase":"{additional} {guest} {solo} performer","children":[{"attrs":{"1":[0,1],"14":[1,null],"194":[0,1],"596":[0,1]},"reverse_phrase":"{additional} {guest} {solo} {instrument}","id":148,"phrase":"{additional} {guest} {solo} {instrument}","descr":"Indicates an artist that performed a particular instrument on this work. (<a href=\"http://musicbrainz.org/doc/Performer_Relationship_Type\">Details</a>)"},{"attrs":{"1":[0,1],"3":[0,null],"194":[0,1],"596":[0,1]},"reverse_phrase":"{additional} {guest} {solo} {vocal} vocals","id":149,"phrase":"{additional} {guest} {solo} {vocal} vocals","descr":"Indicates an artist performed in a particular voice on this work. (<a href=\"http://musicbrainz.org/doc/Performer_Relationship_Type\">Details</a>)"}],"id":156,"phrase":"{additional:additionally} {guest} {solo} performed","descr":"Indicates an artist that performed on this work. (<a href=\"http://musicbrainz.org/doc/Performer_Relationship_Type\">Details</a>)"}],"id":122,"phrase":"performance"},{"reverse_phrase":"remixes","children":[{"attrs":{"1":[0,1],"14":[0,null]},"reverse_phrase":"contains {additional} {instrument} samples by","id":154,"phrase":"produced {instrument} material that was {additional:additionally} sampled in","descr":"Indicates that the track contains samples from material that was originally performed by another artist. Use this only if you really cannot figure out the particular track that has been sampled. (<a href=\"http://musicbrainz.org/doc/Samples_Artist_Relationship_Type\">Details</a>)"}],"id":157,"phrase":"remixes"},{"reverse_phrase":"production","children":[{"attrs":{"1":[0,1],"424":[0,1],"425":[0,1],"526":[0,1],"527":[0,1]},"reverse_phrase":"{additional} {assistant} {associate} {co:co-}{executive:executive }producer","id":141,"phrase":"{additional:additionally} {assistant} {associate} {co:co-}{executive:executive }produced","descr":"Indicates the producer, co-producer, executive producer or co-executive producer for this work. (<a href=\"http://musicbrainz.org/doc/Producer_Relationship_Type\">Details</a>)"}],"id":160,"phrase":"production"}],"recording-work":[{"reverse_phrase":"covers or other versions","children":[{"attrs":{"567":[0,1],"578":[0,1],"579":[0,1],"580":[0,1]},"reverse_phrase":"{partial} {live} {instrumental} {cover} recordings","id":278,"phrase":"{partial} {live} {instrumental} {cover} recording of","descr":"This is used to link works to their recordings. (<a href=\"http://musicbrainz.org/doc/Performance_Relationship_Type\">Details</a>)"}],"id":245,"phrase":"covers or other versions"}]},
+    attrInfo = {"partial":{"name":"partial","id":579},"cover":{"name":"cover","id":567},"executive":{"name":"executive","id":425},"co":{"name":"co","id":424},"solo":{"name":"solo","id":596},"instrumental":{"name":"instrumental","id":580},"instrument":{"name":"instrument","children":[{"name":"strings","children":[{"name":"plucked string instruments","children":[{"name":"guitars","children":[{"name":"guitar","id":229},{"name":"bass guitar","id":277}],"id":75},{"name":"lyre","id":109},{"name":"zither","id":123}],"id":302}],"id":69}],"id":14},"associate":{"name":"associate","id":527},"assistant":{"name":"assistant","id":526},"additional":{"name":"additional","id":1},"live":{"name":"live","id":578},"guest":{"name":"guest","id":194}};
 
 $.extend(MB.text = MB.text || {}, {
     Entity: {
@@ -134,19 +106,19 @@ MB.tests.RelationshipEditor.Util = function() {
         });
 
         tests = [
-            {root: RE.attrMap[424], value: undefined, expected: false},
-            {root: RE.attrMap[424], value: null, expected: false},
-            {root: RE.attrMap[424], value: 0, expected: false},
-            {root: RE.attrMap[424], value: 1, expected: true},
-            {root: RE.attrMap[14], value: undefined, expected: []},
-            {root: RE.attrMap[14], value: null, expected: []},
-            {root: RE.attrMap[14], value: 0, expected: []},
-            {root: RE.attrMap[14], value: [0], expected: []},
-            {root: RE.attrMap[14], value: ["foo", "bar", "baz"], expected: []},
-            {root: RE.attrMap[14], value: 1, expected: [1]},
-            {root: RE.attrMap[14], value: [3, 3, 2, 2, 1, 1], expected: [1, 2, 3]},
-            {root: RE.attrMap[14], value: ["3", "3", "2", "2", "1", "1"], expected: [1, 2, 3]},
-            {root: RE.attrMap[14], value: ["1", 0, "9", 0, "5"], expected: [1, 5, 9]},
+            {root: Util.attrInfo(424), value: undefined, expected: false},
+            {root: Util.attrInfo(424), value: null, expected: false},
+            {root: Util.attrInfo(424), value: 0, expected: false},
+            {root: Util.attrInfo(424), value: 1, expected: true},
+            {root: Util.attrInfo(14), value: undefined, expected: []},
+            {root: Util.attrInfo(14), value: null, expected: []},
+            {root: Util.attrInfo(14), value: 0, expected: []},
+            {root: Util.attrInfo(14), value: [0], expected: []},
+            {root: Util.attrInfo(14), value: ["foo", "bar", "baz"], expected: []},
+            {root: Util.attrInfo(14), value: 1, expected: [1]},
+            {root: Util.attrInfo(14), value: [3, 3, 2, 2, 1, 1], expected: [1, 2, 3]},
+            {root: Util.attrInfo(14), value: ["3", "3", "2", "2", "1", "1"], expected: [1, 2, 3]},
+            {root: Util.attrInfo(14), value: ["1", 0, "9", 0, "5"], expected: [1, 5, 9]},
         ];
 
         $.each(tests, function(i, test) {
@@ -235,23 +207,27 @@ MB.tests.RelationshipEditor.Relationship = function() {
             // test attribute interpolation
             {
                 linkType: 148,
-                attrs: {instrument: [229], solo: true},
-                expected: "solo guitar"
+                backward: false,
+                attrs: {instrument: [123, 229, 277], solo: true},
+                expected: "solo zither, guitar & bass guitar"
             },
             {
                 linkType: 141,
+                backward: true,
                 attrs: {co: true, executive: true},
                 expected: "co-executive producer"
             },
             {
                 linkType: 154,
-                attrs: {instrument: [18, 22, 427], additional: true},
-                expected: "contains additional bagpipe, oboe & crumhorn samples by"
+                backward: true,
+                attrs: {instrument: [69, 75, 109, 302], additional: true},
+                expected: "contains additional strings, guitars, lyre & plucked string instruments samples by"
             }
         ];
 
         $.each(tests, function(i, test) {
             relationship.link_type(test.linkType);
+            relationship.backward(test.backward);
             relationship.attributes(test.attrs);
             var result = relationship.buildLinkPhrase();
 
@@ -343,10 +319,7 @@ MB.tests.RelationshipEditor.Relationship = function() {
 MB.tests.RelationshipEditor.Run = function() {
     var RE = MB.RelationshipEditor;
 
-    RE.typeInfo = typeInfo;
-    RE.attrMap = attrMap;
-
-    RE.init({}, {});
+    RE.Util.init(typeInfo, attrInfo);
     RE.UI.Dialog.init();
 
     MB.tests.RelationshipEditor.Util();
