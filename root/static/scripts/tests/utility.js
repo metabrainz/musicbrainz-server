cdf5bae246400be0c4e964d3c13c45dd840d2888
warp <kuno@frob.nl>
2012-08-27 09:47:37 +0200

    Revert "Revert "Merge branch 'mbs-4382' into beta""
    This reverts commit 423360abfa0da5b368b36e7edf3efc9ea4f6ed40.

--- root/static/scripts/tests/utility.js
+++ root/static/scripts/tests/utility.js
@@ -45,7 +45,25 @@ MB.tests.utility.All = function() {
 
         var input1sha = b64_sha1 (MB.utility.structureToString (input1));
         QUnit.equal (input1sha, "aIkUXodpaNX7Q1YfttiKMkKCxB0", "SHA-1 of input1");
+
+        var seconds = 1000;
+        var minutes = 60 * seconds;
+        var hours = 60 * minutes;
+
+        QUnit.equal (MB.utility.formatTrackLength (23), '23 ms', 'formatTrackLength');
+        QUnit.equal (MB.utility.formatTrackLength (23 * seconds), '00:23', 'formatTrackLength');
+        QUnit.equal (MB.utility.formatTrackLength (59 * minutes), '59:00', 'formatTrackLength');
+        QUnit.equal (MB.utility.formatTrackLength (60 * minutes), '01:00:00', 'formatTrackLength');
+        QUnit.equal (MB.utility.formatTrackLength (14 * hours + 15 * minutes + 16 * seconds), '14:15:16', 'formatTrackLength');
+
+        QUnit.equal (MB.utility.unformatTrackLength ('23 ms'), 23, 'unformatTrackLength');
+        QUnit.equal (MB.utility.unformatTrackLength ('00:23'), 23 * seconds, 'unformatTrackLength');
+        QUnit.equal (MB.utility.unformatTrackLength (':57'), 57 * seconds, 'MBS-3352: Handle the case of ":57"');
+        QUnit.equal (MB.utility.unformatTrackLength ('59:00'), 59 * minutes, 'unformatTrackLength');
+        QUnit.equal (MB.utility.unformatTrackLength ('01:00:00'), 60 * minutes, 'unformatTrackLength');
+        QUnit.equal (MB.utility.unformatTrackLength ('14:15:16'), 14 * hours + 15 * minutes + 16 * seconds, 'unformatTrackLength');
     });
+
 };
 
 MB.tests.utility.Run = function() {
