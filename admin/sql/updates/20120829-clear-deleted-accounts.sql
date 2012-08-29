7b472649a0e4568d3daee04050a6d043c25e6882
Oliver Charles <ollie@ocharles.org.uk>
2012-08-29 18:37:44 +0100

    MBS-4946: Add a script to cleanup the previous account deletions

---
+++ admin/sql/updates/20120829-clear-deleted-accounts.sql
@@ -0,0 +1,21 @@
+BEGIN;
+
+SELECT id INTO TEMPORARY cleanup
+FROM editor
+WHERE (password IS NULL OR password = '')
+  AND name LIKE 'Deleted Editor #%';
+
+UPDATE editor SET
+  password = '',
+  privs = 0,
+  email = NULL,
+  website = NULL,
+  bio = NULL,
+  country = NULL,
+  birth_date = NULL,
+  gender = NULL
+WHERE id IN (SELECT id FROM cleanup);
+
+DELETE FROM editor_language WHERE editor IN (SELECT id FROM cleanup);
+
+COMMIT;
