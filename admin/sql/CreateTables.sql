9447fd28f45c4d5d50a98c1971d800e4848cdb0d
Oliver Charles <ollie@ocharles.org.uk>
2012-09-17 14:28:21 +0100

    MBS-5253: Add a `created` column to `release_group_secondary_type_join`
    The column has a default value of 2012-05-15 as that is when the table was
    added (thus no row can be older than that). By using a default date we able to
    add a NOT NULL constraint on this column too.

--- admin/sql/CreateTables.sql
+++ admin/sql/CreateTables.sql
@@ -1193,7 +1193,8 @@ CREATE TABLE release_group_primary_type (
 
 CREATE TABLE release_group_secondary_type (
     id SERIAL NOT NULL, -- pk
-    name TEXT NOT NULL
+    name TEXT NOT NULL,
+    created TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now()
 );
 
 CREATE TABLE release_group_secondary_type_join (
---
+++ admin/sql/updates/20120917-rg-st-created.sql
@@ -0,0 +1,8 @@
+BEGIN;
+
+ALTER TABLE release_group_secondary_type_join
+ADD COLUMN created TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT '2012-05-15';
+
+ALTER TABLE release_group_secondary_type_join ALTER COLUMN created SET DEFAULT now();
+
+COMMIT;
