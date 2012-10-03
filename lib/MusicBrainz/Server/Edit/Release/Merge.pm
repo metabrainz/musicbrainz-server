e013eb5f82f83f97fc9d6a46cb3bacb38d1ae702
Ian McEwen <ianmcorvidae@ianmcorvidae.net>
2012-10-03 02:42:32 -0700

    MBS-4104: Change to explicit if/else

--- lib/MusicBrainz/Server/Edit/Release/Merge.pm
+++ lib/MusicBrainz/Server/Edit/Release/Merge.pm
@@ -56,15 +56,17 @@ sub related_recordings
 {
     my ($self, $releases) = @_;
 
-    my @tracks;
     if ($self->data->{merge_strategy} == $MusicBrainz::Server::Data::Release::MERGE_MERGE) {
+        my @tracks;
         $self->c->model('Medium')->load_for_releases(@$releases);
         my @tracklists = map { $_->tracklist }
                          map { $_->all_mediums } @$releases;
         $self->c->model('Track')->load_for_tracklists(@tracklists);
         @tracks = map { $_->all_tracks } @tracklists;
+        return $self->c->model('Recording')->load(@tracks);
+    } else {
+        return ();
     }
-    return $self->c->model('Recording')->load(@tracks);
 }
 
 sub foreign_keys
