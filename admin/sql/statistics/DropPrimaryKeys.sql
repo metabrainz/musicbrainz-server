a0f77f7dc9668bab64f26b3f03958f6536846f42
Ian McEwen <ianmcorvidae@ianmcorvidae.net>
2012-10-05 20:15:32 -0700

    MBS-5289: log-statistics schema change scripts only

--- admin/sql/statistics/DropPrimaryKeys.sql
+++ admin/sql/statistics/DropPrimaryKeys.sql
@@ -1,5 +1,6 @@
 -- Automatically generated, do not edit.
 \unset ON_ERROR_STOP
 
+ALTER TABLE log_statistic DROP CONSTRAINT log_statistic_pkey;
 ALTER TABLE statistic DROP CONSTRAINT statistic_pkey;
 ALTER TABLE statistic_event DROP CONSTRAINT statistic_event_pkey;
--- upgrade.sh
+++ upgrade.sh
@@ -25,6 +25,12 @@ then
     ./admin/psql READWRITE < ./admin/sql/DropReplicationTriggers.sql
 fi
 
+if [ "$REPLICATION_TYPE" != "$RT_SLAVE" ]
+then
+    echo `date` : Disabling last_updated triggers
+    ./admin/sql/DisableLastUpdatedTriggers.pl
+fi
+
 ################################################################################
 # Scripts that should run on *all* nodes (master/slave/standalone)
 
@@ -37,8 +43,23 @@ OUTPUT=`./admin/psql READWRITE < ./admin/sql/updates/20120822-more-text-constrai
 echo `date` : Applying admin/sql/updates/20120917-rg-st-created.sql
 OUTPUT=`./admin/psql READWRITE < ./admin/sql/updates/20120917-rg-st-created.sql 2>&1` || ( echo "$OUTPUT" ; exit 1 )
 
+echo `date` : Applying admin/sql/updates/20120921-drop-url-descriptions.sql
+OUTPUT=`./admin/psql READWRITE < ./admin/sql/updates/20120921-drop-url-descriptions.sql 2>&1` || ( echo "$OUTPUT" ; exit 1 )
+
 echo `date` : Applying admin/sql/updates/20120922-move-statistics-tables.sql
 OUTPUT=`./admin/psql READWRITE < ./admin/sql/updates/20120922-move-statistics-tables.sql 2>&1` || ( echo "$OUTPUT" ; exit 1 )
+
+echo `date` : Applying admin/sql/updates/20120927-add-log-statistics.sql
+OUTPUT=`./admin/psql READWRITE < ./admin/sql/updates/20120927-add-log-statistics.sql 2>&1` || ( echo "$OUTPUT" ; exit 1 )
+
+if [ "$REPLICATION_TYPE" = "$RT_SLAVE" ]
+then
+    echo `date` : Applying admin/sql/updates/20120911-not-null-comments.sql
+    OUTPUT=`./admin/psql READWRITE < ./admin/sql/updates/20120911-not-null-comments.sql 2>&1` || ( echo "$OUTPUT" ; exit 1 )
+fi
+
+echo `date` : Applying admin/sql/updates/20120921-release-group-cover-art.sql
+OUTPUT=`./admin/psql READWRITE < ./admin/sql/updates/20120921-release-group-cover-art.sql 2>&1` || ( echo "$OUTPUT" ; exit 1 )
 
 ################################################################################
 # Re-enable replication
@@ -54,10 +75,16 @@ fi
 
 if [ "$REPLICATION_TYPE" != "$RT_SLAVE" ]
 then
-   echo `date` : Adding master constraints
+    echo `date` : Adding master constraints
 
-   echo `date` : Applying admin/sql/updates/20120822-more-text-constraints-master.sql
-   OUTPUT=`./admin/psql READWRITE < ./admin/sql/updates/20120822-more-text-constraints-master.sql 2>&1` || ( echo "$OUTPUT" ; exit 1 )
+    echo `date` : Enabling last_updated triggers
+    ./admin/sql/EnableLastUpdatedTriggers.pl
+
+    echo `date` : Applying admin/sql/updates/20120822-more-text-constraints-master.sql
+    OUTPUT=`./admin/psql READWRITE < ./admin/sql/updates/20120822-more-text-constraints-master.sql 2>&1` || ( echo "$OUTPUT" ; exit 1 )
+
+    echo `date` : Applying admin/sql/updates/20120911-not-null-comments-master.sql
+    OUTPUT=`./admin/psql READWRITE < ./admin/sql/updates/20120911-not-null-comments-master.sql 2>&1` || ( echo "$OUTPUT" ; exit 1 )
 fi
 
 ################################################################################
