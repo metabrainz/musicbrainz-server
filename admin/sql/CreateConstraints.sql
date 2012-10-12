bc078016c641f555c537a66bc439ec730a0ce354
Oliver Charles <ollie@ocharles.org.uk>
2012-10-12 11:04:23 +0100

    Revert "Remove constraints that haven't been applied to master"
    This reverts commit 4114e3baa55094f624611210189c615d9657c40a.

--- admin/sql/CreateConstraints.sql
+++ admin/sql/CreateConstraints.sql
@@ -1,6 +1,7 @@
 BEGIN;
 
 ALTER TABLE artist        ADD CHECK (controlled_for_whitespace(comment));
+ALTER TABLE artist_name   ADD CHECK (controlled_for_whitespace(name));
 ALTER TABLE label         ADD CHECK (controlled_for_whitespace(comment));
 ALTER TABLE label_name    ADD CHECK (controlled_for_whitespace(name));
 ALTER TABLE medium        ADD CHECK (controlled_for_whitespace(name));
@@ -10,8 +11,15 @@ ALTER TABLE release_group ADD CHECK (controlled_for_whitespace(comment));
 ALTER TABLE release_label ADD CHECK (controlled_for_whitespace(catalog_number));
 ALTER TABLE release_name  ADD CHECK (controlled_for_whitespace(name));
 ALTER TABLE track         ADD CHECK (controlled_for_whitespace(number));
+ALTER TABLE track_name    ADD CHECK (controlled_for_whitespace(name));
+ALTER TABLE url           ADD CHECK (controlled_for_whitespace(description));
 ALTER TABLE work          ADD CHECK (controlled_for_whitespace(comment));
 ALTER TABLE work_name     ADD CHECK (controlled_for_whitespace(name));
+
+ALTER TABLE artist_name ADD CHECK (name != '');
+ALTER TABLE label_name ADD CHECK (name != '');
+ALTER TABLE release_name ADD CHECK (name != '');
+ALTER TABLE work_name ADD CHECK (name != '');
 
 ALTER TABLE artist
 ADD CHECK (
