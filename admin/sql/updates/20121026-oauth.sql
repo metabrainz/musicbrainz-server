03878fce1160954f1d624d9ab864fa41f37c7815
Lukáš Lalinský <lalinsky@gmail.com>
2012-11-11 16:57:00 +0100

    Split the upgrade script

--- admin/sql/updates/20121026-oauth.sql
+++ admin/sql/updates/20121026-oauth.sql
@@ -1,10 +1,6 @@
 BEGIN;
 
 
-DROP TABLE editor_oauth_token;
-DROP TABLE application;
-
-
 CREATE TABLE application
 (
     id                  SERIAL,
@@ -36,27 +32,17 @@ ALTER TABLE editor_oauth_token ADD CONSTRAINT editor_oauth_token_pkey PRIMARY KE
 
 
 CREATE UNIQUE INDEX application_idx_oauth_id ON application (oauth_id);
+CREATE UNIQUE INDEX application_idx_owner ON application (owner);
+
+CREATE INDEX editor_oauth_token_idx_editor ON editor_oauth_token (editor);
 CREATE UNIQUE INDEX editor_oauth_token_idx_access_token ON editor_oauth_token (access_token);
 CREATE UNIQUE INDEX editor_oauth_token_idx_refresh_token ON editor_oauth_token (refresh_token);
 
 
-ALTER TABLE application
-   ADD CONSTRAINT application_fk_owner
-   FOREIGN KEY (owner)
-   REFERENCES editor(id);
-
-ALTER TABLE editor_oauth_token
-   ADD CONSTRAINT editor_oauth_token_fk_editor
-   FOREIGN KEY (editor)
-   REFERENCES editor(id);
-
-ALTER TABLE editor_oauth_token
-   ADD CONSTRAINT editor_oauth_token_fk_application
-   FOREIGN KEY (application)
-   REFERENCES application(id);
-
-
 INSERT INTO application (owner, name, oauth_id, oauth_secret)
    VALUES (1, 'MusicBrainz Test Application', 'uTuPnUfMRQPx8HBnHf22eg', '7Fjfp0ZBr1KtDRbnfVdmIw');
+
+SELECT setval('application_id_seq', (SELECT MAX(id) FROM application));
+SELECT setval('editor_oauth_token_id_seq', (SELECT MAX(id) FROM editor_oauth_token));
 
 COMMIT;
