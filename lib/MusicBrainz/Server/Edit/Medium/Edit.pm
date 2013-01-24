ae52f26515e40b6e24582bcdae7147b3aed7f98c
Ian McEwen <ianmcorvidae@ianmcorvidae.net>
2013-01-23 23:35:41 -0700

    MBS-3514: further improve recording & artist-credit displays

--- lib/MusicBrainz/Server/Edit/Medium/Edit.pm
+++ lib/MusicBrainz/Server/Edit/Medium/Edit.pm
@@ -270,29 +270,22 @@ sub build_display_data
         }
 
         $data->{artist_credit_changes} = [
-            grep { $_->[0] eq 'c' || $_->[0] eq '+' }
             grep {
                 ($_->[1] && hash_artist_credit($_->[1]->artist_credit))
                     ne
                 ($_->[2] && hash_artist_credit($_->[2]->artist_credit))
             }
-            @{ sdiff(
-                [ $data->{old}{tracklist}->all_tracks ],
-                [ $data->{new}{tracklist}->all_tracks ],
-                sub {
-                    my $track = shift;
-                    return join(
-                        '',
-                        $track->name,
-                        format_track_length($track->length),
-                        hash_artist_credit($track->artist_credit)
-                    );
-                }) }
-        ];
+            grep { $_->[0] ne '-' }
+            @$tracklist_changes ];
 
         $data->{recording_changes} = [
-            grep { $_->[1] && $_->[2] && $_->[1]->recording->id != $_->[2]->recording->id } @$tracklist_changes
-        ];
+            grep {
+                ($_->[1] && $_->[1]->recording->id)
+                    !=
+                ($_->[2] && $_->[2]->recording->id)
+            }
+            grep { $_->[0] ne '+' && $_->[0] ne '-' }
+            @$tracklist_changes ];
     }
 
     return $data;
