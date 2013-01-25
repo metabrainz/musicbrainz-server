a0eea5a8c85498ab49c17980534259bb33ed27cb
Oliver Charles <ollie@ocharles.org.uk>
2013-01-25 15:59:25 +0000

    MBS-5696: Catch DuplicateViolation errors in Generic::Edit and echo them via ModBot

--- lib/MusicBrainz/Server/Edit/Generic/Edit.pm
+++ lib/MusicBrainz/Server/Edit/Generic/Edit.pm
@@ -66,7 +66,22 @@ override 'accept' => sub
     }
 
     my $data = $self->_edit_hash(clone($self->data->{new}));
-    $self->c->model( $self->_edit_model )->update($self->entity_id, $data);
+    try {
+        $self->c->model( $self->_edit_model )->update($self->entity_id, $data);
+    }
+    catch {
+        if (blessed($_) && $_->isa('MusicBrainz::Server::Exceptions::DuplicateViolation')) {
+            my $conflict = $_->conflict;
+            MusicBrainz::Server::Edit::Exceptions::GeneralError->throw(
+                sprintf(
+                    'The changes in this edit cause it to conflict with another artist. ' .
+                    'You may need to merge this artist with "' . $conflict->name . '" ' .
+                    '(http://%s/artist/%s/).',
+                    DBDefs->WEB_SERVER, $conflict->gid
+                )
+            );
+        }
+    };
 };
 
 sub _edit_hash
