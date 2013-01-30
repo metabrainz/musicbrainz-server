7e233f68be81d8e7dda5aa45aa0743c5fed0f0d2
reosarevok <reosarevok@rika.musicbrainz.org>
2013-01-30 13:23:16 +0000

    Added an autoselect test too

--- root/static/scripts/tests/Control/URLCleanup.js
+++ root/static/scripts/tests/Control/URLCleanup.js
@@ -179,8 +179,11 @@ MB.tests.URLCleanup.GuessType = function() {
                 [
                     'recording', 'http://www.allmusic.com/performance/le-nozze-di-figaro-the-marriage-of-figaro-opera-k-492-mq0000061129/credits',
                     MB.constants.LINK_TYPES.allmusic.recording
+                ],
+                [
+                    'artist', 'http://www.secondhandsongs.com/artist/103',
+                    MB.constants.LINK_TYPES.secondhandsongs.artist
                 ]
-
             ];
 
         $.each(tests, function(i, test) {
