b6133497d6aa7225b9903e33fb5c4f60941f7bf2
Ian McEwen <ianmcorvidae@ianmcorvidae.net>
2013-01-29 22:24:53 -0700

    MBS-4997: also support lookup by a linked 'resource' that is the URL itself

--- lib/MusicBrainz/Server/Controller/WS/2/URL.pm
+++ lib/MusicBrainz/Server/Controller/WS/2/URL.pm
@@ -8,6 +8,12 @@ use Readonly;
 my $ws_defs = Data::OptList::mkopt([
      url => {
                          method   => 'GET',
+                         linked   => [ qw(resource) ],
+                         inc      => [ qw(_relations) ],
+                         optional => [ qw(fmt) ],
+     },
+     url => {
+                         method   => 'GET',
                          inc      => [ qw(_relations) ],
                          optional => [ qw(fmt) ],
      },
@@ -50,6 +56,35 @@ sub url_toplevel
 
     $self->load_relationships($c, $stash, $url);
 }
+
+sub url_browse : Private
+{
+    my ($self, $c) = @_;
+
+    my ($resource, $id) = @{ $c->stash->{linked} };
+
+    my $url;
+    if ($resource eq 'resource')
+    {
+        ($url) = $c->model('URL')->find_by_url($id);
+        $c->detach('not_found') unless ($url);
+    }
+
+    my $stash = WebServiceStash->new;
+
+    $self->url_toplevel ($c, $stash, $url);
+
+    $c->res->content_type($c->stash->{serializer}->mime_type . '; charset=utf-8');
+    $c->res->body($c->stash->{serializer}->serialize('url', $url, $c->stash->{inc}, $stash));
+}
+
+sub url_search : Chained('root') PathPart('url') Args(0)
+{
+    my ($self, $c) = @_;
+
+    $c->detach('url_browse') if ($c->stash->{linked});
+}
+
 
 __PACKAGE__->meta->make_immutable;
 1;
