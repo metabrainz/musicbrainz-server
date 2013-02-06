536b9e55d760bc525a7aeccdb2fc7a0d882fe998
Ben Ockmore <ben.sput@gmail.com>
2013-02-06 18:30:37 +0000

    Fixed observable assignment error.

--- root/static/scripts/relationship-editor/Fields.js
+++ root/static/scripts/relationship-editor/Fields.js
@@ -76,7 +76,7 @@ var validationHandlers = {
     },
 
     "entity.0": function(field, value, relationship) {
-        var entity0 = relationship.entity[0], entity1 = relationship.entity[1];
+        var entity0 = relationship.entity[0](), entity1 = relationship.entity[1]();
 
         if (!Util.isMBID(value.gid)) {
             field.error(MB.text.RequiredField);
