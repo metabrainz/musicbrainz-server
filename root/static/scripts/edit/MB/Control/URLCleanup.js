9b0a5693bec42b5471f5d977a5fd6891dd6b7b98
reosarevok <reosarevok@rika.musicbrainz.org>
2013-02-15 19:27:44 +0000

    Adding lyricsnmusic to the whitelist

--- root/static/scripts/edit/MB/Control/URLCleanup.js
+++ root/static/scripts/edit/MB/Control/URLCleanup.js
@@ -275,7 +275,7 @@ MB.constants.CLEANUPS = {
         }
     },
     lyrics: {
-        match: new RegExp("^(https?://)?([^/]+\\.)?(lyrics\\.wikia\\.com|directlyrics\\.com|lyricstatus\\.com|kasi-time\\.com|wikisource\\.org|recmusic\\.org|utamap\\.com|j-lyric\\.net)", "i"),
+        match: new RegExp("^(https?://)?([^/]+\\.)?(lyrics\\.wikia\\.com|directlyrics\\.com|lyricstatus\\.com|kasi-time\\.com|wikisource\\.org|recmusic\\.org|utamap\\.com|j-lyric\\.net|lyricsnmusic\\.com)", "i"),
         type: MB.constants.LINK_TYPES.lyrics,
         clean: function(url) {
             return url.replace(/^https:\/\/([a-z-]+\.)?wikisource\.org/, "http://$1wikisource.org");
