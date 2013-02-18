b023581240348a155ae4eabe90724fb073786ec0
Ian McEwen <ianmcorvidae@ianmcorvidae.net>
2013-02-17 22:12:31 -0700

    MBS-5358: Use Date::Calc::Days_in_Month for month/year-specific calculations

--- lib/MusicBrainz/Server/Form/Field/DatePeriod.pm
+++ lib/MusicBrainz/Server/Form/Field/DatePeriod.pm
@@ -28,8 +28,11 @@ after 'validate' => sub {
     return if any { $_->has_errors } map { $_->fields }
         $self->field('begin_date'), $self->field('end_date');
 
+    # If we got here, the dates are valid partial dates but may not be valid full dates
     return if !Date::Calc::check_date($begin->{year}, $begin->{month} || 1, $begin->{day} || 1);
-    return if !Date::Calc::check_date($end->{year}, $end->{month} || 12, $end->{day} || 31);
+    # Use a default year in case of a completely absent end date
+    return if !Date::Calc::check_date($end->{year}, $end->{month} || 12,
+                   $end->{day} || Date::Calc::Days_in_Month($end->{year} || 1, $end->{month} || 12));
 
     if ($end->{year} || $end->{month} || $end->{day}) {
         $self->field('ended')->value(1);
@@ -37,9 +40,10 @@ after 'validate' => sub {
 
     return 1 unless $begin->{year} && $end->{year};
 
+    # Use the end of the year/month so the end date can be less precise than the begin date
     my ($days) = Date::Calc::Delta_Days(
         $begin->{year}, $begin->{month} || 1, $begin->{day} || 1,
-        $end->{year},   $end->{month} || 12,   $end->{day} || 31
+        $end->{year},   $end->{month} || 12,   $end->{day} || Date::Calc::Days_in_Month($end->{year}, $end->{month} || 12)
     );
 
     if ($days < 0) {
