11461fd6cd710186486f5eefb4696ad53d321b27
Ian McEwen <ianmcorvidae@ianmcorvidae.net>
2013-02-27 23:12:34 -0700

    MBS-5955: HTML::FormHandler, cursed be its name, won't allow localize_meth while overloading a field definition

--- lib/MusicBrainz/Server/Form/Recording/Standalone.pm
+++ lib/MusicBrainz/Server/Form/Recording/Standalone.pm
@@ -1,12 +1,11 @@
 package MusicBrainz::Server::Form::Recording::Standalone;
 use HTML::FormHandler::Moose;
 extends 'MusicBrainz::Server::Form::Recording';
-use MusicBrainz::Server::Translation qw(l N_l);
+use MusicBrainz::Server::Translation qw( N_l );
 
 has_field '+edit_note' => (
     required => 1,
     required_message => N_l('You must provide an edit note when adding a standalone recording'),
-    localize_meth => sub { my ($self, @message) = @_; return l(@message); }
 );
 
 1;
