5c16061e35509cbb526de4975adbabe93f727dfd
warp <kuno@frob.nl>
2013-03-11 20:35:31 +0100

    MBS-1839, Don't confuse TEST and READWRITE databases in db setup scripts.

--- script/setup_development_db.sh
+++ script/setup_development_db.sh
@@ -9,13 +9,12 @@ if [ -z "$1" ]; then
     echo "  WARNING: running this script will overwrite the production database."
     echo "  If you are sure you want to do this, run this:"
     echo " "
-    echo "  carton exec -Ilib -- script/setup_development_db.pl --destroy-all-the-things"
+    echo "  carton exec -Ilib -- script/setup_development_db.sh --destroy-all-the-things"
     echo " "
     echo "=========================================================================="
 elif [ "$1" = "--destroy-all-the-things" ]; then
 
-    # FIXME: initialize the production database, not "create_test_db".
-    script/create_test_db.sh
+    script/create_test_db.sh READWRITE
     ./admin/psql READWRITE < ./t/sql/webservice.sql
     ./admin/psql READWRITE < ./t/sql/editor.sql
     ./admin/psql READWRITE < ./admin/sql/SetSequences.sql
