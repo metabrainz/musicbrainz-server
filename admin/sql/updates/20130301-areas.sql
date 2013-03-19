04f19a3081f2d8853eae975018686230c7a8dda9
Ian McEwen <ianmcorvidae@ianmcorvidae.net>
2013-03-18 19:04:56 -0700

    MBS-799/MBS-5919: add missing 'ended' column on area table

--- admin/sql/updates/20130301-areas.sql
+++ admin/sql/updates/20130301-areas.sql
@@ -18,7 +18,22 @@ CREATE TABLE area (id                SERIAL PRIMARY KEY,
                    begin_date_day    SMALLINT,
                    end_date_year     SMALLINT,
                    end_date_month    SMALLINT,
-                   end_date_day      SMALLINT);
+                   end_date_day      SMALLINT
+                   ended             BOOLEAN NOT NULL DEFAULT FALSE
+                   CHECK (
+                     (
+                       -- If any end date fields are not null, then ended must be true
+                       (end_date_year IS NOT NULL OR
+                        end_date_month IS NOT NULL OR
+                        end_date_day IS NOT NULL) AND
+                       ended = TRUE
+                     ) OR (
+                       -- Otherwise, all end date fields must be null
+                       (end_date_year IS NULL AND
+                        end_date_month IS NULL AND
+                        end_date_day IS NULL)
+                     )
+                   ));
 
 CREATE TABLE area_code_type (id    SERIAL PRIMARY KEY,
                              name  VARCHAR(255) NOT NULL);
