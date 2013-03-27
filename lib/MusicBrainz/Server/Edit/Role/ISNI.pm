6cf621ae4a78b9f401f460651fe8e61cac8fece5
Oliver Charles <ollie@ocharles.org.uk>
2013-03-27 12:19:08 +0000

    MBS-5302: Remove duplication between Edit::Role::IPI and Edit::Role::ISNI

--- lib/MusicBrainz/Server/Edit/Role/ISNI.pm
+++ lib/MusicBrainz/Server/Edit/Role/ISNI.pm
@@ -2,68 +2,14 @@ package MusicBrainz::Server::Edit::Role::ISNI;
 use 5.10.0;
 use Moose::Role;
 
-use Algorithm::Merge qw( diff3 );
-use Clone 'clone';
-
-before initialize => sub {
-    my ($self, %opts) = @_;
-    die "You must specify isni_codes" unless defined $opts{isni_codes};
-};
-
-around new_data => sub {
-    my $orig = shift;
-    my $self = shift;
-    my $new = clone ($self->$orig (@_));
-
-    # merge_changes only looks at keys in whatever is returned from
-    # new_data(), make it skip isni_codes so we can handle that
-    # seperately.
-    delete $new->{isni_codes};
-    return $new;
-};
-
-sub isni_changes
-{
-    my ($self, $old, $current, $new) = @_;
-
-    my $old_set     = Set::Scalar->new(@$old);
-    my $current_set = Set::Scalar->new(@$current);
-    my $new_set     = Set::Scalar->new(@$new);
-
-    # An ISNI can be present in or absent from each set.
-    # There are these seven possible cases:
-    #   OCN: never changed
-    #   O-N: removed by previous edit, we shouldn't undo that (*)
-    #   O--: has already been deleted by previous edit
-    #   -CN: has already been added by previous edit
-    #   -C-: has been added by previous edit (*)
-    #   --N: add completely new ISN
-    #   OC-: delete ISNIs that weren't touched by previous edit
-    # (*) marks the cases where the intended result diverges from N.
-
-    my $result_set = ($new_set
-        - ($old_set - $current_set))  # leave out those removed in the meantime
-        + ($current_set - $old_set);  # ... and include those added
-
-    return [ $result_set->members ];
-}
-
-around merge_changes => sub {
-    my $orig = shift;
-    my $self = shift;
-
-    my $merged = $self->$orig (@_);
-
-    my $current_isnis = $self->c->model($self->_edit_model)
-        ->isni->find_by_entity_id($self->entity_id);
-
-    $merged->{isni_codes} = $self->isni_changes (
-        $self->data->{old}->{isni_codes},
-        [ map { $_->isni } @$current_isnis ],
-        $self->data->{new}->{isni_codes})
-        if $self->data->{new}->{isni_codes};
-
-    return $merged;
+with 'MusicBrainz::Server::Edit::Role::ValueSet' => {
+    prop_name => 'isni_codes',
+    get_current => sub {
+        my $self = shift;
+        $self->c->model($self->_edit_model)
+            ->isni->find_by_entity_id($self->entity_id);
+    },
+    extract_value => sub { shift->isni }
 };
 
 no Moose;
