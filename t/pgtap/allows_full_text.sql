497d069f26b4306cee1e2dbc96fbf2171c7b2332
reosarevok <reosarevok@rika.musicbrainz.org>
2013-04-17 21:02:41 +0000

    Extending the test

--- t/pgtap/allows_full_text.sql
+++ t/pgtap/allows_full_text.sql
@@ -9,15 +9,24 @@ INSERT INTO work_attribute_type (id, name, comment, free_text)
   VALUES (1, 'with_text', '', TRUE),
          (2, 'no_text', '', FALSE);
 
--- Ensure that only attribute types that allow text can have text added
+-- Ensure that only attribute types that allow text can have text added and that the CHECK constraint works
 
 SELECT lives_ok(
   'INSERT INTO work_attribute (id, work, work_attribute_type, work_attribute_type_allowed_value, work_attribute_text)
    VALUES (1, 1, 1, NULL, ''This type of attribute allows text'')');
 
+SELECT lives_ok(
+  'INSERT INTO work_attribute (id, work, work_attribute_type, work_attribute_type_allowed_value, work_attribute_text)
+   VALUES (1, 1, 1, 1, NULL)'); -- No text and an allowed_value link
+
+
 SELECT throws_ok(
   'INSERT INTO work_attribute (id, work, work_attribute_type, work_attribute_type_allowed_value, work_attribute_text)
-   VALUES (1, 1, 2, 1, ''This type of attribute does not allow text'')')
+   VALUES (1, 1, 1, 1, ''This type of attribute allows text but allowed_value is not null'')');
+
+SELECT throws_ok(
+  'INSERT INTO work_attribute (id, work, work_attribute_type, work_attribute_type_allowed_value, work_attribute_text)
+   VALUES (1, 1, 2, NULL, ''This type of attribute does not allow text'')');
 
 SELECT finish();
 ROLLBACK;
