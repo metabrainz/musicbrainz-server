4d46fc86722bc3236099a889dfc6097bd99758a9
reosarevok <reosarevok@rika.musicbrainz.org>
2013-05-03 01:02:29 +0000

    Fix some references to country

--- admin/sql/CreateConstraints.sql
+++ admin/sql/CreateConstraints.sql
@@ -34,7 +34,9 @@ ALTER TABLE artist ADD CONSTRAINT artist_va_check
     CHECK (id <> 1 OR
            (type = 3 AND
             gender IS NULL AND
-            country IS NULL AND
+            area IS NULL AND
+            begin_area IS NULL AND
+            end_area IS NULL AND
             begin_date_year IS NULL AND
             begin_date_month IS NULL AND
             begin_date_day IS NULL AND
