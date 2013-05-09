938c46c1699577f4fbef6da95f9a00021b23d718
reosarevok <reosarevok@rika.musicbrainz.org>
2013-05-09 21:27:09 +0000

    Merging warp's change

--- t/lib/t/MusicBrainz/Server/Controller/Release/ReorderCoverArt.pm
+++ t/lib/t/MusicBrainz/Server/Controller/Release/ReorderCoverArt.pm
@@ -15,9 +15,10 @@ test 'Test reordering cover art' => sub {
 
     $c->sql->do(<<'EOSQL');
 
-INSERT INTO edit (id, editor, type, data, status, expire_time) VALUES (222, 10, 316, '', 2, now());
+INSERT INTO edit (id, editor, type, data, status, expire_time) VALUES (1, 1, 316, '', 2, now());
+INSERT INTO cover_art_archive.image_type (mime_type, suffix) VALUES ('image/jpeg', 'jpg');
 INSERT INTO cover_art_archive.cover_art (id, release, mime_type, edit, ordering)
-  VALUES (12346, 1, 'image/jpeg', 1, 1), (12347, 1, 'image/jpeg', 1, 2);
+  VALUES (12345, 1, 'image/jpeg', 1, 1), (12346, 1, 'image/jpeg', 1, 2);
 EOSQL
 
     $mech->get_ok('/login');
@@ -25,6 +26,7 @@ EOSQL
 
     my $new_comment = 'Adding a comment';
     $mech->get_ok('/release/14b9d183-7dab-42ba-94a3-7388a66604b8/reorder-cover-art');
+
     my @edits = capture_edits {
         $mech->submit_form(
             with_fields => {
