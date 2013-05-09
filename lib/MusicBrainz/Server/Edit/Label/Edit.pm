5a8ec5d07d94824c2dbeca2d2018bfcdddaffde0
Ian McEwen <ianmcorvidae@ianmcorvidae.net>
2013-05-09 14:58:44 -0700

    MBS-799/MBS-5920: show area properly in Edit edits when going from undef -> something

--- lib/MusicBrainz/Server/Edit/Label/Edit.pm
+++ lib/MusicBrainz/Server/Edit/Label/Edit.pm
@@ -67,7 +67,7 @@ sub foreign_keys
     my $relations = {};
     changed_relations($self->data, $relations,
         LabelType => 'type_id',
-        Area      => defined($self->data->{old}{area_id}) ? 'area_id' : 'country_id',
+        Area      => exists($self->data->{old}{area_id}) ? 'area_id' : 'country_id',
     );
 
     $relations->{Label} = [ $self->data->{entity}{id} ];
