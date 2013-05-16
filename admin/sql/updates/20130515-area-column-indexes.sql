6e528b50dc5af42301cd9ab73b431398dab0fde7
Ian McEwen <ianmcorvidae@ianmcorvidae.net>
2013-05-16 16:45:37 -0700

    MBS-6306: create indexes concurrently, and drop after replacements are made

--- admin/sql/updates/20130515-area-column-indexes.sql
+++ admin/sql/updates/20130515-area-column-indexes.sql
@@ -1,11 +1,9 @@
-BEGIN;
-    CREATE INDEX artist_idx_area ON artist (area);
-    CREATE INDEX artist_idx_begin_area ON artist (begin_area);
-    CREATE INDEX artist_idx_end_area ON artist (end_area);
+CREATE INDEX CONCURRENTLY artist_idx_area ON artist (area);
+CREATE INDEX CONCURRENTLY artist_idx_begin_area ON artist (begin_area);
+CREATE INDEX CONCURRENTLY artist_idx_end_area ON artist (end_area);
 
-    CREATE INDEX label_idx_area ON label (area);
+CREATE INDEX CONCURRENTLY label_idx_area ON label (area);
 
-    -- rename index, functionally
-    DROP INDEX IF EXISTS release_country_country_idx;
-    CREATE INDEX release_country_idx_country ON release_country (country);
-COMMIT;
+-- rename index, functionally
+CREATE INDEX CONCURRENTLY release_country_idx_country ON release_country (country);
+DROP INDEX IF EXISTS release_country_country_idx;
