5d3c4f0bc954718f01d143c27d406e74f0dd3e1a
Ian McEwen <ianmcorvidae@ianmcorvidae.net>
2013-05-21 01:11:27 -0700

    MBS-6357: ALTER INDEX IF EXISTS is only postgres 9.2

--- admin/sql/updates/20130520-rename-indexes-constraints.sql
+++ admin/sql/updates/20130520-rename-indexes-constraints.sql
@@ -1,8 +1,8 @@
 \set ON_ERROR_STOP 1
 
 BEGIN;
-    ALTER INDEX IF EXISTS medium_2013_pkey RENAME TO medium_pkey;
-    ALTER INDEX IF EXISTS track_2013_pkey RENAME TO track_pkey;
+    ALTER INDEX medium_2013_pkey RENAME TO medium_pkey;
+    ALTER INDEX track_2013_pkey RENAME TO track_pkey;
 
     ALTER TABLE track DROP CONSTRAINT IF EXISTS track2013_edits_pending_check;
     ALTER TABLE track DROP CONSTRAINT IF EXISTS track2013_length_check;
