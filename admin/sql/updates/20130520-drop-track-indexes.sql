2210aef60a23330a34f54e08470a4e15ad547d1f
Ian McEwen <ianmcorvidae@ianmcorvidae.net>
2013-05-25 10:27:34 -0700

    Schema 18: drop track indexes IF EXISTS, add --noupdate-replication-control to upgrade script

--- admin/sql/updates/20130520-drop-track-indexes.sql
+++ admin/sql/updates/20130520-drop-track-indexes.sql
@@ -1,9 +1,9 @@
 \set ON_ERROR_STOP 1
 
 BEGIN;
-    DROP INDEX track_idx_artist_credit;
-    DROP INDEX track_idx_gid;
-    DROP INDEX track_idx_medium;
-    DROP INDEX track_idx_name;
-    DROP INDEX track_idx_recording;
+    DROP INDEX IF EXISTS track_idx_artist_credit;
+    DROP INDEX IF EXISTS track_idx_gid;
+    DROP INDEX IF EXISTS track_idx_medium;
+    DROP INDEX IF EXISTS track_idx_name;
+    DROP INDEX IF EXISTS track_idx_recording;
 COMMIT;
