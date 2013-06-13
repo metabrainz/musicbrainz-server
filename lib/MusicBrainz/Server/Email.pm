8ea80571eca0bece6eb5b36964c931e04b5fc0ec
Ian McEwen <ianmcorvidae@ianmcorvidae.net>
2013-06-13 01:05:33 -0700

    MBS-3663: Use editor preferences, clarify wording slightly.

--- lib/MusicBrainz/Server/Email.pm
+++ lib/MusicBrainz/Server/Email.pm
@@ -214,7 +214,11 @@ sub _create_no_vote_email
     my $url = sprintf 'http://%s/edit/%d', DBDefs->WEB_SERVER_USED_IN_EMAIL, $edit_id;
     my $prefs_url = sprintf 'http://%s/account/preferences', DBDefs->WEB_SERVER_USED_IN_EMAIL;
 
-    my $close_time = DateTime->now()->add_duration($EDIT_MINIMUM_RESPONSE_PERIOD)->add( hours => 1 )->strftime('%F %H:00 UTC');
+    my $close_time = DateTime->now()->add_duration($EDIT_MINIMUM_RESPONSE_PERIOD)->truncate( to => 'hour' )->add( hours => 1 );
+    if ($editor->preferences) {
+        $close_time->set_time_zone($editor->preferences->timezone);
+    }
+    $close_time = $close_time->strftime('%F %H:%M %Z');
 
     my $body = <<EOS;
 '${\ $voter->name }' has voted against your edit #$edit_id.
@@ -234,8 +238,8 @@ You can disable this notification by changing your preferences at
 $prefs_url.
 
 To ensure time for you and other editors to respond, the soonest this edit will
-close is $close_time, 72 hours from the time of this email, unless
-it is approved by an autoeditor.
+be rejected, if applicable, is $close_time, 72 hours from the time of
+this email.
 
 -- The MusicBrainz Team
 EOS
