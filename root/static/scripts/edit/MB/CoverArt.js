e4e96f489844dde0e57dd7e5f0999e251916f494
warp <kuno@frob.nl>
2013-06-14 16:34:34 -0500

    MBS-4376, port ajax uploader to knockout.js (work in progress).

--- root/static/scripts/edit/MB/CoverArt.js
+++ root/static/scripts/edit/MB/CoverArt.js
@@ -1,6 +1,6 @@
 /*
    This file is part of MusicBrainz, the open internet music database.
-   Copyright (C) 2010 MetaBrainz Foundation
+   Copyright (C) 2013 MetaBrainz Foundation
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -126,26 +126,303 @@ MB.CoverArt.reorder_position = function () {
     $('div.editimage script').remove ();
 };
 
-MB.CoverArt.add_cover_art = function (mbid) {
-    $('#add-cover-art-submit').prop('disabled', false);
-
-    $('button.submit').bind ('click.mb', function (event) {
-        event.preventDefault ();
-
-        var mime_type = MB.CoverArt.get_image_mime_type ();
-        $('#id-add-cover-art\\.mime_type').val(mime_type);
-
-        if (mime_type)
+MB.CoverArt.add_files = function (event) {
+    $.each ($('input.add-files')[0].files, function (idx, file) {
+        console.log ("add files", file.type, file.name, filesize (file.size), file);
+/*
+        var $filebox;
+        if (file.type === "image/jpeg")
         {
-            $('iframe')[0].contentWindow.upload (
-                mbid, $('#id-add-cover-art\\.id').val (), mime_type);
+            $filebox = $('.file-box.template').clone ().removeClass ('template');
         }
         else
         {
-            $('iframe').contents ().find ('#cover-art-file-error').show ();
+            $filebox = $('.file-box-error.template').clone ().removeClass ('template');
         }
-
-
-        return false;
+        $filebox.insertBefore ($('#add-files-end'))
+            .show ()
+            .find ('.filename').text (file.name).end ()
+            .find ('.filesize').text ('(' + filesize (file.size) + ')').end ()
+            .find ('.file-down').click (MB.CoverArt.file_down).end ()
+            .find ('.file-up').click (MB.CoverArt.file_up).end ()
+            .data ('file', file);
+*/
     });
+
+    /* Only display the the cover art type help once. */
+//    $('.cover-art-types-help').hide ().eq (1).show ();
+};
+
+console.log ("initializing MB.CoverArt");
+
+
+MB.CoverArt.CoverArtType = function (name, id) {
+    var self = this;
+    self.name = name;
+    self.id = id;
+    self.checked = ko.observable(false);
+}
+
+MB.CoverArt.CoverArtTypes = function () {
+    var self = this;
+    var ret = ko.observableArray ();
+
+    _(MB.cover_art_types_json).each (function (item) {
+        ret.push (new MB.CoverArt.CoverArtType(item.l_name, item.id));
+    });
+
+    return ret;
+};
+
+
+/*
+   For each image the upload process is:
+
+   1. validating   Validate the file the user has selected.
+   2. waiting      Wait for the user to make their selections and submit the edit.
+   3. signing      Request postfields from /ws/js/cover-art-upload/:mbid.
+   4. uploading    Upload image to postfields.action.
+   5. submitting   POST edit to /release/:mbid/add-cover-art.
+   6. done         All actions completed successfully.
+
+   each of these (except waiting) has an accompanying error state.
+*/
+
+MB.CoverArt.upload_status_enum = {
+    'validating':     'validating',
+    'validate_error': 'validate_error',
+    'waiting':        'waiting',
+    'signing':        'signing',
+    'sign_error':     'sign_error',
+    'uploading':      'uploading',
+    'upload_error':   'upload_error',
+    'submitting':     'submitting',
+    'submit_error':   'submit_error',
+    'done':           'done',
+};
+
+/* NOTE: javascript objects do not allow integer keys, these are
+   coerced to strings. */
+MB.CoverArt.image_signatures = {
+    0x38464947: 'image/gif',  /* GIF signature. "GIF8" */
+    0x474E5089: 'image/png',  /* PNG signature, [137 "PNG"] */
+    0xE0FFD8FF: 'image/jpeg', /* JPEG signature. */
+};
+
+MB.CoverArt.validate_file = function (file) {
+    var deferred = $.Deferred ();
+    var reader = new FileReader();
+    reader.addEventListener("loadend", function() {
+        var uint32view = new Uint32Array(reader.result);
+
+        var mime_type = MB.CoverArt.image_signatures[uint32view[0]];
+        if (mime_type)
+        {
+            deferred.resolve (mime_type);
+        }
+        else
+        {
+            deferred.reject ();
+        }
+    });
+    reader.readAsArrayBuffer(file.slice (0, 4));
+
+    return deferred.promise ();
+};
+
+MB.CoverArt.sign_upload = function (file, gid) {
+    var deferred = $.Deferred ();
+
+    var postfields = $.getJSON('/ws/js/cover-art-upload/' + gid);
+    postfields.fail (function (jqxhr, status, error) {
+        deferred.reject ();
+    });
+
+    postfields.done (function (data, status, jqxhr) {
+        deferred.resolve (data);
+    });
+
+    return deferred.promise ();
+};
+
+MB.CoverArt.upload_image = function (postfields, file) {
+    var deferred = $.Deferred ();
+
+    var formdata = new FormData();
+    formdata.append("file", file);
+    $.each (postfields.formdata, function (key, val) {
+        formdata.append (key, val);
+    });
+
+    var xhr = new XMLHttpRequest ();
+    xhr.upload.addEventListener("progress", function (event) {
+        if (event.lengthComputable)
+        {
+            deferred.notify (100 * event.loaded / event.total);
+        }
+    });
+
+    xhr.addEventListener("load", function (event) {
+        if (xhr.status >= 200 && xhr.status < 210)
+        {
+            deferred.notify (100);
+            deferred.resolve ();
+        }
+        else
+        {
+            deferred.reject();
+        }
+    });
+
+    xhr.addEventListener("error", deferred.reject);
+    xhr.addEventListener("abort", deferred.reject);
+    xhr.open ("POST", postfields.action);
+    xhr.send (formdata);
+
+    return deferred.promise ();
+};
+
+MB.CoverArt.submit_edit = function () {
+    var deferred = $.Deferred ();
+
+    console.log ("submit edit not implemented. got nothing");
+    deferred.resolve ();
+    return deferred.promise ();
+};
+
+MB.CoverArt.FileUpload = function(file) {
+    var self = this;
+    var statuses = MB.CoverArt.upload_status_enum;
+
+    self.name = file.name;
+    self.size = filesize (file.size);
+    self.comment = ko.observable ();
+    self.types = MB.CoverArt.CoverArtTypes ();
+    self.data = file;
+    self.image_id = null;
+    self.mime_type = null;
+
+    self.progress = ko.observable (0);
+    self.status = ko.observable ('validating');
+
+    self.startUpload = function (gid, position) {
+        var deferred = $.Deferred ();
+        self.status (statuses.signing);
+
+        var signing = MB.CoverArt.sign_upload (self.data, gid);
+        signing.fail (function () {
+            self.status (statuses.sign_error);
+            deferred.reject ();
+        });
+
+        signing.done (function (postfields) {
+            self.status (statuses.uplading);
+
+            var uploading = MB.CoverArt.upload_image (postfields, self.data);
+            uploading.fail (function () {
+                self.status (statuses.upload_error);
+                deferred.reject ();
+            });
+            uploading.done (function () {
+                self.status (statuses.submitting);
+
+                var submitting = MB.CoverArt.submit_edit ();
+                submitting.fail (function () {
+                    self.status (statuses.submit_error);
+                    deferred.reject ();
+                })
+                submitting.done (function () {
+                    self.status (statuses.done);
+                    deferred.resolve ();
+                });
+            });
+        });
+
+        return deferred.promise ();
+    };
+
+    MB.CoverArt.validate_file (self.data)
+        .fail (function () { self.status (statuses.validate_error) })
+        .then (function (mime_type) {
+            self.mime_type = mime_type;
+            self.status (statuses.waiting)
+        });
+}
+
+MB.CoverArt.UploadProcessViewModel = function () {
+    var self = this;
+    self.files_to_upload = ko.observableArray ();
+
+    self.addFile = function (file) {
+        self.files_to_upload.push (new MB.CoverArt.FileUpload (file));
+        self.files_to_upload()[0].comment ("file was added");
+    }
+};
+
+MB.CoverArt.add_cover_art_submit = function (gid, upvm) {
+    var pos = parseInt ($('#id-add-cover-art\\.position').val (), 10);
+    console.log ("submit some cover arts!!", gid, pos);
+
+    var queue = _(upvm.files_to_upload ()).map (function (item, idx) {
+        return function () {
+            return item.startUpload (gid, pos++);
+        };
+    });
+
+    MB.utility.iteratePromises (queue).then (function () {
+        console.log ("all promises done, yay!");
+    });
+};
+
+MB.CoverArt.add_cover_art = function (gid) {
+    window.upvm = new MB.CoverArt.UploadProcessViewModel ();
+    ko.applyBindings (window.upvm);
+
+    if (typeof (FormData) === "function")
+    {
+        /* FormData is supported, so we can present the multifile ajax
+         * upload form. */
+
+        $('button.add-files').on ('click', function (event) {
+            $('input.add-files').trigger ('click');
+        });
+
+        $('input.add-files').on ('change', function (event) {
+            $.each ($('input.add-files')[0].files, function (idx, file) {
+                upvm.addFile (file);
+            });
+
+            $('#add-cover-art-submit').prop ('disabled', false);
+        });
+
+        $('#add-cover-art-submit').on ('click.mb', function (event) {
+            event.preventDefault ();
+            console.log ("submit!");
+            MB.CoverArt.add_cover_art_submit (gid, window.upvm);
+        });
+    }
+    else
+    {
+        $('#add-cover-art-submit').prop('disabled', false);
+
+        $('#add-cover-art-submit').on ('click.mb', function (event) {
+            event.preventDefault ();
+
+            var mime_type = MB.CoverArt.get_image_mime_type ();
+            $('#id-add-cover-art\\.mime_type').val(mime_type);
+
+            if (mime_type)
+            {
+                $('iframe')[0].contentWindow.upload (
+                    mbid, $('#id-add-cover-art\\.id').val (), mime_type);
+            }
+            else
+            {
+                $('iframe').contents ().find ('#cover-art-file-error').show ();
+            }
+
+            return false;
+        });
+    }
+
 };
