5338f95b4a4602e7ddc7ae36feb00927faac49c2
Oliver Charles <ollie@ocharles.org.uk>
2013-06-27 14:57:25 +0000

    Move MB::S::get_collator to Translation, fix Form::Area test
    The Form::Area test builds a custom testing context so it doesn't have to bring
    up the entire Catalyst stack. However, 83afe0f5 added a new method to the
    Catalyst context, which won't be shared with this new form context. I've
    factored out get_collator to MusicBrainz::Server::Translation so I can at least
    provide this in the test context.

--- lib/MusicBrainz/Server/Translation.pm
+++ lib/MusicBrainz/Server/Translation.pm
@@ -1,16 +1,16 @@
 package MusicBrainz::Server::Translation;
 use MooseX::Singleton;
 
+use Cwd qw (abs_path);
+use DBDefs;
+use DateTime::Locale;
 use Encode;
 use I18N::LangTags ();
 use I18N::LangTags::Detect;
-use DBDefs;
-
+use List::UtilsBy qw( sort_by );
 use Locale::Messages qw( bindtextdomain LC_MESSAGES );
 use Locale::Util qw( web_set_locale );
-use Cwd qw (abs_path);
-use DateTime::Locale;
-use List::UtilsBy qw( sort_by );
+use Unicode::ICU::Collator qw( UCOL_NUMERIC_COLLATION UCOL_ON );
 
 with 'MusicBrainz::Server::Role::Translation' => { domain => 'mb_server' };
 
@@ -60,16 +60,16 @@ sub _bind_domain
     my ($self, $domain) = @_;
     # copied from Locale::TextDomain lines 321-346, in sub __find_domain
     # I changed $try_dirs to @search_dirs, which I set myself based on line 303, in sub import
-    # Otherwise the same. This is so we can use Locale::TextDomain's 
+    # Otherwise the same. This is so we can use Locale::TextDomain's
     # search and textdomain binding code without using its crazy way
     # of determining which domain to use for a given string.
     my @search_dirs = map $_ . '/LocaleData', @INC;
     my $found_dir = '';
-         
+
     TRYDIR: foreach my $dir (map { abs_path $_ } grep { -d $_ } @search_dirs) {
         local *DIR;
         if (opendir DIR, $dir) {
-            my @files = map { "$dir/$_/LC_MESSAGES/$domain.mo" } 
+            my @files = map { "$dir/$_/LC_MESSAGES/$domain.mo" }
                 grep { ! /^\.\.?$/ } readdir DIR;
 
             foreach my $file (@files) {
@@ -83,7 +83,7 @@ sub _bind_domain
             }
         }
     }
-     
+
     bindtextdomain $domain => $found_dir;
     $self->{bound} = 1;
 }
@@ -117,7 +117,7 @@ sub set_language
         @avail_lang = ($lang);
     } elsif (DBDefs->LANGUAGE_FALLBACK_TO_BROWSER) {
         # change e.g. 'en-aq' to 'en_AQ'
-        @avail_lang = map { s/-([a-z]{2})/_\U$1/; $_; } 
+        @avail_lang = map { s/-([a-z]{2})/_\U$1/; $_; }
             grep {
                 my $l = $_;
                 grep { $l eq $_ } DBDefs->MB_LANGUAGES
@@ -139,15 +139,15 @@ sub set_language
     # Change en_AQ back to en-aq to compare with MB_LANGUAGES
     if (grep { $set_lang eq $_ || $set_lang_munge eq $_ } DBDefs->MB_LANGUAGES) {
         return $set_lang;
-    } 
+    }
     # Check if the language without country code is in MB_LANGUAGES
     elsif (grep { $set_lang_nocountry eq $_ } DBDefs->MB_LANGUAGES) {
         return $set_lang_nocountry;
-    } 
+    }
     # Give up, return the full language even though it looks wrong
     else {
         return $set_lang;
-    } 
+    }
 }
 
 sub unset_language
@@ -162,10 +162,10 @@ sub language_from_cookie
     $cookie_munge =~ s/_([A-Z]{2})/-\L$1/;
     my $cookie_nocountry = defined $cookie ? $cookie->value : '';
     $cookie_nocountry =~ s/_[A-Z]{2}//;
-    if (defined $cookie && 
+    if (defined $cookie &&
         grep { $cookie->value eq $_ || $cookie_munge eq $_ } DBDefs->MB_LANGUAGES) {
         return $cookie->value;
-    } elsif (defined $cookie && 
+    } elsif (defined $cookie &&
              grep { $cookie_nocountry eq $_ } DBDefs->MB_LANGUAGES) {
         return $cookie_nocountry;
     } else {
@@ -175,15 +175,15 @@ sub language_from_cookie
 
 sub all_languages
 {
-    my @lang_with_locale = sort_by { ucfirst $_->[1]->native_language } 
-                           map { [ $_ => DateTime::Locale->load($_) ] } 
-                           grep { my $l = $_; 
-                                  grep { $l eq $_ } DateTime::Locale->ids() } 
+    my @lang_with_locale = sort_by { ucfirst $_->[1]->native_language }
+                           map { [ $_ => DateTime::Locale->load($_) ] }
+                           grep { my $l = $_;
+                                  grep { $l eq $_ } DateTime::Locale->ids() }
                            map { s/-([a-z]{2})/_\U$1/; $_; } DBDefs->MB_LANGUAGES;
-    my @lang_without_locale = sort_by { $_->[1]->{id} } 
-                              map { [ $_ => {'id' => $_, 'native_language' => ''} ] } 
-                              grep { my $l = $_; 
-                                     !(grep { $l eq $_ } DateTime::Locale->ids()) } 
+    my @lang_without_locale = sort_by { $_->[1]->{id} }
+                              map { [ $_ => {'id' => $_, 'native_language' => ''} ] }
+                              grep { my $l = $_;
+                                     !(grep { $l eq $_ } DateTime::Locale->ids()) }
                               map { s/-([a-z]{2})/_\U$1/; $_; } DBDefs->MB_LANGUAGES;
     my @languages = (@lang_with_locale, @lang_without_locale);
     return \@languages;
@@ -201,6 +201,15 @@ sub _expand
     $string =~ s/\{($re)\}/defined $args{$1} ? $args{$1} : "{$1}"/ge;
 
     return $string;
+}
+
+sub get_collator
+{
+    my ($language) = @_;
+    my $coll = Unicode::ICU::Collator->new($language);
+    # make sure to update the postgresql collate extension as well
+    $coll->setAttribute(UCOL_NUMERIC_COLLATION(), UCOL_ON());
+    return $coll;
 }
 
 1;
