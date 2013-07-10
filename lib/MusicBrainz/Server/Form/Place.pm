dc6580b3b15da29b0c21061745e37be8b2feb91e
reosarevok <reosarevok@rika.musicbrainz.org>
2013-07-10 23:14:04 +0000

    mbs-6200: add Form::Place

---
+++ lib/MusicBrainz/Server/Form/Place.pm
@@ -0,0 +1,71 @@
+package MusicBrainz::Server::Form::Place;
+use HTML::FormHandler::Moose;
+
+extends 'MusicBrainz::Server::Form';
+with 'MusicBrainz::Server::Form::Role::Edit';
+
+has '+name' => ( default => 'edit-place' );
+
+has_field 'name' => (
+    type => '+MusicBrainz::Server::Form::Field::Text',
+    required => 1,
+);
+
+has_field 'sort_name' => (
+    type => '+MusicBrainz::Server::Form::Field::Text',
+    required => 1,
+);
+
+has_field 'type_id' => (
+    type => 'Select',
+);
+
+has_field 'address' => (
+    type => '+MusicBrainz::Server::Form::Field::Text',
+);
+
+has_field 'comment' => (
+    type => '+MusicBrainz::Server::Form::Field::Comment',
+);
+
+has_field 'period' => (
+    type => '+MusicBrainz::Server::Form::Field::DatePeriod',
+    not_nullable => 1
+);
+
+sub edit_field_names
+{
+    return qw( name sort_name type_id address comment period.begin_date period.end_date period.ended );
+}
+
+sub options_type_id     { shift->_select_all('PlaceType') }
+
+sub dupe_model { shift->ctx->model('Place') }
+
+sub validate {
+    my ($self) = @_;
+
+    return 1;
+}
+
+1;
+
+=head1 COPYRIGHT
+
+Copyright (C) 2013 MetaBrainz Foundation
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+=cut
