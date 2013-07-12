4b46c67e7005b60ba684856b55a81319c3c9b78c
Oliver Charles <ollie@ocharles.org.uk>
2013-07-12 11:53:12 +0000

    MBS-6578: Make area merges possible when areas are country areas
    If an area is a country area, then there is a corresponding row
    for that area in country_area. However, Area->merge does not
    consider these rows when merging, so it is possible to violate
    foreign key constraints on merge.
    
    This commit considers these rows, by making the destination area
    a country_area, if any of the old areas are country_areas. It then
    moves all release_country references to point to the new country_area
    and deletes the old ones.

--- t/lib/t/MusicBrainz/Server/Edit/Area/Merge.pm
+++ t/lib/t/MusicBrainz/Server/Edit/Area/Merge.pm
@@ -35,5 +35,37 @@ EOSQL
     is exception { $edit->accept }, undef;
 };
 
+test 'Can merge country areas' => sub {
+    my $test = shift;
+    my $c = $test->c;
+
+    MusicBrainz::Server::Test->prepare_test_database($c, '+area');
+    $c->sql->do(<<'EOSQL');
+INSERT INTO artist_name (id, name) VALUES (1, 'Artist');
+INSERT INTO artist (id, gid, name, sort_name)
+VALUES (1, '8469c1b7-04a1-4ca7-a090-a5ed2df2e7ac', 1, 1);
+INSERT INTO artist_credit (id, name, artist_count) VALUES (1, 1, 1);
+
+INSERT INTO release_name (id, name) VALUES (1, 'Release');
+
+INSERT INTO release_group (id, gid, name, artist_credit)
+VALUES (1, '14928cab-363c-4457-951e-9b1c3ca404cd', 1, 1);
+
+INSERT INTO release (id, gid, name, artist_credit, release_group)
+VALUES (1, 'a2d13b15-4002-4d04-8a08-b9a9a7fbe9ad', 1, 1, 1);
+
+INSERT INTO release_country (release, country) VALUES (1, 13);
+EOSQL
+
+    my $edit = $c->model('Edit')->create(
+        edit_type => $EDIT_AREA_MERGE,
+        editor_id => 1,
+        old_entities => [ { id => 13, name => 'Australia' } ],
+        new_entity => { id => 81, name => 'Germany' }
+    );
+
+    is exception { $edit->accept }, undef;
+};
+
 1;
 
