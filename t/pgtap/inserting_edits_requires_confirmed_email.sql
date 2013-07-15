5162a57aedac1e3a48b96dbc3c6f8db641baa705
Oliver Charles <ollie@ocharles.org.uk>
2013-07-15 14:08:55 +0000

    Remove trigger DDL from inserting edits pgtap test

--- t/pgtap/inserting_edits_requires_confirmed_email.sql
+++ t/pgtap/inserting_edits_requires_confirmed_email.sql
@@ -3,24 +3,6 @@ SET search_path = 'musicbrainz', 'public';
 BEGIN;
 SELECT no_plan();
 
-CREATE OR REPLACE FUNCTION inserting_edits_requires_confirmed_email_address()
-RETURNS trigger AS $$
-BEGIN
-  IF NOT (
-    SELECT email_confirm_date IS NOT NULL AND email_confirm_date <= now()
-    FROM editor
-    WHERE editor.id = NEW.editor
-  ) THEN
-    RAISE EXCEPTION 'Editor tried to create edit without a confirmed email address';
-  ELSE
-    RETURN NEW;
-  END IF;
-END;
-$$ LANGUAGE 'plpgsql';
-
-CREATE TRIGGER inserting_edits_requires_confirmed_email_address BEFORE INSERT ON edit
-    FOR EACH ROW EXECUTE PROCEDURE inserting_edits_requires_confirmed_email_address();
-
 INSERT INTO editor (id, name, password, ha1)
 VALUES (1, 'New editor', '{CLEARTEXT}mb', '');
 
