10189a432be1736679bd162157cd5e48952b3eb1
Oliver Charles <ollie@ocharles.org.uk>
2013-07-26 13:15:57 +0000

    Fix artist.end_area tests to not use VA and have valid syntax

--- t/pgtap/end_area_implies_ended.sql
+++ t/pgtap/end_area_implies_ended.sql
@@ -7,24 +7,24 @@ SELECT no_plan();
 -- Test setup. See below for tests.
 INSERT INTO artist_name (id, name) VALUES (1, 'blah');
 INSERT INTO artist (id, gid, name, sort_name)
-VALUES (1, 'a4f27229-557b-4b32-be83-06a309903314', 1, 1);
+VALUES (10, 'a4f27229-557b-4b32-be83-06a309903314', 1, 1);
 
 INSERT INTO area (id, gid, name, sort_name)
 VALUES (1, '8939baa6-d3ce-4355-8a6c-bb8345e44d45', 'A', 'A');
 
-SELECT is( 'SELECT ended FROM artist WHERE id = 1', false);
+SELECT is( (SELECT ended FROM artist WHERE id = 10), false);
 
-UPDATE artist SET end_area = 1 WHERE id = 1;
+UPDATE artist SET end_area = 1 WHERE id = 10;
 
-SELECT is( 'SELECT ended FROM artist WHERE id = 1', true);
+SELECT is( (SELECT ended FROM artist WHERE id = 10), true);
 
-UPDATE artist SET end_area = null WHERE id = 1;
+UPDATE artist SET end_area = null WHERE id = 10;
 
-SELECT is( 'SELECT ended FROM artist WHERE id = 1', true);
+SELECT is( (SELECT ended FROM artist WHERE id = 10), true);
 
-UPDATE artist SET ended = null WHERE id = 1;
+UPDATE artist SET ended = false WHERE id = 10;
 
-SELECT is( 'SELECT ended FROM artist WHERE id = 1', false);
+SELECT is( (SELECT ended FROM artist WHERE id = 10), false);
 
 SELECT finish();
 ROLLBACK;
