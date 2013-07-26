07ea2ca763afba156fb5498f73f6bc2e40e5ad90
Oliver Charles <ollie@ocharles.org.uk>
2013-07-26 11:53:33 +0000

    Fix Edit tests

--- t/sql/cdtoc.sql
+++ t/sql/cdtoc.sql
@@ -1,9 +1,3 @@
-INSERT INTO editor (id, name, password, privs, email, website, bio, member_since,
-        email_confirm_date, last_login_date, edits_accepted, edits_rejected,
-        auto_edits_accepted, edits_failed, ha1)
-    VALUES (4, 'ModBot', '{CLEARTEXT}mb', 0, 'support@musicbrainz.org', 'http://musicbrainz.org/doc/ModBot',
-         'See the above link for more information.', NULL, now(), NULL, 2, 1, 99951, 3560, '9bcacf185adc9268d460694f78615c33');
-
 INSERT INTO artist_name (id, name) VALUES (1, 'Kate Bush');
 INSERT INTO artist (id, gid, name, sort_name)
     VALUES (1, '5f9913b0-7219-11de-8a39-0800200c9a66', 1, 1);
