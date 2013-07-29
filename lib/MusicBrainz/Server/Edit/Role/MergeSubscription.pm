9359dc563511d0cb72dc067031096878f1693833
Oliver Charles <ollie@ocharles.org.uk>
2013-07-29 11:29:30 +0000

    MBS-6628: Correctly accept merge label edits with subscriptions
    The Edit::Role::MergeSubscription role was incorrectly generalised, and
    still had 'artist' hard-coded. This works fine for artist merges, but
    fails for label merges as the deleted subscriptions have the 'label' key,
    rather than 'artist'. This commit changes this code to call model_to_type
    to identify the key name.

--- lib/MusicBrainz/Server/Edit/Role/MergeSubscription.pm
+++ lib/MusicBrainz/Server/Edit/Role/MergeSubscription.pm
@@ -2,13 +2,15 @@ package MusicBrainz::Server::Edit::Role::MergeSubscription;
 use Moose::Role;
 use namespace::autoclean;
 
+use MusicBrainz::Server::Data::Utils qw( model_to_type );
+
 requires 'subscription_model', '_entity_ids', 'do_merge', '_merge_model';
 
 around do_merge => sub {
     my ($orig, $self, @args) = @_;
 
     my $editors = $self->subscription_model->delete($self->_old_ids);
-    my $artists = $self->c->model($self->_merge_model)->get_by_ids($self->_old_ids);
+    my $entities = $self->c->model($self->_merge_model)->get_by_ids($self->_old_ids);
 
     $self->$orig(@args);
 
@@ -16,7 +18,7 @@ around do_merge => sub {
         $self->id,
         map +{
             editor => $_->{editor},
-            gid => $artists->{ $_->{artist} }->gid
+            gid => $entities->{ $_->{ model_to_type($self->_merge_model) } }->gid
         }, @$editors
     );
 };
