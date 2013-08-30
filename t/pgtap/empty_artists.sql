bb9c0cd5ce20f87f68979a0cb0f500b18235c535
Oliver Charles <ollie@ocharles.org.uk>
2013-08-30 16:07:33 +0000

    Fix PgTAP tests for empty_* checks

--- t/pgtap/empty_artists.sql
+++ t/pgtap/empty_artists.sql
@@ -21,8 +21,8 @@ INSERT INTO track_name (id, name) VALUES (1, 'Test');
 INSERT INTO work_name (id, name) VALUES (1, 'Test');
 
 INSERT INTO artist (id, gid, name, sort_name, last_updated, edits_pending, comment)
-  VALUES (1, '159cb1fa-dbe9-4777-abf6-7ecb3ce84f91', 1, 1, now(), 0, 'Artist 1'),
-         (2, 'fbbf7950-eebe-49e5-86d6-058ecc2bf4ac', 1, 1, now(), 10, 'Artist 2');
+  VALUES (3, '159cb1fa-dbe9-4777-abf6-7ecb3ce84f91', 1, 1, now(), 0, 'Artist 1'),
+         (4, 'fbbf7950-eebe-49e5-86d6-058ecc2bf4ac', 1, 1, now(), 10, 'Artist 2');
 INSERT INTO artist_credit (id, name, artist_count) VALUES (1, 1, 1);
 
 INSERT INTO label (id, gid, name, sort_name)
@@ -44,79 +44,79 @@ ALTER TABLE artist DISABLE TRIGGER USER;
 --------------------------------------------------------------------------------
 -- Newly created artists are not in empty_artists()
 SELECT set_eq(
-  'SELECT id FROM empty_artists()', '{}'::INT[]
+  'SELECT * FROM empty_artists()', '{}'::INT[]
 );
 
 --------------------------------------------------------------------------------
 -- Artists > 1 day old are eligible for empty_artists().
-UPDATE artist SET last_updated = '1970-01-01' WHERE id = 1;
+UPDATE artist SET last_updated = '1970-01-01' WHERE id = 3;
 
 SELECT set_eq(
-  'SELECT id FROM empty_artists()',
-  ARRAY[ 1 ]
+  'SELECT * FROM empty_artists()',
+  ARRAY[ 3 ]
 );
 
 --------------------------------------------------------------------------------
 -- Artists with edits pending are not eligible for empty_artists()
-UPDATE artist SET edits_pending = edits_pending + 1 WHERE id = 1;
+UPDATE artist SET edits_pending = edits_pending + 1 WHERE id = 3;
 SELECT set_eq(
-  'SELECT id FROM empty_artists()', '{}'::INT[]
+  'SELECT * FROM empty_artists()', '{}'::INT[]
 );
 UPDATE artist SET edits_pending = 0;
 
 --------------------------------------------------------------------------------
 -- l_artist_artist entries exclude artists from empty_artists()
-INSERT INTO l_artist_artist (id, entity0, entity1, link) VALUES (1, 1, 1, 1);
+INSERT INTO l_artist_artist (id, entity0, entity1, link) VALUES (1, 3, 3, 1);
 SELECT set_eq(
-  'SELECT id FROM empty_artists()', '{}'::INT[]
+  'SELECT * FROM empty_artists()', '{}'::INT[]
 );
 DELETE FROM l_artist_artist;
 
 --------------------------------------------------------------------------------
 -- l_artist_label entries exclude artists from empty_artists()
-INSERT INTO l_artist_label (id, entity0, entity1, link) VALUES (1, 1, 1, 1);
+INSERT INTO l_artist_label (id, entity0, entity1, link) VALUES (1, 3, 1, 1);
 SELECT set_eq(
-  'SELECT id FROM empty_artists()', '{}'::INT[]
+  'SELECT * FROM empty_artists()', '{}'::INT[]
 );
 DELETE FROM l_artist_label;
 
 --------------------------------------------------------------------------------
 -- l_artist_recording entries exclude artists from empty_artists()
-INSERT INTO l_artist_recording (id, entity0, entity1, link) VALUES (1, 1, 1, 1);
+INSERT INTO l_artist_recording (id, entity0, entity1, link) VALUES (1, 3, 1, 1);
 SELECT set_eq(
-  'SELECT id FROM empty_artists()', '{}'::INT[]
+  'SELECT * FROM empty_artists()', '{}'::INT[]
 );
 DELETE FROM l_artist_recording;
 
 --------------------------------------------------------------------------------
 -- l_artist_recording entries exclude artists from empty_artists()
-INSERT INTO l_artist_release (id, entity0, entity1, link) VALUES (1, 1, 1, 1);
+INSERT INTO l_artist_release (id, entity0, entity1, link) VALUES (1, 3, 1, 1);
 SELECT set_eq(
-  'SELECT id FROM empty_artists()', '{}'::INT[]
+  'SELECT * FROM empty_artists()', '{}'::INT[]
 );
 DELETE FROM l_artist_release;
 
 --------------------------------------------------------------------------------
 -- l_artist_release_group entries exclude artists from empty_artists()
-INSERT INTO l_artist_release_group (id, entity0, entity1, link) VALUES (1, 1, 1, 1);
+INSERT INTO l_artist_release_group (id, entity0, entity1, link) VALUES (1, 3, 1, 1);
 SELECT set_eq(
-  'SELECT id FROM empty_artists()', '{}'::INT[]
+  'SELECT * FROM empty_artists()', '{}'::INT[]
 );
 DELETE FROM l_artist_release_group;
 
 --------------------------------------------------------------------------------
 -- l_artist_url entries exclude artists from empty_artists()
-INSERT INTO l_artist_url (id, entity0, entity1, link) VALUES (1, 1, 1, 1);
+INSERT INTO l_artist_url (id, entity0, entity1, link) VALUES (1, 3, 1, 1);
 SELECT set_eq(
-  'SELECT id FROM empty_artists()', '{}'::INT[]
+  'SELECT * FROM empty_artists()', '{}'::INT[]
 );
 DELETE FROM l_artist_url;
 
 --------------------------------------------------------------------------------
 -- l_artist_work entries exclude artists from empty_artists()
-INSERT INTO l_artist_work (id, entity0, entity1, link) VALUES (1, 1, 1, 1);
+INSERT INTO l_artist_work (id, entity0, entity1, link) VALUES (1, 3, 1, 1);
 SELECT set_eq(
-  'SELECT id FROM empty_artists()', '{}'::INT[]
+  'SELECT * FROM empty_artists()', '{}'::INT[]
 );
 DELETE FROM l_artist_work;
 
@@ -124,47 +124,47 @@ DELETE FROM l_artist_work;
 -- A artist with recordings is excluded from empty_artists()
 INSERT INTO artist_credit (id, artist_count, name) VALUES (2, 1, 1);
 INSERT INTO artist_credit_name (artist_credit, artist, name, join_phrase, position)
-  VALUES (2, 1, 1, '', 1);
+  VALUES (2, 3, 1, '', 1);
 
 UPDATE recording SET artist_credit = 2;
 SELECT set_eq(
-  'SELECT id FROM empty_artists()', '{}'::INT[]
+  'SELECT * FROM empty_artists()', '{}'::INT[]
 );
 DELETE FROM recording;
 
 -- A artist with releases is excluded from empty_artists()
 INSERT INTO artist_credit (id, artist_count, name) VALUES (2, 1, 1);
 INSERT INTO artist_credit_name (artist_credit, artist, name, join_phrase, position)
-  VALUES (2, 1, 1, '', 1);
+  VALUES (2, 3, 1, '', 1);
 
 UPDATE release SET artist_credit = 2;
 SELECT set_eq(
-  'SELECT id FROM empty_artists()', '{}'::INT[]
+  'SELECT * FROM empty_artists()', '{}'::INT[]
 );
 DELETE FROM release;
 
 -- A artist with release groups is excluded from empty_artists()
 INSERT INTO artist_credit (id, artist_count, name) VALUES (2, 1, 1);
 INSERT INTO artist_credit_name (artist_credit, artist, name, join_phrase, position)
-  VALUES (2, 1, 1, '', 1);
+  VALUES (2, 3, 1, '', 1);
 
 UPDATE release_group SET artist_credit = 2;
 SELECT set_eq(
-  'SELECT id FROM empty_artists()', '{}'::INT[]
+  'SELECT * FROM empty_artists()', '{}'::INT[]
 );
 DELETE FROM release_group;
 
 --------------------------------------------------------------------------------
 -- A artist with open edits linked to it is excluded from empty_artists()
-INSERT INTO edit_artist (edit, artist) VALUES (1, 1);
+INSERT INTO edit_artist (edit, artist) VALUES (1, 3);
 SELECT set_eq(
-  'SELECT id FROM empty_artists()', '{}'::INT[]
+  'SELECT * FROM empty_artists()', '{}'::INT[]
 );
 
 -- But edits that aren't open don't block empty_artists()
 UPDATE edit SET status = 2;
 SELECT set_eq(
-  'SELECT id FROM empty_artists()', '{1}'::INT[]
+  'SELECT * FROM empty_artists()', '{3}'::INT[]
 );
 
 SELECT finish();
