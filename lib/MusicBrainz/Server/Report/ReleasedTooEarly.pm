0c5c83201ab1ff64b14578b858e55c4f11e0802a
Ian McEwen <ianmcorvidae@ianmcorvidae.net>
2013-09-07 05:00:03 -0700

    MBS-6068: fix up reports for lack of _name tables

--- lib/MusicBrainz/Server/Report/ReleasedTooEarly.pm
+++ lib/MusicBrainz/Server/Report/ReleasedTooEarly.pm
@@ -8,7 +8,7 @@ sub query {
     "
         SELECT DISTINCT ON (r.id)
             r.id AS release_id,
-            row_number() OVER (ORDER BY musicbrainz_collate(an.name), musicbrainz_collate(rn.name))
+            row_number() OVER (ORDER BY musicbrainz_collate(ac.name), musicbrainz_collate(r.name))
         FROM
           ( SELECT r.*
             FROM release r
@@ -28,8 +28,6 @@ sub query {
               (mf.year IS NOT NULL AND date_year < mf.year)
           ) r
         JOIN artist_credit ac ON r.artist_credit = ac.id
-        JOIN artist_name an ON ac.name = an.id
-        JOIN release_name rn ON r.name = rn.id
     ";
 }
 
