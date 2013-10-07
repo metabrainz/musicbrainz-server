ec9eb7a58c669939c80f9f46416d8d85245ff8a2
Ian McEwen <ianmcorvidae@ianmcorvidae.net>
2013-10-07 14:49:29 -0700

    ERROR:  subquery in FROM must have an alias

--- admin/sql/CreateViews.sql
+++ admin/sql/CreateViews.sql
@@ -11,7 +11,7 @@ CREATE OR REPLACE VIEW release_event AS
         UNION ALL
         SELECT release, date_year, date_month, date_day, NULL
         FROM release_unknown_country
-    );
+    ) as q;
 
 COMMIT;
 
