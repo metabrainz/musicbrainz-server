8dcb4a903b1a1a697977a082ded1308f78360c6b
Ian McEwen <ianmcorvidae@ianmcorvidae.net>
2013-10-07 16:49:36 -0700

    fix typo

--- upgrade.sh
+++ upgrade.sh
@@ -100,7 +100,7 @@ then
     OUTPUT=`./admin/psql READWRITE < ./admin/sql/updates/20130830-name-table-fks.sql 2>&1` || ( echo "$OUTPUT" ; exit 1 )
 
     echo `date` : Adding non-whitespace constraint to area comments
-    OUTPUT=`./admin/psql READWRITE < .admin/sql/updates/20130919-area-comments-constraints.sql 2>&1` || ( echo "$OUTPUT" ; exit 1 )
+    OUTPUT=`./admin/psql READWRITE < ./admin/sql/updates/20130919-area-comments-constraints.sql 2>&1` || ( echo "$OUTPUT" ; exit 1 )
 
     echo `date` : 'Adding link_type.is_deprecated triggers'
     OUTPUT=`./admin/psql READWRITE < ./admin/sql/updates/20130910-deprecated-link-types-triggers.sql 2>&1` || ( echo "$OUTPUT" ; exit 1 )
