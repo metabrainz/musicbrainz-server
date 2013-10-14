f4722a8e2b84fdff33f886fa38466fff5b0612a8
Ian McEwen <ianmcorvidae@ianmcorvidae.net>
2013-10-14 13:43:47 -0700

    alias-ended: split into two scripts

--- admin/sql/updates/20130704-ended.sql
+++ admin/sql/updates/20130704-ended.sql
@@ -10,22 +10,6 @@ ALTER TABLE artist_alias ADD COLUMN ended BOOLEAN NOT NULL DEFAULT FALSE;
 ALTER TABLE label_alias ADD COLUMN ended BOOLEAN NOT NULL DEFAULT FALSE;
 ALTER TABLE work_alias ADD COLUMN ended BOOLEAN NOT NULL DEFAULT FALSE;
 
-------------------------
--- ADD ENDED TRIGGERS --
-------------------------
-
-CREATE TRIGGER end_date_implies_ended BEFORE UPDATE OR INSERT ON area_alias
-FOR EACH ROW EXECUTE PROCEDURE end_date_implies_ended();
-
-CREATE TRIGGER end_date_implies_ended BEFORE UPDATE OR INSERT ON artist_alias
-FOR EACH ROW EXECUTE PROCEDURE end_date_implies_ended();
-
-CREATE TRIGGER end_date_implies_ended BEFORE UPDATE OR INSERT ON label_alias
-FOR EACH ROW EXECUTE PROCEDURE end_date_implies_ended();
-
-CREATE TRIGGER end_date_implies_ended BEFORE UPDATE OR INSERT ON work_alias
-FOR EACH ROW EXECUTE PROCEDURE end_date_implies_ended();
-
 -----------------------
 -- TRUE IF END DATES --
 -----------------------
