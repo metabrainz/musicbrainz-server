7414517d321129594a42c08a7f1ddb5bb15a0337
Oliver Charles <ollie@ocharles.org.uk>
2013-10-21 10:42:51 +0000

    MBS-3820: Factor out EditNote stuff to Role::EditNote; use in release editor
    The parent commit introduced validation on the 'edit_note' field,
    allowing it to be configurable whether or not it is required. The
    release editor also has conditional validation on the edit note field,
    but defines that itself. This commit unifies the two by creating
    Form::Role::EditNote, and having both Form::Role::Edit and
    Form::ReleaseEditor::EditNote compose this role.

--- lib/MusicBrainz/Server/Form/Role/Edit.pm
+++ lib/MusicBrainz/Server/Form/Role/Edit.pm
@@ -2,25 +2,9 @@ package MusicBrainz::Server::Form::Role::Edit;
 use HTML::FormHandler::Moose::Role;
 use MusicBrainz::Server::Translation qw( l );
 
+with 'MusicBrainz::Server::Form::Role::EditNote';
+
 requires 'edit_field_names';
-
-has 'requires_edit_note' => ( is => 'ro', default => 0 );
-
-has_field 'edit_note' => (
-    type => 'TextArea',
-    label => 'Edit note:',
-    localize_meth => sub { my ($self, @message) = @_; return l(@message); }
-);
-
-has_field 'as_auto_editor' => (
-    type => 'Checkbox',
-);
-
-sub default_as_auto_editor
-{
-    my $self = shift;
-    return $self->ctx->user->is_auto_editor;
-};
 
 sub edit_fields
 {
@@ -30,14 +14,6 @@ sub edit_fields
     } map {
         $self->field($_)
     } $self->edit_field_names;
-}
-
-sub validate {
-    my $self = shift;
-
-    if ($self->requires_edit_note && (!defined $self->field('edit_note')->value || $self->field('edit_note')->value eq '')) {
-        $self->field('edit_note')->add_error( l("You must provide an edit note"));
-    }
 }
 
 1;
