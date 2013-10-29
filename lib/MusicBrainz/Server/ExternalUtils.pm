69f48a5792a4f4704c58983019a6a6a59bc74e3f
Ian McEwen <ianmcorvidae@ianmcorvidae.net>
2013-10-29 12:01:06 -0700

    MBS-6066: use ocharles' chunked transfer encoding retry code for the search server as well

---
+++ lib/MusicBrainz/Server/ExternalUtils.pm
@@ -0,0 +1,27 @@
+package MusicBrainz::Server::ExternalUtils;
+
+use base 'Exporter';
+
+our @EXPORT_OK = qw(
+    get_chunked_with_retry
+);
+
+sub get_chunked_with_retry {
+    my ($ua, $url) = @_;
+    my $response;
+    my $retries_remaining = int(25.0 / $ua->timeout);
+    while (!defined($response) && --$retries_remaining > 0) {
+        $response = $ua->get($url);
+
+	# When using chunked transfer encoding, occasionally, a chunk gets
+	# delayed, and the LWP timeout fires causing the response to only be
+	# partially completed. In this case, the X-Died header is set.  If this
+	# happens, we retry the request.
+        my $x_died = $response->headers->header("X-Died");
+        $response = undef if ($x_died && $x_died =~ /read timeout/);
+    }
+    return $response;
+}
+
+1;
+
