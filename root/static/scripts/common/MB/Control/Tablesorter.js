metabrainz/musicbrainz-server

commit 5455da396170824bb49cbeb10655fb5a11dc45f3

Remove document.ready handlers or replace with IIFEs where needed

root/statistics/countries.tt
@@ -39,10 +39,8 @@
 [%- PROCESS "statistics/macros-footer.tt" -%]
 
 <script type="text/javascript">//<![CDATA[
-  $(function () {
-    MB.Control.Tablesorter.options = {widgets: ['indexFirstColumn', 'evenRowClasses'], headers : {0: {sorter: false}, 2: { sorter: 'fancyNumber' } , 3: { sorter: 'fancyNumber' }, 4: { sorter: 'fancyNumber' }, 5: { sorter: 'fancyNumber' }}, sortList: [[5,1]] }
-    MB.Control.Tablesorter();
-  });
+  MB.Control.Tablesorter.options = {widgets: ['indexFirstColumn', 'evenRowClasses'], headers : {0: {sorter: false}, 2: { sorter: 'fancyNumber' } , 3: { sorter: 'fancyNumber' }, 4: { sorter: 'fancyNumber' }, 5: { sorter: 'fancyNumber' }}, sortList: [[5,1]] }
+  MB.Control.Tablesorter();
 //]]></script>
 
 [% END %]

root/statistics/languages_scripts.tt
@@ -65,10 +65,8 @@
 [%- PROCESS "statistics/macros-footer.tt" -%]
 
 <script type="text/javascript">//<![CDATA[
-  $(function () {
-    MB.Control.Tablesorter.options = {widgets: ['indexFirstColumn', 'evenRowClasses'], headers : {0: {sorter: false}, 2: { sorter: 'fancyNumber' } , 3: { sorter: 'fancyNumber' }, 4: { sorter: 'fancyNumber' }}, sortList: [[4,2,3,1]] }
-    MB.Control.Tablesorter();
-  });
+  MB.Control.Tablesorter.options = {widgets: ['indexFirstColumn', 'evenRowClasses'], headers : {0: {sorter: false}, 2: { sorter: 'fancyNumber' } , 3: { sorter: 'fancyNumber' }, 4: { sorter: 'fancyNumber' }}, sortList: [[4,2,3,1]] }
+  MB.Control.Tablesorter();
 //]]></script>
 
 [% END %]

root/account/edit.tt
@@ -96,9 +96,7 @@
     </form>
 
     <script type="text/javascript">//<![CDATA[
-      $(function () {
-        MB.Control.AccountEdit();
-      });
+      MB.Control.EntityAutocomplete({ inputs: $("span.area.autocomplete") });
     //]]></script>
 
 [% END %]

root/account/register_application.tt
@@ -15,25 +15,17 @@
     </form>
 
     <script type="text/javascript">//<![CDATA[
-      $(function () {
+      (function () {
+        var $oauthType = $("#id-application\\.oauth_type");
+        var $oauthRedirectURI = $("#id-application\\.oauth_redirect_uri");
 
-        function toggleRedirectUri(type) {
-            if (type.val() == 'web') {
-              $('#id-application\\.oauth_redirect_uri').parent('.row').show();
-              $('#id-application\\.oauth_redirect_uri').prop('disabled', false);
-            }
-            else {
-              $('#id-application\\.oauth_redirect_uri').parent('.row').hide();
-              $('#id-application\\.oauth_redirect_uri').prop('disabled', true);
-            }
-        }
-
-        $('#id-application\\.oauth_type').click(function () {
-            toggleRedirectUri($(this));
+        $oauthType.change(function () {
+          $oauthRedirectURI.prop("disabled", this.value !== "web");
+          $oauthRedirectURI.parent().toggle(this.value === "web");
         });
 
-        toggleRedirectUri($('#id-application\\.oauth_type'));
-      });
+        $oauthType.change();
+      }());
     //]]></script>
 
 [% END %]

root/alias/edit_form.tt
@@ -46,8 +46,8 @@
   </div>
 </form>
 
-<script>
-$(function () {
+<script>//<![CDATA[
+(function () {
     function togglePrimaryForLocale() {
         var allowed = $('#id-edit-alias\\\.locale').val() != '';
         $('#allow_primary_for_locale').toggle(allowed);
@@ -74,5 +74,5 @@ $(function () {
     MB.Control.initialize_guess_case(bubbles, '[% entity_type_name %]', 'id-edit-alias');
 
     MB.utility.setDefaultAction ('form.edit-alias', 'button.submit.positive');
-});
-</script>
+}());
+//]]></script>

root/annotation/history.tt
@@ -41,7 +41,27 @@
 [% END %]
 
 <script type="text/javascript">//<![CDATA[
-  $(function () {
-    MB.Control.AnnotationHistory('#annotation-history');
-  });
+  (function () {
+    var $table = $("#annotation-history");
+
+    function reset() {
+        var seenOld = false, seenNew = false;
+
+        $table.find("tr").each(function (tr) {
+            var $tr = $(this),
+                $old = $tr.find("input.old"),
+                $new = $tr.find("input.new");
+
+            seenOld = seenOld || !!$old.prop("checked");
+
+            $old.toggle(seenNew);
+            $new.toggle(!seenOld);
+
+            seenNew = seenNew || !!$new.prop("checked");
+        });
+    }
+
+    reset();
+    $table.find("input.old, input.new").change(reset);
+  }());
 //]]></script>

root/area/edit_form.tt
@@ -39,10 +39,10 @@
 </form>
 
 <script type="text/javascript">//<![CDATA[
-  $(function () {
+  (function () {
     var bubbles = MB.Control.BubbleCollection();
     MB.Control.initialize_guess_case(bubbles, "area", "id-edit-area");
 
     MB.utility.setDefaultAction("form.edit-area", "button.submit.positive");
-  });
+  }());
 //]]></script>

root/area/edits.tt
@@ -6,16 +6,4 @@
         </p>
     </div>
     [% INCLUDE 'edit/list.tt' %]
-
-    <script type="text/javascript">//<![CDATA[
-      $(function () {
-        $('.edit-list').each(function () {
-            MB.Control.EditSummary(this);
-        });
-        [% IF c.user.is_auto_editor %]
-          MB.Control.EditList('#edits');
-        [% END %]
-      });
-    //]]></script>
-
 [% END %]

commit 76deb4fbacaed1a9078f1442249b33d1325f4f9d

MBS-7139: Fix table sorter for statistics pages
The countries and languages tables on the statistics pages use the
jQuery tablesorter plugin so that the user can choose by which columns
the list should be sorted. However, this was broken in commit
5455da396170824bb49cbeb10655fb5a11dc45f3 that removed several
unnecessary document-ready handlers; because Tablesorter.js continued
to use such a handler, it ran after other code that depended on it and
failed.

This is fixed by removing the document-ready handler also in
Tablesorter.js. Additionally, MB.Control.Tablesorter is changed to
accept parameters such that it can be applied differently to separate
tables that share one page.

The commit is best viewed ignoring whitespace changes.

root/static/scripts/common/MB/Control/Tablesorter.js
@@ -1,39 +1,36 @@
-$(document).ready(function() {
-    $.tablesorter.addWidget({
-        id: "indexFirstColumn",
-        format: function(table) {                               
-            $('tbody tr', table).each(function (index) {
-                $(this).find('td:first').html((index + 1));
-            });
-        }
-    });
-
-    $.tablesorter.addWidget({
-        id: "evenRowClasses",
-        format: function(table) {
-            $('tbody tr', table).each(function (index) {
-                if ((index + 1) % 2 == 0) {
-                    $(this).addClass("ev");
-                } else {
-                    $(this).removeClass("ev");
-                }
-            });
-        }
-    });
-
-    $.tablesorter.addParser({
-      id: "fancyNumber",
-      is: function(s) {
-          return /^[0-9]?[0-9,\.]*$/.test(s);
-      },
-      format: function(s) {
-          return $.tablesorter.formatFloat( s.replace(/,/g,'') );
-      },
-      type: "numeric"
-    });
-
-    MB.Control.Tablesorter = function () {
-        $('table.tbl').tablesorter(MB.Control.Tablesorter.options);
+$.tablesorter.addWidget({
+    id: "indexFirstColumn",
+    format: function(table) {
+        $('tbody tr', table).each(function (index) {
+            $(this).find('td:first').html((index + 1));
+        });
     }
-    MB.Control.Tablesorter.options = {};
 });
+
+$.tablesorter.addWidget({
+    id: "evenRowClasses",
+    format: function(table) {
+        $('tbody tr', table).each(function (index) {
+            if ((index + 1) % 2 == 0) {
+                $(this).addClass("ev");
+            } else {
+                $(this).removeClass("ev");
+            }
+        });
+    }
+});
+
+$.tablesorter.addParser({
+    id: "fancyNumber",
+    is: function(s) {
+        return /^[0-9]?[0-9,\.]*$/.test(s);
+    },
+    format: function(s) {
+        return $.tablesorter.formatFloat( s.replace(/,/g,'') );
+    },
+    type: "numeric"
+});
+
+MB.Control.Tablesorter = function ($table, options) {
+    $table.tablesorter(options);
+}

root/statistics/countries.tt
@@ -39,8 +39,11 @@
 [%- PROCESS "statistics/macros-footer.tt" -%]
 
 <script type="text/javascript">//<![CDATA[
-  MB.Control.Tablesorter.options = {widgets: ['indexFirstColumn', 'evenRowClasses'], headers : {0: {sorter: false}, 2: { sorter: 'fancyNumber' } , 3: { sorter: 'fancyNumber' }, 4: { sorter: 'fancyNumber' }, 5: { sorter: 'fancyNumber' }}, sortList: [[5,1]] }
-  MB.Control.Tablesorter();
+  MB.Control.Tablesorter($('table.tbl'), {
+      widgets: [ 'indexFirstColumn', 'evenRowClasses' ],
+      headers: { 0: {sorter: false}, 2: { sorter: 'fancyNumber' }, 3: { sorter: 'fancyNumber' }, 4: { sorter: 'fancyNumber' }, 5: { sorter: 'fancyNumber' } },
+      sortList: [ [5,1] ]
+     });
 //]]></script>
 
 [% END %]

root/statistics/languages_scripts.tt
@@ -25,7 +25,7 @@
 
 <div style="float:left; display:inline-block; margin-right: 50px;">
   <h2 style="margin-top:0;">[%- l('Languages') -%]</h2>
-  <table class="tbl">
+  <table class="tbl" id="languages-table">
     <thead>
       <tr>
         <th class="pos">[% l('Rank') %]</th>
@@ -65,8 +65,11 @@
 [%- PROCESS "statistics/macros-footer.tt" -%]
 
 <script type="text/javascript">//<![CDATA[
-  MB.Control.Tablesorter.options = {widgets: ['indexFirstColumn', 'evenRowClasses'], headers : {0: {sorter: false}, 2: { sorter: 'fancyNumber' } , 3: { sorter: 'fancyNumber' }, 4: { sorter: 'fancyNumber' }}, sortList: [[4,2,3,1]] }
-  MB.Control.Tablesorter();
+  MB.Control.Tablesorter($('#languages-table'), {
+      widgets: [ 'indexFirstColumn', 'evenRowClasses' ],
+      headers: { 0: {sorter: false}, 2: { sorter: 'fancyNumber' }, 3: { sorter: 'fancyNumber' }, 4: { sorter: 'fancyNumber' } },
+      sortList: [ [4,2,3,1] ]
+    });
 //]]></script>
 
 [% END %]