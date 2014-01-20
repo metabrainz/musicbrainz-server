5e19f40654f7f27c7ca66a554d396838d42d63eb
Michael Wiencek <mwtuea@gmail.com>
2014-01-20 03:17:30 -0600

    Add MB.utility.request for rate-limited requests

--- root/static/scripts/common/MB/utility.js
+++ root/static/scripts/common/MB/utility.js
@@ -246,3 +246,58 @@ MB.utility.filesize = function (size) {
 MB.utility.percentOf = function(x, y) {
     return x * y / 100;
 };
+
+MB.utility.request = (function () {
+    var nextAvailableTime = new Date().getTime(),
+        prevDeferred = null,
+        timeout = 1000;
+
+    function makeRequest(args, context, deferred) {
+        deferred.jqXHR = $.ajax(_.extend({ dataType: "json" }, args))
+            .done(function () { deferred.resolveWith(context, arguments) })
+            .fail(function () { deferred.rejectWith(context, arguments) });
+
+        deferred.jqXHR.sentData = args.data;
+    }
+
+    return function (args, context) {
+        var deferred = $.Deferred(),
+            now = new Date().getTime();
+
+        if (nextAvailableTime - now <= 0) {
+            makeRequest(args, context, deferred);
+
+            // nextAvailableTime is in the past.
+            nextAvailableTime = now + timeout;
+        } else {
+            var later = function () {
+                if (!deferred.aborted && !deferred.complete) {
+                    makeRequest(args, context, deferred);
+
+                } else if (deferred.next) {
+                    deferred.next();
+                }
+                deferred.complete = true;
+            };
+
+            prevDeferred && (prevDeferred.next = later);
+            prevDeferred = deferred;
+
+            _.delay(later, nextAvailableTime - now);
+
+            // nextAvailableTime is in the future.
+            nextAvailableTime += timeout;
+        }
+
+        var promise = deferred.promise();
+
+        promise.abort = function () {
+            if (deferred.jqXHR) {
+                deferred.jqXHR.abort();
+            } else {
+                deferred.aborted = true;
+            }
+        };
+        return promise;
+    }
+}());
