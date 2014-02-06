80022ed5b99c5d2b7b0f014ead7561d25e4d9aa0
Michael Wiencek <mwtuea@gmail.com>
2014-02-06 16:48:54 -0600

    Add a test for auto-opening the add-disc dialog

--- root/static/scripts/tests/release-editor/dialogs.js
+++ root/static/scripts/tests/release-editor/dialogs.js
@@ -3,13 +3,11 @@
 // Licensed under the GPL version 2, or (at your option) any later version:
 // http://www.gnu.org/licenses/gpl-2.0.txt
 
-releaseEditor.test.module("release editor dialogs", releaseEditor.test.setupReleaseAdd);
+$.ui.dialog.prototype.options.appendTo = "#qunit-fixture";
 
 
-test("adding an empty medium via the add-disc dialog (MBS-7221)", function () {
-    var addDiscDialog = releaseEditor.addDiscDialog;
-    var trackParserDialog = releaseEditor.trackParserDialog;
-    var mediums = this.release.mediums;
+releaseEditor.test.module("release editor dialogs", function () {
+    releaseEditor.test.setupReleaseAdd();
 
     releaseEditor.trackParser.options = {
         trackArtists: false,
@@ -17,6 +15,20 @@ test("adding an empty medium via the add-disc dialog (MBS-7221)", function () {
         trackTimes: true,
         vinylNumbers: false
     };
+
+    $("#qunit-fixture").append(
+        $("<div>").attr("id", "add-disc-dialog").hide(),
+        $("<div>").attr("id", "track-parser-dialog").hide()
+    );
+
+    releaseEditor.activeTabID("#tracklist");
+});
+
+
+test("adding an empty medium via the add-disc dialog is allowed (MBS-7221)", function () {
+    var addDiscDialog = releaseEditor.addDiscDialog;
+    var trackParserDialog = releaseEditor.trackParserDialog;
+    var mediums = this.release.mediums;
 
     ok(!mediums()[0].hasTracks(), "first medium is empty");
 
@@ -32,4 +44,55 @@ test("adding an empty medium via the add-disc dialog (MBS-7221)", function () {
     addDiscDialog.addDisc();
 
     ok(!mediums()[1].hasTracks(), "new empty medium was added");
+});
+
+
+test("switching to the tracklist tab opens the add-disc dialog if there's only one empty medium", function () {
+    var addDiscDialog = releaseEditor.addDiscDialog;
+    var trackParserDialog = releaseEditor.trackParserDialog;
+
+    releaseEditor.activeTabID("#tracklist");
+    releaseEditor.autoOpenTheAddDiscDialog(this.release);
+
+    var uiDialog = $(addDiscDialog.element).data("ui-dialog");
+
+    ok(uiDialog.isOpen(), "add-disc dialog is open after switching to the tracklist tab");
+
+    releaseEditor.activeTabID("#information");
+    releaseEditor.autoOpenTheAddDiscDialog(this.release);
+
+    ok(!uiDialog.isOpen(), "add-disc dialog is closed after switching back to the information tab");
+
+    this.release.mediums()[0].tracks.push(
+        releaseEditor.fields.Track({ name: "~fooo~", position: 1, length: 12345 })
+    );
+
+    releaseEditor.activeTabID("#information");
+    releaseEditor.autoOpenTheAddDiscDialog(this.release);
+
+    ok(!uiDialog.isOpen(), "add-disc dialog remains closed after switching to the tracklist tab with a non-empty medium");
+});
+
+
+test("clearing the tracks of an existing medium via the track parser doesn't cause the add-disc dialog to open", function () {
+    var addDiscDialog = releaseEditor.addDiscDialog;
+    var trackParserDialog = releaseEditor.trackParserDialog;
+    var medium = this.release.mediums()[0];
+
+    medium.tracks.push(
+        releaseEditor.fields.Track({ name: "~fooo~", position: 1, length: 12345 })
+    );
+
+    ok(medium.hasTracks(), "medium has tracks");
+
+    releaseEditor.activeTabID("#tracklist");
+    trackParserDialog.open(medium);
+    trackParserDialog.toBeParsed("");
+    trackParserDialog.parse();
+    releaseEditor.autoOpenTheAddDiscDialog(this.release);
+
+    ok(!medium.hasTracks(), "medium does not have tracks");
+
+    var uiDialog = $(addDiscDialog.element).data("ui-dialog");
+    ok(!uiDialog, "add-disc dialog is not open");
 });
