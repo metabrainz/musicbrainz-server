2c61029341953cae14e421ac6f7f21435a47200b
Michael Wiencek <mwtuea@gmail.com>
2014-02-06 13:10:24 -0600

    Make sure comments are defined when inserting recording/RG edits
    This allows passing comment => undef to the edit constructor, which
    is consistent with the edit data allowing this field to be Nullable.

--- lib/MusicBrainz/Server/Edit/ReleaseGroup/Edit.pm
+++ lib/MusicBrainz/Server/Edit/ReleaseGroup/Edit.pm
@@ -208,6 +208,7 @@ sub _edit_hash
         if (exists $data->{artist_credit});
     $data->{primary_type_id} = delete $data->{type_id}
         if exists $data->{type_id};
+    $data->{comment} //= '';
     return $data;
 }
 
