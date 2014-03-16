5232a293f53e59f438dc00169c681adeb768f5ab
Ulrich Klauer <ulrich@chirlu.de>
2014-03-16 16:10:52 +0100

    MBS-7350: Load cover art types for Edit Cover Art
    Cover art types were not loaded for the Edit Cover Art page. This
    meant that the types for an image where not selected in the form by
    default.

--- lib/MusicBrainz/Server/Controller/Release.pm
+++ lib/MusicBrainz/Server/Controller/Release.pm
@@ -628,6 +628,7 @@ sub edit_cover_art : Chained('load') PathPart('edit-cover-art') Args(1) Edit
     my @artwork = @{
         $c->model('Artwork')->find_by_release($entity)
     } or $c->detach('/error_404');
+    $c->model('CoverArtType')->load_for(@artwork);
 
     my $artwork = first { $_->id == $id } @artwork;
 
