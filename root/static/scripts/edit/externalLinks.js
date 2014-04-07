metabrainz/musicbrainz-server

commit 3bd1a5a435c0085d155e25db612cfa054f2722c7

MBS-7420: Can't submit artist edit form after adding and removing a URL
After a link is removed, the events it registered on the form still
exist. Now they're unregistered beforehand.

root/static/scripts/edit/externalLinks.js
@@ -113,6 +113,7 @@
                 this.entity1ID(this.original.entity1);
             }
             else {
+                this.cleanup.toggleEvents("off");
                 this.viewModel.links.remove(this);
             }
 

root/static/scripts/edit/MB/Control/URLCleanup.js
@@ -872,14 +872,18 @@ MB.Control.URLCleanup = function (sourceType, typeControl, urlControl, errorObse
         }
     };
 
-    self.typeControl.on("change", typeChanged);
-    self.urlControl.on("change keydown keyup input propertychange", urlChanged);
-
-    self.urlControl.on("blur", function () {
+    function trimInputValue() {
         this.value = _.str.trim(this.value);
-    });
+    }
 
-    self.urlControl.parents('form').submit(urlChanged);
+    self.toggleEvents = function (prop) {
+        self.typeControl[prop]("change", typeChanged);
+        self.urlControl[prop]("change keydown keyup input propertychange", urlChanged);
+        self.urlControl[prop]("blur", trimInputValue);
+        self.urlControl.parents('form')[prop]("submit", urlChanged);
+    };
+
+    self.toggleEvents("on");
 
     return self;
 };

commit 39d5701690a0a46a02f24107368276c5b67ff0e7

Fix qunit failure introduced by 3bd1a5a

root/static/scripts/edit/externalLinks.js
@@ -113,7 +113,9 @@
                 this.entity1ID(this.original.entity1);
             }
             else {
-                this.cleanup.toggleEvents("off");
+                // this.cleanup is undefined for tests that don't deal with
+                // markup (since it's set by the urlCleanup bindingHandler).
+                this.cleanup && this.cleanup.toggleEvents("off");
                 this.viewModel.links.remove(this);
             }
 