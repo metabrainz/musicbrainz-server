173ef0b56d891898692c92ba5a2b90362f1f36d0
nikki <nikki@rika.musicbrainz.org>
2014-04-10 17:02:03 +0000

    Add missing upgrade scripts to upgrade.sh

--- upgrade.sh
+++ upgrade.sh
@@ -71,6 +71,9 @@ OUTPUT=`./admin/psql READWRITE < ./admin/sql/updates/20140214-add-instruments.sq
 echo `date` : 'Add instrument entity documentation tables'
 OUTPUT=`./admin/psql READWRITE < ./admin/sql/updates/20140215-add-instruments-documentation.sql 2>&1` || ( echo "$OUTPUT" ; exit 1 )
 
+echo `date` : 'Add instrument entity primary keys tables'
+OUTPUT=`./admin/psql READWRITE < ./admin/sql/updates/20140308-instrument-pk.sql 2>&1` || ( echo "$OUTPUT" ; exit 1 )
+
 ################################################################################
 # Re-enable replication
 
@@ -90,6 +93,12 @@ then
 
     echo `date` : 'Adding has_dates trigger'
     OUTPUT=`./admin/psql READWRITE < ./admin/sql/updates/20140312-dates-trigger.sql 2>&1` || ( echo "$OUTPUT" ; exit 1 )
+
+    echo `date` : 'Adding foreign keys for instruments'
+    OUTPUT=`./admin/psql READWRITE < ./admin/sql/updates/20140308-instrument-fk.sql 2>&1` || ( echo "$OUTPUT" ; exit 1 )
+
+    echo `date` : 'Adding triggers for instruments'
+    OUTPUT=`./admin/psql READWRITE < ./admin/sql/updates/20140217-instrument-triggers.sql 2>&1` || ( echo "$OUTPUT" ; exit 1 )
 
     echo `date` : Enabling last_updated triggers
     ./admin/sql/EnableLastUpdatedTriggers.pl
