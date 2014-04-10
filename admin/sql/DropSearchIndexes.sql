f1ca09686b734533f8c74731b42c763644ee38d8
nikki <nikki@rika.musicbrainz.org>
2014-04-10 14:03:51 +0000

    Run GenerateSQLScripts

--- admin/sql/DropSearchIndexes.sql
+++ admin/sql/DropSearchIndexes.sql
@@ -1,14 +1,26 @@
 -- Automatically generated, do not edit.
 \unset ON_ERROR_STOP
 
+DROP INDEX area_alias_idx_txt;
+DROP INDEX area_alias_idx_txt_sort;
 DROP INDEX area_idx_name_txt;
+DROP INDEX artist_alias_idx_txt;
+DROP INDEX artist_alias_idx_txt_sort;
 DROP INDEX artist_credit_idx_txt;
 DROP INDEX artist_credit_name_idx_txt;
 DROP INDEX artist_idx_txt;
+DROP INDEX artist_idx_txt_sort;
+DROP INDEX label_alias_idx_txt;
+DROP INDEX label_alias_idx_txt_sort;
 DROP INDEX label_idx_txt;
+DROP INDEX place_alias_idx_txt;
+DROP INDEX place_alias_idx_txt_sort;
+DROP INDEX place_idx_name_txt;
 DROP INDEX recording_idx_txt;
 DROP INDEX release_group_idx_txt;
 DROP INDEX release_idx_txt;
 DROP INDEX tag_idx_name_txt;
 DROP INDEX track_idx_txt;
+DROP INDEX work_alias_idx_txt;
+DROP INDEX work_alias_idx_txt_sort;
 DROP INDEX work_idx_txt;
