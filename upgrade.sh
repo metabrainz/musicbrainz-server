f9fcda0d6adf3f4c5d98f125c04689625f2929f5
Ian McEwen <ianmcorvidae@ianmcorvidae.net>
2014-04-09 18:21:00 -0700

    Fix ordering of upgrade.sh.

--- upgrade.sh
+++ upgrade.sh
@@ -82,11 +82,11 @@ then
     echo `date` : 'Adding foreign keys for ordering columns'
     OUTPUT=`./admin/psql READWRITE < ./admin/sql/updates/20140308-ordering-columns-fk.sql 2>&1` || ( echo "$OUTPUT" ; exit 1 )
 
-    echo `date` : Enabling last_updated triggers
-    ./admin/sql/EnableLastUpdatedTriggers.pl
-
     echo `date` : 'Adding has_dates trigger'
     OUTPUT=`./admin/psql READWRITE < ./admin/sql/updates/20140312-dates-trigger.sql 2>&1` || ( echo "$OUTPUT" ; exit 1 )
+
+    echo `date` : Enabling last_updated triggers
+    ./admin/sql/EnableLastUpdatedTriggers.pl
 fi
 
 ################################################################################
