e09ab6e58bc87002c311461da51bba0b2e40c4ae
nikki <nikki@rika.musicbrainz.org>
2014-04-13 22:24:47 +0000

    Merge 20140308-instrument-fk.sql and 20140308-instrument-pk.sql into 20140217-instrument-triggers.sql and 20140214-add-instruments.sql

--- admin/sql/updates/20140214-add-instruments.sql
+++ admin/sql/updates/20140214-add-instruments.sql
@@ -256,4 +256,22 @@ SET description = regexp_replace(description, ' ?Other names(?: include)?:? (.*?
 WHERE description ~ '.*Other names(?: include)?:? (.*?).? *$';
 
 
+ALTER TABLE edit_instrument ADD CONSTRAINT edit_instrument_pkey PRIMARY KEY (edit, instrument);
+ALTER TABLE instrument ADD CONSTRAINT instrument_pkey PRIMARY KEY (id);
+ALTER TABLE instrument_alias ADD CONSTRAINT instrument_alias_pkey PRIMARY KEY (id);
+ALTER TABLE instrument_alias_type ADD CONSTRAINT instrument_alias_type_pkey PRIMARY KEY (id);
+ALTER TABLE instrument_annotation ADD CONSTRAINT instrument_annotation_pkey PRIMARY KEY (instrument, annotation);
+ALTER TABLE instrument_gid_redirect ADD CONSTRAINT instrument_gid_redirect_pkey PRIMARY KEY (gid);
+ALTER TABLE instrument_type ADD CONSTRAINT instrument_type_pkey PRIMARY KEY (id);
+ALTER TABLE l_area_instrument ADD CONSTRAINT l_area_instrument_pkey PRIMARY KEY (id);
+ALTER TABLE l_artist_instrument ADD CONSTRAINT l_artist_instrument_pkey PRIMARY KEY (id);
+ALTER TABLE l_instrument_instrument ADD CONSTRAINT l_instrument_instrument_pkey PRIMARY KEY (id);
+ALTER TABLE l_instrument_label ADD CONSTRAINT l_instrument_label_pkey PRIMARY KEY (id);
+ALTER TABLE l_instrument_place ADD CONSTRAINT l_instrument_place_pkey PRIMARY KEY (id);
+ALTER TABLE l_instrument_recording ADD CONSTRAINT l_instrument_recording_pkey PRIMARY KEY (id);
+ALTER TABLE l_instrument_release ADD CONSTRAINT l_instrument_release_pkey PRIMARY KEY (id);
+ALTER TABLE l_instrument_release_group ADD CONSTRAINT l_instrument_release_group_pkey PRIMARY KEY (id);
+ALTER TABLE l_instrument_url ADD CONSTRAINT l_instrument_url_pkey PRIMARY KEY (id);
+ALTER TABLE l_instrument_work ADD CONSTRAINT l_instrument_work_pkey PRIMARY KEY (id);
+
 COMMIT;
--- admin/sql/updates/20140217-instrument-triggers.sql
+++ admin/sql/updates/20140217-instrument-triggers.sql
@@ -48,4 +48,195 @@ ALTER TABLE instrument_alias
   ADD CONSTRAINT control_for_whitespace_sort_name CHECK (controlled_for_whitespace(sort_name)),
   ADD CONSTRAINT only_non_empty_sort_name CHECK (sort_name != '');
 
+ALTER TABLE edit_instrument
+   ADD CONSTRAINT edit_instrument_fk_edit
+   FOREIGN KEY (edit)
+   REFERENCES edit(id);
+
+ALTER TABLE edit_instrument
+   ADD CONSTRAINT edit_instrument_fk_instrument
+   FOREIGN KEY (instrument)
+   REFERENCES instrument(id)
+   ON DELETE CASCADE;
+
+ALTER TABLE instrument
+   ADD CONSTRAINT instrument_fk_type
+   FOREIGN KEY (type)
+   REFERENCES instrument_type(id);
+
+ALTER TABLE instrument_alias
+   ADD CONSTRAINT instrument_alias_fk_instrument
+   FOREIGN KEY (instrument)
+   REFERENCES instrument(id);
+
+ALTER TABLE instrument_alias
+   ADD CONSTRAINT instrument_alias_fk_type
+   FOREIGN KEY (type)
+   REFERENCES instrument_alias_type(id);
+
+ALTER TABLE instrument_annotation
+   ADD CONSTRAINT instrument_annotation_fk_instrument
+   FOREIGN KEY (instrument)
+   REFERENCES instrument(id);
+
+ALTER TABLE instrument_annotation
+   ADD CONSTRAINT instrument_annotation_fk_annotation
+   FOREIGN KEY (annotation)
+   REFERENCES annotation(id);
+
+ALTER TABLE instrument_gid_redirect
+   ADD CONSTRAINT instrument_gid_redirect_fk_new_id
+   FOREIGN KEY (new_id)
+   REFERENCES instrument(id);
+
+ALTER TABLE l_area_instrument
+   ADD CONSTRAINT l_area_instrument_fk_link
+   FOREIGN KEY (link)
+   REFERENCES link(id);
+
+ALTER TABLE l_area_instrument
+   ADD CONSTRAINT l_area_instrument_fk_entity0
+   FOREIGN KEY (entity0)
+   REFERENCES area(id);
+
+ALTER TABLE l_area_instrument
+   ADD CONSTRAINT l_area_instrument_fk_entity1
+   FOREIGN KEY (entity1)
+   REFERENCES instrument(id);
+
+ALTER TABLE l_artist_instrument
+   ADD CONSTRAINT l_artist_instrument_fk_link
+   FOREIGN KEY (link)
+   REFERENCES link(id);
+
+ALTER TABLE l_artist_instrument
+   ADD CONSTRAINT l_artist_instrument_fk_entity0
+   FOREIGN KEY (entity0)
+   REFERENCES artist(id);
+
+ALTER TABLE l_artist_instrument
+   ADD CONSTRAINT l_artist_instrument_fk_entity1
+   FOREIGN KEY (entity1)
+   REFERENCES instrument(id);
+
+ALTER TABLE l_instrument_instrument
+   ADD CONSTRAINT l_instrument_instrument_fk_link
+   FOREIGN KEY (link)
+   REFERENCES link(id);
+
+ALTER TABLE l_instrument_instrument
+   ADD CONSTRAINT l_instrument_instrument_fk_entity0
+   FOREIGN KEY (entity0)
+   REFERENCES instrument(id);
+
+ALTER TABLE l_instrument_instrument
+   ADD CONSTRAINT l_instrument_instrument_fk_entity1
+   FOREIGN KEY (entity1)
+   REFERENCES instrument(id);
+
+ALTER TABLE l_instrument_label
+   ADD CONSTRAINT l_instrument_label_fk_link
+   FOREIGN KEY (link)
+   REFERENCES link(id);
+
+ALTER TABLE l_instrument_label
+   ADD CONSTRAINT l_instrument_label_fk_entity0
+   FOREIGN KEY (entity0)
+   REFERENCES instrument(id);
+
+ALTER TABLE l_instrument_label
+   ADD CONSTRAINT l_instrument_label_fk_entity1
+   FOREIGN KEY (entity1)
+   REFERENCES label(id);
+
+ALTER TABLE l_instrument_place
+   ADD CONSTRAINT l_instrument_place_fk_link
+   FOREIGN KEY (link)
+   REFERENCES link(id);
+
+ALTER TABLE l_instrument_place
+   ADD CONSTRAINT l_instrument_place_fk_entity0
+   FOREIGN KEY (entity0)
+   REFERENCES instrument(id);
+
+ALTER TABLE l_instrument_place
+   ADD CONSTRAINT l_instrument_place_fk_entity1
+   FOREIGN KEY (entity1)
+   REFERENCES place(id);
+
+ALTER TABLE l_instrument_recording
+   ADD CONSTRAINT l_instrument_recording_fk_link
+   FOREIGN KEY (link)
+   REFERENCES link(id);
+
+ALTER TABLE l_instrument_recording
+   ADD CONSTRAINT l_instrument_recording_fk_entity0
+   FOREIGN KEY (entity0)
+   REFERENCES instrument(id);
+
+ALTER TABLE l_instrument_recording
+   ADD CONSTRAINT l_instrument_recording_fk_entity1
+   FOREIGN KEY (entity1)
+   REFERENCES recording(id);
+
+ALTER TABLE l_instrument_release
+   ADD CONSTRAINT l_instrument_release_fk_link
+   FOREIGN KEY (link)
+   REFERENCES link(id);
+
+ALTER TABLE l_instrument_release
+   ADD CONSTRAINT l_instrument_release_fk_entity0
+   FOREIGN KEY (entity0)
+   REFERENCES instrument(id);
+
+ALTER TABLE l_instrument_release
+   ADD CONSTRAINT l_instrument_release_fk_entity1
+   FOREIGN KEY (entity1)
+   REFERENCES release(id);
+
+ALTER TABLE l_instrument_release_group
+   ADD CONSTRAINT l_instrument_release_group_fk_link
+   FOREIGN KEY (link)
+   REFERENCES link(id);
+
+ALTER TABLE l_instrument_release_group
+   ADD CONSTRAINT l_instrument_release_group_fk_entity0
+   FOREIGN KEY (entity0)
+   REFERENCES instrument(id);
+
+ALTER TABLE l_instrument_release_group
+   ADD CONSTRAINT l_instrument_release_group_fk_entity1
+   FOREIGN KEY (entity1)
+   REFERENCES release_group(id);
+
+ALTER TABLE l_instrument_url
+   ADD CONSTRAINT l_instrument_url_fk_link
+   FOREIGN KEY (link)
+   REFERENCES link(id);
+
+ALTER TABLE l_instrument_url
+   ADD CONSTRAINT l_instrument_url_fk_entity0
+   FOREIGN KEY (entity0)
+   REFERENCES instrument(id);
+
+ALTER TABLE l_instrument_url
+   ADD CONSTRAINT l_instrument_url_fk_entity1
+   FOREIGN KEY (entity1)
+   REFERENCES url(id);
+
+ALTER TABLE l_instrument_work
+   ADD CONSTRAINT l_instrument_work_fk_link
+   FOREIGN KEY (link)
+   REFERENCES link(id);
+
+ALTER TABLE l_instrument_work
+   ADD CONSTRAINT l_instrument_work_fk_entity0
+   FOREIGN KEY (entity0)
+   REFERENCES instrument(id);
+
+ALTER TABLE l_instrument_work
+   ADD CONSTRAINT l_instrument_work_fk_entity1
+   FOREIGN KEY (entity1)
+   REFERENCES work(id);
+
 COMMIT;
