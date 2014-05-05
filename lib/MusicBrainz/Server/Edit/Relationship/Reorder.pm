a5a61bf7a6daa74643220ce42575a5bf0599ad02
Michael Wiencek <mwtuea@gmail.com>
2014-05-04 21:38:00 -0500

    Revert "Modify foreign_keys to show relationship reorder edits in related entities' edit histories"
    This reverts commit 28eb73ab3893c52d096868f2f38e9168c6d1e9f3.
    
    This code is useless and does not do what I thought it did.

--- lib/MusicBrainz/Server/Edit/Relationship/Reorder.pm
+++ lib/MusicBrainz/Server/Edit/Relationship/Reorder.pm
@@ -30,7 +30,8 @@ has '+data' => (
     ]
 );
 
-sub alter_edit_pending {
+sub alter_edit_pending
+{
     my $self = shift;
     return {
         Relationship => [
@@ -60,29 +61,15 @@ sub initialize {
 sub foreign_keys {
     my $self = shift;
 
-    my ($link_type, @relationships) = $self->_load_relationships;
-
-    my $model0 = type_to_model($link_type->entity0_type);
-    my $model1 = type_to_model($link_type->entity1_type);
-
-    my $load = {
+    return {
         LinkType => {
-            $link_type->id => [],
+            $self->data->{link_type_id} => [],
         },
         Relationship => {
-            map { $_->id => [] } @relationships
+            map { $_->{relationship_id} => [] }
+                @{ $self->data->{relationship_order} }
         }
     };
-
-    my $load_model0 = $load->{$model0} = {};
-    my $load_model1 = $load->{$model1} = {};
-
-    for (@relationships) {
-        $load_model0->{$_->entity0_id} = ['ArtistCredit'];
-        $load_model1->{$_->entity1_id} = ['ArtistCredit'];
-    }
-
-    return $load;
 }
 
 sub build_display_data {
@@ -104,25 +91,6 @@ sub build_display_data {
 sub accept {
     my $self = shift;
 
-    my ($link_type, @relationships) = $self->_load_relationships;
-
-    MusicBrainz::Server::Edit::Exceptions::FailedDependency->throw(
-        "The relationships cannot be reordered because they no longer share the same link type."
-    ) if any { $_->link->type_id != $link_type_id } @relationships;
-
-    # FIXME this should perform a three way merge and check for conflicts
-
-    $self->c->model('Relationship')->reorder(
-        $link_type->entity0_type, $link_type->entity1_type,
-        map {
-            $_->{relationship_id} => $_->{new}
-        } @{ $self->data->{relationship_order} }
-    );
-}
-
-sub _load_relationships {
-    my $self = shift;
-
     my $link_type_id = $self->data->{link_type_id};
     my $link_type = $self->c->model('LinkType')->get_by_id($link_type_id);
 
@@ -136,7 +104,18 @@ sub _load_relationships {
     my @relationships = values %$relationships;
     $self->c->model('Link')->load(@relationships);
 
-    return $link_type, @relationships;
+    MusicBrainz::Server::Edit::Exceptions::FailedDependency->throw(
+        "The relationships cannot be reordered because they no longer share the same link type."
+    ) if any { $_->link->type_id != $link_type_id } @relationships;
+
+    # FIXME this should perform a three way merge and check for conflicts
+
+    $self->c->model('Relationship')->reorder(
+        $link_type->entity0_type, $link_type->entity1_type,
+        map {
+            $_->{relationship_id} => $_->{new}
+        } @{ $self->data->{relationship_order} }
+    );
 }
 
 1;
