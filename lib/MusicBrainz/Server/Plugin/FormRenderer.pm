044b6f1f7b26581445f8fee6c21a81b501588622
Michael Wiencek <mwtuea@gmail.com>
2014-05-12 14:59:47 -0500

    Don't add an empty option to the series type <select>, even though it's not required when editing

--- lib/MusicBrainz/Server/Plugin/FormRenderer.pm
+++ lib/MusicBrainz/Server/Plugin/FormRenderer.pm
@@ -196,7 +196,10 @@ sub select
         }
     }
 
-    if (!$field->required || delete $attrs->{no_default})
+    my $disallow_empty = delete $attrs->{disallow_empty};
+    my $no_default = delete $attrs->{no_default};
+
+    if (!$disallow_empty && (!$field->required || $no_default))
     {
         unshift @options, $self->h->option({
             selected => !defined $field->value ? "selected" : undef,
