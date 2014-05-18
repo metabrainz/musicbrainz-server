5bb6578674f04eaebc12a36ded104576ef5bceaf
nikki <nikki@rika.musicbrainz.org>
2014-05-18 13:58:30 +0000

    Stop place/MBID/relationships from trying to load a missing template

--- lib/MusicBrainz/Server/Controller/Place.pm
+++ lib/MusicBrainz/Server/Controller/Place.pm
@@ -12,7 +12,6 @@ with 'MusicBrainz::Server::Controller::Role::Alias';
 with 'MusicBrainz::Server::Controller::Role::Cleanup';
 with 'MusicBrainz::Server::Controller::Role::Details';
 with 'MusicBrainz::Server::Controller::Role::EditListing';
-with 'MusicBrainz::Server::Controller::Role::Relationship';
 with 'MusicBrainz::Server::Controller::Role::Tag';
 with 'MusicBrainz::Server::Controller::Role::WikipediaExtract';
 with 'MusicBrainz::Server::Controller::Role::CommonsImage';
