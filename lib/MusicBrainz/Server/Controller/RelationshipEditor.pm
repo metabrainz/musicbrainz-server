3f9cee607def9e09236b0c1dc1631894b3a7a81a
Michael Wiencek <mwtuea@gmail.com>
2014-05-25 15:55:48 -0500

    Various /relationship-editor endpoint fixes
    Fixes detection/error messages for unrecognized parameters, and
    validates link type ids with the given entity types.

--- lib/MusicBrainz/Server/Controller/RelationshipEditor.pm
+++ lib/MusicBrainz/Server/Controller/RelationshipEditor.pm
@@ -4,7 +4,7 @@ use Moose;
 BEGIN { extends 'MusicBrainz::Server::Controller' }
 
 use Encode;
-use JSON qw( encode_json );
+use JSON;
 use MusicBrainz::Server::CGI::Expand qw( expand_hash );
 use MusicBrainz::Server::Constants qw(
     $EDIT_RELATIONSHIP_CREATE
@@ -24,9 +24,8 @@ our $valid_params = qr/
             |id
             |link_type
             |entity\.(0|1)\.(gid|type)
-            |period\.(begin_date|end_date)\.(year|month|day)
-            |ended
-            |attrs\.[^\.](\.[0-9]+)?
+            |period\.((begin_date|end_date)\.(year|month|day)|ended)
+            |attrs\.[^\.]+(\.[0-9]+)?
         )
         |edit_note
         |as_auto_editor
@@ -51,15 +50,13 @@ sub base : Path('/relationship-editor') Args(0) Edit {
                 }
             }
         } catch {
-            $c->res->status(400);
-            $c->res->body(encode_json({ error => $_ }));
+            detach_with_error($c, $_);
         };
 
         $params = expand_hash($params);
-        $self->submit_edits($c, $params->{'rel-editor'});
+        $self->submit_edits($c, $params->{'rel-editor'} // {});
     } else {
-        $c->res->status(400);
-        $c->res->body(encode_json({ error => 'Invalid submission' }));
+        detach_with_error($c, 'Invalid submission');
     }
 }
 
@@ -67,13 +64,17 @@ sub submit_edits {
     my ($self, $c, $params) = @_;
 
     my $attr_tree = $c->model('LinkAttributeType')->get_tree;
+    my @rels = @{ $params->{rels} // [] };
 
-    foreach my $rel (@{ $params->{rels} // [] }) {
-        $rel->{entity}->[0]->{entityType} = delete $rel->{entity}->[0]->{type}
-            or die "Missing field: entity.0.type";
+    foreach my $rel (@rels) {
+        my $type0 = delete $rel->{entity}->[0]->{type}
+            or detach_with_error($c, "Missing field: entity.0.type");
 
-        $rel->{entity}->[1]->{entityType} = delete $rel->{entity}->[1]->{type}
-            or die "Missing field: entity.1.type";
+        my $type1 = delete $rel->{entity}->[1]->{type}
+            or detach_with_error($c, "Missing field: entity.1.type");
+
+        $rel->{entity}->[0]->{entityType} = $type0;
+        $rel->{entity}->[1]->{entityType} = $type1;
 
         $rel->{entities} = delete $rel->{entity};
         $rel->{linkTypeID} = delete $rel->{link_type};
@@ -107,15 +108,24 @@ sub submit_edits {
         } elsif ($action eq 'edit') {
             $rel->{edit_type} = $EDIT_RELATIONSHIP_EDIT;
         } else {
-            die "Missing field: action";
+            detach_with_error($c, "Missing field: action");
         }
     }
 
     MusicBrainz::Server::Controller::WS::js::Edit->submit_edits($c, {
-        edits => $params->{rels},
+        edits => \@rels,
         asAutoEditor => $params->{as_auto_editor},
         editNote => $params->{edit_note},
     });
+}
+
+sub detach_with_error {
+    my ($c, $error) = @_;
+
+    my $json = JSON->new;
+    $c->res->body($json->encode({ error => $error }));
+    $c->res->status(400);
+    $c->detach;
 }
 
 __PACKAGE__->meta->make_immutable;
