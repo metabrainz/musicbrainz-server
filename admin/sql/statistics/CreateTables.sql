9025827838a3ce2353e8748d369f553148edfca8
Ian McEwen <ianmcorvidae@ianmcorvidae.net>
2014-05-28 16:24:27 -0700

    Generate CreateReplicationTriggers.sql files by way of additional comments in CreateTables. Add upgrade script for fixing the documentation ones.

--- admin/sql/statistics/CreateTables.sql
+++ admin/sql/statistics/CreateTables.sql
@@ -10,15 +10,14 @@ CREATE TABLE log_statistic
     data                TEXT NOT NULL -- JSON data
 );
 
-CREATE TABLE statistic
-(
+CREATE TABLE statistic ( -- replicate
     id                  SERIAL,
     name                VARCHAR(100) NOT NULL,
     value               INTEGER NOT NULL,
     date_collected      date NOT NULL DEFAULT NOW()
 );
 
-CREATE TABLE statistic_event (
+CREATE TABLE statistic_event ( -- replicate
     date DATE NOT NULL CHECK (date >= '2000-01-01'), -- PK
     title TEXT NOT NULL,
     link TEXT NOT NULL,
