2f84c714d36b564a462f3a995989ff81e998d699
nikki <nikki@rika.musicbrainz.org>
2014-07-19 12:00:02 +0000

    Add report for possible duplicate release groups (MBS-3586)

--- lib/MusicBrainz/Server/ReportFactory.pm
+++ lib/MusicBrainz/Server/ReportFactory.pm
@@ -33,6 +33,7 @@ use MusicBrainz::Server::PagedReport;
     DuplicateRelationshipsRecordings
     DuplicateRelationshipsWorks
     DuplicateRelationshipsLabels
+    DuplicateReleaseGroups
     FeaturingRecordings
     FeaturingReleaseGroups
     FeaturingReleases
@@ -92,6 +93,7 @@ use MusicBrainz::Server::Report::DuplicateRelationshipsReleases;
 use MusicBrainz::Server::Report::DuplicateRelationshipsRecordings;
 use MusicBrainz::Server::Report::DuplicateRelationshipsWorks;
 use MusicBrainz::Server::Report::DuplicateRelationshipsLabels;
+use MusicBrainz::Server::Report::DuplicateReleaseGroups;
 use MusicBrainz::Server::Report::FeaturingRecordings;
 use MusicBrainz::Server::Report::FeaturingReleaseGroups;
 use MusicBrainz::Server::Report::FeaturingReleases;
