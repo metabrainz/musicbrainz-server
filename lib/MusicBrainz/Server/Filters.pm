20873a726f08d53b56f6d9f61ea0ea004469b481
reosarevok <reosarevok@rika.musicbrainz.org>
2014-07-21 15:30:28 +0000

    mbs-6201: add special setlist formatting

--- lib/MusicBrainz/Server/Filters.pm
+++ lib/MusicBrainz/Server/Filters.pm
@@ -54,10 +54,51 @@ sub format_setlist {
     $text =~ s/</&lt;/g;
     $text =~ s/>/&gt;/g;
 
+    # Lines starting with @ are artists
+    $text =~ s/^@ ([^\r\n]*)/format_setlist_artist($1)/meg;
+
+    # Lines starting with * are works
+    $text =~ s/^\* ([^\r\n]*)/format_setlist_work($1)/meg;
+
+    # Lines starting with # are comments
+    $text =~ s/^# ([^\r\n]*)/<span class=\"comment\">$1<\/span>/mg;
+
     # Fix newlines
     $text =~ s/(\015\012|\012\015|\012|\015)/<br\/>/g;
 
     return $text;
+}
+
+sub format_setlist_artist {
+    my ($line) = @_;
+
+    $line =~ s/
+      \[
+      ([0-9a-f]{8} -
+       [0-9a-f]{4} -
+       [0-9a-f]{4} -
+       [0-9a-f]{4} -
+       [0-9a-f]{12})(?:\|([^\]]+))?\]
+    /_make_link("artist",$1,$2)/eixg;
+
+    $line = "<b>Artist: $line</b>";
+
+    return $line;
+}
+
+sub format_setlist_work {
+    my ($line) = @_;
+
+    $line =~ s/
+      \[
+      ([0-9a-f]{8} -
+       [0-9a-f]{4} -
+       [0-9a-f]{4} -
+       [0-9a-f]{4} -
+       [0-9a-f]{12})(?:\|([^\]]+))?\]
+    /_make_link("work",$1,$2)/eixg;
+
+    return $line;
 }
 
 sub format_wikitext
