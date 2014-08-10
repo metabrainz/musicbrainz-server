1cc5a16f8416f5eb3b3c6f8a9a9b4c8c9659180c
Ian McEwen <ianmcorvidae@ianmcorvidae.net>
2014-08-10 16:15:09 -0700

    Make EditSearch::Predicate::Entity automatically create the various bits, using %ENTITIES and a dose of eval magic.

--- lib/MusicBrainz/Server/EditSearch/Predicate/Entity.pm
+++ lib/MusicBrainz/Server/EditSearch/Predicate/Entity.pm
@@ -1,55 +1,49 @@
-use MusicBrainz::Server::EditSearch::Predicate::LinkedEntity;
+use MusicBrainz::Server::Constants qw( %ENTITIES entities_with );
+
+for my $type (entities_with(['mbid', 'relatable'])) {
+    my $model = $ENTITIES{$type}{model};
+    my $has_subs = $ENTITIES{$type}{subscriptions};
+    my $subs_section = '';
+
+    if ($has_subs) {
+        $subs_section = <<EOF;
 use MusicBrainz::Server::EditSearch::Predicate::SubscribedEntity;
+with 'MusicBrainz::Server::EditSearch::Predicate::SubscribedEntity' => { type => '$type' };
+EOF
+    }
 
-package MusicBrainz::Server::EditSearch::Predicate::Area;
+    # This eval is what actually creates the package. We have to do it because
+    # the 'package' function thinks it's a version number if you pass it a
+    # string, and we can't interpolate $model any other way than this (the rest
+    # could presumably just be done with a normal block).
+    eval <<EOF;
+package MusicBrainz::Server::EditSearch::Predicate::$model;
 use Moose;
-with 'MusicBrainz::Server::EditSearch::Predicate::LinkedEntity' => { type => 'area' };
+use MusicBrainz::Server::EditSearch::Predicate::LinkedEntity;
+with 'MusicBrainz::Server::EditSearch::Predicate::LinkedEntity' => { type => '$type' };
+$subs_section
 with 'MusicBrainz::Server::EditSearch::Predicate';
+EOF
+};
 
-package MusicBrainz::Server::EditSearch::Predicate::Artist;
-use Moose;
-with 'MusicBrainz::Server::EditSearch::Predicate::LinkedEntity' => { type => 'artist' };
-with 'MusicBrainz::Server::EditSearch::Predicate::SubscribedEntity' => { type => 'artist' };
-with 'MusicBrainz::Server::EditSearch::Predicate';
+1;
 
-package MusicBrainz::Server::EditSearch::Predicate::Instrument;
-use Moose;
-with 'MusicBrainz::Server::EditSearch::Predicate::LinkedEntity' => { type => 'instrument' };
-with 'MusicBrainz::Server::EditSearch::Predicate';
+=head1 COPYRIGHT
 
-package MusicBrainz::Server::EditSearch::Predicate::Label;
-use Moose;
-with 'MusicBrainz::Server::EditSearch::Predicate::LinkedEntity' => { type => 'label' };
-with 'MusicBrainz::Server::EditSearch::Predicate::SubscribedEntity' => { type => 'label' };
-with 'MusicBrainz::Server::EditSearch::Predicate';
+Copyright (C) 2014 MetaBrainz Foundation
 
-package MusicBrainz::Server::EditSearch::Predicate::Place;
-use Moose;
-with 'MusicBrainz::Server::EditSearch::Predicate::LinkedEntity' => { type => 'place' };
-with 'MusicBrainz::Server::EditSearch::Predicate';
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
 
-package MusicBrainz::Server::EditSearch::Predicate::Recording;
-use Moose;
-with 'MusicBrainz::Server::EditSearch::Predicate::LinkedEntity' => { type => 'recording' };
-with 'MusicBrainz::Server::EditSearch::Predicate';
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
 
-package MusicBrainz::Server::EditSearch::Predicate::Release;
-use Moose;
-with 'MusicBrainz::Server::EditSearch::Predicate::LinkedEntity' => { type => 'release' };
-with 'MusicBrainz::Server::EditSearch::Predicate';
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 
-package MusicBrainz::Server::EditSearch::Predicate::ReleaseGroup;
-use Moose;
-with 'MusicBrainz::Server::EditSearch::Predicate::LinkedEntity' => { type => 'release_group' };
-with 'MusicBrainz::Server::EditSearch::Predicate';
-
-package MusicBrainz::Server::EditSearch::Predicate::Series;
-use Moose;
-with 'MusicBrainz::Server::EditSearch::Predicate::LinkedEntity' => { type => 'series' };
-with 'MusicBrainz::Server::EditSearch::Predicate::SubscribedEntity' => { type => 'series' };
-with 'MusicBrainz::Server::EditSearch::Predicate';
-
-package MusicBrainz::Server::EditSearch::Predicate::Work;
-use Moose;
-with 'MusicBrainz::Server::EditSearch::Predicate::LinkedEntity' => { type => 'work' };
-with 'MusicBrainz::Server::EditSearch::Predicate';
+=cut
