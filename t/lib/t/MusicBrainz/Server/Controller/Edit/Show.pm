6667f9486865149c4cc33b0498d8fdebc84eceb1
Ian McEwen <ianmcorvidae@ianmcorvidae.net>
2014-08-10 17:11:02 -0700

    Fix Controller::Edit::Show test failing due to the 'remove_label' template no longer existing.

--- t/lib/t/MusicBrainz/Server/Controller/Edit/Show.pm
+++ t/lib/t/MusicBrainz/Server/Controller/Edit/Show.pm
@@ -22,7 +22,8 @@ my $hard_mock_edit_class = $mock_edit_class + 1;
     package t::Controller::Edit::Show::MockEdit::Hard;
     use Moose;
     extends 't::Controller::Edit::Show::MockEdit';
-    sub edit_name { 'Remove label' } # Just so we use an edit template
+    sub edit_name { 'Remove label' }
+    sub edit_template { "remove_entity" };
     sub edit_type { $hard_mock_edit_class }
 
     use MusicBrainz::Server::Constants qw( :expire_action :quality );
