69dc0d68569535d8782ec92dd4ea6ce91535c50b
Ian McEwen <ianmcorvidae@ianmcorvidae.net>
2014-09-02 11:57:08 -0700

    Use Role::RelationshipWrapper in instrument/place controllers, not Role::Relationship.

--- lib/MusicBrainz/Server/Controller/Place.pm
+++ lib/MusicBrainz/Server/Controller/Place.pm
@@ -12,7 +12,7 @@ with 'MusicBrainz::Server::Controller::Role::Alias';
 with 'MusicBrainz::Server::Controller::Role::Cleanup';
 with 'MusicBrainz::Server::Controller::Role::Details';
 with 'MusicBrainz::Server::Controller::Role::EditListing';
-with 'MusicBrainz::Server::Controller::Role::Relationship';
+with 'MusicBrainz::Server::Controller::Role::RelationshipWrapper';
 with 'MusicBrainz::Server::Controller::Role::Tag';
 with 'MusicBrainz::Server::Controller::Role::WikipediaExtract';
 with 'MusicBrainz::Server::Controller::Role::CommonsImage';
