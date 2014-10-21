499e7a3887583aabf115223510a5df2974c5645f
Michael Wiencek <mwtuea@gmail.com>
2014-10-20 21:47:29 -0500

    Merge scripts so that functions are added to slaves

--- admin/sql/updates/schema-change/21.slave.sql
+++ admin/sql/updates/schema-change/21.slave.sql
@@ -972,4 +972,11 @@ SELECT '20141014-mbs-7784-data-tracks.sql';
 
 ALTER TABLE track ADD COLUMN is_data_track BOOLEAN NOT NULL DEFAULT FALSE;
 
+CREATE OR REPLACE FUNCTION track_count_matches_cdtoc(medium, int) RETURNS boolean AS $$
+    SELECT $1.track_count = $2 + COALESCE(
+        (SELECT count(*) FROM track
+         WHERE medium = $1.id AND (position = 0 OR is_data_track = true)
+    ), 0);
+$$ LANGUAGE SQL IMMUTABLE;
+
 COMMIT;
