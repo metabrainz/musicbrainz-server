9828e56b232f1ee7bb211f2ecbb730135737fd05
reosarevok <reosarevok@rika.musicbrainz.org>
2014-11-05 10:38:02 +0000

    Generating things with scripts

--- admin/sql/updates/schema-change/21.slave.sql
+++ admin/sql/updates/schema-change/21.slave.sql
@@ -976,6 +976,7 @@ SELECT '20140806-collection-types.sql';
 CREATE TABLE editor_collection_type ( -- replicate
     id                  SERIAL,
     name                VARCHAR(255) NOT NULL,
+    entity_type         VARCHAR(50) NOT NULL,
     parent              INTEGER, -- references editor_collection_type.id
     child_order         INTEGER NOT NULL DEFAULT 0,
     description         TEXT
@@ -983,15 +984,20 @@ CREATE TABLE editor_collection_type ( -- replicate
 
 ALTER TABLE editor_collection_type ADD CONSTRAINT editor_collection_type_pkey PRIMARY KEY (id);
 
-INSERT INTO editor_collection_type (id, name, child_order) VALUES
-	(1, 'Owned music', 1),
-	(2, 'Wishlist', 2),
-	(3, 'Other', 99);
+INSERT INTO editor_collection_type (id, name, entity_type, parent, child_order) VALUES
+        (1, 'Release', 'release', NULL, 1),
+	(2, 'Owned music', 'release', 1, 1),
+	(3, 'Wishlist', 'release', 1, 2);
 
 SELECT setval('editor_collection_type_id_seq', (SELECT MAX(id) FROM editor_collection_type));
 
 ALTER TABLE editor_collection
     ADD COLUMN type INTEGER;
+
+UPDATE editor_collection SET type = 1;
+
+ALTER TABLE editor_collection
+    ALTER COLUMN type SET NOT NULL;
 
 --------------------------------------------------------------------------------
 SELECT '20140906-event-collections.sql';
@@ -1004,9 +1010,10 @@ CREATE TABLE editor_collection_event
 
 ALTER TABLE editor_collection_event ADD CONSTRAINT editor_collection_event_pkey PRIMARY KEY (collection, event);
 
-INSERT INTO editor_collection_type (id, name, child_order) VALUES
-	(4, 'Attending', 3),
-	(5, 'Maybe attending', 4);
+INSERT INTO editor_collection_type (id, name, entity_type, parent, child_order) VALUES
+        (4, 'Event', 'event', NULL, 2)
+	(5, 'Attending', 'event', 4, 1),
+	(6, 'Maybe attending', 'event', 4, 2);
 
 --------------------------------------------------------------------------------
 SELECT '20141014-mbs-7551-tags.sql';
