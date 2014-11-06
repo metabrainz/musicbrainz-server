56b499289c627836483ac28fb83d5678d7483204
reosarevok <reosarevok@rika.musicbrainz.org>
2014-11-06 10:53:54 +0000

    mbs-6201: add the basis for a future event report

---
+++ lib/MusicBrainz/Server/Report/EventReport.pm
@@ -0,0 +1,47 @@
+package MusicBrainz::Server::Report::EventReport;
+use Moose::Role;
+
+with 'MusicBrainz::Server::Report::QueryReport';
+
+around inflate_rows => sub {
+    my $orig = shift;
+    my $self = shift;
+
+    my $items = $self->$orig(@_);
+
+    my $events = $self->c->model('Event')->get_by_ids(
+        map { $_->{event_id} } @$items
+    );
+    $self->$c->model('Event')->load_related_info(values %$events);
+
+    return [
+        map +{
+            %$_,
+            event => $events->{ $_->{event_id} }
+        },
+            @$items
+    ];
+};
+
+1;
+
+=head1 COPYRIGHT
+
+Copyright (C) 2009 MetaBrainz Foundation
+Copyright (C) 2014 MetaBrainz Foundation
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+=cut
