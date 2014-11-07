16c01652afcb58e18670fda9a1e246e74023fc34
reosarevok <reosarevok@rika.musicbrainz.org>
2014-11-07 20:34:19 +0000

    mbs-6201: script to add default collections to every editor

---
+++ admin/sql/updates/20141107-default-event-collections.sql
@@ -0,0 +1,9 @@
+\set ON_ERROR_STOP 1
+BEGIN;
+
+INSERT INTO editor_collection (editor, name, public, description, type) 
+   SELECT (id, "Attending", 1, 'A list of events I attended or plan to attend', 5) FROM editor WHERE deleted = 0;
+INSERT INTO editor_collection (editor, name, public, description, type) 
+   SELECT (id, "Maybe attending", 1, 'A list of events I might attend', 6) FROM editor WHERE deleted = 0;
+
+COMMIT;
