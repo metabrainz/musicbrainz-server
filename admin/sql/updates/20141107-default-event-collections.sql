12452e41c1e1d1aa84ac6ac5493516f90b1393c7
reosarevok <reosarevok@rika.musicbrainz.org>
2014-11-07 20:37:45 +0000

    single quotes, duh

--- admin/sql/updates/20141107-default-event-collections.sql
+++ admin/sql/updates/20141107-default-event-collections.sql
@@ -2,8 +2,8 @@
 BEGIN;
 
 INSERT INTO editor_collection (editor, name, public, description, type) 
-   SELECT (id, "Attending", 1, 'A list of events I attended or plan to attend', 5) FROM editor WHERE deleted = 0;
+   SELECT (id, 'Attending', 1, 'A list of events I attended or plan to attend', 5) FROM editor WHERE deleted = 0;
 INSERT INTO editor_collection (editor, name, public, description, type) 
-   SELECT (id, "Maybe attending", 1, 'A list of events I might attend', 6) FROM editor WHERE deleted = 0;
+   SELECT (id, 'Maybe attending', 1, 'A list of events I might attend', 6) FROM editor WHERE deleted = 0;
 
 COMMIT;
