72aeb5a0a2781dcf1033ba037ab9321c3c865914
reosarevok <reosarevok@rika.musicbrainz.org>
2014-11-11 03:41:20 +0000

    gid, small fixes

--- admin/sql/updates/20141107-default-event-collections.sql
+++ admin/sql/updates/20141107-default-event-collections.sql
@@ -1,9 +1,9 @@
 \set ON_ERROR_STOP 1
 BEGIN;
 
-INSERT INTO editor_collection (editor, name, public, description, type) 
-   SELECT (id, 'Attending', 1, 'A list of events I attended or plan to attend', 5) FROM editor WHERE deleted = 0;
-INSERT INTO editor_collection (editor, name, public, description, type) 
-   SELECT (id, 'Maybe attending', 1, 'A list of events I might attend', 6) FROM editor WHERE deleted = 0;
+INSERT INTO editor_collection (editor, gid, name, public, description, type) 
+   SELECT id, generate_uuid_v3('6ba7b8119dad11d180b400c04fd430c8', 'http://musicbrainz.org/collection/attending'),'Attending', TRUE, 'A list of events I attended or plan to attend', 5 FROM editor WHERE NOT deleted;
+INSERT INTO editor_collection (editor, gid, name, public, description, type) 
+   SELECT id, generate_uuid_v3('6ba7b8119dad11d180b400c04fd430c8', 'http://musicbrainz.org/collection/maybeattending'), 'Maybe attending', TRUE, 'A list of events I might attend', 6 FROM editor WHERE NOT deleted;
 
 COMMIT;
