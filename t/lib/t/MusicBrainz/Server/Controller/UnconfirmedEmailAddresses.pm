9e9620c80e153871636386860b39ef4f1b25cb3b
Ian McEwen <ianmcorvidae@ianmcorvidae.net>
2014-12-22 14:54:24 -0700

    Fix UnconfirmedEmailAddresses

--- t/lib/t/MusicBrainz/Server/Controller/UnconfirmedEmailAddresses.pm
+++ t/lib/t/MusicBrainz/Server/Controller/UnconfirmedEmailAddresses.pm
@@ -195,7 +195,6 @@ test 'Paths that allow browsing without a confirmed email address' => sub {
   "Controller::ISWC::base",
   "Controller::ISWC::show",
   "Controller::JS::js_text_setup",
-  "Controller::JS::js_text_strings",
   "Controller::JS::statistics_js_text_strings",
   "Controller::Label::alias",
   "Controller::Label::aliases",
