a0f81cfcc81a65cf40e139a3fabca00954f2815f
Ulrich Klauer <ulrich@chirlu.de>
2015-01-21 00:23:59 +0100

    MBS-8159: Show full RG type in recording search results
    Recording search results from the search server include release group
    information, in particular the types, but only the primary type was
    parsed from the search server response and hence displayed. Use the
    existing code (for releases and release groups) to also parse the
    secondary types.

--- lib/MusicBrainz/Server/Data/Search.pm
+++ lib/MusicBrainz/Server/Data/Search.pm
@@ -567,9 +567,7 @@ sub schema_fixup
                 ) ]
             );
             my $release_group = MusicBrainz::Server::Entity::ReleaseGroup->new(
-                primary_type => MusicBrainz::Server::Entity::ReleaseGroupType->new(
-                    name => $release->{"release-group"}->{"primary-type"} || ''
-                )
+                fixup_rg($release->{"release-group"})
             );
             push @releases, MusicBrainz::Server::Entity::Release->new(
                 gid     => $release->{id},
