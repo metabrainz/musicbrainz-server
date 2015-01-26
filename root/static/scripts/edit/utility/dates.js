a6e3c4b8bfc115f47db0521d7c5643563a9a3a4d
Michael Wiencek <mwtuea@gmail.com>
2015-01-26 15:20:51 -0600

    Fix copyright dates to the best of my ability

--- root/static/scripts/edit/utility/dates.js
+++ root/static/scripts/edit/utility/dates.js
@@ -1,5 +1,5 @@
 // This file is part of MusicBrainz, the open internet music database.
-// Copyright (C) 2015 MetaBrainz Foundation
+// Copyright (C) 2012-2014 MetaBrainz Foundation
 // Licensed under the GPL version 2, or (at your option) any later version:
 // http://www.gnu.org/licenses/gpl-2.0.txt
 
