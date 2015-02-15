175fae6f34104b43a977cd5fd66d46c9ef1efd0c
Ulrich Klauer <ulrich@chirlu.de>
2015-02-15 13:42:21 +0100

    MBS-8224: Allow rating events via the web service
    The controller for POST requests to /ws/2/rating used a hardcoded list
    of entity types that support ratings, from which events were missing.
    Use the information from %ENTITIES instead.

--- lib/MusicBrainz/Server/Controller/WS/2/Rating.pm
+++ lib/MusicBrainz/Server/Controller/WS/2/Rating.pm
@@ -3,6 +3,7 @@ use Moose;
 BEGIN { extends 'MusicBrainz::Server::ControllerBase::WS::2' }
 
 use aliased 'MusicBrainz::Server::WebService::WebServiceStash';
+use MusicBrainz::Server::Constants qw( entities_with );
 use MusicBrainz::Server::Data::Utils qw( type_to_model );
 use MusicBrainz::Server::WebService::XML::XPath;
 use MusicBrainz::Server::Validation qw( is_guid );
@@ -25,6 +26,9 @@ with 'MusicBrainz::Server::WebService::Validator' =>
      defs => $ws_defs,
 };
 
+Readonly our %ratable => entities_with('ratings', take =>
+    sub { my ($type, $info) = @_; (($info->{url} // $type) => $info) });
+
 sub rating_submit : Private
 {
     my ($self, $c) = @_;
@@ -34,14 +38,6 @@ sub rating_submit : Private
 
     my $xp = MusicBrainz::Server::WebService::XML::XPath->new( xml => $c->request->body );
 
-    my %ratable = (
-        artist          => 1,
-        label           => 1,
-        recording       => 1,
-        'release-group' => 1,
-        work            => 1,
-    );
-
     my @submit;
     for my $node ($xp->find('/mb:metadata/*/*')->get_nodelist)
     {
@@ -50,17 +46,13 @@ sub rating_submit : Private
                            "Supported types are: " . join(', ', keys %ratable))
             unless $ratable{$type};
 
-        $type =~ s/-/_/;
-
-        my $model = type_to_model($type);
-        $self->_error($c, "Unrecognized entity $type.") unless $model;
-
         my $gid = $xp->find('@mb:id', $node)->string_value;
         $self->_error($c, "Cannot parse MBID: $gid.")
             unless is_guid($gid);
 
+        my $model = $ratable{$type}->{model};
         my $entity = $c->model($model)->get_by_gid($gid);
-        $self->_error($c, "Cannot find $type $gid.") unless $entity;
+        $self->_error($c, "Cannot find $type entity $gid.") unless $entity;
 
         my $rating = $xp->find('mb:user-rating', $node)->string_value;
         $self->_error($c, "Rating should be an integer between 0 and 100")
