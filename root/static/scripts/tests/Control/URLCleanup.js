08f2eb838143c27a01f01c4f696c178afa4756ef
Frederik “Freso” S. Olesen <freso.dk@gmail.com>
2015-02-15 20:10:14 +0100

    Fix "header" for Sina Weibo URL Cleanup tests.

--- root/static/scripts/tests/Control/URLCleanup.js
+++ root/static/scripts/tests/Control/URLCleanup.js
@@ -1412,7 +1412,7 @@ test('Cleanup', function (t) {
                 'http://www.cdjapan.co.jp/product/COCC-72267',
                 'release'
             ],
-            // CDJapan.co.jp
+            // Sina Weibo
             [
                 'www.weibo.com/mchotdog2010#test',
                 'http://weibo.com/mchotdog2010',
