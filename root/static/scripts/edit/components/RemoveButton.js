08715bf14b14d7a9b9fd634d88886d2053a8fdd1
Michael Wiencek <mwtuea@gmail.com>
2015-02-18 11:17:39 -0600

    require('React') -> require('react')

--- root/static/scripts/edit/components/RemoveButton.js
+++ root/static/scripts/edit/components/RemoveButton.js
@@ -3,7 +3,7 @@
 // Licensed under the GPL version 2, or (at your option) any later version:
 // http://www.gnu.org/licenses/gpl-2.0.txt
 
-var React = require('React');
+var React = require('react');
 
 var RemoveButton = React.createClass({
   render: function () {
