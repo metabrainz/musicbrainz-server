e609264e5e96fc10c308794a3dba402209d87b45
Michael Wiencek <mwtuea@gmail.com>
2015-03-12 00:48:12 -0500

    Another server email test that should use https

--- t/lib/t/MusicBrainz/Server/Data/Vote.pm
+++ t/lib/t/MusicBrainz/Server/Data/Vote.pm
@@ -127,7 +127,7 @@ is($email->get_header('References'), sprintf '<edit-%d@%s>', $edit->id, DBDefs->
 is($email->get_header('To'), '"editor1" <editor1@example.com>', 'To header contains editor email');
 
 my $server = DBDefs->WEB_SERVER_USED_IN_EMAIL;
-like($email->get_body, qr{http://$server/edit/${\ $edit->id }}, 'body contains link to edit');
+like($email->get_body, qr{https://$server/edit/${\ $edit->id }}, 'body contains link to edit');
 like($email->get_body, qr{'editor2'}, 'body mentions editor2');
 
 $edit = $test->c->model('Edit')->get_by_id($edit->id);
