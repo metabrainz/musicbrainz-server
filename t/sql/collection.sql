b609df9af785b0ce3995d594961f510d642876be
Jesse Weinstein <jesse@wefu.org>
2015-03-29 21:00:33 -0700

    Move test changes to schema-change branch

--- t/sql/collection.sql
+++ t/sql/collection.sql
@@ -25,14 +25,6 @@ INSERT INTO event (id, gid, name, type) VALUES
        (3, 'bb857d0e-a4bc-11e4-a0f5-23485fda2851', 'Copy of the Better Festival', 1),
        (4, 'e024804e-a4c4-11e4-884d-df918190e80e', 'Another Event', 1);
 
-INSERT INTO work_type (id, name) VALUES (1, 'Composition');
-
-INSERT INTO work (id, gid, name, type, edits_pending, comment) VALUES
-    (1, '745c079d-374e-4436-9448-da92dedef3ce', 'Dancing Queen', 1, 0, 'Work'),
-    (2, '755c079d-374e-4436-9448-da92dedef3cf', 'Test', 1, 0, 'Another Work');
-
-SELECT setval('work_id_seq', (SELECT MAX(id) FROM work));
-
 INSERT INTO editor (id, name, password, ha1, email, email_confirm_date) VALUES
 (1, 'editor1', '{CLEARTEXT}pass', '16a4862191803cb596ee4b16802bb7ee', 'foo@example.com', now()),
 (2, 'editor2', '{CLEARTEXT}pass', 'ba025a52cc5ff57d5d10f31874a83de6', 'foo@example.com', now()),
@@ -40,15 +32,13 @@ INSERT INTO editor (id, name, password, ha1, email, email_confirm_date) VALUES
 
 INSERT INTO editor_collection_type (id, name, entity_type, parent, child_order)
     VALUES (1, 'Release', 'release', NULL, 1),
-           (2, 'Event', 'event', NULL, 2),
-           (3, 'Work', 'work', NULL, 3);
+           (2, 'Event', 'event', NULL, 2);
 
 INSERT INTO editor_collection (id, gid, editor, name, public, description, type)
     VALUES (1, 'f34c079d-374e-4436-9448-da92dedef3cd', 1, 'collection1', FALSE, '', 1),
            (2, 'f34c079d-374e-4436-9448-da92dedef3cb', 2, 'collection2', TRUE, 'Testy!', 1),
            (3, 'f34c079d-374e-4436-9448-da92dedef3c9', 1, 'event_collection1', FALSE, '', 2),
-           (4, '24375a7a-a4bd-11e4-a92c-3b5e54a633eb', 1, 'event_collection2', FALSE, '', 2),
-           (5, '24375a7a-a4bd-11e4-a92c-3b5e54a633ec', 1, 'work_collection', FALSE, '', 3);
+           (4, '24375a7a-a4bd-11e4-a92c-3b5e54a633eb', 1, 'event_collection2', FALSE, '', 2);
 
 SELECT setval('editor_collection_id_seq', (SELECT MAX(id) FROM editor_collection));
 
@@ -57,9 +47,6 @@ INSERT INTO editor_collection_release (collection, release)
 
 INSERT INTO editor_collection_event (collection, event)
     VALUES (3, 2), (4, 3), (3, 4), (4, 4);
-
-INSERT INTO editor_collection_work (collection, work)
-    VALUES (5, 1), (5, 2);
 
 INSERT INTO editor_subscribe_collection (id, editor, collection, last_edit_sent, available, last_seen_name)
     VALUES (1, 2, 1, 0, FALSE, 'collection1'),
