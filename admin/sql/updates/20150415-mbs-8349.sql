6b4328c2b87c017afc724694ad23f4915251b8c6
ianmcorvidae <ianmcorvidae@ianmcorvidae.net>
2015-05-04 14:09:30 -0700

    MBS-8349: drop new indexes in the rare case they exist, in upgrade script (so there's no error about it already existing); fix syntax error.

--- admin/sql/updates/20150415-mbs-8349.sql
+++ admin/sql/updates/20150415-mbs-8349.sql
@@ -3,9 +3,13 @@ BEGIN;
 
 -- Drop old indexes where they exist.
 DROP INDEX IF EXISTS track_idx_medium;
-DROP INDEX IF EXISTS track_idx_uniq_medium_position
+DROP INDEX IF EXISTS track_idx_uniq_medium_position;
 DROP INDEX IF EXISTS medium_idx_release;
 DROP INDEX IF EXISTS medium_idx_uniq;
+
+-- Drop new indexes in the case of very new slave databases
+DROP INDEX IF EXISTS medium_idx_release_position;
+DROP INDEX IF EXISTS track_idx_medium_position;
 
 -- Drop new constraints (should only exist if they were created manually).
 ALTER TABLE track DROP CONSTRAINT IF EXISTS track_uniq_medium_position;
