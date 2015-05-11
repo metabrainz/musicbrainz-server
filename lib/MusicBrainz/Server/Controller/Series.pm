9f16254859a69cf65eebbb2feb77092cf91a9f43
Michael Wiencek <mwtuea@gmail.com>
2015-05-11 13:18:27 -0500

    MBS-8384: Series-series relationships aren't displayed

--- lib/MusicBrainz/Server/Controller/Series.pm
+++ lib/MusicBrainz/Server/Controller/Series.pm
@@ -14,7 +14,11 @@ BEGIN { extends 'MusicBrainz::Server::Controller'; }
 with 'MusicBrainz::Server::Controller::Role::Load' => {
     model           => 'Series',
     entity_name     => 'series',
-    relationships   => { cardinal => ['edit'], subset => { show => ['artist', 'url'] }, default => ['url'] },
+    relationships => {
+        cardinal => ['edit'],
+        subset => {show => ['artist', 'series', 'url']},
+        default => ['url']
+    },
 };
 with 'MusicBrainz::Server::Controller::Role::LoadWithRowID';
 with 'MusicBrainz::Server::Controller::Role::Annotation';
