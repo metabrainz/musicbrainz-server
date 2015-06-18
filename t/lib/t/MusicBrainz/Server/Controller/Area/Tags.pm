852165a2cab5a252283acaa6178178924f46b6e2
Michael Wiencek <mwtuea@gmail.com>
2015-06-18 12:25:02 -0500

    Test /tags -> /tags/upvote for submission

--- t/lib/t/MusicBrainz/Server/Controller/Area/Tags.pm
+++ t/lib/t/MusicBrainz/Server/Controller/Area/Tags.pm
@@ -16,13 +16,7 @@ test all => sub {
     $mech->submit_form( with_fields => { username => 'new_editor', password => 'password' } );
 
     # Test tagging
-    $mech->get_ok('/area/489ce91b-6658-3307-9877-795b68554c98/tags');
-    html_ok($mech->content);
-    my $response = $mech->submit_form(
-        with_fields => {
-            'tag.tags' => 'Broken, Fixme',
-        }
-    );
+    $mech->get_ok('/area/489ce91b-6658-3307-9877-795b68554c98/tags/upvote?tags=Broken+Fixme');
     $mech->get_ok('/area/489ce91b-6658-3307-9877-795b68554c98/tags');
     html_ok($mech->content);
 
