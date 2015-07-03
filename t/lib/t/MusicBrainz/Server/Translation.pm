89d725556fca0ef18e70c3c8b708a933ab473981
Michael Wiencek <mwtuea@gmail.com>
2015-07-03 18:36:32 -0500

    Use expand directly to prevent double-translation
    See https://bitbucket.org/metabrainz/musicbrainz-server/pull-request/1398/allow-using-entity0-and-entity1-as/activity#comment-4395280

--- t/lib/t/MusicBrainz/Server/Translation.pm
+++ t/lib/t/MusicBrainz/Server/Translation.pm
@@ -6,19 +6,19 @@ use Test::More;
 use MusicBrainz::Server::Translation;
 
 test 'Check _expand_link' => sub {
-    is(MusicBrainz::Server::Translation->instance->_expand('An {apple_fruit}',
+    is(MusicBrainz::Server::Translation->instance->expand('An {apple_fruit}',
                                                   apple_fruit => 'apple'),
         'An apple', 'Simple replacement');
-    is(MusicBrainz::Server::Translation->instance->_expand('An {apple_fruit|Apple}',
+    is(MusicBrainz::Server::Translation->instance->expand('An {apple_fruit|Apple}',
                                                   apple_fruit => 'http://www.apple.com'),
         'An <a href="http://www.apple.com">Apple</a>', 'Replacement with links');
-    is(MusicBrainz::Server::Translation->instance->_expand('A {apple_fruit|apple}',
+    is(MusicBrainz::Server::Translation->instance->expand('A {apple_fruit|apple}',
                                                   apple_fruit => 'http://www.apple.com', apple => "pear"),
         'A <a href="http://www.apple.com">pear</a>', 'Replacement with link description evaluation');
-    is(MusicBrainz::Server::Translation->instance->_expand('A {apple_fruit|apple}',
+    is(MusicBrainz::Server::Translation->instance->expand('A {apple_fruit|apple}',
                                                   apple_fruit => {href => 'http://www.apple.com', target => '_blank'}, apple => "pear"),
         'A <a href="http://www.apple.com" target="_blank">pear</a>', 'Replacement with link description evaluation and hash argument');
-    is(MusicBrainz::Server::Translation->instance->_expand('A {apple_fruit|{condition} apple …}',
+    is(MusicBrainz::Server::Translation->instance->expand('A {apple_fruit|{condition} apple …}',
                                                   apple_fruit => {href => 'http://www.apple.com', target => '_blank'}, condition => "bad"),
         'A <a href="http://www.apple.com" target="_blank">bad apple …</a>', 'Replacement with link description evaluation and nested hash argument');
 };
