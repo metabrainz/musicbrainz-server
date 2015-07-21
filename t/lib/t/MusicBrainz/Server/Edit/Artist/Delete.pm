12e3f11adfffb49ed9e5b31ef170aa247e9d24d5
Michael Wiencek <mwtuea@gmail.com>
2015-07-20 21:55:49 -0500

    Fix indentation

--- t/lib/t/MusicBrainz/Server/Edit/Artist/Delete.pm
+++ t/lib/t/MusicBrainz/Server/Edit/Artist/Delete.pm
@@ -12,56 +12,51 @@ use MusicBrainz::Server::Constants ':edit_status';
 use MusicBrainz::Server::Test qw( accept_edit reject_edit );
 
 test all => sub {
+    my $test = shift;
+    my $c = $test->c;
 
-my $test = shift;
-my $c = $test->c;
+    MusicBrainz::Server::Test->prepare_test_database($c, '+edit_artist_delete');
 
-MusicBrainz::Server::Test->prepare_test_database($c, '+edit_artist_delete');
+    my $artist = $c->model('Artist')->get_by_id(3);
 
-my $artist = $c->model('Artist')->get_by_id(3);
+    my $edit = _create_edit($c, $artist);
+    isa_ok($edit, 'MusicBrainz::Server::Edit::Artist::Delete');
 
-my $edit = _create_edit($c, $artist);
-isa_ok($edit, 'MusicBrainz::Server::Edit::Artist::Delete');
+    my ($edits, $hits) = $c->model('Edit')->find({ artist => 3 }, 10, 0);
+    is($hits, 1);
+    is($edits->[0]->id, $edit->id);
 
-my ($edits, $hits) = $c->model('Edit')->find({ artist => 3 }, 10, 0);
-is($hits, 1);
-is($edits->[0]->id, $edit->id);
+    $artist = $c->model('Artist')->get_by_id(3);
+    is($artist->edits_pending, 1);
 
-$artist = $c->model('Artist')->get_by_id(3);
-is($artist->edits_pending, 1);
+    # Test rejecting the edit
+    reject_edit($c, $edit);
+    $artist = $c->model('Artist')->get_by_id(3);
+    ok(defined $artist);
+    is($artist->edits_pending, 0);
 
-# Test rejecting the edit
-reject_edit($c, $edit);
-$artist = $c->model('Artist')->get_by_id(3);
-ok(defined $artist);
-is($artist->edits_pending, 0);
+    # Test accepting the edit
+    # This should fail as the artist has a recording linked
+    $edit = _create_edit($c, $artist);
+    accept_edit($c, $edit);
+    $artist = $c->model('Artist')->get_by_id(3);
+    is($edit->status, $STATUS_FAILEDDEP);
+    ok(defined $artist);
 
-# Test accepting the edit
-# This should fail as the artist has a recording linked
-$edit = _create_edit($c, $artist);
-accept_edit($c, $edit);
-$artist = $c->model('Artist')->get_by_id(3);
-is($edit->status, $STATUS_FAILEDDEP);
-ok(defined $artist);
+    # Delete the recording and enter the edit
+    my $sql = $c->sql;
+    Sql::run_in_transaction(sub { $c->model('Recording')->delete(1) }, $sql);
 
-# Delete the recording and enter the edit
-my $sql = $c->sql;
-Sql::run_in_transaction(
-    sub {
-        $c->model('Recording')->delete(1);
-    }, $sql);
+    $edit = _create_edit($c, $artist);
+    accept_edit($c, $edit);
+    $artist = $c->model('Artist')->get_by_id(3);
+    ok(!defined $artist);
 
-$edit = _create_edit($c, $artist);
-accept_edit($c, $edit);
-$artist = $c->model('Artist')->get_by_id(3);
-ok(!defined $artist);
+    my $ipi_codes = $c->model('Artist')->ipi->find_by_entity_id(3);
+    is(scalar @$ipi_codes, 0, "IPI codes for deleted artist removed from database");
 
-my $ipi_codes = $c->model('Artist')->ipi->find_by_entity_id(3);
-is(scalar @$ipi_codes, 0, "IPI codes for deleted artist removed from database");
-
-my $isni_codes = $c->model('Artist')->isni->find_by_entity_id(3);
-is(scalar @$isni_codes, 0, "ISNI codes for deleted artist removed from database");
-
+    my $isni_codes = $c->model('Artist')->isni->find_by_entity_id(3);
+    is(scalar @$isni_codes, 0, "ISNI codes for deleted artist removed from database");
 };
 
 test 'Can be entered as an auto-edit' => sub {
--- t/lib/t/MusicBrainz/Server/Edit/Label/Delete.pm
+++ t/lib/t/MusicBrainz/Server/Edit/Label/Delete.pm
@@ -12,45 +12,43 @@ use MusicBrainz::Server::Constants qw( $EDIT_LABEL_DELETE );
 use MusicBrainz::Server::Test qw( accept_edit reject_edit );
 
 test all => sub {
+    my $test = shift;
+    my $c = $test->c;
 
-my $test = shift;
-my $c = $test->c;
+    MusicBrainz::Server::Test->prepare_test_database($c, '+edit_label_delete');
 
-MusicBrainz::Server::Test->prepare_test_database($c, '+edit_label_delete');
+    my $label = $c->model('Label')->get_by_id(2);
 
-my $label = $c->model('Label')->get_by_id(2);
+    my $edit = create_edit($c, $label);
+    isa_ok($edit, 'MusicBrainz::Server::Edit::Label::Delete');
 
-my $edit = create_edit($c, $label);
-isa_ok($edit, 'MusicBrainz::Server::Edit::Label::Delete');
+    my ($edits, $hits) = $c->model('Edit')->find({ label => $label->id }, 10, 0);
+    is($hits, 1);
+    is($edits->[0]->id, $edit->id);
 
-my ($edits, $hits) = $c->model('Edit')->find({ label => $label->id }, 10, 0);
-is($hits, 1);
-is($edits->[0]->id, $edit->id);
+    $edit = $c->model('Edit')->get_by_id($edit->id);
+    $label = $c->model('Label')->get_by_id(2);
+    is($label->edits_pending, 1);
 
-$edit = $c->model('Edit')->get_by_id($edit->id);
-$label = $c->model('Label')->get_by_id(2);
-is($label->edits_pending, 1);
+    reject_edit($c, $edit);
+    $label = $c->model('Label')->get_by_id(2);
+    is($label->edits_pending, 0);
 
-reject_edit($c, $edit);
-$label = $c->model('Label')->get_by_id(2);
-is($label->edits_pending, 0);
+    $edit = create_edit($c, $label);
+    accept_edit($c, $edit);
+    $label = $c->model('Label')->get_by_id(2);
+    ok(!defined $label);
 
-$edit = create_edit($c, $label);
-accept_edit($c, $edit);
-$label = $c->model('Label')->get_by_id(2);
-ok(!defined $label);
+    my $ipi_codes = $c->model('Artist')->ipi->find_by_entity_id(2);
+    is(scalar @$ipi_codes, 0, "IPI codes for deleted label removed from database");
 
-my $ipi_codes = $c->model('Artist')->ipi->find_by_entity_id(2);
-is(scalar @$ipi_codes, 0, "IPI codes for deleted label removed from database");
-
-my $isni_codes = $c->model('Artist')->isni->find_by_entity_id(2);
-is(scalar @$isni_codes, 0, "ISNI codes for deleted label removed from database");
-
+    my $isni_codes = $c->model('Artist')->isni->find_by_entity_id(2);
+    is(scalar @$isni_codes, 0, "ISNI codes for deleted label removed from database");
 };
 
 sub create_edit {
     my ($c, $label) = @_;
-    return  $c->model('Edit')->create(
+    return $c->model('Edit')->create(
         edit_type => $EDIT_LABEL_DELETE,
         to_delete => $label,
         editor_id => 1
