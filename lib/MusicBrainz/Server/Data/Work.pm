f6a87e3228d1bcdf989b981363e792049e2d14d6
Michael Wiencek <mwtuea@gmail.com>
2015-07-24 03:13:05 -0500

    Merging a work that's in a collection should work

--- lib/MusicBrainz/Server/Data/Work.pm
+++ lib/MusicBrainz/Server/Data/Work.pm
@@ -171,6 +171,7 @@ sub _merge_impl
     $self->tags->merge($new_id, @old_ids);
     $self->rating->merge($new_id, @old_ids);
     $self->c->model('Edit')->merge_entities('work', $new_id, @old_ids);
+    $self->c->model('Collection')->merge_entities('work', $new_id, @old_ids);
     $self->c->model('Relationship')->merge_entities('work', $new_id, \@old_ids);
     $self->c->model('ISWC')->merge_works($new_id, @old_ids);
 
