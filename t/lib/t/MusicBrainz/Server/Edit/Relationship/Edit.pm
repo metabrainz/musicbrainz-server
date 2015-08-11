metabrainz/musicbrainz-server

commit 88c0e9358d8c8cf81d5298dd0494ae58c7833622

Relationship credits don't determine uniqueness
This is wrong because it makes the subroutine return 0 for values that would
otherwise cause a duplicate entry on (entity0, entity1, link, link_order).

lib/MusicBrainz/Server/Data/Relationship.pm
@@ -511,17 +511,8 @@ sub exists {
     return 0 unless $link;
     push @values, $link;
 
-    for (qw(entity0_credit entity1_credit)) {
-        if (exists $values->{$_}) {
-            push @props, $_;
-            push @values, $values->{$_};
-        }
-    }
-
-    my $conditions = join(' AND ', map { "$_ = ?" } @props);
-
     return $self->sql->select_single_value(
-        "SELECT 1 FROM l_${type0}_${type1} WHERE $conditions",
+        "SELECT 1 FROM l_${type0}_${type1} WHERE " . join(' AND ', map { "$_ = ?" } @props),
         @values
     );
 }

commit cb510c5ef2868777079e03930b108a9f5082635e

MBS-8514: relationship credits can't be edited
This issue was introduced in 88c0e9358d8c8cf81d5298dd0494ae58c7833622. While
that commit's reasoning is correct, Edit::Relationship::Edit was using
`exists` to mean "including entity credits," because otherwise, the
relationship being edited would always be a duplicate of itself if only the
credits were changed.

This modifies `exists` to return the ID of the relationship that exists, and
makes Edit::Relationship::Edit check if the ID differs from the relationship
being edited.

lib/MusicBrainz/Server/Data/Relationship.pm
@@ -492,6 +492,16 @@ sub delete_entities
     }
 }
 
+=method exists
+
+Checks if a relationship with the given values already exists. This doesn't
+consider entity credits, because those don't determine uniqueness at the
+database level.
+
+Returns a relationship ID, if one exists.
+
+=cut
+
 sub exists {
     my ($self, $type0, $type1, $values) = @_;
 
@@ -512,7 +522,7 @@ sub exists {
     push @values, $link;
 
     return $self->sql->select_single_value(
-        "SELECT 1 FROM l_${type0}_${type1} WHERE " . join(' AND ', map { "$_ = ?" } @props),
+        "SELECT id FROM l_${type0}_${type1} WHERE " . join(' AND ', map { "$_ = ?" } @props),
         @values
     );
 }

lib/MusicBrainz/Server/Edit/Relationship/Edit.pm
@@ -362,21 +362,22 @@ sub initialize
         long_link_phrase => $opts{link_type}->long_link_phrase
     } if $opts{link_type};
 
-    MusicBrainz::Server::Edit::Exceptions::NoChanges->throw
-        if $self->c->model('Relationship')->exists(
-            $new_link_type->entity0_type,
-            $new_link_type->entity1_type, {
-            link_type_id => $new_link_type->id,
-            begin_date   => $opts{begin_date},
-            end_date     => $opts{end_date},
-            ended        => $opts{ended},
-            attributes   => $opts{attributes},
-            entity0_id   => $new_entity0->id,
-            entity1_id   => $new_entity1->id,
-            link_order   => $relationship->link_order,
-            entity0_credit  => $opts{entity0_credit},
-            entity1_credit  => $opts{entity1_credit},
-        });
+    my $existent_id = $self->c->model('Relationship')->exists(
+        $new_link_type->entity0_type,
+        $new_link_type->entity1_type, {
+        link_type_id => $new_link_type->id,
+        begin_date   => $opts{begin_date},
+        end_date     => $opts{end_date},
+        ended        => $opts{ended},
+        attributes   => $opts{attributes},
+        entity0_id   => $new_entity0->id,
+        entity1_id   => $new_entity1->id,
+        link_order   => $relationship->link_order,
+    });
+
+    if ($existent_id && $relationship->id != $existent_id) {
+        MusicBrainz::Server::Edit::Exceptions::NoChanges->throw;
+    }
 
     $self->relationship($relationship);
     $self->data({

t/lib/t/MusicBrainz/Server/Edit/Relationship/Edit.pm
@@ -402,6 +402,76 @@ test 'Edits that change endpoints are auto-editable by auto-editors' => sub {
     ok(!$edit->is_open, 'edit changing an endpoint was applied immediately by an auto-editor');
 };
 
+test 'Entity credits can be added to an existing relationship' => sub {
+    my $test = shift;
+    my $c = $test->c;
+
+    MusicBrainz::Server::Test->prepare_test_database($c, '+edit_relationship_edit');
+
+    my $relationship = $c->model('Relationship')->get_by_id('artist', 'artist', 1);
+    $c->model('Link')->load($relationship);
+    $c->model('LinkType')->load($relationship->link);
+
+    my $edit = $c->model('Edit')->create(
+        edit_type => $EDIT_RELATIONSHIP_EDIT,
+        editor_id => 1,
+        relationship => $relationship,
+        entity0_credit => 'Foo Credit',
+        entity1_credit => 'Bar Credit',
+    );
+
+    is_deeply($edit->data, {
+      edit_version => 2,
+      link => {
+            attributes => [
+                {
+                    type => {
+                        id => 1,
+                        gid => '7610b0e9-40c1-48b3-b06c-2c1d30d9dc3e',
+                        name => 'Attribute',
+                        root => {
+                            gid => '7610b0e9-40c1-48b3-b06c-2c1d30d9dc3e',
+                            id => 1,
+                            name => 'Attribute'
+                        }
+                    }
+                }
+            ],
+            begin_date => { month => undef, day => undef, year => undef },
+            end_date => { year => undef, month => undef, day => undef },
+            ended => '0',
+            entity0 => {
+               name => 'Artist 1',
+               gid => '945c079d-374e-4436-9448-da92dedef3cf',
+               id => 3
+            },
+            entity1 => {
+               gid => '75a40343-ff6e-45d6-a5d2-110388d34858',
+               id => 4,
+               name => 'Artist 2'
+            },
+           link_type => {
+                id => 1,
+                link_phrase => 'member',
+                long_link_phrase => 'f',
+                name => 'member',
+                reverse_link_phrase => 'oof',
+            },
+        },
+        new => {
+            entity1_credit => 'Bar Credit',
+            entity0_credit => 'Foo Credit'
+        },
+        old => {
+            entity1_credit => '',
+            entity0_credit => ''
+        },
+        relationship_id => 1,
+        type1 => 'artist',
+        type0 => 'artist',
+    });
+};
+
 sub _create_edit {
     my ($c, %args) = @_;
 