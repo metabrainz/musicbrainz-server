22ddf1925854e50fcc2e10f208161161bb539c88
Michael Wiencek <mwtuea@gmail.com>
2015-08-12 17:56:29 -0500

    Fix indentation

--- t/lib/t/MusicBrainz/Server/Edit/Release/EditReleaseLabel.pm
+++ t/lib/t/MusicBrainz/Server/Edit/Release/EditReleaseLabel.pm
@@ -15,47 +15,45 @@ use MusicBrainz::Server::Constants qw(
 use MusicBrainz::Server::Test qw( accept_edit reject_edit );
 
 test all => sub {
+    my $test = shift;
+    my $c = $test->c;
 
-my $test = shift;
-my $c = $test->c;
+    MusicBrainz::Server::Test->prepare_test_database($c, '+edit_release_label');
 
-MusicBrainz::Server::Test->prepare_test_database($c, '+edit_release_label');
+    my $rl = $c->model('ReleaseLabel')->get_by_id(1);
 
-my $rl = $c->model('ReleaseLabel')->get_by_id(1);
+    my $edit = create_edit($c, $rl);
+    isa_ok($edit, 'MusicBrainz::Server::Edit::Release::EditReleaseLabel');
 
-my $edit = create_edit($c, $rl);
-isa_ok($edit, 'MusicBrainz::Server::Edit::Release::EditReleaseLabel');
+    my ($edits) = $c->model('Edit')->find({ release => 1 }, 10, 0);
+    is(scalar @$edits, 1);
+    is($edits->[0]->id, $edit->id);
 
-my ($edits) = $c->model('Edit')->find({ release => 1 }, 10, 0);
-is(scalar @$edits, 1);
-is($edits->[0]->id, $edit->id);
+    my $release = $c->model('Release')->get_by_id(1);
+    is($release->edits_pending, 1);
 
-my $release = $c->model('Release')->get_by_id(1);
-is($release->edits_pending, 1);
+    $rl = $c->model('ReleaseLabel')->get_by_id(1);
+    is($rl->label_id, 1);
+    is($rl->catalog_number, 'ABC-123');
 
-$rl = $c->model('ReleaseLabel')->get_by_id(1);
-is($rl->label_id, 1);
-is($rl->catalog_number, 'ABC-123');
+    reject_edit($c, $edit);
 
-reject_edit($c, $edit);
+    $rl = $c->model('ReleaseLabel')->get_by_id(1);
+    is($rl->label_id, 1);
+    is($rl->catalog_number, 'ABC-123');
 
-$rl = $c->model('ReleaseLabel')->get_by_id(1);
-is($rl->label_id, 1);
-is($rl->catalog_number, 'ABC-123');
+    $release = $c->model('Release')->get_by_id($rl->release_id);
+    is($release->edits_pending, 0);
 
-$release = $c->model('Release')->get_by_id($rl->release_id);
-is($release->edits_pending, 0);
+    $edit = create_edit($c, $rl);
+    accept_edit($c, $edit);
 
-$edit = create_edit($c, $rl);
-accept_edit($c, $edit);
+    $rl = $c->model('ReleaseLabel')->get_by_id(1);
+    is($rl->label_id, 2);
+    is($rl->catalog_number, 'FOO');
 
-$rl = $c->model('ReleaseLabel')->get_by_id(1);
-is($rl->label_id, 2);
-is($rl->catalog_number, 'FOO');
-
-$release = $c->model('Release')->get_by_id($rl->release_id);
-is($release->edits_pending, 0);
-
+    $release = $c->model('Release')->get_by_id($rl->release_id);
+    is($release->edits_pending, 0);
 };
 
 test 'Editing the label can fail as a conflict' => sub {
