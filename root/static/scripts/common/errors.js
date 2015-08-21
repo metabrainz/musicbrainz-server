3ec3350b638effcec540d146b7d7d192769cfbc5
Michael Wiencek <mwtuea@gmail.com>
2015-08-21 16:01:29 -0500

    Remove unnecessary IIFE and run eslint on errors.js

--- root/static/scripts/common/errors.js
+++ root/static/scripts/common/errors.js
@@ -6,76 +6,73 @@
 var parseStack = require('parse-stack');
 var request = require('./utility/request.js');
 
-(function () {
-    // https://wiki.musicbrainz.org/Development/Supported_browsers
-    var browser = $.browser,
-        browserVersion = browser.version,
-        browserIsSupported = (
-            (browser.safari && browserVersion >= "5.1") ||
-            (browser.chrome && browserVersion >= "31") ||
-            (browser.msie && browserVersion >= "8.0") ||
-            (browser.edge) ||
-            (browser.mozilla && browserVersion >= "24") ||
-            (browser.opera && browserVersion >= "12.10")
-        );
+// https://wiki.musicbrainz.org/Development/Supported_browsers
+var browser = $.browser;
+var browserVersion = browser.version;
+var browserIsSupported = (
+  (browser.safari && browserVersion >= "5.1") ||
+  (browser.chrome && browserVersion >= "31") ||
+  (browser.msie && browserVersion >= "8.0") ||
+  (browser.edge) ||
+  (browser.mozilla && browserVersion >= "24") ||
+  (browser.opera && browserVersion >= "12.10")
+);
 
-    if (!browserIsSupported) {
-        var notice = "Unsupported browser detected: " + window.navigator.userAgent;
-        if (window.console) {
-            console.log(notice);
-        } else if (window.opera) {
-            opera.postError(notice);
-        }
-        return;
+if (browserIsSupported) {
+  let location = window.location;
+  let origin = location.origin || (location.protocol + "//" + location.host);
+  let urlRegex = new RegExp("^" + origin + "/static/build/.*\\.js$");
+  let reported = {};
+
+  window.onerror = function (message, url, line, column, error) {
+    if (!urlRegex.test(url)) {
+      return;
     }
 
-    var location = window.location,
-        origin = location.origin || (location.protocol + "//" + location.host),
-        urlRegex = new RegExp("^" + origin + "/static/build/.*\\.js$"),
-        reported = {};
+    message += "\n\nURL: " + url + "\nLine: " + line;
 
-    window.onerror = function (message, url, line, column, error) {
-        if (!urlRegex.test(url)) {
-            return;
-        }
+    // Unavailable in Firefox<31 or Opera 12
+    if (column !== undefined) {
+      message += "\nColumn: " + column;
+    }
 
-        message += "\n\nURL: " + url + "\nLine: " + line;
+    // Unavailable in IE<10 or Opera 12
+    let stack;
+    try {
+      stack = parseStack(error);
+    } catch (e) {
+      // https://github.com/lydell/parse-stack/blob/3e1a2d3/lib/parse-stack.js#L46
+    }
 
-        // Unavailable in Firefox<31 or Opera 12
-        if (column !== undefined) {
-            message += "\nColumn: " + column;
-        }
+    if (stack) {
+      // Check that the first (source) file in the stack originates from
+      // root/static/build. This excludes errors from .js files that
+      // userscripts inject into the page. The '.replace' removes line
+      // numbers.
+      let errOrigin = _.last(stack);
+      if (errOrigin && errOrigin.filepath && !urlRegex.test(errOrigin.filepath.replace(/:\d+$/, ''))) {
+        return;
+      }
 
-        // Unavailable in IE<10 or Opera 12
-        var stack;
-        try {
-            stack = parseStack(error);
-        } catch (e) {
-            // https://github.com/lydell/parse-stack/blob/3e1a2d3/lib/parse-stack.js#L46
-        }
+      message += "\n\n" + error.stack;
+    }
 
-        if (stack) {
-            // Check that the first (source) file in the stack originates from
-            // root/static/build. This excludes errors from .js files that
-            // userscripts inject into the page. The '.replace' removes line
-            // numbers.
-            var errOrigin = _.last(stack);
-            if (errOrigin && errOrigin.filepath && !urlRegex.test(errOrigin.filepath.replace(/:\d+$/, ''))) {
-                return;
-            }
+    if (reported[message] === undefined) {
+      reported[message] = true;
 
-            message += "\n\n" + error.stack;
-        }
-
-        if (reported[message] === undefined) {
-            reported[message] = true;
-
-            request({
-                type: "POST",
-                url: "/ws/js/error",
-                data: JSON.stringify({ error: message }),
-                contentType: "application/json; charset=utf-8"
-            });
-        }
-    };
-}());
+      request({
+        type: "POST",
+        url: "/ws/js/error",
+        data: JSON.stringify({error: message}),
+        contentType: "application/json; charset=utf-8"
+      });
+    }
+  };
+} else {
+  let notice = "Unsupported browser detected: " + window.navigator.userAgent;
+  if (window.console) {
+    console.log(notice);
+  } else if (window.opera) {
+    opera.postError(notice);
+  }
+}
