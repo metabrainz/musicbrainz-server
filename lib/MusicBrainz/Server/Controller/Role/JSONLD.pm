3e1fea8a0a67672b1b3896c7a0fc30d99aa13a45
Michael Wiencek <mwtuea@gmail.com>
2015-09-21 21:32:25 -0500

    Return raw JSON-LD if matching accept header is given
    A script will run hourly to check such pages for changes in their JSON-LD
    markup. This allows requesting *just* the markup, making the requests much
    faster and easier to handle.

--- lib/MusicBrainz/Server/Controller/Role/JSONLD.pm
+++ lib/MusicBrainz/Server/Controller/Role/JSONLD.pm
@@ -1,4 +1,5 @@
 package MusicBrainz::Server::Controller::Role::JSONLD;
+use JSON qw( encode_json );
 use MooseX::MethodAttributes::Role;
 use MooseX::Role::Parameterized;
 use MusicBrainz::Server::WebService::Serializer::JSON::LD::Utils qw( serialize_entity );
@@ -27,12 +28,22 @@ role
             my ($self, $c, @args) = @_;
             my $entity = $c->stash->{entity};
             my $stash = $c->stash->{jsonld_stash};
+
             for my $copy (@{ $endpoints->{$endpoint}{copy_stash} // []}) {
                 my $from = ref $copy eq 'HASH' ? $copy->{from} : $copy;
                 my $to = ref $copy eq 'HASH' ? $copy->{to} : $copy;
                 $stash->store($entity)->{$to} = $c->stash->{$from};
             }
-            $c->stash->{jsonld_data} = serialize_entity($entity, undef, $stash, 1);
+
+            my $jsonld_data = serialize_entity($entity, undef, $stash, 1);
+
+            if ($c->req->header('Accept') eq 'application/ld+json') {
+                $c->res->content_type('application/ld+json; charset=utf-8');
+                $c->res->body(encode_json($jsonld_data));
+                $c->detach;
+            } else {
+                $c->stash->{jsonld_data} = $jsonld_data;
+            }
         };
     }
 };
