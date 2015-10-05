eef8adf1edba94158d4c33da0761e32f2fa8c66c
Michael Wiencek <mwtuea@gmail.com>
2015-10-05 18:13:32 -0500

    undef cannot be a default value for Str
    Switch to ''

--- lib/MusicBrainz/Server/Sitemap/Builder.pm
+++ lib/MusicBrainz/Server/Sitemap/Builder.pm
@@ -99,10 +99,10 @@ has replication_access_uri => (
 has current_time => (
     is => 'ro',
     isa => 'Str',
-    default => undef,
+    default => '',
     traits => ['Getopt'],
     cmd_flag => 'current-time',
-    documentation => 'substitute for DateTime::now, for testing purposes (default: undef)',
+    documentation => 'substitute for DateTime::now, for testing purposes (default: \'\')',
 );
 
 has index => (
@@ -278,7 +278,7 @@ sub build_one_sitemap {
     $map->write($local_filename);
     $self->add_sitemap_file($filename);
 
-    my $modtime = $self->current_time // DateTime::Format::W3CDTF->new->format_datetime(DateTime->now);
+    my $modtime = $self->current_time or DateTime::Format::W3CDTF->new->format_datetime(DateTime->now);
     my $old_sitemap_modtimes = $self->old_sitemap_modtimes;
 
     if ($existing_md5 && $existing_md5 eq hash_sitemap($map) && $old_sitemap_modtimes->{$remote_filename}) {
