dc6ea548a8aeaa84866415ae6969482f8ef5a277
Michael Wiencek <mwtuea@gmail.com>
2015-10-06 00:52:29 -0500

    Make requests to localhost, and don't rate limit them
    This is kind of a hack, and the server/port should definitely be configurable,
    but this will get things working on our production server in the meantime.

--- lib/MusicBrainz/Server/Sitemap/Incremental.pm
+++ lib/MusicBrainz/Server/Sitemap/Incremental.pm
@@ -185,10 +185,9 @@ sub fetch_and_handle_jsonld($$$$$$$) {
     state $attempts = {};
     state $canonical_json = JSON->new->canonical->utf8;
 
-    my $web_server = DBDefs->WEB_SERVER;
     my $canonical_server = DBDefs->CANONICAL_SERVER;
     my $request_url = $url;
-    $request_url =~ s{\Q$canonical_server\E}{http://$web_server};
+    $request_url =~ s{\Q$canonical_server\E}{http://localhost:80};
 
     my $response = make_jsonld_request($c, $request_url);
 
