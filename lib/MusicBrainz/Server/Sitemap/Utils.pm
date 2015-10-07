2abdfa59e2e2cd823e949f7b5840bc5729165c17
Michael Wiencek <mwtuea@gmail.com>
2015-10-07 18:43:25 -0500

    Missing copyright

--- lib/MusicBrainz/Server/Sitemap/Utils.pm
+++ lib/MusicBrainz/Server/Sitemap/Utils.pm
@@ -83,3 +83,12 @@ sub log($) {
 }
 
 1;
+
+=head1 COPYRIGHT
+
+This file is part of MusicBrainz, the open internet music database.
+Copyright (C) 2015 MetaBrainz Foundation
+Licensed under the GPL version 2, or (at your option) any later version:
+http://www.gnu.org/licenses/gpl-2.0.txt
+
+=cut
