metabrainz/musicbrainz-server

commit 2d64080751192bb234ee1327ebc41df6db3530d2

Add Data::Role::QueryToList
 * Standardizes the argument order to (limit, offset) everywhere.
 * Moves offset handling to query_to_list*, out from the call sites.
 * Defaults $builder to _new_from_row, so it doesn't have to be specified.
 * Removes Data::Editor::find_by_name, which is unused.

lib/MusicBrainz/Server/Data/CoverArt.pm
@@ -12,7 +12,7 @@ use aliased 'MusicBrainz::Server::Entity::URL';
 
 use DateTime::Format::Pg;
 use List::UtilsBy qw( rev_sort_by );
-use MusicBrainz::Server::Data::Utils qw( placeholders query_to_list );
+use MusicBrainz::Server::Data::Utils qw( placeholders );
 use MusicBrainz::Server::CoverArt;
 
 with 'MusicBrainz::Server::Data::Role::Context';
@@ -131,11 +131,12 @@ sub find_outdated_releases
     ORDER BY last_updated ASC';
 
     my $pg_date_formatter = DateTime::Format::Pg->new;
-    return query_to_list($self->c->sql, sub {
-        my $row = shift;
+    $self->query_to_list($query, [\@url_types, $pg_date_formatter->format_duration($since)], sub {
+        my ($model, $row) = shift;
+
         # Construction of these rows is slow, so this is lazy
         return sub {
-            my $release = $self->c->model('Release')->_new_from_row($row, 'r_');
+            my $release = $model->_new_from_row($row, 'r_');
             $release->cover_art(
                 MusicBrainz::Server::CoverArt->new(
                     $row->{c_last_updated} ? (last_updated => $row->{c_last_updated}) : ()
@@ -153,8 +154,8 @@ sub find_outdated_releases
             );
 
             return $release;
-        }
-    }, $query, @url_types, $pg_date_formatter->format_duration($since));
+        };
+    });
 }
 
 sub cache_cover_art

lib/MusicBrainz/Script/RemoveBarcodeCoverArt.pm
@@ -5,7 +5,7 @@ use DBDefs;
 use DateTime::Duration;
 use MusicBrainz::Server::Context;
 use MusicBrainz::Server::Log qw( log_debug log_notice );
-use MusicBrainz::Server::Data::Utils qw( placeholders query_to_list );
+use MusicBrainz::Server::Data::Utils qw( placeholders );
 
 with 'MooseX::Runnable';
 with 'MooseX::Getopt';
@@ -36,16 +36,13 @@ sub find_releases
       WHERE NOT (url_link_types && ?);
     ";
 
-    return query_to_list($self->c->sql, sub {
-        my $row = shift;
-        return sub {
-            my $release = $self->c->model('Release')->_new_from_row($row, 'r_');
-            $release->cover_art(
-                MusicBrainz::Server::CoverArt->new()
-            );
-            return $release;
-        }
-    }, $query, \@url_types);
+    $self->c->model('Release')->query_to_list($query, [\@url_types], sub {
+        my ($model, $row) = @_;
+
+        my $release = $model->_new_from_row($row, 'r_');
+        $release->cover_art(MusicBrainz::Server::CoverArt->new);
+        $release;
+    });
 }
 
 sub run

lib/MusicBrainz/Server/Controller/WS/1/Track.pm
@@ -175,7 +175,8 @@ sub lookup : Chained('load') PathPart('')
     }
 
     if ($c->stash->{inc}->releases) {
-        my @releases = $c->model('Release')->find_by_recording([ $track->id ]);
+        my ($releases, $hits) = $c->model('Release')->find_by_recording([ $track->id ]);
+        my @releases = @$releases;
         my %releases = map { $_->id => $_ } @releases;
 
         $c->model('ReleaseStatus')->load(@releases);

lib/MusicBrainz/Server/Controller/WS/2/DiscID.pm
@@ -53,7 +53,7 @@ sub discid : Chained('root') PathPart('discid') {
             my $opts = $stash->store($cdtoc);
 
             my @releases = $c->model('Release')->find_by_medium(
-                [ map { $_->medium_id } @mediumcdtocs ], $c->stash->{status}, $c->stash->{type}
+                map { $_->medium_id } @mediumcdtocs
             );
 
             $opts->{releases} = $self->make_list(\@releases);

lib/MusicBrainz/Server/Data/Alias.pm
@@ -7,7 +7,6 @@ use MusicBrainz::Server::Data::Utils qw(
     add_partial_date_to_row
     load_subobjects
     placeholders
-    query_to_list
 );
 use MusicBrainz::Server::Entity::PartialDate;
 

lib/MusicBrainz/Server/Data/Application.pm
@@ -5,7 +5,6 @@ use namespace::autoclean;
 use MusicBrainz::Server::Entity::Application;
 use MusicBrainz::Server::Data::Utils qw(
     load_subobjects
-    query_to_list_limited
     generate_token
 );
 
@@ -59,11 +58,8 @@ sub find_by_owner
     my $query = "SELECT " . $self->_columns . "
                  FROM " . $self->_table . "
                  WHERE owner = ?
-                 ORDER BY id
-                 OFFSET ?";
-    return query_to_list_limited(
-        $self->c->sql, $offset, $limit, sub { $self->_new_from_row(@_) },
-        $query, $editor_id, $offset || 0);
+                 ORDER BY id";
+    $self->query_to_list_limited($query, [$editor_id], $limit, $offset);
 }
 
 before 'insert' => sub

lib/MusicBrainz/Server/Data/Artist.pm
@@ -19,7 +19,6 @@ use MusicBrainz::Server::Data::Utils qw(
     merge_date_period
     order_by
     placeholders
-    query_to_list_limited
 );
 use MusicBrainz::Server::Data::Utils::Cleanup qw( used_in_relationship );
 use MusicBrainz::Server::Data::Utils::Uniqueness qw( assert_uniqueness_conserved );
@@ -96,11 +95,8 @@ sub find_by_subscribed_editor
                  FROM " . $self->_table . "
                     JOIN editor_subscribe_artist s ON artist.id = s.artist
                  WHERE s.editor = ?
-                 ORDER BY musicbrainz_collate(artist.sort_name), artist.id
-                 OFFSET ?";
-    return query_to_list_limited(
-        $self->c->sql, $offset, $limit, sub { $self->_new_from_row(@_) },
-        $query, $editor_id, $offset || 0);
+                 ORDER BY musicbrainz_collate(artist.sort_name), artist.id";
+    $self->query_to_list_limited($query, [$editor_id], $limit, $offset);
 }
 
 sub find_by_area {
@@ -111,11 +107,8 @@ sub find_by_area {
                     LEFT JOIN area begin_area ON artist.begin_area = begin_area.id
                     LEFT JOIN area end_area ON artist.end_area = end_area.id
                  WHERE ? IN (area.id, begin_area.id, end_area.id)
-                 ORDER BY musicbrainz_collate(artist.name), artist.id
-                 OFFSET ?";
-    return query_to_list_limited(
-        $self->c->sql, $offset, $limit, sub { $self->_new_from_row(@_) },
-        $query, $area_id, $offset || 0);
+                 ORDER BY musicbrainz_collate(artist.name), artist.id";
+    $self->query_to_list_limited($query, [$area_id], $limit, $offset);
 }
 
 sub find_by_recording
@@ -126,11 +119,8 @@ sub find_by_recording
                     JOIN artist_credit_name acn ON acn.artist = artist.id
                     JOIN recording ON recording.artist_credit = acn.artist_credit
                  WHERE recording.id = ?
-                 ORDER BY musicbrainz_collate(artist.name), artist.id
-                 OFFSET ?";
-    return query_to_list_limited(
-        $self->c->sql, $offset, $limit, sub { $self->_new_from_row(@_) },
-        $query, $recording_id, $offset || 0);
+                 ORDER BY musicbrainz_collate(artist.name), artist.id";
+    $self->query_to_list_limited($query, [$recording_id], $limit, $offset);
 }
 
 sub find_by_release
@@ -149,11 +139,8 @@ sub find_by_release
                      JOIN artist_credit_name acn ON acn.artist = artist.id
                      JOIN release ON release.artist_credit = acn.artist_credit
                      wHERE release.id = ?)
-                 ORDER BY musicbrainz_collate(artist.name), artist.id
-                 OFFSET ?";
-    return query_to_list_limited(
-        $self->c->sql, $offset, $limit, sub { $self->_new_from_row(@_) },
-        $query, $release_id, $release_id, $offset || 0);
+                 ORDER BY musicbrainz_collate(artist.name), artist.id";
+    $self->query_to_list_limited($query, [($release_id) x 2], $limit, $offset);
 }
 
 sub find_by_release_group
@@ -164,11 +151,8 @@ sub find_by_release_group
                     JOIN artist_credit_name acn ON acn.artist = artist.id
                     JOIN release_group ON release_group.artist_credit = acn.artist_credit
                  WHERE release_group.id = ?
-                 ORDER BY musicbrainz_collate(artist.name), artist.id
-                 OFFSET ?";
-    return query_to_list_limited(
-        $self->c->sql, $offset, $limit, sub { $self->_new_from_row(@_) },
-        $query, $recording_id, $offset || 0);
+                 ORDER BY musicbrainz_collate(artist.name), artist.id";
+    $self->query_to_list_limited($query, [$recording_id], $limit, $offset);
 }
 
 sub find_by_work
@@ -186,11 +170,8 @@ sub find_by_work
                    JOIN l_artist_work law ON law.entity0 = artist.id
                    WHERE law.entity1 = ?
                  ) s
-                 ORDER BY musicbrainz_collate(name), id
-                 OFFSET ?";
-    return query_to_list_limited(
-        $self->c->sql, $offset, $limit, sub { $self->_new_from_row(@_) },
-        $query, $work_id, $work_id, $offset || 0);
+                 ORDER BY musicbrainz_collate(name), id";
+    $self->query_to_list_limited($query, [($work_id) x 2], $limit, $offset);
 }
 
 sub _order_by {

lib/MusicBrainz/Server/Data/Artwork.pm
@@ -5,7 +5,6 @@ use MusicBrainz::Server::Entity::Release;
 use MusicBrainz::Server::Data::Utils qw(
     object_to_ids
     placeholders
-    query_to_list
 );
 
 extends 'MusicBrainz::Server::Data::Entity';
@@ -91,8 +90,7 @@ sub find_by_release
         IN (" . placeholders(@ids) . ")
         ORDER BY cover_art_archive.index_listing.ordering";
 
-    my @artwork = query_to_list($self->c->sql, sub { $self->_new_from_row(@_) },
-                                $query, @ids);
+    my @artwork = $self->query_to_list($query, \@ids);
     for my $image (@artwork) {
         $image->release($id_to_release{$image->release_id}->[0]);
     }
@@ -130,8 +128,7 @@ sub find_front_cover_by_release
         IN (" . placeholders(@ids) . ")
         AND is_front = true";
 
-    my @artwork = query_to_list($self->c->sql, sub { $self->_new_from_row(@_) },
-                                $query, @ids);
+    my @artwork = $self->query_to_list($query, \@ids);
     foreach my $image (@artwork) {
         foreach my $release (@{ $id_to_release{$image->release_id} })
         {

commit 7c28ab3212be77c680dae490f6e3f9c1b40a8a63

Fix Data::CoverArt::find_outdated_releases
Broken in 2d64080

lib/MusicBrainz/Server/Data/CoverArt.pm
@@ -16,6 +16,7 @@ use MusicBrainz::Server::Data::Utils qw( placeholders );
 use MusicBrainz::Server::CoverArt;
 
 with 'MusicBrainz::Server::Data::Role::Context';
+with 'MusicBrainz::Server::Data::Role::QueryToList';
 
 has 'providers' => (
     isa => 'ArrayRef',
@@ -131,12 +132,12 @@ sub find_outdated_releases
     ORDER BY last_updated ASC';
 
     my $pg_date_formatter = DateTime::Format::Pg->new;
-    $self->query_to_list($query, [\@url_types, $pg_date_formatter->format_duration($since)], sub {
-        my ($model, $row) = shift;
+    $self->query_to_list($query, [@url_types, $pg_date_formatter->format_duration($since)], sub {
+        my ($model, $row) = @_;
 
         # Construction of these rows is slow, so this is lazy
         return sub {
-            my $release = $model->_new_from_row($row, 'r_');
+            my $release = $self->c->model('Release')->_new_from_row($row, 'r_');
             $release->cover_art(
                 MusicBrainz::Server::CoverArt->new(
                     $row->{c_last_updated} ? (last_updated => $row->{c_last_updated}) : ()