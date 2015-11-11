7492b66e88bf9efb1849857c00eba09c623b67ea
Roman Tsukanov <roman@tsukanov.me>
2015-11-11 18:46:02 +0100

    Code style tweaks.

--- lib/MusicBrainz/Server/Report/ReleaseGroupsWithoutVALink.pm
+++ lib/MusicBrainz/Server/Report/ReleaseGroupsWithoutVALink.pm
@@ -17,8 +17,7 @@ sub query {
     ";
 }
 
-sub template
-{
+sub template {
     return 'report/release_groups_without_va_link.tt';
 }
 
@@ -28,20 +27,9 @@ no Moose;
 
 =head1 COPYRIGHT
 
+This file is part of MusicBrainz, the open internet music database.
 Copyright (C) 2015 MetaBrainz Foundation
-
-This program is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program; if not, write to the Free Software
-Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+Licensed under the GPL version 2, or (at your option) any later version:
+http://www.gnu.org/licenses/gpl-2.0.txt
 
 =cut
