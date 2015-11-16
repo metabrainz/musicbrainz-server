metabrainz/musicbrainz-server

commit 93ffe29f70cd4ffe8b1d113f0f32ddcc0fd4f68e

Add a proper Catalyst view class for the Node template renderer
Fixes e.g. “Couldn't render template "file error - artist/show.tt: not found"”
errors that occur whenever an action detaches to /error_404; that action
doesn't have a `NewTemplate` attribute, but it wouldn't matter even if it did,
because only an attribute on the original action can affect the result at that
point, given the way the current code is written.

The `NewTemplate` attribute setup is flawed, so a `View::Node` class is added
instead, which an action can easily switch to by setting
`$c->stash->{current_view}`.

lib/MusicBrainz/Server/Controller/Root.pm
@@ -113,9 +113,9 @@ this means serving a 404 error page.
 
 =cut
 
-sub default : Path NewTemplate
-{
+sub default : Path {
     my ($self, $c) = @_;
+
     $c->detach('/error_404');
 }
 
@@ -149,6 +149,7 @@ sub error_404 : Private {
     my ($self, $c) = @_;
 
     $c->response->status(404);
+    $c->stash->{current_view} = 'Node';
 }
 
 sub error_500 : Private
@@ -231,11 +232,6 @@ sub begin : Private
         },
         favicon_css_classes => FAVICON_CLASSES,
     );
-
-    # Setup the searchs on the sidebar
-    unless (exists $c->action->attributes->{NewTemplate}) {
-        $c->form(sidebar_search => 'Search::Search');
-    }
 
     # Edit implies RequireAuth
     if (!exists $c->action->attributes->{RequireAuth} && exists $c->action->attributes->{Edit}) {
@@ -327,10 +323,6 @@ sub begin : Private
 
     if (DBDefs->REPLICATION_TYPE == RT_SLAVE) {
         $c->stash( last_replication_date => $c->model('Replication')->last_replication_date );
-    }
-
-    if (exists $c->action->attributes->{NewTemplate}) {
-        MusicBrainz::Server::Renderer::handle_request($c);
     }
 }
 

lib/MusicBrainz/Server/View/Default.pm
@@ -1,7 +1,10 @@
 package MusicBrainz::Server::View::Default;
 
 use strict;
-use base 'Catalyst::View::TT';
+use base qw(
+    Catalyst::View::TT
+    MusicBrainz::Server::View::Base
+);
 use DBDefs;
 use MRO::Compat;
 use Digest::MD5 qw( md5_hex );
@@ -10,42 +13,16 @@ use Date::Calc qw( Today_and_Now Add_Delta_DHMS Date_to_Time );
 
 __PACKAGE__->config(TEMPLATE_EXTENSION => '.tt');
 
-sub process
-{
+sub process {
     my $self = shift;
     my $c = $_[0];
 
-
     $self->next::method(@_) or return 0;
 
-    return 1 unless DBDefs->USE_ETAGS;
+    # Setup the searches on the sidebar.
+    $c->form(sidebar_search => 'Search::Search');
 
-    my $method = $c->request->method;
-    return 1
-        if $method ne 'GET' and $method ne 'HEAD' or
-            $c->stash->{nocache};    # disable caching explicitely
-
-    my $body = $c->response->body;
-    if ($body) {
-        utf8::encode($body)
-            if utf8::is_utf8($body);
-        $c->response->headers->etag(md5_hex($body));
-
-        # MBS-7061: Prevent network providers/proxies from stripping HTML
-        # comments, which are used heavily by knockout.js.
-        $c->response->headers->push_header( 'Cache-Control' => 'no-transform' );
-
-        if (DBDefs->REPLICATION_TYPE eq DBDefs->RT_SLAVE && !$c->res->headers->expires) {
-            my @today = Today_and_Now(1);
-            my $next_hour = Date_to_Time(Add_Delta_DHMS($today[0], $today[1], $today[2], $today[3], 10, 0,
-                                                       0, 1, 0, 0));
-            my $this_hour = Date_to_Time($today[0], $today[1], $today[2], $today[3], 10, 0);
-            $c->res->headers->expires($next_hour);
-            $c->res->headers->last_modified($this_hour);
-        }
-    }
-
-    return 1;
+    $self->_post_process(@_);
 }
 
 sub comma_list {

lib/MusicBrainz/Server/View/Node.pm
@@ -1,9 +1,8 @@
-package MusicBrainz::Server::Renderer;
+package MusicBrainz::Server::View::Node;
 
 use strict;
 use warnings;
-
-use base 'Exporter';
+use base 'MusicBrainz::Server::View::Base';
 use DBDefs;
 use Encode;
 use HTML::Entities qw( encode_entities decode_entities );
@@ -20,22 +19,6 @@ my %URI_DELIMITERS = (
     uri_for_action => "${INFO_SEP}__URI_FOR_ACTION__${INFO_SEP}",
 );
 
-our @EXPORT_OK = qw(
-    create_request
-);
-
-sub create_request {
-    my ($path, $headers, $body) = @_;
-
-    my $uri = URI->new;
-    $uri->scheme('http');
-    $uri->host(DBDefs->RENDERER_HOST || '127.0.0.1');
-    $uri->port(DBDefs->RENDERER_PORT);
-    $uri->path($path);
-
-    HTTP::Request->new('GET', $uri, $headers, $body);
-}
-
 sub replace_uri {
     my ($c, $method, $args) = @_;
 
@@ -43,8 +26,8 @@ sub replace_uri {
     return encode_entities($c->$method(@{$args}));
 }
 
-sub handle_request {
-    my ($c) = @_;
+sub process {
+    my ($self, $c) = @_;
 
     my $user;
     if ($c->user_exists) {
@@ -61,8 +44,14 @@ sub handle_request {
             flash => $c->flash,
         }});
 
+    my $uri = URI->new;
+    $uri->scheme('http');
+    $uri->host(DBDefs->RENDERER_HOST || '127.0.0.1');
+    $uri->port(DBDefs->RENDERER_PORT);
+    $uri->path($c->req->path);
+
     my $response = $c->model('MB')->context->lwp->request(
-        create_request($c->req->path, $c->req->headers->clone, $body)
+        HTTP::Request->new('GET', $uri, $c->req->headers->clone, $body)
     );
 
     my $content = decode('utf-8', $response->content);
@@ -77,6 +66,7 @@ sub handle_request {
 
     $c->res->status($response->code);
     $c->res->body($content);
+    $self->_post_process($c);
 }
 
 1;

app.psgi
@@ -26,16 +26,8 @@ BEGIN {
 use MusicBrainz::Server;
 
 BEGIN {
-    use LWP::UserAgent;
-    use MusicBrainz::Server::Renderer qw( create_request );
-
-    if (DBDefs->RENDERER_HOST eq '') {
-        # Check if the renderer is already running.
-        my $response = LWP::UserAgent->new->request(create_request('/', undef, ''));
-
-        if ($response->code == 500 && !fork) {
-            exec './script/start_renderer.pl';
-        }
+    if (DBDefs->RENDERER_HOST eq '' && !fork) {
+        exec './script/start_renderer.pl';
     }
 }
 

lib/MusicBrainz/Server/View/Base.pm
@@ -1,0 +1,42 @@
+package MusicBrainz::Server::View::Base;
+
+use strict;
+use base 'Catalyst::View';
+use DBDefs;
+use Digest::MD5 qw( md5_hex );
+
+sub _post_process {
+    my ($self, $c) = @_;
+
+    return 1 unless DBDefs->USE_ETAGS;
+
+    my $method = $c->request->method;
+    if ($method ne 'GET' and $method ne 'HEAD' or $c->stash->{nocache}) {
+        # disable caching explicitely
+        return 1;
+    }
+
+    my $body = $c->response->body;
+    if ($body) {
+        utf8::encode($body) if utf8::is_utf8($body);
+        $c->response->headers->etag(md5_hex($body));
+
+        # MBS-7061: Prevent network providers/proxies from stripping HTML
+        # comments, which are used heavily by knockout.js.
+        $c->response->headers->push_header('Cache-Control' => 'no-transform');
+
+        if (DBDefs->REPLICATION_TYPE eq DBDefs->RT_SLAVE && !$c->res->headers->expires) {
+            my @today = Today_and_Now(1);
+            my $next_hour = Date_to_Time(
+                Add_Delta_DHMS($today[0], $today[1], $today[2], $today[3], 10, 0, 0, 1, 0, 0)
+            );
+            my $this_hour = Date_to_Time($today[0], $today[1], $today[2], $today[3], 10, 0);
+            $c->res->headers->expires($next_hour);
+            $c->res->headers->last_modified($this_hour);
+        }
+    }
+
+    return 1;
+}
+
+1;

commit fd6e71613d03056f3212186444511b356e8ad365

Setup sidebar_search before processing the template
Fixes an issue introduced in 93ffe29f70cd4ffe8b1d113f0f32ddcc0fd4f68e. Makes
the search bar visible again.

Adds a different workaround for the Node renderer. We delete sidebar_search
from the stash before encoding it (because it contains code references).

lib/MusicBrainz/Server/Controller/Root.pm
@@ -233,6 +233,9 @@ sub begin : Private
         favicon_css_classes => FAVICON_CLASSES,
     );
 
+    # Setup the searches on the sidebar.
+    $c->form(sidebar_search => 'Search::Search');
+
     # Edit implies RequireAuth
     if (!exists $c->action->attributes->{RequireAuth} && exists $c->action->attributes->{Edit}) {
         $c->action->attributes->{RequireAuth} = 1;

lib/MusicBrainz/Server/View/Default.pm
@@ -18,10 +18,6 @@ sub process {
     my $c = $_[0];
 
     $self->next::method(@_) or return 0;
-
-    # Setup the searches on the sidebar.
-    $c->form(sidebar_search => 'Search::Search');
-
     $self->_post_process(@_);
 }
 

lib/MusicBrainz/Server/View/Node.pm
@@ -34,11 +34,15 @@ sub process {
         $user = {%{$c->user->TO_JSON}, preferences => $c->user->preferences};
     }
 
+    my %stash = %{$c->stash};
+    # XXX contains code references which can't be encoded
+    delete $stash{sidebar_search};
+
     my $body = $c->json_utf8->encode({
         context => {
             user => $user,
             debug => boolean_to_json($c->debug),
-            stash => $c->stash,
+            stash => \%stash,
             sessionid => scalar($c->sessionid),
             session => $c->session,
             flash => $c->flash,