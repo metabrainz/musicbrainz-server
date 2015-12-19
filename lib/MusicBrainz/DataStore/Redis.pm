ccbcf397d497834301fd24b2183bc0b9e9882ab9
Ulrich Klauer <ulrich@chirlu.de>
2015-12-19 04:46:12 +0100

    Factor out repeated code in MB::DataStore::Redis
    Various methods in MB::DataStore::Redis used the same (simple but
    nontrivial) code to encode keys for Redis and to decode values. Move
    this into common helper methods.

--- lib/MusicBrainz/DataStore/Redis.pm
+++ lib/MusicBrainz/DataStore/Redis.pm
@@ -40,39 +40,48 @@ has '_json' => (
     default => sub { return JSON->new->allow_nonref->ascii; }
 );
 
+sub _prepare_key {
+    my ($self, $key) = @_;
+    return encode('utf-8', $self->prefix . $key);
+}
+
+sub _decode_value {
+    my ($self, $value) = @_;
+    return defined $value ? $self->_json->decode($value) : undef;
+}
+
 sub get {
     my ($self, $key) = @_;
 
-    my $value = $self->_connection->get(encode('utf-8', $self->prefix.$key));
+    my $value = $self->_connection->get($self->_prepare_key($key));
 
-    return defined $value ? $self->_json->decode($value) : undef;
+    return $self->_decode_value($value);
 }
 
 sub mget {
     my ($self, @keys) = @_;
 
-    map {
-        defined $_ ? $self->_json->decode($_) : undef
-    } $self->_connection->mget(map { encode('utf-8', $self->prefix . $_) } @keys);
+    map { $self->_decode_value($_) }
+    $self->_connection->mget(map { $self->_prepare_key($_) } @keys);
 }
 
 sub set {
     my ($self, $key, $value) = @_;
 
     return $self->_connection->set(
-        encode('utf-8', $self->prefix.$key), $self->_json->encode($value));
+        $self->_prepare_key($key), $self->_json->encode($value));
 }
 
 sub exists {
     my ($self, $key) = @_;
 
-    return $self->_connection->exists(encode('utf-8', $self->prefix.$key));
+    return $self->_connection->exists($self->_prepare_key($key));
 }
 
 sub del {
     my ($self, $key) = @_;
 
-    return $self->_connection->del(encode('utf-8', $self->prefix.$key));
+    return $self->_connection->del($self->_prepare_key($key));
 }
 
 =method expire
@@ -84,13 +93,13 @@ Expire the specified key in $s seconds
 sub expire {
     my ($self, $key, $s) = @_;
 
-    return $self->_connection->expire(encode('utf-8', $self->prefix.$key), $s);
+    return $self->_connection->expire($self->_prepare_key($key), $s);
 }
 
 sub expireat {
     my ($self, $key, $timestamp) = @_;
 
-    return $self->_connection->expireat(encode('utf-8', $self->prefix.$key), $timestamp);
+    return $self->_connection->expireat($self->_prepare_key($key), $timestamp);
 }
 
 sub _flushdb {
