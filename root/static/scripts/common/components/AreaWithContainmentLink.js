b0f39dfc08d841aa434e0ca8b2e83f8650ab2dc4
Michael Wiencek <mwtuea@gmail.com>
2016-02-04 23:56:35 -0600

    Fix silly case issue

--- root/static/scripts/common/components/AreaWithContainmentLink.js
+++ root/static/scripts/common/components/AreaWithContainmentLink.js
@@ -3,7 +3,7 @@
 // Licensed under the GPL version 2, or (at your option) any later version:
 // http://www.gnu.org/licenses/gpl-2.0.txt
 
-const React = require('React');
+const React = require('react');
 
 const EntityLink = require('./EntityLink');
 const commaOnlyList = require('../../common/i18n/commaOnlyList');
