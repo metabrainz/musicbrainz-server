51c8050856b73836b6eedca18ee86b5be6a5da1f
Michael Wiencek <mwtuea@gmail.com>
2016-02-22 12:23:25 -0600

    Beta: Language menu entries overflow (MBS-8811)
    Fixed by (1) removing the height: 15px limitation on items, (2) increasing the
    default width, and (3) aligning the menu on the right side.

--- root/static/scripts/common/MB/Control/Menu.js
+++ root/static/scripts/common/MB/Control/Menu.js
@@ -24,13 +24,21 @@ MB.Control.HeaderMenu = function () {
     self.timeout = null;
     self.timeout_msecs = 200;
 
+    function getLeft(li) {
+        var $li = $(li);
+        if ($li.hasClass('language-selector')) {
+            return '-' + ($li.children('ul:eq(0)').outerWidth() - $li.outerWidth()) + 'px';
+        }
+        return 'auto';
+    }
+
     $('.header ul.menu > li').bind('mouseenter.mb', function (event) {
         if (self.timeout) {
             clearTimeout(self.timeout);
             $('.header ul.menu li ul').css('left', '-10000px');
         }
 
-        $(this).children('ul').css('left', 'auto');
+        $(this).children('ul').css('left', getLeft(this));
     });
 
     $('.header ul.menu li').bind('mouseleave.mb', function (event) {
@@ -46,7 +54,7 @@ MB.Control.HeaderMenu = function () {
         event.stopPropagation();
         var ul = $(this).siblings('ul');
         $('.header ul.menu li ul').not(ul).css('left', '-10000px');
-        ul.css('left', ul.css('left') === 'auto' ? '-10000px' : 'auto');
+        ul.css('left', ul.css('left') !== '-10000px' ? '-10000px' : getLeft(this.parentNode));
         if ($.browser.android) {
             $(this).parent().siblings().removeClass('fake-active');
             $(this).parent().toggleClass('fake-active');
