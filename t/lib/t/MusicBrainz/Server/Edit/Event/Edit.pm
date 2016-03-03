metabrainz/musicbrainz-server

commit 6a8e2e6127cbdec3e23590368e78c2490c200768

Replace Algorithm::Merge with Text::Diff3 (MBS-7475, MBS-8736, ...)
Algorithm::Merge is broken, see test case which results in a missing track.

lib/MusicBrainz/Server/Edit/CheckForConflicts.pm
@@ -3,7 +3,7 @@ use 5.10.0;
 use Moose::Role;
 use namespace::autoclean;
 
-use Algorithm::Merge qw( merge );
+use Text::Diff3 qw( merge );
 use MusicBrainz::Server::Edit::Utils qw( merge_value );
 use Try::Tiny;
 
@@ -123,23 +123,22 @@ sub merge_changes {
                 );
 
             # Stores a mapping of the JSON serialization to the object value
-            my %hashed = map { @$_ } ($ancestor, $current, $new);
+            my %hashed = map { @$_ } ($new, $ancestor, $current);
 
             # Attempt to merge all JSON values together
-            my ($json_val) = merge(
-                (map +[ $_->[0] ], ($ancestor, $current, $new)),
-                { CONFLICT => sub {
-                    die bless({
-                        property => $name,
-                        ancestor => $ancestor,
-                        current  => $current,
-                        new      => $new
-                    }, 'Conflict')
-                } }
-            );
+            my $merged_json = merge(map +[ $_->[0] ], ($new, $ancestor, $current));
+
+            if ($merged_json->{conflict}) {
+                die bless({
+                    property => $name,
+                    ancestor => $ancestor,
+                    current  => $current,
+                    new      => $new
+                }, 'Conflict');
+            }
 
             # Resolve that JSON value back to it's actual value
-            $merged->{$name} = $hashed{$json_val};
+            $merged->{$name} = $hashed{$merged_json->{body}->[0]};
         }
     }
     catch {

lib/MusicBrainz/Server/Edit/Medium/Edit.pm
@@ -3,7 +3,7 @@ use Carp;
 use Clone 'clone';
 use List::AllUtils qw( any );
 use Algorithm::Diff qw( sdiff );
-use Algorithm::Merge qw( merge );
+use Text::Diff3 qw( merge );
 use Data::Compare;
 use Set::Scalar;
 use Moose;
@@ -331,8 +331,12 @@ sub build_display_data
 
 my $UNDEF_MARKER = time();
 sub track_column {
-    my ($column, $tracklist) = @_;
-    return [ map { $_->{$column} // $UNDEF_MARKER } @$tracklist ];
+    my ($column, $tracklist, $key_generation) = @_;
+
+    [ map {
+        my $value = $_->{$column};
+        ($key_generation ? $key_generation->($value) : $value) // $UNDEF_MARKER
+    } @$tracklist ];
 }
 
 sub accept {
@@ -364,6 +368,15 @@ sub accept {
         my (@merged_row_ids, @merged_numbers, @merged_names, @merged_recordings,
             @merged_lengths, @merged_artist_credits, @merged_is_data_tracks);
 
+        my %hashed_artist_credits;
+        my $hash_artist_credit = sub {
+            my ($artist_credit) = @_;
+
+            my $hash = hash_artist_credit($artist_credit);
+            $hashed_artist_credits{$hash} = $artist_credit;
+            $hash;
+        };
+
         my $current_tracklist = tracks_to_hash($medium->tracks);
         try {
             for my $merge (
@@ -372,17 +385,17 @@ sub accept {
                 [ name => \@merged_names ],
                 [ recording_id => \@merged_recordings ],
                 [ length => \@merged_lengths ],
-                [ artist_credit => \@merged_artist_credits, \&hash_artist_credit ],
+                [ artist_credit => \@merged_artist_credits, $hash_artist_credit ],
                 [ is_data_track => \@merged_is_data_tracks ]
             ) {
                 my ($property, $container, $key_generation) = @$merge;
-                push @$container, merge(
-                    track_column($property, $self->data->{old}{tracklist}),
-                    track_column($property, $current_tracklist),
-                    track_column($property, $data_new_tracklist),
-                    { CONFLICT => sub { die } },
-                    $key_generation // ()
+                my $merged = merge(
+                    track_column($property, $data_new_tracklist, $key_generation),
+                    track_column($property, $self->data->{old}{tracklist}, $key_generation),
+                    track_column($property, $current_tracklist, $key_generation),
                 );
+                die if $merged->{conflict};
+                push @$container, @{ $merged->{body} };
             }
         }
         catch {
@@ -446,7 +459,7 @@ sub accept {
                 number => $number eq $UNDEF_MARKER ? undef : $number,
                 length => $length eq $UNDEF_MARKER ? undef : $length,
                 recording_id => $recording_id,
-                artist_credit => shift(@merged_artist_credits),
+                artist_credit => $hashed_artist_credits{shift(@merged_artist_credits)},
                 is_data_track => $is_data_track
             }
         }

t/lib/t/MusicBrainz/Server/Edit/Medium/Edit.pm
@@ -606,6 +606,98 @@ test 'Tracks can be reordered' => sub {
     })
 };
 
+test 'Tracklist merging (MBS-8752 / MBS-7475)' => sub {
+    my $test = shift;
+    my $c = $test->c;
+
+    MusicBrainz::Server::Test->prepare_test_database($c, '+mbs-8752');
+
+    my $artist_credit = ArtistCredit->new(
+        names => [
+            ArtistCreditName->new(
+                artist => Artist->new( id => 9113, name => 'Meat Loaf' ),
+                join_phrase => '',
+                name => 'Meat Loaf',
+            ),
+        ],
+    );
+
+    my $medium = $c->model('Medium')->get_by_id(1819308);
+    $c->model('Track')->load_for_mediums($medium);
+    $c->model('ArtistCredit')->load($medium->all_tracks);
+
+    my $expected_tracklist = [
+        Track->new(
+            artist_credit => $artist_credit,
+            is_data_track => '0',
+            length => 481000,
+            name => 'Life Is a Lemon and I Want My Money Back (live)',
+            number => '1',
+            position => 1,
+            recording_id => 9724192,
+        ),
+        Track->new(
+            artist_credit => $artist_credit,
+            id => 20036049,
+            is_data_track => '0',
+            length => 507000,
+            name => 'Rock and Roll Dreams Come Through (live)',
+            number => '2',
+            position => 2,
+            recording_id => 9724193,
+        ),
+        Track->new(
+            artist_credit => $artist_credit,
+            id => 20036047,
+            is_data_track => '0',
+            length => 522000,
+            name => 'Out of the Frying Pan (And Into the Fire) (live)',
+            number => '3',
+            position => 3,
+            recording_id => 9724194,
+        ),
+        Track->new(
+            artist_credit => $artist_credit,
+            is_data_track => '0',
+            length => 583000,
+            name => 'Everything Louder Than Everything Else (live)',
+            number => '4',
+            position => 4,
+            recording_id => 9724195,
+        ),
+        Track->new(
+            artist_credit => $artist_credit,
+            id => 20036050,
+            is_data_track => '0',
+            length => 356000,
+            name => 'Objects in the Rear View Mirror May Appear Closer Than They Are (edit)',
+            number => '5',
+            position => 5,
+            recording_id => 9724196,
+        ),
+    ];
+
+    my $expected_tracklist_hash = tracks_to_hash($expected_tracklist);
+
+    my $edit = $c->model('Edit')->create(
+        editor_id => 1,
+        edit_type => $EDIT_MEDIUM_EDIT,
+        to_edit => $medium,
+        tracklist => $expected_tracklist,
+    );
+
+    accept_edit($c, $edit);
+
+    $medium = $c->model('Medium')->get_by_id(1819308);
+    $c->model('Track')->load_for_mediums($medium);
+    $c->model('ArtistCredit')->load($medium->all_tracks);
+
+    my $got_tracklist_hash = tracks_to_hash($medium->tracks);
+    $expected_tracklist_hash->[0]{id} = 20036051;
+    $expected_tracklist_hash->[3]{id} = 20036052;
+    cmp_deeply($got_tracklist_hash, $expected_tracklist_hash);
+};
+
 sub create_edit {
     my ($c, $medium, $tracklist) = @_;
 

Makefile.PL
@@ -11,7 +11,6 @@ version_from 'lib/MusicBrainz/Server.pm';
 
 # Mandatory modules
 requires 'Algorithm::Diff'                            => '1.1902';
-requires 'Algorithm::Merge'                           => '0.08';
 requires 'Authen::Passphrase'                         => '0.007';
 requires 'Cache::Memcached'                           => '1.29';
 requires 'Captcha::reCAPTCHA'                         => '0.93';
@@ -97,6 +96,7 @@ requires 'Sys::Hostname';
 requires 'Template::Plugin::Class'                    => '0.13';
 requires 'Template::Plugin::JavaScript'               => '0.02';
 requires 'Template::Plugin::JSON::Escape'             => '0.02';
+requires 'Text::Diff3'                                => '0.10';
 requires 'Text::Markdown'                             => '1.000026';
 requires 'Text::WikiFormat'                           => '0.81';
 requires 'Text::Unaccent'                             => '1.08';

t/sql/mbs-8752.sql
@@ -1,0 +1,36 @@
+SET client_min_messages TO 'warning';
+
+INSERT INTO artist (area, begin_area, begin_date_day, begin_date_month, begin_date_year, comment, edits_pending, end_area, end_date_day, end_date_month, end_date_year, ended, gender, gid, id, last_updated, name, sort_name, type) VALUES
+    (NULL, NULL, 27, 9, 1947, '', 0, NULL, NULL, NULL, NULL, '0', 1, 'b134d1bf-c7c7-4427-93ac-9fdbc2b59ef1', 9113, '2015-06-21 09:12:12.187989+00', 'Meat Loaf', 'Meat Loaf', 1);
+
+INSERT INTO artist_credit (artist_count, created, id, name, ref_count) VALUES
+    (1, '2011-05-16 16:32:11.963929+00', 9113, 'Meat Loaf', 4332);
+
+INSERT INTO artist_credit_name (artist, artist_credit, join_phrase, name, position) VALUES
+    (9113, 9113, '', 'Meat Loaf', 0);
+
+INSERT INTO release_group (artist_credit, comment, edits_pending, gid, id, last_updated, name, type) VALUES
+    (9113, '', 0, '758d7dc5-4aa2-3731-b970-549251a98232', 17814, '2010-03-19 18:03:48.279801+00', 'Bat Out of Hell II: Back Into Hell', 1);
+
+INSERT INTO release (artist_credit, barcode, comment, edits_pending, gid, id, language, last_updated, name, packaging, quality, release_group, script, status) VALUES
+    (9113, '094637912355', '', 2, '3a1a6bd7-dbe1-4004-813b-11debf0b61e8', 1718385, 120, '2016-01-22 10:53:51.139298+00', 'Bat Out of Hell II: Back Into Hell', NULL, -1, 17814, NULL, 1);
+
+INSERT INTO medium (edits_pending, format, id, last_updated, name, position, release, track_count) VALUES
+    (0, NULL, 1819308, '2016-01-22 10:13:55.59614+00', '', 3, 1718385, 5);
+
+INSERT INTO recording (artist_credit, comment, edits_pending, gid, id, last_updated, length, name, video) VALUES
+    (9113, '', 0, '7cb020c5-c7c4-495e-bbdb-774c0ac438b3', 14428606, '2013-01-10 18:11:06.367649+00', 562000, 'Back Into Hell: Meat Loaf and Jim Steinman Interview', '0'),
+    (9113, '', 0, '23aec39a-896c-4a16-a1f5-d3ef9811cd5f', 14428607, '2016-01-22 10:13:55.59614+00', 460000, 'I’ll Do Anything for Love (But I Won’t Do That)', '0'),
+    (9113, '', 0, 'c3a91791-1c5a-477e-8440-d74ac1ce5bf2', 14428608, '2013-01-10 18:11:06.367649+00', 347000, 'Rock and Roll Dreams Come Through', '0'),
+    (9113, '', 0, '6e505245-98c3-47d6-8ee2-269cffeb97dc', 14428609, '2015-01-18 12:04:18.111683+00', 463000, 'Objects in the Rear View Mirror May Appear Closer Than They Are', '0'),
+    (9113, '', 0, '69c38d37-901b-45d6-b469-0708d6d4a289', 9724192, NULL, 481000, 'Life Is a Lemon and I Want My Money Back (live)', '0'),
+    (9113, '', 0, 'bff3edaf-c019-4cb5-8ede-78872ff472a9', 9724194, '2016-01-22 10:30:11.549976+00', 522000, 'Out of the Frying Pan (And Into the Fire) (live)', '0'),
+    (9113, '', 0, '6a4ad015-a7b0-4687-be4c-1183858868c9', 9724195, '2016-01-22 10:27:51.598498+00', 583000, 'Everything Louder Than Everything Else (live)', '0'),
+    (9113, '', 0, 'f531199f-db87-451b-ad3c-bea08435e9b3', 9724196, NULL, 356000, 'Objects in the Rear View Mirror May Appear Closer Than They Are (edit)', '0'),
+    (9113, '', 0, '25a9c7a1-f9a0-4929-87ba-eef3d3661cee', 9724193, NULL, 507000, 'Rock and Roll Dreams Come Through (live)', '0');
+
+INSERT INTO track (artist_credit, edits_pending, gid, id, is_data_track, last_updated, length, medium, name, number, position, recording) VALUES
+    (9113, 0, '420e76c4-5d0e-493d-9af1-b113d45eec1f', 20036047, '0', '2016-01-22 10:30:11.549976+00', 562000, 1819308, 'Back Into Hell: Meat Loaf and Jim Steinman Interview', '1', 1, 14428606),
+    (9113, 0, 'fb9b72a5-b8a1-4e66-9b8c-b7b2b712a364', 20036048, '0', '2016-01-22 10:30:11.549976+00', 460000, 1819308, 'I’ll Do Anything for Love (But I Won’t Do That)', '2', 2, 14428607),
+    (9113, 0, 'b2237b04-eb5f-411e-901c-024cc430cf0a', 20036049, '0', '2016-01-22 10:30:11.549976+00', 347000, 1819308, 'Rock and Roll Dreams Come Through', '3', 4, 14428608),
+    (9113, 0, '683d5ae0-fd1a-405b-be66-541d60d8d6d4', 20036050, '0', '2016-01-22 10:30:11.549976+00', 463000, 1819308, 'Objects in the Rear View Mirror May Appear Closer Than They Are', '4', 5, 14428609);

commit 212c5b2accea2db3c10a7c1d789fa95450d979c4

MBS-8837: rare ISE editing event time
Broken in 6a8e2e6127cbdec3e23590368e78c2490c200768

This ISE can occur if the time being set is already the current value.

The source of the bug is due to the change in parameter order associated with
Text::Diff3's merge subroutine.

Before the referenced commit, CheckForConflicts hashed the results of
merge_time in the order (ancestor, current, new), which would be something like
[['19:30', '19:30'], ['19:00', (DateTime object)], ['19:00', '19:00]], where
the first value in each array ref is the key. Since the last two array refs
have the same key, the second would override the first.

After 6a8e2e6, those array refs are hashed in the order new, ancestor, current,
which means the DateTime object is the result.

The fix here is to revert the change in the order they're hashed (new should be
last), and always return a formatted string for the current value, not a
DateTime object, since the object isn't stringified to the same format that
ancestor and new are in.

lib/MusicBrainz/Server/Edit/CheckForConflicts.pm
@@ -123,7 +123,7 @@ sub merge_changes {
                 );
 
             # Stores a mapping of the JSON serialization to the object value
-            my %hashed = map { @$_ } ($new, $ancestor, $current);
+            my %hashed = map { @$_ } ($ancestor, $current, $new);
 
             # Attempt to merge all JSON values together
             my $merged_json = merge(map +[ $_->[0] ], ($new, $ancestor, $current));

lib/MusicBrainz/Server/Edit/Utils.pm
@@ -390,10 +390,11 @@ sub merge_barcode {
 sub merge_time {
     my ($name, $ancestor, $current, $new) = @_;
 
+    $current = $current->$name ? $current->$name->strftime('%H:%M') : undef;
     return (
-        [ defined $ancestor->{$name} ? $ancestor->{$name} : undef, $ancestor->{$name} ],
-        [ defined $current->$name ? $current->$name->strftime('%H:%M') : undef, $current->$name ],
-        [ defined $new->{$name} ? $new->{$name} : undef, $new->{$name} ],
+        [$ancestor->{$name}, $ancestor->{$name}],
+        [$current, $current],
+        [$new->{$name}, $new->{$name}],
     );
 }
 

t/lib/t/MusicBrainz/Server/Edit/Event/Edit.pm
@@ -1,0 +1,36 @@
+package t::MusicBrainz::Server::Edit::Event::Edit;
+
+use Test::Routine;
+use Test::More;
+use Test::Fatal;
+
+with 't::Edit';
+with 't::Context';
+
+use MusicBrainz::Server::Context;
+use MusicBrainz::Server::Constants qw( $EDIT_EVENT_EDIT $UNTRUSTED_FLAG );
+
+test 'MBS-8837' => sub {
+    my $test = shift;
+    my $c = $test->c;
+
+    $c->sql->do(<<'EOSQL');
+        INSERT INTO event (id, name, gid, time)
+        VALUES (1, 'test', '5140d04f-a0f9-4846-8b23-b53a36b5c5ff', '19:30:00');
+EOSQL
+
+    my $edit = $c->model('Edit')->create(
+        edit_type => $EDIT_EVENT_EDIT,
+        editor_id => 1,
+        to_edit => $c->model('Event')->get_by_id(1),
+        time => '19:00',
+        privileges => $UNTRUSTED_FLAG,
+    );
+
+    $c->sql->do('UPDATE event SET time = ? WHERE id = 1', '19:00:00');
+    ok !exception { $edit->accept };
+    my $time = $c->sql->select_single_value('SELECT time FROM event WHERE id = 1');
+    is($time, '19:00:00');
+};
+
+1;