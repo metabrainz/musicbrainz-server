a57117672a8f9bb035c7873bc5246271ac51b662
Roman Tsukanov <roman@tsukanov.me>
2016-03-30 23:54:18 +0200

    Fix Data::Gender test

--- t/lib/t/MusicBrainz/Server/Data/Gender.pm
+++ t/lib/t/MusicBrainz/Server/Data/Gender.pm
@@ -44,7 +44,10 @@ is($gs[1]->id, 2);
 my $sql = $test->c->sql;
 $sql->begin;
 
-my $new_gender = $gender_data->insert({ name => 'Unknown' });
+my $new_gender = $gender_data->insert({
+	name => 'Unknown',
+	gid => '181c0bf5-da60-37b0-95f8-2207a3f7f9d6',
+});
 ok(defined $new_gender, 'should return instantiated object');
 isa_ok($new_gender, 'MusicBrainz::Server::Entity::Gender');
 ok(defined $new_gender->id, 'id should be defined');
