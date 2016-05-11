44b0959220cdabe93edcc8e6ef4578f35cdabce3
Michael Wiencek <mwtuea@gmail.com>
2016-05-10 22:31:47 -0500

    Fix method of superuser access in 867428f
    That's what --system is for, otherwise it connects to template1.

--- upgrade.sh
+++ upgrade.sh
@@ -51,14 +51,14 @@ fi
 if [ "$REPLICATION_TYPE" = "$RT_SLAVE" ]
 then
     echo `date` : 'Running upgrade scripts for slave nodes'
-    ./admin/psql SYSTEM < ./admin/sql/updates/schema-change/${NEW_SCHEMA_SEQUENCE}.slave_only.sql || exit 1
+    ./admin/psql --system MAINTENANCE < ./admin/sql/updates/schema-change/${NEW_SCHEMA_SEQUENCE}.slave_only.sql || exit 1
 fi
 
 ################################################################################
 # Scripts that should run on *all* nodes (master/slave/standalone)
 
 echo `date` : 'Running upgrade scripts for all nodes'
-./admin/psql SYSTEM < ./admin/sql/updates/schema-change/${NEW_SCHEMA_SEQUENCE}.slave.sql || exit 1
+./admin/psql --system MAINTENANCE < ./admin/sql/updates/schema-change/${NEW_SCHEMA_SEQUENCE}.slave.sql || exit 1
 
 ################################################################################
 # Re-enable replication
@@ -81,7 +81,7 @@ fi
 if [ "$REPLICATION_TYPE" != "$RT_SLAVE" ]
 then
     echo `date` : 'Running upgrade scripts for master/standalone nodes'
-    ./admin/psql SYSTEM < ./admin/sql/updates/schema-change/${NEW_SCHEMA_SEQUENCE}.standalone.sql || exit 1
+    ./admin/psql --system MAINTENANCE < ./admin/sql/updates/schema-change/${NEW_SCHEMA_SEQUENCE}.standalone.sql || exit 1
 
     echo `date` : Enabling last_updated triggers
     ./admin/sql/EnableLastUpdatedTriggers.pl
