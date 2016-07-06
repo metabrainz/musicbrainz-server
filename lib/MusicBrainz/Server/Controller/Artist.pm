c8f2180dd62d75573e51dd4cc34a92cb3c1241f0
Tim Van Holder <tim.vanholder@gmail.com>
2016-07-06 21:17:18 +0200

    Use exported server constants instead of hardcoded values. The Artist controller now puts the instrument/vocal root id constant in the stash for the template to use.

--- lib/MusicBrainz/Server/Controller/Artist.pm
+++ lib/MusicBrainz/Server/Controller/Artist.pm
@@ -53,6 +53,8 @@ use MusicBrainz::Server::Constants qw(
     $EDIT_ARTIST_EDITCREDIT
     $EDIT_RELATIONSHIP_DELETE
     $ARTIST_ARTIST_COLLABORATION
+    $INSTRUMENT_ROOT_ID
+    $VOCAL_ROOT_ID
 );
 use MusicBrainz::Server::Form::Artist;
 use MusicBrainz::Server::Form::Confirm;
@@ -267,7 +269,15 @@ sub show : PathPart('') Chained('load')
               identities => \@identities);
 }
 
-sub relationships : Chained('load') PathPart('relationships') {}
+sub relationships : Chained('load') PathPart('relationships')
+{
+  my ($self, $c) = @_;
+  $c->stash(server_constants =>
+            {
+             INSTRUMENT_ROOT_ID => $INSTRUMENT_ROOT_ID,
+             VOCAL_ROOT_ID      => $VOCAL_ROOT_ID,
+            });
+}
 
 =head2 works
 
