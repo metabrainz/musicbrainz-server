1f63182e125697aae6baf41a1d0f20408f76c827
Michael Wiencek <mwtuea@gmail.com>
2016-11-12 20:11:50 -0600

    Deploy static files directly to build/, not /tmp/
    Ideally, /tmp/staticbrainz would have been a Docker volume prior to this patch,
    so that the build state (and file mtimes) were preserved even after the
    container was removed. But root/static/build/ is already a volume (because
    the rev-manifest.json needs to be shared with the template-renderer). And
    there's no reason to have two volumes storing the same files; we can just have
    the .gz files go in root/static/build/ as well.

--- docker/musicbrainz-website/deploy_static_resources.sh
+++ docker/musicbrainz-website/deploy_static_resources.sh
@@ -2,31 +2,37 @@
 
 source /etc/mbs_constants.sh
 
+BUILD_DIR=$MBS_ROOT/root/static/build
+TMP_BUILD_DIR=$MBS_ROOT/root/static/tmp-build
+
 _compile_static_resources() {
-    local BUILD_DIR=$MBS_ROOT/root/static/build
-    mkdir -p $BUILD_DIR
-    chown musicbrainz:musicbrainz $BUILD_DIR
+    mkdir -p $TMP_BUILD_DIR
+    chown musicbrainz:musicbrainz $TMP_BUILD_DIR
 
     (
         cd $MBS_ROOT;
         export HOME=$MBS_HOME;
+        export MBS_STATIC_BUILD_DIR=$TMP_BUILD_DIR;
         chpst -u musicbrainz:musicbrainz \
             carton exec -- ./script/compile_resources.sh
     )
 }
 
-_push_static_resources() {
-    local TMP=/tmp/staticbrainz
-    mkdir -p $TMP/MB
+_deploy_static_resources() {
+    mkdir -p $BUILD_DIR
+    chown musicbrainz:musicbrainz $BUILD_DIR
 
     # Delete files older than 30 days. If they're still unchanged, they'll just
     # have to be compressed again below.
-    find $TMP/MB/ -type f -mtime +30 -not -name 'rev-manifest.json' -delete
+    find $BUILD_DIR -type f -mtime +30 -not -name 'rev-manifest.json' -delete
 
     # -n will not clobber existing files, preserving their mtimes and allowing
     # us to avoid recompressing files that haven't changed (zopfli is slow).
-    cp -Rn $MBS_ROOT/root/{favicon.ico,robots.txt.*,static/build/*} $TMP/MB/
-    find $TMP/MB/ -type f -newermt '-10 seconds' | xargs zopfli -v
+    cp -Rn \
+        $MBS_ROOT/root/{favicon.ico,robots.txt.*} \
+        $TMP_BUILD_DIR/* \
+        $BUILD_DIR/
+    find $BUILD_DIR -type f -newermt '-10 seconds' | xargs zopfli -v
 
     # copy resources into the staticbrainz data volume
     for server in $STATICBRAINZ_SERVERS; do
@@ -40,18 +46,20 @@ _push_static_resources() {
             $TMP/ \
             brainz@$host:/data/staticbrainz/
     done
+
+    rm -rf "$TMP_BUILD_DIR"
 }
 
 compile_static_resources() {
     (flock -e 220; _compile_static_resources $@) 220>/tmp/.static_resources.lock
 }
 
-push_static_resources() {
+deploy_static_resources() {
     if [ -z "$STATICBRAINZ_SERVERS" ]; then
         return
     fi
-    (flock -e 220; _push_static_resources) 220>/tmp/.static_resources.lock
+    (flock -e 220; _deploy_static_resources) 220>/tmp/.static_resources.lock
 }
 
 compile_static_resources
-push_static_resources
+deploy_static_resources
--- root/static/gulpfile.js
+++ root/static/gulpfile.js
@@ -22,7 +22,7 @@ const PO_DIR = path.resolve(CHECKOUT_DIR, 'po');
 const ROOT_DIR = path.resolve(CHECKOUT_DIR, 'root');
 const STATIC_DIR = path.resolve(ROOT_DIR, 'static');
 const STYLES_DIR = path.resolve(STATIC_DIR, 'styles');
-const BUILD_DIR = path.resolve(STATIC_DIR, 'build');
+const BUILD_DIR = process.env.MBS_STATIC_BUILD_DIR || path.resolve(STATIC_DIR, 'build');
 const SCRIPTS_DIR = path.resolve(STATIC_DIR, 'scripts');
 const IMAGES_DIR = path.resolve(STATIC_DIR, 'images');
 
