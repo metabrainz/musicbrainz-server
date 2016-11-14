69af62048bb783aa1d68f6eb8f5c37e4f823a509
Michael Wiencek <mwtuea@gmail.com>
2016-11-13 23:28:43 -0600

    Perl needs the script path
    It doesn't look in $PATH.

--- docker/musicbrainz-website/start_musicbrainz_website.sh
+++ docker/musicbrainz-website/start_musicbrainz_website.sh
@@ -6,6 +6,6 @@ source /etc/mbs_constants.sh
 
 cd $MBS_ROOT
 
-carton exec -- perl -Ilib install_language_packs.pl
+carton exec -- perl -Ilib /usr/local/bin/install_language_packs.pl
 deploy_static_resources.sh
 start_musicbrainz_server.sh
