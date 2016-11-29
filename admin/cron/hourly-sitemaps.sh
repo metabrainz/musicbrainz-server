7dacbb25aaebe587ae69ce76d5a89a5857094eda
Michael Wiencek <mwtuea@gmail.com>
2016-11-29 00:06:15 -0600

    Sitemaps build no longer uses a slave server

--- admin/cron/hourly-sitemaps.sh
+++ admin/cron/hourly-sitemaps.sh
@@ -3,8 +3,6 @@
 MB_SERVER_ROOT=$(cd "$(dirname "${BASH_SOURCE[0]}")/../../" && pwd)
 cd "$MB_SERVER_ROOT"
 
-OUTPUT=`./admin/cron/slave.sh 2>&1` || echo "$OUTPUT"
-
 OUTPUT=`
     ./admin/BuildIncrementalSitemaps.pl --ping --worker-count 7 2>&1
 ` || echo "$OUTPUT"
