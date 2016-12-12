metabrainz/musicbrainz-server

commit 9ca6bafa879473a23d7bb205fa9ccb01f0a4cc40

Remove pre-NGS ratings star, simplified ratings CSS and remove unused favicons CSS

root/components/rating-macros.tt
@@ -16,7 +16,7 @@ END -%]
 
 [%- MACRO rating_stars(entity, prevent_rating) BLOCK -%]
 <span class="inline-rating">
-    <span class="star-rating small-star">
+    <span class="star-rating">
         [%- current_star_rating = entity.user_rating ? 5 * entity.user_rating / 100 : 0 -%]
         [%- IF entity.user_rating -%]
         <span class="current-user-rating" style="width:[% entity.user_rating %]%;">[% current_star_rating %]</span>
@@ -40,7 +40,7 @@ END -%]
 
 [%- MACRO show_rating(rating) BLOCK -%]
 <span class="inline-rating">
-    <span class="star-rating small-star">
+    <span class="star-rating">
         <span class="current-rating" style="width:[% rating.rating %]%;">[% 5 * rating.rating / 100 %]</span>
     </span>
 </span>

root/static/styles/widgets.less
@@ -148,14 +148,15 @@ form input[type=submit] {
 .star-rating a:focus,
 .star-rating .current-user-rating,
 .star-rating .current-rating {
-    background: url('../images/icons/rating_star.gif') left -1000px repeat-x;
+    background-image: url('../images/icons/rating_star_small.gif');
 }
 
 .star-rating {
     position:relative;
     display:block;
-    width:125px;
-    height:25px;
+    width: 50px;
+    height: 10px;
+    line-height: 10px;
     overflow:hidden;
     list-style:none;
     margin:0;
@@ -174,15 +175,13 @@ form input[type=submit] {
     top:0;
     left:0;
     text-indent:-1000em;
-    height:25px;
-    line-height:25px;
     outline:none;
     overflow:hidden;
     border: none;
 }
 
 .star-rating a:hover {
-    background-position: 0px -25px;
+    background-position: 0px -20px;
 }
 
 .star-rating a.stars-1 {
@@ -211,13 +210,11 @@ form input[type=submit] {
 }
 
 .star-rating .current-rating {
-    z-index:1;
-    background-position: 0px -75px;
+    background-position: 0px -30px;
 }
 
 .star-rating .current-user-rating {
-    z-index:1;
-    background-position: 0px -50px;
+    background-position: 0px -10px;
 }
 
 /* for an inline rater */
@@ -225,35 +222,6 @@ form input[type=submit] {
     display:-moz-inline-block;
     display:-moz-inline-box;
     display:inline-block;
-}
-
-/* smaller star */
-.small-star {
-    width:50px;
-    height:10px;
-}
-
-.small-star,
-.small-star a:hover,
-.small-star a:active,
-.small-star a:focus,
-.small-star .current-user-rating,
-.small-star .current-rating {
-    background-image: url('../images/icons/rating_star_small.gif');
-    line-height: 10px;
-    height: 10px;
-}
-
-.small-star a:hover {
-    background-position: 0px -20px;
-}
-
-.small-star .current-rating {
-    background-position: 0px -30px;
-}
-
-.small-star .current-user-rating {
-    background-position: 0px -10px;
 }
 
 /*
@@ -283,26 +251,6 @@ ul.tag-cloud li.tag4 { font-size: 1.7em; }
 ul.tag-cloud li.tag5 { font-size: 1.7em; font-weight: bold; }
 ul.tag-cloud li.tag6 { font-size: 2em; font-weight: bold;}
 ul.tag-cloud li.tag7 { font-size: 2.5em; font-weight: bold; }
-
-/*
-** Favicons
-*/
-
-ul.favicons {
-    list-style-image: none;
-    list-style-type: none;
-    margin: 0;
-    padding: 0;
-}
-
-ul.favicons li {
-    margin-right: 0.5em;
-    margin-bottom: 0.5em;
-}
-
-ul.favicons li img {
-    vertical-align: middle;
-}
 
 /*
 ** CAA image viewer

root/release/index.tt
@@ -119,7 +119,7 @@
               [% UNLESS hide_rating %]
                 <td class="rating c" data-bind="with: recording">
                   <span class="inline-rating">
-                    <span class="star-rating small-star">
+                    <span class="star-rating">
                       <!-- ko if: userRating !== null -->
                       <span class="current-user-rating"
                             data-bind="style: { width: userRating + '%' }, text: MB.utility.percentOf(userRating, 5)"></span>

commit cc45aea1449ece3bef878be218ca1774c5fcba38

MBS-8268: Fix an issue with rating stars display
Before commit 9ca6bafa879473a23d7bb205fa9ccb01f0a4cc40, which
inadvertently removed the properties "background-repeat: repeat-x" and
"background-position: left -1000px" from elements of the rating
control, rating links were transparent unless the mouse hovered above
them. With that change, they became grey when having focus, such as
after a mouse click, partially covering the current rating and the
other rating links; in many cases, it appeared to the user as if
clicking had not had any effect. The regular display was restored only
when the user clicked elsewhere, thereby removing the focus from the
rating link.

While the old CSS before the commit mentioned above did not show these
issues for mouse users, it did not work very well for keyboard users
who tabbed to a rating link: because the link stayed transparent when
having focus, there was no indication to the user about their current
position. Hover styling is never invoked for keyboard users.

Due to this difference, it doesn't appear to be possible to have CSS
styling that works well for both user groups. Therefore, the following
two steps are implemented:
1. The stars are shown green when a rating link has focus, i.e. the
   same colour used for hover. This provides keyboard users with the
   same visual indication as mouse users before clicking, but causes
   the stars to remain green after a click even when the mouse pointer
   has been moved elsewhere.
2. To remedy this, focus is removed from the rating link after its
   activation, via JavaScript. It is instead moved to the container,
   i.e. in the tab order immediately before the rating links.

root/components/rating-macros.tt
@@ -16,7 +16,7 @@ END -%]
 
 [%- MACRO rating_stars(entity, prevent_rating) BLOCK -%]
 <span class="inline-rating">
-    <span class="star-rating">
+    <span class="star-rating" tabindex="-1">
         [%- current_star_rating = entity.user_rating ? 5 * entity.user_rating / 100 : 0 -%]
         [%- IF entity.user_rating -%]
         <span class="current-user-rating" style="width:[% entity.user_rating %]%;">[% current_star_rating %]</span>

root/static/scripts/common/ratings.js
@@ -19,9 +19,10 @@ $(document).on("click", "span.star-rating a", function () {
 
     $.getJSON(url, function (data) {
         var currentRatingSpan = $ratingLink.siblings('span');
+        var container = $ratingLink.parent();
         if (!currentRatingSpan.length) {
             currentRatingSpan = $('<span/>');
-            $ratingLink.parent().prepend(currentRatingSpan);
+            container.prepend(currentRatingSpan);
         }
         var rating;
         if (data.rating > 0) {
@@ -46,7 +47,10 @@ $(document).on("click", "span.star-rating a", function () {
             currentRatingSpan.remove();
         }
 
-        $ratingLink.parent().children('a').each(function (i) {
+        // Take focus away from the clicked link as a visual indication
+        container.focus();
+
+        container.children('a').each(function (i) {
             var originalRating = 100 * (1 + i) / 5;
             var newRating = data.rating == originalRating ? 0 : originalRating;
             var oldRatingMatch = this.href.match(/rating=(\d+)/);

root/static/styles/widgets.less
@@ -152,7 +152,6 @@ div.buttons, span.buttons { display: inline-block; }
         top:0;
         left:0;
         text-indent:-1000em;
-        outline:none;
         overflow:hidden;
         border: none;
     }
@@ -164,9 +163,9 @@ div.buttons, span.buttons { display: inline-block; }
     .current-user-rating,
     .current-rating {
         background-image: data-uri('../images/icons/rating_star_small.gif');
+        background-position: 0px 0px;   // grey star
+        outline: none;
     }
-
-    background-position: 0px 0px;       // grey star
 
     .current-rating {
         background-position: 0px -30px; // blue star
@@ -174,6 +173,8 @@ div.buttons, span.buttons { display: inline-block; }
     .current-user-rating {
         background-position: 0px -10px; // yellow star
     }
+    a:active,
+    a:focus,
     a:hover {
         background-position: 0px -20px; // green star
     }