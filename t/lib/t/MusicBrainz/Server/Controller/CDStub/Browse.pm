36d87afe728a676f6b9c092c5715fa4390d98e1f
Michael Wiencek <mwtuea@gmail.com>
2017-01-10 14:36:53 -0600

    Make CDStub test independent of the current year
    Fixes the lexically-scoped wrapper that ocharles added to wrap the
    correct subroutine.

--- t/lib/t/MusicBrainz/Server/Controller/CDStub/Browse.pm
+++ t/lib/t/MusicBrainz/Server/Controller/CDStub/Browse.pm
@@ -2,8 +2,6 @@ package t::MusicBrainz::Server::Controller::CDStub::Browse;
 use Test::Routine;
 use Test::More;
 use MusicBrainz::Server::Test qw( html_ok );
-
-use DateTime;
 use Hook::LexWrap;
 
 with 't::Mechanize', 't::Context';
@@ -18,11 +16,11 @@ MusicBrainz::Server::Test->prepare_raw_test_database($c, '+cdstub_raw');
 
 {
     # This test is dependent on the current time to generate the 'x years ago'
-    # content. I'm using a lexically scoped wrapper around DateTime::now in
+    # content. I'm using a lexically scoped wrapper around Date::Calc::Today in
     # order to 'lock' the date to 2012/01/01. -- ocharles
-    my $wrapper = wrap 'DateTime::now',
+    my $wrapper = wrap 'Date::Calc::Today',
         post => sub {
-            $_[-1] = DateTime->new( year => 2012, month => 01, day => 01 );
+            $_[-1] = [2012, 1, 1];
         };
 
     $mech->get_ok("/cdstub/browse", 'fetch top cdstubs page');
@@ -34,8 +32,8 @@ html_ok($mech->content);
 $mech->title_like(qr/Top CD Stubs/, 'title is correct');
 $mech->content_like(qr/Test Artist/, 'content has artist name');
 $mech->content_like(qr/YfSgiOEayqN77Irs.VNV.UNJ0Zs-/, 'content has disc id');
-$mech->content_like(qr/Added 16 years ago/, 'content has added timestamp');
-$mech->content_like(qr/last modified 15 years ago/, 'content has last modified timestamp');
+$mech->content_like(qr/Added 12 years ago/, 'content has added timestamp');
+$mech->content_like(qr/last modified 11 years ago/, 'content has last modified timestamp');
 
 };
 
