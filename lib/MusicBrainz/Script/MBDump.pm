b2809ab2e89d6a278ea7665da1c6b9baa1ebe474
Michael Wiencek <mwtuea@gmail.com>
2017-01-11 01:56:20 -0600

    Move make_tar to Script::MBDump

--- lib/MusicBrainz/Script/MBDump.pm
+++ lib/MusicBrainz/Script/MBDump.pm
@@ -3,15 +3,19 @@ package MusicBrainz::Script::MBDump;
 use base 'Exporter';
 
 use DBDefs;
+use MusicBrainz::Script::Utils qw( log );
+use Time::HiRes qw( gettimeofday tv_interval );
 
 # overrides the user-specified $keep_files
 our $erase_files_on_exit = 1;
 our $export_dir = '';
+our $output_dir = '.';
 our $row_counts = {};
 
 our @EXPORT_OK = qw(
     copy_readme
     gpg_sign
+    make_tar
     write_file
 );
 
@@ -39,6 +43,39 @@ sub gpg_sign {
         print STDERR "Failed to sign $file_to_be_signed\n",
                      "GPG returned $?\n";
     }
+}
+
+sub make_tar {
+    my ($tar_file, @tables) = @_;
+
+    my @files = map { "mbdump/$_" }
+                grep { $row_counts->{$_} } @tables;
+
+    # These ones go first, so MBImport can quickly find them.
+    unshift @files, qw(
+        TIMESTAMP
+        COPYING
+        README
+        REPLICATION_SEQUENCE
+        SCHEMA_SEQUENCE
+    );
+
+    my $t0 = [gettimeofday];
+    log("Creating $tar_file");
+    chomp (my $tar_bin = `which gtar` || `which tar`);
+    system $tar_bin,
+           '-C', $export_dir,
+           '--bzip2',
+           '--create',
+           '--verbose',
+           '--file', "$output_dir/$tar_file",
+           '--',
+           @files;
+
+    $? == 0 or die "Tar returned $?";
+    log(sprintf "Tar completed in %d seconds\n", tv_interval($t0));
+
+    gpg_sign("$output_dir/$tar_file");
 }
 
 sub write_file {
