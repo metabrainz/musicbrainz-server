dcc80c04e4d44fe11f361f2fbd9e0e5fe59b9935
Michael Wiencek <mwtuea@gmail.com>
2017-01-11 01:43:12 -0600

    Move gpg_sign to Script::MBDump

--- lib/MusicBrainz/Script/MBDump.pm
+++ lib/MusicBrainz/Script/MBDump.pm
@@ -2,6 +2,8 @@ package MusicBrainz::Script::MBDump;
 
 use base 'Exporter';
 
+use DBDefs;
+
 # overrides the user-specified $keep_files
 our $erase_files_on_exit = 1;
 our $export_dir = '';
@@ -9,6 +11,7 @@ our $row_counts = {};
 
 our @EXPORT_OK = qw(
     copy_readme
+    gpg_sign
     write_file
 );
 
@@ -18,6 +21,24 @@ The files in this directory are snapshots of the MusicBrainz database,
 in a format suitable for import into a PostgreSQL database. To import
 them, you need a compatible version of the MusicBrainz server software.
 EOF
+}
+
+sub gpg_sign {
+    my ($file_to_be_signed) = @_;
+
+    my $sign_with = DBDefs->GPG_SIGN_KEY;
+    return unless $sign_with;
+
+    system 'gpg',
+           '--default-key', $sign_with,
+           '--detach-sign',
+           '--armor',
+           $file_to_be_signed;
+
+    if ($? != 0) {
+        print STDERR "Failed to sign $file_to_be_signed\n",
+                     "GPG returned $?\n";
+    }
 }
 
 sub write_file {
