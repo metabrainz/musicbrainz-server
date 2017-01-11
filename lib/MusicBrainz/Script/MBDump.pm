304cc8e41f7fbf3df576fbb2a0d72f57a1dd3541
Michael Wiencek <mwtuea@gmail.com>
2017-01-11 01:29:53 -0600

    Move write_file to Script::MBDump

--- lib/MusicBrainz/Script/MBDump.pm
+++ lib/MusicBrainz/Script/MBDump.pm
@@ -1,8 +1,22 @@
 package MusicBrainz::Script::MBDump;
 
+use base 'Exporter';
+
 # overrides the user-specified $keep_files
 our $erase_files_on_exit = 1;
 our $export_dir = '';
 our $row_counts = {};
+
+our @EXPORT_OK = qw(
+    write_file
+);
+
+sub write_file {
+    my ($file, $contents) = @_;
+
+    open(my $fh, ">$export_dir/$file") or die $!;
+    print $fh $contents or die $!;
+    close $fh or die $!;
+}
 
 1;
