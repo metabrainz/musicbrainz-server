1ab7fd1a12496c70bad9903250b7252a23be88d0
Michael Wiencek <mwtuea@gmail.com>
2017-02-07 21:21:43 -0600

    Create the build directory
    Otherwise the gulpfile fails when it tries to create rev-manifest.json
    where it doesn't exist.

--- script/compile_resources.sh
+++ script/compile_resources.sh
@@ -3,6 +3,9 @@
 MB_SERVER_ROOT=$(cd "$(dirname "${BASH_SOURCE[0]}")/../" && pwd)
 cd "$MB_SERVER_ROOT"
 
+BUILD_DIR=${MBS_STATIC_BUILD_DIR:-root/static/build/}
+mkdir -p "$BUILD_DIR"
+
 # Remove the symbolic link first. Since it points to a directory, we'd
 # otherwise need to pass the no-dereference flag to ln, but the name of that
 # flag differs between GNU coreutils and BSD/macOS.
