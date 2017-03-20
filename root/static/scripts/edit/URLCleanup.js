4a7f1fcceaf3e3f9d9b58e5adf6b80e0b02a3234
yvanzo <yvanzo@metabrainz.org>
2017-03-20 20:56:54 +0100

    MBS-9291: Add Dailymotion to URL cleanup
    - Match `dailymotion.com` URLs with `videochannel` and
      `streamingmusic` relationship types
    
    - Normalize URLs to `http` scheme and `www` subdomain
    - Drop possible playlist prefix and slug suffix for videos
    - Drop trailing query and fragment for videos and channels
    
    - Validate clean URLs only
    - Restrict to `videos/` paths for `streamingmusic` relationships
    - Restrict to other clean paths for `videochannel` relationships
    - Discard all other paths and relationship types

--- root/static/scripts/edit/URLCleanup.js
+++ root/static/scripts/edit/URLCleanup.js
@@ -863,6 +863,44 @@ const CLEANUPS = {
       return /^http:\/\/viaf\.org\/viaf\/[1-9][0-9]*$/.test(url);
     }
   },
+  dailymotion: {
+    match: [new RegExp("^(https?://)?([^/]+\\.)?(dailymotion\\.com/)", "i")],
+    type: _.defaults({}, LINK_TYPES.videochannel, LINK_TYPES.streamingmusic),
+    clean: function (url) {
+      var m = /^(?:https?:\/\/)?(?:www\.)?dailymotion\.com\/((([^\/?#]+)(?:\/[^?#]*)?)(?:\?[^#]*)?(?:#(.+)?)?)$/.exec(url);
+      if (m) {
+        var afterSlash = m[1];
+        var path = m[2];
+        var root = m[3];
+        var fragment = m[4];
+        switch (root) {
+          case 'playlist':
+            afterSlash = /^video=/.test(fragment) ? fragment.replace('=', '/') : afterSlash;
+            break;
+          case 'video':
+            afterSlash = path.replace(/([^_]+).*/, "$1");
+            break;
+          default:
+            afterSlash = new RegExp('^' + root + '/*$').test(path) ? root : afterSlash;
+            break;
+        }
+        return 'http://www.dailymotion.com/' + afterSlash;
+      }
+      return url;
+    },
+    validate: function (url, id) {
+      var m = /^http:\/\/www\.dailymotion\.com\/(?:(video\/)?[^\/?#]+)$/.exec(url);
+      if (m) {
+        var prefix = m[1];
+        if (_.includes(LINK_TYPES.videochannel, id)) {
+          return prefix === undefined;
+        } else {
+          return prefix === 'video/';
+        }
+      }
+      return false;
+    }
+  },
   twitch: {
     match: [new RegExp("^(https?://)?([^/]+\\.)?(twitch\\.tv/)", "i")],
     type: _.defaults({}, LINK_TYPES.videochannel, LINK_TYPES.streamingmusic),
