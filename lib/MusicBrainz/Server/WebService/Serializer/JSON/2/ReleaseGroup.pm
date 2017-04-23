afa18d882c002f81372d6ea9688b7c20440662d5
Michael Wiencek <mwtuea@gmail.com>
2017-04-23 10:09:10 -0500

    Replace JSON::2::Role::Aliases with function

--- lib/MusicBrainz/Server/WebService/Serializer/JSON/2/ReleaseGroup.pm
+++ lib/MusicBrainz/Server/WebService/Serializer/JSON/2/ReleaseGroup.pm
@@ -3,7 +3,6 @@ use Moose;
 use MusicBrainz::Server::WebService::Serializer::JSON::2::Utils qw( serialize_entity list_of );
 
 extends 'MusicBrainz::Server::WebService::Serializer::JSON::2';
-with 'MusicBrainz::Server::WebService::Serializer::JSON::2::Role::Aliases';
 with 'MusicBrainz::Server::WebService::Serializer::JSON::2::Role::Annotation';
 with 'MusicBrainz::Server::WebService::Serializer::JSON::2::Role::GID';
 with 'MusicBrainz::Server::WebService::Serializer::JSON::2::Role::Rating';
