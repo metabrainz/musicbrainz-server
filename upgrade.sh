metabrainz/musicbrainz-server

commit 22a5d75fd08a523a7d211d7b6e2f88a72bed536b

Remove ShowDBDefs
Introduces admin/config.sh: These configuration settings aren't used outside of
admin-related shell scripts (and likely by no one outside of us in production),
so this makes them easier to access and removes a perl dependency in some cases
(one less place we need to worry about carton exec -- though this point is moot
in cases where the shell script invokes another perl script).

The only remaining case was upgrade.sh, where we just invoke perl to get the
two settings we need directly.

upgrade.sh
@@ -5,7 +5,8 @@ set -o errexit
 MB_SERVER_ROOT=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 cd "$MB_SERVER_ROOT"
 
-eval `./admin/ShowDBDefs`
+DB_SCHEMA_SEQUENCE=$(perl -Ilib -e 'use DBDefs; print DBDefs->DB_SCHEMA_SEQUENCE;')
+REPLICATION_TYPE=$(perl -Ilib -e 'use DBDefs; print DBDefs->REPLICATION_TYPE;')
 
 NEW_SCHEMA_SEQUENCE=23
 OLD_SCHEMA_SEQUENCE=$((NEW_SCHEMA_SEQUENCE - 1))

.gitignore
@@ -1,3 +1,4 @@
+/admin/config.user.sh
 /admin/vh_httpd.conf
 /admin/startup.pl
 /admin/apache/vh_mb_backend.conf

admin/CopyReplication
@@ -5,7 +5,7 @@ set -o errexit
 MB_SERVER_ROOT=$(cd "$(dirname "${BASH_SOURCE[0]}")/../" && pwd)
 cd "$MB_SERVER_ROOT"
 
-eval `./admin/ShowDBDefs`
+source admin/config.sh
 
 TEMP_DIR=$1 ; shift
 

admin/RunExport
@@ -9,7 +9,7 @@ trap 'echo "Disk space when RunExport ends:" ; df -m' 0
 MB_SERVER_ROOT=$(cd "$(dirname "${BASH_SOURCE[0]}")/../" && pwd)
 cd "$MB_SERVER_ROOT"
 
-eval `./admin/ShowDBDefs`
+source admin/config.sh
 
 . ./admin/functions.sh
 make_temp_dir

admin/ShowDBDefs
@@ -1,42 +1,0 @@
-#!/usr/bin/env perl
-
-use warnings;
-# vi: set ts=4 sw=4 :
-
-#____________________________________________________________________________
-#
-#   MusicBrainz -- the open internet music database
-#
-#   Copyright (C) 2002 Robert Kaye
-#
-#   This program is free software; you can redistribute it and/or modify
-#   it under the terms of the GNU General Public License as published by
-#   the Free Software Foundation; either version 2 of the License, or
-#   (at your option) any later version.
-#
-#   This program is distributed in the hope that it will be useful,
-#   but WITHOUT ANY WARRANTY; without even the implied warranty of
-#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#   GNU General Public License for more details.
-#
-#   You should have received a copy of the GNU General Public License
-#   along with this program; if not, write to the Free Software
-#   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
-#
-#   $Id$
-#____________________________________________________________________________
-
-# This script echoes the settings in DBDefs.pm to standard output, in
-# a format usable by the shell (e.g. eval `./admin/ShowDBDefs`)
-
-use strict;
-use warnings;
-
-use FindBin;
-use lib "$FindBin::Bin/../lib";
-
-use DBDefs;
-use String::ShellQuote;
-
-my $export = DBDefs->get_environment_hash;
-printf("export %s=%s\n", $_, shell_quote($export->{$_})) for sort keys %$export;

admin/config.default.sh
@@ -1,0 +1,24 @@
+#!/bin/bash
+
+# Where to send admin emails (currently only ModBot output).
+ADMIN_EMAILS=root
+
+# Were to put database exports, and replication data, for public consumption;
+# who should own them, and what mode they should have.
+FTP_DATA_DIR=/var/ftp/pub/musicbrainz/data
+FTP_USER=musicbrainz
+FTP_GROUP=musicbrainz
+FTP_DIR_MODE=755
+FTP_FILE_MODE=644
+
+# Where to back things up to, who should own the backup files, and what mode
+# those files should have.
+# The backups include a full database export, and all replication data.
+BACKUP_DIR=/home/musicbrainz/backup
+BACKUP_USER=musicbrainz
+BACKUP_GROUP=musicbrainz
+BACKUP_DIR_MODE=700
+BACKUP_FILE_MODE=600
+
+RSYNC_FULLEXPORT_SERVER='ftp-data@ftp-data.localdomain'
+RSYNC_REPLICATION_SERVER='metabrainz@sakura.localdomain'

admin/config.sh
@@ -1,0 +1,11 @@
+#!/bin/bash
+
+pushd "$(dirname "${BASH_SOURCE[0]}")"
+
+source config.default.sh
+
+if [ -e config.user.sh ]; then
+    source config.user.sh
+fi
+
+popd

admin/cron/daily.sh
@@ -5,7 +5,7 @@ export PATH=/usr/local/bin:$PATH
 MB_SERVER_ROOT=$(cd "$(dirname "${BASH_SOURCE[0]}")/../../" && pwd)
 cd "$MB_SERVER_ROOT"
 
-eval `./admin/ShowDBDefs`
+source admin/config.sh
 
 # Only run one "daily.sh" at a time
 if [ "${1:-}" != "gotlock" ]

commit 5b2bd1153a1435a752576db1f5dfe458a4b6d11b

Add missing RT_* constants to upgrade.sh
Broken in 22a5d75fd08a523a7d211d7b6e2f88a72bed536b where these were
previously magically obtained from the DBDefs module, where they are
imported.

upgrade.sh
@@ -11,6 +11,10 @@ REPLICATION_TYPE=$(perl -Ilib -e 'use DBDefs; print DBDefs->REPLICATION_TYPE;')
 NEW_SCHEMA_SEQUENCE=24
 OLD_SCHEMA_SEQUENCE=$((NEW_SCHEMA_SEQUENCE - 1))
 
+RT_MASTER=1
+RT_SLAVE=2
+RT_STANDALONE=3
+
 ################################################################################
 # Assert pre-conditions
 