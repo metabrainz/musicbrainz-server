10d2fa22c0dbaeddfd72b1e431a07499f55bc270
Michael Wiencek <mwtuea@gmail.com>
2017-09-01 23:50:11 -0500

    json_dump SQL schema

---
+++ admin/sql/json_dump/DropIndexes.sql
@@ -0,0 +1,7 @@
+-- Automatically generated, do not edit.
+\unset ON_ERROR_STOP
+
+SET search_path = json_dump, public;
+
+DROP INDEX deleted_entities_idx_replication_sequence;
+DROP INDEX tmp_checked_entities_idx_uniq;
---
+++ admin/sql/json_dump/DropTables.sql
@@ -0,0 +1,19 @@
+-- Automatically generated, do not edit.
+\unset ON_ERROR_STOP
+
+SET search_path = json_dump, public;
+
+DROP TABLE area_json;
+DROP TABLE artist_json;
+DROP TABLE control;
+DROP TABLE deleted_entities;
+DROP TABLE event_json;
+DROP TABLE instrument_json;
+DROP TABLE label_json;
+DROP TABLE place_json;
+DROP TABLE recording_json;
+DROP TABLE release_group_json;
+DROP TABLE release_json;
+DROP TABLE series_json;
+DROP TABLE tmp_checked_entities;
+DROP TABLE work_json;
