0183fb71b5f6b02ec458882850ff5cfca5a70d89
Michael Wiencek <mwtuea@gmail.com>
2017-10-09 13:11:01 -0500

    Add flow types to the tag editor

---
+++ root/static/scripts/common/components/TagLink.js
@@ -0,0 +1,14 @@
+// @flow
+// Copyright (C) 2015 MetaBrainz Foundation
+//
+// This file is part of MusicBrainz, the open internet music database,
+// and is licensed under the GPL version 2, or (at your option) any
+// later version: http://www.gnu.org/licenses/gpl-2.0.txt
+
+const React = require('react');
+
+const TagLink = ({tag}: {tag: string}) => (
+  <a href={`/tag/${encodeURIComponent(tag)}`}>{tag}</a>
+);
+
+module.exports = TagLink;
