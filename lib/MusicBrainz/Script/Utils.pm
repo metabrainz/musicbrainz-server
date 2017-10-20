7ead65df47e136974d7ee051728a475b66100201
Michael Wiencek <mwtuea@gmail.com>
2017-10-20 18:40:51 -0500

    Move get_primary_keys to Script::Utils

--- lib/MusicBrainz/Script/Utils.pm
+++ lib/MusicBrainz/Script/Utils.pm
@@ -1,8 +1,39 @@
 package MusicBrainz::Script::Utils;
 
+use feature 'state';
+
 use base 'Exporter';
 
-our @EXPORT_OK = qw( log retry );
+our @EXPORT_OK = qw( get_primary_keys log retry );
+
+=sub get_primary_keys
+
+Get a list of primary key column names for $schema.$table.
+
+=cut
+
+sub get_primary_keys($$$) {
+    my ($c, $schema, $table) = @_;
+
+    state $cache = {};
+    if (defined $cache->{$table}) {
+        return @{ $cache->{$table} };
+    }
+
+    # retry: transient "server closed the connection unexpectedly",
+    # "no statement executing", and "Field 'attnum' does not exist" errors
+    # have happened here.
+    my @keys = retry(
+        sub { $c->sql->dbh->primary_key(undef, $schema, $table) },
+        reason => 'getting primary keys',
+    );
+    @keys = map {
+        # Some columns are wrapped in quotes, others aren't...
+        s/^"(.*?)"$/$1/r
+    } @keys;
+    $cache->{$table} = \@keys;
+    return @keys;
+}
 
 =sub log
 
