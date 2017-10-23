c67ffa745480b236c62aafbdc7552477fdc05a8a
yvanzo <yvanzo@metabrainz.org>
2017-10-23 22:03:03 +0200

    Partially revert 1330e433fba6c8b6c700ed2a8bb21afdff547b2e
    because MBS-9519 supersedes MBS-9353 and MBS-9355.

--- t/lib/t/MusicBrainz/Server/Controller/User/Edit.pm
+++ t/lib/t/MusicBrainz/Server/Controller/User/Edit.pm
@@ -22,12 +22,19 @@ $mech->submit_form( with_fields => { username => 'new_editor', password => 'pass
 $mech->get_ok('/account/edit');
 html_ok($mech->content);
 $mech->submit_form( with_fields => {
+    'profile.website' => 'foo',
+    'profile.biography' => 'hello world!',
+} );
+$mech->content_contains('Invalid URL format', "Invalid URL format 'foo' triggers validation failure.");
+$mech->submit_form( with_fields => {
     'profile.birth_date.year' => 0,
     'profile.birth_date.month' => 1,
     'profile.birth_date.day' => 1
 } );
 $mech->content_contains('invalid date', "Invalid date 0-1-1 triggers validation failure.");
 $mech->submit_form( with_fields => {
+    'profile.website' => 'http://example.com/~new_editor/',
+    'profile.biography' => 'hello world!',
     'profile.email' => 'new_email@example.com',
     'profile.birth_date.year' => '',
     'profile.birth_date.month' => '',
@@ -53,64 +60,6 @@ $mech->content_contains("Thank you, your email address has now been verified!");
 $mech->get('/user/new_editor');
 $mech->content_contains('new_email@example.com');
 
-
-};
-
-test 'Beginners/limited users cannot edit website and biography' => sub {
-    my $test = shift;
-    my $mech = $test->mech;
-    my $c    = $test->c;
-
-    MusicBrainz::Server::Test->prepare_test_database($c, '+editor');
-
-    $mech->get('/login');
-    $mech->submit_form( with_fields => { username => 'new_editor', password => 'password' } );
-
-    $mech->get_ok('/account/edit');
-    html_ok($mech->content);
-
-    my $tx = test_xpath_html($mech->content);
-    $tx->ok('//input[@id="id-profile.email"]', 'email field for all users');
-    $tx->not_ok('//input[@id="id-profile.website"]', 'no website field for beginners/limited users');
-    $tx->not_ok('//textarea[@id="id-profile.biography"]', 'no biography field for beginners/limited users');
-
-    $test->c->sql->do(<<EOSQL);
-INSERT INTO edit (id, editor, type, status, expire_time, autoedit) VALUES
-    ( 1, 1, 1, $STATUS_APPLIED, now(), 0),
-    ( 2, 1, 1, $STATUS_APPLIED, now(), 0),
-    ( 3, 1, 1, $STATUS_APPLIED, now(), 0),
-    ( 4, 1, 1, $STATUS_APPLIED, now(), 0),
-    ( 5, 1, 1, $STATUS_APPLIED, now(), 0),
-    ( 6, 1, 1, $STATUS_APPLIED, now(), 0),
-    ( 7, 1, 1, $STATUS_APPLIED, now(), 0),
-    ( 8, 1, 1, $STATUS_APPLIED, now(), 0),
-    ( 9, 1, 1, $STATUS_APPLIED, now(), 0),
-    (10, 1, 1, $STATUS_APPLIED, now(), 0);
-EOSQL
-
-    $mech->get_ok('/account/edit');
-    html_ok($mech->content);
-
-    $tx = test_xpath_html($mech->content);
-    $tx->ok('//input[@id="id-profile.email"]', 'email field for all users');
-    $tx->ok('//input[@id="id-profile.website"]', 'website field for normal (not imited) users');
-    $tx->ok('//textarea[@id="id-profile.biography"]', 'biography field for normal (not beginner/limited) users');
-
-    $mech->submit_form( with_fields => {
-        'profile.website' => 'foo',
-        'profile.biography' => 'hello world!',
-    } );
-    $mech->content_contains('Invalid URL format', "Invalid URL format 'foo' triggers validation failure.");
-    $mech->submit_form( with_fields => {
-        'profile.website' => 'http://example.com/~new_editor/',
-        'profile.biography' => 'hello world!',
-        'profile.email' => 'new_email@example.com',
-    } );
-    $mech->content_contains('Your profile has been updated');
-
-    $mech->get('/user/new_editor');
-    $mech->content_contains('http://example.com/~new_editor/');
-    $mech->content_contains('hello world!');
 };
 
 test 'After removing email address, editors cannot edit' => sub {
