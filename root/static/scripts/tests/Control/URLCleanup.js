7443bbc8cd8f0be7729ae280e93443ec4bb86a29
haruute0 <haruute0@gmail.com>
2017-12-19 21:12:22 +0700

    Revert "Add only_valid_entity_types for Drip"
    This reverts commit c3a6900a593b3f7729272c34b136f205b649be4b.

--- root/static/scripts/tests/Control/URLCleanup.js
+++ root/static/scripts/tests/Control/URLCleanup.js
@@ -921,14 +921,18 @@ const {LINK_TYPES, cleanURL, guessType, validationRules} = require('../../edit/U
                      input_entity_type: 'artist',
             expected_relationship_type: 'patronage',
                     expected_clean_url: 'https://d.rip/ehaidle',
-               only_valid_entity_types: ['artist']
+        },
+        {
+                             input_url: 'https://d.rip/ehaidle/post/',
+                     input_entity_type: 'artist',
+            expected_relationship_type: 'patronage',
+                    expected_clean_url: 'https://d.rip/ehaidle',
         },
         {
                              input_url: 'https://d.rip/ehaidle/',
                      input_entity_type: 'artist',
             expected_relationship_type: 'patronage',
                     expected_clean_url: 'https://d.rip/ehaidle',
-               only_valid_entity_types: ['artist']
         },
         // Drip (old)
         {
@@ -936,7 +940,6 @@ const {LINK_TYPES, cleanURL, guessType, validationRules} = require('../../edit/U
                      input_entity_type: 'artist',
             expected_relationship_type: 'patronage',
                     expected_clean_url: 'https://d.rip/willits',
-               only_valid_entity_types: ['artist']
         },
         // e-onkyo music
         {
