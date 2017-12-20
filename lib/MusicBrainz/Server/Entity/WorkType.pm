308e89ac48e83635d3e09b71b64e3c2cca20889e
Eshan Singh <eshansingh@gmail.com>
2017-12-20 21:20:31 +0000

    Get all descriptions at once
    Plus translation fix. Thanks @mwiencek!

--- lib/MusicBrainz/Server/Entity/WorkType.pm
+++ lib/MusicBrainz/Server/Entity/WorkType.pm
@@ -14,6 +14,11 @@ sub l_name {
     return lp($self->name, 'work_type')
 }
 
+sub l_description {
+    my $self = shift;
+    return lp($self->description, 'work_type');
+}
+
 __PACKAGE__->meta->make_immutable;
 no Moose;
 1;
