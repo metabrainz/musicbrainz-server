62bce9ed47e69b92a7ab76df647c7044351ef86a
Michael Wiencek <mwtuea@gmail.com>
2018-02-02 14:52:07 -0600

    Add an i18n module for instrument_descriptions

---
+++ root/static/scripts/common/i18n/instrument_descriptions.js
@@ -0,0 +1,12 @@
+/* Copyright (C) 2018 MetaBrainz Foundation
+ *
+ * This file is part of MusicBrainz, the open internet music database,
+ * and is licensed under the GPL version 2, or (at your option) any
+ * later version: http://www.gnu.org/licenses/gpl-2.0.txt
+ */
+
+const wrapGettext = require('./wrapGettext');
+
+exports.l_instrument_descriptions = wrapGettext('dgettext', 'instrument_descriptions');
+exports.ln_instrument_descriptions = wrapGettext('dngettext', 'instrument_descriptions');
+exports.lp_instrument_descriptions = wrapGettext('dpgettext', 'instrument_descriptions');
