44dd3963fce458a729e22f71e3f29a42c5f200c7
Sambhav Kothari <sambhavs.email@gmail.com>
2018-02-17 00:32:53 +0530

    Run eslint on ArtistCreditLink.js

--- root/static/scripts/common/components/ArtistCreditLink.js
+++ root/static/scripts/common/components/ArtistCreditLink.js
@@ -1,7 +1,9 @@
-// This file is part of MusicBrainz, the open internet music database.
-// Copyright (C) 2015–2016 MetaBrainz Foundation
-// Licensed under the GPL version 2, or (at your option) any later version:
-// http://www.gnu.org/licenses/gpl-2.0.txt
+/*
+ * This file is part of MusicBrainz, the open internet music database.
+ * Copyright (C) 2015–2016 MetaBrainz Foundation
+ * Licensed under the GPL version 2, or (at your option) any later version:
+ * http://www.gnu.org/licenses/gpl-2.0.txt
+ */
 
 const React = require('react');
 
@@ -9,9 +11,9 @@ const EntityLink = require('./EntityLink');
 
 const ArtistCreditLink = ({artistCredit, showDeleted = true, ...props}) => {
   const names = artistCredit.names;
-  let parts = [];
+  const parts = [];
   for (let i = 0; i < names.size; i++) {
-    let credit = names.get(i);
+    const credit = names.get(i);
     if (props.plain) {
       parts.push(credit.name);
     } else {
@@ -25,8 +27,9 @@ const ArtistCreditLink = ({artistCredit, showDeleted = true, ...props}) => {
           entity={artist}
           key={i}
           showDeleted={showDeleted}
-          target={props.target} />
-        );
+          target={props.target}
+        />,
+      );
     }
     parts.push(credit.joinPhrase);
   }
