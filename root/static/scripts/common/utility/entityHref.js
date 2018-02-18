b69f5fe7f67c3e7390f594150e0357973ae782a7
Michael Wiencek <mwtuea@gmail.com>
2018-02-18 15:11:39 -0600

    Add license, types to entityHref.js

--- root/static/scripts/common/utility/entityHref.js
+++ root/static/scripts/common/utility/entityHref.js
@@ -1,10 +1,24 @@
+/*
+ * @flow
+ * Copyright (C) 2015 MetaBrainz Foundation
+ *
+ * This file is part of MusicBrainz, the open internet music database,
+ * and is licensed under the GPL version 2, or (at your option) any
+ * later version: http://www.gnu.org/licenses/gpl-2.0.txt
+ */
+
 const {ENTITIES} = require('../constants');
 const nonEmpty = require('./nonEmpty');
 
 const leadingSlash = /^\/?(.*)/;
 
-function entityHref(entityType, id, subPath) {
-  let href = '/' + ENTITIES[entityType].url + '/' + encodeURIComponent(id);
+function entityHref(
+  entityType: $Keys<ENTITIES>,
+  id: number | string,
+  subPath?: string,
+) {
+  let href = '/' + ENTITIES[entityType].url + '/' +
+    encodeURIComponent(String(id));
 
   if (nonEmpty(subPath)) {
     subPath = subPath.replace(leadingSlash, '$1');
