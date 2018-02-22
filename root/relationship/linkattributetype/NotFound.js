9fe966656306c60da5bad5d74d57b2ffef79afb1
Michael Wiencek <mwtuea@gmail.com>
2018-02-22 14:24:33 -0600

    @flow and license header style

--- root/relationship/linkattributetype/NotFound.js
+++ root/relationship/linkattributetype/NotFound.js
@@ -1,8 +1,11 @@
-// Copyright (C) 2018 Shamroy Pellew
-//
-// This file is part of MusicBrainz, the open internet music database,
-// and is licensed under the GPL version 2, or (at your option) any
-// later version: http://www.gnu.org/licenses/gpl-2.0.txt
+/*
+ * @flow
+ * Copyright (C) 2018 Shamroy Pellew
+ *
+ * This file is part of MusicBrainz, the open internet music database,
+ * and is licensed under the GPL version 2, or (at your option) any
+ * later version: http://www.gnu.org/licenses/gpl-2.0.txt
+ */
 
 const React = require('react');
 const NotFound = require('../components/NotFound');
