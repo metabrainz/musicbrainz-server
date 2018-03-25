d4f3c3adbfdec1cebfd331b8fa9c6df1a0ba2622
Michael Wiencek <mwtuea@gmail.com>
2018-03-25 15:54:19 -0500

    Sidebar remove link component

---
+++ root/layout/components/sidebar/RemoveLink.js
@@ -0,0 +1,32 @@
+/*
+ * @flow
+ * Copyright (C) 2018 MetaBrainz Foundation
+ *
+ * This file is part of MusicBrainz, the open internet music database,
+ * and is licensed under the GPL version 2, or (at your option) any
+ * later version: http://www.gnu.org/licenses/gpl-2.0.txt
+ */
+
+import React from 'react';
+
+import {l} from '../../../static/scripts/common/i18n';
+import entityHref from '../../../static/scripts/common/utility/entityHref';
+
+type Props = {|
+  +entity:
+    | AreaT
+    | InstrumentT
+    | LabelT
+    | RecordingT
+    | ReleaseT,
+|};
+
+const RemoveLink = ({entity}: Props) => (
+  <li>
+    <a href={entityHref(entity, 'delete')}>
+      {l('Remove')}
+    </a>
+  </li>
+);
+
+export default RemoveLink;
