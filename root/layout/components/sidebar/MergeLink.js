4fe73796534a5cf5fe03ea11000bf00c90bec344
Michael Wiencek <mwtuea@gmail.com>
2018-03-25 15:54:04 -0500

    Sidebar merge link component

---
+++ root/layout/components/sidebar/MergeLink.js
@@ -0,0 +1,32 @@
+/*
+ * @flow
+ * Copyright (C) 2018 MetaBrainz Foundation
+ *
+ * This file is part of MusicBrainz, the open internet music database,
+ * and is licensed under the GPL version 2, or (at your option) any
+ * later version: http://www.gnu.org/licenses/gpl-2.0.txt
+ */
+
+import React from 'react';
+
+import {l} from '../../../static/scripts/common/i18n';
+
+type Props = {|
+  +entity: CoreEntityT,
+|};
+
+const mergeUrl = entity => {
+  const entityType = entity.entityType;
+  const id = encodeURIComponent(String(entity.id));
+  return `/${entityType}/merge_queue?add-to-merge=${id}`;
+};
+
+const MergeLink = ({entity}: Props) => (
+  <li>
+    <a href={mergeUrl(entity)}>
+      {l('Merge')}
+    </a>
+  </li>
+);
+
+export default MergeLink;
