044bcaeb17890c373facabcc7f1648165de0aecc
yvanzo <yvanzo@metabrainz.org>
2018-03-29 21:37:18 +0200

    Add test for browser accepted languages (MBS-8417)
    to fall back on, for Wikipedia extract.
    
    (*.lwp-mock file is generated by replacing 'playback' with 'record')

--- t/lib/t/MusicBrainz/Server/Data/WikipediaExtract.pm
+++ t/lib/t/MusicBrainz/Server/Data/WikipediaExtract.pm
@@ -124,4 +124,37 @@ test 'Request eo page via wikidata, fallback to de' => sub {
     LWP::UserAgent::Mockable->finished;
 };
 
+test 'Request ja page via wikidata, fallback to it (according to browser accepted language)' => sub {
+    my $test = shift;
+    my $c = $test->c;
+
+    # Assuming that browser accepted languages are set to Japanese (Japan) then Italian (Italy),
+    # set all_system_languages as usually set by sub build_languages_from_header
+    MusicBrainz::Server::Translation->instance->languages([
+        'ja-ja',
+        'ja',
+        'it-it',
+        'it',
+        'i-default'
+    ]);
+
+    LWP::UserAgent::Mockable->reset(
+      playback => $Bin.'/lwp-sessions/data_wikipedia.ja-it-fallback-browser.lwp-mock'
+    );
+
+    # german municipality of schladen-werla
+    my $entity = Wikidata->new(url => 'https://www.wikidata.org/wiki/Q1462109');
+    # No cache
+    my $extract = $c->model('WikipediaExtract')->get_extract([$entity], 'ja', cache_only => 1);
+    ok(!defined $extract);
+
+    # Now let it use the network
+    $extract = $c->model('WikipediaExtract')->get_extract([$entity], 'ja', cache_only => 0);
+    ok(defined $extract);
+
+    like($extract->content, qr{è un comune tedesco}, "contains italian text");
+
+    LWP::UserAgent::Mockable->finished;
+};
+
 1;
