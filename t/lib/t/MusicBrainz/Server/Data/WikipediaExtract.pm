1f9fb33d316464064a7fd29b97c44bdc58017942
yvanzo <yvanzo@metabrainz.org>
2018-03-30 01:11:03 +0200

    Add test for editor known languages (MBS-8417)
    to fall back on, for Wikipedia extract.
    
    (*.lwp-mock file is generated by replacing 'playback' with 'record')

--- t/lib/t/MusicBrainz/Server/Data/WikipediaExtract.pm
+++ t/lib/t/MusicBrainz/Server/Data/WikipediaExtract.pm
@@ -157,4 +157,49 @@ test 'Request ja page via wikidata, fallback to it (according to browser accepte
     LWP::UserAgent::Mockable->finished;
 };
 
+test 'Request tr page via wikidata, fallback to it (according to editor known language)' => sub {
+    my $test = shift;
+    my $c = $test->c;
+
+    # Assuming that browser accepted language is set to Finnish (Finland),
+    # set all_system_languages as usually set by sub build_languages_from_header
+    MusicBrainz::Server::Translation->instance->languages([
+        'fi-fi',
+        'fi',
+        'i-default'
+    ]);
+
+    # Set editor known languages to native Finnish and basic Macedonian
+    $c->sql->do(<<'EOSQL');
+INSERT INTO area (id, gid, name, type) VALUES
+  (221, '8a754a16-0027-3a29-b6d7-2b40ea0481ed', 'United Kingdom', 1);
+INSERT INTO iso_3166_1 (area, code) VALUES (221, 'GB');
+INSERT INTO editor (id, name, password, email, website, bio, member_since, email_confirm_date, last_login_date, privs, birth_date, area, gender, ha1) VALUES (1, 'Bob', '{CLEARTEXT}bob', 'bob@bob.bob', 'http://bob.bob/', 'Bobography', now(), now(), now(), 1, now(), 221, 1, '026299da47965340ef66ca485a57975d');
+INSERT INTO language (id, iso_code_2t, iso_code_1, name) VALUES (131, 'fin', 'fi', 'Finnish'), (254, 'mkd', 'mk', 'Macedonian');
+INSERT INTO editor_language (editor, language, fluency) VALUES (1, 131, 'native'), (1, 254, 'basic');
+EOSQL
+
+    my $model = $c->model('Editor');
+    my $bob = $model->get_by_id(1);
+    $c->model('EditorLanguage')->load_for_editor($bob);
+
+    LWP::UserAgent::Mockable->reset(
+      playback => $Bin.'/lwp-sessions/data_wikipedia.fi-mk-fallback-browser.lwp-mock'
+    );
+
+    # german municipality of schladen-werla
+    my $entity = Wikidata->new(url => 'https://www.wikidata.org/wiki/Q1462109');
+    # No cache
+    my $extract = $c->model('WikipediaExtract')->get_extract([$entity], 'fi', cache_only => 1, editor => $bob);
+    ok(!defined $extract);
+
+    # Now let it use the network
+    $extract = $c->model('WikipediaExtract')->get_extract([$entity], 'fi', cache_only => 0, editor => $bob);
+    ok(defined $extract);
+
+    like($extract->content, qr{општина во округот}, "contains macedonian text");
+
+    LWP::UserAgent::Mockable->finished;
+};
+
 1;
