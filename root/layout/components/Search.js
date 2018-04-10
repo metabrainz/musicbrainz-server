2b354515fe1306a8ed5412e82969ed07d9d2e2e3
yvanzo <yvanzo@metabrainz.org>
2018-04-10 23:44:09 +0200

    Group and tidy search type options up
    Search type options were loosely sorted by implementation date.
    
    This patch groups search type options: artists, their musical
    productions, other core entities, derived data, and so on.
    
    Groups are displayed in this order.  Elements of each group are
    sorted alphabetically according to the current UI language.
    
    Note: Groups are not visually separated from each other, because of
    CSS limitations regarding `option` and `optgroup` tags.

--- root/layout/components/Search.js
+++ root/layout/components/Search.js
@@ -11,38 +11,54 @@ import React from 'react';
 
 import * as manifest from '../../static/manifest';
 import * as DBDefs from '../../static/scripts/common/DBDefs';
-import {l, lp} from '../../static/scripts/common/i18n';
+import {compare, l, lp} from '../../static/scripts/common/i18n';
 
-/* eslint-disable sort-keys, flowtype/sort-keys */
-const TYPE_OPTIONS = {
-  artist:        l('Artist'),
-  release_group: l('Release Group'),
-  release:       l('Release'),
-  recording:     l('Recording'),
-  work:          l('Work'),
-  label:         l('Label'),
-  area:          l('Area'),
-  place:         l('Place'),
-  annotation:    l('Annotation'),
-  cdstub:        l('CD Stub'),
-  editor:        l('Editor'),
-  tag:           lp('Tag', 'noun'),
-  instrument:    l('Instrument'),
-  series:        lp('Series', 'singular'),
-  event:         l('Event'),
-  doc:           DBDefs.GOOGLE_CUSTOM_SEARCH ? l('Documentation') : null,
-};
-/* eslint-enable sort-keys, flowtype/sort-keys */
+const TYPE_OPTION_GROUPS = [
+  {
+    artist:        l('Artist'),
+  },
+  { // musical production
+    event:         l('Event'),
+    recording:     l('Recording'),
+    release:       l('Release'),
+    release_group: l('Release Group'),
+    series:        lp('Series', 'singular'),
+    work:          l('Work'),
+  },
+  { // other core entities
+    area:          l('Area'),
+    instrument:    l('Instrument'),
+    label:         l('Label'),
+    place:         l('Place'),
+  },
+  { // derived data
+    annotation:    l('Annotation'),
+    tag:           lp('Tag', 'noun'),
+  },
+  {
+    cdstub:        l('CD Stub'),
+  },
+  {
+    editor:        l('Editor'),
+  },
+  {
+    doc:           DBDefs.GOOGLE_CUSTOM_SEARCH ? l('Documentation') : null,
+  },
+];
 
 const searchOptions = (
   <select id="headerid-type" name="type">
-    {Object.keys(TYPE_OPTIONS).map(function (key, index) {
-      const text = TYPE_OPTIONS[key];
-      if (!text) {
-        return null;
-      }
-      return <option key={index} value={key}>{text}</option>;
-    })}
+    {TYPE_OPTION_GROUPS.map(<TogT: {}>(group: TogT, groupIndex) => (
+      Object.keys(group).sort(function (a, b) {
+        return compare(group[a], group[b]);
+      }).map(function (key, index) {
+        const text = group[key];
+        if (!text) {
+          return null;
+        }
+        return <option key={groupIndex + '.' + index} value={key}>{text}</option>;
+      })
+    ))}
   </select>
 );
 
