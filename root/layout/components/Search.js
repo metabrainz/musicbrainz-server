f558ddf7ba84728acaa61a9c8bf36a7ad0de5e11
yvanzo <yvanzo@metabrainz.org>
2018-04-10 12:38:50 +0200

    Format search form code with ESLint
    without changing keys order due to MBS-9672.

--- root/layout/components/Search.js
+++ root/layout/components/Search.js
@@ -13,24 +13,26 @@ import * as manifest from '../../static/manifest';
 import * as DBDefs from '../../static/scripts/common/DBDefs';
 import {l, lp} from '../../static/scripts/common/i18n';
 
+/* eslint-disable sort-keys, flowtype/sort-keys */
 const TYPE_OPTIONS = {
-  artist:         l('Artist'),
-  release_group:  l('Release Group'),
-  release:        l('Release'),
-  recording:      l('Recording'),
-  work:           l('Work'),
-  label:          l('Label'),
-  area:           l('Area'),
-  place:          l('Place'),
-  annotation:     l('Annotation'),
-  cdstub:         l('CD Stub'),
-  editor:         l('Editor'),
-  tag:            lp('Tag', 'noun'),
-  instrument:     l('Instrument'),
-  series:         lp('Series', 'singular'),
-  event:          l('Event'),
-  doc:            DBDefs.GOOGLE_CUSTOM_SEARCH ? l('Documentation') : null,
+  artist:        l('Artist'),
+  release_group: l('Release Group'),
+  release:       l('Release'),
+  recording:     l('Recording'),
+  work:          l('Work'),
+  label:         l('Label'),
+  area:          l('Area'),
+  place:         l('Place'),
+  annotation:    l('Annotation'),
+  cdstub:        l('CD Stub'),
+  editor:        l('Editor'),
+  tag:           lp('Tag', 'noun'),
+  instrument:    l('Instrument'),
+  series:        lp('Series', 'singular'),
+  event:         l('Event'),
+  doc:           DBDefs.GOOGLE_CUSTOM_SEARCH ? l('Documentation') : null,
 };
+/* eslint-enable sort-keys, flowtype/sort-keys */
 
 const searchOptions = (
   <select id="headerid-type" name="type">
