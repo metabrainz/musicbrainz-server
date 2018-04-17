29420433bc4bbbd156615cc01732efae630701ce
yvanzo <yvanzo@metabrainz.org>
2018-04-17 23:44:52 +0200

    MBS-6530: Remove the rest of Google Analytics

--- root/layout/components/Head.js
+++ root/layout/components/Head.js
@@ -96,21 +96,6 @@ const Head = (props) => (
         type="application/ld+json"
       />
     ) : null}
-
-    {DBDefs.GOOGLE_ANALYTICS_CODE ? (
-      <script type="text/javascript" dangerouslySetInnerHTML={{__html: `
-        var _gaq = _gaq || [];
-        _gaq.push(['_setAccount', '${DBDefs.GOOGLE_ANALYTICS_CODE}']);
-        _gaq.push(['_setCustomVar', 1, 'User is logged in', '${$c.user ? "Yes" : "No"}', 2]);
-        _gaq.push(['_trackPageview']);
-
-        (function () {
-          var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-          ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-          var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-        })();
-      `}}></script>
-    ) : null}
   </head>
 );
 
