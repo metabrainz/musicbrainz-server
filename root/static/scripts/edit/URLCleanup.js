558743f434fd5ce1f5fc512140042767e2b83757
Nicolás Tamargo <reosarevok@gmail.com>
2018-04-23 20:30:47 +0300

    MBS-9639: Extend Soundcloud relationship to places

--- root/static/scripts/edit/URLCleanup.js
+++ root/static/scripts/edit/URLCleanup.js
@@ -123,6 +123,7 @@ const LINK_TYPES = {
   soundcloud: {
     artist: "89e4a949-0976-440d-bda1-5f772c1e5710",
     label: "a31d05ba-3b82-47b2-ab8b-1fe73b5459e2",
+    place: "1cd2eb89-2997-4901-87e9-838ac9a68da9",
     series: "4789521b-57b9-4689-9644-46de63190f66"
   },
   blog: {
