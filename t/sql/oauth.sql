2e22fa4fba6c08666aa9eb50a50e7f7eb3c403a8
Michael Wiencek <mwtuea@gmail.com>
2018-06-20 05:21:32 -0500

    MBS-9744: /oauth2/userinfo should return UTF-8
    This removes the View::JSON module, which does bad things for browsers
    from 10 years ago, and encodes the JSON in the same way we do everywhere
    else.
    
    This also touches the tag /rating/rate endpoint, but should have no
    real effect there since it only returns numbers.

--- t/sql/oauth.sql
+++ t/sql/oauth.sql
@@ -18,4 +18,5 @@ INSERT INTO editor_oauth_token (editor, application, authorization_code, refresh
 INSERT INTO editor_oauth_token (editor, application, refresh_token, access_token, expire_time, scope)
     VALUES (1, 1, 'yi3qjrMf4hG9VVUxXMVIuQ', '7Fjfp0ZBr1KtDRbnfVdmIw', now() + interval '1 hour', 1),
            (1, 1, 'uTuPnUfMRQPx8HBnHf22eg', 'Nlaa7v15QHm9g8rUOmT3dQ', now() + interval '1 hour', 1 + 2 + 4 + 8 + 16 + 32 + 64 + 128),
-           (1, 1, 'xi2Aq4235zX32XUx1231u1', '3fxf40Z5r6K78D9b031xaw', now() - interval '1 hour', 1);
+           (1, 1, 'xi2Aq4235zX32XUx1231u1', '3fxf40Z5r6K78D9b031xaw', now() - interval '1 hour', 1),
+           (4, 1, 'r29KLDbKINaCcrEEpv89XA', 'h_UngEx7VcA6I-XybPS13Q', now() + interval '1 hour', 1);
