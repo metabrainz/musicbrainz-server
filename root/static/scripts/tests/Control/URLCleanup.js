962c5c01e64988b8197eb6ea1325bec6af69ff7f
yvanzo <yvanzo@metabrainz.org>
2018-06-23 22:38:03 +0200

    Update LC Authority File URL cleanup for Work
    as requested in comment to MBS-9742.

--- root/static/scripts/tests/Control/URLCleanup.js
+++ root/static/scripts/tests/Control/URLCleanup.js
@@ -1746,14 +1746,14 @@ const {LINK_TYPES, cleanURL, guessType, validationRules} = require('../../edit/U
                      input_entity_type: 'artist',
             expected_relationship_type: 'otherdatabases',
                     expected_clean_url: 'http://id.loc.gov/authorities/names/n79018119',
-               only_valid_entity_types: ['artist']
+               only_valid_entity_types: ['artist', 'work']
         },
         {
                              input_url: 'https://id.loc.gov/authorities/names/no2016104748.html',
                      input_entity_type: 'artist',
             expected_relationship_type: 'otherdatabases',
                     expected_clean_url: 'http://id.loc.gov/authorities/names/no2016104748',
-               only_valid_entity_types: ['artist']
+               only_valid_entity_types: ['artist', 'work']
         },
         // LiederNet Archive
         {
