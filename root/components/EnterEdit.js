5c131568c71f24005b5a3e485201edfc677dc3f9
Michael Wiencek <mwtuea@gmail.com>
2018-08-13 11:32:40 -0500

    Convert the artist merge form to React

---
+++ root/components/EnterEdit.js
@@ -0,0 +1,47 @@
+/*
+ * @flow
+ * Copyright (C) 2018 MetaBrainz Foundation
+ *
+ * This file is part of MusicBrainz, the open internet music database,
+ * and is licensed under the GPL version 2, or (at your option) any
+ * later version: http://www.gnu.org/licenses/gpl-2.0.txt
+ */
+
+import React from 'react';
+import type {Node as ReactNode} from 'react';
+
+import {l} from '../static/scripts/common/i18n';
+
+import Frag from './Frag';
+
+type Props<F> = {|
+  +children: ReactNode,
+  +form: FormT<F & {+make_votable: FieldT<boolean>}>,
+|};
+
+const EnterEdit = <F>({children, form}: Props<F>) => (
+  <Frag>
+    <div className="row no-label">
+      <div className="auto-editor">
+        <label>
+          <input
+            className="make-votable"
+            defaultChecked={form.field.make_votable.value}
+            name={form.field.make_votable.html_name}
+            type="checkbox"
+            value="1"
+          />
+          {l('Make all edits votable.')}
+        </label>
+      </div>
+    </div>
+    <div className="row no-label buttons">
+      <button className="submit positive" type="submit">
+        {l('Enter edit')}
+      </button>
+      {children}
+    </div>
+  </Frag>
+);
+
+export default EnterEdit;
---
+++ root/components/EnterEditNote.js
@@ -0,0 +1,55 @@
+/*
+ * @flow
+ * Copyright (C) 2018 MetaBrainz Foundation
+ *
+ * This file is part of MusicBrainz, the open internet music database,
+ * and is licensed under the GPL version 2, or (at your option) any
+ * later version: http://www.gnu.org/licenses/gpl-2.0.txt
+ */
+
+import React from 'react';
+
+import {l} from '../static/scripts/common/i18n';
+
+import FieldErrors from './FieldErrors';
+import FormRow from './FormRow';
+import Frag from './Frag';
+
+type Props = {|
+  +field: FieldT<string>,
+  +hideHelp?: boolean,
+|};
+
+const EnterEditNote = ({
+  field,
+  hideHelp = false,
+}: Props) => (
+  <fieldset className="editnote">
+    <legend>{l('Edit Note')}</legend>
+    {hideHelp ? null : (
+      <Frag>
+        <p>
+          {l('Entering an {note|edit note} that describes where you got your information is highly recommended. Not only does it make it clear where you got your information, but it can also encourage other users to vote on your edit — thus making your edit get applied faster.', {
+            __react: true,
+            note: {href: '/doc/Edit_Note', target: '_blank'},
+          })}
+        </p>
+        <p>{l('Even just providing a URL or two is helpful!')}</p>
+      </Frag>
+    )}
+    <FormRow>
+      <label htmlFor="edit-note-text">{l('Edit note:')}</label>
+      <textarea
+        className="edit-note"
+        cols="80"
+        defaultValue={field.value}
+        id="edit-note-text"
+        name={field.html_name}
+        rows="5"
+      />
+      <FieldErrors field={field} />
+    </FormRow>
+  </fieldset>
+);
+
+export default EnterEditNote;
