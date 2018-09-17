f4e2caec26adbdb3fe7016f45674facb438c3269
Nicolás Tamargo <reosarevok@gmail.com>
2018-09-17 18:00:09 -0500

    Uppercase ISRCs

--- root/static/scripts/common/MB/Control/Autocomplete.js
+++ root/static/scripts/common/MB/Control/Autocomplete.js
@@ -638,7 +638,7 @@ MB.Control.autocomplete_formatters = {
 
         if (item.isrcs && item.isrcs.length)
         {
-            a.append('<br /><span class="autocomplete-isrcs">isrcs: ' +
+            a.append('<br /><span class="autocomplete-isrcs">ISRCs: ' +
                      _.escape(commaOnlyList(item.isrcs)) + '</span>');
         }
 
