07455f87248bb313f9752281c995d32f9f0b6af8
Michael Wiencek <mwtuea@gmail.com>
2018-09-30 09:19:51 +0200

    Run eslint on linkPhrase.js

--- root/static/scripts/edit/utility/linkPhrase.js
+++ root/static/scripts/edit/utility/linkPhrase.js
@@ -1,7 +1,9 @@
-// This file is part of MusicBrainz, the open internet music database.
-// Copyright (C) 2015 MetaBrainz Foundation
-// Licensed under the GPL version 2, or (at your option) any later version:
-// http://www.gnu.org/licenses/gpl-2.0.txt
+/*
+ * This file is part of MusicBrainz, the open internet music database.
+ * Copyright (C) 2015 MetaBrainz Foundation
+ * Licensed under the GPL version 2, or (at your option) any later version:
+ * http://www.gnu.org/licenses/gpl-2.0.txt
+ */
 
 import _ from 'lodash';
 
@@ -10,99 +12,114 @@ import commaOnlyList from '../../common/i18n/commaOnlyList';
 import {link_type as linkTypeInfo} from '../../common/typeInfo';
 import clean from '../../common/utility/clean';
 
-var attributeRegex = /\{(.*?)(?::(.*?))?\}/g;
+const attributeRegex = /\{(.*?)(?::(.*?))?\}/g;
 
 function mapNameToID(result, info, id) {
-    result[info.attribute.name] = id;
+  result[info.attribute.name] = id;
 }
 
 export const stripAttributes = _.memoize(function (linkTypeID, backward) {
-    var linkType = linkTypeInfo.byId[linkTypeID];
-    var idsByName = _.transform(linkType.attributes, mapNameToID);
+  const linkType = linkTypeInfo.byId[linkTypeID];
+  const idsByName = _.transform(linkType.attributes, mapNameToID);
 
-    // remove {foo} {bar} junk, unless it's for a required attribute.
-    const phrase = backward
-        ? l_relationships(linkType.reverse_link_phrase)
-        : l_relationships(linkType.link_phrase);
+  // remove {foo} {bar} junk, unless it's for a required attribute.
+  const phrase = backward
+    ? l_relationships(linkType.reverse_link_phrase)
+    : l_relationships(linkType.link_phrase);
 
-    return clean(phrase.replace(attributeRegex, function (match, name, alt) {
-        var id = idsByName[name];
+  return clean(phrase.replace(attributeRegex, function (match, name, alt) {
+    const id = idsByName[name];
 
-        if (id !== undefined && linkType.attributes[id].min < 1) {
-            return (alt ? alt.split('|')[1] : '') || '';
-        }
+    if (id !== undefined && linkType.attributes[id].min < 1) {
+      return (alt ? alt.split('|')[1] : '') || '';
+    }
 
-        return match;
-    }));
+    return match;
+  }));
 }, (a, b) => a + String(b));
 
 export const interpolate = function (linkType, attributes) {
-    if (!linkType) {
-        return ['', '', ''];
+  if (!linkType) {
+    return ['', '', ''];
+  }
+
+  let phrase = l_relationships(linkType.link_phrase);
+  let reversePhrase = l_relationships(linkType.reverse_link_phrase);
+  let cleanPhrase = '';
+  let cleanReversePhrase = '';
+  let cleanExtraAttributes;
+  const attributesByName = {};
+  let usedAttributes = [];
+
+  _.each(attributes, function (attribute) {
+    const type = attribute.type;
+    let value = type.l_name;
+
+    if (type.freeText) {
+      value = clean(attribute.textValue());
+      if (value) {
+        value = texp.l('{attribute}: {value}', {attribute: type.l_name, value: value});
+      }
     }
 
-    var phrase = l_relationships(linkType.link_phrase);
-    var reversePhrase = l_relationships(linkType.reverse_link_phrase);
-    var cleanPhrase = '';
-    var cleanReversePhrase = '';
-    var cleanExtraAttributes;
-    var attributesByName = {};
-    var usedAttributes = [];
-
-    _.each(attributes, function (attribute) {
-        var type = attribute.type;
-        var value = type.l_name;
-
-        if (type.freeText) {
-            value = clean(attribute.textValue());
-            if (value) {
-                value = texp.l('{attribute}: {value}', {attribute: type.l_name, value: value});
-            }
-        }
-
-        if (type.creditable) {
-            var credit = clean(attribute.creditedAs());
-            if (credit) {
-                value = texp.l('{attribute} [{credited_as}]', {attribute: type.l_name, credited_as: credit});
-            }
-        }
-
-        if (value) {
-            var rootName = type.root.name;
-            (attributesByName[rootName] = attributesByName[rootName] || []).push(value);
-        }
-    });
-
-    function interpolate(match, name, alts) {
-        usedAttributes.push(name);
-
-        var values = attributesByName[name] || [];
-        var replacement = commaList(values)
-
-        if (alts && (alts = alts.split('|'))) {
-            replacement = values.length ? alts[0].replace(/%/g, replacement) : alts[1] || '';
-        }
-
-        return replacement;
+    if (type.creditable) {
+      const credit = clean(attribute.creditedAs());
+      if (credit) {
+        value = texp.l('{attribute} [{credited_as}]', {attribute: type.l_name, credited_as: credit});
+      }
     }
 
-    phrase = clean(phrase.replace(attributeRegex, interpolate));
-    reversePhrase = clean(reversePhrase.replace(attributeRegex, interpolate));
-    const extraAttributes = commaOnlyList(_(attributesByName).omit(usedAttributes).values().flatten().value());
+    if (value) {
+      const rootName = type.root.name;
+      (attributesByName[rootName] =
+        attributesByName[rootName] || []).push(value);
+    }
+  });
 
-    if (linkType.orderable_direction > 0) {
-        usedAttributes = [];
-        cleanPhrase = clean(stripAttributes(linkType.id, false).replace(attributeRegex, interpolate));
-        cleanReversePhrase = clean(stripAttributes(linkType.id, true).replace(attributeRegex, interpolate));
-        cleanExtraAttributes = commaOnlyList(_(attributesByName).omit(usedAttributes).values().flatten().value());
+  function interpolate(match, name, alts) {
+    usedAttributes.push(name);
+
+    const values = attributesByName[name] || [];
+    let replacement = commaList(values);
+
+    if (alts && (alts = alts.split('|'))) {
+      replacement = values.length ? alts[0].replace(/%/g, replacement) : alts[1] || '';
     }
 
-    return [
-        phrase,
-        reversePhrase,
-        extraAttributes,
-        cleanPhrase,
-        cleanReversePhrase,
-        cleanExtraAttributes,
-    ];
+    return replacement;
+  }
+
+  phrase = clean(phrase.replace(attributeRegex, interpolate));
+  reversePhrase = clean(reversePhrase.replace(attributeRegex, interpolate));
+  const extraAttributes = commaOnlyList(
+    _(attributesByName).omit(usedAttributes).values()
+      .flatten()
+      .value(),
+  );
+
+  if (linkType.orderable_direction > 0) {
+    usedAttributes = [];
+    cleanPhrase = clean(
+      stripAttributes(linkType.id, false)
+        .replace(attributeRegex, interpolate),
+    );
+    cleanReversePhrase = clean(
+      stripAttributes(linkType.id, true)
+        .replace(attributeRegex, interpolate),
+    );
+    cleanExtraAttributes = commaOnlyList(
+      _(attributesByName).omit(usedAttributes).values()
+        .flatten()
+        .value(),
+    );
+  }
+
+  return [
+    phrase,
+    reversePhrase,
+    extraAttributes,
+    cleanPhrase,
+    cleanReversePhrase,
+    cleanExtraAttributes,
+  ];
 };
