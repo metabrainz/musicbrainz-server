64ec2670e58c48ed0b7ef05d38aa7595b729d12b
Enjeck Cleopatra <32180937+PROTechThor@users.noreply.github.com>
2018-11-18 21:10:40 +0100

    Update List.js

--- root/genre/List.js
+++ root/genre/List.js
@@ -33,7 +33,7 @@ const GenreList = ({genres}: PropsT) => (
         ))}
       </ul>
       <p>
-        {l('You cannot find a genre which exists on the list? Request it by')} + 
+        {l('Is a genre missing from the list? Request it by')} + 
          <a href="https://tickets.metabrainz.org/secure/CreateIssueDetails!init.jspa?pid=10032&issuetype=2&summary=Enter%20the%20genre%20name%20here!">
          {l(' adding a style ticket ')}
       </a> + 
