41e9c787e86067fdd90f7f6bfe603315673b4050
Enjeck Cleopatra <32180937+PROTechThor@users.noreply.github.com>
2018-11-19 12:29:50 +0100

    MBS-9900:  Add links to request instrument/genre (#752)
    Add information on requesting instruments/genres to their lists

--- root/genre/List.js
+++ root/genre/List.js
@@ -32,6 +32,13 @@ const GenreList = ({genres}: PropsT) => (
           </li>
         ))}
       </ul>
+      <p>
+        {l('Is a genre missing from the list? Request it by {link|adding a style ticket} with the "Genres" component', 
+            {
+             __react: true,
+            link: 'https://tickets.metabrainz.org/secure/CreateIssueDetails!init.jspa?pid=10032&issuetype=2&summary=Enter%20the%20genre%20name%20here!'}
+           )}
+      </p>
     </div>
   </Layout>
 );
