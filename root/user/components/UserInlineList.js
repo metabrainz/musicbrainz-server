067a1358f49a3cb89a4be57a29a392db4305845a
Theodore Fabian Rudy <31211200+TheFaR8@users.noreply.github.com>
2018-12-08 22:53:30 +0700

    Add license

--- root/user/components/UserInlineList.js
+++ root/user/components/UserInlineList.js
@@ -1,6 +1,7 @@
 /*
  * @flow
  * Copyright (C) 2018 MetaBrainz Foundation
+ * Copyright (C) 2018 Theodore Fabian Rudy
  *
  * This file is part of MusicBrainz, the open internet music database,
  * and is licensed under the GPL version 2, or (at your option) any
