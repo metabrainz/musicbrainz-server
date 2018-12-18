metabrainz/musicbrainz-server

commit 8f94292ca5ff0cecb6fe6320cc9088bda60a7b1c

Bump Flow to 0.89.0

root/static/scripts/edit/externalLinks.js
@@ -384,7 +384,10 @@ function withOneEmptyLink(links, dontRemove) {
 }
 
 function parseRelationships(relationships?: $ReadOnlyArray<RelationshipT>) {
-  return _.transform(relationships || [], function (accum, data) {
+  if (!relationships) {
+    return [];
+  }
+  return relationships.reduce(function (accum, data) {
     var target = data.target;
 
     if (target.entityType === 'url') {
@@ -392,10 +395,12 @@ function parseRelationships(relationships?: $ReadOnlyArray<RelationshipT>) {
         relationship: data.id,
         url: target.name,
         type: data.linkTypeID,
-        video: _.some(data.attributes, (attr) => attr.type.gid === VIDEO_ATTRIBUTE_GID)
+        video: data.attributes.some((attr) => attr.type.gid === VIDEO_ATTRIBUTE_GID),
       });
     }
-  });
+
+    return accum;
+  }, []);
 }
 
 var protocolRegex = /^(https?|ftp):$/;
@@ -493,8 +498,8 @@ MB.createExternalLinksEditor = function (options: InitialOptionsT) {
   }
 
   initialLinks.sort(function (a, b) {
-    var typeA = linkTypeInfo.byId[a.type];
-    var typeB = linkTypeInfo.byId[b.type];
+    var typeA = a.type && linkTypeInfo.byId[a.type];
+    var typeB = b.type && linkTypeInfo.byId[b.type];
 
     return compare(typeA ? typeA.phrase.toLowerCase() : '',
                    typeB ? typeB.phrase.toLowerCase() : '');

root/utility/hydrate.js
@@ -8,17 +8,16 @@
  */
 
 import $ from 'jquery';
-import React from 'react';
+import * as React from 'react';
 import ReactDOM from 'react-dom';
-import type {ComponentType as ReactComponentType} from 'react';
 
 import * as lens from '../static/scripts/common/utility/lens';
 
-export default function hydrate<T>(
+export default function hydrate<Config: {}>(
   rootClass: string,
-  Component: ReactComponentType<T>,
-  mungeProps?: (T) => T,
-): ReactComponentType<T> {
+  Component: React.AbstractComponent<Config>,
+  mungeProps?: (Config) => Config,
+): React.AbstractComponent<Config, void> {
   if (typeof document !== 'undefined') {
     // This should only run on the client.
     $(function () {
@@ -27,13 +26,13 @@ export default function hydrate<T>(
         const propString = root.getAttribute('data-props');
         root.removeAttribute('data-props');
         if (propString) {
-          const props: T = JSON.parse(propString);
+          const props: Config = JSON.parse(propString);
           ReactDOM.hydrate(<Component {...props} />, root);
         }
       }
     });
   }
-  return (props: T) => {
+  return (props) => {
     let dataProps = props;
     if (mungeProps) {
       dataProps = mungeProps(dataProps);

commit 553cb6040e4c0a40ff1775c128249ea6b8097755

Handle undefined relationship attributes
Make the attributes field optional in RelationshipT to handle seeded
relationships in the release editor.

Broken in 8f94292ca5ff0cecb6fe6320cc9088bda60a7b1c

root/static/scripts/edit/externalLinks.js
@@ -383,6 +383,8 @@ function withOneEmptyLink(links, dontRemove) {
   }
 }
 
+const isVideoAttribute = attr => attr.type.gid === VIDEO_ATTRIBUTE_GID;
+
 function parseRelationships(relationships?: $ReadOnlyArray<RelationshipT>) {
   if (!relationships) {
     return [];
@@ -395,7 +397,9 @@ function parseRelationships(relationships?: $ReadOnlyArray<RelationshipT>) {
         relationship: data.id,
         url: target.name,
         type: data.linkTypeID,
-        video: data.attributes.some((attr) => attr.type.gid === VIDEO_ATTRIBUTE_GID),
+        video: data.attributes
+          ? data.attributes.some(isVideoAttribute)
+          : false,
       });
     }
 

root/types.js
@@ -666,7 +666,8 @@ declare type RelationshipAttributeT = {|
 |};
 
 declare type RelationshipT = {|
-  +attributes: $ReadOnlyArray<RelationshipAttributeT>,
+  // `attributes` may not exist when seeding.
+  +attributes?: $ReadOnlyArray<RelationshipAttributeT>,
   +id: number,
   +linkTypeID: number,
   +target: CoreEntityT,