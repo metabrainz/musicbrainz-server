metabrainz/musicbrainz-server

commit 65ad3b115cfe3618113caf554887c762b52363d7

Decode HTML entities when loading annotations
We should prefer not to store HTML in the database at all, but some of the old
data does have HTML entities in. This will get stripped when we add new
annotations, but we don't want to create annotation edits if the text is
logically the same. Decoding HTML entities when loading annotations makes text
comparisons work as expected.

lib/MusicBrainz/Server/Data/EntityAnnotation.pm
@@ -1,6 +1,7 @@
 package MusicBrainz::Server::Data::EntityAnnotation;
 use Moose;
 
+use HTML::Entities qw( decode_entities );
 use MusicBrainz::Server::Entity::Annotation;
 use MusicBrainz::Server::Data::Utils qw(
     placeholders
@@ -44,6 +45,12 @@ sub get_history
         $self->c->sql, $offset, $limit, sub { $self->_new_from_row(@_) },
         $query, $id, $offset || 0
     );
+}
+
+sub _column_mapping {
+    return {
+        text => sub { decode_entities(shift->{text}) }
+    }
 }
 
 sub get_latest

commit 3580add8d19c885117698a4296a0e4dc844c7f6a

MBS-4709: Fix square brackets in annotation text
As a side effect, the commit 65ad3b115cfe36 caused square brackets
encoded as HTML entities &#91; and &#93; to be decoded before wiki
formatting, thus being mistakenly interpreted as a wiki link.  The
assumption that HTML entities are equivalent to their decoded form
is not true in the particular case of square brackets.

This patch mostly reverts 65ad3b115cfe36 and subsequent commits,
thus fixing display of square brackets entered as HTML entities so
as not to create a link, and showing HTML entities being replaced
with their decoded form in annotation diff again.

lib/MusicBrainz/Server/Data/EntityAnnotation.pm
@@ -2,7 +2,6 @@ package MusicBrainz::Server::Data::EntityAnnotation;
 use Moose;
 use namespace::autoclean;
 
-use HTML::Entities qw( decode_entities );
 use List::MoreUtils qw( uniq );
 
 use MusicBrainz::Server::Constants qw( $EDITOR_MODBOT %ENTITIES );
@@ -46,19 +45,6 @@ sub get_history
                 ' WHERE ' . $self->type . ' = ?' .
                 ' ORDER BY created DESC';
     $self->query_to_list_limited($query, [$id], $limit, $offset);
-}
-
-sub _column_mapping {
-    return {
-        id => 'id',
-        text => sub {
-            my $row = shift;
-            $row->{text} && decode_entities($row->{text});
-        },
-        changelog => 'changelog',
-        editor_id => 'editor_id',
-        creation_date => 'creation_date'
-    }
 }
 
 sub get_latest