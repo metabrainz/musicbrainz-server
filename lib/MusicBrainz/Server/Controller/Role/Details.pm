2d813045b05e7183d932d7935b4fd6f399dfc722
Nicolás Tamargo <reosarevok@gmail.com>
2019-01-09 22:29:17 +0200

    MBS-9849 (II): Port entity Details tab to React and use it for RG
    Change-Id: I4fd6b517c6e43bc481781d9fa9a7b5b17d925978

--- lib/MusicBrainz/Server/Controller/Role/Details.pm
+++ lib/MusicBrainz/Server/Controller/Role/Details.pm
@@ -1,9 +1,27 @@
 package MusicBrainz::Server::Controller::Role::Details;
 use Moose::Role -traits => 'MooseX::MethodAttributes::Role::Meta::Role';
+use MusicBrainz::Server::Plugin::Canonicalize qw ( replace_gid );
 
 sub details : Chained('load') PathPart {
     my ($self, $c) = @_;
-    $c->stash( template => 'entity/details.tt' );
+
+    my $entity = $c->stash->{$self->{entity_name}};
+
+    if ($entity->entity_type eq 'release_group') {
+        my %props = (
+            canonicalURL => MusicBrainz::Server::Plugin::Canonicalize::replace_gid($self, $c, $entity->gid),
+            entity       => $entity,
+            lastUpdated  => $entity->{last_updated},
+        );
+
+        $c->stash(
+            component_path  => 'entity/Details.js',
+            component_props => \%props,
+            current_view    => 'Node',
+        );
+    } else {
+        $c->stash( template => 'entity/details.tt' );
+    }
 }
 
 no Moose::Role;
