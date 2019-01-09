123a8730508ac2ab6ce08f45004f77aa338eec42
Nicolás Tamargo <reosarevok@gmail.com>
2019-01-09 22:15:56 +0200

    MBS-9849 (I): Add React RG layout
    Change-Id: I42d407b35fc193706273286319e0cd1148f0c51c
    Reviewed-on: https://review.gerrithub.io/c/439813
    Reviewed-by: MusicBrainz
    Tested-by: Michael Wiencek <bitmap@metabrainz.org>

---
+++ root/release_group/ReleaseGroupLayout.js
@@ -0,0 +1,52 @@
+/*
+ * @flow
+ * Copyright (C) 2018 MetaBrainz Foundation
+ *
+ * This file is part of MusicBrainz, the open internet music database,
+ * and is licensed under the GPL version 2, or (at your option) any
+ * later version: http://www.gnu.org/licenses/gpl-2.0.txt
+ */
+
+import React from 'react';
+import type {Node as ReactNode} from 'react';
+
+import Layout from '../layout';
+import {hyphenateTitle, l} from '../static/scripts/common/i18n';
+import ReleaseGroupSidebar from '../layout/components/sidebar/ReleaseGroupSidebar';
+import {artistCreditFromArray, reduceArtistCredit} from '../static/scripts/common/immutable-entities';
+
+import ReleaseGroupHeader from './ReleaseGroupHeader';
+
+type Props = {|
+  +canonicalURL: string,
+  +children: ReactNode,
+  +fullWidth?: boolean,
+  +page: string,
+  +releaseGroup: ReleaseGroupT,
+  +title?: string,
+|};
+
+const ReleaseGroupLayout = ({
+  canonicalURL,
+  children,
+  fullWidth,
+  page,
+  releaseGroup,
+  title,
+}: Props) => {
+  const mainTitle = l('Release group “{name}” by {artist}', {
+    artist: reduceArtistCredit(artistCreditFromArray(releaseGroup.artistCredit)),
+    name: releaseGroup.name,
+  });
+  return (
+    <Layout canonical_url={canonicalURL} title={title ? hyphenateTitle(mainTitle, title) : mainTitle}>
+      <div id="content">
+        <ReleaseGroupHeader page={page} releaseGroup={releaseGroup} />
+        {children}
+      </div>
+      {fullWidth ? null : <ReleaseGroupSidebar releaseGroup={releaseGroup} />}
+    </Layout>
+  );
+};
+
+export default ReleaseGroupLayout;
