metabrainz/musicbrainz-server

commit 677ec64971a1a294094b796863fe62c1f90c6f31

Move link type serialization into TO_JSON
There shouldn't be more than one method for serializing link types, so
this consolidates them into the standard place (TO_JSON).

Some properties (phrase, reversePhrase, description) are no longer
translated in the serialization, but done so in JavaScript (either on
the client or server). Putting translated strings in the JSON made
caching harder, and we'll likely be moving all translation work to
JavaScript in the future as the website moves to React.

Properties have also been renamed to match their corresponding Moose
attribute names in order to come to a more standardized format and avoid
friction in the codebase.

root/static/scripts/relationship-editor/common/dialog.js
@@ -11,6 +11,7 @@ require('../../../lib/jquery-ui');
 
 const {PART_OF_SERIES_LINK_TYPES} = require('../../common/constants');
 const i18n = require('../../common/i18n');
+import {l_relationships} from '../../common/i18n/relationships';
 const linkTypeInfo = require('../../common/typeInfo').link_type;
 const URLCleanup = require('../../edit/URLCleanup');
 const dates = require('../../edit/utility/dates');
@@ -334,7 +335,7 @@ const PART_OF_SERIES_LINK_TYPE_GIDS = _.values(PART_OF_SERIES_LINK_TYPES);
 
             if (linkType) {
                 description = i18n.l("{description} ({url|more documentation})", {
-                    description: linkType.description,
+                    description: l_relationships(linkType.description),
                     url: { href: "/relationship/" + linkType.gid, target: "_blank" }
                 });
             }

lib/MusicBrainz/Server/Entity/LinkType.pm
@@ -1,6 +1,7 @@
 package MusicBrainz::Server::Entity::LinkType;
 use Moose;
 
+use MusicBrainz::Server::Data::Utils qw( boolean_to_json );
 use MusicBrainz::Server::Entity::Types;
 use MusicBrainz::Server::Translation::Relationships qw( l );
 
@@ -123,7 +124,26 @@ around TO_JSON => sub {
     my ($orig, $self) = @_;
 
     my $json = $self->$orig;
+
+    my %attrs = map {
+        $_->type_id => {
+            min => defined $_->min ? 0 + $_->min : undef,
+            max => defined $_->max ? 0 + $_->max : undef,
+        }
+    } $self->all_attributes;
+
+    $json->{attributes} = \%attrs;
+    $json->{cardinality0} = $self->entity0_cardinality;
+    $json->{cardinality1} = $self->entity1_cardinality;
+    $json->{deprecated} = boolean_to_json($self->is_deprecated);
+    $json->{hasDates} = boolean_to_json($self->has_dates);
+    $json->{id} = $self->id;
     $json->{link_phrase} = $self->link_phrase;
+    $json->{long_link_phrase} = $self->long_link_phrase;
+    $json->{orderableDirection} = $self->orderable_direction;
+    $json->{reverse_link_phrase} = $self->reverse_link_phrase;
+    $json->{type0} = $self->entity0_type;
+    $json->{type1} = $self->entity1_type;
 
     return $json;
 };

lib/MusicBrainz/Server/Form/Utils.pm
@@ -157,30 +157,8 @@ sub build_type_info {
     sub build_type {
         my $root = shift;
 
-        my %attrs = map {
-            $_->type_id => {
-                min     => defined $_->min ? 0 + $_->min : undef,
-                max     => defined $_->max ? 0 + $_->max : undef,
-            }
-        } $root->all_attributes;
+        my $result = $root->TO_JSON;
 
-        my $result = {
-            id                  => $root->id,
-            gid                 => $root->gid,
-            phrase              => $root->l_link_phrase,
-            reversePhrase       => $root->l_reverse_link_phrase,
-            deprecated          => boolean_to_json($root->is_deprecated),
-            hasDates            => boolean_to_json($root->has_dates),
-            type0               => $root->entity0_type,
-            type1               => $root->entity1_type,
-            cardinality0        => $root->entity0_cardinality,
-            cardinality1        => $root->entity1_cardinality,
-            orderableDirection  => $root->orderable_direction,
-            childOrder          => $root->child_order,
-        };
-
-        $result->{description} = $root->l_description if $root->description;
-        $result->{attributes} = \%attrs if %attrs;
         $result->{children} = build_child_info($root, \&build_type) if $root->all_children;
 
         return $result;

root/static/scripts/common/typeInfo.js
@@ -7,9 +7,9 @@
  * later version: http://www.gnu.org/licenses/gpl-2.0.txt
  */
 
-const linkTypeInfoByTypes: {+[string]: $ReadOnlyArray<LinkTypeInfoT>} = {};
+const linkTypeInfoByTypes: {+[string]: $ReadOnlyArray<LinkTypeT>} = {};
 
-const linkTypeInfoById: {+[number | string]: LinkTypeInfoT} = {};
+const linkTypeInfoById: {+[number | string]: LinkTypeT} = {};
 
 const linkAttributeTypeInfoById: {+[number | string]: AttrInfoT} = {};
 

root/static/scripts/edit/externalLinks.js
@@ -17,6 +17,7 @@ const {
   } = require('../common/constants');
 const {compare, l} = require('../common/i18n');
 const expand2 = require('../common/i18n/expand2').default;
+import {l_relationships} from '../common/i18n/relationships';
 const MB = require('../common/MB');
 const linkTypeInfo = require('../common/typeInfo').link_type;
 const {hasSessionStorage} = require('../common/utility/storage');
@@ -276,14 +277,14 @@ type LinkProps = {
 class ExternalLink extends React.Component<LinkProps> {
   render() {
     var props = this.props;
-    var linkType = props.type ? linkTypeInfo.byId[props.type] : {};
+    var linkType = props.type ? linkTypeInfo.byId[props.type] : null;
     var typeDescription = '';
     var faviconClass: string | void;
 
-    if (linkType.description) {
+    if (linkType && linkType.description) {
       typeDescription = l('{description} ({url|more documentation})', {
         __react: true,
-        description: expand2(linkType.description),
+        description: l_relationships(linkType.description, {__react: true}),
         url: '/relationship/' + linkType.gid
       });
     }
@@ -315,7 +316,8 @@ class ExternalLink extends React.Component<LinkProps> {
               </LinkTypeSelect>
             : <label>
                 {faviconClass && <span className={'favicon ' + faviconClass + '-favicon'}></span>}
-                {linkType.phrase || (props.isOnlyLink ? l('Add link:') : l('Add another link:'))}
+                {(linkType ? l_relationships(linkType.link_phrase) : null) ||
+                  (props.isOnlyLink ? l('Add link:') : l('Add another link:'))}
               </label>}
         </td>
         <td>
@@ -325,7 +327,7 @@ class ExternalLink extends React.Component<LinkProps> {
                  onChange={props.urlChangeCallback}
                  onBlur={props.urlBlurCallback} />
           {props.errorMessage && <div className="error field-error" data-visible="1">{props.errorMessage}</div>}
-          {_.has(linkType.attributes, String(VIDEO_ATTRIBUTE_ID)) &&
+          {linkType && _.has(linkType.attributes, String(VIDEO_ATTRIBUTE_ID)) &&
             <div className="attribute-container">
               <label>
                 <input type="checkbox" checked={props.video} onChange={props.videoChangeCallback} /> {l('video')}
@@ -505,8 +507,8 @@ MB.createExternalLinksEditor = function (options: InitialOptionsT) {
     var typeA = a.type && linkTypeInfo.byId[a.type];
     var typeB = b.type && linkTypeInfo.byId[b.type];
 
-    return compare(typeA ? typeA.phrase.toLowerCase() : '',
-                   typeB ? typeB.phrase.toLowerCase() : '');
+    return compare(typeA ? l_relationships(typeA.link_phrase).toLowerCase() : '',
+                   typeB ? l_relationships(typeB.link_phrase).toLowerCase() : '');
   });
 
   initialLinks = initialLinks.map(function (link) {

root/static/scripts/edit/utility/linkPhrase.js
@@ -8,6 +8,7 @@ const _ = require('lodash');
 const {l} = require('../../common/i18n');
 const commaList = require('../../common/i18n/commaList');
 const commaOnlyList = require('../../common/i18n/commaOnlyList');
+const {l_relationships} = require('../../common/i18n/relationships');
 const linkTypeInfo = require('../../common/typeInfo').link_type;
 const clean = require('../../common/utility/clean');
 
@@ -22,7 +23,9 @@ exports.clean = _.memoize(function (linkTypeID, backward) {
     var idsByName = _.transform(linkType.attributes, mapNameToID);
 
     // remove {foo} {bar} junk, unless it's for a required attribute.
-    var phrase = backward ? linkType.reversePhrase : linkType.phrase;
+    const phrase = backward
+        ? l_relationships(linkType.reverse_link_phrase)
+        : l_relationships(linkType.link_phrase);
 
     return clean(phrase.replace(attributeRegex, function (match, name, alt) {
         var id = idsByName[name];
@@ -40,8 +43,8 @@ exports.interpolate = function (linkType, attributes) {
         return ['', '', ''];
     }
 
-    var phrase = linkType.phrase;
-    var reversePhrase = linkType.reversePhrase;
+    var phrase = l_relationships(linkType.link_phrase);
+    var reversePhrase = l_relationships(linkType.reverse_link_phrase);
     var cleanPhrase = '';
     var cleanReversePhrase = '';
     var cleanExtraAttributes;

root/types.js
@@ -189,7 +189,7 @@ type CatalystContextT = {|
       +[number]: LanguageT,
     |},
     +link_type: {|
-      +[number]: LinkTypeInfoT,
+      +[number]: LinkTypeT,
     |},
     +release_group_primary_type: {|
       [number]: ReleaseGroupTypeT,
@@ -559,20 +559,19 @@ declare type LinkTypeAttrTypeT = {|
   +min: number | null,
 |};
 
-declare type LinkTypeInfoT = {|
-  +attributes?: {+[number]: LinkTypeAttrTypeT},
+declare type LinkTypeT = {|
+  ...OptionTreeT,
+  +attributes: {+[number]: LinkTypeAttrTypeT},
   +cardinality0: number,
   +cardinality1: number,
-  +childOrder: number,
-  +children?: $ReadOnlyArray<LinkTypeInfoT>,
+  +children?: $ReadOnlyArray<LinkTypeT>,
   +deprecated: boolean,
-  +description?: string,
-  +gid: string,
   +hasDates: boolean,
   +id: number,
+  +link_phrase: string,
+  +long_link_phrase: string,
   +orderableDirection: number,
-  +phrase: string,
-  +reversePhrase: string,
+  +reverse_link_phrase: string,
   +type0: string,
   +type1: string,
 |};

commit ed45eaee1a96351d9ee8e331f72299e18bdac696

Handle falsey link type descriptions (MBS-9984)
677ec64971a1a294094b796863fe62c1f90c6f31 broke this by unconditionally
passing linkType.description to l_relationships.

A link type can have an empty description if it's only used for
grouping. Normally these can't be selected and this code would never be
hit, but in the case of MBS-9984, there already exist 'child'
relationships from before it became a grouping type.

root/static/scripts/relationship-editor/common/dialog.js
@@ -334,7 +334,7 @@ const PART_OF_SERIES_LINK_TYPE_GIDS = _.values(PART_OF_SERIES_LINK_TYPES);
             var linkType = this.relationship().getLinkType();
             var description;
 
-            if (linkType) {
+            if (linkType && linkType.description) {
                 description = ReactDOMServer.renderToStaticMarkup(
                     i18n.l("{description} ({url|more documentation})", {
                         description: l_relationships(linkType.description),