900fd280112dfac702ebc08856f7901f71efe65c
Nicolás Tamargo <reosarevok@gmail.com>
2019-01-29 14:45:53 +0200

    Run eslint on scripts/statistics

--- root/static/scripts/statistics.js
+++ root/static/scripts/statistics.js
@@ -1,45 +1,48 @@
 const tablesorter = require('tablesorter');
 
 tablesorter.addWidget({
-    id: "indexFirstColumn",
-    format: function (table) {
-        $('tbody tr', table).each(function (index) {
-            $(this).find('td:first').html((index + 1));
-        });
-    }
+  format: function (table) {
+    $('tbody tr', table).each(function (index) {
+      $(this).find('td:first').html((index + 1));
+    });
+  },
+  id: 'indexFirstColumn',
 });
 
 tablesorter.addWidget({
-    id: "evenRowClasses",
-    format: function (table) {
-        $('tbody tr', table).each(function (index) {
-            if ((index + 1) % 2 == 0) {
-                $(this).addClass("even");
-            } else {
-                $(this).removeClass("even");
-            }
-        });
-    }
+  format: function (table) {
+    $('tbody tr', table).each(function (index) {
+      if ((index + 1) % 2 === 0) {
+        $(this).addClass('even');
+      } else {
+        $(this).removeClass('even');
+      }
+    });
+  },
+  id: 'evenRowClasses',
 });
 
 tablesorter.addParser({
-    id: "fancyNumber",
-    is: function (s) {
-        return /^[0-9]?[0-9,\.]*$/.test(s);
-    },
-    format: function (s) {
-        return tablesorter.formatFloat( s.replace(/,/g,'') );
-    },
-    type: "numeric"
+  format: function (s) {
+    return tablesorter.formatFloat(s.replace(/,/g, ''));
+  },
+  id: 'fancyNumber',
+  is: function (s) {
+    return /^[0-9]?[0-9,.]*$/.test(s);
+  },
+  type: 'numeric',
 });
 
 $('#languages-table').tablesorter({
-    widgets: [ 'indexFirstColumn', 'evenRowClasses' ],
-    headers: { 0: {sorter: false}, 2: { sorter: 'fancyNumber' }, 3: { sorter: 'fancyNumber' }, 4: { sorter: 'fancyNumber' } },
-    sortList: [ [4,1], [1,0] ] // order by descending number of entities, then name
+  headers: {0: {sorter: false}, 2: {sorter: 'fancyNumber'}, 3: {sorter: 'fancyNumber'}, 4: {sorter: 'fancyNumber'}},
+  // order by descending number of entities, then name
+  sortList: [[4, 1], [1, 0]],
+  widgets: ['indexFirstColumn', 'evenRowClasses'],
 });
+
 $('#scripts-table').tablesorter({
-    widgets: [ 'indexFirstColumn', 'evenRowClasses' ],
-    headers: { 0: {sorter: false}, 2: { sorter: 'fancyNumber' } },
-    sortList: [ [2,1], [1,0] ] // order by descending number of entities, then name
+  headers: {0: {sorter: false}, 2: {sorter: 'fancyNumber'}},
+  // order by descending number of entities, then name
+  sortList: [[2, 1], [1, 0]],
+  widgets: ['indexFirstColumn', 'evenRowClasses'],
 });
