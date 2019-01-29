c8c2bf25efc4fe01316f0c01f549c7faea5c374a
Nicolás Tamargo <reosarevok@gmail.com>
2019-01-29 14:29:08 +0200

    "guitars" has been renamed

--- root/static/scripts/tests/relationship-editor.js
+++ root/static/scripts/tests/relationship-editor.js
@@ -198,7 +198,7 @@ relationshipEditorTest("link phrase interpolation", function (t) {
             linkTypeID: 154,
             attributes: ids2attrs([1, 69, 75, 109, 302]),
             expected: "contains additional samples by",
-            expectedExtra: "strings, guitars, lyre, plucked string instruments"
+            expectedExtra: "strings, guitar family, lyre, plucked string instruments"
         },
         // MBS-6129
         {
