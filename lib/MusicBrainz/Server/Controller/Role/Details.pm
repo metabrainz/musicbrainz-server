5556b0376fa47b200d6ad0ef5209c5b26ed7a9ee
Nicolás Tamargo <reosarevok@gmail.com>
2019-01-30 10:21:34 +0200

    MBS-10005: Make Series use React Details and Tag tabs

--- lib/MusicBrainz/Server/Controller/Role/Details.pm
+++ lib/MusicBrainz/Server/Controller/Role/Details.pm
@@ -6,7 +6,7 @@ sub details : Chained('load') PathPart {
 
     my $entity = $c->stash->{$self->{entity_name}};
 
-    if ($entity->entity_type =~ /^(?:area|artist|label|release_group)$/) {
+    if ($entity->entity_type =~ /^(?:area|artist|label|release_group|series)$/) {
         my %props = (
             entity       => $entity,
             lastUpdated  => $entity->{last_updated},
