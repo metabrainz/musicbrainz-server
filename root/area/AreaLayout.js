e15199380635b1cd4deb4cef95ad938a49acec02
Nicolás Tamargo <reosarevok@gmail.com>
2019-01-30 10:11:13 +0200

    MBS-10005: Convert Area layout to React

---
+++ root/area/AreaLayout.js
@@ -0,0 +1,46 @@
+/*
+ * @flow
+ * Copyright (C) 2018 MetaBrainz Foundation
+ *
+ * This file is part of MusicBrainz, the open internet music database,
+ * and is licensed under the GPL version 2, or (at your option) any
+ * later version: http://www.gnu.org/licenses/gpl-2.0.txt
+ */
+
+import React from 'react';
+import type {Node as ReactNode} from 'react';
+
+import Layout from '../layout';
+import AreaSidebar from '../layout/components/sidebar/AreaSidebar';
+import {hyphenateTitle, l} from '../static/scripts/common/i18n';
+
+import AreaHeader from './AreaHeader';
+
+type Props = {|
+  +children: ReactNode,
+  +entity: AreaT,
+  +fullWidth?: boolean,
+  +page: string,
+  +title?: string,
+|};
+
+const AreaLayout = ({
+  children,
+  entity: area,
+  fullWidth,
+  page,
+  title,
+}: Props) => (
+  <Layout
+    title={title ? hyphenateTitle(l(area.name), title) : l(area.name)}
+  >
+    <div id="content">
+      <AreaHeader area={area} page={page} />
+      {children}
+    </div>
+    {fullWidth ? null : <AreaSidebar area={area} />}
+  </Layout>
+);
+
+
+export default AreaLayout;
