f4fe3b00b6211afff2ce67d4885257cb8e276d28
Nicolás Tamargo <reosarevok@gmail.com>
2019-01-31 12:00:55 +0200

    MBS-9923 (I): Convert URL Layout to React

---
+++ root/url/UrlLayout.js
@@ -0,0 +1,45 @@
+/*
+ * @flow
+ * Copyright (C) 2019 MetaBrainz Foundation
+ *
+ * This file is part of MusicBrainz, the open internet music database,
+ * and is licensed under the GPL version 2, or (at your option) any
+ * later version: http://www.gnu.org/licenses/gpl-2.0.txt
+ */
+
+import React from 'react';
+import type {Node as ReactNode} from 'react';
+
+import Layout from '../layout';
+import UrlSidebar from '../layout/components/sidebar/UrlSidebar';
+
+import URLHeader from './URLHeader';
+
+type Props = {|
+  +children: ReactNode,
+  +entity: UrlT,
+  +fullWidth?: boolean,
+  +page: string,
+  +title?: string,
+|};
+
+const UrlLayout = ({
+  children,
+  entity: url,
+  fullWidth,
+  page,
+  title,
+}: Props) => (
+  <Layout
+    title={title}
+  >
+    <div id="content">
+      <URLHeader page={page} url={url} />
+      {children}
+    </div>
+    {fullWidth ? null : <UrlSidebar url={url} />}
+  </Layout>
+);
+
+
+export default UrlLayout;
