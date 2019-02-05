9ffc37f53143066b5cc7980b9ce65a120d1f22f6
Nicolás Tamargo <reosarevok@gmail.com>
2019-02-05 11:52:10 +0200

    Eslint fixes for files touched by MBS-10011

--- root/static/scripts/common/banner.js
+++ root/static/scripts/common/banner.js
@@ -1,23 +1,32 @@
-// This file is part of MusicBrainz, the open internet music database.
-// Copyright (C) 2015 MetaBrainz Foundation
-// Licensed under the GPL version 2, or (at your option) any later version:
-// http://www.gnu.org/licenses/gpl-2.0.txt
+/*
+ * This file is part of MusicBrainz, the open internet music database.
+ * Copyright (C) 2015 MetaBrainz Foundation
+ * Licensed under the GPL version 2, or (at your option) any later version:
+ * http://www.gnu.org/licenses/gpl-2.0.txt
+ */
 
 const $ = require('jquery');
 
 const setCookie = require('./utility/setCookie');
 
 $(function () {
-    $('.dismiss-banner').on('click', function () {
-        var bannerName = $(this).parent().remove().end().data('banner-name');
+  $('.dismiss-banner').on('click', function () {
+    const bannerName = $(this).parent().remove()
+      .end()
+      .data('banner-name');
 
-        if (bannerName === 'birthday_message') {
-            var oneDayFromNow = new Date(Date.now() + (1000 * 60 * 60 * 24));
-            setCookie(bannerName + '_dismissed_mtime', Math.ceil(Date.now() / 1000), oneDayFromNow);
-
-        } else {
-            setCookie(bannerName + '_dismissed_mtime', Math.ceil(Date.now() / 1000));
-        }
-
-    });
+    if (bannerName === 'birthday_message') {
+      const oneDayFromNow = new Date(Date.now() + (1000 * 60 * 60 * 24));
+      setCookie(
+        bannerName + '_dismissed_mtime',
+        Math.ceil(Date.now() / 1000),
+        oneDayFromNow,
+      );
+    } else {
+      setCookie(
+        bannerName + '_dismissed_mtime',
+        Math.ceil(Date.now() / 1000),
+      );
+    }
+  });
 });
--- root/static/scripts/common/utility/setCookie.js
+++ root/static/scripts/common/utility/setCookie.js
@@ -3,20 +3,20 @@ const cookie = require('cookie');
 const _cookies = require('./_cookies');
 
 function oneYearFromNow() {
-    return new Date(Date.now() + (1000 * 60 * 60 * 24 * 365));
+  return new Date(Date.now() + (1000 * 60 * 60 * 24 * 365));
 }
 
 function setCookieFallback(name, value) {
-    _cookies[name] = value;
+  _cookies[name] = value;
 }
 
 function setCookie(name, value, expiration = oneYearFromNow()) {
-    document.cookie = cookie.serialize(name, value, {path: '/', expires: expiration});
+  document.cookie = cookie.serialize(name, value, {expires: expiration, path: '/'});
 }
 
 if (typeof document === 'undefined' ||
     window.location.protocol === 'file:') {
-    module.exports = setCookieFallback;
+  module.exports = setCookieFallback;
 } else {
-    module.exports = setCookie;
+  module.exports = setCookie;
 }
