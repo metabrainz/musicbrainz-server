843d6358197c4d97b2f52a514c5297043673a48b
Nicolás Tamargo <reosarevok@gmail.com>
2019-02-14 17:47:59 +0200

    MBS-9632: Convert Stats to React (VII)
    Edits

---
+++ root/statistics/Edits.js
@@ -0,0 +1,89 @@
+/*
+ * @flow
+ * Copyright (C) 2018 MetaBrainz Foundation
+ *
+ * This file is part of MusicBrainz, the open internet music database,
+ * and is licensed under the GPL version 2, or (at your option) any
+ * later version: http://www.gnu.org/licenses/gpl-2.0.txt
+ */
+
+import React from 'react';
+
+import {addColon, l as lMbServer} from '../static/scripts/common/i18n';
+import {l_statistics as l} from '../static/scripts/common/i18n/statistics';
+import {withCatalystContext} from '../context';
+import manifest from '../static/manifest';
+
+import {formatCount, formatPercentage} from './utilities';
+import StatisticsLayout from './StatisticsLayout';
+
+type EditCategoryT = {|
+  +edit_name: string,
+  +edit_type: string,
+|};
+
+type EditsStatsT = {|
+  +$c: CatalystContextT,
+  +dateCollected: string,
+  +stats: {[string]: number},
+  +statsByCategory: {[string]: $ReadOnlyArray<EditCategoryT>},
+|};
+
+const Edits = ({$c, dateCollected, stats, statsByCategory}: EditsStatsT) => (
+  <StatisticsLayout fullWidth page="edits" title={l('Edits')}>
+    {manifest.css('statistics')}
+    <p>
+      {l('Last updated: {date}',
+        {date: dateCollected})}
+    </p>
+    <h2>{l('Edits')}</h2>
+    {Object.keys(statsByCategory).length === 0 ? (
+      <p>
+        {l('No edit statistics available.')}
+      </p>
+    ) : (
+      <table className="database-statistics">
+        <tbody>
+          <tr>
+            <th colSpan="2">{addColon(l('Edits'))}</th>
+            <td>{formatCount($c, stats['count.edit'])}</td>
+            <td />
+          </tr>
+          {Object.keys(statsByCategory)
+            .sort()
+            .map((categoryKey) => {
+              const category = statsByCategory[categoryKey];
+              return (
+                <>
+                  <tr className="thead">
+                    <th colSpan="4">{categoryKey}</th>
+                  </tr>
+                  {category.map((type) => (
+                    <tr key={type.edit_type}>
+                      <th />
+                      <th>{lMbServer(type.edit_name)}</th>
+                      <td>
+                        {formatCount(
+                          $c,
+                          stats['count.edit.type.' + type.edit_type],
+                        )}
+                      </td>
+                      <td>
+                        {formatPercentage(
+                          $c,
+                          stats['count.edit.type.' + type.edit_type] / stats['count.edit'],
+                          2,
+                        )}
+                      </td>
+                    </tr>
+                  ))}
+                </>
+              );
+            })}
+        </tbody>
+      </table>
+    )}
+  </StatisticsLayout>
+);
+
+export default withCatalystContext(Edits);
