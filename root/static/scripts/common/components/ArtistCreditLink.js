d315939ca9ffb56db0f701446924c87752b7441d
Nicolás Tamargo <reosarevok@gmail.com>
2019-02-21 21:51:47 +0200

    Add @flow to ArtistCreditLink and fix accordingly (#925)

--- root/static/scripts/common/components/ArtistCreditLink.js
+++ root/static/scripts/common/components/ArtistCreditLink.js
@@ -1,4 +1,5 @@
 /*
+ * @flow
  * This file is part of MusicBrainz, the open internet music database.
  * Copyright (C) 2015–2016 MetaBrainz Foundation
  * Licensed under the GPL version 2, or (at your option) any later version:
@@ -7,28 +8,42 @@
 
 import React from 'react';
 
-import EntityLink from './EntityLink';
+import EntityLink, {DeletedLink} from './EntityLink';
 
-const ArtistCreditLink = ({artistCredit, showDeleted = true, ...props}) => {
+type Props = {
+  +artistCredit: ArtistCreditT,
+  +plain?: boolean,
+  +showDeleted?: boolean,
+  +target?: '_blank',
+};
+
+const ArtistCreditLink = ({
+  artistCredit,
+  showDeleted = true,
+  ...props
+}: Props) => {
   const parts = [];
   for (let i = 0; i < artistCredit.length; i++) {
     const credit = artistCredit[i];
     if (props.plain) {
       parts.push(credit.name);
     } else {
-      let artist = credit.artist;
-      if (!artist) {
-        artist = {entityType: 'artist', name: credit.name};
+      const artist = credit.artist;
+      if (artist) {
+        parts.push(
+          <EntityLink
+            content={credit.name}
+            entity={artist}
+            key={i}
+            showDeleted={showDeleted}
+            target={props.target}
+          />,
+        );
+      } else {
+        parts.push(
+          <DeletedLink allowNew={false} name={credit.name} />,
+        );
       }
-      parts.push(
-        <EntityLink
-          content={credit.name}
-          entity={artist}
-          key={i}
-          showDeleted={showDeleted}
-          target={props.target}
-        />,
-      );
     }
     parts.push(credit.joinPhrase);
   }
