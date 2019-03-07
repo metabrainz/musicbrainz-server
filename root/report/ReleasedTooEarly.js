b625c0fe03908eeddd8a95cdf25a97346852b690
Nicolás Tamargo <reosarevok@gmail.com>
2019-03-07 21:37:27 +0200

    MBS-9909 (14 - releases): ReleasedTooEarly

---
+++ root/report/ReleasedTooEarly.js
@@ -0,0 +1,50 @@
+/*
+ * @flow
+ * Copyright (C) 2018 MetaBrainz Foundation
+ *
+ * This file is part of MusicBrainz, the open internet music database,
+ * and is licensed under the GPL version 2, or (at your option) any
+ * later version: http://www.gnu.org/licenses/gpl-2.0.txt
+ */
+
+import * as React from 'react';
+
+import {withCatalystContext} from '../context';
+import Layout from '../layout';
+import formatUserDate from '../utility/formatUserDate';
+
+import ReleaseList from './components/ReleaseList';
+import FilterLink from './FilterLink';
+import type {ReportDataT, ReportReleaseT} from './types';
+
+const ReleasedTooEarly = ({
+  $c,
+  canBeFiltered,
+  filtered,
+  generated,
+  items,
+  pager,
+}: ReportDataT<ReportReleaseT>) => (
+  <Layout fullWidth title={l('Releases released too early')}>
+    <h1>{l('Releases released too early')}</h1>
+
+    <ul>
+      <li>
+        {l(`This report shows releases which have disc IDs even though they
+            were released too early to have disc IDs, where one of the medium
+            formats didn't exist at the time the release was released or
+            where a disc ID is attached to a medium whose format does not
+            have disc IDs.`)}
+      </li>
+      <li>{texp.l('Total releases found: {count}', {count: pager.total_entries})}</li>
+      <li>{texp.l('Generated on {date}', {date: formatUserDate($c.user, generated)})}</li>
+
+      {canBeFiltered ? <FilterLink filtered={filtered} /> : null}
+    </ul>
+
+    <ReleaseList items={items} pager={pager} />
+
+  </Layout>
+);
+
+export default withCatalystContext(ReleasedTooEarly);
