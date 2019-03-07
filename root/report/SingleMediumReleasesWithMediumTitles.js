0413b9f2a0aa7bd92d0e9616ed07a558baa948bd
Nicolás Tamargo <reosarevok@gmail.com>
2019-03-07 21:40:02 +0200

    MBS-9909 (14 - releases): SingleMediumReleasesWithMediumTitles

---
+++ root/report/SingleMediumReleasesWithMediumTitles.js
@@ -0,0 +1,48 @@
+/*
+ * @flow
+ * Copyright (C) 2018 MetaBrainz Foundation
+ *
+ * This file is part of MusicBrainz, the open internet music database,
+ * and is licensed under the GPL version 2, or (at your option) any
+ * later version: http://www.gnu.org/licenses/gpl-2.0.txt
+ */
+
+import * as React from 'react';
+
+import {withCatalystContext} from '../context';
+import Layout from '../layout';
+import formatUserDate from '../utility/formatUserDate';
+
+import ReleaseList from './components/ReleaseList';
+import FilterLink from './FilterLink';
+import type {ReportDataT, ReportReleaseT} from './types';
+
+const SingleMediumReleasesWithMediumTitles = ({
+  $c,
+  canBeFiltered,
+  filtered,
+  generated,
+  items,
+  pager,
+}: ReportDataT<ReportReleaseT>) => (
+  <Layout fullWidth title={l('Releases with a single medium that has a name')}>
+    <h1>{l('Releases with a single medium that has a name')}</h1>
+
+    <ul>
+      <li>
+        {l(`This report shows releases that have a single medium, where this
+            medium also has a specific name. Usually, this is not necessary
+            and is duplicate information which can be removed.`)}
+      </li>
+      <li>{texp.l('Total releases found: {count}', {count: pager.total_entries})}</li>
+      <li>{texp.l('Generated on {date}', {date: formatUserDate($c.user, generated)})}</li>
+
+      {canBeFiltered ? <FilterLink filtered={filtered} /> : null}
+    </ul>
+
+    <ReleaseList items={items} pager={pager} />
+
+  </Layout>
+);
+
+export default withCatalystContext(SingleMediumReleasesWithMediumTitles);
