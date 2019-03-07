e68ed59c65c784c1005361d0a1ce6f8e96a18210
Nicolás Tamargo <reosarevok@gmail.com>
2019-03-07 21:40:39 +0200

    MBS-9909 (14 - releases): TracksNamedWithSequence

---
+++ root/report/TracksNamedWithSequence.js
@@ -0,0 +1,50 @@
+/*
+ * @flow
+ * Copyright (C) 2018 MetaBrainz Foundation
+ *
+ * This file is part of MusicBrainz, the open internet music database,
+ * and is licensed under the GPL version 2, or (at your option) any
+ * later version: http://www.gnu.org/licenses/gpl-2.0.txt
+ */
+
+import * as React from 'react';
+
+import {withCatalystContext} from '../context';
+import Layout from '../layout';
+import formatUserDate from '../utility/formatUserDate';
+
+import ReleaseList from './components/ReleaseList';
+import FilterLink from './FilterLink';
+import type {ReportDataT, ReportReleaseT} from './types';
+
+const TracksNamedWithSequence = ({
+  $c,
+  canBeFiltered,
+  filtered,
+  generated,
+  items,
+  pager,
+}: ReportDataT<ReportReleaseT>) => (
+  <Layout fullWidth title={l('Releases where track names start with their track number')}>
+    <h1>{l('Releases where track names start with their track number')}</h1>
+
+    <ul>
+      <li>
+        {l(`This report aims to identify releases where track names include
+            their own track number, e.g. "1) Some Name" (instead of just
+            "Some Name"). Notice that sometimes this is justified and correct,
+             don't automatically assume it is a mistake! If you confirm it
+             is a mistake, please correct it.`)}
+      </li>
+      <li>{texp.l('Total releases found: {count}', {count: pager.total_entries})}</li>
+      <li>{texp.l('Generated on {date}', {date: formatUserDate($c.user, generated)})}</li>
+
+      {canBeFiltered ? <FilterLink filtered={filtered} /> : null}
+    </ul>
+
+    <ReleaseList items={items} pager={pager} />
+
+  </Layout>
+);
+
+export default withCatalystContext(TracksNamedWithSequence);
