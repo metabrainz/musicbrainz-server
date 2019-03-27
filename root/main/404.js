cfcfe64cacd4c128e4562940afcd04a7d8b16584
Nicolás Tamargo <reosarevok@gmail.com>
2019-03-27 20:54:06 +0200

    Run eslint --fix on 404.js

--- root/main/404.js
+++ root/main/404.js
@@ -1,7 +1,9 @@
-// This file is part of MusicBrainz, the open internet music database.
-// Copyright (C) 2015 MetaBrainz Foundation
-// Licensed under the GPL version 2, or (at your option) any later version:
-// http://www.gnu.org/licenses/gpl-2.0.txt
+/*
+ * This file is part of MusicBrainz, the open internet music database.
+ * Copyright (C) 2015 MetaBrainz Foundation
+ * Licensed under the GPL version 2, or (at your option) any later version:
+ * http://www.gnu.org/licenses/gpl-2.0.txt
+ */
 
 import React from 'react';
 
@@ -9,11 +11,13 @@ import {withCatalystContext} from '../context';
 import Layout from '../layout';
 import bugTrackerURL from '../static/scripts/common/utility/bugTrackerURL';
 
-// Please try and keep the WikiDoc error template (doc/error.tt)
-// looking similar to how this template looks.
+/*
+ * Please try and keep the WikiDoc error template (doc/error.tt)
+ * looking similar to how this template looks.
+ */
 
 const _404 = ({$c, ...props}) => (
-  <Layout {...props} title={l('Page Not Found')} fullWidth={true}>
+  <Layout {...props} fullWidth title={l('Page Not Found')}>
     <div id="content">
       <h1>{l('Page Not Found')}</h1>
       <p>
@@ -26,14 +30,14 @@ const _404 = ({$c, ...props}) => (
         </p>}
       <p>
         {exp.l('Looking for help? Check out our {doc|documentation} or {faq|FAQ}.',
-           {doc: '/doc/MusicBrainz_Documentation', faq: '/doc/FAQ'})}
+               {doc: '/doc/MusicBrainz_Documentation', faq: '/doc/FAQ'})}
       </p>
       <p>
         {exp.l('Found a broken link on our site? Please {report|report a bug} and include any error message that is shown above.', {
           report: bugTrackerURL(
             'Nonexistent page: ' + $c.req.uri + '\n' +
-            'Referrer: ' + ($c.req.headers.referer || '')
-          )
+            'Referrer: ' + ($c.req.headers.referer || ''),
+          ),
         })}
       </p>
     </div>
