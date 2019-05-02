af0239cce921771230928ca87b3a171db4807a47
Nicolás Tamargo <reosarevok@gmail.com>
2019-05-02 19:02:27 +0300

    MBS-9491: Add last_updated column to genre (#1051)

--- admin/sql/updates/20190317-mbs-9941-mbs-10062-fks.sql
+++ admin/sql/updates/20190317-mbs-9941-mbs-10062-fks.sql
@@ -6,6 +6,12 @@ ALTER TABLE genre_alias
    FOREIGN KEY (genre)
    REFERENCES genre(id);
 
+CREATE TRIGGER b_upd_genre BEFORE UPDATE ON genre
+    FOR EACH ROW EXECUTE PROCEDURE b_upd_last_updated_table();
+
+CREATE TRIGGER b_upd_genre_alias BEFORE UPDATE ON genre_alias
+    FOR EACH ROW EXECUTE PROCEDURE b_upd_last_updated_table();
+
 CREATE TRIGGER unique_primary_for_locale BEFORE UPDATE OR INSERT ON genre_alias
     FOR EACH ROW EXECUTE PROCEDURE unique_primary_genre_alias();
 
