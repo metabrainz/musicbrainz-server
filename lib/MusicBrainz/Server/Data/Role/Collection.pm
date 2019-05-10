8c9b014a99af324d02c57d744b17286cb73040c8
yvanzo <yvanzo@metabrainz.org>
2019-05-10 07:38:15 +0200

    [fixup next] copy find_by_collection function
    as find_items_by_collection
    both functions in Data::Role::Collection

--- lib/MusicBrainz/Server/Data/Role/Collection.pm
+++ lib/MusicBrainz/Server/Data/Role/Collection.pm
@@ -29,6 +29,32 @@ sub find_by_collection {
     $self->query_to_list_limited($query, [$collection_id], $limit, $offset);
 }
 
+sub find_items_by_collection {
+    my ($self, $collection_id, $limit, $offset, $order) = @_;
+
+    my ($order_by, $extra_join, $also_select) = $self->_order_by($order);
+    $extra_join //= '';
+
+    my $table = $self->_table;
+    my $type = $self->_type;
+
+    my $query = "
+      SELECT *
+      FROM (
+      SELECT DISTINCT ON (" . $self->_id_column . ")
+        " . $self->_columns .
+          ($also_select ? ", $also_select" : "") . "
+        FROM $table
+        JOIN editor_collection_$type ec ON " . $self->_id_column . " = ec.$type
+        $extra_join
+        WHERE ec.collection = ?
+        ORDER BY id
+      ) $type
+      ORDER BY $order_by";
+
+    $self->query_to_list_limited($query, [$collection_id], $limit, $offset);
+}
+
 no Moose::Role;
 1;
 
