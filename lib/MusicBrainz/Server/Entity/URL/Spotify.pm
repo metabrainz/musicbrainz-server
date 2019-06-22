4d92b8e1d7bfa30c91617f4f4b3525d31fd3f2b1
yvanzo <yvanzo@metabrainz.org>
2019-06-22 15:43:20 +0200

    MBS-10250: Distinguish Spotify links sidebar name
    Spotify user accounts are 'social network' links,
    where users are able to publicly share playlists,
    of recordings that not necessarily belong to them.
    
    It differs from other Spotify links (artist, album, track) which are
    automatically associated to the 'stream for free' relationship type.
    
    Hence the link names 'Playlists at Spotify' and 'Stream at Spotify'.

--- lib/MusicBrainz/Server/Entity/URL/Spotify.pm
+++ lib/MusicBrainz/Server/Entity/URL/Spotify.pm
@@ -7,7 +7,15 @@ use MusicBrainz::Server::Translation qw( l );
 extends 'MusicBrainz::Server::Entity::URL';
 with 'MusicBrainz::Server::Entity::URL::Sidebar';
 
-sub sidebar_name { l('Stream at Spotify') }
+sub sidebar_name {
+    my $self = shift;
+
+    if ($self->url =~ m{^(?:https?:)?//(?:[^/]+.)?spotify.com/user/[^/?&#]+/?}i) {
+        return l('Playlists at Spotify');
+    } else {
+        return l('Stream at Spotify');
+    }
+};
 
 __PACKAGE__->meta->make_immutable;
 no Moose;
