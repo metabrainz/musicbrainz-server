c4456469947fae26d214c8c09a4268559bcf68b9
Anirudh Jain <cyna@Anirudhs-MacBook-Pro.local>
2019-06-30 00:35:23 +0530

    Convert add_place.tt

--- root/edit/details/AddPlace.js
+++ root/edit/details/AddPlace.js
@@ -1,22 +1,75 @@
 import React from 'react';
 
 import DescriptiveLink from '../../static/scripts/common/components/DescriptiveLink';
+import isDateEmpty from '../../static/scripts/common/utility/isDateEmpty';
+import formatDate from '../../static/scripts/common/utility/formatDate';
+import yesNo from '../../static/scripts/common/utility/yesNo';
 
 const AddPlace = ({edit}) => {
+  console.log(edit);
   const display = edit.display_data;
-  const entityType = display.entity_type;
-  const entity = display[entityType];
+  const entity = display["place"];
   return (
     <>
       <table className="details">
         <tr>
           <th>{l('Place:')}</th>
-          <td>
-            <DescriptiveLink entity={entity} />
-          </td>
+          <td><DescriptiveLink entity={entity} /></td>
         </tr>
       </table>
-      <table />
+      <table className="details add-place">
+        <tr>
+          <th>{l('Name:')}</th>
+          <td>{display.name}</td>
+        </tr>
+        {display.comment ? (
+          <tr>
+            <th>{addColon(l('Disambiguation'))}</th>
+            <td>{display.comment}</td>
+          </tr>
+        ): null}
+        {display.type ? (
+          <tr>
+            <th>{l('Type:')}</th>
+            <td>{display.type.name}</td>
+          </tr>
+        ): null}
+        {display.address ? (
+          <tr>
+            <th>{l('Address:')}</th>
+            <td>{display.address}</td>
+          </tr>
+        ): null}
+        {display.area ? (
+          <tr>
+            <th>{l('Area:')}</th>
+            <td>{DescriptiveLink(display.area)}</td>
+          </tr>
+        ): null}
+        {display.coordinates ? (
+          <tr>
+            <th>{l('Coordinates:')}</th>
+            <td>{display.coordinates.format}</td>
+          </tr>
+        ): null}
+        {isDateEmpty(display.begin_date) ? null : (
+          <tr>
+            <th>{l('Begin date:')}</th>
+            <td>{formatDate(display.begin_date)}</td>
+          </tr>
+        )}
+        {isDateEmpty(display.end_date) ? null : (
+          <tr>
+            <th>{l('End date:')}</th>
+            <td>{formatDate(display.end_date)}</td>
+          </tr>
+        )}
+
+        <tr>
+          <th>{l('Ended:')}</th>
+          <td>{yesNo(display.ended)}</td>
+        </tr>
+      </table>
     </>
   );
 };
--- root/server/components.js
+++ root/server/components.js
@@ -256,6 +256,7 @@ module.exports = {
   'edit/details/AddArea': require('../edit/details/AddArea'),
   'edit/details/AddArtist': require('../edit/details/AddArtist'),
   'edit/details/AddEvent': require('../edit/details/AddEvent'),
+  'edit/details/AddPlace': require('../edit/details/AddPlace'),
   'edit/details/AddRemoveAlias': require('../edit/details/AddRemoveAlias'),
   'event/EventHeader': require('../event/EventHeader'),
   'instrument/InstrumentHeader': require('../instrument/InstrumentHeader'),
