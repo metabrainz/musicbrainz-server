7d48a14335990ac748416f134b63f8119698cd23
Suyash Garg <suyashgargsfam@gmail.com>
2019-07-01 18:22:57 +0200

    MBS-10274: Make sure to run postgres to listen on localhost
    Run postgres and listen on localhost before trying to create database

--- docker/musicbrainz-test-database/create_test_db.sh
+++ docker/musicbrainz-test-database/create_test_db.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+pg_ctl -o "-c listen_addresses='localhost'" -w restart
 cd /home/musicbrainz/musicbrainz-server
 if [ "$MB_IMPORT_DUMPS" = "true" ]; then
     carton exec -- ./docker/scripts/import_db.sh
