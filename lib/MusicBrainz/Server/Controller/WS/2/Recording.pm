6a5179380c98a8909edd5ee632bbb9797c93b90b
Nicolás Tamargo <reosarevok@gmail.com>
2019-07-26 14:30:49 +0300

    MBS-8521: work-level-rels for recording browse (#1104)

--- lib/MusicBrainz/Server/Controller/WS/2/Recording.pm
+++ lib/MusicBrainz/Server/Controller/WS/2/Recording.pm
@@ -24,7 +24,8 @@ my $ws_defs = Data::OptList::mkopt([
                          method   => 'GET',
                          linked   => [ qw(artist release collection work) ],
                          inc      => [ qw(aliases artist-credits puids isrcs annotation
-                                          _relations tags user-tags genres user-genres ratings user-ratings) ],
+                                          _relations tags user-tags genres user-genres ratings user-ratings
+                                          work-level-rels) ],
                          optional => [ qw(fmt limit offset) ],
      },
      recording => {
