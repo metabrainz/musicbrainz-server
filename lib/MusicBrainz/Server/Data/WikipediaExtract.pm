d7c49794ebbbbe6bfb14883b780964e4f1cdaa87
Michael Wiencek <mwtuea@gmail.com>
2019-08-14 13:13:26 -0500

    Simplify redirected languages handling

--- lib/MusicBrainz/Server/Data/WikipediaExtract.pm
+++ lib/MusicBrainz/Server/Data/WikipediaExtract.pm
@@ -20,44 +20,41 @@ Readonly my $LANG_CACHE_TIMEOUT => 60 * 60 * 24 * 7; # 1 week
 # we still want to keep them around a while
 Readonly my $EXTRACT_CACHE_TIMEOUT => 60 * 60 * 24 * 3; # 3 days
 
+our %redirected_languages;
+
 sub get_extract
 {
     my ($self, $links, $wanted_language, %opts) = @_;
     my $cache_only = $opts{cache_only} // 0;
 
     my ($first_link) = $links->[0];
-    my @redirected_languages;
+    local %redirected_languages = ();
 
     if ($first_link->isa('MusicBrainz::Server::Entity::URL::Wikipedia') && $wanted_language eq $first_link->language) {
         if (not $self->_check_for_redirect($first_link->page_name, $wanted_language)) {
             return $self->get_extract_by_language($first_link->page_name, $wanted_language, cache_only => $cache_only);
         }
-        push @redirected_languages, $wanted_language;
     }
 
     # We didn't by luck get a link in the right language
     my ($languages, $link) = $self->get_available_languages($links, cache_only => $cache_only);
 
     if (defined $languages && scalar @$languages) {
+        my %languages_by_code = map { $_->{lang} => $_ } @$languages;
+
         # Use desired language if available
-        my $lang_to_use;
-        if (not any { $_ eq $wanted_language } @redirected_languages) {
-            $lang_to_use = first { $_->{lang} eq $wanted_language } @$languages;
-            if ($lang_to_use && $self->_check_for_redirect($lang_to_use->{title}, $wanted_language)) {
-                $lang_to_use = undef;
-            }
+        my $lang_to_use = $languages_by_code{$wanted_language};
+        if ($lang_to_use && $self->_check_for_redirect($lang_to_use->{title}, $wanted_language)) {
+            $lang_to_use = undef;
         }
 
         # Fall back to browser accepted languages
         if (!$lang_to_use) {
             for my $lang (Translation->all_system_languages) {
-                if (not any { $_ eq $lang } @redirected_languages) {
-                    $lang_to_use = first { $_->{lang} eq $lang } @$languages;
-                    if ($lang_to_use) {
-                        last unless $self->_check_for_redirect($lang_to_use->{title}, $lang);
-                        push @redirected_languages, $lang;
-                        $lang_to_use = undef;
-                    }
+                $lang_to_use = $languages_by_code{$lang};
+                if ($lang_to_use) {
+                    last unless $self->_check_for_redirect($lang_to_use->{title}, $lang);
+                    $lang_to_use = undef;
                 }
             }
 
@@ -67,13 +64,10 @@ sub get_extract
                 if (defined $editor) {
                     my @editor_languages = grep { $_ } map { $_->{language}->{iso_code_1} } @{ $editor->languages };
                     for my $lang (@editor_languages) {
-                        if (not any { $_ eq $lang } @redirected_languages) {
-                            $lang_to_use = first { $_->{lang} eq $lang } @$languages;
-                            if ($lang_to_use) {
-                                last unless $self->_check_for_redirect($lang_to_use->{title}, $lang);
-                                push @redirected_languages, $lang;
-                                $lang_to_use = undef;
-                            }
+                        $lang_to_use = $languages_by_code{$lang};
+                        if ($lang_to_use) {
+                            last unless $self->_check_for_redirect($lang_to_use->{title}, $lang);
+                            $lang_to_use = undef;
                         }
                     }
                 }
@@ -81,31 +75,24 @@ sub get_extract
                 # Fall back to most frequent languages
                 if (!$lang_to_use) {
                     for my $lang (qw(en ja de fr fi it sv es ru pl nl pt et da ko ca cs cy el he hu id lt lv no ro sk sl tr uk vi zh)) {
-                        if (not any { $_ eq $lang } @redirected_languages) {
-                            $lang_to_use = first { $_->{lang} eq $lang } @$languages;
-                            if ($lang_to_use) {
-                                last unless $self->_check_for_redirect($lang_to_use->{title}, $lang);
-                                push @redirected_languages, $lang;
-                                $lang_to_use = undef;
-                            }
+                        $lang_to_use = $languages_by_code{$lang};
+                        if ($lang_to_use) {
+                            last unless $self->_check_for_redirect($lang_to_use->{title}, $lang);
+                            $lang_to_use = undef;
                         }
                     }
 
                     # Fall back to languages that are explicitly linked
                     if (!$lang_to_use) {
                         $link = first { $_->isa('MusicBrainz::Server::Entity::URL::Wikipedia') } @$links;
-                        if (defined $link && not any { $_ eq $link->language } @redirected_languages) {
-                            unless ($self->_check_for_redirect($link->page_name, $link->language)) {
-                                $lang_to_use = {'title' => $link->page_name, 'lang' => $link->language};
-                            } else {
-                                push @redirected_languages, $link->language;
-                            }
+                        if (defined $link && !$self->_check_for_redirect($link->page_name, $link->language)) {
+                            $lang_to_use = {'title' => $link->page_name, 'lang' => $link->language};
                         }
 
                         # Finally fall back to “whatever we have”
                         if (!$lang_to_use) {
                             $lang_to_use = $languages->[0];
-                            $lang_to_use = undef if (any { $_ eq $lang_to_use->lang } @redirected_languages) ||
+                            $lang_to_use = undef if
                                 $self->_check_for_redirect($lang_to_use->{title}, $lang_to_use->{lang});
                         }
                     }
@@ -203,6 +190,7 @@ sub _extract_by_language_callback
 sub _check_for_redirect
 {
     my ($self, $title, $language, %opts) = @_;
+    return 1 if exists $redirected_languages{$language};
     # We use formatversion=2 so that "redirect" is returned as an actual boolean, not the empty string
     my $url_pattern = "https://%s.wikipedia.org/w/api.php?action=query&prop=info&format=json&formatversion=2&titles=%s";
     my $return = $self->_fetch_cache_or_url($url_pattern, 'redirect',
@@ -210,6 +198,7 @@ sub _check_for_redirect
                                       $title, $language,
                                       \&_check_for_redirect_callback,
                                       %opts);
+    $redirected_languages{$language} = 1 if $return;
     return $return;
 }
 
