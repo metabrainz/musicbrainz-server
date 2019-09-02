ac9fd5f36b6929c23a87e3255164908c135b789d
Nicolás Tamargo <reosarevok@gmail.com>
2019-09-02 12:39:03 +0300

    Eslint: max-len

--- root/recording/RecordingIndex.js
+++ root/recording/RecordingIndex.js
@@ -56,7 +56,9 @@ const RecordingAppearancesTable = ({
     <tbody>
       {tracks.map((tracksWithReleaseStatus) => {
         const sampleRelease =
-          linkedEntities.release[tracksWithReleaseStatus[0].medium.release_id];
+          linkedEntities.release[
+            tracksWithReleaseStatus[0].medium.release_id
+          ];
         const status = sampleRelease.status;
         return (
           <React.Fragment key={status ? status.name : 'no-status'}>
