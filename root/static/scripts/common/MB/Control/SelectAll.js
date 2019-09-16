51cacd5fca6e1ce784e9e89a38cef15f10f9e77c
Nicolás Tamargo <reosarevok@gmail.com>
2019-09-16 18:57:34 +0300

    Eslint fixes: indent

--- root/static/scripts/common/MB/Control/SelectAll.js
+++ root/static/scripts/common/MB/Control/SelectAll.js
@@ -11,42 +11,42 @@ import $ from 'jquery';
 import MB from '../../MB';
 
 MB.Control.SelectAll = function (table) {
-    var self = {};
+  var self = {};
 
-    self.$table = $(table);
-    self.$checkboxes = self.$table.find('td input[type="checkbox"]');
-    self.lastChecked = null;
+  self.$table = $(table);
+  self.$checkboxes = self.$table.find('td input[type="checkbox"]');
+  self.lastChecked = null;
 
-    self.$selector = self.$table.find('th input[type="checkbox"]');
+  self.$selector = self.$table.find('th input[type="checkbox"]');
 
-    self.$selector.toggle(self.$checkboxes.length > 0);
+  self.$selector.toggle(self.$checkboxes.length > 0);
 
-    self.$selector.change(function () {
-        var $input = $(this);
-        self.$checkboxes.prop('checked', $input.prop('checked'));
-    });
+  self.$selector.change(function () {
+    var $input = $(this);
+    self.$checkboxes.prop('checked', $input.prop('checked'));
+  });
 
-    self.$checkboxes.click(function (event) {
-        if (event.shiftKey && self.lastChecked && self.lastChecked != this) {
-            const first = self.$checkboxes.index(self.lastChecked);
-            const last = self.$checkboxes.index(this);
+  self.$checkboxes.click(function (event) {
+    if (event.shiftKey && self.lastChecked && self.lastChecked != this) {
+      const first = self.$checkboxes.index(self.lastChecked);
+      const last = self.$checkboxes.index(this);
 
-            if (first > last) {
-                self.$checkboxes.slice(last, first + 1)
-                    .prop('checked', this.checked);
-            } else if (last > first) {
-                self.$checkboxes.slice(first, last + 1)
-                    .prop('checked', this.checked);
-            }
-        }
-        self.lastChecked = this;
-    });
+      if (first > last) {
+        self.$checkboxes.slice(last, first + 1)
+          .prop('checked', this.checked);
+      } else if (last > first) {
+        self.$checkboxes.slice(first, last + 1)
+          .prop('checked', this.checked);
+      }
+    }
+    self.lastChecked = this;
+  });
 
-    return self;
+  return self;
 };
 
 $(function () {
-    $('table.tbl').each(function () {
-        MB.Control.SelectAll(this);
-    });
+  $('table.tbl').each(function () {
+    MB.Control.SelectAll(this);
+  });
 });
