819af0552b88d056869fb8bb29c489c0ed5016d2
Nicolás Tamargo <reosarevok@gmail.com>
2019-09-16 21:05:39 +0300

    Split RangeSelect from SelectAll
    This allows us to use it in places where we just want the shift-click
    option available.

--- root/static/scripts/common/MB/Control/SelectAll.js
+++ root/static/scripts/common/MB/Control/SelectAll.js
@@ -10,39 +10,41 @@ import $ from 'jquery';
 
 import MB from '../../MB';
 
-MB.Control.SelectAll = function (table) {
-  const self = {};
+MB.Control.RangeSelect = function ($checkboxes) {
+  let lastChecked = null;
+  $checkboxes.click(function (event) {
+    const nowChecked = event.currentTarget;
 
-  self.$table = $(table);
-  self.$checkboxes = self.$table.find('td input[type="checkbox"]');
-  self.lastChecked = null;
-
-  self.$selector = self.$table.find('th input[type="checkbox"]');
-
-  self.$selector.toggle(self.$checkboxes.length > 0);
-
-  self.$selector.change(function () {
-    const $input = $(this);
-    self.$checkboxes.prop('checked', $input.prop('checked'));
-  });
-
-  self.$checkboxes.click(function (event) {
-    if (event.shiftKey && self.lastChecked && self.lastChecked != this) {
-      const first = self.$checkboxes.index(self.lastChecked);
-      const last = self.$checkboxes.index(this);
+    if (event.shiftKey && lastChecked && lastChecked !== nowChecked) {
+      const first = $checkboxes.index(lastChecked);
+      const last = $checkboxes.index(nowChecked);
 
       if (first > last) {
-        self.$checkboxes.slice(last, first + 1)
-          .prop('checked', this.checked);
+        $checkboxes.slice(last, first + 1)
+          .prop('checked', nowChecked.checked);
       } else if (last > first) {
-        self.$checkboxes.slice(first, last + 1)
-          .prop('checked', this.checked);
+        $checkboxes.slice(first, last + 1)
+          .prop('checked', nowChecked.checked);
       }
     }
-    self.lastChecked = this;
+    lastChecked = nowChecked;
+  });
+};
+
+MB.Control.SelectAll = function (table) {
+  const $table = $(table);
+  const $checkboxes = $table.find('td input[type="checkbox"]');
+
+  const $selector = $table.find('th input[type="checkbox"]');
+
+  $selector.toggle($checkboxes.length > 0);
+
+  $selector.change(function () {
+    const $input = $(this);
+    $checkboxes.prop('checked', $input.prop('checked'));
   });
 
-  return self;
+  MB.Control.RangeSelect($checkboxes);
 };
 
 $(function () {
