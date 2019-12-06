a570d0ed9aaef0db4fe063488e5a016861f6eb10
ABCbum <50205705+ABCbum@users.noreply.github.com>
2019-12-07 03:33:10 +0700

    Make line break inside parentheses more consistent (#1301)

--- root/static/scripts/edit/forms.js
+++ root/static/scripts/edit/forms.js
@@ -60,8 +60,12 @@ MB.forms = {
 
     linkTypeOptions: function (root, backward) {
         function getText(data) {
-            return stripAttributes(data, l_relationships(
-                backward ? data.reverse_link_phrase : data.link_phrase));
+            return stripAttributes(
+                data, 
+                l_relationships(
+                    backward ? data.reverse_link_phrase : data.link_phrase,
+                ),
+            );
         }
 
         var options = MB.forms.buildOptionsTree(root, getText, 'id');
