metabrainz/musicbrainz-server

commit d2f9ced19a20a45e25b5a7e5beb0b7fdb4b726a4

MBS-10500: Allow localizing ModBot edit notes
ModBot's notes are currently always stored and displayed in English.
With this commit, a note can be stored in the database in a special
format:

  localize:{"message": "...", "args": {...}}

where "message" is a message in our gettext catalog, and "args" are any
substitution arguments needed.

When loading a note from ModBot, we check if it's in this format before
displaying it, and parse it to form a localized string using those
parameters.

This syntax can only be used in notes left by ModBot (a regular user
could not).

lib/MusicBrainz/Server/Entity/EditNote.pm
@@ -1,8 +1,14 @@
 package MusicBrainz::Server::Entity::EditNote;
+
+use 5.18.2;
+
+use JSON::XS;
 use Moose;
 use namespace::autoclean;
 
+use MusicBrainz::Server::Constants qw( $EDITOR_MODBOT );
 use MusicBrainz::Server::Entity::Types;
+use MusicBrainz::Server::Translation qw( l );
 use MusicBrainz::Server::Types qw( DateTime );
 
 has 'editor_id' => (
@@ -36,6 +42,38 @@ has 'post_time' => (
     is => 'rw',
     coerce => 1
 );
+
+sub _localize_text {
+    my ($text) = @_;
+
+    state $json = JSON::XS->new->utf8(0);
+
+    if (my ($source) = ($text =~ m/^localize:(.+)$/)) {
+        $source = $json->decode($source);
+
+        my $source_args = $source->{args} // {};
+        my %args = map {
+            my $value = $source_args->{$_};
+            $_ => (ref($value) ? $value : _localize_text($value // ''))
+        } keys %{$source_args};
+
+        $text = l($source->{message} // '', \%args);
+    }
+
+    return $text;
+}
+
+sub localize {
+    my ($self) = @_;
+
+    my $text = $self->text;
+
+    if ($self->editor_id == $EDITOR_MODBOT) {
+        return _localize_text($text);
+    }
+
+    return $text;
+}
 
 no Moose;
 __PACKAGE__->meta->make_immutable;

lib/MusicBrainz/Server/Data/Utils.pm
@@ -1,5 +1,6 @@
 package MusicBrainz::Server::Data::Utils;
 
+use 5.18.2;
 use strict;
 use warnings;
 
@@ -14,6 +15,7 @@ use Math::Random::Secure qw( irand );
 use MIME::Base64 qw( encode_base64url );
 use Digest::SHA qw( sha1_base64 );
 use Encode qw( decode encode );
+use JSON::XS;
 use List::MoreUtils qw( natatime uniq zip );
 use List::UtilsBy qw( sort_by );
 use MusicBrainz::Server::Constants qw(
@@ -50,6 +52,7 @@ our @EXPORT_OK = qw(
     hash_to_row
     is_special_artist
     is_special_label
+    localized_note
     load_everything_for_edits
     load_meta
     load_subobjects
@@ -662,6 +665,16 @@ sub datetime_to_iso8601 {
     $date->set_time_zone('UTC');
     $date = $date->iso8601 . 'Z';
     return $date;
+}
+
+sub localized_note {
+    my ($message, $args) = @_;
+
+    state $json = JSON::XS->new;
+    'localize:' . $json->encode({
+        message => $message,
+        defined $args ? (args => $args) : (),
+    });
 }
 
 1;

root/components/common-macros.tt
@@ -950,7 +950,11 @@ note-[%- edit.id -%]-[%- loop.count() -%]
       [%- editor_type_info(note.editor) -%]
     </h3>
     <div class="edit-note-text [% 'modbot' IF note.editor_id == 4 %]">
-      [%- note.text | format_editnote -%]
+      [%- IF note.editor_id == 4;
+            note.localize;
+          ELSE;
+            note.text | format_editnote;
+          END; -%]
     </div>
   </div>
 [%~ END ~%]

commit e30062fd305273e532e3b53093d0667ca1286b04

MBS-10511: Regression: Modbot edit references are no longer linkified
This was broken in d2f9ced19a20a45e25b5a7e5beb0b7fdb4b726a4 due to all
ModBot notes no longer being passed to format_editnote. (We don't
need to do that if the note is actually localized, because it should be
using our translated string syntax to link/format things in that case.)

lib/MusicBrainz/Server/Entity/EditNote.pm
@@ -8,6 +8,7 @@ use namespace::autoclean;
 
 use MusicBrainz::Server::Constants qw( $EDITOR_MODBOT );
 use MusicBrainz::Server::Entity::Types;
+use MusicBrainz::Server::Filters qw( format_editnote );
 use MusicBrainz::Server::Translation qw( l );
 use MusicBrainz::Server::Types qw( DateTime );
 
@@ -58,6 +59,9 @@ sub _localize_text {
         } keys %{$source_args};
 
         $text = l($source->{message} // '', \%args);
+    } else {
+        # Otherwise, assume this message uses edit note syntax.
+        $text = format_editnote($text);
     }
 
     return $text;