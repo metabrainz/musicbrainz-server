8791f6d6c2c53a5e85752ad21683d4c2bbd56cb7
Monica Qiu <m1102778586@gmail.com>
2019-12-09 12:13:48 -0500

    fixed indent and no-var for root/static/scripts/common/MB/Control/Autocomplete.js

--- root/static/scripts/common/MB/Control/Autocomplete.js
+++ root/static/scripts/common/MB/Control/Autocomplete.js
@@ -464,8 +464,8 @@ $.widget("mb.entitylookup", $.ui.autocomplete, {
             action: _.bind(this._searchAgain, this, true)
         });
 
-        var allowCreation = window === window.top;
-        var entity = this.entity.replace("-", "_");
+        let allowCreation = window === window.top;
+            let entity = this.entity.replace("-", "_");
 
         if (allowCreation && addNewEntityLabels[entity]) {
             const label = addNewEntityLabels[entity]();
