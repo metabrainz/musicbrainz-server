metabrainz/musicbrainz-server

commit 434f499f57238420b586c091d6c152d85cd737c4

Fix release-recording merge display (MBS-10279)
Prior to this commit, there were three different subroutines for
calculating recording merges for $EDIT_RELEASE_MERGE edits. As you might
expect, these did not share a common implementation.

 1. Data::Release::determine_recording_merges:

    This was used for detecting "bad" recording merges where the artists
    differed (see root/release/merge.tt).

 2. Edit::Utils::calculate_recording_merges:

    This was added for storing `recording_merges` in the edit data, for
    display and historical purposes (see 0e41a26), but still not for
    deciding what would actually get merged. (And after 0e41a26,
    $EDIT_RELEASE_MERGE edits did not display any recording merges that
    would actually happen, only those that were predicted to happen
    when the edit was opened. Thus, the edit might say "All recordings
    for these releases are already merged" while open, yet that may
    have been false.)

 3. Data::Medium::merge:

    This is what implemented the actual merge logic. It received a
    couple fixes over the years in dae5b54 and c1992b2, both of which
    could cause the result of the edit to disagree with the display
    (assuming, at a minimum, the tracklist didn't change in the
    meantime).

These three methods have been unified into
Data::Release::determine_recording_merges, but the implementation is
largely based on the previous Data::Medium::merge method. The reasoning
for its being moved to Data::Release (and Data::Medium::merge being
removed) is that (1) a recording could appear multiple times on the
release, which needs to be factored into the merge calculation, and (2)
it doesn't make sense to merge two arbitrary mediums outside the context
of a release merge.

This commit also fixes the display issue mentioned in point 2 above. We
now display the recording merges that will actually happen when the edit
is applied, not those that were (incorrectly) calculated when the edit

lib/MusicBrainz/Server/Controller/Release.pm
@@ -40,7 +40,6 @@ use MusicBrainz::Server::Form::Utils qw(
 );
 use Scalar::Util qw( looks_like_number );
 use MusicBrainz::Server::Data::Utils qw( partial_date_to_hash artist_credit_to_ref );
-use MusicBrainz::Server::Edit::Utils qw( calculate_recording_merges );
 
 =head1 NAME
 
@@ -382,11 +381,17 @@ sub _merge_form_arguments {
     }
 
     my @bad_recording_merges;
-    my @recording_merges = $c->model('Release')->determine_recording_merges(@releases);
-    for my $recordings (@recording_merges) {
-        my @ac_ids = map { $_->artist_credit_id } @$recordings;
+    my $recording_merges = $c->model('Release')->determine_recording_merges(@releases);
+    for my $recording_merge (@{$recording_merges}) {
+        my @ac_ids = (
+            $recording_merge->{destination}{artist_credit_id},
+            map { $_->{artist_credit_id} } @{$recording_merge->{sources}},
+        );
         if (uniq(@ac_ids) > 1) {
-            push @bad_recording_merges, $recordings;
+            push @bad_recording_merges, (
+                Recording->new($recording_merge->{destination}),
+                map { Recording->new($_) } @{$recording_merge->{sources}},
+            );
         }
     }
     if (@bad_recording_merges) {
@@ -438,29 +443,6 @@ sub _merge_parameters {
                 }, keys %medium_changes
             ]
         );
-    } elsif ($form->field('merge_strategy')->value == $MusicBrainz::Server::Data::Release::MERGE_MERGE) {
-        my %release_map = map { $_->id => $_ } @$releases;
-
-        my $new_id = $form->field('target')->value;
-        my $new = $release_map{$new_id};
-        my $old = [map { $release_map{$_} } grep { $_ != $new_id } @{ $form->field('merging')->value }];
-
-        my $recording_merges = [map +{
-            medium => $_->{medium},
-            track => $_->{track},
-            destination => {
-                id => $_->{destination}->id,
-                name => $_->{destination}->name,
-                length => $_->{destination}->length
-            },
-            sources => [map +{
-                id => $_->id,
-                name => $_->name,
-                length => $_->length
-            }, @{ $_->{sources} }]
-        }, @{ calculate_recording_merges($new, $old) } ];
-
-        return (recording_merges => $recording_merges);
     } else {
         return ();
     }

lib/MusicBrainz/Server/Data/Release.pm
@@ -1,13 +1,16 @@
 package MusicBrainz::Server::Data::Release;
 
+use 5.18.2; # enables the state feature
+
 use Moose;
 use namespace::autoclean -also => [qw( _where_status_in _where_type_in )];
 
 use Carp 'confess';
 use DBDefs;
+use JSON::XS;
 use List::AllUtils qw( all );
 use List::MoreUtils qw( part );
-use List::UtilsBy qw( partition_by );
+use List::UtilsBy qw( nsort_by partition_by );
 use MusicBrainz::Server::Constants qw( :quality $EDIT_RELEASE_CREATE $STATUS_APPLIED );
 use MusicBrainz::Server::Entity::Barcode;
 use MusicBrainz::Server::Entity::PartialDate;
@@ -880,54 +883,155 @@ sub can_merge {
     }
 }
 
-sub determine_recording_merges
-{
-    my ($self, @releases) = @_;
+sub determine_medium_merges {
+    my ($self, $new_id, @old_ids) = @_;
 
-    my %medium_by_position;
-    foreach my $release (@releases) {
-        foreach my $medium ($release->all_mediums) {
-            if (exists $medium_by_position{$medium->position}) {
-                push @{ $medium_by_position{$medium->position} }, $medium;
-            }
-            else {
-                $medium_by_position{$medium->position} = [ $medium ];
+    $self->sql->select_list_of_hashes(
+        'SELECT newm.id AS new_id,
+                array_agg(oldm.id) AS old_ids
+           FROM medium newm,
+                medium oldm
+          WHERE newm.release = ?
+            AND oldm.release = any(?)
+            AND newm.position = oldm.position
+            AND newm.track_count = oldm.track_count
+          GROUP BY newm.id',
+        $new_id,
+        \@old_ids,
+    );
+}
+
+sub determine_recording_merges {
+    my ($self, $new_release_id, @old_release_ids) = @_;
+
+    my $possible_merges = $self->sql->select_list_of_hashes(q{
+        SELECT newm.position AS new_medium_position,
+               newt.number AS new_track_number,
+               newt.position AS new_track_position,
+               jsonb_build_object(
+                 'id', newr.id,
+                 'gid', newr.gid,
+                 'name', newr.name,
+                 'length', newr.length,
+                 'artist_credit_id', newr.artist_credit
+               ) AS new_recording,
+               array_agg(DISTINCT jsonb_build_object(
+                 'id', oldr.id,
+                 'gid', oldr.gid,
+                 'name', oldr.name,
+                 'length', oldr.length,
+                 'artist_credit_id', oldr.artist_credit
+               )) AS old_recordings
+          FROM medium newm,
+               medium oldm,
+               track newt,
+               track oldt,
+               recording newr,
+               recording oldr
+         WHERE newm.release = ?
+           AND oldm.release = any(?)
+           AND newm.position = oldm.position
+           AND newm.track_count = oldm.track_count
+           AND newt.medium = newm.id
+           AND oldt.medium = oldm.id
+           AND newt.position = oldt.position
+           AND newr.id = newt.recording
+           AND oldr.id = oldt.recording
+           AND newr.id != oldr.id
+         GROUP BY new_medium_position,
+                  new_track_number,
+                  new_track_position,
+                  newr.id
+         ORDER BY new_medium_position,
+                  new_track_position
+    }, $new_release_id, \@old_release_ids);
+
+    state $json = JSON::XS->new->utf8(0);
+    my %target_count;
+
+    for my $possible_merge (@{$possible_merges}) {
+        $possible_merge->{new_recording} = $json->decode($possible_merge->{new_recording});
+        $possible_merge->{old_recordings} = [map { $json->decode($_) } @{$possible_merge->{old_recordings}}];
+
+        # We need to make sure that for each old recording, there is only 1
+        # new recording to merge into. If there is > 1, then it's not clear
+        # what we should merge into.
+        $target_count{$_->{id}}++ for @{$possible_merge->{old_recordings}};
+    }
+
+    # MBS-8614. Track recording merges, to resolve cases where a recording is
+    # a merge source on one track (after which it gets deleted), and a merge
+    # target on another track (in which case we should instead use the ID of
+    # the target from the first merge). Example where recording 3 should be
+    # merged into recording 2:
+    # 1 -> 2
+    # 3 -> 1
+    my %merge_targets;
+
+    $possible_merges = [grep {
+        # Assigned to avoid confusion with unrelated uses of $_ below.
+        my $possible_merge = $_;
+
+        my ($new_recording, $old_recordings) = @{$possible_merge}{qw(
+            new_recording
+            old_recordings
+        )};
+
+        # As mentioned above, remove old (source) recordings that have
+        # multiple targets.
+        #
+        # Perl fact: this assignment modifies the array in $possible_merge.
+        @{$old_recordings} = grep { $target_count{$_->{id}} == 1 } @{$old_recordings};
+
+        $merge_targets{$_->{id}} = $new_recording for @{$old_recordings};
+
+        # Make sure there's at least one merge source left for this target.
+        # (This is the expression for which `grep` is evaluated on.)
+        scalar @{$old_recordings} > 0
+    } @{$possible_merges}];
+
+    my %recording_merges;
+    for my $possible_merge (@{$possible_merges}) {
+        my ($new_recording, $old_recordings) = @{$possible_merge}{qw(
+            new_recording
+            old_recordings
+        )};
+
+        $new_recording = $merge_targets{$new_recording->{id}} // $new_recording;
+        my $new_id = $new_recording->{id};
+
+        for my $old_recording (@{$old_recordings}) {
+            my $old_id = $old_recording->{id};
+
+            # If two recordings' positions are swapped (e.g. recording 1 is being
+            # merged into recording 2, and recording 2 is being merged into
+            # recording 1), then we don't merge them in that case, because it's
+            # probably not intentional.
+            if ($new_id != $old_id) {
+                my $merge = ($recording_merges{$new_id} //= {
+                    new_recording       => $new_recording,
+                    new_medium_position => $possible_merge->{new_medium_position},
+                    new_track_number    => $possible_merge->{new_track_number},
+                    new_track_position  => $possible_merge->{new_track_position},
+                });
+
+                push @{$merge->{old_recordings}}, $old_recording;
+
+                $merge_targets{$old_id} = $new_recording;
             }
         }
     }
 
-    my %recording_by_position;
-    for my $m_pos (keys %medium_by_position) {
-        # must have at least two mediums
-        my @mediums = @{ $medium_by_position{$m_pos} };
-        next if @mediums <= 1;
-        # all mediums must have the same number of tracks
-        my $track_count = $mediums[0]->track_count;
-        next if grep { $_->track_count != $track_count } @mediums;
-        # group recordings by track position
-        $recording_by_position{$m_pos} = {};
-        for my $medium (@mediums) {
-            for my $tr ($medium->all_tracks) {
-                my $tr_pos = $tr->position;
-                if (exists $recording_by_position{$m_pos}->{$tr_pos}) {
-                    push @{ $recording_by_position{$m_pos}->{$tr_pos} }, $tr->recording;
-                }
-                else {
-                    $recording_by_position{$m_pos}->{$tr_pos} = [ $tr->recording ];
-                }
-            }
-        }
-    }
-
-    my @merges;
-    for my $m_pos (sort { $a <=> $b } keys %recording_by_position) {
-        for my $tr_pos (sort { $a <=> $b } keys %{ $recording_by_position{$m_pos} }) {
-            my $recordings = $recording_by_position{$m_pos}->{$tr_pos};
-            push @merges, $recordings if scalar @$recordings;
-        }
-    }
-
-    return @merges;
+    # Sort, then convert to the format expected by
+    # MusicBrainz::Server::Edit::Release::Merge.
+    [map +{
+        medium      => $_->{new_medium_position},
+        track       => $_->{new_track_number},
+        destination => $_->{new_recording},
+        sources     => $_->{old_recordings},
+    }, nsort_by {
+        $_->{new_medium_position}, $_->{new_track_position}
+    } values %recording_merges];
 }
 
 sub merge
@@ -1078,22 +1182,24 @@ sub merge
                 old_ids => \@old_ids,
             });
 
-        my @merges = @{
-            $self->sql->select_list_of_hashes(
-                'SELECT newmed.id AS new_id,
-                        oldmed.id AS old_id
-                   FROM medium newmed, medium oldmed
-                  WHERE newmed.release = ?
-                    AND oldmed.release IN (' . placeholders(@old_ids) . ')
-                    AND newmed.position = oldmed.position',
-                $new_id, @old_ids
-            )
-        };
-        for my $merge (@merges) {
-            $self->c->model('Medium')->merge($merge->{new_id}, $merge->{old_id});
+        my $recording_merges = $opts{recording_merges} //
+            $self->determine_recording_merges($new_id, @old_ids);
+
+        for my $recording_merge (@{$recording_merges}) {
+            $self->c->model('Recording')->merge(
+                $recording_merge->{destination}{id},
+                map { $_->{id} } @{$recording_merge->{sources}},
+            );
+        }
+
+        for my $medium_merge (@{ $self->determine_medium_merges($new_id, @old_ids) }) {
+            $self->c->model('Track')->merge_mediums(
+                $medium_merge->{new_id},
+                @{$medium_merge->{old_ids}},
+            );
             $self->c->model('MediumCDTOC')->merge_mediums(
-                $merge->{new_id},
-                $merge->{old_id}
+                $medium_merge->{new_id},
+                @{$medium_merge->{old_ids}},
             );
         }
 

lib/MusicBrainz/Server/Data/Medium.pm
@@ -231,61 +231,6 @@ sub set_lengths_to_cdtoc
     $self->c->model('Recording')->_delete_from_cache(@recording_ids);
 }
 
-sub merge
-{
-    my ($self, $new_medium_id, $old_medium_id) = @_;
-    my @recording_merges = @{
-        $self->sql->select_list_of_hashes(
-            'SELECT DISTINCT newt.recording AS new, oldt.recording AS old
-               FROM track oldt
-               JOIN track newt ON newt.position = oldt.position
-              WHERE newt.medium = ? AND oldt.medium = ?
-                AND newt.recording != oldt.recording',
-            $new_medium_id, $old_medium_id
-        )
-    };
-
-    # We need to make sure that for each old recording, there is only 1 new recording
-    # to merge into. If there is > 1, then it's not clear what we should merge into.
-    my %target_count;
-    $target_count{ $_->{old} }++ for @recording_merges;
-
-    # MBS-8614. Track recording merges, to resolve cases where a recording is
-    # a merge source on one track (after which it gets deleted), and a merge
-    # target on another track (in which case we should instead use the ID of
-    # the target from the first merge). Example where recording 3 should be
-    # merged into recording 2:
-    # 1 -> 2
-    # 3 -> 1
-    my %merge_targets;
-
-    @recording_merges = grep {
-        my ($new, $old) = @{$_}{qw( new old )};
-
-        $merge_targets{$old} = $new;
-
-        $target_count{$old} == 1
-    } @recording_merges;
-
-    for my $recording_merge (@recording_merges) {
-        my ($new, $old) = @{$recording_merge}{qw( new old )};
-
-        $new = $merge_targets{$new} // $new;
-
-        # If two recordings' positions are swapped (e.g. recording 1 is being
-        # merged into recording 2, and recording 2 is being merged into
-        # recording 1), then we don't merge them in that case, because it's
-        # probably not intentional.
-        if ($new != $old) {
-            $self->c->model('Recording')->merge($new, $old);
-            $merge_targets{$old} = $new;
-        }
-    }
-
-    $self->c->model('Track')->merge_mediums($new_medium_id, $old_medium_id);
-}
-
-
 =method reorder
 
     reorder

lib/MusicBrainz/Server/Data/Track.pm
@@ -204,22 +204,26 @@ sub delete
 
 sub merge_mediums
 {
-    my ($self, $new_medium, $old_medium) = @_;
+    my ($self, $new_medium_id, @old_medium_ids) = @_;
 
     my @track_merges = @{
         $self->sql->select_list_of_lists(
-            'SELECT DISTINCT newt.id AS new, oldt.id AS old
+            'SELECT newt.id AS new_id,
+                    array_agg(oldt.id) AS old_ids
                FROM track oldt
                JOIN track newt ON newt.position = oldt.position
-              WHERE newt.medium = ? AND oldt.medium = ?',
-            $new_medium, $old_medium
+              WHERE newt.medium = ?
+                AND oldt.medium = any(?)
+              GROUP BY newt.id',
+            $new_medium_id,
+            \@old_medium_ids,
         )
     };
 
     for my $track_merge (@track_merges) {
-        my ($new, $old) = @$track_merge;
+        my ($new_id, $old_ids) = @$track_merge;
 
-        $self->_delete_and_redirect_gids('track', $new, $old);
+        $self->_delete_and_redirect_gids('track', $new_id, @{$old_ids});
     }
 }
 

commit 3ac0e4872e624bc91f4a81af52bdea75fd0e251a

MBS-10510: Fix ISE merging releases with different recording artists
Broken in commit 434f499f57238420b586c091d6c152d85cd737c4

Added a Selenium test.

lib/MusicBrainz/Server/Controller/Release.pm
@@ -1,6 +1,7 @@
 package MusicBrainz::Server::Controller::Release;
 use Moose;
 use MusicBrainz::Server::Track;
+use aliased 'MusicBrainz::Server::Entity::Recording';
 
 BEGIN { extends 'MusicBrainz::Server::Controller' }
 
@@ -467,10 +468,10 @@ around _validate_merge => sub {
                 map { $_->{artist_credit_id} } @{$recording_merge->{sources}},
             );
             if (uniq(@ac_ids) > 1) {
-                push @bad_recording_merges, (
+                push @bad_recording_merges, [
                     Recording->new($recording_merge->{destination}),
                     map { Recording->new($_) } @{$recording_merge->{sources}},
-                );
+                ];
             }
         }
         if (@bad_recording_merges) {

t/lib/t/MusicBrainz/Server/Data/ReleasePackaging.pm
@@ -29,8 +29,8 @@ ok(!exists $lts->{666});
 
 does_ok($lt_data, 'MusicBrainz::Server::Data::Role::SelectAll');
 my @types = $lt_data->get_all;
-is(@types, 3);
-is_deeply([sort { $a <=> $b } map { $_->id } @types], [1, 3, 5]);
+is(@types, 4);
+is_deeply([sort { $a <=> $b } map { $_->id } @types], [1, 3, 5, 7]);
 
 };
 

t/selenium.js
@@ -438,6 +438,7 @@ const seleniumTests = [
   {name: 'MBS-9669.html'},
   {name: 'MBS-9941.html', login: true},
   {name: 'MBS-10188.html', login: true, sql: 'mbs-10188.sql'},
+  {name: 'MBS-10510.html', login: true, sql: 'mbs-10510.sql'},
   {name: 'Artist_Credit_Editor.html', login: true},
   {name: 'External_Links_Editor.html', login: true},
   {name: 'Work_Editor.html', login: true},

t/selenium/MBS-10510.html
@@ -1,0 +1,47 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head profile="http://selenium-ide.openqa.org/profiles/test-case">
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<link rel="selenium.base" href="http://localhost:5000" />
+<title>MBS-10510</title>
+</head>
+<body>
+<table cellpadding="1" cellspacing="1" border="1">
+<thead>
+<tr><td rowspan="1" colspan="3">MBS-10510</td></tr>
+</thead><tbody>
+<tr>
+    <td>open</td>
+    <td>/release/546d165a-3fa9-423b-acb8-0fd6342a44f1</td>
+    <td></td>
+</tr>
+<tr>
+    <td>clickAndWait</td>
+    <td>xpath=//a[text()="Merge"]</td>
+    <td></td>
+</tr>
+<tr>
+    <td>open</td>
+    <td>/release/45c1c4d2-ee85-4872-8af4-acd1ae9c3c31</td>
+    <td></td>
+</tr>
+<tr>
+    <td>clickAndWait</td>
+    <td>xpath=//a[text()="Merge"]</td>
+    <td></td>
+</tr>
+<tr>
+    <td>select</td>
+    <td>id=id-merge.merge_strategy</td>
+    <td>label=Merge mediums and recordings</td>
+</tr>
+<tr>
+    <td>assertTextMatches</td>
+    <td>css=#merge-strategy-2:not([style*="display: none"]) div.warning</td>
+    <td>The recording artists do not match</td>
+</tr>
+</tbody>
+</table>
+</body>
+</html>

t/sql/initial.sql
@@ -916,6 +916,7 @@ INSERT INTO release_group_secondary_type VALUES (10, 'Demo', NULL, 0, NULL, '815
 INSERT INTO release_packaging VALUES (1, 'Jewel Case', NULL, 0, 'The traditional CD case, made of hard, brittle plastic.', 'ec27701a-4a22-37f4-bfac-6616e0f9750a');
 INSERT INTO release_packaging VALUES (3, 'Digipak', NULL, 0, NULL, '8f931351-d2e2-310f-afc6-37b89ddba246');
 INSERT INTO release_packaging VALUES (5, 'Other', NULL, 1, NULL, '815b7785-8284-3926-8f04-e48bc6c4d102');
+INSERT INTO release_packaging VALUES (7, 'None', NULL, 2, NULL, '119eba76-b343-3e02-a292-f0f00644bb9b');
 
 INSERT INTO release_status VALUES (1, 'Official', NULL, 1, 'Any release officially sanctioned by the artist and/or their record company. Most releases will fit into this category.', '4e304316-386d-3409-af2e-78857eec5cfe');
 INSERT INTO release_status VALUES (2, 'Promotion', NULL, 2, 'A give-away release or a release intended to promote an upcoming official release (e.g. pre-release versions, releases included with a magazine, versions supplied to radio DJs for air-play).', '518ffc83-5cde-34df-8627-81bff5093d92');

t/sql/mbs-10510.sql
@@ -1,0 +1,57 @@
+-- Automatically generated, do not edit.
+-- release 546d165a-3fa9-423b-acb8-0fd6342a44f1 45c1c4d2-ee85-4872-8af4-acd1ae9c3c31
+
+SET client_min_messages TO 'warning';
+
+-- Temporarily drop triggers.
+DROP TRIGGER deny_deprecated ON link;
+
+INSERT INTO musicbrainz.area (id, gid, name, type, edits_pending, last_updated, begin_date_year, begin_date_month, begin_date_day, end_date_year, end_date_month, end_date_day, ended, comment) VALUES
+	(269, 'af59135f-38b5-4ea4-b4e2-dd28c5f0bad7', 'Washington, D.C.', 2, 0, '2013-08-17 16:31:00.306126+00', NULL, NULL, NULL, NULL, NULL, NULL, '0', ''),
+	(7952, '759f9567-9107-40ef-a825-e57824a62e70', 'St. Louis', 3, 0, '2013-05-29 08:22:56.510164+00', NULL, NULL, NULL, NULL, NULL, NULL, '0', '');
+INSERT INTO musicbrainz.iso_3166_2 (area, code) VALUES
+	(269, 'US-DC');
+INSERT INTO musicbrainz.artist (id, gid, name, sort_name, begin_date_year, begin_date_month, begin_date_day, end_date_year, end_date_month, end_date_day, type, area, gender, comment, edits_pending, last_updated, ended, begin_area, end_area) VALUES
+	(288820, '6b82c839-ea15-4d3b-96f1-d1be924e9cb7', 'Hamilton Leithauser', 'Leithauser, Hamilton', NULL, NULL, NULL, NULL, NULL, NULL, 1, 222, 1, '', 0, '2015-07-15 23:29:08.652546+00', '0', 269, NULL),
+	(804408, '185527bf-c293-4c24-8213-ed98fb8976be', 'Angel Olsen', 'Olsen, Angel', 1987, 1, 22, NULL, NULL, NULL, 1, 222, 2, '', 0, '2016-09-04 19:37:51.849992+00', '0', 7952, NULL);
+INSERT INTO musicbrainz.artist_ipi (artist, ipi, edits_pending, created) VALUES
+	(804408, '00686537595', 0, '2014-11-05 03:00:13.359654+00');
+INSERT INTO musicbrainz.artist_isni (artist, isni, edits_pending, created) VALUES
+	(288820, '0000000406679798', 0, '2015-07-15 23:29:08.652546+00');
+INSERT INTO musicbrainz.artist_credit (id, name, artist_count, ref_count, created, edits_pending) VALUES
+	(2088588, 'Hamilton Leithauser featuring Angel Olsen', 2, 4, '2017-10-13 17:53:24.63341+00', 0),
+	(2091168, 'Hamilton Leithauser & Angel Olsen', 2, 4, '2017-10-17 22:10:19.31387+00', 0);
+INSERT INTO musicbrainz.artist_credit_name (artist_credit, position, artist, name, join_phrase) VALUES
+	(2088588, 0, 288820, 'Hamilton Leithauser', ' featuring '),
+	(2088588, 1, 804408, 'Angel Olsen', ''),
+	(2091168, 0, 288820, 'Hamilton Leithauser', ' & '),
+	(2091168, 1, 804408, 'Angel Olsen', '');
+INSERT INTO musicbrainz.release_group (id, gid, name, artist_credit, type, comment, edits_pending, last_updated) VALUES
+	(1869896, '0e633641-0910-4084-bbb5-6b3aaabf1e9d', 'Heartstruck (Wild Hunger)', 2088588, 2, '', 0, '2017-10-29 13:09:45.96357+00'),
+	(1871823, 'f919772e-425e-4060-b654-e0526c2baf7b', 'Heartstruck (Wild Hunger)', 2091168, 2, '', 0, '2017-10-29 13:09:45.96357+00');
+INSERT INTO musicbrainz.release (id, gid, name, artist_credit, release_group, status, packaging, language, script, barcode, comment, edits_pending, quality, last_updated) VALUES
+	(2047097, '45c1c4d2-ee85-4872-8af4-acd1ae9c3c31', 'Heartstruck (Wild Hunger)', 2088588, 1869896, 1, NULL, NULL, NULL, NULL, '', 0, -1, '2017-10-29 13:10:45.95418+00'),
+	(2049596, '546d165a-3fa9-423b-acb8-0fd6342a44f1', 'Heartstruck (Wild Hunger)', 2091168, 1871823, 1, 7, 120, 28, NULL, '', 0, -1, '2017-10-29 13:10:45.95418+00');
+INSERT INTO musicbrainz.release_unknown_country (release, date_year, date_month, date_day) VALUES
+	(2047097, 2017, NULL, NULL);
+INSERT INTO musicbrainz.release_country (release, country, date_year, date_month, date_day) VALUES
+	(2049596, 222, 2017, 10, 12);
+INSERT INTO musicbrainz.label (id, gid, name, begin_date_year, begin_date_month, begin_date_day, end_date_year, end_date_month, end_date_day, label_code, type, area, comment, edits_pending, last_updated, ended) VALUES
+	(19791, '58c69b6f-5c5b-4341-8be6-a43c3e69b408', 'Glassnote', 2007, NULL, NULL, NULL, NULL, NULL, NULL, 9, 222, 'logo is a capital G with an eighth note; imprint of Glassnote Entertainment Group LLC', 0, '2016-04-26 07:13:14.300186+00', '0');
+INSERT INTO musicbrainz.release_label (id, release, label, catalog_number, last_updated) VALUES
+	(1571316, 2049596, 19791, NULL, '2017-10-17 22:10:24.561209+00');
+INSERT INTO musicbrainz.medium (id, release, position, format, name, edits_pending, last_updated, track_count) VALUES
+	(2198755, 2047097, 1, 12, '', 0, '2017-10-13 17:53:30.633711+00', 1),
+	(2201664, 2049596, 1, 12, '', 0, '2017-10-17 22:10:26.919104+00', 1);
+INSERT INTO musicbrainz.medium_index (medium, toc) VALUES
+	(2198755, '(210000, 0, 0, 0, 0, 0)'),
+	(2201664, '(210651, 0, 0, 0, 0, 0)');
+INSERT INTO musicbrainz.recording (id, gid, name, artist_credit, length, comment, edits_pending, last_updated, video) VALUES
+	(21622705, 'fab8ace7-2f64-4990-889b-581b309298ab', 'Heartstruck (Wild Hunger)', 2088588, 210000, '', 0, '2017-10-13 17:53:30.633711+00', '0'),
+	(21643730, '13de1d98-e302-4922-9f25-4dbd776c5ebf', 'Heartstruck (Wild Hunger)', 2091168, 210651, '', 0, '2017-10-17 22:10:26.919104+00', '0');
+INSERT INTO musicbrainz.track (id, gid, recording, medium, position, number, name, artist_credit, length, edits_pending, last_updated, is_data_track) VALUES
+	(23961922, 'ba2dafe8-1f21-4d0c-a034-3a9257fa575f', 21622705, 2198755, 1, '1', 'Heartstruck (Wild Hunger)', 2088588, 210000, 0, '2017-10-13 17:53:30.633711+00', '0'),
+	(23990533, '9dc36e26-06cd-4a32-b158-34aeedb16b53', 21643730, 2201664, 1, '1', 'Heartstruck (Wild Hunger)', 2091168, 210651, 0, '2017-10-17 22:10:26.919104+00', '0');
+
+-- Restore triggers.
+CREATE TRIGGER deny_deprecated BEFORE UPDATE OR INSERT ON link FOR EACH ROW EXECUTE PROCEDURE deny_deprecated_links();