bb2389b739fbd72e92ab1534fc971ddaa39a8719
yvanzo <yvanzo@metabrainz.org>
2019-12-20 02:24:47 +0100

    MBS-10322: Port 'fab pot' to shell script
    Replace 'pot' Fabric task with new 'po/update_pot.sh' script and
    '--commit' option. It is a derivative of 'po/update_translations.sh'
    and has the same behavior:
    
    - Prevent incidentally committing pre-existing local changes
    - No more ask to confirm commit message before committing
    - Show commit statistics if any, print a notice otherwise
    - Test (remote) database connection before querying it
    
    Plus, by Michael: Set MB_POT_DB=PROD_STANDBY in update_pot.sh
    
    The intent of this script is to update pot files using the production
    database, so the developer is now required to have a PROD_STANDBY
    database configured in lib/DBDefs.pm which does that, via SSH tunnel or
    otherwise.

---
+++ po/update_pot.sh
@@ -0,0 +1,75 @@
+#!/usr/bin/env bash
+
+set -e -u
+
+SCRIPT_NAME=$(basename "$0")
+
+HELP=$(cat <<EOH
+Usage: $SCRIPT_NAME [option]"
+
+Update POT files from codebase and production database.
+
+Option:
+  --commit  Commit changes to Git, if any
+EOH
+)
+
+if [ $# -gt 1 ]
+then
+  echo >&2 "$SCRIPT_NAME: too many arguments"
+  echo >&2 "$HELP"
+  exit 64
+elif [ $# -eq 1 ]
+then
+  if [ "$1" != '--commit' ]
+  then
+    echo >&2 "$SCRIPT_NAME: unrecognized option: $1"
+    echo >&2 "$HELP"
+    exit 64
+  fi
+  DO_COMMIT='yes'
+else
+  DO_COMMIT='no'
+fi
+
+MB_SERVER_ROOT=$(cd "$(dirname "${BASH_SOURCE[0]}")/../" && pwd)
+
+cd "$MB_SERVER_ROOT"
+
+if [ $DO_COMMIT = 'yes' ] && \
+  ! (git diff --quiet && git diff --cached --quiet)
+then
+  sed -E 's/^ *> ?//' >&2 << ..EOM
+  > $SCRIPT_NAME: Git working tree has local changes already
+  >
+  > Your local changes would be incidentally committed with POT files.
+  > Please clean your Git working tree before updating POT files.
+..EOM
+  exit 70
+fi
+
+export MB_POT_DB=PROD_STANDBY
+
+if ! script/database_exists $MB_POT_DB
+then
+  exit 78
+fi
+
+cd po
+
+touch extract_pot_db extract_pot_templates
+make pot
+
+if [ $DO_COMMIT = 'yes' ]
+then
+  if git diff --quiet
+  then
+    echo "POT files already up-to-date, nothing to commit."
+  else
+    git add -- *.po
+    git commit -m 'Update POT files using the production database'
+    git show --stat
+  fi
+fi
+
+# vi: set et sts=2 sw=2 ts=2 :
