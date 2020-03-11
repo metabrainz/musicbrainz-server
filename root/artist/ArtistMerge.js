c4dd1b1e21b3a7ee31837fd27b4c0c010e829594
Nicolás Tamargo <reosarevok@gmail.com>
2020-03-11 21:39:32 +0200

    MBS-10693: Restore FieldErrors for target on artist merges (#1419)
    This got accidentally removed with 379959b

--- root/artist/ArtistMerge.js
+++ root/artist/ArtistMerge.js
@@ -12,6 +12,7 @@ import sortBy from 'lodash/sortBy';
 
 import EnterEdit from '../components/EnterEdit';
 import EnterEditNote from '../components/EnterEditNote';
+import FieldErrors from '../components/FieldErrors';
 import FormRowCheckbox from '../components/FormRowCheckbox';
 import ArtistList from '../components/list/ArtistList';
 import {withCatalystContext} from '../context';
@@ -37,6 +38,7 @@ const ArtistMerge = ({$c, form, toMerge}: Props) => (
           artists={sortBy(toMerge, 'name')}
           mergeForm={form}
         />
+        <FieldErrors field={form.field.target} />
 
         <FormRowCheckbox
           field={form.field.rename}
