d7240a3c9a9b4f7b5bead90c9ec058a47b6e6fd9
mihkel-t <47502080+mihkel-t@users.noreply.github.com>
2020-04-01 14:19:15 +0200

    MBS-5380: contextualize "Edit"
    A bit more generic context

--- root/admin/attributes/Language.js
+++ root/admin/attributes/Language.js
@@ -60,7 +60,7 @@ const Language = ({model, attributes}: Props) => {
               <td>{frequencyLabels[attr.frequency]()}</td>
               <td>
                 <a href={`/admin/attributes/${model}/edit/${attr.id}`}>
-                  {lp('Edit', 'button')}
+                  {lp('Edit', 'button/link')}
                 </a>
                 {' | '}
                 <a href={`/admin/attributes/${model}/delete/${attr.id}`}>
