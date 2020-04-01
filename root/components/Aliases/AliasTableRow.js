77a7d17992e53d5618ba1bba238923b61aef1b79
mihkel-t <47502080+mihkel-t@users.noreply.github.com>
2020-04-01 14:23:07 +0200

    MBS-5380: contextualize "Edit"
    A bit more generic context

--- root/components/Aliases/AliasTableRow.js
+++ root/components/Aliases/AliasTableRow.js
@@ -54,7 +54,7 @@ const AliasTableRow = ({alias, allowEditing, entity, row}: Props) => (
         ? (
           <>
             <a href={entityHref(entity, `/alias/${alias.id}/edit`)}>
-              {lp('Edit', 'button')}
+              {lp('Edit', 'button/link')}
             </a>
             {' | '}
             <a href={entityHref(entity, `/alias/${alias.id}/delete`)}>
