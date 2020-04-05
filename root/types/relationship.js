55de0e1080f8153c9b3db5b10817dc90c20e4b07
Michael Wiencek <mwtuea@gmail.com>
2020-04-05 18:52:30 -0500

    Use CoreEntityTypeT for type0/type1 on LinkTypeT

--- root/types/relationship.js
+++ root/types/relationship.js
@@ -51,8 +51,8 @@ declare type LinkTypeT = {
   +orderable_direction: number,
   +reverse_link_phrase: string,
   +root_id: number | null,
-  +type0: string,
-  +type1: string,
+  +type0: CoreEntityTypeT,
+  +type1: CoreEntityTypeT,
 };
 
 declare type PagedLinkTypeGroupT = {
