ad6e480135a6a528252333624719fa501d8935fe
Michael Wiencek <mwtuea@gmail.com>
2020-04-05 02:00:58 -0500

    compareDates / compareDatePeriods changes
     * compareDates previously returned 0 if both dates had a null year.
       This is changed so that it now compares by month (or day if both
       months are null) in that case. This behavior is needed by the new
       relationship editor, where relationship comparison functions should
       only return 0 if both values are exactly equal.
    
     * compareDatePeriods now compares begin years, then end years, then
       begin months, then end months, etc., instead of comparing begin dates
       as a whole before end dates as a whole. Coupled with the previous
       change, this means that (for example) date periods can continue to
       sort by end year if both begin years are null. This previously only
       happened because, as described above, compareDates returned 0 in this
       case.
    
     * Sorting null values first is now done consistently in all cases.

--- root/static/scripts/tests/utility.js
+++ root/static/scripts/tests/utility.js
@@ -10,6 +10,9 @@ import test from 'tape';
 
 import formatDate from '../common/utility/formatDate';
 import * as age from '../../../utility/age';
+import compareDates, {
+  compareDatePeriods,
+} from '../common/utility/compareDates';
 import formatDatePeriod from '../common/utility/formatDatePeriod';
 import formatTrackLength from '../common/utility/formatTrackLength';
 import parseDate from '../common/utility/parseDate';
@@ -88,6 +91,133 @@ test('age', function (t) {
     end_date: {year: 2012, month: 1, day: null},
     ended: true,
   }), [1, 0, 1], 'age with partial dates is 1 year, 1 day');
+});
+
+test('compareDates', function (t) {
+  t.plan(7);
+
+  t.ok(compareDates(null, null) === 0);
+  t.ok(compareDates({}, {}) === 0);
+  t.ok(compareDates(null, {}) === 0);
+  t.ok(compareDates({}, null) === 0);
+
+  const sortedDates = [
+    null,
+    {day: 1},
+    {day: 2},
+    {month: 1},
+    {month: 1, day: 1},
+    {month: 1, day: 2},
+    {month: 2},
+    {month: 2, day: 1},
+    {month: 2, day: 2},
+    {year: 0},
+    {year: 0, day: 1},
+    {year: 0, day: 2},
+    {year: 0, month: 1},
+    {year: 0, month: 1, day: 1},
+    {year: 0, month: 1, day: 2},
+    {year: 0, month: 2},
+    {year: 0, month: 2, day: 1},
+    {year: 0, month: 2, day: 2},
+    {year: 2000},
+    {year: 2000, day: 1},
+    {year: 2000, day: 2},
+    {year: 2000, month: 1},
+    {year: 2000, month: 1, day: 1},
+    {year: 2000, month: 1, day: 2},
+    {year: 2000, month: 2},
+    {year: 2000, month: 2, day: 1},
+    {year: 2000, month: 2, day: 2},
+  ];
+
+  let copy = sortedDates.slice(0)
+    .sort((a, b) => (a?.year ?? 0) - (b?.year ?? 0));
+  copy.sort(compareDates);
+  t.deepEqual(copy, sortedDates);
+
+  copy = sortedDates.slice(0)
+    .sort((a, b) => (a?.month ?? 0) - (b?.month ?? 0));
+  copy.sort(compareDates);
+  t.deepEqual(copy, sortedDates);
+
+  copy = sortedDates.slice(0)
+    .sort((a, b) => (a?.day ?? 0) - (b?.day ?? 0));
+  copy.sort(compareDates);
+  t.deepEqual(copy, sortedDates);
+});
+
+test('compareDatePeriods', function (t) {
+  t.plan(9);
+
+  t.ok(compareDatePeriods(
+    null,
+    {begin_date: {year: 0}, end_date: {year: 0}, ended: true},
+  ) < 0, 'null date periods sort before non-null ones');
+
+  t.ok(compareDatePeriods(
+    null,
+    {},
+  ) === 0, 'empty date period objects are considered null');
+
+  t.ok(compareDatePeriods(
+    {
+      begin_date: {month: 12, day: 1},
+      end_date: {month: 1, day: 1},
+      ended: true,
+    },
+    {
+      begin_date: {month: 1, day: 1},
+      end_date: {month: 1, day: 1},
+      ended: true,
+    },
+  ) > 0, 'date periods without years are sorted by month, day');
+
+  t.ok(compareDatePeriods(
+    {
+      begin_date: {month: 1, day: 1},
+      end_date: {month: 1, day: 1},
+      ended: true,
+    },
+    {
+      begin_date: {month: 1, day: 1},
+      end_date: {month: 12, day: 1},
+      ended: true,
+    },
+  ) < 0, 'date periods without years are sorted by month, day');
+
+  t.ok(compareDatePeriods(
+    {begin_date: {month: 12, day: 1}, end_date: {year: 1977}, ended: true},
+    {begin_date: {month: 1, day: 1}, end_date: {year: 2001}, ended: true},
+  ) < 0, 'date periods with no begin years are sorted by end years');
+
+  t.ok(compareDatePeriods(
+    {begin_date: {day: 12}, end_date: {month: 1}, ended: true},
+    {begin_date: {day: 1}, end_date: {month: 12}, ended: true},
+  ) < 0, 'date periods with no begin months are sorted by end months');
+
+  t.ok(compareDatePeriods(
+    {begin_date: null, end_date: {day: 12}, ended: true},
+    {begin_date: null, end_date: {day: 1}, ended: true},
+  ) > 0, 'date periods with only end days are sorted');
+
+  t.ok(compareDatePeriods(
+    {
+      begin_date: {month: 12, day: 1},
+      end_date: {year: 1977},
+      ended: true,
+    },
+    {
+      begin_date: {month: 1, day: 1},
+      end_date: {month: 1, day: 1},
+      ended: true,
+    },
+  ) > 0, 'date periods with null years are sorted before ones with years');
+
+  t.ok(compareDatePeriods(
+    {begin_date: null, end_date: null, ended: true},
+    {begin_date: null, end_date: null, ended: false},
+  ) < 0, 'ended date periods are sorted before non-ended ones');
 });
 
 test('hasAge', function (t) {
