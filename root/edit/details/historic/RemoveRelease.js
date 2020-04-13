407cef7808dc7bf8a23e122b4a09a7c6bbf12370
Nicolás Tamargo <reosarevok@gmail.com>
2020-04-13 14:13:49 +0300

    MBS-10762: Convert historic Remove Release edit to React
    Changes from the TT:
    
    Adds the "remove-release" class, for consistency.
    
    Doesn't show the "Artist:" label if artistCredit doesn't exist.
    
    Uses HistoricReleaseList for consistency.
    That means it uses DescriptiveLink to show the releases' ACs. This might
    seem redundant given the Artist field below, but that shows the artist
    at the time the edit was entered, and is also sometimes missing.

---
+++ root/edit/details/historic/RemoveRelease.js
@@ -0,0 +1,43 @@
+/*
+ * @flow
+ * Copyright (C) 2020 MetaBrainz Foundation
+ *
+ * This file is part of MusicBrainz, the open internet music database,
+ * and is licensed under the GPL version 2, or (at your option) any
+ * later version: http://www.gnu.org/licenses/gpl-2.0.txt
+ */
+
+import * as React from 'react';
+
+import HistoricReleaseList
+  from '../../components/HistoricReleaseList';
+import ArtistCreditLink
+  from '../../../static/scripts/common/components/ArtistCreditLink';
+
+type RemoveReleaseEditT = {
+  ...EditT,
+  +display_data: {
+    +artist_credit?: ArtistCreditT,
+    +name: string,
+    +releases: $ReadOnlyArray<ReleaseT>,
+  },
+};
+
+const RemoveRelease = ({edit}: {+edit: RemoveReleaseEditT}) => {
+  const artistCredit = edit.display_data.artist_credit;
+  return (
+    <table className="details remove-release">
+      <HistoricReleaseList releases={edit.display_data.releases} />
+      {artistCredit ? (
+        <tr>
+          <th>{l('Artist:')}</th>
+          <td>
+            <ArtistCreditLink artistCredit={artistCredit} />
+          </td>
+        </tr>
+      ) : null}
+    </table>
+  );
+};
+
+export default RemoveRelease;
