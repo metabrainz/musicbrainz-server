c9d723f98ded42518da2e2a036f6b4ba04ab168f
Nicolás Tamargo <reosarevok@gmail.com>
2020-04-17 22:50:14 +0300

    MBS-10773: Convert historic Change Release Quality edit to React
    Changes from the TT:
    
    Added the edit classes to be more consistent and improve the display of the data.
    
    Use DescriptiveLink to also display the artist credit for the release
    if available.

--- root/server/components.js
+++ root/server/components.js
@@ -291,6 +291,7 @@ module.exports = {
   'edit/details/MergeSeries': require('../edit/details/MergeSeries'),
   'edit/details/MergeWorks': require('../edit/details/MergeWorks'),
   'edit/details/historic/AddDiscId': require('../edit/details/historic/AddDiscId'),
+  'edit/details/historic/ChangeReleaseQuality': require('../edit/details/historic/ChangeReleaseQuality'),
   'edit/details/historic/MergeReleases': require('../edit/details/historic/MergeReleases'),
   'edit/details/historic/MoveDiscId': require('../edit/details/historic/MoveDiscId'),
   'edit/details/historic/RemoveDiscId': require('../edit/details/historic/RemoveDiscId'),
