a140d68d1476e3ca6d0e4706024d815a3acf671b
Nicolás Tamargo <reosarevok@gmail.com>
2020-04-29 08:30:32 +0300

    MBS-10799: Convert historic Move Release to RG edit to React
    Changes display_data to use the standard old/new format rather
    than passing separate old_group and new_group.

---
+++ root/edit/details/historic/MoveReleaseToReleaseGroup.js
@@ -0,0 +1,45 @@
+/*
+ * @flow
+ * Copyright (C) 2020 MetaBrainz Foundation
+ *
+ * This file is part of MusicBrainz, the open internet music database,
+ * and is licensed under the GPL version 2, or (at your option) any
+ * later version: http://www.gnu.org/licenses/gpl-2.0.txt
+ */
+
+import * as React from 'react';
+
+import DescriptiveLink
+  from '../../../static/scripts/common/components/DescriptiveLink';
+
+type MoveReleaseToReleaseGroupT = {
+  ...EditT,
+  +display_data: {
+    +release: ReleaseT,
+    +release_group: CompT<ReleaseGroupT>,
+  },
+};
+
+const MoveReleaseToReleaseGroup = ({
+  edit,
+}: {+edit: MoveReleaseToReleaseGroupT}) => (
+  <table className="details edit-release">
+    <tr>
+      <th>{l('Release:')}</th>
+      <td colSpan="2">
+        <DescriptiveLink entity={edit.display_data.release} />
+      </td>
+    </tr>
+    <tr>
+      <th>{l('Release group:')}</th>
+      <td className="old">
+        <DescriptiveLink entity={edit.display_data.release_group.old} />
+      </td>
+      <td className="new">
+        <DescriptiveLink entity={edit.display_data.release_group.new} />
+      </td>
+    </tr>
+  </table>
+);
+
+export default MoveReleaseToReleaseGroup;
