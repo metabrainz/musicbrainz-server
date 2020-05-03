5563a1f24d9c1812694144f5c9a571317493b1e0
Michael Wiencek <mwtuea@gmail.com>
2020-05-03 03:08:20 -0500

    MBS-10808: Fix schema in ll_to_earth
    This is required in order for pg_upgrade v12 to work. Should also
    hopefully prevent autovacuum from spamming our logs.

--- admin/sql/Extensions.sql
+++ admin/sql/Extensions.sql
@@ -5,6 +5,24 @@ CREATE EXTENSION IF NOT EXISTS cube WITH SCHEMA public;
 CREATE EXTENSION IF NOT EXISTS earthdistance WITH SCHEMA public;
 CREATE EXTENSION IF NOT EXISTS unaccent WITH SCHEMA public;
 
+-- Substitute public.ll_to_earth for a version that schema-qualifies
+-- references to public.cube and public.earth, to avoid errors during
+-- inlining/execution of the function while running an autovacuum or
+-- pg_upgrade.
+--
+-- The issue in question arises since postgres 10.3: a change was made to
+-- only include pg_catalog in the search_path settings of postgres client
+-- programs in order to resolve a security issue. So, since `cube` and
+-- `earth` are installed in the public schema, they're invisible in these
+-- contexts without the qualification.
+
+CREATE OR REPLACE FUNCTION musicbrainz.ll_to_earth(float8, float8)
+RETURNS public.earth
+LANGUAGE SQL
+IMMUTABLE STRICT
+PARALLEL SAFE
+AS 'SELECT public.cube(public.cube(public.cube(public.earth()*cos(radians($1))*cos(radians($2))),public.earth()*cos(radians($1))*sin(radians($2))),public.earth()*sin(radians($1)))::public.earth';
+
 -- The unaccent function, but IMMUTABLE. Based on a solution provided by
 -- Erwin Brandstetter in [1], which removes the dependency on search_path.
 -- Warning: changing the unaccent dictionary on the filesystem can still
