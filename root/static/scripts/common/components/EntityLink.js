5b8e4164d328365b90240c910303e563d8847036
loujine <loujine@protonmail.com>
2020-06-08 14:31:02 +0000

    MBS-8625: background image on span applied on all release links

--- root/static/scripts/common/components/EntityLink.js
+++ root/static/scripts/common/components/EntityLink.js
@@ -234,6 +234,26 @@ $ReadOnlyArray<Expand2ReactOutput> | Expand2ReactOutput | null => {
     );
   }
 
+  if (!subPath && entity.entityType === 'release') {
+    if (entity.quality == 2) {
+      // unicode large green circle U+1F7E2
+      content = (
+        <>
+          <span className="high-data-quality" title={l('High quality: All available data has been added, if possible including cover art with liner info that proves it')}>🟢</span>
+          {content}
+        </>
+      );
+    } else if (entity.quality == 0) {
+      // unicode large orange circle U+1F7E0
+      content = (
+        <>
+          <span className="low-data-quality" title={l("Low quality: The release needs serious fixes, or its existence is hard to prove (but it's not clearly fake)")}>🟠</span>
+          {content}
+        </>
+      );
+    }
+  }
+
   if (!showDisambiguation && !infoLink) {
     return content;
   }
