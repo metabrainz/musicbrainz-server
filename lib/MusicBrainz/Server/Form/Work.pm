a21fa61e3ed06d238c3619e815a03bd1797040b5
Nicolás Tamargo <reosarevok@gmail.com>
2020-06-08 22:23:42 +0300

    MBS-10381: Disallow using a work language multiple times
    While this seems to already be dropped silently on the DB level
    (since entering an Add work edit with the same language twice
    results on a work with just one language) it makes sense to not
    allow entering this in the first place, since it is meaningless and
    likely to be a user error.

--- lib/MusicBrainz/Server/Form/Work.pm
+++ lib/MusicBrainz/Server/Form/Work.pm
@@ -77,20 +77,30 @@ sub validate_languages {
 
     # If we have only one language, then it can be whatever
     # If we have two or more, we check it's not mul (248) nor zxx (486)
-    # because combining those with anything else doesn't make sense
+    # because combining those with anything else doesn't make sense,
+    # and we check the same language hasn't been selected multiple times
     if (scalar @languages > 1) {
+        my %used_languages;
         for my $language_field (@languages) {
-            if ($language_field->value == 284) {
+            my $language_id = $language_field->value;
+
+            if ($language_id == 284) {
                 $language_field->push_errors(
                     l('You cannot select “[Multiple languages]” and specific languages at the same time.')
                 );
                 $is_valid = 0;
-            } elsif ($language_field->value == 486) {
+            } elsif ($language_id == 486) {
                 $language_field->push_errors(
                     l('You cannot select “[No lyrics]” and a lyrics language at the same time.')
                 );
                 $is_valid = 0;
+            } elsif ($used_languages{$language_id}) {
+                $language_field->add_error(
+                    l('You cannot select the same language more than once.')
+                );
+                $is_valid = 0;
             }
+            $used_languages{$language_id} = 1;
         }
     }
 
