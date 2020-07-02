440204931c1a6a3a3f9a7edbac7aa3c4bb6d5d35
Michael Wiencek <mwtuea@gmail.com>
2020-07-01 20:30:33 -0500

    Cache the number of hits in query_to_list_limited
    Running the result count(*) of a paginated query for every page can be
    expensive, as it naturally has to first retrieve all results from the DB
    in order to run the count(*), then throw away any results outside of the
    LIMIT+OFFSET (though it's true that it has to do this for rows up until
    OFFSET anyway). This commit allows query_to_list_limited to optionally
    cache the number of hits for a fixed amount of time (currently 30
    minutes). This can make sense for lists that don't have their number of
    pages change often, like releases by an artist or label. Less so for
    lists of edits to be voted on.

--- lib/MusicBrainz/Server/Data/Role/QueryToList.pm
+++ lib/MusicBrainz/Server/Data/Role/QueryToList.pm
@@ -3,7 +3,11 @@ package MusicBrainz::Server::Data::Role::QueryToList;
 use strict;
 use warnings;
 
+use Digest::MD5 qw( md5 );
 use Moose::Role;
+use Readonly;
+
+Readonly my $HITS_CACHE_TIMEOUT => 60 * 30; # 30 minutes
 
 sub query_to_list {
     my ($self, $query, $args, $builder) = @_;
@@ -18,44 +22,71 @@ sub query_to_list {
 sub query_to_list_limited {
     my ($self, $query, $args, $limit, $offset, $builder, %opts) = @_;
 
-    $builder //= $self->can('_new_from_row');
-
     my @args = @$args;
-    my $count = @args;
+    my $arg_count = @args;
+
+    my $hits;
+    my $hits_cache_key;
+    if ($opts{cache_hits}) {
+        # Running count(*) over the entire query for every page is
+        # expensive, so we cache the number of hits for 30 minutes if
+        # the option is specified. The count doesn't have to be exact,
+        # just close enough that we can estimate the number of pages.
+        # In all likelihood, the number of pages isn't going to change
+        # that often for certain lists unless a large number of
+        # entities are added or removed. Other lists, like for voting
+        # on edits, are likely to change more frequently.
+        $hits_cache_key =
+            'query_to_list_limited:' .
+            md5(join "\0",
+                $query,
+                map { ref($_) eq 'ARRAY' ? (@$_) : $_ } @args);
+        $hits = $self->c->cache->get($hits_cache_key);
+    }
+
+    $builder //= $self->can('_new_from_row');
 
     if (defined $offset) {
         if ($opts{dollar_placeholders}) {
-            $query .= ' OFFSET $' . (++$count);
+            $query .= ' OFFSET $' . (++$arg_count);
         } else {
             $query .= ' OFFSET ?';
         }
         push @args, $offset;
     }
 
-    my $wrapping_query = qq{
-        WITH x AS ($query)
-        SELECT x.*, c.count AS total_row_count
-        FROM x, (SELECT count(*) from x) c
-    };
+    if (!defined $hits) {
+        $query = qq{
+            WITH x AS ($query)
+            SELECT x.*, c.count AS total_row_count
+            FROM x, (SELECT count(*) from x) c
+        };
+    }
 
     if (defined $limit) {
         if ($opts{dollar_placeholders}) {
-            $wrapping_query .= ' LIMIT $' . (++$count);
+            $query .= ' LIMIT $' . (++$arg_count);
         } else {
-            $wrapping_query .= ' LIMIT ?';
+            $query .= ' LIMIT ?';
         }
         push @args, $limit;
     }
 
-    my $hits = 0;
+    my $total_row_count;
     my @rows = map {
-        $hits = delete $_->{total_row_count};
-        $builder->($self, $_);
-    } @{$self->c->sql->select_list_of_hashes($wrapping_query, @args)};
+        my $row = $_;
+        $total_row_count = delete $row->{total_row_count};
+        $builder->($self, $row);
+    } @{$self->c->sql->select_list_of_hashes($query, @args)};
 
-    $hits += ($offset // 0);
+    if (!defined $hits) {
+        $hits = ($total_row_count // 0) + ($offset // 0);
+        if ($opts{cache_hits}) {
+            $self->c->cache->set($hits_cache_key, $hits, $HITS_CACHE_TIMEOUT);
+        }
+    }
 
-    (\@rows, $hits);
+    (\@rows, ($hits // 0));
 }
 
 no Moose::Role;
