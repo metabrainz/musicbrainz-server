8d155545796d3adfd6c866fe7006aaa529ad31c6
Nicolás Tamargo <reosarevok@gmail.com>
2020-08-02 23:33:10 +0300

    MBS-10975: Convert Remove ISWC edit to React (#1605)

--- root/server/components.js
+++ root/server/components.js
@@ -309,6 +309,7 @@ module.exports = {
   'edit/details/MergeReleaseGroups': require('../edit/details/MergeReleaseGroups'),
   'edit/details/MergeSeries': require('../edit/details/MergeSeries'),
   'edit/details/MergeWorks': require('../edit/details/MergeWorks'),
+  'edit/details/RemoveIswc': require('../edit/details/RemoveIswc'),
   'edit/details/RemoveRelationship': require('../edit/details/RemoveRelationship'),
   'edit/details/ReorderRelationships': require('../edit/details/ReorderRelationships'),
   'edit/details/historic/AddDiscId': require('../edit/details/historic/AddDiscId'),
