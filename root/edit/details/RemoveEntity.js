3c2a98813330d8e5885ed92fffffe93d0aa92fae
Nicolás Tamargo <reosarevok@gmail.com>
2020-08-03 21:49:20 +0300

    MBS-10980: Convert Remove Entity edit to React (#1606)

---
+++ root/edit/details/RemoveEntity.js
@@ -0,0 +1,41 @@
+/*
+ * @flow
+ * Copyright (C) 2020 MetaBrainz Foundation
+ *
+ * This file is part of MusicBrainz, the open internet music database,
+ * and is licensed under the GPL version 2, or (at your option) any
+ * later version: http://www.gnu.org/licenses/gpl-2.0.txt
+ */
+
+import * as React from 'react';
+
+import {ENTITY_NAMES} from '../../static/scripts/common/constants';
+import DescriptiveLink
+  from '../../static/scripts/common/components/DescriptiveLink';
+
+type RemoveEntityEditT = {
+  ...EditT,
+  +display_data: {
+    +entity: CoreEntityT,
+    +entity_type: CoreEntityTypeT,
+  },
+};
+
+type Props = {
+  +edit: RemoveEntityEditT,
+};
+
+const RemoveEntity = ({edit}: Props): React.Element<'table'> => {
+  const display = edit.display_data;
+
+  return (
+    <table className={`details remove-${display.entity_type}`}>
+      <tr>
+        <th>{addColonText(ENTITY_NAMES[display.entity_type]())}</th>
+        <td><DescriptiveLink entity={display.entity} /></td>
+      </tr>
+    </table>
+  );
+};
+
+export default RemoveEntity;
