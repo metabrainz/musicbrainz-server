73a5416c3181104b1d9da7462a77a7c65ec24a46
Nicolás Tamargo <reosarevok@gmail.com>
2020-08-10 16:55:03 +0300

    MBS-11028: Convert historic Edit Release Language edit to React
    Changes:
    
    Translate language/script names
    
    When new language/script is null, display '?' (like with old values)
    rather than nothing.
    
    When the release array is empty, show [removed] rather than nothing.

---
+++ root/edit/details/historic/EditReleaseLanguage.js
@@ -0,0 +1,82 @@
+/*
+ * @flow
+ * Copyright (C) 2020 MetaBrainz Foundation
+ *
+ * This file is part of MusicBrainz, the open internet music database,
+ * and is licensed under the GPL version 2, or (at your option) any
+ * later version: http://www.gnu.org/licenses/gpl-2.0.txt
+ */
+
+import * as React from 'react';
+
+import {HistoricReleaseListContent}
+  from '../../components/HistoricReleaseList';
+
+type EditReleaseLanguageEditT = {
+  ...EditT,
+  +display_data: {
+    +language: LanguageT | null,
+    +old: $ReadOnlyArray<{
+      +language: LanguageT | null,
+      +releases: $ReadOnlyArray<ReleaseT | null>,
+      +script: ScriptT | null,
+    }>,
+    +script: ScriptT | null,
+  },
+};
+
+type Props = {
+  +edit: EditReleaseLanguageEditT,
+};
+
+const EditReleaseLanguage = ({edit}: Props): React.Element<'table'> => (
+  <table className="details edit-release">
+    <tr>
+      <th>{l('Old:')}</th>
+      <td>
+        <table>
+          {edit.display_data.old.map((change, index) => (
+            <tr key={index}>
+              <td className="old">
+                {texp.l(
+                  'Language: {language}, script: {script}',
+                  {
+                    language: change.language
+                      ? l_languages(change.language.name)
+                      : '?',
+                    script: change.script
+                      ? l_scripts(change.script.name)
+                      : '?',
+                  },
+                )}
+              </td>
+              <td>
+                <HistoricReleaseListContent releases={change.releases} />
+              </td>
+            </tr>
+          ))}
+        </table>
+      </td>
+    </tr>
+
+    <tr>
+      <th>{l('New language:')}</th>
+      <td className="new">
+        {edit.display_data.language
+          ? l_languages(edit.display_data.language.name)
+          : '?'}
+      </td>
+    </tr>
+
+    <tr>
+      <th>{l('New script:')}</th>
+      <td className="new">
+        {edit.display_data.script
+          ? l_scripts(edit.display_data.script.name)
+          : '?'}
+      </td>
+    </tr>
+  </table>
+);
+
+export default EditReleaseLanguage;
