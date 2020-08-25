65090b8089f4f6b65717a0fc54c465aa3aeb3b83
Nicolás Tamargo <reosarevok@gmail.com>
2020-08-25 16:19:12 +0300

    MBS-11052: Convert Edit Relationship Attribute edit to React
    Changes:
    
    We don't show a description line at all anymore if the description
    is (unchangedly) empty.
    
    Parent is no longer diffed by name, since the edit might change
    the parent between two attributes of the same name. If the IDs
    differ, the names are shown. In effect this probably won't happen
    since attributes with the same name are rare to begin with, and the
    display might still be confusing if a diff is shown between the same
    names, but otherwise it wouldn't have shown a change happening at all.
    
    We indicate the fields that have been explicitly left untranslated
    with IntentionallyRawIcon.

---
+++ root/edit/details/EditRelationshipAttribute.js
@@ -0,0 +1,111 @@
+/*
+ * @flow strict-local
+ * Copyright (C) 2020 MetaBrainz Foundation
+ *
+ * This file is part of MusicBrainz, the open internet music database,
+ * and is licensed under the GPL version 2, or (at your option) any
+ * later version: http://www.gnu.org/licenses/gpl-2.0.txt
+ */
+
+import * as React from 'react';
+
+import IntentionallyRawIcon
+  from '../components/IntentionallyRawIcon';
+import expand2react from '../../static/scripts/common/i18n/expand2react';
+import localizeLinkAttributeTypeName
+  from '../../static/scripts/common/i18n/localizeLinkAttributeTypeName';
+import WordDiff from '../../static/scripts/edit/components/edit/WordDiff';
+
+type EditRelationshipAttributeEditT = {
+  ...EditT,
+  +display_data: {
+    +child_order?: CompT<number>,
+    +description: CompT<string | null>,
+    +name: CompT<string>,
+    +original_description: string | null,
+    +original_name: string,
+    +parent?: CompT<LinkAttrTypeT | null>,
+  },
+};
+
+type Props = {
+  +edit: EditRelationshipAttributeEditT,
+};
+
+const EditRelationshipAttribute = ({edit}: Props): React.Element<'table'> => {
+  const display = edit.display_data;
+  const childOrder = display.child_order;
+  const oldDescription = display.description.old ?? '';
+  const newDescription = display.description.new ?? '';
+  const descriptionChanges = newDescription !== oldDescription;
+  const name = display.name;
+  const parent = display.parent;
+  const newParentName = parent?.new
+    ? localizeLinkAttributeTypeName(parent.new)
+    : '';
+  const oldParentName = parent?.old
+    ? localizeLinkAttributeTypeName(parent.old)
+    : '';
+  const rawIconSection = (
+    <>
+      {' '}
+      <IntentionallyRawIcon />
+    </>
+  );
+
+  return (
+    <table className="details edit-relationship-attribute">
+      {name.new === name.old ? (
+        <tr>
+          <th>{addColonText(l('Name'))}</th>
+          <td colSpan="2">
+            {l_relationships(name.old)}
+          </td>
+        </tr>
+      ) : (
+        <WordDiff
+          extraNew={rawIconSection}
+          extraOld={rawIconSection}
+          label={addColonText(l('Name'))}
+          newText={name.new}
+          oldText={name.old}
+        />
+      )}
+
+      {descriptionChanges ? (
+        <WordDiff
+          extraNew={nonEmpty(newDescription) ? rawIconSection : null}
+          extraOld={nonEmpty(oldDescription) ? rawIconSection : null}
+          label={addColonText(l('Description'))}
+          newText={newDescription}
+          oldText={oldDescription}
+        />
+      ) : nonEmpty(oldDescription) ? (
+        <tr>
+          <th>{addColonText(l('Description'))}</th>
+          <td colSpan="2">
+            {expand2react(l_relationships(oldDescription))}
+          </td>
+        </tr>
+      ) : null}
+
+      {parent?.old?.id === parent?.new?.id ? null : (
+        <tr>
+          <th>{addColonText(l('Parent'))}</th>
+          <td className="old">{oldParentName}</td>
+          <td className="new">{newParentName}</td>
+        </tr>
+      )}
+
+      {childOrder ? (
+        <WordDiff
+          label={addColonText(l('Child order'))}
+          newText={childOrder.new.toString()}
+          oldText={childOrder.old.toString()}
+        />
+      ) : null}
+    </table>
+  );
+};
+
+export default EditRelationshipAttribute;
--- root/static/scripts/edit/components/edit/WordDiff.js
+++ root/static/scripts/edit/components/edit/WordDiff.js
@@ -12,11 +12,15 @@ import * as React from 'react';
 import Diff, {type DiffProps} from './Diff';
 
 const WordDiff = ({
+  extraNew,
+  extraOld,
   label,
   newText,
   oldText,
 }: DiffProps): React.Element<typeof Diff> => (
   <Diff
+    extraNew={extraNew}
+    extraOld={extraOld}
     label={label}
     newText={newText}
     oldText={oldText}
