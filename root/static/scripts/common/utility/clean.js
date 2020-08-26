df553e4ad0f9c09af4fae4314691c48e21bcb2ce
Nicolás Tamargo <reosarevok@gmail.com>
2020-08-26 21:51:31 +0300

    Add missing copyright header

--- root/static/scripts/common/utility/clean.js
+++ root/static/scripts/common/utility/clean.js
@@ -1,4 +1,11 @@
-// @flow strict
+/*
+ * @flow strict
+ * Copyright (C) 2015 MetaBrainz Foundation
+ *
+ * This file is part of MusicBrainz, the open internet music database,
+ * and is licensed under the GPL version 2, or (at your option) any
+ * later version: http://www.gnu.org/licenses/gpl-2.0.txt
+ */
 
 export default function clean(str: ?string): string {
   return String(str || '').trim().replace(/\s+/g, ' ');
