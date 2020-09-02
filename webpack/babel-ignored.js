45e22c518d0872584ea3c0bc301a6c59eeb05a12
Michael Wiencek <mwtuea@gmail.com>
2020-09-02 12:09:02 -0500

    Transpile Jed, mutate-cow for IE11
    node_modules/ is excluded from Babel processing, because it's very large
    and almost everything under it is already suitably transpiled before
    publishing.
    
    Jed (or our fork of it), however, uses Object.assign which isn't
    available in IE11. And mutate-cow uses boatloads of methods that'll
    cause IE to scream. So these are added as exceptions (and they may not
    be the only ones in the future).

--- webpack/babel-ignored.js
+++ webpack/babel-ignored.js
@@ -1,5 +1,10 @@
 module.exports = [
-  /node_modules/,
+  /*
+   * The modules in the negative-lookahead assertion (?!...) are exceptions
+   * to the node_modules exclusion. These most likely use language features
+   * that aren't supported in IE11.
+   */
+  /node_modules\/(?!jed|mutate-cow)/,
   /root\/static\/scripts\/tests\/typeInfo\.js/,
   /root\/static\/build\/jed-[A-z_-]+?\.source\.js$/,
 ];
