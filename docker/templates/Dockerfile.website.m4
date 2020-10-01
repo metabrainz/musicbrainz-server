metabrainz/musicbrainz-server

commit aed356a10c0551d26f585d03a30a95918231f9c9

Compile static resources in the docker images
This will reduce container startup cost and downtime when deploying a
new release. To copy the resources to StaticBrainz, we'll be setting a
job on Jenkins instead. This may address some hard-to-debug issue with
favicons 404ing after beta deployments.

docker/templates/Dockerfile.website.m4
@@ -2,13 +2,15 @@ m4_include(`server_base.m4')m4_dnl
 
 ARG DEBIAN_FRONTEND=noninteractive
 
-RUN apt_install(`lsof zopfli')
+RUN apt_install(`lsof')
+
+copy_common_mbs_files
+
+git_info
 
 install_javascript_and_templates(` --only=production')
 
 install_translations()
-
-copy_common_mbs_files
 
 COPY \
     docker/musicbrainz-website/consul-template-template-renderer.conf \
@@ -23,16 +25,11 @@ COPY \
     /etc/service/website/run
 RUN chmod 755 \
         /etc/service/template-renderer/run \
-        /etc/service/website/run && \
-    touch /etc/service/template-renderer/down
+        /etc/service/website/run
 
 COPY \
-    docker/musicbrainz-website/deploy_static_resources.sh \
-    docker/musicbrainz-website/start_musicbrainz_website.sh \
     docker/scripts/start_musicbrainz_server.sh \
     docker/scripts/start_template_renderer.sh \
     /usr/local/bin/
 
 copy_mb(`docker/templates/DBDefs.pm.ctmpl lib/')
-
-git_info

docker/musicbrainz-website/consul-template-website.conf
@@ -5,7 +5,7 @@ template {
 }
 
 exec {
-    command = "start_musicbrainz_website.sh"
+    command = "start_musicbrainz_server.sh"
     splay = "60s"
     reload_signal = "SIGHUP"
     kill_signal = "SIGTERM"

docker/musicbrainz-website/deploy_static_resources.sh
@@ -1,78 +1,0 @@
-#!/bin/bash
-
-MBS_HOME=/home/musicbrainz
-MBS_ROOT=$MBS_HOME/musicbrainz-server
-
-source "$MBS_ROOT/script/functions.sh"
-
-BUILD_DIR=$MBS_ROOT/root/static/build
-TMP_BUILD_DIR=$MBS_ROOT/root/static/tmp-build
-LOCK_FILE=/tmp/.static_resources.lock
-
-_compile_static_resources() {
-    mkdir -p $TMP_BUILD_DIR
-    chown musicbrainz:musicbrainz $TMP_BUILD_DIR
-
-    pushd "$MBS_ROOT" > /dev/null
-    HOME="$MBS_HOME" \
-    MBS_STATIC_BUILD_DIR="$TMP_BUILD_DIR" \
-    NODE_ENV=production \
-        eval 'chpst -u musicbrainz:musicbrainz carton exec -- ./script/compile_resources.sh client &'
-    trap_jobs
-    popd > /dev/null
-}
-
-_deploy_static_resources() {
-    mkdir -p $BUILD_DIR
-    chown musicbrainz:musicbrainz $BUILD_DIR
-
-    # Delete files older than 30 days. If they're still unchanged, they'll just
-    # have to be compressed again below.
-    find $BUILD_DIR -type f -mtime +30 -not -name 'rev-manifest.json' -delete
-
-    # -n will not clobber existing files, preserving their mtimes and allowing
-    # us to avoid recompressing files that haven't changed (zopfli is slow).
-    # -a will preserve ownership to the musicbrainz user.
-    cp -Rna $TMP_BUILD_DIR/* $BUILD_DIR/
-
-    # These files are not versioned with any hash, so must always be copied in
-    # case they changed.
-    cp -a $MBS_ROOT/root/{favicon.ico,robots.txt.*} $BUILD_DIR/
-
-    find $BUILD_DIR -type f -newermt '-10 seconds' -not -name '*.gz' | \
-        chpst -u musicbrainz:musicbrainz xargs zopfli -v &
-    trap_jobs
-
-    # copy resources into the staticbrainz data volume
-    pushd "$MBS_ROOT" > /dev/null
-    ./bin/rsync-staticbrainz-files \
-        rsync-staticbrainz-mb \
-        "$BUILD_DIR/" \
-        ./ \
-        '--exclude "server*" --recursive' \
-        &
-    trap_jobs
-    popd > /dev/null
-
-    # We can copy the new rev-manifest.json only after the files it references
-    # have been rsynced.
-    cp -a $TMP_BUILD_DIR/rev-manifest.json $BUILD_DIR/
-
-    rm -rf "$TMP_BUILD_DIR"
-}
-
-compile_static_resources() {
-    (flock -e 220; _compile_static_resources $@) 220>$LOCK_FILE
-}
-
-deploy_static_resources() {
-    if [ -z "$STATICBRAINZ_SERVERS" ]; then
-        return
-    fi
-    (flock -e 220; _deploy_static_resources) 220>$LOCK_FILE
-}
-
-trap "rm -rf '$LOCK_FILE' '$TMP_BUILD_DIR'" EXIT
-
-compile_static_resources
-deploy_static_resources

docker/musicbrainz-website/start_musicbrainz_website.sh
@@ -1,23 +1,0 @@
-#!/bin/bash
-
-set -e
-
-MBS_HOME=/home/musicbrainz
-MBS_ROOT=$MBS_HOME/musicbrainz-server
-
-source "$MBS_ROOT/script/functions.sh"
-
-cd $MBS_ROOT
-
-deploy_static_resources.sh &
-trap_jobs
-
-export NODE_ENV=production
-
-sudo -E -H -u musicbrainz \
-    carton exec -- ./script/compile_resources.sh server &
-trap_jobs
-
-sv start template-renderer
-
-exec start_musicbrainz_server.sh

docker/templates/macros.m4
@@ -41,6 +41,8 @@ copy_mb(``script/compile_resources.sh script/dbdefs_to_js.pl script/start_render
 copy_mb(``webpack.client.config.js webpack.server.config.js webpack.tests.config.js ./'')
 copy_mb(``webpack/ webpack/'')
 
+ENV NODE_ENV production
+RUN sudo_mb(``carton exec -- ./script/compile_resources.sh'')
 RUN chown_mb(``/tmp/ttc'')')
 
 m4_define(

script/compile_resources.sh
@@ -28,7 +28,12 @@ if [ -z "$GIT_SHA" ]; then
     export GIT_SHA=$(./script/git_info sha)
 fi
 
-./script/dbdefs_to_js.pl
+# lib/DBDefs.pm doesn't exist when building Docker images.
+# In production, dbdefs_to_js.pl is run by consul-template once
+# DBDefs.pm is rendered.
+if [ -f lib/DBDefs.pm ]; then
+    ./script/dbdefs_to_js.pl
+fi
 
 BUILD_CLIENT=0
 BUILD_SERVER=0

commit 178825b57006efb3061284468658e56843f63cc5

MBS-11140: Build Jed language bundles in prod
Language packs /probably/ need to be installed before
compile_resources.sh is run in order for translations to work. Just a
guess.

Broken in aed356a10c0551d26f585d03a30a95918231f9c9

docker/templates/Dockerfile.website.m4
@@ -8,9 +8,9 @@ copy_common_mbs_files
 
 git_info
 
-install_javascript_and_templates(` --only=production')
-
 install_translations()
+
+install_javascript_and_templates(` --only=production')
 
 COPY \
     docker/musicbrainz-website/consul-template-template-renderer.conf \