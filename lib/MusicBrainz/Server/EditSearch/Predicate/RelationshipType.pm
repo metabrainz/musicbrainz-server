16bad991cccf057a645df9b09cdc660c3733715c
Nicolás Tamargo <reosarevok@gmail.com>
2020-10-28 21:05:54 +0200

    MBS-11185: Add "is not" operator to link_type edit search (#1756)

--- lib/MusicBrainz/Server/EditSearch/Predicate/RelationshipType.pm
+++ lib/MusicBrainz/Server/EditSearch/Predicate/RelationshipType.pm
@@ -13,20 +13,23 @@ use MusicBrainz::Server::Constants qw(
 sub operator_cardinality_map {
     return (
         '=' => undef,
+        '!=' => undef,
     )
 }
 
 sub combine_with_query {
     my ($self, $query) = @_;
+
+    my $and_condition = $self->operator eq '!=' ? 'AND NOT' : 'AND';
     $query->add_where([
         "type IN ($EDIT_RELATIONSHIP_CREATE, $EDIT_RELATIONSHIP_EDIT, $EDIT_RELATIONSHIP_DELETE, $EDIT_RELATIONSHIPS_REORDER)
-         AND ? && array_remove(ARRAY[
+         $and_condition (? && array_remove(ARRAY[
                                    (data#>>'{link_type,id}')::int,
                                    (data#>>'{link,link_type,id}')::int,
                                    (data#>>'{old,link_type,id}')::int,
                                    (data#>>'{new,link_type,id}')::int,
                                    (data#>>'{relationship,link,type,id}')::int
-                               ], NULL)",
+                               ], NULL))",
         [ $self->sql_arguments ],
     ]);
 }
