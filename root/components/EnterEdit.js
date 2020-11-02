efa7ae0c6ead2c628ee57dc8c56bf7e58d48d8ee
Nicolás Tamargo <reosarevok@gmail.com>
2020-11-02 20:43:45 +0200

    Preparing EnterEdit for AliasEditForm
    Make children optional (not everything has a Cancel button!), and allow
    disabling the button.

--- root/components/EnterEdit.js
+++ root/components/EnterEdit.js
@@ -11,13 +11,18 @@ import * as React from 'react';
 
 type Props = {
   +children?: React.Node,
+  +disabled?: boolean,
   +form: ReadOnlyFormT<{
     +make_votable: ReadOnlyFieldT<boolean>,
     ...
   }>,
 };
 
-const EnterEdit = ({children, form}: Props): React.MixedElement => (
+const EnterEdit = ({
+  children,
+  disabled = false,
+  form,
+}: Props): React.MixedElement => (
   <>
     <div className="row no-label">
       <div className="auto-editor">
@@ -25,6 +30,7 @@ const EnterEdit = ({children, form}: Props): React.MixedElement => (
           <input
             className="make-votable"
             defaultChecked={form.field.make_votable.value}
+            disabled={disabled}
             name={form.field.make_votable.html_name}
             type="checkbox"
             value="1"
@@ -34,7 +40,11 @@ const EnterEdit = ({children, form}: Props): React.MixedElement => (
       </div>
     </div>
     <div className="row no-label buttons">
-      <button className="submit positive" type="submit">
+      <button
+        className="submit positive"
+        disabled={disabled}
+        type="submit"
+      >
         {l('Enter edit')}
       </button>
       {children}
