metabrainz/musicbrainz-server

commit 9e2e988960a9f2c4df3b1b2769c40d3d527795b8

mbs-6212: add edit profile link to profile page (and other related changes)

lib/MusicBrainz/Server/Controller/Admin.pm
@@ -21,6 +21,8 @@ sub edit_user : Path('/admin/user/edit') Args(1) RequireAuth HiddenOnSlaves
         unless $c->user->is_account_admin or DBDefs->DB_STAGING_TESTING_FEATURES;
 
     my $user = $c->model('Editor')->get_by_name($user_name);
+    $c->stash->{viewing_own_profile} = $c->user_exists && $c->user->id == $user->id;
+
     my $form = $c->form(
         form => 'User::AdjustFlags',
         item => {
@@ -92,6 +94,8 @@ sub delete_user : Path('/admin/user/delete') Args(1) RequireAuth HiddenOnSlaves 
     my ($self, $c, $name) = @_;
 
     my $editor = $c->model('Editor')->get_by_name($name);
+    $c->stash->{viewing_own_profile} = $c->user_exists && $c->user->id == $editor->id;
+
     $c->detach('/error_404') if !$editor || $editor->deleted;
 
     my $id = $editor->id;

root/account/change_password.tt
@@ -1,6 +1,6 @@
-[% WRAPPER "layout.tt" title=l("Change Password") full_width=1 %]
+[% WRAPPER "user/profile/layout.tt" page="change_password" title=l("Change Password") %]
 
-    <h1>[% l("Change Password") %]</h1>
+    <h2>[% l("Change Password") %]</h2>
 
     [%- IF mandatory -%]
     <p>[% l('Please change your password. Unfortunately we\'ve discovered that

root/account/edit.tt
@@ -1,7 +1,7 @@
-[% WRAPPER "layout.tt" title=l("Edit Profile") full_width=1 %]
+[% WRAPPER "user/profile/layout.tt" page="edit_profile" title=l("Edit Profile") %]
     [% script_manifest('edit.js') %]
 
-    <h1>[% l("Edit Profile") %]</h1>
+    <h2>[% l("Edit Profile") %]</h2>
 
     <p>
         [%- l('See also your {uri|user preferences}, which include your privacy settings.', {uri => c.uri_for_action('/account/preferences')}) %]
@@ -106,10 +106,6 @@
         </span>
       </div>
     </form>
-
-    <h2>
-        [%- l('{uri|Delete my account}',  {uri => c.uri_for_action("/admin/delete_user", user.name) })%]
-    </h2>
 
     <script type="text/javascript">//<![CDATA[
       MB.Control.EntityAutocomplete({ inputs: $("span.area.autocomplete") });

root/account/preferences.tt
@@ -1,7 +1,5 @@
 [% USE dt = Class('DateTime') %]
-[% WRAPPER "layout.tt" title=l("Preferences") full_width=1 %]
-
-<h1>[% l('Preferences') %]</h1>
+[% WRAPPER "user/profile/layout.tt" page="preferences" title=l("Preferences") %]
 
 <form action="[% c.req.uri %]" method="post" style="margin-top:1em">
 

root/admin/delete_user.tt
@@ -1,5 +1,7 @@
+[% WRAPPER "user/profile/layout.tt" title=l('Delete User') page='delete' %]
+
+ <h2>[% l('Delete Account') %]</h2>
 [% IF c.user.is_account_admin && c.user.id != user.id %]
-    [% WRAPPER "user/profile/layout.tt" title=l('Delete User') page='delete' %]
     <h2>[% l('Delete Account') %]</h2>
     <p>[% l('Are you sure you want to delete all information about {e}? This cannot be undone!',
         { e => link_editor(user) }) %]</p>
@@ -8,11 +10,7 @@
         <button class="negative" type="submit">[% l('Delete {e}', { e => user.name } ) %]</button>
       </span>
     </form>
-    [% END %]
 [% ELSE %]
-    [% WRAPPER 'layout.tt' title=l('Delete Account') full_width=1 %]
-    <h1>[% l('Delete Account') %]</h1>
-
     <p>[% l('For information about the account deletion process, please read the {uri|account FAQ}.',
            { uri => 'http://wiki.musicbrainz.org/Account_FAQ#How_do_I_delete_my_account.3F' }) %]
     </p>
@@ -23,5 +21,6 @@
         <button class="negative" type="submit">[% l('Delete my account') %]</button>
       </span>
     </form>
-    [% END %]
+[% END %]
+
 [% END %]

root/user/profile/layout.tt
@@ -17,19 +17,20 @@
 [%- info_links.push(
     ['tags', simple_link(c.uri_for_action("/user/tags", [ user.name ]), l('Tags'))],
     ['ratings', simple_link(c.uri_for_action("/user/ratings", [ user.name ]), l('Ratings'))],
-    ['open_edits', simple_link(c.uri_for_action("/user/edits/open", [ user.name ]), l('Open Edits'))],
-    ['all_edits', simple_link(c.uri_for_action("/user/edits/all", [ user.name ]), l('All Edits'))],
 ) -%]
+
+[%- IF viewing_own_profile -%]
+[%- info_links.push (
+    ['edit_profile', simple_link(c.uri_for_action("/account/edit"), l('Edit Profile'))],
+    ['preferences', simple_link(c.uri_for_action("/account/preferences"), l('Preferences'))],
+    ['change_password', simple_link(c.uri_for_action("/account/change_password"), l('Change Password'))],
+    ['donation', simple_link(c.uri_for_action("/account/donation"), l('Donation Check'))],
+) -%]
+[%- END -%]
 
 [%- IF c.user.is_account_admin || (server_details.testing_features && c.user_exists)-%]
 [%- info_links.push(
     ['edit_user', simple_link(c.uri_for_action("/admin/edit_user", user.name), l('Edit User'))],
-) -%]
-[%- END -%]
-
-[%- IF viewing_own_profile -%]
-[%- info_links.push(
-    ['donation', simple_link(c.uri_for_action("/account/donation"), l('Donation Check'))],
 ) -%]
 [%- END -%]
 

commit 8bf3ea6373f8858ec28f2c8e0e4b5be074318519

Move broken viewing_own_profile check
Re-fixes ecbcf763871acbd36b84151b40f69468107fc6c9, which was broken in
9e2e988960a9f2c4df3b1b2769c40d3d527795b8 by the possibly-undef
`$editor->id` invocation.

Also detach to the more specific /user/not_found rather than /error_404.

lib/MusicBrainz/Server/Controller/Admin.pm
@@ -90,9 +90,9 @@ sub delete_user : Path('/admin/user/delete') Args(1) RequireAuth HiddenOnSlaves 
     my ($self, $c, $name) = @_;
 
     my $editor = $c->model('Editor')->get_by_name($name);
-    $c->stash->{viewing_own_profile} = $c->user_exists && $c->user->id == $editor->id;
+    $c->detach('/user/not_found') if !$editor || $editor->deleted;
 
-    $c->detach('/error_404') if !$editor || $editor->deleted;
+    $c->stash->{viewing_own_profile} = $c->user_exists && $c->user->id == $editor->id;
 
     my $id = $editor->id;
     if ($id != $c->user->id && !$c->user->is_account_admin) {