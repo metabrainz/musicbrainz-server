e86497f01d40a5aaf209e5be20d0116c811694b5
Michael Wiencek <mwtuea@gmail.com>
2020-11-05 14:48:42 -0600

    Merge global DBDefs/$c namespaces
    This is simpler and makes it easier to add other global data without
    resorting to more GLOBAL_* namespaces.

--- root/layout/components/globalsScript.js
+++ root/layout/components/globalsScript.js
@@ -15,49 +15,60 @@ import escapeClosingTags from '../../utility/escapeClosingTags';
 import sanitizedContext from '../../utility/sanitizedContext';
 
 /*
- * In production, lib/DBDefs.pm is rendered by consul-template, and so
- * may be updated if we change a DBDefs value in consul. In order for
- * client scripts to pick up on these changes without having to
- * recompile/bundle all of our JavaScript, we define the DBDefs
- * configuration values as a <script> on the page external from any JS
- * bundle.
+ * window.__MB__ (GLOBAL_JS_NAMESPACE) is defined as a non-configurable,
+ * non-enumerable global namespace under which we store global data to
+ * avoid conflicts with other scripts (libraries, plugins, userscripts)
+ * that may pollute the global namespace. Currently, it stores the
+ * following keys:
  *
- * (CLIENT_DBDEFS_CODE is a const for the lifetime of the module;
- * modules, including this one, are reloaded when root/server.js
- * receives a SIGHUP from consul-template.)
+ * "DBDefs"
+ *   Configuration values used by client code.
  *
- * __MB_DBDefs__ (GLOBAL_DBDEFS_NAMESPACE) is used for the global var
- * name to avoid conflicts with other scripts (libraries, plugins,
- * userscripts) that pollute the global namespace.
+ *   In production, lib/DBDefs.pm is rendered by consul-template, and so may
+ *   be updated if we change a DBDefs value in consul. In order for client
+ *   scripts to pick up on these changes without having to recompile/bundle
+ *   all of our JavaScript, we define the DBDefs configuration values in a
+ *   <script> on the page external from any JS bundle.
  *
- * We have another, unrelated global named __MB_Catalyst_Context__
- * which stores a sanitized version of $c for React-hydrated client
- * scripts to use. (See root/utility/hydrate.js.) The <script> exported
- * by this file defines both globals.
+ *   (CLIENT_DBDEFS_CODE below is a const for the lifetime of the module;
+ *   modules, including this one, are reloaded when root/server.js receives
+ *   a SIGHUP from consul-template.)
+ *
+ * "$c"
+ *   A sanitized version of $c (Catalyst context) from the server, for React-
+ *   hydrated client scripts to use. (See root/utility/hydrate.js.)
  */
 
-const CLIENT_DBDEFS_CODE =
-  'Object.defineProperty(window,' +
-  JSON.stringify(GLOBAL_DBDEFS_NAMESPACE) +
-  ',{value:Object.freeze(' +
-  escapeClosingTags(JSON.stringify(DBDefs)) +
-  ')})';
+const renderValue = (value: mixed): string => {
+  let result = escapeClosingTags(JSON.stringify(value) ?? '');
+  if (typeof value === 'object' && value != null) {
+    result = 'Object.freeze(' + result + ')';
+  }
+  return result;
+};
+
+export const renderKeyAndValue = (
+  key: string,
+  value: mixed,
+): string => (
+  JSON.stringify(key) + ':' + renderValue(value)
+);
+
+const CLIENT_DBDEFS_CODE = renderKeyAndValue('DBDefs', DBDefs);
 
 export default ((
   <CatalystContext.Consumer>
     {$c => {
-      const CLIENT_CATALYST_CONTEXT_CODE =
+      const GLOBAL_JS_CODE =
         'Object.defineProperty(window,' +
-        JSON.stringify(GLOBAL_CATALYST_CONTEXT_NAMESPACE) +
-        ',{value:Object.freeze(' +
-        escapeClosingTags(JSON.stringify(sanitizedContext($c))) +
-        ')})';
+        JSON.stringify(GLOBAL_JS_NAMESPACE) +
+        ',{value:Object.freeze({' +
+        CLIENT_DBDEFS_CODE + ',' +
+        renderKeyAndValue('$c', sanitizedContext($c)) +
+        '})})';
       return (
         <script
-          dangerouslySetInnerHTML={{
-            __html: CLIENT_DBDEFS_CODE + ';' +
-              CLIENT_CATALYST_CONTEXT_CODE,
-          }}
+          dangerouslySetInnerHTML={{__html: GLOBAL_JS_CODE}}
           nonce={$c.stash.globals_script_nonce}
         />
       );
