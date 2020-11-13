d3d0d8bb1bccd8426172c4a62b2ee6002e2df7e7
Nicolás Tamargo <reosarevok@gmail.com>
2020-11-13 09:51:48 +0200

    MBS-11222: Add a report for releases with media without a tracklist

---
+++ root/report/ReleasesWithEmptyMediums.js
@@ -0,0 +1,40 @@
+/*
+ * @flow strict-local
+ * Copyright (C) 2020 MetaBrainz Foundation
+ *
+ * This file is part of MusicBrainz, the open internet music database,
+ * and is licensed under the GPL version 2, or (at your option) any
+ * later version: http://www.gnu.org/licenses/gpl-2.0.txt
+ */
+
+import * as React from 'react';
+
+import ReleaseList from './components/ReleaseList';
+import ReportLayout from './components/ReportLayout';
+import type {ReportDataT, ReportReleaseT} from './types';
+
+const ReleasesWithEmptyMediums = ({
+  canBeFiltered,
+  filtered,
+  generated,
+  items,
+  pager,
+}: ReportDataT<ReportReleaseT>): React.Element<typeof ReportLayout> => (
+  <ReportLayout
+    canBeFiltered={canBeFiltered}
+    description={l(
+      `This report shows releases with at least one medium
+       that is missing a tracklist. If you can find the tracklist,
+       please help complete the data!`,
+    )}
+    entityType="release"
+    filtered={filtered}
+    generated={generated}
+    title={l('Releases with empty mediums')}
+    totalEntries={pager.total_entries}
+  >
+    <ReleaseList items={items} pager={pager} />
+  </ReportLayout>
+);
+
+export default ReleasesWithEmptyMediums;
