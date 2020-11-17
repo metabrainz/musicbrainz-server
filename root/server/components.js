efa370c4934dff4cef013a54ec06f69f93295b57
Nicolás Tamargo <reosarevok@gmail.com>
2020-11-17 15:52:15 +0200

    MBS-11229: Convert Edit Cover Art edit to React
    There's no such thing as position in the display data at all
    on the Perl side, so removing that from the template too
    since it will never be there.
    Also, we might as well pass the types and get the translated names
    on the JS side rather than just using l_name on Perl,
    which we don't do elsewhere either (admittedly, elsewhere
    we pass the English titles only, but I feel this is better).

--- root/server/components.js
+++ root/server/components.js
@@ -311,6 +311,7 @@ module.exports = {
   'edit/details/EditArea': require('../edit/details/EditArea'),
   'edit/details/EditArtist': require('../edit/details/EditArtist'),
   'edit/details/EditArtistCredit': require('../edit/details/EditArtistCredit'),
+  'edit/details/EditCoverArt': require('../edit/details/EditCoverArt'),
   'edit/details/EditEvent': require('../edit/details/EditEvent'),
   'edit/details/EditLabel': require('../edit/details/EditLabel'),
   'edit/details/EditPlace': require('../edit/details/EditPlace'),
