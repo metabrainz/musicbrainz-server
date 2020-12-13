e70f2472378152d50d547d11dfd25f65966a4775
Michael Wiencek <mwtuea@gmail.com>
2020-12-13 13:29:44 -0600

    Add `Data::Role::ArtType`

--- lib/MusicBrainz/Server/Data/CoverArtType.pm
+++ lib/MusicBrainz/Server/Data/CoverArtType.pm
@@ -2,86 +2,19 @@ package MusicBrainz::Server::Data::CoverArtType;
 
 use Moose;
 use namespace::autoclean;
-use MusicBrainz::Server::Entity::CoverArtType;
-use MusicBrainz::Server::Data::Utils qw(
-    load_subobjects object_to_ids placeholders );
 
 extends 'MusicBrainz::Server::Data::Entity';
-with 'MusicBrainz::Server::Data::Role::EntityCache',
-     'MusicBrainz::Server::Data::Role::OptionsTree',
-     'MusicBrainz::Server::Data::Role::Attribute';
+with 'MusicBrainz::Server::Data::Role::ArtType';
 
 sub _type { 'cover_art_type' }
 
-sub _table
-{
-    return 'cover_art_archive.art_type';
-}
+sub art_schema { 'cover_art_archive' }
 
-sub _columns
-{
-    return 'art_type.id, art_type.gid, art_type.name, art_type.parent AS parent_id,
-            art_type.child_order, art_type.description';
-}
+sub art_type_table { 'cover_art_archive.cover_art_type' }
 
 sub _entity_class
 {
     return 'MusicBrainz::Server::Entity::CoverArtType';
-}
-
-sub load
-{
-    my ($self, @objs) = @_;
-    load_subobjects($self, 'type', @objs);
-}
-
-sub get_by_name
-{
-    my ($self, @names) = @_;
-
-    my %types_by_name = map { $_->name => $_ } $self->get_all();
-
-    return map { $types_by_name{$_} } @names;
-}
-
-sub find_by_cover_art_ids
-{
-    my ($self, @ids) = @_;
-    return () unless @ids;
-
-    my $query = 'SELECT cover_art_type.id AS cover_art_id, ' . $self->_columns . ' ' .
-        'FROM ' . $self->_table . ' ' .
-        'JOIN cover_art_archive.cover_art_type ' .
-        'ON cover_art_type.type_id = art_type.id ' .
-        'WHERE cover_art_type.id IN (' . placeholders(@ids) . ')';
-
-    my %map;
-    for my $row (@{ $self->sql->select_list_of_hashes($query, @ids) }) {
-        $map{ $row->{cover_art_id} } ||= [];
-        push @{ $map{ $row->{cover_art_id} } }, $self->_new_from_row($row);
-    }
-
-    return %map;
-}
-
-sub load_for
-{
-    my ($self, @objects) = @_;
-    my %obj_id_map = object_to_ids(@objects);
-
-    my %id_types_map = $self->find_by_cover_art_ids(keys %obj_id_map);
-
-    while (my ($cover_art_id, $types) = each %id_types_map)
-    {
-        $obj_id_map{$cover_art_id}[0]->types($types);
-    }
-}
-
-sub in_use {
-    my ($self, $id) = @_;
-    return $self->sql->select_single_value(
-        'SELECT 1 FROM cover_art_archive.cover_art_type WHERE type_id = ? LIMIT 1',
-        $id);
 }
 
 __PACKAGE__->meta->make_immutable;
---
+++ lib/MusicBrainz/Server/Data/Role/ArtType.pm
@@ -0,0 +1,115 @@
+package MusicBrainz::Server::Data::Role::ArtType;
+
+use Moose::Role;
+use namespace::autoclean;
+use MusicBrainz::Server::Entity::CoverArtType;
+use MusicBrainz::Server::Data::Utils qw(
+    load_subobjects
+    object_to_ids
+);
+
+with 'MusicBrainz::Server::Data::Role::EntityCache';
+with 'MusicBrainz::Server::Data::Role::SelectAll';
+with 'MusicBrainz::Server::Data::Role::OptionsTree';
+with 'MusicBrainz::Server::Data::Role::Attribute';
+
+requires qw(
+  _type
+  _entity_class
+  art_schema
+  art_type_table
+);
+
+sub _table {
+    my $schema = shift->art_schema;
+    return "$schema.art_type";
+}
+
+our @column_names = (
+  'id',
+  'gid',
+  'name',
+  'parent AS parent_id',
+  'child_order',
+  'description',
+);
+
+sub _columns {
+    my $schema = shift->art_schema;
+    return join q(, ), map { "$schema.art_type.$_" } @column_names;
+}
+
+sub load {
+    my ($self, @objs) = @_;
+    load_subobjects($self, 'type', @objs);
+}
+
+sub get_by_name {
+    my ($self, @names) = @_;
+
+    my %types_by_name = map { $_->name => $_ } $self->get_all();
+
+    return map { $types_by_name{$_} } @names;
+}
+
+sub find_by_art_ids {
+    my ($self, @ids) = @_;
+
+    return {} unless @ids;
+
+    my $table = $self->_table;
+    my $type_table = $self->art_type_table;
+    my $columns = $self->_columns;
+
+    my $query = <<~"SQL";
+        SELECT $type_table.id AS art_id, $columns
+          FROM $table
+          JOIN $type_table ON $type_table.type_id = art_type.id
+         WHERE $type_table.id = any(?)
+        SQL
+
+    my %map;
+    for my $row (@{ $self->sql->select_list_of_hashes($query, \@ids) }) {
+        $map{ $row->{art_id} } //= [];
+        push @{ $map{ $row->{art_id} } }, $self->_new_from_row($row);
+    }
+
+    return \%map;
+}
+
+sub load_for {
+    my ($self, @objects) = @_;
+    my %obj_id_map = object_to_ids(@objects);
+
+    my $id_types_map = $self->find_by_art_ids(keys %obj_id_map);
+
+    while (my ($art_id, $types) = each %$id_types_map) {
+        $obj_id_map{$art_id}[0]->types($types);
+    }
+}
+
+sub in_use {
+    my ($self, $id) = @_;
+
+    my $schema = $self->art_schema;
+    my $type_table = $self->art_type_table;
+
+    return $self->sql->select_single_value(
+        "SELECT 1 FROM $schema.$type_table WHERE type_id = ? LIMIT 1",
+        $id,
+    );
+}
+
+no Moose::Role;
+
+1;
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2012 MetaBrainz Foundation
+
+This file is part of MusicBrainz, the open internet music database,
+and is licensed under the GPL version 2, or (at your option) any
+later version: http://www.gnu.org/licenses/gpl-2.0.txt
+
+=cut
