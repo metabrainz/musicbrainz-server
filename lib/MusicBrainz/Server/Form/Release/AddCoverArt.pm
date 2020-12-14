829f9e96614c42ce2e62c035d09513f176b6ceeb
Michael Wiencek <mwtuea@gmail.com>
2020-12-14 10:43:47 -0600

    Add `Form::Role::AddArt`

--- lib/MusicBrainz/Server/Form/Release/AddCoverArt.pm
+++ lib/MusicBrainz/Server/Form/Release/AddCoverArt.pm
@@ -3,64 +3,13 @@ use strict;
 use warnings;
 
 use HTML::FormHandler::Moose;
-use MusicBrainz::Server::Translation qw( l );
 
 extends 'MusicBrainz::Server::Form::ReleaseCoverArt';
-with 'MusicBrainz::Server::Form::Role::Edit';
+with 'MusicBrainz::Server::Form::Role::AddArt';
 
 has '+name' => ( default => 'add-cover-art' );
 
-sub edit_field_names { qw( id ) }
-
-has_field 'id' => (
-    type      => '+MusicBrainz::Server::Form::Field::Integer',
-    required  => 1,
-);
-
-has_field 'nonce' => (
-    type => '+MusicBrainz::Server::Form::Field::Text',
-    required  => 1,
-);
-
-has_field 'position' => (
-    type      => '+MusicBrainz::Server::Form::Field::Integer',
-    required  => 1,
-    default => 1,
-);
-
-has_field 'mime_type' => (
-    type      => 'Select',
-    required  => 1,
-);
-
-sub options_mime_type {
-    my @types = map {
-        {
-            'value' => $_->{mime_type},
-            'label' => $_->{suffix},
-        }
-    } @{ shift->ctx->model('CoverArtArchive')->mime_types };
-
-    return \@types;
-}
-
-sub validate_nonce {
-    my ($self, $field) = @_;
-
-    my $nonce = $field->value;
-    my $cover_art_id = $self->field('id')->value;
-
-    my $store = $self->ctx->model('MB')->context->store;
-    my $nonce_key = 'cover_art_upload_nonce:' . $cover_art_id;
-    my $stored_nonce = $store->get($nonce_key);
-
-    if (!defined $stored_nonce || $stored_nonce ne $nonce) {
-        return $field->push_errors(l(
-            'The form you’ve submitted has expired. ' .
-            'Please resubmit your request.',
-        ));
-    }
-}
+sub art_archive_model { shift->ctx->model('CoverArtArchive') }
 
 no Moose;
 __PACKAGE__->meta->make_immutable;
