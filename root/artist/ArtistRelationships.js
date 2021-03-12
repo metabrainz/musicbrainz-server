7e8f0d125dd688c30e7e32b5c7e2ea0c2a3810fc
Nicolás Tamargo <reosarevok@gmail.com>
2021-03-12 12:07:24 +0200

    MBS-8820: Show explanation if relationships tab shows no rels
    For an artist that only has event relationships (or, I assume, a label
    that only has relationships of a type that gets filtered out
    by groupRelationships), the relationships tab is currently just empty.
    This changes it so that Relationships displays an informative message
    if all existing relationships have been filtered out.
    While at it, I also got rid of the separate "no rels" message
    on ArtistRelationships and LabelRelationships, and moved that
    to Relationships as well.

--- root/artist/ArtistRelationships.js
+++ root/artist/ArtistRelationships.js
@@ -11,7 +11,6 @@ import * as React from 'react';
 
 import Relationships from '../components/Relationships';
 import RelationshipsTable from '../components/RelationshipsTable';
-import EntityLink from '../static/scripts/common/components/EntityLink';
 
 import ArtistLayout from './ArtistLayout';
 
@@ -30,19 +29,7 @@ const ArtistRelationships = ({
     page="relationships"
     title={l('Relationships')}
   >
-    {artist.relationships?.length ? (
-      <Relationships source={artist} />
-    ) : (
-      <>
-        <h2 className="relationships">{l('Relationships')}</h2>
-        <p>
-          {exp.l(
-            '{link} has no relationships.',
-            {link: <EntityLink entity={artist} />},
-          )}
-        </p>
-      </>
-    )}
+    <Relationships showIfEmpty source={artist} />
     <RelationshipsTable
       entity={artist}
       heading={l('Appearances')}
