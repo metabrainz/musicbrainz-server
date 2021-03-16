476f5a816e955b7ffac9aa90a2127133963a07ab
Nicolás Tamargo <reosarevok@gmail.com>
2021-03-16 09:43:20 +0200

    MBS-11461: Add TO_JSON to ExampleRelationships

--- lib/MusicBrainz/Server/Entity/ExampleRelationship.pm
+++ lib/MusicBrainz/Server/Entity/ExampleRelationship.pm
@@ -1,6 +1,9 @@
 package MusicBrainz::Server::Entity::ExampleRelationship;
 use Moose;
 
+use MusicBrainz::Server::Data::Utils qw( boolean_to_json );
+use MusicBrainz::Server::Entity::Util::JSON qw( to_json_object );
+
 has name => (
     isa => 'Str',
     required => 1,
@@ -17,5 +20,15 @@ has relationship => (
     is => 'ro',
     required => 1
 );
+
+sub TO_JSON {
+    my ($self) = @_;
+
+    return {
+        name            => $self->name,
+        published       => boolean_to_json($self->published),
+        relationship    => to_json_object($self->relationship),
+    };
+}
 
 1;
