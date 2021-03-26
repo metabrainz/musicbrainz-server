d84e5e5787a3269e928aa65c89c8a110a953980e
Nicolás Tamargo <reosarevok@gmail.com>
2021-03-26 08:01:50 +0200

    MBS-9349: Add Pinterest links to the sidebar
    This displays username when available, like other social media links.

---
+++ lib/MusicBrainz/Server/Entity/URL/Pinterest.pm
@@ -0,0 +1,31 @@
+package MusicBrainz::Server::Entity::URL::Pinterest;
+
+use Moose;
+
+extends 'MusicBrainz::Server::Entity::URL';
+with 'MusicBrainz::Server::Entity::URL::Sidebar';
+
+sub sidebar_name {
+    my $self = shift;
+
+    if ($self->url =~ m{^https?://(?:www.)?pinterest.com/([^/]+)/?$}i) {
+        return $1;
+    }
+    else {
+        return 'Pinterest';
+    }
+};
+
+__PACKAGE__->meta->make_immutable;
+no Moose;
+1;
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2021 MetaBrainz Foundation
+
+This file is part of MusicBrainz, the open internet music database,
+and is licensed under the GPL version 2, or (at your option) any
+later version: http://www.gnu.org/licenses/gpl-2.0.txt
+
+=cut
