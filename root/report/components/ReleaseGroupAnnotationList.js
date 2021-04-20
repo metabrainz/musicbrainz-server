ceac5f0d972c3b3daddb15dd60ccf2c9a03caf2b
Nicolás Tamargo <reosarevok@gmail.com>
2021-04-20 19:40:44 +0300

    MBS-10711: Convert report ReleaseGroupAnnotationList to react-table
    This used to be ordered with the RG artist before the RG
    title / link. This is something we do not do anywhere else, and we don't
    even consistently do it for reports (releases list artist after title),
    so I'm changing it to be consistent with other RG displays.

--- root/report/components/ReleaseGroupAnnotationList.js
+++ root/report/components/ReleaseGroupAnnotationList.js
@@ -10,11 +10,14 @@
 import * as React from 'react';
 
 import PaginatedResults from '../../components/PaginatedResults';
-import EntityLink from '../../static/scripts/common/components/EntityLink';
-import loopParity from '../../utility/loopParity';
+import Table from '../../components/Table';
+import {
+  defineArtistCreditColumn,
+  defineEntityColumn,
+  defineTextColumn,
+  defineTextHtmlColumn,
+} from '../../utility/tableColumns';
 import type {ReportReleaseGroupAnnotationT} from '../types';
-import ArtistCreditLink
-  from '../../static/scripts/common/components/ArtistCreditLink';
 
 type Props = {
   +items: $ReadOnlyArray<ReportReleaseGroupAnnotationT>,
@@ -24,49 +27,67 @@ type Props = {
 const ReleaseGroupAnnotationList = ({
   items,
   pager,
-}: Props): React.Element<typeof PaginatedResults> => (
-  <PaginatedResults pager={pager}>
-    <table className="tbl">
-      <thead>
-        <tr>
-          <th>{l('Artist')}</th>
-          <th>{l('Release Group')}</th>
-          <th>{l('Type')}</th>
-          <th>{l('Annotation')}</th>
-          <th style={{width: '10em'}}>{l('Last edited')}</th>
-        </tr>
-      </thead>
-      <tbody>
-        {items.map((item, index) => (
-          <tr className={loopParity(index)} key={item.release_group_id}>
-            {item.release_group ? (
-              <>
-                <td>
-                  <ArtistCreditLink
-                    artistCredit={item.release_group.artistCredit}
-                  />
-                </td>
-                <td>
-                  <EntityLink entity={item.release_group} />
-                </td>
-                <td>
-                  {nonEmpty(item.release_group.l_type_name)
-                    ? item.release_group.l_type_name
-                    : l('Unknown')}
-                </td>
-              </>
-            ) : (
-              <td colSpan="3">
-                {l('This release group no longer exists.')}
-              </td>
-            )}
-            <td dangerouslySetInnerHTML={{__html: item.text}} />
-            <td>{item.created}</td>
-          </tr>
-        ))}
-      </tbody>
-    </table>
-  </PaginatedResults>
-);
+}: Props): React.Element<typeof PaginatedResults> => {
+  const existingReleaseGroupItems = items.reduce((result, item) => {
+    if (item.release_group != null) {
+      result.push(item);
+    }
+    return result;
+  }, []);
+
+  const columns = React.useMemo(
+    () => {
+      const releaseGroupColumn =
+        defineEntityColumn<ReportReleaseGroupAnnotationT>({
+          columnName: 'release_group',
+          descriptive: false,
+          getEntity: result => result.release_group ?? null,
+          title: l('Release Group'),
+        });
+      const artistCreditColumn =
+        defineArtistCreditColumn<ReportReleaseGroupAnnotationT>({
+          columnName: 'artist',
+          getArtistCredit:
+            result => result.release_group?.artistCredit ?? null,
+          title: l('Artist'),
+        });
+      const typeColumn = defineTextColumn<ReportReleaseGroupAnnotationT>({
+        columnName: 'type',
+        getText: result => {
+          const typeName = result.release_group?.l_type_name;
+          return nonEmpty(typeName) ? typeName : l('Unknown');
+        },
+        title: l('Type'),
+      });
+      const annotationColumn =
+        defineTextHtmlColumn<ReportReleaseGroupAnnotationT>({
+          columnName: 'annotation',
+          getText: result => result.text,
+          title: l('Annotation'),
+        });
+      const editedColumn = defineTextColumn<ReportReleaseGroupAnnotationT>({
+        columnName: 'created',
+        getText: result => result.created,
+        headerProps: {className: 'last-edited-heading'},
+        title: l('Last edited'),
+      });
+
+      return [
+        releaseGroupColumn,
+        artistCreditColumn,
+        typeColumn,
+        annotationColumn,
+        editedColumn,
+      ];
+    },
+    [],
+  );
+
+  return (
+    <PaginatedResults pager={pager}>
+      <Table columns={columns} data={existingReleaseGroupItems} />
+    </PaginatedResults>
+  );
+};
 
 export default ReleaseGroupAnnotationList;
