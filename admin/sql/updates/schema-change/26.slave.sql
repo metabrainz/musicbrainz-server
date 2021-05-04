0fa0a59ac5c430236b2016ee4fd169952c85b7a2
Nicolás Tamargo <reosarevok@gmail.com>
2021-05-05 02:03:44 +0300

    MBS-11459: Create edit_data_type_info function on mirrors (#2039)
    This is already on CreateFunctions but it should also be added
    to existing mirrors.

--- admin/sql/updates/schema-change/26.slave.sql
+++ admin/sql/updates/schema-change/26.slave.sql
@@ -3,6 +3,7 @@
 -- 20200914-oauth-pkce.sql
 -- 20201028-mbs-1424.sql
 -- 20210309-mbs-11431.sql
+-- 20210406-mbs-11459.sql
 \set ON_ERROR_STOP 1
 BEGIN;
 SET search_path = musicbrainz, public;
@@ -348,5 +349,40 @@ CREATE INDEX CONCURRENTLY series_alias_idx_lower_unaccent_name ON series_alias (
 
 DROP INDEX CONCURRENTLY IF EXISTS artist_idx_lower_name;
 DROP INDEX CONCURRENTLY IF EXISTS label_idx_lower_name;
+
+--------------------------------------------------------------------------------
+SELECT '20210406-mbs-11459.sql';
+
+
+CREATE OR REPLACE FUNCTION edit_data_type_info(data JSONB) RETURNS TEXT AS $$
+BEGIN
+    CASE jsonb_typeof(data)
+    WHEN 'object' THEN
+        RETURN '{' ||
+            (SELECT string_agg(
+                to_json(key) || ':' ||
+                edit_data_type_info(jsonb_extract_path(data, key)),
+                ',' ORDER BY key)
+               FROM jsonb_object_keys(data) AS key) ||
+            '}';
+    WHEN 'array' THEN
+        RETURN '[' ||
+            (SELECT string_agg(
+                DISTINCT edit_data_type_info(item),
+                ',' ORDER BY edit_data_type_info(item))
+               FROM jsonb_array_elements(data) AS item) ||
+            ']';
+    WHEN 'string' THEN
+        RETURN '1';
+    WHEN 'number' THEN
+        RETURN '2';
+    WHEN 'boolean' THEN
+        RETURN '4';
+    WHEN 'null' THEN
+        RETURN '8';
+    END CASE;
+    RETURN '';
+END;
+$$ LANGUAGE plpgsql IMMUTABLE PARALLEL SAFE STRICT;
 
 COMMIT;
