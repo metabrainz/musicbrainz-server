0c570b31c9666900907299986155648ca1cc36a2
yvanzo <yvanzo@metabrainz.org>
2021-05-09 18:01:01 +0200

    MBS-11456 (V): Add update scripts to schema 27

---
+++ admin/sql/updates/20210419-mbs-11456.sql
@@ -0,0 +1,27 @@
+\set ON_ERROR_STOP 1
+
+BEGIN;
+
+-- Artist credit GID
+
+-- Creating column
+ALTER TABLE artist_credit ADD COLUMN gid uuid;
+
+-- Generating GIDs
+UPDATE artist_credit SET gid =
+    generate_uuid_v3('6ba7b8119dad11d180b400c04fd430c8', 'artist_credit' || id);
+
+-- Adding NOT NULL constraint
+ALTER TABLE artist_credit ALTER COLUMN gid SET NOT NULL;
+
+-- Creating index
+CREATE UNIQUE INDEX artist_credit_idx_gid ON artist_credit (gid);
+
+-- Artist credit GID redirect
+CREATE TABLE artist_credit_gid_redirect ( -- replicate (verbose)
+    gid                 UUID NOT NULL, -- PK
+    new_id              INTEGER NOT NULL, -- references artist_credit.id
+    created             TIMESTAMP WITH TIME ZONE DEFAULT NOW()
+);
+
+COMMIT;
