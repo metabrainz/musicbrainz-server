f5b7c27b222aba941ff1dbe5689b70a38ca2d608
Michael Wiencek <mwtuea@gmail.com>
2021-05-09 01:48:47 -0500

    CREATE INDEX IF NOT EXISTS

--- admin/sql/updates/20210309-mbs-11431.sql
+++ admin/sql/updates/20210309-mbs-11431.sql
@@ -2,15 +2,15 @@
 
 BEGIN;
 
-CREATE INDEX artist_idx_lower_unaccent_name_comment ON artist (lower(musicbrainz_unaccent(name)), lower(musicbrainz_unaccent(comment)));
-CREATE INDEX label_idx_lower_unaccent_name_comment ON label (lower(musicbrainz_unaccent(name)), lower(musicbrainz_unaccent(comment)));
-CREATE INDEX place_idx_lower_unaccent_name_comment ON place (lower(musicbrainz_unaccent(name)), lower(musicbrainz_unaccent(comment)));
-CREATE INDEX series_idx_lower_unaccent_name_comment ON series (lower(musicbrainz_unaccent(name)), lower(musicbrainz_unaccent(comment)));
+CREATE INDEX IF NOT EXISTS artist_idx_lower_unaccent_name_comment ON artist (lower(musicbrainz_unaccent(name)), lower(musicbrainz_unaccent(comment)));
+CREATE INDEX IF NOT EXISTS label_idx_lower_unaccent_name_comment ON label (lower(musicbrainz_unaccent(name)), lower(musicbrainz_unaccent(comment)));
+CREATE INDEX IF NOT EXISTS place_idx_lower_unaccent_name_comment ON place (lower(musicbrainz_unaccent(name)), lower(musicbrainz_unaccent(comment)));
+CREATE INDEX IF NOT EXISTS series_idx_lower_unaccent_name_comment ON series (lower(musicbrainz_unaccent(name)), lower(musicbrainz_unaccent(comment)));
 
-CREATE INDEX artist_alias_idx_lower_unaccent_name ON artist_alias (lower(musicbrainz_unaccent(name)));
-CREATE INDEX label_alias_idx_lower_unaccent_name ON label_alias (lower(musicbrainz_unaccent(name)));
-CREATE INDEX place_alias_idx_lower_unaccent_name ON place_alias (lower(musicbrainz_unaccent(name)));
-CREATE INDEX series_alias_idx_lower_unaccent_name ON series_alias (lower(musicbrainz_unaccent(name)));
+CREATE INDEX IF NOT EXISTS artist_alias_idx_lower_unaccent_name ON artist_alias (lower(musicbrainz_unaccent(name)));
+CREATE INDEX IF NOT EXISTS label_alias_idx_lower_unaccent_name ON label_alias (lower(musicbrainz_unaccent(name)));
+CREATE INDEX IF NOT EXISTS place_alias_idx_lower_unaccent_name ON place_alias (lower(musicbrainz_unaccent(name)));
+CREATE INDEX IF NOT EXISTS series_alias_idx_lower_unaccent_name ON series_alias (lower(musicbrainz_unaccent(name)));
 
 DROP INDEX IF EXISTS artist_idx_lower_name;
 DROP INDEX IF EXISTS label_idx_lower_name;
--- admin/sql/updates/schema-change/26.slave.sql
+++ admin/sql/updates/schema-change/26.slave.sql
@@ -373,15 +373,15 @@ CREATE OR REPLACE VIEW medium_track_durations AS
 SELECT '20210309-mbs-11431.sql';
 
 
-CREATE INDEX artist_idx_lower_unaccent_name_comment ON artist (lower(musicbrainz_unaccent(name)), lower(musicbrainz_unaccent(comment)));
-CREATE INDEX label_idx_lower_unaccent_name_comment ON label (lower(musicbrainz_unaccent(name)), lower(musicbrainz_unaccent(comment)));
-CREATE INDEX place_idx_lower_unaccent_name_comment ON place (lower(musicbrainz_unaccent(name)), lower(musicbrainz_unaccent(comment)));
-CREATE INDEX series_idx_lower_unaccent_name_comment ON series (lower(musicbrainz_unaccent(name)), lower(musicbrainz_unaccent(comment)));
+CREATE INDEX IF NOT EXISTS artist_idx_lower_unaccent_name_comment ON artist (lower(musicbrainz_unaccent(name)), lower(musicbrainz_unaccent(comment)));
+CREATE INDEX IF NOT EXISTS label_idx_lower_unaccent_name_comment ON label (lower(musicbrainz_unaccent(name)), lower(musicbrainz_unaccent(comment)));
+CREATE INDEX IF NOT EXISTS place_idx_lower_unaccent_name_comment ON place (lower(musicbrainz_unaccent(name)), lower(musicbrainz_unaccent(comment)));
+CREATE INDEX IF NOT EXISTS series_idx_lower_unaccent_name_comment ON series (lower(musicbrainz_unaccent(name)), lower(musicbrainz_unaccent(comment)));
 
-CREATE INDEX artist_alias_idx_lower_unaccent_name ON artist_alias (lower(musicbrainz_unaccent(name)));
-CREATE INDEX label_alias_idx_lower_unaccent_name ON label_alias (lower(musicbrainz_unaccent(name)));
-CREATE INDEX place_alias_idx_lower_unaccent_name ON place_alias (lower(musicbrainz_unaccent(name)));
-CREATE INDEX series_alias_idx_lower_unaccent_name ON series_alias (lower(musicbrainz_unaccent(name)));
+CREATE INDEX IF NOT EXISTS artist_alias_idx_lower_unaccent_name ON artist_alias (lower(musicbrainz_unaccent(name)));
+CREATE INDEX IF NOT EXISTS label_alias_idx_lower_unaccent_name ON label_alias (lower(musicbrainz_unaccent(name)));
+CREATE INDEX IF NOT EXISTS place_alias_idx_lower_unaccent_name ON place_alias (lower(musicbrainz_unaccent(name)));
+CREATE INDEX IF NOT EXISTS series_alias_idx_lower_unaccent_name ON series_alias (lower(musicbrainz_unaccent(name)));
 
 DROP INDEX IF EXISTS artist_idx_lower_name;
 DROP INDEX IF EXISTS label_idx_lower_name;
