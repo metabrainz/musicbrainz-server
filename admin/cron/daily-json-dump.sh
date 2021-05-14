metabrainz/musicbrainz-server

commit 7d5c1b0db252dd12c8c7b5b6cf2184d242e32b95

Replace #!/bin/bash with #!/usr/bin/env bash
I ran into an issue trying to run ./upgrade.sh on macOS: it imports
admin/config.sh, which uses `-v` since
d2b92501113fa83ecd102a1f62c4732e3133a520. `-v` isn't supported in bash
v3, which is what's bundled with macOS (and this likely won't ever be
changed).

Luckily, I have bash v5 installed from Homebrew, as I expect most people
do. Unluckily, we hardcode `#!/bin/bash` in almost every script. I'm
changing all occurrences of that to `#!/usr/bin/env bash` so that our
scripts use the correct version of bash according to my environment.

admin/cron/daily-json-dump.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -u
+#!/usr/bin/env bash -u
 
 MB_SERVER_ROOT=$(cd "$(dirname "${BASH_SOURCE[0]}")/../../" && pwd)
 cd "$MB_SERVER_ROOT"

admin/cron/daily-sitemaps.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -u
+#!/usr/bin/env bash -u
 
 set -e
 

admin/cron/daily.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -u
+#!/usr/bin/env bash -u
 
 export PATH=/usr/local/bin:$PATH
 

admin/cron/hourly-json-dump.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -u
+#!/usr/bin/env bash -u
 
 MB_SERVER_ROOT=$(cd "$(dirname "${BASH_SOURCE[0]}")/../../" && pwd)
 cd "$MB_SERVER_ROOT"

admin/cron/hourly-sitemaps.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -u
+#!/usr/bin/env bash -u
 
 set -e
 

admin/cron/hourly.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -u
+#!/usr/bin/env bash -u
 
 export PATH=/usr/local/bin:$PATH
 export PERL_CARTON_PATH=~/carton-local

admin/cron/monthly.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -u
+#!/usr/bin/env bash -u
 
 MB_SERVER_ROOT=$(cd "$(dirname "${BASH_SOURCE[0]}")/../../" && pwd)
 cd "$MB_SERVER_ROOT"

bin/rsync-staticbrainz-files
@@ -1,4 +1,4 @@
-#!/bin/bash -u
+#!/usr/bin/env bash -u
 
 KEY="$1"
 SOURCE="$2"

commit d28ef511d74874f4e2f0874f74d3e1a947ceb57c

Move -u from shebang to script
Broken by 7d5c1b0db252dd12c8c7b5b6cf2184d242e32b95

There's a way to pass multiple arguments to env with -S, but this seems
more straightforward.

admin/cron/daily-json-dump.sh
@@ -1,4 +1,6 @@
-#!/usr/bin/env bash -u
+#!/usr/bin/env bash
+
+set -u
 
 MB_SERVER_ROOT=$(cd "$(dirname "${BASH_SOURCE[0]}")/../../" && pwd)
 cd "$MB_SERVER_ROOT"

admin/cron/daily-sitemaps.sh
@@ -1,5 +1,6 @@
-#!/usr/bin/env bash -u
+#!/usr/bin/env bash
 
+set -u
 set -e
 
 MB_SERVER_ROOT=$(cd "$(dirname "${BASH_SOURCE[0]}")/../../" && pwd)

admin/cron/daily.sh
@@ -1,4 +1,6 @@
-#!/usr/bin/env bash -u
+#!/usr/bin/env bash
+
+set -u
 
 export PATH=/usr/local/bin:$PATH
 

admin/cron/hourly-json-dump.sh
@@ -1,4 +1,6 @@
-#!/usr/bin/env bash -u
+#!/usr/bin/env bash
+
+set -u
 
 MB_SERVER_ROOT=$(cd "$(dirname "${BASH_SOURCE[0]}")/../../" && pwd)
 cd "$MB_SERVER_ROOT"

admin/cron/hourly-sitemaps.sh
@@ -1,5 +1,6 @@
-#!/usr/bin/env bash -u
+#!/usr/bin/env bash
 
+set -u
 set -e
 
 MB_SERVER_ROOT=$(cd "$(dirname "${BASH_SOURCE[0]}")/../../" && pwd)

admin/cron/hourly.sh
@@ -1,4 +1,6 @@
-#!/usr/bin/env bash -u
+#!/usr/bin/env bash
+
+set -u
 
 export PATH=/usr/local/bin:$PATH
 export PERL_CARTON_PATH=~/carton-local

admin/cron/monthly.sh
@@ -1,4 +1,6 @@
-#!/usr/bin/env bash -u
+#!/usr/bin/env bash
+
+set -u
 
 MB_SERVER_ROOT=$(cd "$(dirname "${BASH_SOURCE[0]}")/../../" && pwd)
 cd "$MB_SERVER_ROOT"

bin/rsync-staticbrainz-files
@@ -1,4 +1,6 @@
-#!/usr/bin/env bash -u
+#!/usr/bin/env bash
+
+set -u
 
 KEY="$1"
 SOURCE="$2"

upgrade.sh
@@ -1,5 +1,6 @@
-#!/usr/bin/env bash -u
+#!/usr/bin/env bash
 
+set -u
 set -o errexit
 
 MB_SERVER_ROOT=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)