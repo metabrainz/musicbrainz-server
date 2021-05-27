9157840bdaff1a23ac7f544b241becd12330f3eb
Michael Wiencek <mwtuea@gmail.com>
2021-05-26 19:48:13 -0500

    a_upd_release_event: check for changes before doing work
    Avoid re-calculating `*first_release_date` data if the release event
    didn't actually change.
    
    This can happen if the trigger fired on the `release_country` table and
    only the country changed. (Rather than checking for that, it's still
    correct to test the data we're concerned about: `UPDATE` triggers fire
    even when no data changed.)
    
    Additionally, make sure we're only calling
    `set_release_first_release_date` once if the release column is
    unchanged.  (This would occur on merges, and probably no other time.)

--- admin/sql/CreateFunctions.sql
+++ admin/sql/CreateFunctions.sql
@@ -1184,14 +1184,23 @@ $$ LANGUAGE 'plpgsql';
 CREATE OR REPLACE FUNCTION a_upd_release_event()
 RETURNS TRIGGER AS $$
 BEGIN
-  PERFORM set_release_first_release_date(OLD.release);
-  PERFORM set_release_first_release_date(NEW.release);
+  IF (
+    NEW.release != OLD.release OR
+    NEW.date_year IS DISTINCT FROM OLD.date_year OR
+    NEW.date_month IS DISTINCT FROM OLD.date_month OR
+    NEW.date_day IS DISTINCT FROM OLD.date_day
+  ) THEN
+    PERFORM set_release_first_release_date(OLD.release);
+    IF NEW.release != OLD.release THEN
+        PERFORM set_release_first_release_date(NEW.release);
+    END IF;
 
-  PERFORM set_release_group_first_release_date(release_group)
-  FROM release
-  WHERE release.id IN (NEW.release, OLD.release);
+    PERFORM set_release_group_first_release_date(release_group)
+    FROM release
+    WHERE release.id IN (NEW.release, OLD.release);
 
-  PERFORM set_releases_recordings_first_release_dates(ARRAY[NEW.release, OLD.release]);
+    PERFORM set_releases_recordings_first_release_dates(ARRAY[NEW.release, OLD.release]);
+  END IF;
 
   IF TG_TABLE_NAME = 'release_country' THEN
     IF NEW.country != OLD.country THEN
