13448a582b5256467ecfac2540335616b1d074a7
Nicolás Tamargo <reosarevok@gmail.com>
2021-07-06 11:53:56 +0300

    MBS-11774: Add fullwidth ｆｅａｔ / ｆｅａｔｕｒｉｎｇ to the featuring reports
    These are already on guessFeat, but not on the reports.
    These might not always be followed by a space, but since I'm
    not sure, I'm not making the end space optional for now.

--- lib/MusicBrainz/Server/Report/FeaturingReleases.pm
+++ lib/MusicBrainz/Server/Report/FeaturingReleases.pm
@@ -15,7 +15,7 @@ sub query {
             JOIN artist_credit ac ON r.artist_credit = ac.id
             JOIN release_meta rm ON r.id = rm.id
         WHERE
-            r.name COLLATE musicbrainz ~* E' \\\\((duet with|συμμετέχει|(f|w)/|(f|feat|ft|συμμ)\\\\.|featuring) '
+            r.name COLLATE musicbrainz ~* E' \\\\((duet with|συμμετέχει|(f|w)/|(f|feat|ｆｅａｔ|ft|συμμ)(\\\\.|．)|(featuring|ｆｅａｔｕｒｉｎｇ)) '
     ";
 }
 
