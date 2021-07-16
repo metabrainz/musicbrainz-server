b5f1105d023c2ae9ff9a2dc43ad03fb1d3d55663
Nicolás Tamargo <reosarevok@gmail.com>
2021-07-16 15:07:19 +0300

    MBS-11689: Report for pseudo-releases marked as the original tracklist

---
+++ root/report/MislinkedPseudoReleases.js
@@ -0,0 +1,43 @@
+/*
+ * @flow strict-local
+ * Copyright (C) 2021 MetaBrainz Foundation
+ *
+ * This file is part of MusicBrainz, the open internet music database,
+ * and is licensed under the GPL version 2, or (at your option) any
+ * later version: http://www.gnu.org/licenses/gpl-2.0.txt
+ */
+
+import * as React from 'react';
+
+import ReleaseList from './components/ReleaseList';
+import ReportLayout from './components/ReportLayout';
+import type {ReportDataT, ReportReleaseT} from './types';
+
+const MislinkedPseudoReleases = ({
+  canBeFiltered,
+  filtered,
+  generated,
+  items,
+  pager,
+}: ReportDataT<ReportReleaseT>): React.Element<typeof ReportLayout> => (
+  <ReportLayout
+    canBeFiltered={canBeFiltered}
+    description={l(
+      `This report shows releases with status Pseudo-Release that are
+       marked as the original version of a translation/transliteration
+       relationship. The pseudo-release should be the one marked as a
+       translated/transliterated version instead. If both releases
+       are pseudo-releases, consider linking both to an official release
+       rather than to each other.`,
+    )}
+    entityType="release"
+    filtered={filtered}
+    generated={generated}
+    title={l('Translated/transliterated pseudo-releases marked as original')}
+    totalEntries={pager.total_entries}
+  >
+    <ReleaseList items={items} pager={pager} />
+  </ReportLayout>
+);
+
+export default MislinkedPseudoReleases;
