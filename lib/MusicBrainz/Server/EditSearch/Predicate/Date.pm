fc0a7527440320e7efe95a8918744cb90df817fd
Nicolás Tamargo <reosarevok@gmail.com>
2021-07-23 16:13:35 +0300

    MBS-11804: Allow filtering edit search by time of edit note
    We now have two different predicates that require natural date
    processing, so this moves the relevant sections to a role.
    
    I also moved the cardinality map to the role - Date was reusing
    the ID predicate operators, but that is confusing (it's not that
    the two are connected in any way, other than just happening to use
    the same operators). As such, it seems sensible to just
    stop making Date depend on ID. Other than moving the operators
    to the role, that makes combine_with_query larger but, IMO,
    a lot clearer.

--- lib/MusicBrainz/Server/EditSearch/Predicate/Date.pm
+++ lib/MusicBrainz/Server/EditSearch/Predicate/Date.pm
@@ -1,47 +1,32 @@
 package MusicBrainz::Server::EditSearch::Predicate::Date;
 use Moose;
 use namespace::autoclean;
+use feature 'switch';
 
-use DateTime::Format::Natural;
-use DateTime::Format::Pg;
+with 'MusicBrainz::Server::EditSearch::Predicate::Role::NaturalDate';
 
-# Happens to share the same operators as ID searches, just handles the
-# input slightly differently
-extends 'MusicBrainz::Server::EditSearch::Predicate::ID';
-with 'MusicBrainz::Server::EditSearch::Predicate';
-
-sub transform_user_input {
-    my ($self, $argument) = @_;
-    my $parser = DateTime::Format::Natural->new;
-    DateTime::Format::Pg->format_datetime($parser->parse_datetime($argument));
-}
-
-override combine_with_query => sub {
+sub combine_with_query {
     my ($self, $query) = @_;
 
-    if ($self->operator eq '=') {
-        $query->add_where([
-            'edit.' . $self->field_name . ' BETWEEN '.
-            q(date_trunc('day', ? AT TIME ZONE 'UTC') AND ) .
-            q(date_trunc('day', ? AT TIME ZONE 'UTC') + interval '1 day'),
-            [ $self->sql_arguments->[0], @{ $self->sql_arguments } ],
-        ]);
-    }
-    else {
-        super();
-    }
-};
+    my $sql;
+    my @arguments = @{ $self->sql_arguments };
 
-sub valid {
-    my $self = shift;
-    my $parser = DateTime::Format::Natural->new;
-    for my $arg_index (1.. $self->operator_cardinality($self->operator)) {
-        my $arg = $self->argument($arg_index - 1) or return;
-        $parser->parse_datetime($arg);
-        $parser->success or return;
+    given ($self->operator) {
+        when ('BETWEEN') {
+            $sql = 'edit.' . $self->field_name . ' BETWEEN SYMMETRIC ? AND ?';
+        }
+        when ('=') {
+            $sql = 'edit.' . $self->field_name . ' BETWEEN '.
+            q[date_trunc('day', ? AT TIME ZONE 'UTC') AND ] .
+            q[date_trunc('day', ? AT TIME ZONE 'UTC') + interval '1 day'];
+            unshift @arguments, $arguments[0];
+        }
+        default {
+           $sql = join(' ', 'edit.'.$self->field_name, $self->operator, '?')
+       }
     }
 
-    return 1;
+    $query->add_where([ $sql, \@arguments ]);
 }
 
 1;
