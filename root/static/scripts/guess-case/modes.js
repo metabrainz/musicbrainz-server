8dd145e9f9512866c126e2048fe3781c75ad690e
Nicolás Tamargo <reosarevok@gmail.com>
2021-08-04 18:42:49 +0300

    Make fixEnglishKeyNames directly part of runPostProcess
    This will be needed for easy flow typing, plus flow no longer allows
    this inside objects.

--- root/static/scripts/guess-case/modes.js
+++ root/static/scripts/guess-case/modes.js
@@ -296,22 +296,6 @@ export const English = {
     },
   )),
 
-  /*
-   * This changes key names in titles to follow
-   * the English classical music guidelines.
-   * See https://musicbrainz.org/doc/Style/Classical/Language/English#Keys
-   */
-  fixEnglishKeyNames(is) {
-    return is.replace(
-      /\bin ([a-g])(?:[\s-]([Ff]lat|[Ss]harp))?\s(dorian|lydian|major|minor|mixolydian)(?:\b|$)/ig,
-      function (match, p1, p2, p3) {
-        return 'in ' + p1.toUpperCase() +
-          (p2 ? '-' + p2.toLowerCase() : '') +
-          ' ' + p3.toLowerCase();
-      },
-    );
-  },
-
   isSentenceCaps() {
     return false;
   },
@@ -320,7 +304,19 @@ export const English = {
 
   runPostProcess(is) {
     is = DefaultMode.runPostProcess(is);
-    is = this.fixEnglishKeyNames(is);
+    /*
+     * This changes key names in titles to follow
+     * the English classical music guidelines.
+     * See https://musicbrainz.org/doc/Style/Classical/Language/English#Keys
+     */
+    is = is.replace(
+      /\bin ([a-g])(?:[\s-]([Ff]lat|[Ss]harp))?\s(dorian|lydian|major|minor|mixolydian)(?:\b|$)/ig,
+      function (match, p1, p2, p3) {
+        return 'in ' + p1.toUpperCase() +
+          (p2 ? '-' + p2.toLowerCase() : '') +
+          ' ' + p3.toLowerCase();
+      },
+    );
     return is;
   },
 };
