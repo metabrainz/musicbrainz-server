108f402336941dc60cc52ff32cd3a668a552afa7
Nicolás Tamargo <reosarevok@gmail.com>
2021-11-22 20:17:07 +0200

    MBS-11917: New report: Releases with "Disc n" medium titles (#2338)
    This checks for any releases that have mediums where
    the name is "CD n", "Disc n" or similar, with n being
    the medium position.
    These should only very, very rarely be correct, and only
    if they can be proven to really be intended as a disc *title*.

---
+++ root/report/MediumsWithOrderInTitle.js
@@ -0,0 +1,42 @@
+/*
+ * @flow strict-local
+ * Copyright (C) 2021 MetaBrainz Foundation
+ *
+ * This file is part of MusicBrainz, the open internet music database,
+ * and is licensed under the GPL version 2, or (at your option) any
+ * later version: http://www.gnu.org/licenses/gpl-2.0.txt
+ */
+
+import * as React from 'react';
+
+import ReleaseList from './components/ReleaseList';
+import ReportLayout from './components/ReportLayout';
+import type {ReportDataT, ReportReleaseT} from './types';
+
+const MediumsWithOrderInTitle = ({
+  canBeFiltered,
+  filtered,
+  generated,
+  items,
+  pager,
+}: ReportDataT<ReportReleaseT>): React.Element<typeof ReportLayout> => (
+  <ReportLayout
+    canBeFiltered={canBeFiltered}
+    description={exp.l(
+      `This report lists releases where at least one medium has a title
+       that seems to just be indicating its position (such a first medium
+       with the title “Disc 1”). These should usually be removed, as per
+       {release_style|the release guidelines}.`,
+      {release_style: '/doc/Style/Release#Medium_title'},
+    )}
+    entityType="release"
+    filtered={filtered}
+    generated={generated}
+    title={l('Releases with mediums named after their position')}
+    totalEntries={pager.total_entries}
+  >
+    <ReleaseList items={items} pager={pager} />
+  </ReportLayout>
+);
+
+export default MediumsWithOrderInTitle;
