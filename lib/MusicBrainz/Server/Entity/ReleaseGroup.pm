metabrainz/musicbrainz-server

commit 7d28f5c7efb224b670ee3853c1336ad3e1c53abc

MBS-11059: Indicate which release groups have CAA art in listings (#1868)
This adds a new 'load_has_cover_art' method and a new 'has_cover_art'
boolean to be able to have a quicker query for this that doesn't
actually load all the artwork for the release group.

Since 'has_cover_art' was already the predicate for 'cover_art',
I changed that to 'has_loaded_cover_art'. I also renamed that
for releases, since it seems more consistent that way.

lib/MusicBrainz/Server/Entity/ReleaseGroup.pm
@@ -4,6 +4,7 @@ use Moose;
 
 use DBDefs;
 use List::AllUtils qw( any );
+use MusicBrainz::Server::Data::Utils qw( boolean_to_json );
 use MusicBrainz::Server::Entity::PartialDate;
 use MusicBrainz::Server::Entity::Types;
 use MusicBrainz::Server::Entity::Util::JSON qw( to_json_object );
@@ -73,11 +74,16 @@ has 'release_count' => (
 has 'cover_art' => (
     isa       => 'MusicBrainz::Server::Entity::Artwork::ReleaseGroup',
     is        => 'rw',
-    predicate => 'has_cover_art',
+    predicate => 'has_loaded_cover_art',
+);
+
+has 'has_cover_art' => (
+    is  => 'rw',
+    isa => 'Bool',
 );
 
 # Cannot set cover art if none of the associated releases has cover art.
-sub can_set_cover_art { return shift->has_cover_art; }
+sub can_set_cover_art { return shift->has_loaded_cover_art; }
 
 has 'review_count' => (
     is => 'rw',
@@ -127,6 +133,7 @@ around TO_JSON => sub {
         %{ $self->$orig },
         $self->has_cover_art ? (cover_art => to_json_object($self->cover_art)) : (),
         firstReleaseDate    => $self->first_release_date ? $self->first_release_date->format : undef,
+        hasCoverArt         => boolean_to_json($self->has_cover_art),
         # TODO: remove this once Autocomplete.js can use $c and releaseGroupType.js
         l_type_name         => $self->l_type_name,
         release_count       => $self->release_count,

lib/MusicBrainz/Server/Data/ReleaseGroup.pm
@@ -10,6 +10,7 @@ use MusicBrainz::Server::Data::Utils qw(
     hash_to_row
     load_subobjects
     merge_table_attributes
+    object_to_ids
     order_by
     placeholders
 );
@@ -155,6 +156,34 @@ sub has_materialized_artist_release_group_data {
         'SELECT 1 FROM artist_release_group LIMIT 1',
     ) ? 1 : 0;
     return $has_data;
+}
+
+sub load_has_cover_art {
+    my ($self, @release_groups) = @_;
+    my %id_to_rg = object_to_ids(@release_groups);
+    my @ids = keys %id_to_rg;
+
+    return unless @ids; # nothing to do
+
+    my $query = <<~'SQL';
+        SELECT release.release_group
+          FROM release
+          JOIN cover_art_archive.cover_art ca ON ca.release = release.id
+          JOIN cover_art_archive.cover_art_type cat ON cat.id = ca.id
+          JOIN release_meta ON release_meta.id = release.id
+         WHERE release.release_group = any(?)
+           AND cat.type_id = 1
+           AND ca.mime_type != 'application/pdf'
+           AND release_meta.cover_art_presence != 'darkened'
+        SQL
+
+    my $ids_with_art = $self->sql->select_single_column_array($query, \@ids);
+
+    for my $id (@{ $ids_with_art }) {
+        for my $rg (@{ $id_to_rg{$id} }) {
+            $rg->has_cover_art(1);
+        }
+    }
 }
 
 sub pick_status_condition
@@ -716,6 +745,31 @@ sub _hash_to_row
     });
 
     return $row;
+}
+
+=method load_ids
+Load internal IDs for release group objects that only have GIDs.
+=cut
+
+sub load_ids
+{
+    my ($self, @rgs) = @_;
+
+    my @gids = map { $_->gid } @rgs;
+    return () unless @gids;
+
+    my $query = <<~'SQL';
+        SELECT gid, id
+        FROM release_group
+        WHERE gid = any(?)
+        SQL
+
+    my %map = map { $_->[0] => $_->[1] }
+        @{ $self->sql->select_list_of_lists($query, \@gids) };
+
+    for my $rg (@rgs) {
+        $rg->id($map{$rg->gid}) if exists $map{$rg->gid};
+    }
 }
 
 sub load_meta

lib/MusicBrainz/Server/Controller/Artist.pm
@@ -255,6 +255,8 @@ sub show : PathPart('') Chained('load')
         $c->model('ReleaseGroup')->rating->load_user_ratings($c->user->id, @$release_groups);
     }
 
+    $c->model('ReleaseGroup')->load_has_cover_art(@$release_groups);
+
     $c->model('ArtistCredit')->load(@$release_groups);
     $c->model('ArtistType')->load(map { map { $_->artist } $_->artist_credit->all_names} @$release_groups);
     $c->model('ReleaseGroupType')->load(@$release_groups);

lib/MusicBrainz/Server/Controller/Collection.pm
@@ -147,6 +147,7 @@ sub show : Chained('load') PathPart('') {
         $c->model('ArtistCredit')->load(@$entities);
         $c->model('ReleaseGroupType')->load(@$entities);
         $c->model('ReleaseGroupSecondaryType')->load_for_release_groups(@$entities);
+        $c->model('ReleaseGroup')->load_has_cover_art(@$entities);
     } elsif ($entity_type eq 'event') {
         $c->model('EventType')->load(@$entities);
         $model->load_performers(@$entities);

lib/MusicBrainz/Server/Controller/Search.pm
@@ -130,6 +130,7 @@ sub direct : Private
         }
         when ('release_group') {
             $c->model('ReleaseGroupType')->load(@entities);
+            $c->model('ReleaseGroup')->load_has_cover_art(@entities);
         }
         when ('release') {
             $c->model('Language')->load(@entities);

lib/MusicBrainz/Server/Controller/Series.pm
@@ -99,6 +99,7 @@ sub show : PathPart('') Chained('load') {
         $c->model('ReleaseGroupType')->load(@entities);
         $c->model('ReleaseGroup')->load_meta(@entities);
         $c->model('ReleaseGroup')->rating->load_user_ratings($c->user->id, @entities) if $c->user_exists;
+        $c->model('ReleaseGroup')->load_has_cover_art(@entities);
     }
 
     if ($series->type->item_entity_type eq 'work') {

lib/MusicBrainz/Server/Data/Search.pm
@@ -926,6 +926,13 @@ sub external_search
             $self->c->model('Area')->load_containment(@entities);
         }
 
+        if ($type eq 'release-group')
+        {
+            my @entities = map { $_->entity } @results;
+            $self->c->model('ReleaseGroup')->load_ids(@entities);
+            $self->c->model('Artwork')->load_for_release_groups(@entities);
+        }
+
         my $pager = Data::Page->new;
         $pager->current_page($page);
         $pager->entries_per_page($limit);

lib/MusicBrainz/Server/Entity/Release.pm
@@ -193,7 +193,7 @@ has [qw( cover_art_url info_url amazon_asin amazon_store )] => (
 has 'cover_art' => (
     isa       => 'MusicBrainz::Server::CoverArt',
     is        => 'rw',
-    predicate => 'has_cover_art',
+    predicate => 'has_loaded_cover_art',
 );
 
 has 'cover_art_presence' => (

commit 4c3a813e72a7490daa17a87139f8a20e690c7dff

MBS-12102: Fix cover art check on RG TO_JSON
This broke in 7d28f5c7efb224b670ee3853c1336ad3e1c53abc
since I changed the meaning of has_cover_art
but did not change this check to still use the original variable.

lib/MusicBrainz/Server/Entity/ReleaseGroup.pm
@@ -131,7 +131,7 @@ around TO_JSON => sub {
 
     return {
         %{ $self->$orig },
-        $self->has_cover_art ? (cover_art => to_json_object($self->cover_art)) : (),
+        $self->has_loaded_cover_art ? (cover_art => to_json_object($self->cover_art)) : (),
         firstReleaseDate    => $self->first_release_date ? $self->first_release_date->format : undef,
         hasCoverArt         => boolean_to_json($self->has_cover_art),
         # TODO: remove this once Autocomplete.js can use $c and releaseGroupType.js