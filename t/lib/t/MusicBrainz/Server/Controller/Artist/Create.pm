ec352408b2b0fdb9218ba2ae7c575b9b5c81259d
Nicolás Tamargo <reosarevok@gmail.com>
2022-01-28 14:55:43 +0200

    Document Artist::Create test
    Changed this to use separate tests rather than subtest since each test
    really seemed self-contained.
    Removed additional fields set to '' on the second test,
    since they're not needed and I think not having them makes the test
    more clear, not less.

--- t/lib/t/MusicBrainz/Server/Controller/Artist/Create.pm
+++ t/lib/t/MusicBrainz/Server/Controller/Artist/Create.pm
@@ -2,200 +2,291 @@ package t::MusicBrainz::Server::Controller::Artist::Create;
 use utf8;
 use Test::Routine;
 use Test::More;
-use MusicBrainz::Server::Test qw( html_ok );
+use MusicBrainz::Server::Test qw( capture_edits html_ok );
 
 with 't::Mechanize', 't::Context';
 
 use aliased 'MusicBrainz::Server::Entity::PartialDate';
 
-test all => sub {
+=head2 Test description
 
-my $test = shift;
-my $mech = $test->mech;
-my $c    = $test->c;
+This test checks that the artist create form works properly with both complete
+and minimal data, plus some edge cases.
 
-MusicBrainz::Server::Test->prepare_test_database($c, '+controller_artist');
-
-# Test creating new artists via the create artist form
-$mech->get_ok('/login');
-$mech->submit_form( with_fields => { username => 'new_editor', password => 'password' } );
+=cut
 
 my $artist_page_regexp = qr{/artist/([a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12})};
 
-subtest 'Create artists with all fields' => sub {
+test 'Test creating artist with most fields' => sub {
+    my $test = shift;
+    my $mech = $test->mech;
+
+    prepare_test($test);
+
     $mech->get_ok('/artist/create');
     html_ok($mech->content);
-    $mech->submit_form(
-        with_fields => {
-            'edit-artist.name' => 'controller artist',
-            'edit-artist.sort_name' => 'artist, controller',
-            'edit-artist.type_id' => 1,
-            'edit-artist.area_id' => 222,
-            'edit-artist.gender_id' => 2,
-            'edit-artist.period.begin_date.year' => 1990,
-            'edit-artist.period.begin_date.month' => 1,
-            'edit-artist.period.begin_date.day' => 2,
-            'edit-artist.begin_area_id' => 222,
-            'edit-artist.period.end_date.year' => 2003,
-            'edit-artist.period.end_date.month' => 4,
-            'edit-artist.period.end_date.day' => 15,
-            'edit-artist.end_area_id' => 222,
-            'edit-artist.comment' => 'artist created in controller_artist.t',
-        }
-    );
-    ok($mech->success);
-    ok($mech->uri =~ $artist_page_regexp, 'should redirect to artist page via gid');
 
-    my $edit = MusicBrainz::Server::Test->get_latest_edit($c);
+    my @edits = capture_edits {
+        $mech->submit_form_ok({
+            with_fields => {
+                'edit-artist.name' => 'controller artist',
+                'edit-artist.sort_name' => 'artist, controller',
+                'edit-artist.type_id' => 1,
+                'edit-artist.area_id' => 222,
+                'edit-artist.gender_id' => 2,
+                'edit-artist.period.begin_date.year' => 1990,
+                'edit-artist.period.begin_date.month' => 1,
+                'edit-artist.period.begin_date.day' => 2,
+                'edit-artist.begin_area_id' => 222,
+                'edit-artist.period.end_date.year' => 2003,
+                'edit-artist.period.end_date.month' => 4,
+                'edit-artist.period.end_date.day' => 15,
+                'edit-artist.end_area_id' => 222,
+                'edit-artist.comment' => 'artist created in controller_artist.t',
+            },
+        },
+        'The form returned a 2xx response code')
+    } $test->c;
+
+    ok(
+        $mech->uri =~ $artist_page_regexp,
+        'The user is redirected to the artist page after entering the edit',
+    );
+
+    is(@edits, 1, 'The edit was entered');
+
+    my $edit = shift(@edits);
+
     isa_ok($edit, 'MusicBrainz::Server::Edit::Artist::Create');
-    is_deeply($edit->data, {
-        name => 'controller artist',
-        sort_name => 'artist, controller',
-        type_id => 1,
-        area_id => 222,
-        gender_id => 2,
-        comment => 'artist created in controller_artist.t',
-        begin_date => {
-            year => 1990,
-            month => 1,
-            day => 2
+    is_deeply(
+        $edit->data,
+        {
+            name => 'controller artist',
+            sort_name => 'artist, controller',
+            type_id => 1,
+            area_id => 222,
+            gender_id => 2,
+            comment => 'artist created in controller_artist.t',
+            begin_date => {
+                year => 1990,
+                month => 1,
+                day => 2
+            },
+            begin_area_id => 222,
+            end_date => {
+                year => 2003,
+                month => 4,
+                day => 15
+            },
+            end_area_id => 222,
+            ended => 1,
+            ipi_codes => [],
+            isni_codes => [],
         },
-        begin_area_id => 222,
-        end_date => {
-            year => 2003,
-            month => 4,
-            day => 15
-        },
-        end_area_id => 222,
-        ended => 1,
-        ipi_codes => [],
-        isni_codes => [],
-    });
+        'The edit contains the right data',
+    );
 
 
     # Test display of edit data
-    $mech->get_ok('/edit/' . $edit->id, 'Fetch the edit page');
+    $mech->get_ok('/edit/' . $edit->id, 'Fetched the edit page');
     html_ok($mech->content);
-    $mech->content_contains('controller artist', '.. contains artist name');
-    $mech->content_contains('artist, controller', '.. contains sort name');
-    $mech->content_contains('Person', '.. contains artist type');
-    $mech->content_contains('United States', '.. contains area');
-    $mech->content_contains('Female', '.. contains artist gender');
-    $mech->content_contains('artist created in controller_artist.t',
-                             '.. contains artist comment');
-    $mech->content_contains('1990-01-02', '.. contains begin date');
-    $mech->content_contains('2003-04-15', '.. contains end date');
-
-    # Cleaning up.
-    _delete_artist($c, $edit->entity_id);
-
-    done_testing;
+    $mech->content_contains(
+        'controller artist',
+        'The edit page contains the artist name',
+    );
+    $mech->content_contains(
+        'artist, controller',
+        'The edit page contains the artist sort name',
+    );
+    $mech->content_contains(
+        'Person',
+        'The edit page contains the artist type',
+    );
+    $mech->content_contains(
+        'United States',
+        'The edit page contains the area',
+    );
+    $mech->content_contains(
+        'Female',
+        'The edit page contains the artist gender',
+    );
+    $mech->content_contains(
+        'artist created in controller_artist.t',
+        'The edit page contains the disambiguation',
+    );
+    $mech->content_contains(
+        '1990-01-02',
+        'The edit page contains the artist begin date',
+    );
+    $mech->content_contains(
+        '2003-04-15',
+        'The edit page contains the artist end date',
+    );
 };
 
-subtest 'Creating artists with only the minimal amount of fields' => sub {
+test 'Test creating artists with only the minimal amount of fields' => sub {
+    my $test = shift;
+    my $mech = $test->mech;
+
+    prepare_test($test);
+
     $mech->get_ok('/artist/create');
     html_ok($mech->content);
-    $mech->submit_form(
-        with_fields => {
-            'edit-artist.name' => 'Alice Artist',
-            'edit-artist.sort_name' => 'Artist, Alice',
-            'edit-artist.type_id' => '',
-            'edit-artist.area_id' => '',
-            'edit-artist.gender_id' => '',
-            'edit-artist.period.begin_date.year' => '',
-            'edit-artist.period.begin_date.month' => '',
-            'edit-artist.period.begin_date.day' => '',
-            'edit-artist.end_area_id' => '',
-            'edit-artist.period.end_date.year' => '',
-            'edit-artist.period.end_date.month' => '',
-            'edit-artist.period.end_date.day' => '',
-            'edit-artist.begin_area_id' => '',
-            'edit-artist.comment' => '',
-        }
-    );
-    ok($mech->success);
-    ok($mech->uri =~ $artist_page_regexp, 'should redirect to artist page via gid');
 
-    my $edit = MusicBrainz::Server::Test->get_latest_edit($c);
+    my @edits = capture_edits {
+        $mech->submit_form_ok({
+            with_fields => {
+                'edit-artist.name' => 'Alice Artist',
+                'edit-artist.sort_name' => 'Artist, Alice',
+            },
+        },
+        'The form returned a 2xx response code')
+    } $test->c;
+
+    ok(
+        $mech->uri =~ $artist_page_regexp,
+        'The user is redirected to the artist page after entering the edit',
+    );
+
+    is(@edits, 1, 'The edit was entered');
+
+    my $edit = shift(@edits);
+
     isa_ok($edit, 'MusicBrainz::Server::Edit::Artist::Create');
 
-    is($edit->data->{name}, 'Alice Artist');
-    is($edit->data->{sort_name}, 'Artist, Alice');
-    is($edit->data->{type_id}, undef);
-    is($edit->data->{area_id}, undef);
-    is($edit->data->{begin_area_id}, undef);
-    is($edit->data->{end_area_id}, undef);
-    is($edit->data->{gender_id}, undef);
-    is($edit->data->{comment}, '');
-
-    ok( PartialDate->new($edit->data->{begin_date})->is_empty );
-    ok( PartialDate->new($edit->data->{end_date})->is_empty );
+    is(
+        $edit->data->{name},
+        'Alice Artist',
+        'The edit data contains the right name',
+    );
+    is(
+        $edit->data->{sort_name},
+        'Artist, Alice',
+        'The edit data contains the right sort name',
+    );
+    is($edit->data->{type_id}, undef, 'The edit data has no type ID');
+    is($edit->data->{area_id}, undef, 'The edit data has no area');
+    is(
+        $edit->data->{begin_area_id},
+        undef,
+        'The edit data has no begin area',
+    );
+    is($edit->data->{end_area_id}, undef, 'The edit data has no end area');
+    is($edit->data->{gender_id}, undef, 'The edit data has no gender');
+    is(
+        $edit->data->{comment},
+        '',
+        'The edit data has an empty disambiguation',
+    );
+    ok(
+        PartialDate->new($edit->data->{begin_date})->is_empty,
+        'The edit data has an empty begin date'
+    );
+    ok(
+        PartialDate->new($edit->data->{end_date})->is_empty,
+        'The edit data has an empty end date'
+    );
 
     # Test display of edit data
-    $mech->get_ok('/edit/' . $edit->id, 'Fetch the edit page');
+    $mech->get_ok('/edit/' . $edit->id, 'Fetched the edit page');
     html_ok($mech->content);
-    $mech->content_contains('Alice Artist', '.. contains artist name');
-    $mech->content_contains('Artist, Alice', '.. contains sort name');
-
-    # Cleaning up.
-    _delete_artist($c, $edit->entity_id);
-
-    done_testing;
+    $mech->content_contains(
+        'Alice Artist',
+        'The edit page contains the artist name',
+    );
+    $mech->content_contains(
+        'Artist, Alice',
+        'The edit page contains the artist sort name',
+    );
 };
 
-subtest 'MBS-10976: No ISE if only invalid characters are submitted' => sub {
+test 'MBS-10976: No ISE if only invalid characters are submitted' => sub {
+    my $test = shift;
+    my $mech = $test->mech;
+
+    prepare_test($test);
+
     $mech->get_ok('/artist/create');
 
     my $invalid = "\x{200B}\x{00AD}\x{FEFF}";
 
-    $mech->submit_form(
-        with_fields => {
-            'edit-artist.name' => $invalid,
-            'edit-artist.sort_name' => $invalid,
-        }
+    my @edits = capture_edits {
+        $mech->submit_form_ok({
+            with_fields => {
+                'edit-artist.name' => $invalid,
+                'edit-artist.sort_name' => $invalid,
+            },
+        },
+        'The form returned a 2xx response code')
+    } $test->c;
+
+    ok(
+        $mech->uri =~ qr{/artist/create$},
+        'The add artist page is shown again',
     );
-    ok($mech->success);
-    ok($mech->uri =~ qr{/artist/create$}, 'is still on the create page');
+
+    is(@edits, 0, 'No edit was entered');
 
     $mech->content_contains(
         'The characters you’ve entered are invalid or not allowed.',
         'contains error for invalid characters',
     );
-
-    done_testing;
 };
 
-subtest 'MBS-10976: Private use characters U+E000..U+F8FF are allowed' => sub {
+test 'MBS-10976: Private use characters U+E000..U+F8FF are allowed' => sub {
+    my $test = shift;
+    my $mech = $test->mech;
+
+    prepare_test($test);
+
     $mech->get_ok('/artist/create');
 
     my $klingon = "\x{F8D3}\x{F8D4}\x{F8D5}";
     my $other_private_use = "\x{E000}\x{F8FF}";
 
-    $mech->submit_form(
-        with_fields => {
-            'edit-artist.name' => $klingon,
-            'edit-artist.sort_name' => $other_private_use,
-        }
+    my @edits = capture_edits {
+        $mech->submit_form_ok({
+            with_fields => {
+                'edit-artist.name' => $klingon,
+                'edit-artist.sort_name' => $other_private_use,
+            },
+        },
+        'The form returned a 2xx response code')
+    } $test->c;
+
+    ok(
+        $mech->uri =~ $artist_page_regexp,
+        'The user is redirected to the artist page after entering the edit',
     );
-    ok($mech->uri =~ $artist_page_regexp, 'should redirect to artist page via gid');
 
-    my $edit = MusicBrainz::Server::Test->get_latest_edit($c);
-    is($edit->data->{name}, $klingon);
-    is($edit->data->{sort_name}, $other_private_use);
+    is(@edits, 1, 'The edit was entered');
 
-    # Cleaning up.
-    _delete_artist($c, $edit->entity_id);
+    my $edit = shift(@edits);
 
-    done_testing;
+    is(
+        $edit->data->{name},
+        $klingon,
+        'The edit data contains the right name',
+    );
+    is(
+        $edit->data->{sort_name},
+        $other_private_use,
+        'The edit data contains the right sort name',
+    );
 };
 
-};
+sub prepare_test {
+    my $test = shift;
 
-sub _delete_artist
-{
-    my $c = shift;
-    $c->sql->do('DELETE FROM editor_subscribe_artist WHERE artist IN (?)', @_);
-    $c->sql->do('DELETE FROM artist WHERE id IN (?)', @_);
+    MusicBrainz::Server::Test->prepare_test_database(
+        $test->c,
+        '+controller_artist',
+    );
+
+    $test->mech->get('/login');
+    $test->mech->submit_form(
+        with_fields => { username => 'new_editor', password => 'password' }
+    );
 }
 
 1;
