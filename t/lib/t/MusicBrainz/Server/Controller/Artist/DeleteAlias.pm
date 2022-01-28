10c7af10fe8847c9d28847c7e92e36b612135346
Nicolás Tamargo <reosarevok@gmail.com>
2022-01-28 21:03:17 +0200

    Also check edit note is required on Artist::DeleteAlias

--- t/lib/t/MusicBrainz/Server/Controller/Artist/DeleteAlias.pm
+++ t/lib/t/MusicBrainz/Server/Controller/Artist/DeleteAlias.pm
@@ -1,13 +1,14 @@
 package t::MusicBrainz::Server::Controller::Artist::DeleteAlias;
 use Test::Routine;
 use Test::More;
-use MusicBrainz::Server::Test qw( html_ok );
+use MusicBrainz::Server::Test qw( capture_edits html_ok );
 
 with 't::Mechanize', 't::Context';
 
 =head2 Test description
 
-This test checks that artist alias deletion works.
+This test checks that artist alias deletion works, and that it requires
+an edit note.
 
 =cut
 
@@ -25,15 +26,21 @@ test 'Test alias deletion' => sub {
     $mech->submit_form( with_fields => { username => 'new_editor', password => 'password' } );
 
     $mech->get_ok('/artist/745c079d-374e-4436-9448-da92dedef3ce/alias/1/delete');
-    $mech->submit_form_ok({
-            with_fields => {
-                'confirm.edit_note' => q(Some edit note since it's required)
-            }
-        },
-        'The form returned a 2xx response code',
-    );
+    my @edits = capture_edits {
+        $mech->submit_form_ok({
+                with_fields => {
+                    'confirm.edit_note' =>
+                        q(Some edit note since it's required)
+                }
+            },
+            'The form returned a 2xx response code',
+        );
+    } $c;
 
-    my $edit = MusicBrainz::Server::Test->get_latest_edit($c);
+    is(@edits, 1, 'The edit was entered');
+
+    my $edit = shift(@edits);
+
     isa_ok($edit, 'MusicBrainz::Server::Edit::Artist::DeleteAlias');
     is_deeply(
         $edit->data,
@@ -72,6 +79,38 @@ test 'Test alias deletion' => sub {
     $mech->content_contains(
         'Test Alias',
         'The edit page contains the alias name',
+    );
+};
+
+test 'Test edit note is required' => sub {
+    my $test = shift;
+    my $mech = $test->mech;
+    my $c    = $test->c;
+
+    MusicBrainz::Server::Test->prepare_test_database(
+        $c,
+        '+controller_artist',
+    );
+
+    $mech->get_ok('/login');
+    $mech->submit_form( with_fields => { username => 'new_editor', password => 'password' } );
+
+    $mech->get_ok('/artist/745c079d-374e-4436-9448-da92dedef3ce/alias/1/delete');
+    my @edits = capture_edits {
+        $mech->submit_form_ok({
+                with_fields => {
+                    'confirm.edit_note' => ''
+                }
+            },
+            'The form returned a 2xx response code',
+        );
+    } $c;
+
+    is(@edits, 0, 'No edit was entered');
+
+    $mech->content_contains(
+        'You must provide an edit note',
+        'Contains warning about edit note being required',
     );
 };
 
