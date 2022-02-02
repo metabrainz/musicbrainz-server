4c0fb81ce23a5dc940120f4343b9d6843e5065fd
Nicolás Tamargo <reosarevok@gmail.com>
2022-02-02 22:15:12 +0200

    Document Artist::Works test

--- t/lib/t/MusicBrainz/Server/Controller/Artist/Works.pm
+++ t/lib/t/MusicBrainz/Server/Controller/Artist/Works.pm
@@ -4,21 +4,40 @@ use MusicBrainz::Server::Test qw( html_ok );
 
 with 't::Mechanize', 't::Context';
 
+=head2 Test description
+
+This test checks whether the artist releases page properly displays
+works for the artist.
+
+=cut
+
 test all => sub {
+    my $test = shift;
+    my $mech = $test->mech;
+    my $c    = $test->c;
 
-my $test = shift;
-my $mech = $test->mech;
-my $c    = $test->c;
+    MusicBrainz::Server::Test->prepare_test_database(
+      $c,
+      '+controller_artist',
+    );
 
-MusicBrainz::Server::Test->prepare_test_database($c, '+controller_artist');
-
-$mech->get_ok('/artist/745c079d-374e-4436-9448-da92dedef3ce/works', 'get Test Artist works page');
-html_ok($mech->content);
-$mech->title_like(qr/Test Artist/, 'title has artist');
-$mech->title_like(qr/works/i, 'title indicates works listing');
-$mech->content_contains('Test Work');
-$mech->content_contains('/work/745c079d-374e-4436-9448-da92dedef3ce', 'has a link to the work');
-
+    $mech->get_ok(
+      '/artist/745c079d-374e-4436-9448-da92dedef3ce/works',
+      'Fetched the artists works page');
+    html_ok($mech->content);
+    $mech->title_like(
+        qr/Test Artist/,
+        'The page title contains Test Artist',
+    );
+    $mech->title_like(
+        qr/works/i,
+        'The page title indicates this is a works listing',
+    );
+    $mech->content_contains('Test Work', 'The work name is listed');
+    $mech->content_contains(
+        '/work/745c079d-374e-4436-9448-da92dedef3ce',
+        'A link to the work is present',
+    );
 };
 
 1;
