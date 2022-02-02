9f1893d478e61e98c1584f46bd448531c975cbc7
Nicolás Tamargo <reosarevok@gmail.com>
2022-02-02 20:17:49 +0200

    Document Artist::Ratings test

--- t/lib/t/MusicBrainz/Server/Controller/Artist/Ratings.pm
+++ t/lib/t/MusicBrainz/Server/Controller/Artist/Ratings.pm
@@ -5,32 +5,65 @@ use MusicBrainz::Server::Test qw( html_ok test_xpath_html );
 
 with 't::Mechanize', 't::Context';
 
-test all => sub {
+=head2 Test description
 
-my $test = shift;
-my $mech = $test->mech;
-my $c    = $test->c;
+This test checks whether artist ratings are properly displayed on the ratings
+page, and that private ratings are hidden.
 
-MusicBrainz::Server::Test->prepare_test_database($c, '+controller_artist');
-MusicBrainz::Server::Test->prepare_raw_test_database($c, '
-INSERT INTO artist_tag_raw (artist, editor, tag) VALUES (3, 1, 1), (3, 2, 1);
-INSERT INTO artist_rating_raw (artist, editor, rating) VALUES (3, 1, 20);
-INSERT INTO artist_rating_raw (artist, editor, rating) VALUES (3, 2, 100);
-');
+=cut
 
-# Test ratings
-$mech->get_ok('/artist/745c079d-374e-4436-9448-da92dedef3ce/ratings', 'get artist ratings');
-html_ok($mech->content);
-$mech->content_contains('new_editor');
-$mech->content_lacks('alice');
+test 'Test artists rating page' => sub {
+    my $test = shift;
+    my $mech = $test->mech;
+    my $c = $test->c;
 
-my $tx = test_xpath_html($mech->content);
-$tx->is('count(//li//span[@class="inline-rating"])', 1, '1 rating is shown');
-$tx->is('(//span[@class="current-rating"])[1]', 1, 'user rating is shown');
-$tx->is('(//span[@class="current-rating"])[2]', 3.5, 'average rating is shown');
+    MusicBrainz::Server::Test->prepare_test_database(
+      $c,
+      '+controller_artist',
+    );
 
-$mech->content_contains('1 private rating not listed');
+    MusicBrainz::Server::Test->prepare_raw_test_database($c, <<~'SQL');
+        INSERT INTO artist_tag_raw (artist, editor, tag) 
+             VALUES (3, 1, 1), (3, 2, 1);
+        INSERT INTO artist_rating_raw (artist, editor, rating)
+             VALUES (3, 1, 20), (3, 2, 100);
+        SQL
 
+    $mech->get_ok(
+      '/artist/745c079d-374e-4436-9448-da92dedef3ce/ratings',
+      'Fetched artist ratings',
+    );
+    html_ok($mech->content);
+    $mech->content_contains(
+        'new_editor',
+        'The editor who gave a public rating is named',
+    );
+    $mech->content_lacks(
+        'alice',
+        'The editor who gave a private rating is not named',
+    );
+
+    my $tx = test_xpath_html($mech->content);
+    $tx->is(
+        'count(//li//span[@class="inline-rating"])',
+        1,
+        'One user rating is shown'
+    );
+    $tx->is(
+        '(//span[@class="current-rating"])[1]',
+        1,
+        'The public user rating is shown',
+    );
+    $tx->is(
+        '(//span[@class="current-rating"])[2]',
+        3.5,
+        'The average rating is shown',
+    );
+
+    $mech->content_contains(
+        '1 private rating not listed',
+        'The notice about a private rating is present',
+    );
 };
 
 1;
