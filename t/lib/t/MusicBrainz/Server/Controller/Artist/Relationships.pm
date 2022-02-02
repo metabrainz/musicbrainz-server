922bda3dbd1911ec790d5e8c099c46f2ccda73d3
Nicolás Tamargo <reosarevok@gmail.com>
2022-02-02 20:45:26 +0200

    Document Artist::Relationships test
    And add a check for the relationship type name.

--- t/lib/t/MusicBrainz/Server/Controller/Artist/Relationships.pm
+++ t/lib/t/MusicBrainz/Server/Controller/Artist/Relationships.pm
@@ -4,44 +4,64 @@ use MusicBrainz::Server::Test qw( html_ok page_test_jsonld );
 
 with 't::Mechanize', 't::Context';
 
-test all => sub {
+=head2 Test description
 
-my $test = shift;
-my $mech = $test->mech;
-my $c    = $test->c;
+This test checks whether the artist relationships page properly displays
+relationships for the artist, and also the expected JSON-LD data.
 
-MusicBrainz::Server::Test->prepare_test_database($c, '+controller_artist');
+=cut
 
-# Test relationships
-$mech->get_ok('/artist/745c079d-374e-4436-9448-da92dedef3ce/relationships', 'get artist relationships');
-html_ok($mech->content);
-$mech->content_contains('guitar');
-$mech->content_contains('/recording/123c079d-374e-4436-9448-da92dedef3ce');
+test 'Test artists relationships page' => sub {
+    my $test = shift;
+    my $mech = $test->mech;
+    my $c    = $test->c;
 
-page_test_jsonld $mech => {
-    'location' => {
-        'name' => 'United Kingdom',
-        '@id' => 'http://musicbrainz.org/area/8a754a16-0027-3a29-b6d7-2b40ea0481ed',
-        '@type' => 'Country'
-    },
-    '@context' => 'http://schema.org',
-    '@type' => ['Person', 'MusicGroup'],
-    'birthDate' => '2008-01-02',
-    'birthPlace' => {
-        '@type' => 'Country',
-        '@id' => 'http://musicbrainz.org/area/8a754a16-0027-3a29-b6d7-2b40ea0481ed',
-        'name' => 'United Kingdom'
-    },
-    'name' => 'Test Artist',
-    'deathDate' => '2009-03-04',
-    'deathPlace' => {
-        'name' => 'United Kingdom',
-        '@id' => 'http://musicbrainz.org/area/8a754a16-0027-3a29-b6d7-2b40ea0481ed',
-        '@type' => 'Country'
-    },
-    '@id' => 'http://musicbrainz.org/artist/745c079d-374e-4436-9448-da92dedef3ce'
-};
+    MusicBrainz::Server::Test->prepare_test_database(
+        $c,
+        '+controller_artist',
+    );
 
+    $mech->get_ok(
+        '/artist/745c079d-374e-4436-9448-da92dedef3ce/relationships',
+        'Fetched artist relationships page',
+    );
+    html_ok($mech->content);
+    $mech->content_contains(
+        'instruments',
+        'The relationship type is listed',
+    );
+    $mech->content_contains(
+        'guitar',
+        'The relationship attribute is listed',
+    );
+    $mech->content_contains(
+        '/recording/123c079d-374e-4436-9448-da92dedef3ce',
+        'A link to the related entity is present'
+    );
+
+    page_test_jsonld $mech => {
+        'location' => {
+            'name' => 'United Kingdom',
+            '@id' => 'http://musicbrainz.org/area/8a754a16-0027-3a29-b6d7-2b40ea0481ed',
+            '@type' => 'Country'
+        },
+        '@context' => 'http://schema.org',
+        '@type' => ['Person', 'MusicGroup'],
+        'birthDate' => '2008-01-02',
+        'birthPlace' => {
+            '@type' => 'Country',
+            '@id' => 'http://musicbrainz.org/area/8a754a16-0027-3a29-b6d7-2b40ea0481ed',
+            'name' => 'United Kingdom'
+        },
+        'name' => 'Test Artist',
+        'deathDate' => '2009-03-04',
+        'deathPlace' => {
+            'name' => 'United Kingdom',
+            '@id' => 'http://musicbrainz.org/area/8a754a16-0027-3a29-b6d7-2b40ea0481ed',
+            '@type' => 'Country'
+        },
+        '@id' => 'http://musicbrainz.org/artist/745c079d-374e-4436-9448-da92dedef3ce'
+    };
 };
 
 1;
