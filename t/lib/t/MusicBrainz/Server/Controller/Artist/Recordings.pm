e3af63bd1eacb7216e96f726398a9bb815c46b6f
Nicolás Tamargo <reosarevok@gmail.com>
2022-02-02 20:26:58 +0200

    Document Artist::Recordings test

--- t/lib/t/MusicBrainz/Server/Controller/Artist/Recordings.pm
+++ t/lib/t/MusicBrainz/Server/Controller/Artist/Recordings.pm
@@ -4,54 +4,72 @@ use MusicBrainz::Server::Test qw( html_ok page_test_jsonld );
 
 with 't::Mechanize', 't::Context';
 
-test all => sub {
+=head2 Test description
 
-my $test = shift;
-my $mech = $test->mech;
-my $c    = $test->c;
+This test checks whether the artist recordings page properly displays
+recordings for the artist, both on the site itself and on the JSON-LD data.
 
-MusicBrainz::Server::Test->prepare_test_database($c, '+controller_artist');
+=cut
 
-# Test /artist/gid/recordings
-$mech->get_ok('/artist/745c079d-374e-4436-9448-da92dedef3ce/recordings', 'get Test Artist page');
-html_ok($mech->content);
-$mech->title_like(qr/Test Artist/, 'title has Test Artist');
-$mech->title_like(qr/recordings/i, 'title indicates recordings listing');
-$mech->content_contains('Test Recording');
-$mech->content_contains('2:03');
-$mech->content_contains('/recording/123c079d-374e-4436-9448-da92dedef3ce', 'has a link to the recording');
+test 'Test artists recording page' => sub {
+    my $test = shift;
+    my $mech = $test->mech;
+    my $c    = $test->c;
 
-page_test_jsonld $mech => {
-    'birthDate' => '2009-03-04',
-    'track' => {
-        'duration' => 'PT02M03S',
-        '@type' => 'MusicRecording',
-        '@id' => 'http://musicbrainz.org/recording/123c079d-374e-4436-9448-da92dedef3ce',
-        'name' => 'Test Recording'
-    },
-    'name' => 'Test Artist',
-    'location' => {
-        'name' => 'United Kingdom',
-        '@type' => 'Country',
-        '@id' => 'http://musicbrainz.org/area/8a754a16-0027-3a29-b6d7-2b40ea0481ed'
-    },
-    '@type' => ['Person', 'MusicGroup'],
-    '@context' => 'http://schema.org',
-    'birthDate' => '2008-01-02',
-    'birthPlace' => {
-        '@id' => 'http://musicbrainz.org/area/8a754a16-0027-3a29-b6d7-2b40ea0481ed',
-        '@type' => 'Country',
-        'name' => 'United Kingdom'
-    },
-    'deathDate' => '2009-03-04',
-    'deathPlace' => {
-        '@type' => 'Country',
-        '@id' => 'http://musicbrainz.org/area/8a754a16-0027-3a29-b6d7-2b40ea0481ed',
-        'name' => 'United Kingdom'
-    },
-    '@id' => 'http://musicbrainz.org/artist/745c079d-374e-4436-9448-da92dedef3ce'
-};
+    MusicBrainz::Server::Test->prepare_test_database(
+        $c,
+        '+controller_artist');
 
+    $mech->get_ok(
+        '/artist/745c079d-374e-4436-9448-da92dedef3ce/recordings',
+        'Fetched artist recordings page',
+    );
+    html_ok($mech->content);
+    $mech->title_like(
+        qr/Test Artist/,
+        'The page title contains Test Artist',
+    );
+    $mech->title_like(
+        qr/recordings/i,
+        'The page title indicates this is a recordings listing',
+    );
+    $mech->content_contains('Test Recording', 'The recording name is listed');
+    $mech->content_contains('2:03', 'The recording length is listed');
+    $mech->content_contains(
+        '/recording/123c079d-374e-4436-9448-da92dedef3ce',
+        'A link to the recording is present',
+    );
+
+    page_test_jsonld $mech => {
+        'birthDate' => '2009-03-04',
+        'track' => {
+            'duration' => 'PT02M03S',
+            '@type' => 'MusicRecording',
+            '@id' => 'http://musicbrainz.org/recording/123c079d-374e-4436-9448-da92dedef3ce',
+            'name' => 'Test Recording'
+        },
+        'name' => 'Test Artist',
+        'location' => {
+            'name' => 'United Kingdom',
+            '@type' => 'Country',
+            '@id' => 'http://musicbrainz.org/area/8a754a16-0027-3a29-b6d7-2b40ea0481ed'
+        },
+        '@type' => ['Person', 'MusicGroup'],
+        '@context' => 'http://schema.org',
+        'birthDate' => '2008-01-02',
+        'birthPlace' => {
+            '@id' => 'http://musicbrainz.org/area/8a754a16-0027-3a29-b6d7-2b40ea0481ed',
+            '@type' => 'Country',
+            'name' => 'United Kingdom'
+        },
+        'deathDate' => '2009-03-04',
+        'deathPlace' => {
+            '@type' => 'Country',
+            '@id' => 'http://musicbrainz.org/area/8a754a16-0027-3a29-b6d7-2b40ea0481ed',
+            'name' => 'United Kingdom'
+        },
+        '@id' => 'http://musicbrainz.org/artist/745c079d-374e-4436-9448-da92dedef3ce'
+    };
 };
 
 1;
