f3e231157e0f3542c00a7a5d2aaf4ddd732e0d75
Nicolás Tamargo <reosarevok@gmail.com>
2022-02-16 13:11:02 +0200

    MBS-12202: Allow parens on JazzMusicArchives URLs

--- root/static/scripts/edit/URLCleanup.js
+++ root/static/scripts/edit/URLCleanup.js
@@ -2419,7 +2419,7 @@ const CLEANUPS: CleanupEntries = {
       return url;
     },
     validate: function (url, id) {
-      const m = /^https:\/\/www\.jazzmusicarchives\.com\/(\w+)\/(?:[\w%-]+\/)?[\w%-]*$/.exec(url);
+      const m = /^https:\/\/www\.jazzmusicarchives\.com\/(\w+)\/(?:[\w%()-]+\/)?[\w%()-]*$/.exec(url);
       if (m) {
         const type = m[1];
         switch (id) {
