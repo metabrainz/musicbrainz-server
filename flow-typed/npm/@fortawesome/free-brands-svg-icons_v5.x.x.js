e6299484987c5fd3c2dec232b50e1205fafb5260
Michael Wiencek <mwtuea@gmail.com>
2022-02-26 17:45:14 -0600

    Add fontawesome Flow types
    These were converted to Flow syntax by hand from the official TypeScript
    types.

---
+++ flow-typed/npm/@fortawesome/free-brands-svg-icons_v5.x.x.js
@@ -0,0 +1,10 @@
+// @flow strict
+
+declare module '@fortawesome/free-brands-svg-icons' {
+  import type {
+    IconLookup,
+  } from '@fortawesome/fontawesome-common-types';
+
+  declare export var faGooglePlay: IconLookup;
+  declare export var faAndroid: IconLookup;
+}
