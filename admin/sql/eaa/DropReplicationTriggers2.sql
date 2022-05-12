17b4c059e501a8638c8fc87bd910ae04ce546b35
Michael Wiencek <mwtuea@gmail.com>
2022-05-12 17:16:55 -0500

    Generate DropReplicationTriggers2.sql for non-musicbrainz schemas

---
+++ admin/sql/eaa/DropReplicationTriggers2.sql
@@ -0,0 +1,8 @@
+-- Automatically generated, do not edit.
+\unset ON_ERROR_STOP
+
+SET search_path = 'event_art_archive';
+
+DROP TRIGGER reptg2_art_type ON art_type;
+DROP TRIGGER reptg2_event_art ON event_art;
+DROP TRIGGER reptg2_event_art_type ON event_art_type;
