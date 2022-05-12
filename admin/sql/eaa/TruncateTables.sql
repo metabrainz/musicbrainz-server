9ab838720491a4ebfa285286c34fd34d78cb1d19
Michael Wiencek <mwtuea@gmail.com>
2022-05-12 17:25:40 -0500

    Generate TruncateTables.sql for non-musicbrainz schemas

---
+++ admin/sql/eaa/TruncateTables.sql
@@ -0,0 +1,8 @@
+-- Automatically generated, do not edit.
+\set ON_ERROR_STOP 1
+
+SET search_path = 'event_art_archive';
+
+TRUNCATE TABLE art_type RESTART IDENTITY CASCADE;
+TRUNCATE TABLE event_art RESTART IDENTITY CASCADE;
+TRUNCATE TABLE event_art_type RESTART IDENTITY CASCADE;
