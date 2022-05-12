aea3a1c80f350f7efef84726f8be5127d662bca4
Michael Wiencek <mwtuea@gmail.com>
2022-05-12 13:05:40 -0500

    Bump old DB_SCHEMA_SEQUENCE in CheckSchemaMigration.sh

--- t/script/CheckSchemaMigration.sh
+++ t/script/CheckSchemaMigration.sh
@@ -56,7 +56,7 @@ git restore admin/sql
 git restore admin/InitDb.pl
 
 export REPLICATION_TYPE
-DB_SCHEMA_SEQUENCE=25 DATABASE=$DB2 ./upgrade.sh
+DB_SCHEMA_SEQUENCE=26 DATABASE=$DB2 ./upgrade.sh
 
 DB1SCHEMA="$DB1.schema.sql"
 DB2SCHEMA="$DB2.schema.sql"
