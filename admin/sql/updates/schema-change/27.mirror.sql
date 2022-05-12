4aeacfb1297f46c6f38457a655eb5cb618a04ee9
Michael Wiencek <mwtuea@gmail.com>
2022-05-12 15:11:34 -0500

    MBS-12225 SQL fixes

--- admin/sql/updates/schema-change/27.mirror.sql
+++ admin/sql/updates/schema-change/27.mirror.sql
@@ -5,6 +5,7 @@
 -- 20210606-mbs-11682.sql
 -- 20220207-mbs-12224-mirror.sql
 -- 20220218-mbs-12208.sql
+-- 20220302-mbs-12225.sql
 -- 20220314-mbs-12252.sql
 -- 20220314-mbs-12253.sql
 -- 20220314-mbs-12254.sql
@@ -604,6 +605,46 @@ BEGIN
         END IF;
     END IF;
 END $$;
+
+--------------------------------------------------------------------------------
+SELECT '20220302-mbs-12225.sql';
+
+
+-- The internal comment had the word "slaves" changed to "mirrors."
+-- The function is otherwise unchanged.
+CREATE OR REPLACE FUNCTION set_recordings_first_release_dates(recording_ids INTEGER[])
+RETURNS VOID AS $$
+BEGIN
+  -- DO NOT modify any replicated tables in this function; it's used
+  -- by a trigger on mirrors.
+  DELETE FROM recording_first_release_date
+  WHERE recording = ANY(recording_ids);
+
+  INSERT INTO recording_first_release_date
+  SELECT * FROM get_recording_first_release_date_rows(
+    format('track.recording = any(%L)', recording_ids)
+  );
+END;
+$$ LANGUAGE 'plpgsql' STRICT;
+
+-- The internal comment had the word "slaves" changed to "mirrors."
+-- The function is otherwise unchanged.
+CREATE OR REPLACE FUNCTION set_release_first_release_date(release_id INTEGER)
+RETURNS VOID AS $$
+BEGIN
+  -- DO NOT modify any replicated tables in this function; it's used
+  -- by a trigger on mirrors.
+  DELETE FROM release_first_release_date
+  WHERE release = release_id;
+
+  INSERT INTO release_first_release_date
+  SELECT * FROM get_release_first_release_date_rows(
+    format('release = %L', release_id)
+  );
+
+  INSERT INTO artist_release_pending_update VALUES (release_id);
+END;
+$$ LANGUAGE 'plpgsql' STRICT;
 
 --------------------------------------------------------------------------------
 SELECT '20220314-mbs-12252.sql';
