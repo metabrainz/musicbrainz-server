37c614858c2a9dc93a674b7300d1dea94ff01f85
Nicolás Tamargo <reosarevok@gmail.com>
2022-05-31 14:52:01 +0300

    MBS-12376: Don't show spammers on area pages
    When searching for users in a specific area, it's probably best
    not to show "Online Medical Marijuana Card in DC" or whatnot.

--- lib/MusicBrainz/Server/Data/Role/Area.pm
+++ lib/MusicBrainz/Server/Data/Role/Area.pm
@@ -1,5 +1,6 @@
 package MusicBrainz::Server::Data::Role::Area;
 use Moose::Role;
+use MusicBrainz::Server::Constants qw( $SPAMMER_FLAG );
 use MusicBrainz::Server::Data::Utils qw( get_area_containment_join );
 
 requires '_columns', '_table', '_area_columns';
@@ -15,16 +16,23 @@ sub find_by_area {
     my $table = $self->can('_find_by_area_table') ? $self->_find_by_area_table : $self->_table;
     my $columns = $self->_columns;
     my $order_by = $self->can('_find_by_area_order') ? $self->_find_by_area_order : 'name, id';
+    my $extra_conditions;
+
+    if ($table eq 'editor') {
+        $extra_conditions = " AND (editor.privs & $SPAMMER_FLAG) = 0";
+    }
 
     my $query = <<~SQL;
         SELECT $columns
           FROM $table
          WHERE \$1 $area_cols_condition
+               $extra_conditions
          UNION
         SELECT $columns
           FROM $table
           JOIN $area_containment_join ac ON ac.descendant $area_cols_condition
          WHERE ac.parent = \$1
+               $extra_conditions
          ORDER BY $order_by
         SQL
 
--- t/lib/t/MusicBrainz/Server/Controller/Area/Users.pm
+++ t/lib/t/MusicBrainz/Server/Controller/Area/Users.pm
@@ -1,5 +1,6 @@
 package t::MusicBrainz::Server::Controller::Area::Users;
 use Test::Routine;
+use MusicBrainz::Server::Constants qw( $SPAMMER_FLAG );
 use MusicBrainz::Server::Test qw( html_ok );
 
 with 't::Mechanize', 't::Context';
@@ -17,7 +18,10 @@ test 'MBS-6511: List of editors in the area' => sub {
 
     MusicBrainz::Server::Test->prepare_test_database($test->c, '+area');
 
-    $mech->get_ok('/area/106e0bec-b638-3b37-b731-f53d507dc00e/users');
+    $mech->get_ok(
+        '/area/106e0bec-b638-3b37-b731-f53d507dc00e/users',
+        'Fetched the area users page',
+    );
     html_ok($mech->content);
     $mech->content_contains(
         'There are currently no users in this area',
@@ -29,7 +33,10 @@ test 'MBS-6511: List of editors in the area' => sub {
         VALUES (13, 'Editor 1', 'hunter2', '');
     });
 
-    $mech->get_ok('/area/106e0bec-b638-3b37-b731-f53d507dc00e/users');
+    $mech->get_ok(
+        '/area/106e0bec-b638-3b37-b731-f53d507dc00e/users',
+        'Fetched the area users page again',
+    );
     html_ok($mech->content);
     $mech->content_contains(
         'There is currently 1 user in this area',
@@ -42,7 +49,10 @@ test 'MBS-6511: List of editors in the area' => sub {
         VALUES (666, 13, 'Editor 2', 'hunter2', '', 'hunter2@hotmail.com');
     });
 
-    $mech->get_ok('/area/106e0bec-b638-3b37-b731-f53d507dc00e/users');
+    $mech->get_ok(
+        '/area/106e0bec-b638-3b37-b731-f53d507dc00e/users',
+        'Fetched the area users page again',
+    );
     html_ok($mech->content);
     $mech->content_contains(
         'There are currently 2 users in this area',
@@ -56,7 +66,10 @@ test 'MBS-6511: List of editors in the area' => sub {
         VALUES (999, 5126, 'Editor 3', 'hunter2', '', 'hunter2@hotmail.com');
     });
 
-    $mech->get_ok('/area/106e0bec-b638-3b37-b731-f53d507dc00e/users');
+    $mech->get_ok(
+        '/area/106e0bec-b638-3b37-b731-f53d507dc00e/users',
+        'Fetched the area users page again',
+    );
     html_ok($mech->content);
     $mech->content_contains(
         'There are currently 3 users in this area',
@@ -64,6 +77,41 @@ test 'MBS-6511: List of editors in the area' => sub {
     );
     $mech->content_contains('Editor 3', 'The added editor is also listed');
 
+    $test->c->sql->do(qq{
+        UPDATE editor SET privs = $SPAMMER_FLAG WHERE name = 'Editor 2';
+    });
+
+    $mech->get_ok(
+        '/area/106e0bec-b638-3b37-b731-f53d507dc00e/users',
+        'Fetched the area users page again',
+    );
+    html_ok($mech->content);
+    $mech->content_contains(
+        'There are currently 2 users in this area',
+        'The user count is 1 lower after marking an editor as a spammer',
+    );
+    $mech->content_lacks(
+        'Editor 2',
+        'The spammer editor is no longer listed',
+    );
+
+    $test->c->sql->do(qq{
+        UPDATE editor SET privs = $SPAMMER_FLAG WHERE name = 'Editor 3';
+    });
+
+    $mech->get_ok(
+        '/area/106e0bec-b638-3b37-b731-f53d507dc00e/users',
+        'Fetched the area users page again',
+    );
+    html_ok($mech->content);
+    $mech->content_contains(
+        'There is currently 1 user in this area',
+        'The user count is 1 lower after marking another editor as a spammer',
+    );
+    $mech->content_lacks(
+        'Editor 3',
+        'The new spammer editor (from a contained area) is no longer listed',
+    );
 };
 
 1;
