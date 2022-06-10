1a300d3f1c9ded2ed6d6be4af0a32d0f703435d3
Nicolás Tamargo <reosarevok@gmail.com>
2022-06-10 19:02:19 +0300

    MBS-12351: Also trim space only disambiguations (#2510)
    * MBS-12351: Also trim space only disambiguations
    
    The transform was not being applied on spaces-only
    disambiguation fields, because FormHandler considers it
    empty. As such, I added validate_when_empty to enforce
    always running the transform on the comment field.
    
    This broke the test adding a rel, because we weren't entering
    the comment field. That seemed weird to begin with (we are also
    not changing name and sort name, yet we list them) so adding it here.
    
    Co-authored-by: Michael Wiencek <mwtuea@gmail.com>

--- lib/MusicBrainz/Server/Form/Field/Comment.pm
+++ lib/MusicBrainz/Server/Form/Field/Comment.pm
@@ -1,16 +1,28 @@
 package MusicBrainz::Server::Form::Field::Comment;
 use HTML::FormHandler::Moose;
-use MusicBrainz::Server::Data::Utils;
+use MusicBrainz::Server::Data::Utils qw( trim_comment );
 
 extends 'HTML::FormHandler::Field::Text';
 
 has '+maxlength' => ( default => 255 );
 has '+not_nullable' => ( default => 1 );
+has '+validate_when_empty' => (
+    default => 1
+);
 
-apply ([
-    {
-        transform => sub { MusicBrainz::Server::Data::Utils::trim_comment(shift) }
+sub validate {
+    my $self = shift;
+
+    if ($self->has_input) {
+        $self->_set_value(trim_comment($self->value));
+        return $self->SUPER::validate;
+    } else {
+        # validate_when_empty causes the $field value to become set even
+        # if there was no input. We'd like to keep the field unset if it
+        # was omitted so that existing comments aren't blanked, while still
+        # trimming submitted blank values like '  '.
+        $self->_clear_value;
     }
-]);
+}
 
 1;
