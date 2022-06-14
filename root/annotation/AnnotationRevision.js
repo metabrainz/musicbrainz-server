c128d40a9911db4440c8b81b56d3c6caa9093c9b
Nicolás Tamargo <reosarevok@gmail.com>
2022-06-14 13:44:26 +0300

    MBS-12455: Show annotation info when loading empty revision
    When trying to load a blank text annotation revision, the page just
    contains nothing (the Annotation component returns null).
    This is desirable for entity pages, but not for the specific
    /annotation page, where we'd still expect to see the specific info
    that the annotation is blank, and the changelog.

--- root/annotation/AnnotationRevision.js
+++ root/annotation/AnnotationRevision.js
@@ -38,6 +38,7 @@ const AnnotationRevision = ({
         entity={entity}
         numberOfRevisions={numberOfRevisions}
         showChangeLog
+        showEmpty
       />
     </LayoutComponent>
   );
