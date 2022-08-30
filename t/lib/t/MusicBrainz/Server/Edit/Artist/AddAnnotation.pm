4889fec5625409c1f26ee978d5a79d03c7f46213
Nicolás Tamargo <reosarevok@gmail.com>
2022-08-30 18:20:26 +0300

    Format Edit::Artist::AddAnnotation test better
    This also extracts a create_edit method for reuse in further commits.

--- t/lib/t/MusicBrainz/Server/Edit/Artist/AddAnnotation.pm
+++ t/lib/t/MusicBrainz/Server/Edit/Artist/AddAnnotation.pm
@@ -9,42 +9,47 @@ BEGIN { use MusicBrainz::Server::Edit::Artist::AddAnnotation }
 use MusicBrainz::Server::Constants qw( $EDIT_ARTIST_ADD_ANNOTATION );
 use MusicBrainz::Server::Test;
 
-test all => sub {
+test 'Entering add annotation edit works as expected' => sub {
 
-my $test = shift;
-my $c = $test->c;
+    my $test = shift;
+    my $c = $test->c;
 
-MusicBrainz::Server::Test->prepare_test_database($c, '+annotation');
+    MusicBrainz::Server::Test->prepare_test_database($c, '+annotation');
 
-my $edit = $c->model('Edit')->create(
-    edit_type => $EDIT_ARTIST_ADD_ANNOTATION,
-    editor_id => 1,
+    my $edit = create_edit($c, 'Test annotation', 'A changelog');
+    isa_ok($edit, 'MusicBrainz::Server::Edit::Artist::AddAnnotation');
 
-    entity => $c->model('Artist')->get_by_id(1),
-    text => 'Test annotation',
-    changelog => 'A changelog',
-);
-isa_ok($edit, 'MusicBrainz::Server::Edit::Artist::AddAnnotation');
+    my ($edits) = $c->model('Edit')->find({ artist => 1 }, 10, 0);
+    is($edits->[0]->id, $edit->id);
 
-my ($edits) = $c->model('Edit')->find({ artist => 1 }, 10, 0);
-is($edits->[0]->id, $edit->id);
+    $c->model('Edit')->load_all($edit);
+    is($edit->display_data->{artist}{id}, 1);
+    is($edit->display_data->{changelog}, 'A changelog');
 
-$c->model('Edit')->load_all($edit);
-is($edit->display_data->{artist}{id}, 1);
-is($edit->display_data->{changelog}, 'A changelog');
+    my $artist = $c->model('Artist')->get_by_id(1);
 
-my $artist = $c->model('Artist')->get_by_id(1);
+    $c->model('Artist')->annotation->load_latest($artist);
+    my $annotation = $artist->latest_annotation;
+    ok(defined $annotation);
+    is($annotation->editor_id, 1);
+    is($annotation->text, 'Test annotation');
+    is($annotation->changelog, 'A changelog');
 
-$c->model('Artist')->annotation->load_latest($artist);
-my $annotation = $artist->latest_annotation;
-ok(defined $annotation);
-is($annotation->editor_id, 1);
-is($annotation->text, 'Test annotation');
-is($annotation->changelog, 'A changelog');
-
-my $annotation2 = $c->model('Artist')->annotation->get_by_id($edit->annotation_id);
-is_deeply($annotation, $annotation2);
-
+    my $annotation2 = $c->model('Artist')->annotation->get_by_id($edit->annotation_id);
+    is_deeply($annotation, $annotation2);
 };
+
+
+sub create_edit {
+    my ($c, $text, $changelog) = @_;
+    $changelog //= '';
+    return $c->model('Edit')->create(
+        edit_type => $EDIT_ARTIST_ADD_ANNOTATION,
+        editor_id => 1,
+        entity => $c->model('Artist')->get_by_id(1),
+        text => $text,
+        changelog => $changelog,
+    );
+}
 
 1;
