1535bbc77c7a5aa05459c40840de72dcb58e8290
jesus2099 <jesus2099@users.noreply.github.com>
2022-08-31 08:09:00 +0200

    MBS-12560: Stop using track ID for recording ID URL
    MBS-12560: Stop using track ID for recording ID URL
    
    In MB, if you call the /entity/id URL, it will redirect to the matching
    /entity/gid URL.
    
    But it is broken for recording:
    
    NGS has now separate track and recording ID, but pre-NGS, there was some
    redirection necessary or something:
    7acea69a042a4f50f759199fe661e736cee208a4
    
    Example: /recording/272811 should lead to recording with ID 272811
    Not to unrelated recording with ID 316322 via /track/272811

--- lib/MusicBrainz/Server/Controller/Recording.pm
+++ lib/MusicBrainz/Server/Controller/Recording.pm
@@ -83,14 +83,6 @@ after 'load' => sub
     $c->model('ArtistCredit')->load($recording);
 };
 
-sub _row_id_to_gid
-{
-    my ($self, $c, $track_id) = @_;
-    my $track = $c->model('Track')->get_by_id($track_id) or return;
-    $c->model('Recording')->load($track);
-    return $track->recording->gid;
-}
-
 sub show : Chained('load') PathPart('') {
     my ($self, $c) = @_;
     my $recording = $c->stash->{recording};
