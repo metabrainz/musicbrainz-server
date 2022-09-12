c10b25830a333ad1a5177e0275d41d158c42968e
Nicolás Tamargo <reosarevok@gmail.com>
2022-09-12 15:11:11 +0300

    MBS-12587: Convert cdstub/error to React
    Split this into two components using StatusPage, rather
    than overloading one component with entirely different strings
    like we did with TT.

---
+++ root/cdstub/CDStubNotFound.js
@@ -0,0 +1,35 @@
+/*
+ * @flow strict-local
+ * Copyright (C) 2022 MetaBrainz Foundation
+ *
+ * This file is part of MusicBrainz, the open internet music database,
+ * and is licensed under the GPL version 2, or (at your option) any
+ * later version: http://www.gnu.org/licenses/gpl-2.0.txt
+ */
+
+import * as React from 'react';
+
+import StatusPage from '../components/StatusPage.js';
+
+type Props = {
+  +discId: string,
+};
+
+const CDStubNotFound = ({
+  discId,
+}: Props): React.Element<typeof StatusPage> => (
+  <StatusPage title={l('CD Stub Not Found')}>
+    <p>
+      {exp.l(
+        `Sorry, <code>{discid}</code> does not match a CD stub.
+         You can try {search_url|searching for it} instead.`,
+        {
+          discid: discId,
+          search_url: '/search',
+        },
+      )}
+    </p>
+  </StatusPage>
+);
+
+export default CDStubNotFound;
