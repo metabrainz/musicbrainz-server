d4d66bb539b5f515a5b5bfb1f7428c86c9968c3d
Nicolás Tamargo <reosarevok@gmail.com>
2022-09-13 18:13:55 +0300

    Show CD stub logged in error as last option
    It's a lot more user-friendly to redirect the user to the right page
    if the CD TOC they sent is already in use, rather than just saying
    "sorry, you're online, add a release instead?"

--- lib/MusicBrainz/Server/Controller/CDStub.pm
+++ lib/MusicBrainz/Server/Controller/CDStub.pm
@@ -53,12 +53,6 @@ sub add : Path('add') DenyWhenReadonly
 {
     my ($self, $c) = @_;
 
-    if ($c->user_exists) {
-        $c->res->code(403);
-        $c->stash( template => 'cdstub/logged_in.tt' );
-        $c->detach;
-    }
-
     my $toc = CDTOC->new_from_toc( $c->req->query_params->{toc} );
     if (!$toc) {
         $c->stash( message => l('The required TOC parameter was invalid or not present') );
@@ -74,6 +68,12 @@ sub add : Path('add') DenyWhenReadonly
     if ($c->model('CDTOC')->get_by_discid($toc->discid)) {
         $c->response->redirect(
             $c->uri_for_action('/cdtoc/show', [ $toc->discid ]));
+        $c->detach;
+    }
+
+    if ($c->user_exists) {
+        $c->res->code(403);
+        $c->stash( template => 'cdstub/logged_in.tt' );
         $c->detach;
     }
 
