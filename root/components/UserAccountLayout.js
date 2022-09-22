e21b8d8310d43689dd2b32fb988e9249a003b199
Aerozol <aerozol1@gmail.com>
2022-09-23 06:59:41 +1200

    MBS-12623: Editor icon is too large (#2639)
    * MBS-12623: Editor icon is too large
    
    Making the editor icon match the size of the other entity icons, at least until we get avatar support.
    
    * MBS-12623: Editor icon is too large
    
    Changed to 32 to match the other icon CSS

--- root/components/UserAccountLayout.js
+++ root/components/UserAccountLayout.js
@@ -66,7 +66,7 @@ const UserAccountLayout = ({
     {...layoutProps}
   >
     <h1>
-      <EditorLink avatarSize={54} editor={user} />
+      <EditorLink avatarSize={32} editor={user} />
     </h1>
     <UserAccountTabs page={page} user={user} />
     {children}
