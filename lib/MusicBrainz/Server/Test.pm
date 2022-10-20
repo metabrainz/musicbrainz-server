89b646d51b203a815303bdafb13fad260622dfb0
Nicolás Tamargo <reosarevok@gmail.com>
2022-10-20 17:42:15 +0300

    Avoid warning about uninitialized $version
    Check whether $version exists before we try to check
    for other comparisons, rather than last.

--- lib/MusicBrainz/Server/Test.pm
+++ lib/MusicBrainz/Server/Test.pm
@@ -283,8 +283,8 @@ sub schema_validator
 {
     my $version = shift;
 
+    $version = '2.0' if !$version || $version == 2;
     $version = '1.4' if $version == 1;
-    $version = '2.0' if $version == 2 || !$version;
 
     my $mmd_root = $ENV{'MMD_SCHEMA_ROOT'} ||
                    Cwd::realpath( File::Basename::dirname(__FILE__) ) . '/../../../../mmd-schema';
