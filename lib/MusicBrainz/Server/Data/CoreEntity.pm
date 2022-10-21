1c1a6c4dd21fc74bbddf1d7e066d561deb3d738e
yvanzo <yvanzo@metabrainz.org>
2022-10-21 10:47:17 +0100

    Refactor: Move GID data to a separate role
    Note that `_hash_to_row` isn‘t programmatically required as it isn’t
    provided by `Data::CoreEntity` but by its descendants instead.

--- lib/MusicBrainz/Server/Data/CoreEntity.pm
+++ lib/MusicBrainz/Server/Data/CoreEntity.pm
@@ -3,11 +3,11 @@ package MusicBrainz::Server::Data::CoreEntity;
 use Moose;
 use namespace::autoclean;
 use MusicBrainz::Server::Constants qw( %ENTITIES );
-use MusicBrainz::Server::Data::Utils qw( generate_gid );
 use Sql;
 
 extends 'MusicBrainz::Server::Data::Entity';
 with 'MusicBrainz::Server::Data::Role::GetByGID';
+with 'MusicBrainz::Server::Data::Role::GID';
 with 'MusicBrainz::Server::Data::Role::GIDRedirect';
 with 'MusicBrainz::Server::Data::Role::Name';
 
@@ -20,41 +20,6 @@ sub _main_table {
 sub _table { shift->_main_table }
 
 sub _entity_class { 'MusicBrainz::Server::Entity::' . $ENTITIES{shift->_type}{model} }
-
-sub insert {
-    my ($self, @entities) = @_;
-
-    my $extra_data = $self->_insert_hook_prepare(\@entities);
-
-    my @created;
-    for my $entity (@entities) {
-        my $row = $self->_insert_hook_make_row($entity, $extra_data);
-        $row->{gid} = $entity->{gid} || generate_gid();
-
-        my $created = {
-            id => $self->sql->insert_row($self->_main_table, $row, 'id'),
-            gid => $row->{gid},
-        };
-
-        $self->_insert_hook_after_each($created, $entity, $extra_data);
-
-        push @created, $created;
-    }
-
-    $self->_insert_hook_after(\@created, $extra_data);
-    return @entities > 1 ? @created : $created[0];
-}
-
-sub _insert_hook_prepare { {} }
-
-sub _insert_hook_make_row {
-    my ($self, $entity) = @_;
-    return $self->_hash_to_row($entity);
-}
-
-sub _insert_hook_after_each { }
-
-sub _insert_hook_after { }
 
 __PACKAGE__->meta->make_immutable;
 no Moose;
