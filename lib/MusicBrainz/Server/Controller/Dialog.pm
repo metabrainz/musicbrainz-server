metabrainz/musicbrainz-server

commit 1786909dd2aa2a082a63b5dd6704eb21e8c71d69

AddEntityDialog: Pass name via query parameter
Instead of using jQuery to update the name field after the iframe loads,
we can just pass it as a query parameter.

This will also simplify having the name value visible to the
relationship editor form.

lib/MusicBrainz/Server/Controller/Dialog.pm
@@ -11,7 +11,13 @@ sub dialog : Path('/dialog') Edit {
     if (!$path) {
         $self->bad_request($c, 'path not specified');
     }
-    elsif ($c->dispatcher->get_action_by_path($path)) {
+
+    my $path_uri = URI->new($path);
+    $path = $path_uri->path;
+
+    if ($c->dispatcher->get_action_by_path($path)) {
+        %{ $c->req->query_params } = $path_uri->query_form;
+        %{ $c->req->params } = $path_uri->query_form;
         $c->stash( template => 'forms/dialog.tt' );
         $c->forward($path, [ within_dialog => 1 ]);
     }

root/static/scripts/edit/components/AddEntityDialog.js
@@ -61,22 +61,6 @@ const AddEntityDialog = ({
     contentWindow.containingDialog = instanceRef;
     adjustDialogSize(contentWindow);
     setLoading(false);
-
-    if (nonEmpty(name)) {
-      const nameFieldId =
-        'id-edit-' + entityType.replace('_', '-') + '.name';
-
-      // Must use contentWindow's jQuery handle or this won't work.
-      const $ = contentWindow.$;
-      $(function () {
-        contentWindow.setTimeout(function () {
-          $(contentWindow.document.getElementById(nameFieldId))
-            .val(name)
-            .change()
-            .focus();
-        }, 1);
-      });
-    }
   };
 
   const adjustDialogSize = React.useCallback((contentWindow) => {
@@ -90,6 +74,13 @@ const AddEntityDialog = ({
     adjustDialogSize,
     close,
   };
+
+  let dialogPath = '/' + entityType + '/create';
+  if (nonEmpty(name)) {
+    const nameField =
+      'edit-' + entityType.replace('_', '-') + '.name';
+    dialogPath += '?' + nameField + '=' + encodeURIComponent(name);
+  }
 
   return (
     <Modal
@@ -106,8 +97,7 @@ const AddEntityDialog = ({
       <iframe
         onLoad={handlePageLoad}
         ref={iframeRef}
-        src={'/dialog?path=' +
-          encodeURIComponent('/' + entityType + '/create')}
+        src={'/dialog?path=' + encodeURIComponent(dialogPath)}
       />
     </Modal>
   );

commit 4557a48479b71795ebae3914fdbe57450c98ea01

MBS-12695: Fix encoding issue in "Add a new entity" dialog
This was a regression introduced by
1786909dd2aa2a082a63b5dd6704eb21e8c71d69.  Fix it by decoding the path's
query_form before forwarding it to the dialog action.

lib/MusicBrainz/Server/Controller/Dialog.pm
@@ -1,4 +1,5 @@
 package MusicBrainz::Server::Controller::Dialog;
+use Encode qw( decode_utf8 );
 use Moose;
 
 BEGIN { extends 'MusicBrainz::Server::Controller'; }
@@ -16,8 +17,12 @@ sub dialog : Path('/dialog') Edit {
     $path = $path_uri->path;
 
     if ($c->dispatcher->get_action_by_path($path)) {
-        %{ $c->req->query_params } = $path_uri->query_form;
-        %{ $c->req->params } = $path_uri->query_form;
+        my %query_form = $path_uri->query_form;
+        my %decoded_query_form = map {
+            $_ => decode_utf8($query_form{$_})
+        } keys %query_form;
+        %{ $c->req->query_params } = %decoded_query_form;
+        %{ $c->req->params } = %decoded_query_form;
         $c->stash( template => 'forms/dialog.tt' );
         $c->forward($path, [ within_dialog => 1 ]);
     }

t/selenium/Artist_Edit_Form.json5
@@ -29,7 +29,7 @@
     {
       command: 'type',
       target: 'css=#add-relationship-dialog input.relationship-target',
-      value: 'group member',
+      value: 'gröup member',
     },
     {
       command: 'pause',
@@ -99,8 +99,8 @@
           gender_id: null,
           ipi_codes: [],
           isni_codes: [],
-          name: 'group member',
-          sort_name: 'group member',
+          name: 'gröup member',
+          sort_name: 'gröup member',
           type_id: 1,
         },
       },
@@ -236,7 +236,7 @@
           entity0: {
             gid: '$$__IGNORE__$$',
             id: 3,
-            name: 'group member',
+            name: 'gröup member',
           },
           entity1: {
             gid: '$$__IGNORE__$$',
@@ -282,7 +282,7 @@
           entity0: {
             gid: '$$__IGNORE__$$',
             id: 3,
-            name: 'group member',
+            name: 'gröup member',
           },
           entity1: {
             gid: '$$__IGNORE__$$',