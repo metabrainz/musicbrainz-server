fcf576fb5b52f360a2d84e4e2311b3b7e44bc476
Nicolás Tamargo <reosarevok@gmail.com>
2022-11-21 17:20:52 +0200

    Document Entity::ReleaseGroup test
    This documents the test properly, fixes the style,
    and breaks it into three named tests since they're
    all arguaby testing different things.

--- t/lib/t/MusicBrainz/Server/Entity/ReleaseGroup.pm
+++ t/lib/t/MusicBrainz/Server/Entity/ReleaseGroup.pm
@@ -9,20 +9,58 @@ use Test::More;
 use MusicBrainz::Server::Entity::ReleaseGroup;
 use MusicBrainz::Server::Entity::ReleaseGroupType;
 
-test all => sub {
+=head1 DESCRIPTION
 
-my $rg = MusicBrainz::Server::Entity::ReleaseGroup->new();
-ok( !defined $rg->first_release_date );
+This test checks whether RG data is stored and calculated correctly.
 
-is( $rg->type_name, undef );
-$rg->primary_type(MusicBrainz::Server::Entity::ReleaseGroupType->new(id => 1, name => 'Album'));
-is( $rg->type_name, 'Album' );
-is( $rg->primary_type->id, 1 );
-is( $rg->primary_type->name, 'Album' );
+=cut
 
-$rg->edits_pending(2);
-is( $rg->edits_pending, 2 );
+test 'Empty RG has undefined calculated data' => sub {
+    my $rg = MusicBrainz::Server::Entity::ReleaseGroup->new();
+    ok(
+        !defined $rg->first_release_date,
+        'No first release date is defined for empty release group',
+    );
 
+    is(
+        $rg->type_name,
+        undef,
+        'Undefined RG type name is calculated when no type explicitly set',
+    );
+};
+
+test 'RG type data is stored and calculated properly' => sub {
+    my $rg = MusicBrainz::Server::Entity::ReleaseGroup->new();
+    $rg->primary_type(
+        MusicBrainz::Server::Entity::ReleaseGroupType->new(
+            id => 1,
+            name => 'Album',
+        )
+    );
+
+    is(
+        $rg->type_name,
+        'Album',
+        'Expected RG type name is calculated after setting a primary type',
+    );
+
+    is($rg->primary_type->id, 1, 'The primary type id is stored as expected');
+
+    is(
+        $rg->primary_type->name,
+        'Album',
+        'The primary type name is stored as expected',
+    );
+};
+
+test 'Can store RG pending edits' => sub {
+    my $rg = MusicBrainz::Server::Entity::ReleaseGroup->new();
+    $rg->edits_pending(2);
+    is(
+        $rg->edits_pending,
+        2,
+        'The number of pending edits is stored as expected',
+    );
 };
 
 1;
