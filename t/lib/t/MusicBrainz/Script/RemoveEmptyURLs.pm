48582500abd89b429096d9a39960dff234a626e8
Nicolás Tamargo <reosarevok@gmail.com>
2022-11-21 17:24:52 +0200

    Document Script::RemoveEmptyURLs test
    This documents the test properly, and fixes the style.

--- t/lib/t/MusicBrainz/Script/RemoveEmptyURLs.pm
+++ t/lib/t/MusicBrainz/Script/RemoveEmptyURLs.pm
@@ -19,33 +19,41 @@ and have no pending edits.
 =cut
 
 test 'The RemoveEmpty script deletes the expected URLs' => sub {
+    my $test = shift;
+    my $c = $test->c;
 
-my $test = shift;
-my $c = $test->c;
+    MusicBrainz::Server::Test->prepare_test_database($c, '+url');
 
-MusicBrainz::Server::Test->prepare_test_database($c, '+url');
+    my $sql = $c->sql;
+    is(
+        $sql->select_single_value('SELECT COUNT(*) FROM url'),
+        5,
+        'Five URLs exist before the script is run',
+    );
 
-my $sql = $c->sql;
-is($sql->select_single_value('SELECT COUNT(*) FROM url'),
-   5,
-   'Five URLs exist before the script is run');
+    my $script = MusicBrainz::Script::RemoveEmpty->new( c => $c );
+    ok(
+        !exception { $script->run('url') },
+        'The removal script ran successfully',
+    );
 
+    is(
+        $sql->select_single_value('SELECT COUNT(*) FROM url'),
+        4,
+        'Four URLs exist after the script is run, one has been deleted',
+    );
 
-my $script = MusicBrainz::Script::RemoveEmpty->new( c => $c );
-ok !exception { $script->run('url') };
+    is(
+        $sql->select_single_value('SELECT 1 FROM url WHERE id = 2'),
+        1,
+        'Recently updated unused URL has not been deleted',
+    );
 
-is($sql->select_single_value('SELECT COUNT(*) FROM url'),
-   4,
-   'Four URLs exist after the script is run, one has been deleted');
-
-is($sql->select_single_value('SELECT 1 FROM url WHERE id = 2'),
-   1,
-   'Recently updated unused URL has not been deleted');
-
-is($sql->select_single_value('SELECT 1 FROM url WHERE id = 5'),
-   1,
-   'Unused URL with pending edits has not been deleted');
-
+    is(
+        $sql->select_single_value('SELECT 1 FROM url WHERE id = 5'),
+        1,
+        'Unused URL with pending edits has not been deleted',
+    );
 };
 
 1;
