e1ae491ac1360d27d0cecfc16d36e597fdeeb1fb
Nicolás Tamargo <reosarevok@gmail.com>
2022-11-22 11:14:18 +0200

    Document Entity::Artist test
    This documents the test properly, fixes the style,
    and breaks it into several named tests since they're
    all arguaby testing different things.

--- t/lib/t/MusicBrainz/Server/Entity/Artist.pm
+++ t/lib/t/MusicBrainz/Server/Entity/Artist.pm
@@ -13,30 +13,83 @@ use MusicBrainz::Server::Entity::ArtistAlias;
 
 use MusicBrainz::Server::Constants qw( $DARTIST_ID $VARTIST_ID $VARTIST_GID );
 
-test all => sub {
+=head1 DESCRIPTION
 
-my $artist = MusicBrainz::Server::Entity::Artist->new();
-ok( defined $artist->begin_date );
-ok( $artist->begin_date->is_empty );
-ok( defined $artist->end_date );
-ok( $artist->end_date->is_empty );
+This test checks whether artist data is stored and calculated correctly.
 
-is( $artist->type_name, undef );
-is( $artist->last_updated , undef );
-$artist->type(MusicBrainz::Server::Entity::ArtistType->new(id => 1, name => 'Person'));
-is( $artist->type_name, 'Person' );
-is( $artist->type->id, 1 );
-is( $artist->type->name, 'Person' );
+=cut
 
-$artist->edits_pending(2);
-is( $artist->edits_pending, 2 );
+test 'Empty artist has the expected calculated data' => sub {
+    my $artist = MusicBrainz::Server::Entity::Artist->new();
+    ok(defined $artist->begin_date, 'An empty artist still has a begin date');
+    ok($artist->begin_date->is_empty, 'The begin date is empty');
+    ok(defined $artist->end_date, 'An empty artist still has an end date');
+    ok($artist->end_date->is_empty, 'The end date is empty');
 
-ok(MusicBrainz::Server::Entity::Artist->new( id => $DARTIST_ID )->is_special_purpose);
-ok(MusicBrainz::Server::Entity::Artist->new( id => $VARTIST_ID )->is_special_purpose);
-ok(MusicBrainz::Server::Entity::Artist->new( gid => $VARTIST_GID )->is_special_purpose);
-ok(!MusicBrainz::Server::Entity::Artist->new( id => 5 )->is_special_purpose);
-ok(!MusicBrainz::Server::Entity::Artist->new( gid => '7527f6c2-d762-4b88-b5e2-9244f1e34c46' )->is_special_purpose);
+    is(
+        $artist->type_name,
+        undef,
+        'Undefined artist type name is calculated when no type explicitly set',
+    );
+    is(
+        $artist->last_updated,
+        undef,
+        'last_updated is undefined before any updates have happened',
+    );
+};
 
+test 'Artist type data is stored and returned properly' => sub {
+    my $artist = MusicBrainz::Server::Entity::Artist->new();
+    $artist->type(
+        MusicBrainz::Server::Entity::ArtistType->new(
+            id => 1,
+            name => 'Person',
+        )
+    );
+    is(
+        $artist->type_name,
+        'Person',
+        'Expected artist type name is returned after setting a type',
+    );
+    is($artist->type->id, 1, 'The type id is stored as expected');
+    is(
+        $artist->type->name,
+        'Person',
+        'The type name is stored as expected',
+    );
+};
+
+test 'Can store artist pending edits' => sub {
+    my $artist = MusicBrainz::Server::Entity::Artist->new();
+    $artist->edits_pending(2);
+    is(
+        $artist->edits_pending,
+        2,
+        'The number of pending edits is stored as expected',
+    );
+};
+
+test 'The right artists (and only them) are marked as special purpose' => sub {
+    ok(
+        MusicBrainz::Server::Entity::Artist->new( id => $DARTIST_ID )->is_special_purpose,
+        'An artist with the row id reserved for "Deleted Artist" is marked as special purpose',
+    );
+    ok(
+        MusicBrainz::Server::Entity::Artist->new( id => $VARTIST_ID )->is_special_purpose,
+        'An artist with the row id of "Various Artists" is marked as special purpose',
+    );
+    ok(
+        MusicBrainz::Server::Entity::Artist->new( gid => $VARTIST_GID )->is_special_purpose,
+        'An artist with the MBID of "Various Artists" is marked as special purpose',
+    );
+    ok(
+        !MusicBrainz::Server::Entity::Artist->new( id => 5 )->is_special_purpose,
+        'An artist with a bog-standard row id is not marked as special purpose',
+    );
+    ok(
+        !MusicBrainz::Server::Entity::Artist->new( gid => '7527f6c2-d762-4b88-b5e2-9244f1e34c46' )->is_special_purpose,
+        'An artist with a bog-standard MBID is not marked as special purpose',
+    );
 };
 
 1;
