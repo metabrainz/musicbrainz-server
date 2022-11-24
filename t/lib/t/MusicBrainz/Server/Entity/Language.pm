6bb55eca68f2924604009f1950065b9edff95b97
Nicolás Tamargo <reosarevok@gmail.com>
2022-11-24 10:16:32 +0200

    Make Entity::Language test do something useful
    This test was a useless stub, so changed it to actually test
    something useful: whether the code methods
    actually return the expected codes.

--- t/lib/t/MusicBrainz/Server/Entity/Language.pm
+++ t/lib/t/MusicBrainz/Server/Entity/Language.pm
@@ -6,16 +6,48 @@ use Test::Routine;
 use Test::Moose;
 use Test::More;
 
-test all => sub {
-    use MusicBrainz::Server::Entity::Language;
-    ok 1;
+use MusicBrainz::Server::Entity::Language;
+
+=head1 DESCRIPTION
+
+This test ensures that language code accessors return the right code.
+
+=cut
+
+test 'Language codes are returned correctly' => sub {
+    my $language = MusicBrainz::Server::Entity::Language->new();
+    $language->iso_code_2t('alg');
+    is(
+        $language->alpha_3_code,
+        'alg',
+        'alpha_3_code returns iso_code_2t if iso_code_3 is missing',
+    );
+
+    $language->iso_code_3('arp');
+    is(
+        $language->alpha_3_code,
+        'arp',
+        'alpha_3_code returns iso_code_3 if present',
+    );
+    is(
+        $language->bcp47,
+        'arp',
+        'bcp47 returns iso_code_3 if iso_code_1 is missing',
+    );
+
+    $language->iso_code_1('eu');
+    is(
+        $language->bcp47,
+        'eu',
+        'bcp47 returns iso_code_1 if present',
+    );
 };
 
 1;
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright (C) 2011 MetaBrainz Foundation
+Copyright (C) 2022 MetaBrainz Foundation
 
 This file is part of MusicBrainz, the open internet music database,
 and is licensed under the GPL version 2, or (at your option) any
