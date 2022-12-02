6504322832dc0c0922a1363aed809066dd46006b
Nicolás Tamargo <reosarevok@gmail.com>
2022-12-02 20:49:53 +0200

    Document Data::ReleasePackaging test
    This documents the test properly and fixes the style, plus it splits it
    into one test per tested getter.
    I also made it so that we first check the hash returned by get_by_ids
    is setting the keys to the right row ids, and only then
    try to access them.
    I also used verify_name_and_id for the get_all results
    rather than only test the row ids.

--- t/lib/t/MusicBrainz/Server/Data/ReleasePackaging.pm
+++ t/lib/t/MusicBrainz/Server/Data/ReleasePackaging.pm
@@ -5,36 +5,70 @@ use warnings;
 use Test::Routine;
 use Test::Moose;
 use Test::More;
+use Test::Deep qw( cmp_bag );
 
 use MusicBrainz::Server::Data::ReleasePackaging;
 
 use MusicBrainz::Server::Context;
 use MusicBrainz::Server::Test;
+use MusicBrainz::Server::Test::Utils qw( verify_name_and_id );
 
 with 't::Context';
 
-test all => sub {
+=head1 DESCRIPTION
 
-my $test = shift;
+This test checks the different getters for ReleasePackaging.
 
-my $lt_data = MusicBrainz::Server::Data::ReleasePackaging->new(c => $test->c);
+=cut
 
-my $lt = $lt_data->get_by_id(1);
-is ( $lt->id, 1 );
-is ( $lt->name, 'Jewel Case' );
+test 'get_by_id' => sub {
+    my $test = shift;
+    my $c = $test->c;
 
-my $lts = $lt_data->get_by_ids(1, 3, 666);
-is ( $lts->{1}->id, 1 );
-is ( $lts->{1}->name, 'Jewel Case' );
-is ( $lts->{3}->id, 3 );
-is ( $lts->{3}->name, 'Digipak' );
-ok(!exists $lts->{666});
+    my $packaging_data =
+        MusicBrainz::Server::Data::ReleasePackaging->new(c => $c);
 
-does_ok($lt_data, 'MusicBrainz::Server::Data::Role::SelectAll');
-my @types = $lt_data->get_all;
-is(@types, 6);
-is_deeply([sort { $a <=> $b } map { $_->id } @types], [1, 2, 3, 4, 5, 7]);
+    my $packaging = $packaging_data->get_by_id(1);
+    verify_name_and_id(1, 'Jewel Case', $packaging);
+};
 
+test 'get_by_ids' => sub {
+    my $test = shift;
+    my $c = $test->c;
+
+    my $packaging_data =
+        MusicBrainz::Server::Data::ReleasePackaging->new(c => $c);
+
+    my @valid_ids = (1, 3);
+    my @invalid_ids = (666);
+    my @requested_ids = (@valid_ids, @invalid_ids);
+
+    my $packagings = $packaging_data->get_by_ids(@requested_ids);
+    cmp_bag(
+        [keys %$packagings],
+        \@valid_ids,
+        'The keys of the returned hash are the requested *valid* row ids',
+    );
+    verify_name_and_id(1, 'Jewel Case', $packagings->{1});
+    verify_name_and_id(3, 'Digipak', $packagings->{3});
+};
+
+test 'get_all' => sub {
+    my $test = shift;
+    my $c = $test->c;
+
+    my $packaging_data =
+        MusicBrainz::Server::Data::ReleasePackaging->new(c => $c);
+
+    does_ok($packaging_data, 'MusicBrainz::Server::Data::Role::SelectAll');
+    my @packagings = sort { $a->{id} <=> $b->{id} } $packaging_data->get_all;
+    is(@packagings, 6, 'get_all returns all 6 packagings');
+    verify_name_and_id(1, 'Jewel Case', $packagings[0]);
+    verify_name_and_id(2, 'Slim Jewel Case', $packagings[1]);
+    verify_name_and_id(3, 'Digipak', $packagings[2]);
+    verify_name_and_id(4, 'Cardboard/Paper Sleeve', $packagings[3]);
+    verify_name_and_id(5, 'Other', $packagings[4]);
+    verify_name_and_id(7, 'None', $packagings[5]);
 };
 
 1;
