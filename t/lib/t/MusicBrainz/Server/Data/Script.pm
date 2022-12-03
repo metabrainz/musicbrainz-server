a3ecb010c9ab4c1e5a55a0ff553718f47585c3f1
Nicolás Tamargo <reosarevok@gmail.com>
2022-12-03 12:07:42 +0200

    Document Data::Script test
    This documents the test properly and fixes the style, plus it splits it
    into one test per tested getter .
    I also made it so that we first check the hash returned by get_by_ids
    is setting the keys to the right row ids, and only then
    try to access them.
    I also used verify_name_and_id for the get_all results
    rather than only test the row ids.

--- t/lib/t/MusicBrainz/Server/Data/Script.pm
+++ t/lib/t/MusicBrainz/Server/Data/Script.pm
@@ -5,38 +5,72 @@ use warnings;
 use Test::Routine;
 use Test::Moose;
 use Test::More;
+use Test::Deep qw( cmp_bag );
 
 use MusicBrainz::Server::Data::Script;
 
 use MusicBrainz::Server::Context;
 use MusicBrainz::Server::Test;
+use MusicBrainz::Server::Test::Utils qw( verify_name_and_id );
 
 with 't::Context';
 
-test all => sub {
+=head1 DESCRIPTION
 
-my $test = shift;
-my $script_data = MusicBrainz::Server::Data::Script->new(c => $test->c);
+This test checks the different getters for Script.
 
-my $script = $script_data->get_by_id(3);
-is ( $script->id, 3 );
-is ( $script->iso_code, 'Ugar' );
-is ( $script->name, 'Ugaritic' );
+=cut
 
-my $scripts = $script_data->get_by_ids(3, 28, 666);
-is ( $scripts->{3}->id, 3 );
-is ( $scripts->{3}->iso_code, 'Ugar' );
-is ( $scripts->{3}->name, 'Ugaritic' );
-is ( $scripts->{28}->id, 28 );
-is ( $scripts->{28}->iso_code, 'Latn' );
-is ( $scripts->{28}->name, 'Latin' );
-ok(!exists $scripts->{666});
+test 'get_by_id' => sub {
+    my $test = shift;
+    my $c = $test->c;
 
-does_ok($script_data, 'MusicBrainz::Server::Data::Role::SelectAll');
-my @scripts = $script_data->get_all;
-is(@scripts, 4);
-is_deeply([sort { $a <=> $b } map { $_->id } @scripts], [3, 28, 85, 112]);
+    my $script_data = MusicBrainz::Server::Data::Script->new(c => $c);
 
+    my $script = $script_data->get_by_id(3);
+    verify_name_and_id(3, 'Ugaritic', $script);
+    is ($script->iso_code, 'Ugar', 'Expected ISO code Ugar found');
+};
+
+test 'get_by_ids' => sub {
+    my $test = shift;
+    my $c = $test->c;
+
+    my $script_data = MusicBrainz::Server::Data::Script->new(c => $c);
+
+    my @valid_ids = (3, 28);
+    my @invalid_ids = (666);
+    my @requested_ids = (@valid_ids, @invalid_ids);
+
+    my $scripts = $script_data->get_by_ids(@requested_ids);
+    cmp_bag(
+        [keys %$scripts],
+        \@valid_ids,
+        'The keys of the returned hash are the requested *valid* row ids',
+    );
+    verify_name_and_id(3, 'Ugaritic', $scripts->{3});
+    is ($scripts->{3}->iso_code, 'Ugar', 'Expected ISO code Ugar found');
+    verify_name_and_id(28, 'Latin', $scripts->{28});
+    is ($scripts->{28}->iso_code, 'Latn', 'Expected ISO code Latn found');
+};
+
+test 'get_all' => sub {
+    my $test = shift;
+    my $c = $test->c;
+
+    my $script_data = MusicBrainz::Server::Data::Script->new(c => $c);
+
+    does_ok($script_data, 'MusicBrainz::Server::Data::Role::SelectAll');
+    my @scripts = sort { $a->{id} <=> $b->{id} } $script_data->get_all;
+    is(@scripts, 4, 'get_all returns all 4 scripts');
+    verify_name_and_id(3, 'Ugaritic', $scripts[0]);
+    is ($scripts[0]->iso_code, 'Ugar', 'Expected ISO code Ugar found');
+    verify_name_and_id(28, 'Latin', $scripts[1]);
+    is ($scripts[1]->iso_code, 'Latn', 'Expected ISO code Latn found');
+    verify_name_and_id(85, 'Japanese', $scripts[2]);
+    is ($scripts[2]->iso_code, 'Jpan', 'Expected ISO code Jpan found');
+    verify_name_and_id(112, 'Symbols', $scripts[3]);
+    is ($scripts[3]->iso_code, 'Zsym', 'Expected ISO code Zsym found');
 };
 
 1;
