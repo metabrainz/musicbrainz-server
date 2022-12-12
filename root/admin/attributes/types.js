b5f7d084e9226bdd483e8c188f5036534240abb5
Nicolás Tamargo <reosarevok@gmail.com>
2022-12-12 17:10:29 +0200

    MBS-12741: Convert Create/Edit attribute admin pages to React
    TT was happy to have a scrap heap of forms held together with wire
    and glue here, but I thought it would make a lot more sense to split
    the different forms into their own components and pick the right one
    according to the model.
    
    I made it so that the entity type dropdown actually shows
    translatable entity names, rather than the type string.

--- root/admin/attributes/types.js
+++ root/admin/attributes/types.js
@@ -25,3 +25,114 @@ export type AttributeT =
   | SeriesTypeT
   | WorkAttributeTypeT
   | WorkTypeT;
+
+export type CreateOrEditAttributePropsT =
+  | $ReadOnly<{
+      +action: 'add' | 'edit',
+      +form: AttributeEditGenericFormT,
+      +parentSelectOptions: SelectOptionsT,
+      +type:
+        | 'AreaType'
+        | 'ArtistType'
+        | 'CoverArtType'
+        | 'EventType'
+        | 'Gender'
+        | 'InstrumentType'
+        | 'LabelType'
+        | 'PlaceType'
+        | 'ReleaseGroupType'
+        | 'ReleaseGroupSecondaryType'
+        | 'ReleaseStatus'
+        | 'ReleasePackaging'
+        | 'WorkType',
+    }>
+  | $ReadOnly<{
+      +action: 'add' | 'edit',
+      +entityTypeSelectOptions: {
+        [entityType: CollectableEntityTypeT]: CollectableEntityTypeT,
+      },
+      +form: AttributeEditFormWithEntityTypeT,
+      +parentSelectOptions: SelectOptionsT,
+      +type: 'CollectionType',
+    }>
+  | $ReadOnly<{
+      +action: 'add' | 'edit',
+      +entityTypeSelectOptions: {
+        [entityType: SeriesEntityTypeT]: SeriesEntityTypeT,
+      },
+      +form: AttributeEditFormWithEntityTypeT,
+      +parentSelectOptions: SelectOptionsT,
+      +type: 'SeriesType',
+    }>
+  | $ReadOnly<{
+      +action: 'add' | 'edit',
+      +form: LanguageEditFormT,
+      +type: 'Language',
+    }>
+  | $ReadOnly<{
+      +action: 'add' | 'edit',
+      +form: MediumFormatEditFormT,
+      +parentSelectOptions: SelectOptionsT,
+      +type: 'MediumFormat',
+    }>
+  | $ReadOnly<{
+      +action: 'add' | 'edit',
+      +form: ScriptEditFormT,
+      +type: 'Script',
+    }>
+  | $ReadOnly<{
+      +action: 'add' | 'edit',
+      +form: WorkAttributeTypeEditFormT,
+      +parentSelectOptions: SelectOptionsT,
+      +type: 'WorkAttributeType',
+    }>;
+
+export type AnyAttributeEditFormT =
+  | AttributeEditFormWithEntityTypeT
+  | AttributeEditGenericFormT
+  | MediumFormatEditFormT
+  | WorkAttributeTypeEditFormT;
+
+export type AttributeEditGenericFormT = FormT<{
+  ...AttributeEditFormCommonSectionT,
+}>;
+
+export type AttributeEditFormCommonSectionT = {
+  +child_order: FieldT<string>,
+  +description: FieldT<string>,
+  +name: FieldT<string>,
+  +parent_id: FieldT<string>,
+};
+
+export type AttributeEditFormWithEntityTypeT = FormT<{
+  ...AttributeEditFormCommonSectionT,
+  +action: 'add' | 'edit',
+  +item_entity_type: FieldT<string>,
+}>;
+
+export type LanguageEditFormT = FormT<{
+  +frequency: FieldT<string>,
+  +iso_code_1: FieldT<string>,
+  +iso_code_2b: FieldT<string>,
+  +iso_code_2t: FieldT<string>,
+  +iso_code_3: FieldT<string>,
+  +name: FieldT<string>,
+}>;
+
+export type MediumFormatEditFormT = FormT<{
+  ...AttributeEditFormCommonSectionT,
+  +has_discids: FieldT<boolean>,
+  +year: FieldT<string>,
+}>;
+
+export type ScriptEditFormT = FormT<{
+  +frequency: FieldT<string>,
+  +iso_code: FieldT<string>,
+  +iso_number: FieldT<string>,
+  +name: FieldT<string>,
+}>;
+
+export type WorkAttributeTypeEditFormT = FormT<{
+  ...AttributeEditFormCommonSectionT,
+  +free_text: FieldT<boolean>,
+}>;
