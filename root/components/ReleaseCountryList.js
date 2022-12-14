2ee304c535a668b3d25dc6e2f6b0318bceab8058
Nicolás Tamargo <reosarevok@gmail.com>
2022-12-14 20:57:18 +0200

    MBS-12762: Convert Set RG cover art page to React

---
+++ root/components/ReleaseCountryList.js
@@ -0,0 +1,39 @@
+/*
+ * @flow strict
+ * Copyright (C) 2022 MetaBrainz Foundation
+ *
+ * This file is part of MusicBrainz, the open internet music database,
+ * and is licensed under the GPL version 2, or (at your option) any
+ * later version: http://www.gnu.org/licenses/gpl-2.0.txt
+ */
+
+import CountryAbbr from '../static/scripts/common/components/CountryAbbr.js';
+import commaOnlyList from '../static/scripts/common/i18n/commaOnlyList.js';
+
+const displayCountry = (
+  country: AreaT,
+): React$Element<typeof CountryAbbr> => (
+  <CountryAbbr className="release-country" country={country} />
+);
+
+type ReleaseLabelsProps = {
+  +events?: $ReadOnlyArray<ReleaseEventT>,
+};
+
+const ReleaseCountryList = ({
+  events: releaseEvents,
+}: ReleaseLabelsProps): Expand2ReactOutput | null => {
+  if (!releaseEvents || !releaseEvents.length) {
+    return null;
+  }
+  const countries = new Set<AreaT>();
+  for (const releaseEvent of releaseEvents) {
+    const country = releaseEvent.country;
+    if (country) {
+      countries.add(country);
+    }
+  }
+  return commaOnlyList(Array.from(countries.values()).map(displayCountry));
+};
+
+export default ReleaseCountryList;
---
+++ root/release_group/SetCoverArt.js
@@ -0,0 +1,59 @@
+/*
+ * @flow strict-local
+ * Copyright (C) 2022 MetaBrainz Foundation
+ *
+ * This file is part of MusicBrainz, the open internet music database,
+ * and is licensed under the GPL version 2, or (at your option) any
+ * later version: http://www.gnu.org/licenses/gpl-2.0.txt
+ */
+
+import * as manifest from '../static/manifest.mjs';
+import SetCoverArtForm
+  from '../static/scripts/release-group/components/SetCoverArtForm.js';
+
+import ReleaseGroupLayout from './ReleaseGroupLayout.js';
+import {type SetCoverArtFormT} from './types.js';
+
+type Props = {
+  +allReleases: $ReadOnlyArray<ReleaseT>,
+  +artwork: {[releaseId: number]: ArtworkT},
+  +entity: ReleaseGroupT,
+  +form: SetCoverArtFormT,
+  +selectedRelease: string,
+};
+
+const SetCoverArt = ({
+  allReleases,
+  artwork,
+  entity: releaseGroup,
+  form,
+}: Props): React$Element<typeof ReleaseGroupLayout> => (
+  <ReleaseGroupLayout
+    entity={releaseGroup}
+    fullWidth
+    page="edit"
+    title={lp('Set cover art', 'singular, header')}
+  >
+    <h2>{lp('Set cover art', 'singular, header')}</h2>
+
+    {/* If not even default cover art exists, there's nothing to set */}
+    {releaseGroup.cover_art ? (
+      <SetCoverArtForm
+        allReleases={allReleases}
+        artwork={artwork}
+        form={form}
+      />
+    ) : (
+      <p>
+        {l(`No releases have cover art marked as "Front",
+            cannot set cover art.`)}
+      </p>
+    )}
+    {manifest.js(
+      'release-group/components/SetCoverArtForm',
+      {async: 'async'},
+    )}
+  </ReleaseGroupLayout>
+);
+
+export default SetCoverArt;
