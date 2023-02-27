9371f6b2cc6711f3797570b8dd1b10b4f24a24a1
Michael Wiencek <mwtuea@gmail.com>
2023-02-27 09:49:12 -0600

    Unset the Sentry user in the template renderer
    If there's no user in the current context sent to the renderer, we should set
    the user to null in the Sentry context. I haven't confirmed this, but I think
    this would cause it to retain the user of the previously-handled request
    otherwise.

--- root/server/response.mjs
+++ root/server/response.mjs
@@ -36,6 +36,8 @@ export async function getResponse(requestBody, context) {
   const user = context.user;
   if (user) {
     Sentry.setUser({id: user.id, username: user.name});
+  } else {
+    Sentry.setUser(null);
   }
 
   /*
