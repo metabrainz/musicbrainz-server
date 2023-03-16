metabrainz/musicbrainz-server

commit d95b3d3769dc2f494df2751988b8e3bee2c5fcf1

MBS-12928: Sort related works by name before ID

root/static/scripts/relationship-editor/utility/updateWorkStates.js
@@ -12,6 +12,7 @@ import {
   onNotFoundDoNothing,
 } from 'weight-balanced-tree/update';
 
+import {compare} from '../../common/i18n.js';
 import type {
   MediumWorkStateT,
   ReleaseRelationshipEditorStateT,
@@ -31,7 +32,10 @@ export function compareWorkWithWorkState(
   work: WorkT,
   workState: MediumWorkStateT,
 ): number {
-  return work.id - workState.work.id;
+  return (
+    compare(work.name, workState.work.name) ||
+    (work.id - workState.work.id)
+  );
 }
 
 export function* findWorkRecordings(

commit 2cdeff864ff8a094124c01d7d1658432379e269a

MBS-12976: Changing a work can cause duplication/key errors
This was caused by d95b3d3769dc2f494df2751988b8e3bee2c5fcf1. There were other
comparators used for updating the `relatedWorks` entries besides that one. I've
tried to make them call each other here to avoid other issues in the future.

root/static/scripts/relationship-editor/components/RelationshipEditor.js
@@ -94,6 +94,7 @@ export type PropsT = {
 export type InitialStateArgsT = {
   +formName: string,
   +seededRelationships: ?$ReadOnlyArray<SeededRelationshipT>,
+  +source?: CoreEntityT,
 };
 
 export function* getInitialRelationshipUpdates(
@@ -173,7 +174,7 @@ export function createInitialState(
 ): RelationshipEditorStateT {
   const {seededRelationships} = args;
 
-  const source = getSourceEntityDataForRelationshipEditor();
+  const source = args.source ?? getSourceEntityDataForRelationshipEditor();
 
   invariant(
     source.entityType !== 'release',

root/static/scripts/relationship-editor/utility/comparators.js
@@ -7,8 +7,10 @@
  * later version: http://www.gnu.org/licenses/gpl-2.0.txt
  */
 
+import {compare} from '../../common/i18n.js';
 import {compareStrings} from '../../common/utility/compare.js';
 import type {
+  MediumWorkStateT,
   RelationshipSourceGroupT,
 } from '../types.js';
 
@@ -34,5 +36,12 @@ export function compareSourceWithSourceGroup(
 }
 
 export function compareWorks(a: WorkT, b: WorkT): number {
-  return a.id - b.id;
+  return compare(a.name, b.name) || (a.id - b.id);
+}
+
+export function compareWorkStates(
+  a: MediumWorkStateT,
+  b: MediumWorkStateT,
+): number {
+  return compareWorks(a.work, b.work);
 }

root/static/scripts/relationship-editor/utility/updateReleaseRelationships.js
@@ -14,7 +14,11 @@ import type {
   ReleaseRelationshipEditorStateT,
 } from '../types.js';
 
-import {compareRecordings, compareWorks} from './comparators.js';
+import {
+  compareRecordings,
+  compareWorks,
+  compareWorkStates,
+} from './comparators.js';
 import {
   findTargetTypeGroups,
   iterateTargetEntitiesOfType,
@@ -28,13 +32,6 @@ import {
 
 export const ADD_RELATIONSHIP: 1 = 1;
 export const REMOVE_RELATIONSHIP: 2 = 2;
-
-function compareWorkStates(
-  a: MediumWorkStateT,
-  b: MediumWorkStateT,
-): number {
-  return a.work.id - b.work.id;
-}
 
 function workHasNoRecordings(
   writableRootState: ReleaseRelationshipEditorStateT,

root/static/scripts/relationship-editor/utility/updateWorkStates.js
@@ -12,7 +12,6 @@ import {
   onNotFoundDoNothing,
 } from 'weight-balanced-tree/update';
 
-import {compare} from '../../common/i18n.js';
 import type {
   MediumWorkStateT,
   ReleaseRelationshipEditorStateT,
@@ -32,10 +31,7 @@ export function compareWorkWithWorkState(
   work: WorkT,
   workState: MediumWorkStateT,
 ): number {
-  return (
-    compare(work.name, workState.work.name) ||
-    (work.id - workState.work.id)
-  );
+  return compareWorks(work, workState.work);
 }
 
 export function* findWorkRecordings(

root/static/scripts/release/components/ReleaseRelationshipEditor.js
@@ -186,8 +186,11 @@ function compareMediumStateTuples(
   return a[0].position - b[0].position;
 }
 
-export function createInitialState(): ReleaseRelationshipEditorStateT {
+export function createInitialState(
+  source?: ?ReleaseWithMediumsAndReleaseGroupT,
+): ReleaseRelationshipEditorStateT {
   const release: ReleaseWithMediumsAndReleaseGroupT =
+    source ??
     // $FlowIgnore[unclear-type]
     (getSourceEntityDataForRelationshipEditor(): any);
 
@@ -840,7 +843,10 @@ function setWorksAsSelected(
   );
 }
 
-const reducer = reducerWithErrorHandling<
+export const reducer: ((
+  ReleaseRelationshipEditorStateT,
+  ReleaseRelationshipEditorActionT,
+) => ReleaseRelationshipEditorStateT) = reducerWithErrorHandling<
   ReleaseRelationshipEditorStateT,
   ReleaseRelationshipEditorActionT,
 >(function (

root/static/scripts/tests/relationship-editor.js
@@ -13,7 +13,9 @@ import {
   onConflictUseSecondValue,
 } from 'weight-balanced-tree/union';
 
-import {defaultContext} from '../../../context.mjs';
+import {
+  createWorkObject,
+} from '../common/entity2.js';
 import linkedEntities from '../common/linkedEntities.mjs';
 import {uniqueNegativeId} from '../common/utility/numbers.js';
 import {
@@ -33,7 +35,11 @@ import type {
   RelationshipSourceGroupT,
   RelationshipStateT,
   RelationshipTargetTypeGroupT,
+  ReleaseRelationshipEditorStateT,
 } from '../relationship-editor/types.js';
+import type {
+  RelationshipEditorActionT,
+} from '../relationship-editor/types/actions.js';
 import {
   compareLinkAttributeIds,
 } from '../relationship-editor/utility/compareRelationships.js';
@@ -48,27 +54,27 @@ import updateRelationships, {
   type RelationshipUpdateT,
   ADD_RELATIONSHIP,
 } from '../relationship-editor/utility/updateRelationships.js';
+import {
+  createInitialState as createInitialReleaseState,
+  reducer as releaseReducer,
+} from '../release/components/ReleaseRelationshipEditor.js';
 
 import {
   artist,
   event,
   recording,
+  releaseWithMediumsAndReleaseGroup,
 } from './relationship-editor/constants.js';
-
-const $c = {
-  ...defaultContext,
-  stash: {
-    ...defaultContext.stash,
-    source_entity: artist,
-  },
-};
-
-window[GLOBAL_JS_NAMESPACE] = {$c};
 
 const initialState = createInitialState({
   formName: 'edit-artist',
   seededRelationships: undefined,
+  source: artist,
 });
+
+const initialReleaseState = createInitialReleaseState(
+  releaseWithMediumsAndReleaseGroup,
+);
 
 test('merging duplicate relationships', function (t) {
   let nonEndedRelationshipWithBeginDate = {
@@ -737,6 +743,100 @@ test('MBS-12937: Changing credits for other relationships without modifying the 
   );
 });
 
+test('MBS-12976: Changing a work can cause duplication/key errors', function (t) {
+  t.plan(1);
+
+  const work1 = createWorkObject({
+    id: 3,
+    name: 'A',
+  });
+
+  const work2 = createWorkObject({
+    id: 2,
+    name: 'B',
+  });
+
+  const work3 = createWorkObject({
+    id: 1,
+    name: 'C',
+  });
+
+  const relationship1 = Object.freeze({
+    _lineage: [],
+    _original: null,
+    _status: REL_STATUS_ADD,
+    attributes: null,
+    begin_date: null,
+    editsPending: false,
+    end_date: null,
+    ended: false,
+    entity0: recording,
+    entity0_credit: '',
+    entity1: work1,
+    entity1_credit: '',
+    id: -1,
+    linkOrder: 0,
+    linkTypeID: 278,
+  });
+
+  const relationship2 = Object.freeze({
+    ...relationship1,
+    id: -2,
+    entity1: work3,
+  });
+
+  // Start with works A, C
+  let state = addRelationshipsToRelease(
+    initialReleaseState,
+    recording,
+    [relationship1, relationship2],
+  );
+
+  // Replace work C with B
+  state = releaseReducer(
+    state,
+    {
+      batchSelectionCount: undefined,
+      creditsToChangeForSource: '',
+      creditsToChangeForTarget: '',
+      newRelationshipState: Object.freeze({
+        ...relationship2,
+        entity1: work2,
+      }),
+      oldRelationshipState: relationship2,
+      sourceEntity: recording,
+      type: 'update-relationship-state',
+    },
+  );
+
+  const relatedWorks = tree.toArray(
+    tree.toArray(
+      tree.toArray(state.mediums)[0][1],
+    )[0].relatedWorks,
+  ).map(x => x.work);
+
+  t.deepEqual(
+    relatedWorks,
+    [work1, work2],
+    'work C was replaced by work B',
+  );
+});
+
+function getAddRelationshipAction(
+  source: CoreEntityT,
+  relationship: RelationshipStateT,
+): RelationshipEditorActionT {
+  return {
+    batchSelectionCount: undefined,
+    creditsToChangeForSource: '',
+    creditsToChangeForTarget: '',
+    newRelationshipState: relationship,
+    oldRelationshipState: null,
+    sourceEntity: source,
+    type: 'update-relationship-state',
+  };
+}
+
 function addRelationships(
   rootState: RelationshipEditorStateT,
   source: CoreEntityT,
@@ -749,6 +849,18 @@ function addRelationships(
   return newState;
 }
 
+function addRelationshipsToRelease(
+  rootState: ReleaseRelationshipEditorStateT,
+  source: CoreEntityT,
+  relationships: $ReadOnlyArray<RelationshipStateT>,
+): ReleaseRelationshipEditorStateT {
+  let newState = rootState;
+  relationships.forEach((relationship) => {
+    newState = addRelationshipToRelease(newState, source, relationship);
+  });
+  return newState;
+}
+
 function addRelationship(
   rootState: RelationshipEditorStateT,
   source: CoreEntityT,
@@ -756,21 +868,26 @@ function addRelationship(
 ): RelationshipEditorStateT {
   return reducer(
     rootState,
-    {
-      batchSelectionCount: undefined,
-      creditsToChangeForSource: '',
-      creditsToChangeForTarget: '',
-      newRelationshipState: relationship,
-      oldRelationshipState: null,
-      sourceEntity: source,
-      type: 'update-relationship-state',
-    },
+    getAddRelationshipAction(source, relationship),
+  );
+}
+
+function addRelationshipToRelease(
+  rootState: ReleaseRelationshipEditorStateT,
+  source: CoreEntityT,
+  relationship: RelationshipStateT,
+): ReleaseRelationshipEditorStateT {
+  return releaseReducer(
+    rootState,
+    getAddRelationshipAction(source, relationship),
   );
 }
 
 function currentRelationshipsEqual(
   t: tape$Context,
-  rootState: RelationshipEditorStateT,
+  rootState:
+    | RelationshipEditorStateT
+    | ReleaseRelationshipEditorStateT,
   relationships: $ReadOnlyArray<RelationshipStateT | null>,
   msg: string,
 ) {

root/static/scripts/tests/relationship-editor/constants.js
@@ -11,11 +11,14 @@ import {
   createArtistObject,
   createEventObject,
   createRecordingObject,
+  createReleaseGroupObject,
   createReleaseObject,
 } from '../../common/entity2.js';
+import {uniqueNegativeId} from '../../common/utility/numbers.js';
 import {REL_STATUS_ADD} from '../../relationship-editor/constants.js';
 import type {
   RelationshipStateT,
+  ReleaseWithMediumsAndReleaseGroupT,
 } from '../../relationship-editor/types.js';
 
 export const artist: ArtistT = createArtistObject({
@@ -33,6 +36,56 @@ export const recording: RecordingT = createRecordingObject({
 export const release: ReleaseT = createReleaseObject({
   name: 'Release',
 });
+
+const mediumId = uniqueNegativeId();
+
+const track: TrackWithRecordingT = {
+  artist: '',
+  artistCredit: {
+    names: [
+      {
+        artist,
+        joinPhrase: '',
+        name: 'Artist',
+      },
+    ],
+  },
+  editsPending: false,
+  entityType: 'track',
+  gid: 'c8f65672-e007-453d-8ecc-4a0bd9cb4dc6',
+  id: uniqueNegativeId(),
+  isDataTrack: false,
+  last_updated: null,
+  length: 10000,
+  medium_id: mediumId,
+  medium: null,
+  name: 'Track',
+  number: '1',
+  position: 1,
+  recording,
+};
+
+export const releaseWithMediumsAndReleaseGroup:
+  ReleaseWithMediumsAndReleaseGroupT = {
+    ...release,
+    releaseGroup: createReleaseGroupObject(),
+    mediums: [
+      {
+        cdtocs: [],
+        editsPending: false,
+        entityType: 'medium',
+        format_id: null,
+        format: null,
+        id: mediumId,
+        last_updated: null,
+        name: '',
+        position: 1,
+        release_id: release.id,
+        track_count: null,
+        tracks: [track],
+      },
+    ],
+  };
 
 export const emptyRelationship: RelationshipStateT = {
   _lineage: [],