9c6ecc3da39756be14b90366bd5c95a53a2a24c8
Nicolás Tamargo <reosarevok@gmail.com>
2023-04-28 12:44:09 +0300

    MBS-12948: Wrap long entity track names in rels editor
    Without this, very long names make the editor scroll
    rather than wrapping.

--- root/static/scripts/release/components/TrackRelationshipEditor.js
+++ root/static/scripts/release/components/TrackRelationshipEditor.js
@@ -56,6 +56,7 @@ const TrackLink = React.memo<TrackLinkPropsT>(({
   if (showArtists) {
     trackLink = (
       <DescriptiveLink
+        className="wrap-anywhere"
         content={track.name}
         customArtistCredit={track.artistCredit}
         entity={track.recording}
@@ -66,6 +67,7 @@ const TrackLink = React.memo<TrackLinkPropsT>(({
   } else {
     trackLink = (
       <EntityLink
+        className="wrap-anywhere"
         content={track.name}
         entity={track.recording}
         showDisambiguation={false}
