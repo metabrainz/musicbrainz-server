da5ac02a032cbb55b551d01e057d48da13507d8d
Nicolás Tamargo <reosarevok@gmail.com>
2023-05-22 21:01:20 +0300

    Separate fullwidthLatin tests for each function

--- root/static/scripts/tests/utility/fullwidthLatin.js
+++ root/static/scripts/tests/utility/fullwidthLatin.js
@@ -11,23 +11,13 @@ import test from 'tape';
 
 import * as fullwidthLatin from '../../edit/utility/fullwidthLatin.js';
 
-test('fullwidthLatin', function (t) {
-  t.plan(17);
+test('hasFullwidthLatin', function (t) {
+  t.plan(7);
 
   t.equal(
     fullwidthLatin.hasFullwidthLatin(undefined),
     false,
     'undefined has no fullwidth Latin',
-  );
-  t.equal(
-    fullwidthLatin.fromFullwidthLatin(undefined),
-    '',
-    'undefined (fromFullwidthLatin) empty',
-  );
-  t.equal(
-    fullwidthLatin.toFullwidthLatin(undefined),
-    '',
-    'undefined (toFullwidthLatin) empty',
   );
 
   t.equal(
@@ -35,31 +25,11 @@ test('fullwidthLatin', function (t) {
     false,
     'null has no fullwidth Latin',
   );
-  t.equal(
-    fullwidthLatin.fromFullwidthLatin(null),
-    '',
-    'null (fromFullwidthLatin) empty',
-  );
-  t.equal(
-    fullwidthLatin.toFullwidthLatin(null),
-    '',
-    'null (toFullwidthLatin) empty',
-  );
 
   t.equal(
     fullwidthLatin.hasFullwidthLatin(''),
     false,
     'empty has no fullwidth Latin',
-  );
-  t.equal(
-    fullwidthLatin.fromFullwidthLatin(''),
-    '',
-    'empty (fromFullwidthLatin) empty',
-  );
-  t.equal(
-    fullwidthLatin.toFullwidthLatin(''),
-    '',
-    'empty (toFullwidthLatin) empty',
   );
 
   t.equal(
@@ -67,36 +37,87 @@ test('fullwidthLatin', function (t) {
     true,
     'fully fullwidth Latin has fullwidth Latin',
   );
+
   t.equal(
     fullwidthLatin.hasFullwidthLatin(' ｆｅａｔ. '),
     true,
     'fullwidth Latin letters are fullwidth Latin',
   );
+
   t.equal(
     fullwidthLatin.hasFullwidthLatin('　feat.　'),
     true,
     'ideographic space is fullwidth Latin',
   );
+
   t.equal(
     fullwidthLatin.hasFullwidthLatin(' feat． '),
     true,
     'fullwidth full stop is fullwidth Latin',
   );
+});
+
+test('fromFullwidthLatin', function (t) {
+  t.plan(5);
+
+  t.equal(
+    fullwidthLatin.fromFullwidthLatin(undefined),
+    '',
+    'undefined (fromFullwidthLatin) empty',
+  );
+
+  t.equal(
+    fullwidthLatin.fromFullwidthLatin(null),
+    '',
+    'null (fromFullwidthLatin) empty',
+  );
+
+  t.equal(
+    fullwidthLatin.fromFullwidthLatin(''),
+    '',
+    'empty (fromFullwidthLatin) empty',
+  );
+
   t.equal(
     fullwidthLatin.fromFullwidthLatin('　ｆｅａｔ．　'),
     ' feat. ',
     'fully converted fromFullwidthLatin',
   );
+
   t.equal(
     fullwidthLatin.fromFullwidthLatin(' ｆｅａｔ. '),
     ' feat. ',
     'partly converted fromFullwidthLatin',
   );
+});
+
+test('toFullwidthLatin', function (t) {
+  t.plan(5);
+
+  t.equal(
+    fullwidthLatin.toFullwidthLatin(undefined),
+    '',
+    'undefined (toFullwidthLatin) empty',
+  );
+
+  t.equal(
+    fullwidthLatin.toFullwidthLatin(null),
+    '',
+    'null (toFullwidthLatin) empty',
+  );
+
+  t.equal(
+    fullwidthLatin.toFullwidthLatin(''),
+    '',
+    'empty (toFullwidthLatin) empty',
+  );
+
   t.equal(
     fullwidthLatin.toFullwidthLatin('　feat．　'),
     '　ｆｅａｔ．　',
     'partly converted toFullwidthLatin',
   );
+
   t.equal(
     fullwidthLatin.toFullwidthLatin(' feat. '),
     '　ｆｅａｔ．　',
