f610f704b5b5260b9352524260413c0ee6e6242a
Nicolás Tamargo <reosarevok@metabrainz.org>
2023-07-14 13:37:44 +0300

    MBS-13162: Handle Threads links (#2981)
    This adds threads.net links to the sidebar, and cleans them
    to use https://www. which seems to be the default.
    It also selects (mandatory) social media for any entity types
    that support it (currently artist, event, label, place and series),
    while selecting stream for free for recording and blocking other
    free streaming entities.
    Social media links must be user pages, while streaming links
    must be threads ("tweets").
    
    This matches what we do with Twitter, which this is mostly
    a straight clone of.

---
+++ lib/MusicBrainz/Server/Entity/URL/Threads.pm
@@ -0,0 +1,32 @@
+package MusicBrainz::Server::Entity::URL::Threads;
+
+use Moose;
+
+extends 'MusicBrainz::Server::Entity::URL';
+with 'MusicBrainz::Server::Entity::URL::Sidebar';
+
+sub sidebar_name {
+    my $self = shift;
+
+    if ($self->url =~ m{^https?://(?:www.)?threads.net/(@[^/]+)/?$}i) {
+        return $1;
+    }
+    else {
+        return 'Threads';
+    }
+};
+
+
+__PACKAGE__->meta->make_immutable;
+no Moose;
+1;
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2015 MetaBrainz Foundation
+
+This file is part of MusicBrainz, the open internet music database,
+and is licensed under the GPL version 2, or (at your option) any
+later version: http://www.gnu.org/licenses/gpl-2.0.txt
+
+=cut
