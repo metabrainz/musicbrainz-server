d3684f635eb7c1814b9725a7c36916cb85c12111
Nicolás Tamargo <reosarevok@gmail.com>
2023-09-04 15:18:38 +0300

    MBS-10629: Show Anghami links on the sidebar

--- lib/MusicBrainz/Server/Data/URL.pm
+++ lib/MusicBrainz/Server/Data/URL.pm
@@ -23,6 +23,7 @@ my %URL_SPECIALIZATIONS = (
     '45worlds'            => qr{^https?://(?:www\.)?45worlds\.com/}i,
     'Allmusic'            => qr{^https?://(?:www\.)?allmusic\.com/}i,
     'AmazonMusic'         => qr{^https:\/\/music\.amazon\.(?:ae|at|com\.au|com\.br|ca|cn|com|de|es|fr|in|it|jp|co\.jp|com\.mx|nl|pl|se|sg|com\.tr|co\.uk)/}i,
+    'Anghami'             => qr{^https?://([^/]+\.)?anghami\.com/}i,
     'AniDB'               => qr{^https?://(?:www\.)?anidb\.net/}i,
     'AnimeNewsNetwork'    => qr{^https?://(?:www\.)?animenewsnetwork\.com/}i,
     'AnisonGeneration'    => qr{^https?://anison\.info/}i,
