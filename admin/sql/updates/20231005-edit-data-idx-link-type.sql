f89866052f2e42c473af0e39ae8b9f7d1233b753
Michael Wiencek <mwtuea@gmail.com>
2023-10-05 11:02:47 +0200

    fix edit_data_idx_link_type

---
+++ admin/sql/updates/20231005-edit-data-idx-link-type.sql
@@ -0,0 +1,17 @@
+\set ON_ERROR_STOP 1
+
+BEGIN;
+
+DROP INDEX CONCURRENTLY IF EXISTS edit_data_idx_link_type;
+
+CREATE INDEX CONCURRENTLY edit_data_idx_link_type ON edit_data USING GIN (
+    array_remove(ARRAY[
+                     (data#>>'{link_type,id}')::int,
+                     (data#>>'{link,link_type,id}')::int,
+                     (data#>>'{old,link_type,id}')::int,
+                     (data#>>'{new,link_type,id}')::int,
+                     (data#>>'{relationship,link,type,id}')::int
+                 ], NULL)
+);
+
+COMMIT;
