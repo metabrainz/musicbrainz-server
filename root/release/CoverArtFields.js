616d001547e3514c48c9fc09916be869a7fc6348
Michael Wiencek <mwtuea@gmail.com>
2024-01-22 15:21:43 -0600

    Add root/components/ArtFields.js

--- root/release/CoverArtFields.js
+++ root/release/CoverArtFields.js
@@ -7,91 +7,26 @@
  * later version: http://www.gnu.org/licenses/gpl-2.0.txt
  */
 
-import {unwrapNl} from '../static/scripts/common/i18n.js';
-import FormRow from '../static/scripts/edit/components/FormRow.js';
-import FormRowTextLong
-  from '../static/scripts/edit/components/FormRowTextLong.js';
-
-type Props = {
-  +form: FormT<{
-    +comment: FieldT<string | null>,
-    +nonce: FieldT<string>,
-    +position?: FieldT<number>,
-    +type_id: FieldT<Array<StrOrNum>>,
-    ...
-  }>,
-  +typeIdOptions: SelectOptionsT,
-};
+import ArtFields, {
+  type CommonProps as Props,
+} from '../components/ArtFields.js';
 
 const CoverArtFields = ({
   form,
   typeIdOptions,
 }: Props): React.MixedElement => {
-  const typeIdField = form.field.type_id;
-  const selectedTypeIds = new Set(typeIdField.value.map(
-    value => String(value),
-  ));
   return (
-    <>
-      {form.field.position ? (
-        <input
-          id={'id-' + form.field.position.html_name}
-          name={form.field.position.html_name}
-          type="hidden"
-          value={form.field.position.value}
-        />
-      ) : null}
-      {form.field.nonce ? (
-        <input
-          id={'id-' + form.field.nonce.html_name}
-          name={form.field.nonce.html_name}
-          type="hidden"
-          value={form.field.nonce.value}
-        />
-      ) : null}
-      <FormRow>
-        <fieldset className="cover-art-types row">
-          <legend>{addColonText(l('Type'))}</legend>
-          <ul className="cover-art-type-checkboxes">
-            {typeIdOptions.map(option => (
-              <li key={option.value}>
-                <label>
-                  <input
-                    defaultChecked={selectedTypeIds.has(String(option.value))}
-                    name={typeIdField.html_name}
-                    type="checkbox"
-                    value={option.value}
-                  />
-                  {unwrapNl<string>(option.label)}
-                </label>
-              </li>
-            ))}
-          </ul>
-        </fieldset>
-        <ul
-          className="errors"
-          id="cover-art-type-error"
-          style={{display: 'none'}}
-        >
-          <li>{l('Choose one or more cover art types for this image')}</li>
-        </ul>
-        <FormRow>
-          <label>{'\u00A0'}</label>
-          <p>
-            {exp.l(
-              `Please see the {doc|Cover Art Types} documentation
-               for a description of these types.`,
-              {doc: {href: '/doc/Cover_Art/Types', target: '_blank'}},
-            )}
-          </p>
-        </FormRow>
-      </FormRow>
-      <FormRowTextLong
-        field={form.field.comment}
-        label={l('Comment:')}
-        uncontrolled
-      />
-    </>
+    <ArtFields
+      archiveName="cover"
+      chooseMessage={l('Choose one or more cover art types for this image')}
+      documentationMessage={exp.l(
+        `Please see the {doc|Cover Art Types} documentation
+         for a description of these types.`,
+        {doc: {href: '/doc/Cover_Art/Types', target: '_blank'}},
+      )}
+      form={form}
+      typeIdOptions={typeIdOptions}
+    />
   );
 };
 
