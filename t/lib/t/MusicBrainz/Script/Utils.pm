935ecd9ef7e3ac88c3462e13cf963ec1ad764a2d
Michael Wiencek <mwtuea@gmail.com>
2024-02-24 14:38:05 -0600

    Add shared `find_mbdump_file` to `Script::Utils`
    This merges the separate implementations of `find_file` contained in
    admin/MBImport.pl and admin/replication/ImportReplicationChanges into
    `Script::Utils`, and adds tests for said function.
    
    The main difference between the two was that MBImport.pl's implementation would
    return an array of matching files, rather than the first match. We now use
    Perl's `wantarray` built-in to distinguish between the two cases.

---
+++ t/lib/t/MusicBrainz/Script/Utils.pm
@@ -0,0 +1,63 @@
+package t::MusicBrainz::Script::Utils;
+
+use strict;
+use warnings;
+
+use File::Spec;
+use File::Temp qw( tempdir );
+use Test::More;
+use Test::Routine;
+
+use MusicBrainz::Script::Utils qw( find_mbdump_file );
+
+test 'find_mbdump_file works as expected' => sub {
+    my $test = shift;
+
+    my $dir1 = tempdir('find_mbdump_file-XXXXXXXX', DIR => '/tmp', CLEANUP => 1);
+    my $dir2 = File::Spec->catdir($dir1, 'mbdump');
+    my $dir3 = File::Spec->catdir($dir1, 'recording');
+
+    system('mkdir', $dir2, $dir3);
+
+    my $f1_path = File::Spec->catfile($dir1, 'artist');
+    my $f2_path = File::Spec->catfile($dir2, 'recording');
+    my $f3_path = File::Spec->catfile($dir2, 'artist');
+
+    system('touch', $f1_path, $f2_path, $f3_path);
+
+    my ($result, @result);
+
+    $result = find_mbdump_file('dne', $f1_path);
+    is($result, undef,
+       'undef is returned for non-existent file in scalar context');
+
+    @result = find_mbdump_file('dne', $f1_path);
+    is(scalar @result, 0,
+       'empty list is returned for non-existent file in list context');
+
+    $result = find_mbdump_file('recording', $dir3);
+    is($result, undef,
+       'undef is returned for file matching directory name ' .
+       'in scalar context');
+
+    $result = find_mbdump_file('artist', $f1_path);
+    is($result, $f1_path, 'scalar file is found by direct path');
+
+    $result = find_mbdump_file('artist', $dir1);
+    is($result, $f1_path, 'scalar file is found in directory');
+
+    $result = find_mbdump_file('recording', $dir1);
+    is($result, $f2_path, 'scalar file is found in mbdump sub-directory');
+
+    @result = find_mbdump_file('artist', $f1_path, $dir1, $dir2);
+    is(scalar @result, 3, 'three results are found');
+    is($result[0], $f1_path, 'first file is correctly found');
+    is($result[1], $f1_path, 'second file is correctly found');
+    is($result[2], $f3_path, 'third file is correctly found');
+
+    unlink $f1_path, $f2_path;
+    rmdir($dir1);
+    rmdir($dir2);
+};
+
+1;
