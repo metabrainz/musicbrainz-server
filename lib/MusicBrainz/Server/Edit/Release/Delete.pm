70a953f961195a4f5576515c28ae81b7bf7bef57
Nicolás Tamargo <reosarevok@gmail.com>
2024-02-28 12:56:38 +0100

    MBS-13328: Show Remove release edits on track artist / recording history
    While other release-level edits do not usually affect the tracks
    and recordings on the release's mediums, removing the release
    will of course also remove the mediums, so it's equivalent
    to removing all the mediums in it and it should appear in the history
    for any recordings in said mediums.
    
    Admittedly, in many cases this will lead to the recording
    being subsequently removed anyway, but especially
    for orphaned recordings which had relationships
    and were kept behind this would be good info to have.

--- lib/MusicBrainz/Server/Edit/Release/Delete.pm
+++ lib/MusicBrainz/Server/Edit/Release/Delete.pm
@@ -13,6 +13,31 @@ sub edit_name { N_lp('Remove release', 'edit type') }
 sub _delete_model { 'Release' }
 sub release_id { shift->entity_id }
 
+around _build_related_entities => sub {
+    my ($orig, $self) = splice(@_, 0, 2);
+    my $related = $self->$orig(@_);
+
+    my $release = $self->c->model('Release')->get_by_id(
+        $self->data->{entity_id},
+    );
+    $self->c->model('Release')->load_related_info($release);
+
+    for my $medium (@{ $release->{mediums} }) {
+        $self->c->model('Track')->load_for_mediums($medium);
+        my @tracks = $medium->all_tracks;
+        $self->c->model('ArtistCredit')->load(@tracks);
+
+        push @{ $related->{artist} }, map {
+            map { $_->{artist}{id} } @{ $_->{artist_credit}->{names} }
+        } @tracks;
+
+        push @{ $related->{recording} },
+            map { $_->{recording_id} } @tracks;
+    }
+
+    return $related;
+};
+
 override 'foreign_keys' => sub {
     my $self = shift;
     my $data = super();
