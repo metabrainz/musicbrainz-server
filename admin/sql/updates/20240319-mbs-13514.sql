3a94f5935d245c695e7c468e41dfdb20b02907e6
Michael Wiencek <mwtuea@gmail.com>
2024-05-06 23:17:42 -0500

    Split upgrade script for MBS-13514
     * CreateConstraints.sql is not run on mirrors.
    
     * The original inline constraints *were* created on mirrors, so the `DROP`
       statements remain in a `*-mirror.sql` script.

--- admin/sql/updates/20240319-mbs-13514.sql
+++ admin/sql/updates/20240319-mbs-13514.sql
@@ -2,10 +2,6 @@
 
 BEGIN;
 
-ALTER TABLE label DROP CONSTRAINT IF EXISTS label_label_code_check;
-
-ALTER TABLE label DROP CONSTRAINT IF EXISTS label_code_length;
-
 ALTER TABLE label ADD CONSTRAINT label_code_length CHECK (label_code > 0 AND label_code < 1000000);
 
 COMMIT;
