e5d0ff4d39f8e35f08f8c349532b1ed3d550dec2
Michael Wiencek <mwtuea@gmail.com>
2024-05-22 22:14:37 -0500

    Add EAA edit class stubs linking to `BetaOnlyEdit`
    This is a prerequisite step for IMG-84 / PR #3150.
    
    It adds temporary (stub) edit classes that don't do anything except display the
    type of edit. The core display of the edits is delegated to the `BetaOnlyEdit`
    component added in a8e12fd3f5873d61863b416b74f2434133a117bc.

---
+++ lib/MusicBrainz/Server/Edit/Event/RemoveEventArt.pm
@@ -0,0 +1,25 @@
+package MusicBrainz::Server::Edit::Event::RemoveEventArt;
+use Moose;
+use namespace::autoclean;
+
+use MusicBrainz::Server::Constants qw( $EDIT_EVENT_REMOVE_EVENT_ART );
+use MusicBrainz::Server::Translation qw( N_lp );
+
+extends 'MusicBrainz::Server::Edit';
+
+sub edit_kind { 'remove' }
+sub edit_name { N_lp('Remove event art', 'singular, edit type') }
+sub edit_template { 'RemoveEventArt' }
+sub edit_type { $EDIT_EVENT_REMOVE_EVENT_ART }
+
+1;
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2023 MetaBrainz Foundation
+
+This file is part of MusicBrainz, the open internet music database,
+and is licensed under the GPL version 2, or (at your option) any
+later version: http://www.gnu.org/licenses/gpl-2.0.txt
+
+=cut
