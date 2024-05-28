ba6c1367dfde4eeb096a9d0736411b315f8c4e60
Robert Kaye <rob@musicbrainz.org>
2024-05-28 12:42:18 +0200

    Add new menu link

--- root/layout/components/BottomMenu.js
+++ root/layout/components/BottomMenu.js
@@ -137,6 +137,9 @@ component AboutMenu() {
           <a href="https://metabrainz.org/gdpr">{l('GDPR compliance')}</a>
         </li>
         <li>
+          <a href="/doc/Copyright_DMCA">{l('Copyright / DMCA Issues')}</a>
+        </li>
+        <li>
           <a href="/doc/Data_Removal_Policy">{l('Data removal policy')}</a>
         </li>
         <li className="separator">
