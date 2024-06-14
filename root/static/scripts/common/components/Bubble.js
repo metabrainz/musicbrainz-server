719e88cb593b229892ab0799ea8b236c5a448096
Michael Wiencek <mwtuea@gmail.com>
2024-06-14 12:42:41 -0500

    Use floating-ui for the new `Bubble` component

--- root/static/scripts/common/components/Bubble.js
+++ root/static/scripts/common/components/Bubble.js
@@ -7,7 +7,15 @@
  * later version: http://www.gnu.org/licenses/gpl-2.0.txt
  */
 
-import $ from 'jquery';
+import {
+  type UseFloatingReturn,
+  arrow,
+  autoUpdate,
+  FloatingArrow,
+  offset,
+  size,
+  useFloating,
+} from '@floating-ui/react';
 import * as React from 'react';
 
 component Bubble(
@@ -15,33 +23,61 @@ component Bubble(
   controlRef: {+current: HTMLElement | null},
   id: string,
 ) {
-  const bubbleDivRef = React.useRef<HTMLDivElement | null>(null);
+  const arrowRef = React.useRef<Element | null>(null);
+
+  const {refs, floatingStyles, context}: UseFloatingReturn = useFloating({
+    middleware: [
+      arrow({element: arrowRef}),
+      offset(16),
+      size({
+        apply({availableWidth}) {
+          const floatingElement = refs.floating.current;
+          if (floatingElement) {
+            floatingElement.style.width =
+              String(availableWidth - 16) + 'px';
+          }
+        },
+      }),
+    ],
+    open: true,
+    placement: 'right',
+    whileElementsMounted: (referenceEl, floatingEl, update) => {
+      return autoUpdate(referenceEl, floatingEl, update, {
+        ancestorResize: true,
+        ancestorScroll: false,
+        animationFrame: false,
+        elementResize: true,
+        layoutShift: true,
+      });
+    },
+  });
+
   React.useEffect(() => {
-    const $bubble = $(bubbleDivRef.current);
-    const $parent = $bubble.parent();
-
-    $bubble
-      .width($parent.width() - 24)
-      .position({
-        at: 'right center',
-        collision: 'fit none',
-        my: 'left top-30',
-        of: controlRef.current,
-        within: $parent,
-      })
-      .addClass('left-tail');
+    refs.setReference(controlRef.current);
   }, [controlRef]);
-
 
   return (
     <div
       className="bubble"
       id={id}
-      ref={bubbleDivRef}
-      // We hide this by a state check so we don't want the CSS display: none
-      style={{display: 'block'}}
+      ref={refs.setFloating}
+      style={{
+        ...floatingStyles,
+        /*
+         * We hide this by a state check so we don't want the CSS
+         * display: none.
+         */
+        display: 'block',
+      }}
     >
       {children}
+      <FloatingArrow
+        context={context}
+        fill="currentColor"
+        height={12}
+        ref={arrowRef}
+        width={24}
+      />
     </div>
   );
 }
