38418252cef14be5b34d3d2b4c847b1eaa431b1a
Michael Wiencek <mwtuea@gmail.com>
2024-07-13 10:50:14 -0500

    Add JS test fixtures directory
    I'd like to put actual Autocomplete2 tests in
    root/static/scripts/tests/autocomplete2.js, so I'm moving the existing files to
    a fixtures/ subdirectory. (It's currently only used by
    t/selenium/Autocomplete2.json5.) dialog.html isn't currently used by any test,
    but I've moved it there too.

--- webpack/tests.config.mjs
+++ webpack/tests.config.mjs
@@ -26,8 +26,8 @@ const webTestsConfig = {
   devtool: 'source-map',
 
   entry: {
-    'autocomplete2': path.resolve(SCRIPTS_DIR, 'tests', 'autocomplete2.js'),
-    'dialog-test': path.resolve(SCRIPTS_DIR, 'tests', 'dialog.js'),
+    'autocomplete2': path.resolve(SCRIPTS_DIR, 'tests', 'fixtures', 'autocomplete2.js'),
+    'dialog-test': path.resolve(SCRIPTS_DIR, 'tests', 'fixtures', 'dialog.js'),
     'web-tests': path.resolve(SCRIPTS_DIR, 'tests', 'browser-runner.js'),
     'todo-list': path.resolve(SCRIPTS_DIR, 'tests', 'examples', 'todo-list', 'index.js'),
   },
