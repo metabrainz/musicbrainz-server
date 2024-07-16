metabrainz/musicbrainz-server

commit f0613577fd0e58060417afd3ce46a718f8baeb8e

MBS-13579: Preserve existing release labels duplicated by seeded ones (#3260)
Prior to this commit, if you seeded a release label to an existing release that
duplicated an existing release label on that release, the release editor would
throw an error upon submission, as it would be trying to submit data that
already exists.

This patch preserves the existing release label (and its database row ID),
discarding the seeded one. Note that a "release label" is a label and catalog
number pair, meaning the catalog number is included for de-duplication
purposes.

The ticket presents three use cases. The first one is still possible, the
second one is still impossible, and the third one is now possible. An
alternative implementation would be to concatenate the seeded release labels
with the existing ones (discarding any duplicates, like here), and adding a
`removed` field to allow removing existing release labels. However, you'd need
to know the label IDs (and associated catalog numbers) of the release labels
you'd like to remove in advance, which isn't currently a requirement: it
becomes harder to replace all of the existing release labels with a new set.

There is one additional feature this commit implements: if you seed a release
label, and there already exists one on the release with the same label (but
different catalog number), we will edit the existing release label with the new
catalog number. The same is true if the catalog number matches but not the
label (but label matches are always preferred). An exception to this is if the
existing release label is being preserved by the seeded parameters.

root/static/scripts/release-editor/edits.js
@@ -40,6 +40,11 @@ var newReleaseLabels = utils.withRelease(function (release) {
 
 const getReleaseLabel = x => String(x.release_label ?? '');
 
+const getReleaseLabelKey = (releaseLabel) => (
+  String(releaseLabel.label ?? '') + '\0' +
+  clean(releaseLabel.catalog_number)
+);
+
 releaseEditor.edits = {
 
   releaseGroup: function (release) {
@@ -106,10 +111,17 @@ releaseEditor.edits = {
 
   releaseLabel: function (release) {
     var newLabels = newReleaseLabels().map(MB.edit.fields.releaseLabel);
-    var oldLabels = release.labels.original();
+    var oldLabels = release.labels.original().slice(0);
 
-    var newLabelsByID = keyBy(newLabels, getReleaseLabel);
-    var oldLabelsByID = keyBy(oldLabels, getReleaseLabel);
+    const newLabelsByID = keyBy(
+      newLabels.filter((releaseLabel) => (
+        nonEmpty(getReleaseLabel(releaseLabel))
+      )),
+      getReleaseLabel,
+    );
+    const oldLabelsByID = keyBy(oldLabels, getReleaseLabel);
+    const newLabelsByKey = keyBy(newLabels, getReleaseLabelKey);
+    const oldLabelsByKey = keyBy(oldLabels, getReleaseLabelKey);
 
     var edits = [];
 
@@ -123,20 +135,57 @@ releaseEditor.edits = {
           // Edit ReleaseLabel
           edits.push(MB.edit.releaseEditReleaseLabel(newLabel));
         }
-      } else {
-        // Add ReleaseLabel
-        newLabel = {...newLabel};
+      } else if (!oldLabelsByKey.has(getReleaseLabelKey(newLabel))) {
+        /*
+         * If there's a removed release label with the same label or
+         * catalog number, we can edit rather than remove it.
+         */
+        let oldLabel;
+        let oldLabelIndex = -1;
 
-        if (newLabel.label || newLabel.catalog_number) {
-          newLabel.release = release.gid() || null;
-          edits.push(MB.edit.releaseAddReleaseLabel(newLabel));
+        if (newLabel.label) {
+          oldLabelIndex = oldLabels.findIndex(
+            (releaseLabel) => (
+              !newLabelsByKey.has(getReleaseLabelKey(releaseLabel)) &&
+              releaseLabel.label === newLabel.label
+            ),
+          );
+        }
+        if (oldLabelIndex < 0 && nonEmpty(newLabel.catalog_number)) {
+          oldLabelIndex = oldLabels.findIndex(
+            (releaseLabel) => (
+              !newLabelsByKey.has(getReleaseLabelKey(releaseLabel)) &&
+              releaseLabel.catalog_number === newLabel.catalog_number
+            ),
+          );
+        }
+        if (oldLabelIndex >= 0) {
+          oldLabel = oldLabels[oldLabelIndex];
+          oldLabels.splice(oldLabelIndex, 1);
+        }
+
+        if (oldLabel && !deepEqual(newLabel, oldLabel)) {
+          // Edit ReleaseLabel
+          edits.push(MB.edit.releaseEditReleaseLabel({
+            ...newLabel,
+            release_label: oldLabel.release_label,
+          }));
+        } else {
+          // Add ReleaseLabel
+          newLabel = {...newLabel};
+
+          if (newLabel.label || newLabel.catalog_number) {
+            newLabel.release = release.gid() || null;
+            edits.push(MB.edit.releaseAddReleaseLabel(newLabel));
+          }
         }
       }
     }
 
     for (let oldLabel of oldLabels) {
       const id = getReleaseLabel(oldLabel);
-      const newLabel = newLabelsByID.get(id);
+      const newLabel = newLabelsByID.get(id) ??
+        newLabelsByKey.get(getReleaseLabelKey(oldLabel));
 
       if (!newLabel || !(newLabel.label || newLabel.catalog_number)) {
         // Delete ReleaseLabel

t/selenium.mjs
@@ -714,6 +714,7 @@ const seleniumTests = [
   },
   {name: 'release-editor/MBS-13207.json5', login: true},
   {name: 'release-editor/MBS-13273.json5', login: true},
+  {name: 'release-editor/MBS-13579.json5', login: true},
   {
     name: 'Check_Duplicates.json5',
     login: true,

root/static/scripts/tests/release-editor/seeds/mbs-13579-part1.html
@@ -1,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+  <body>
+    <form action="/release/24d4159a-99d9-425d-a7b8-1b9ec0261a33/edit" method="POST">
+      <!-- Remove ISO Records -->
+      <!-- Add Columbia - 88875173862 -->
+      <input type="hidden" name="labels.0.mbid" value="011d1192-6f65-45bd-85c4-0400dd45693e" />
+      <input type="hidden" name="labels.0.catalog_number" value="88875173862" />
+      <button type="submit">Submit</button>
+    </form>
+  </body>
+</html>

root/static/scripts/tests/release-editor/seeds/mbs-13579-part2.html
@@ -1,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+  <body>
+    <!-- This is intended to be run after mbs-13579-part1.html. -->
+    <form action="/release/24d4159a-99d9-425d-a7b8-1b9ec0261a33/edit" method="POST">
+      <!-- Add ISO Records -->
+      <!-- Preserve Columbia - 88875173862 -->
+      <input type="hidden" name="labels.0.mbid" value="10f97e56-1ebe-4b1e-848f-9c5ae0f292ec" />
+      <input type="hidden" name="labels.1.mbid" value="011d1192-6f65-45bd-85c4-0400dd45693e" />
+      <input type="hidden" name="labels.1.catalog_number" value="88875173862" />
+      <button type="submit">Submit</button>
+    </form>
+  </body>
+</html>

root/static/scripts/tests/release-editor/seeds/mbs-13579-part3.html
@@ -1,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+  <body>
+    <!-- This is intended to be run after mbs-13579-part2.html. -->
+    <form action="/release/24d4159a-99d9-425d-a7b8-1b9ec0261a33/edit" method="POST">
+      <!-- Edit ISO Records (adding ISO-3103, seeding the label MBID) -->
+      <!-- Edit Columbia (with 88875173862-1, seeding only the label name) -->
+      <input type="hidden" name="labels.0.mbid" value="10f97e56-1ebe-4b1e-848f-9c5ae0f292ec" />
+      <input type="hidden" name="labels.0.catalog_number" value="ISO-3103" />
+      <input type="hidden" name="labels.1.name" value="Columbia" />
+      <input type="hidden" name="labels.1.catalog_number" value="88875173862-1" />
+      <button type="submit">Submit</button>
+    </form>
+  </body>
+</html>

root/static/scripts/tests/release-editor/seeds/mbs-13579-part4.html
@@ -1,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+  <body>
+    <!-- This is intended to be run after mbs-13579-part3.html. -->
+    <form action="/release/24d4159a-99d9-425d-a7b8-1b9ec0261a33/edit" method="POST">
+      <!-- Preserve ISO Records - ISO-3103 (seeding only the label name) -->
+      <!-- Edit Columbia - 88875173862-1 (removing the label) -->
+      <input type="hidden" name="labels.0.name" value="ISO Records" />
+      <input type="hidden" name="labels.0.catalog_number" value="ISO-3103" />
+      <input type="hidden" name="labels.1.catalog_number" value="88875173862-1" />
+      <button type="submit">Submit</button>
+    </form>
+  </body>
+</html>

root/static/scripts/tests/release-editor/seeds/mbs-13579-part5.html
@@ -1,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+  <body>
+    <!-- This is intended to be run after mbs-13579-part4.html. -->
+    <form action="/release/24d4159a-99d9-425d-a7b8-1b9ec0261a33/edit" method="POST">
+      <!-- Edit ISO Records - ISO-3103 (removing ISO-3103, seeding only the label name) -->
+      <!-- Add Columbia - 88875173862-2 -->
+      <!-- Edit 88875173862-1 (adding Columbia) -->
+      <input type="hidden" name="labels.0.name" value="ISO Records" />
+      <input type="hidden" name="labels.0.catalog_number" value="" />
+      <input type="hidden" name="labels.1.mbid" value="011d1192-6f65-45bd-85c4-0400dd45693e" />
+      <input type="hidden" name="labels.1.catalog_number" value="88875173862-2" />
+      <input type="hidden" name="labels.2.mbid" value="011d1192-6f65-45bd-85c4-0400dd45693e" />
+      <input type="hidden" name="labels.2.catalog_number" value="88875173862-1" />
+      <button type="submit">Submit</button>
+    </form>
+  </body>
+</html>

root/static/scripts/tests/release-editor/seeds/mbs-13579-part6.html
@@ -1,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+  <body>
+    <!-- This is intended to be run after mbs-13579-part5.html. -->
+    <form action="/release/24d4159a-99d9-425d-a7b8-1b9ec0261a33/edit" method="POST">
+      <!-- Remove ISO Records -->
+      <!-- Edit Columbia - 88875173862-2 (removing 88875173862-2, seeding only the label name) -->
+      <input type="hidden" name="labels.0.name" value="Columbia" />
+      <input type="hidden" name="labels.0.catalog_number" value="" />
+      <input type="hidden" name="labels.1.mbid" value="011d1192-6f65-45bd-85c4-0400dd45693e" />
+      <input type="hidden" name="labels.1.catalog_number" value="88875173862-1" />
+      <button type="submit">Submit</button>
+    </form>
+  </body>
+</html>

commit 81b1e62fbfcecde683918ae42513c75807e25e31

MBS-13692: Beta: release editor edits the same release label twice
If a release has at least one existing label, and you add a duplicate of it
(triggering visible error messages adjacent to the fields), and then edit both
of them, then it's expected that this would produce two edits: one to modify
the existing release label, and one to add a new one. Instead,
f0613577fd0e58060417afd3ce46a718f8baeb8e introduced a regression whereby the
same release label is edited twice, causing the second edit to immediately
trigger a failed dependency error.

This patch tracks which existing release labels have already been modified, so
that we don't attempt to reuse the same one twice when producing edits.

root/static/scripts/release-editor/edits.js
@@ -122,6 +122,7 @@ releaseEditor.edits = {
     const oldLabelsByID = keyBy(oldLabels, getReleaseLabel);
     const newLabelsByKey = keyBy(newLabels, getReleaseLabelKey);
     const oldLabelsByKey = keyBy(oldLabels, getReleaseLabelKey);
+    const editedLabelIds = new Set();
 
     var edits = [];
 
@@ -132,6 +133,13 @@ releaseEditor.edits = {
         const oldLabel = oldLabelsByID.get(id);
 
         if (oldLabel && !deepEqual(newLabel, oldLabel)) {
+          invariant(
+            !editedLabelIds.has(id),
+            'An existing release label ID has conflicting edits',
+          );
+
+          editedLabelIds.add(id);
+
           // Edit ReleaseLabel
           edits.push(MB.edit.releaseEditReleaseLabel(newLabel));
         }
@@ -141,30 +149,29 @@ releaseEditor.edits = {
          * catalog number, we can edit rather than remove it.
          */
         let oldLabel;
-        let oldLabelIndex = -1;
 
         if (newLabel.label) {
-          oldLabelIndex = oldLabels.findIndex(
+          oldLabel = oldLabels.find(
             (releaseLabel) => (
+              !editedLabelIds.has(getReleaseLabel(releaseLabel)) &&
               !newLabelsByKey.has(getReleaseLabelKey(releaseLabel)) &&
               releaseLabel.label === newLabel.label
             ),
           );
         }
-        if (oldLabelIndex < 0 && nonEmpty(newLabel.catalog_number)) {
-          oldLabelIndex = oldLabels.findIndex(
+        if (!oldLabel && nonEmpty(newLabel.catalog_number)) {
+          oldLabel = oldLabels.find(
             (releaseLabel) => (
+              !editedLabelIds.has(getReleaseLabel(releaseLabel)) &&
               !newLabelsByKey.has(getReleaseLabelKey(releaseLabel)) &&
               releaseLabel.catalog_number === newLabel.catalog_number
             ),
           );
         }
-        if (oldLabelIndex >= 0) {
-          oldLabel = oldLabels[oldLabelIndex];
-          oldLabels.splice(oldLabelIndex, 1);
-        }
 
         if (oldLabel && !deepEqual(newLabel, oldLabel)) {
+          editedLabelIds.add(getReleaseLabel(oldLabel));
+
           // Edit ReleaseLabel
           edits.push(MB.edit.releaseEditReleaseLabel({
             ...newLabel,
@@ -184,6 +191,9 @@ releaseEditor.edits = {
 
     for (let oldLabel of oldLabels) {
       const id = getReleaseLabel(oldLabel);
+      if (editedLabelIds.has(id)) {
+        continue;
+      }
       const newLabel = newLabelsByID.get(id) ??
         newLabelsByKey.get(getReleaseLabelKey(oldLabel));
 

t/selenium.mjs
@@ -715,6 +715,7 @@ const seleniumTests = [
   {name: 'release-editor/MBS-13207.json5', login: true},
   {name: 'release-editor/MBS-13273.json5', login: true},
   {name: 'release-editor/MBS-13579.json5', login: true},
+  {name: 'release-editor/MBS-13692.json5', login: true},
   {
     name: 'Check_Duplicates.json5',
     login: true,

t/selenium/release-editor/MBS-13692.json5
@@ -1,0 +1,97 @@
+{
+  title: 'MBS-13692',
+  commands: [
+    {
+      command: 'open',
+      target: '/release/24d4159a-99d9-425d-a7b8-1b9ec0261a33/edit',
+      value: '',
+    },
+    {
+      command: 'click',
+      target: "xpath=//button[contains(text(), 'Add label')]",
+      value: '',
+    },
+    {
+      command: 'type',
+      target: 'id=label-1',
+      value: '10f97e56-1ebe-4b1e-848f-9c5ae0f292ec',
+    },
+    {
+      command: 'pause',
+      target: '1000',
+      value: '',
+    },
+    {
+      command: 'type',
+      target: 'id=catno-0',
+      value: '123',
+    },
+    {
+      command: 'type',
+      target: 'id=catno-1',
+      value: '456',
+    },
+    {
+      command: 'click',
+      target: "xpath=//a[@href='#edit-note']",
+      value: '',
+    },
+    {
+      command: 'clickAndWait',
+      target: 'id=enter-edit',
+      value: '',
+    },
+    {
+      command: 'assertEditData',
+      target: 1,
+      value: {
+        type: 37,
+        status: 2,
+        data: {
+          new: {
+            catalog_number: "123",
+          },
+          old: {
+            catalog_number: "",
+            label: {
+              gid: "10f97e56-1ebe-4b1e-848f-9c5ae0f292ec",
+              id: 62565,
+              name: "ISO Records",
+            },
+          },
+          release: {
+            barcode: "888751738621",
+            events: [],
+            gid: "24d4159a-99d9-425d-a7b8-1b9ec0261a33",
+            id: 1693299,
+            medium_formats: ["CD"],
+            name: "★",
+          },
+          release_label_id: 1237936,
+        },
+      },
+    },
+    {
+      command: 'assertEditData',
+      target: 2,
+      value: {
+        type: 34,
+        status: 2,
+        data: {
+          catalog_number: "456",
+          entity_id: 1,
+          label: {
+            gid: "10f97e56-1ebe-4b1e-848f-9c5ae0f292ec",
+            id: 62565,
+            name: "ISO Records",
+          },
+          release: {
+            gid: "24d4159a-99d9-425d-a7b8-1b9ec0261a33",
+            id: 1693299,
+            name: "★",
+          },
+        },
+      },
+    },
+  ],
+}