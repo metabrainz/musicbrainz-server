da444a016064054327837bf57d5574e973163bad
Nicolás Tamargo <reosarevok@metabrainz.org>
2024-11-25 18:21:55 +0100

    MBS-13832: Don't skip PDF files for frontiest check
    We originally skipped PDFs because they didn't use to support thumbnails
    (making them bad choices for the sidebar and the like). That changed
    many years ago though, and especially for events, it's not uncommon
    for the frontiest image to be a PDF.

--- admin/sql/updates/20241125-mbs-13832.sql
+++ admin/sql/updates/20241125-mbs-13832.sql
@@ -2,9 +2,8 @@
 
 BEGIN;
 
-SET search_path = 'cover_art_archive';
-
-CREATE OR REPLACE VIEW index_listing AS
+-- CAA view
+CREATE OR REPLACE VIEW cover_art_archive.index_listing AS
 SELECT cover_art.*,
   (edit.close_time IS NOT NULL) AS approved,
   coalesce(cover_art.id = (SELECT id FROM cover_art_archive.cover_art_type
@@ -25,5 +24,23 @@ SELECT cover_art.*,
         WHERE cover_art_type.id = cover_art.id) AS types
 FROM cover_art_archive.cover_art
 LEFT JOIN musicbrainz.edit ON edit.id = cover_art.edit;
+
+-- EAA view
+CREATE OR REPLACE VIEW event_art_archive.index_listing AS
+SELECT event_art.*,
+  (edit.close_time IS NOT NULL) AS approved,
+  coalesce(event_art.id = (SELECT id FROM event_art_archive.event_art_type
+                   JOIN event_art_archive.event_art ea_front USING (id)
+                   WHERE ea_front.event = event_art.event
+                   AND type_id = 1
+                   ORDER BY ea_front.ordering
+                   LIMIT 1), FALSE) AS is_front,
+  array(SELECT art_type.name
+        FROM event_art_archive.event_art_type
+        JOIN event_art_archive.art_type ON event_art_type.type_id = art_type.id
+        WHERE event_art_type.id = event_art.id) AS types
+FROM event_art_archive.event_art
+LEFT JOIN musicbrainz.edit ON edit.id = event_art.edit;
+
 
 COMMIT;
