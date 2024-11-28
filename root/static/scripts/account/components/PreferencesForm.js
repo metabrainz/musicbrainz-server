5f8656c928b7482919a895bee20fc3ba121f7671
Nicolás Tamargo <reosarevok@metabrainz.org>
2024-11-28 13:40:49 +0100

    MBS-13838: Convert PreferencesForm to component syntax
    This also removes the <p> surrounding form rows since <div>s
    shouldn't be inside <p>s and they're not really needed.
    The only case where it looked worse after the change was for the period
    change select, where I inserted a line break above to keep the separation.

--- root/static/scripts/account/components/PreferencesForm.js
+++ root/static/scripts/account/components/PreferencesForm.js
@@ -36,14 +36,16 @@ type PreferencesFormT = FormT<{
   +timezone: FieldT<string>,
 }>;
 
-type Props = {
-  +form: PreferencesFormT,
-  +timezone_options: MaybeGroupedOptionsT,
-};
+/* eslint-disable ft-flow/sort-keys */
+type ActionT =
+  | {+type: 'guess-timezone', +options: GroupedOptionsT | SelectOptionsT}
+  | {+type: 'set-email-period', +emailPeriod: string}
+  | {+type: 'set-time-format', +timeFormat: string}
+  | {+type: 'set-timezone', +timezone: string};
+/* eslint-enable ft-flow/sort-keys */
 
-type State = {
-  form: PreferencesFormT,
-  timezoneOptions: MaybeGroupedOptionsT,
+type StateT = {
+  +form: PreferencesFormT,
 };
 
 const allowedDateTimeFormats = [
@@ -84,207 +86,210 @@ const subscriptionsEmailPeriodOptions = {
   ],
 };
 
-class PreferencesForm extends React.Component<Props, State> {
-  constructor(props: Props) {
-    super(props);
-    this.state = {form: props.form, timezoneOptions: props.timezone_options};
-    this.handleTimezoneChangeBound = (e) => this.handleTimezoneChange(e);
-    this.handleTimezoneGuessBound = () => this.handleTimezoneGuess();
-    this.handleDateTimeFormatChangeBound =
-      (e) => this.handleDateTimeFormatChange(e);
-    this.handleSubscriptionsEmailPeriodChangeBound =
-      (e) => this.handleSubscriptionsEmailPeriodChange(e);
-  }
-
-  handleTimezoneChangeBound: (e: SyntheticEvent<HTMLSelectElement>) => void;
-
-  handleTimezoneChange(e: SyntheticEvent<HTMLSelectElement>) {
-    const selectedTimezone = e.currentTarget.value;
-    this.setState(prevState => mutate(prevState)
-      .set('form', 'field', 'timezone', 'value', selectedTimezone).final());
-  }
-
-  handleTimezoneGuessBound: () => void;
-
-  handleTimezoneGuess() {
-    let maybeGuess;
-    try {
-      maybeGuess = Intl.DateTimeFormat().resolvedOptions().timeZone;
-    } catch (ignoredError) {
-      // ignored where Intl.DateTimeFormat is unsupported
-    }
-    const guess = maybeGuess;
-    if (nonEmpty(guess)) {
-      for (const option of this.state.timezoneOptions.options) {
-        if (option.value === guess) {
-          this.setState(prevState => mutate(prevState)
-            .set('form', 'field', 'timezone', 'value', guess).final());
-          break;
+function reducer(state: StateT, action: ActionT): StateT {
+  const newStateCtx = mutate(state);
+  switch (action.type) {
+    case 'guess-timezone': {
+      let maybeGuess;
+      try {
+        maybeGuess = Intl.DateTimeFormat().resolvedOptions().timeZone;
+      } catch (ignoredError) {
+        // ignored where Intl.DateTimeFormat is unsupported
+      }
+      const guess = maybeGuess;
+      if (nonEmpty(guess)) {
+        for (const option of action.options) {
+          if (option.value === guess) {
+            newStateCtx
+              .set('form', 'field', 'timezone', 'value', guess);
+            break;
+          }
         }
       }
+      break;
     }
-  }
-
-  handleDateTimeFormatChangeBound:
-    (e: SyntheticEvent<HTMLSelectElement>) => void;
-
-  handleDateTimeFormatChange(e: SyntheticEvent<HTMLSelectElement>) {
-    const selectedDateTimeFormat = e.currentTarget.value;
-    this.setState(prevState => mutate(prevState)
-      .set(
-        'form',
-        'field',
-        'datetime_format',
-        'value',
-        selectedDateTimeFormat,
-      )
-      .final());
-  }
-
-  handleSubscriptionsEmailPeriodChangeBound:
-    (e: SyntheticEvent<HTMLSelectElement>) => void;
-
-  handleSubscriptionsEmailPeriodChange(e: SyntheticEvent<HTMLSelectElement>) {
-    const selectedSubscriptionsEmailPeriod = e.currentTarget.value;
-    this.setState(prevState => mutate(prevState)
-      .set(
+    case 'set-email-period': {
+      newStateCtx.set(
         'form',
         'field',
         'subscriptions_email_period',
         'value',
-        selectedSubscriptionsEmailPeriod,
-      )
-      .final());
+        action.emailPeriod,
+      );
+      break;
+    }
+    case 'set-time-format': {
+      newStateCtx
+        .set('form', 'field', 'datetime_format', 'value', action.timeFormat);
+      break;
+    }
+    case 'set-timezone': {
+      newStateCtx.set('form', 'field', 'timezone', 'value', action.timezone);
+      break;
+    }
+    default: {
+      /*:: exhaustive(action); */
+    }
   }
-
-  render(): React.MixedElement {
-    const field = this.state.form.field;
-    return (
-      <form method="post">
-        <FormCsrfToken form={this.state.form} />
-
-        <fieldset>
-          <legend>{l('Regional settings')}</legend>
-          <FormRowSelect
-            field={field.timezone}
-            helpers={
-              <>
-                {' '}
-                <button
-                  className="guess-timezone icon"
-                  onClick={this.handleTimezoneGuessBound}
-                  title={l('Guess timezone')}
-                  type="button"
-                />
-              </>
-            }
-            label={l('Timezone:')}
-            onChange={this.handleTimezoneChangeBound}
-            options={this.state.timezoneOptions}
-          />
-          <SanitizedCatalystContext.Consumer>
-            {$c => (
-              <FormRowSelect
-                field={field.datetime_format}
-                label={l('Date/time format:')}
-                onChange={this.handleDateTimeFormatChangeBound}
-                options={buildDateTimeFormatOptions(
-                  $c,
-                  field.timezone.value,
-                )}
-              />
-            )}
-          </SanitizedCatalystContext.Consumer>
-        </fieldset>
-        <fieldset>
-          <legend>{l('Privacy')}</legend>
-          <p>
-            {addColonText(l('Allow other users to see'))}
-            <FormRowCheckbox
-              field={field.public_subscriptions}
-              label={l('My subscriptions')}
-              uncontrolled
-            />
-            <FormRowCheckbox
-              field={field.public_tags}
-              label={lp('My tags and genres', 'folksonomy')}
-              uncontrolled
-            />
-            <FormRowCheckbox
-              field={field.public_ratings}
-              label={l('My ratings')}
-              uncontrolled
-            />
-          </p>
-        </fieldset>
-        <fieldset>
-          <legend>{l('Email')}</legend>
-          <p>
-            {addColonText(l('Email me about'))}
-            <FormRowCheckbox
-              field={field.email_on_no_vote}
-              label={l('The first “no” vote on any of my edits')}
-              uncontrolled
-            />
-            <FormRowCheckbox
-              field={field.email_on_notes}
-              label={l('Notes on edits I have left notes on')}
-              uncontrolled
-            />
-            <FormRowCheckbox
-              field={field.email_on_vote}
-              label={l('Notes on edits I have voted on')}
-              uncontrolled
-            />
-            <FormRowCheckbox
-              field={field.email_on_abstain}
-              label={l('Notes on edits I have abstained on')}
-              uncontrolled
-            />
-          </p>
-          <p>
-            <FormRowSelect
-              field={field.subscriptions_email_period}
-              label={addColonText(l(
-                'Send me mails with edits to my subscriptions',
-              ))}
-              onChange={this.handleSubscriptionsEmailPeriodChangeBound}
-              options={subscriptionsEmailPeriodOptions}
-            />
-          </p>
-        </fieldset>
-        <fieldset>
-          <legend>{l('Editing')}</legend>
-          <p>
-            {addColonText(l('Automatically subscribe me when I add'))}
-            <FormRowCheckbox
-              field={field.subscribe_to_created_artists}
-              label={l('Artists')}
-              uncontrolled
-            />
-            <FormRowCheckbox
-              field={field.subscribe_to_created_labels}
-              label={l('Labels')}
-              uncontrolled
-            />
-            <FormRowCheckbox
-              field={field.subscribe_to_created_series}
-              label={lp('Series', 'plural')}
-              uncontrolled
-            />
-          </p>
-        </fieldset>
-        <FormRow hasNoLabel>
-          <FormSubmit label={lp('Save', 'interactive')} />
-        </FormRow>
-      </form>
-    );
-  }
+  return newStateCtx.final();
 }
 
-export type PreferencesFormPropsT = Props;
+component PreferencesForm(
+  form as initialForm: PreferencesFormT,
+  timezone_options: MaybeGroupedOptionsT,
+) {
+  const [state, dispatch] = React.useReducer(
+    reducer,
+    {form: initialForm},
+  );
 
-export default (hydrate<Props>(
+  const handleTimezoneChange = React.useCallback((
+    event: SyntheticEvent<HTMLSelectElement>,
+  ) => {
+    const selectedTimezone = event.currentTarget.value;
+    dispatch({timezone: selectedTimezone, type: 'set-timezone'});
+  }, [dispatch]);
+
+  const handleTimezoneGuess = React.useCallback(() => {
+    dispatch({options: timezone_options.options, type: 'guess-timezone'});
+  }, [dispatch]);
+
+  const handleDateTimeFormatChange = React.useCallback((
+    event: SyntheticEvent<HTMLSelectElement>,
+  ) => {
+    const selectedDateTimeFormat = event.currentTarget.value;
+    dispatch({timeFormat: selectedDateTimeFormat, type: 'set-time-format'});
+  }, [dispatch]);
+
+  const handleSubscriptionsEmailPeriodChange = React.useCallback((
+    event: SyntheticEvent<HTMLSelectElement>,
+  ) => {
+    const selectedSubscriptionsEmailPeriod = event.currentTarget.value;
+    dispatch({
+      emailPeriod: selectedSubscriptionsEmailPeriod,
+      type: 'set-email-period',
+    });
+  }, [dispatch]);
+
+  const field = state.form.field;
+  return (
+    <form method="post">
+      <FormCsrfToken form={state.form} />
+
+      <fieldset>
+        <legend>{l('Regional settings')}</legend>
+        <FormRowSelect
+          field={field.timezone}
+          helpers={
+            <>
+              {' '}
+              <button
+                className="guess-timezone icon"
+                onClick={handleTimezoneGuess}
+                title={l('Guess timezone')}
+                type="button"
+              />
+            </>
+          }
+          label={l('Timezone:')}
+          onChange={handleTimezoneChange}
+          options={timezone_options}
+        />
+        <SanitizedCatalystContext.Consumer>
+          {$c => (
+            <FormRowSelect
+              field={field.datetime_format}
+              label={l('Date/time format:')}
+              onChange={handleDateTimeFormatChange}
+              options={buildDateTimeFormatOptions(
+                $c,
+                field.timezone.value,
+              )}
+            />
+          )}
+        </SanitizedCatalystContext.Consumer>
+      </fieldset>
+      <fieldset>
+        <legend>{l('Privacy')}</legend>
+        {addColonText(l('Allow other users to see'))}
+        <FormRowCheckbox
+          field={field.public_subscriptions}
+          label={l('My subscriptions')}
+          uncontrolled
+        />
+        <FormRowCheckbox
+          field={field.public_tags}
+          label={lp('My tags and genres', 'folksonomy')}
+          uncontrolled
+        />
+        <FormRowCheckbox
+          field={field.public_ratings}
+          label={l('My ratings')}
+          uncontrolled
+        />
+      </fieldset>
+      <fieldset>
+        <legend>{l('Email')}</legend>
+        {addColonText(l('Email me about'))}
+        <FormRowCheckbox
+          field={field.email_on_no_vote}
+          label={l('The first “no” vote on any of my edits')}
+          uncontrolled
+        />
+        <FormRowCheckbox
+          field={field.email_on_notes}
+          label={l('Notes on edits I have left notes on')}
+          uncontrolled
+        />
+        <FormRowCheckbox
+          field={field.email_on_vote}
+          label={l('Notes on edits I have voted on')}
+          uncontrolled
+        />
+        <FormRowCheckbox
+          field={field.email_on_abstain}
+          label={l('Notes on edits I have abstained on')}
+          uncontrolled
+        />
+        <br />
+        <FormRowSelect
+          field={field.subscriptions_email_period}
+          label={addColonText(l(
+            'Send me mails with edits to my subscriptions',
+          ))}
+          onChange={handleSubscriptionsEmailPeriodChange}
+          options={subscriptionsEmailPeriodOptions}
+        />
+      </fieldset>
+      <fieldset>
+        <legend>{l('Editing')}</legend>
+        <div>
+          {addColonText(l('Automatically subscribe me when I add'))}
+          <FormRowCheckbox
+            field={field.subscribe_to_created_artists}
+            label={l('Artists')}
+            uncontrolled
+          />
+          <FormRowCheckbox
+            field={field.subscribe_to_created_labels}
+            label={l('Labels')}
+            uncontrolled
+          />
+          <FormRowCheckbox
+            field={field.subscribe_to_created_series}
+            label={lp('Series', 'plural')}
+            uncontrolled
+          />
+        </div>
+      </fieldset>
+      <FormRow hasNoLabel>
+        <FormSubmit label={lp('Save', 'interactive')} />
+      </FormRow>
+    </form>
+  );
+}
+
+export default (hydrate<React.PropsOf<PreferencesForm>>(
   'div.preferences-form',
   PreferencesForm,
-): component(...Props));
+): component(...React.PropsOf<PreferencesForm>));
