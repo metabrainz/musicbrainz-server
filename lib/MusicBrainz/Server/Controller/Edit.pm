2d6c4bd4dc68f9bb1be7fa0a4b03edd67f50c86c
Nicolás Tamargo <reosarevok@metabrainz.org>
2025-02-25 16:53:44 +0100

    Order RG types together with other entity types

--- lib/MusicBrainz/Server/Controller/Edit.pm
+++ lib/MusicBrainz/Server/Controller/Edit.pm
@@ -296,8 +296,6 @@ sub search : Path('/search/edits')
         quality => [ [$QUALITY_LOW => N_l('Low')], [$QUALITY_NORMAL => N_l('Normal')], [$QUALITY_HIGH => N_l('High')], [$QUALITY_UNKNOWN => N_l('Default')] ],
         languages => [ grep { $_->frequency > 0 } $c->model('Language')->get_all ],
         countries => [ $c->model('CountryArea')->get_all ],
-        rg_types => [ $c->model('ReleaseGroupType')->get_all ],
-        rg_secondary_types => [ $c->model('ReleaseGroupSecondaryType')->get_all ],
         relationship_type => [ $c->model('LinkType')->get_full_tree(get_deprecated_and_empty => 1) ],
         area_types => [ $c->model('AreaType')->get_all ],
         artist_types => [ $c->model('ArtistType')->get_all ],
@@ -305,6 +303,8 @@ sub search : Path('/search/edits')
         instrument_types => [ $c->model('InstrumentType')->get_all ],
         label_types => [ $c->model('LabelType')->get_all ],
         place_types => [ $c->model('PlaceType')->get_all ],
+        rg_types => [ $c->model('ReleaseGroupType')->get_all ],
+        rg_secondary_types => [ $c->model('ReleaseGroupSecondaryType')->get_all ],
         series_types => [ $c->model('SeriesType')->get_all ],
         work_types => [ $c->model('WorkType')->get_all ],
     );
