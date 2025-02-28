d9770ac21c3edb71e38bf2c1bddeb019ce472fa5
Nicolás Tamargo <reosarevok@metabrainz.org>
2025-02-28 11:43:10 +0100

    Update Flow to 0.257.1
    https://github.com/facebook/flow/releases/tag/v0.257.0
    https://github.com/facebook/flow/releases/tag/v0.257.1
    
    Most changes are because of the new ways of evaluating primitives.

--- root/static/scripts/common/components/Autocomplete2/actions.js
+++ root/static/scripts/common/components/Autocomplete2/actions.js
@@ -10,55 +10,55 @@
 export const HIDE_MENU = {
   type: 'set-menu-visibility',
   value: false,
-};
+} as const;
 
 export const HIGHLIGHT_NEXT_ITEM = {
   type: 'highlight-next-item',
-};
+} as const;
 
 export const HIGHLIGHT_PREVIOUS_ITEM = {
   type: 'highlight-previous-item',
-};
+} as const;
 
 export const SHOW_MENU = {
   type: 'set-menu-visibility',
   value: true,
-};
+} as const;
 
 export const SHOW_MORE_RESULTS = {
   type: 'show-more-results',
-};
+} as const;
 
 export const SEARCH_AGAIN = {
   type: 'search-after-timeout',
-};
+} as const;
 
 export const SHOW_LOOKUP_ERROR = {
   type: 'show-lookup-error',
-};
+} as const;
 
 export const SHOW_LOOKUP_TYPE_ERROR = {
   type: 'show-lookup-type-error',
-};
+} as const;
 
 export const SHOW_SEARCH_ERROR = {
   type: 'show-search-error',
-};
+} as const;
 
 export const STOP_SEARCH = {
   type: 'stop-search',
-};
+} as const;
 
 export const TOGGLE_INDEXED_SEARCH = {
   type: 'toggle-indexed-search',
-};
+} as const;
 
 export const OPEN_ADD_ENTITY_DIALOG = {
   isOpen: true,
   type: 'toggle-add-entity-dialog',
-};
+} as const;
 
 export const CLOSE_ADD_ENTITY_DIALOG = {
   isOpen: false,
   type: 'toggle-add-entity-dialog',
-};
+} as const;
