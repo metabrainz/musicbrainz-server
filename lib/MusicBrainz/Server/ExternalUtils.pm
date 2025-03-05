a2b49950d1841c9af2eff8158d495dc7ba39b835
Michael Wiencek <mwtuea@gmail.com>
2025-03-05 15:10:47 -0600

    Log errors in `get_chunked_with_retry`
    I'm seeing these kinds of failures in the Selenium tests:
    
      [browser console log] [SEVERE]
      http://mbtest:5000/ws/js/artist/?q=gr%C3%B6up%20member&page=1&direct=false
      - Failed to load resource: the server responded with a status of 500
      (Internal Server Error)
    
    The plackup logs show the request being dispatched, but the Solr logs don't
    show any indication that it was received. I'm curious what LWP receives as the
    response.
    
    This logging should also be useful in production for debugging search errors.

--- lib/MusicBrainz/Server/ExternalUtils.pm
+++ lib/MusicBrainz/Server/ExternalUtils.pm
@@ -3,6 +3,8 @@ use strict;
 use warnings;
 
 use base 'Exporter';
+use Data::Dumper;
+use MusicBrainz::Server::Log qw( log_error );
 
 our @EXPORT_OK = qw(
     get_chunked_with_retry
@@ -14,6 +16,13 @@ sub get_chunked_with_retry {
     my $retries_remaining = int(25.0 / $ua->timeout);
     while (!defined($response) && --$retries_remaining > 0) {
         $response = $ua->get($url);
+
+        log_error {
+            "Failed to get $url in get_chunked_with_retry:\n" .
+            Dumper($response->content) . "\n" .
+            "Response headers:\n" .
+            Dumper($response->headers->as_string)
+        } if $response->is_error;
 
         # When using chunked transfer encoding, occasionally, a chunk gets
         # delayed, and the LWP timeout fires causing the response to only be
