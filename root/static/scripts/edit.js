b8578447efc6d751594704e69dd2d6b4d1307727
Michael Wiencek <mwtuea@gmail.com>
2025-03-26 21:39:08 -0500

    Remove ReleaseMergeStrategy.js from edit.js

--- root/static/scripts/edit.js
+++ root/static/scripts/edit.js
@@ -1,6 +1,5 @@
 /* eslint-disable import/no-commonjs */
 
-require('./edit/components/ReleaseMergeStrategy.js');
 require('./edit/components/forms.js');
 require('./edit/MB/Control/Bubble.js');
 require('./edit/URLCleanup.js');
