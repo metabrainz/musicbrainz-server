6532a65088457c7e6287d5281d8cdefab48cb4ea
Michael Wiencek <mwtuea@gmail.com>
2025-03-27 02:08:31 -0500

    Remove check-duplicates.js from edit.js

--- root/static/scripts/edit.js
+++ root/static/scripts/edit.js
@@ -3,6 +3,5 @@
 require('./edit/components/forms.js');
 require('./edit/MB/Control/Bubble.js');
 require('./edit/MB/edit.js');
-require('./edit/check-duplicates.js');
 
 require('./guess-case/MB/Control/GuessCase.js');
