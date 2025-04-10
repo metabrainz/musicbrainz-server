abfdb340d20c626f286c2f1c9ea900f852ed0b0c
Michael Wiencek <mwtuea@gmail.com>
2025-04-09 20:16:07 -0500

    `get_chunked_with_retry`: Hardcode `$retries_remaining` to 5
    As noted in the previous commit, if `$ua->timeout` is greater than 25,
    `$retries_remaining` will be initialized to 0 and no requests will be
    performed.
    
    The timeout on the server context's `lwp` object is initialized to 5, so
    I'm hardcoding the number of retries to 25 / 5 = 5.
    
    I'm guessing the original code was a crude attempt at limiting the total
    duration of all retries to fit within 25 seconds. That doesn't work out
    very well in the case above; but another reason it makes no sense is
    that the LWP timeout is a connection activity timeout, not a "total
    request duration" timeout. The actual request duration can be much
    longer. This is spelled out very clearly in the documentation:
    
    https://metacpan.org/pod/LWP::UserAgent#timeout
    
    In order to bring the code closer to what I think the original intent
    was, I additionally added some tracking for the total retry duration,
    and further limit things based on that.

--- lib/MusicBrainz/Server/ExternalUtils.pm
+++ lib/MusicBrainz/Server/ExternalUtils.pm
@@ -5,16 +5,26 @@ use warnings;
 use base 'Exporter';
 use Data::Dumper;
 use MusicBrainz::Server::Log qw( log_error );
+use Readonly;
+use Time::HiRes qw( gettimeofday tv_interval );
 
 our @EXPORT_OK = qw(
     get_chunked_with_retry
 );
 
+Readonly my $retry_timeout => 25;
+
 sub get_chunked_with_retry {
     my ($ua, $url) = @_;
+
+    my $retries_remaining = 5;
+    my @start_time = gettimeofday;
     my $response;
-    my $retries_remaining = int(25.0 / $ua->timeout);
-    while (!defined($response) && --$retries_remaining > 0) {
+    while (
+        !defined($response) &&
+        --$retries_remaining > 0 &&
+        tv_interval(\@start_time) < $retry_timeout
+    ) {
         $response = $ua->get($url);
 
         log_error {
