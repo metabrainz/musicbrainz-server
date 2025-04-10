67db402accf125e7188f5cf53c93b9e2d54eda7e
Michael Wiencek <mwtuea@gmail.com>
2025-04-10 02:37:13 -0500

    Invoke `createExternalLinksEditorForHtmlForm` after the DOM is ready
    I hope this may fix an occasional issue in the Selenium test runs:
    
    Error: Errors were found on the page since executing the previous command:
    Error: createRoot(...): Target container is not a DOM element.
        at createRoot (http://mbtest:5000/static/build/vendors.js:59996:574820)
        at Object.createRoot$1 [as createRoot] (http://mbtest:5000/static/build/vendors.js:59996:590044)
        at push../node_modules/react-dom/client.js.exports.createRoot (http://mbtest:5000/static/build/vendors.js:60015:16)
        at createExternalLinksEditor (http://mbtest:5000/static/build/common-chunks.js:336735:59)
        at createExternalLinksEditorForHtmlForm (http://mbtest:5000/static/build/common-chunks.js:336757:44)
        at ./root/static/scripts/relationship-editor.js (http://mbtest:5000/static/build/relationship-editor.js:156:95)
        at __webpack_require__ (http://mbtest:5000/static/build/runtime.js:23:42)
        at __webpack_exec__ (http://mbtest:5000/static/build/relationship-editor.js:326:48)
        at http://mbtest:5000/static/build/relationship-editor.js:327:71
        at __webpack_require__.O (http://mbtest:5000/static/build/runtime.js:65:23)
        at runCommands (file:///home/musicbrainz/musicbrainz-server/t/selenium.mjs:977:13)
        at process.processTicksAndRejections (node:internal/process/task_queues:95:5)
        at async file:///home/musicbrainz/musicbrainz-server/t/selenium.mjs:1137:13

--- root/static/scripts/relationship-editor.js
+++ root/static/scripts/relationship-editor.js
@@ -1,11 +1,13 @@
 /*
- * @flow strict-local
+ * @flow
  * Copyright (C) 2020 MetaBrainz Foundation
  *
  * This file is part of MusicBrainz, the open internet music database,
  * and is licensed under the GPL version 2, or (at your option) any
  * later version: http://www.gnu.org/licenses/gpl-2.0.txt
  */
+
+import $ from 'jquery';
 
 import './relationship-editor/components/RelationshipEditorWrapper.js';
 
@@ -14,18 +16,20 @@ import {
 } from './common/utility/catalyst.js';
 import {createExternalLinksEditorForHtmlForm} from './edit/externalLinks.js';
 
-const sourceData = getSourceEntityDataForRelationshipEditor();
-const entityType = sourceData.entityType;
-const reactEditors = ['event', 'genre'];
+$(function () {
+  const sourceData = getSourceEntityDataForRelationshipEditor();
+  const entityType = sourceData.entityType;
+  const reactEditors = ['event', 'genre'];
 
-if (
-  /*
-   * Exclude React edit pages which use the <ExternalLinksEditor /> component
-   * directly.
-   */
-  !reactEditors.includes(entityType)
-) {
-  createExternalLinksEditorForHtmlForm(
-    'edit-' + entityType.replace('_', '-'),
-  );
-}
+  if (
+    /*
+     * Exclude React edit pages which use the <ExternalLinksEditor />
+     * component directly.
+     */
+    !reactEditors.includes(entityType)
+  ) {
+    createExternalLinksEditorForHtmlForm(
+      'edit-' + entityType.replace('_', '-'),
+    );
+  }
+});
