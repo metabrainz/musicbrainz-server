d42f55df559f5b4290fe9624e38af174d426a280
Nicolás Tamargo <reosarevok@metabrainz.org>
2025-04-24 18:07:07 +0200

    Drop seemingly useless class
    This class being added to the non-hydrated components is actually
    confusing the hydrate utility and seems to have no actual use - we
    never call this selector anywhere AFAICT.

--- root/static/scripts/edit/components/FormRowTextList.js
+++ root/static/scripts/edit/components/FormRowTextList.js
@@ -212,7 +212,7 @@ export component NonHydratedFormRowTextList(
   ...props: React.PropsOf<FormRowTextList>
 ) {
   return (
-    <FormRow className="form-row-text-list-container" rowRef={rowRef}>
+    <FormRow rowRef={rowRef}>
       <FormRowTextList {...props} />
     </FormRow>
   );
