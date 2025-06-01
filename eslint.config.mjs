ba8faed94652a8f0f32d2766a3d7b63b500b1097
Michael Wiencek <mwtuea@gmail.com>
2025-06-01 10:59:26 -0500

    Move edit/externalLinks.js to external-links-editor/components/ExternalLinksEditor.js
    With other code moved to separate files, this one is only for the
    `ExternalLinksEditor` component now.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -958,7 +958,7 @@ export default [
   {
     files: [
       'root/static/scripts/common/components/TagEditor.js',
-      'root/static/scripts/edit/externalLinks.js',
+      'root/static/scripts/external-links-editor/components/ExternalLinksEditor.js',
     ],
     rules: {
       'react/no-access-state-in-setstate': 'off',
@@ -966,7 +966,7 @@ export default [
   },
   {
     files: [
-      'root/static/scripts/edit/externalLinks.js',
+      'root/static/scripts/external-links-editor/components/ExternalLinksEditor.js',
     ],
     rules: {
       'react/no-multi-comp': 'off',
@@ -989,10 +989,10 @@ export default [
       'root/static/scripts/edit/components/ReleaseMergeStrategy.js',
       'root/static/scripts/edit/components/URLInputPopover.js',
       'root/static/scripts/edit/components/UrlRelationshipCreditFieldset.js',
-      'root/static/scripts/edit/externalLinks.js',
       'root/static/scripts/event/components/EventEditForm.js',
       'root/static/scripts/external-links-editor/components/ExternalLink.js',
       'root/static/scripts/external-links-editor/components/ExternalLinkRelationship.js',
+      'root/static/scripts/external-links-editor/components/ExternalLinksEditor.js',
       'root/static/scripts/relationship-editor/components/DialogPreview.js',
     ],
     rules: {
@@ -1007,7 +1007,7 @@ export default [
       'root/static/scripts/common/i18n/expand2.js',
       'root/static/scripts/common/utility/cloneDeep.mjs',
       'root/static/scripts/edit/components/withLoadedTypeInfo.js',
-      'root/static/scripts/edit/externalLinks.js',
+      'root/static/scripts/external-links-editor/components/ExternalLinksEditor.js',
       'root/static/scripts/relationship-editor/components/DialogEntityCredit.js',
       'root/static/scripts/relationship-editor/components/DialogTargetType.js',
       'root/static/scripts/relationship-editor/components/RelationshipEditor.js',
