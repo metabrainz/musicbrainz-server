8b39a5dd92607a857483a24d242900440a7c1ca5
Michael Wiencek <mwtuea@gmail.com>
2025-08-28 18:26:31 -0500

    Remove unused ESLint rule exclusions for root/static/scripts/external-links-editor/

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -959,18 +959,9 @@ export default [
   {
     files: [
       'root/static/scripts/common/components/TagEditor.js',
-      'root/static/scripts/external-links-editor/components/ExternalLinksEditor.js',
     ],
     rules: {
       'react/no-access-state-in-setstate': 'off',
-    },
-  },
-  {
-    files: [
-      'root/static/scripts/external-links-editor/components/ExternalLinksEditor.js',
-    ],
-    rules: {
-      'react/no-multi-comp': 'off',
     },
   },
   {
@@ -990,11 +981,6 @@ export default [
       'root/static/scripts/edit/components/FormRowSelectList.js',
       'root/static/scripts/edit/components/ReleaseMergeStrategy.js',
       'root/static/scripts/event/components/EventEditForm.js',
-      'root/static/scripts/external-links-editor/components/ExternalLink.js',
-      'root/static/scripts/external-links-editor/components/ExternalLinkRelationship.js',
-      'root/static/scripts/external-links-editor/components/ExternalLinkRelationshipDialog.js',
-      'root/static/scripts/external-links-editor/components/ExternalLinksEditor.js',
-      'root/static/scripts/external-links-editor/components/URLInputPopover.js',
       'root/static/scripts/relationship-editor/components/DialogPreview.js',
     ],
     rules: {
@@ -1009,7 +995,6 @@ export default [
       'root/static/scripts/common/i18n/expand2.js',
       'root/static/scripts/common/utility/cloneDeep.mjs',
       'root/static/scripts/edit/components/withLoadedTypeInfo.js',
-      'root/static/scripts/external-links-editor/components/ExternalLinksEditor.js',
       'root/static/scripts/relationship-editor/components/DialogEntityCredit.js',
       'root/static/scripts/relationship-editor/components/DialogTargetType.js',
       'root/static/scripts/relationship-editor/components/RelationshipEditor.js',
