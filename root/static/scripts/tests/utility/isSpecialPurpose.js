7e3e6d02be05f0ade241dc58326ee861e51807ca
Nicolás Tamargo <reosarevok@metabrainz.org>
2025-09-03 11:43:42 +0200

    MBS-14146: Make all special purpose artists non-editable
    We specifically have an is_special_purpose method to avoid
    special purpose artists being edited and merged, but for some
    reason we were only using it for Various Artists. We don't want
    anyone merging Anonymous and Unknown or something
    similar, which would be very hard to undo, so this sets
    all official special purpose artists as non-editable.
    See https://musicbrainz.org/doc/Style/Unknown_and_untitled/Special_purpose_artist

---
+++ root/static/scripts/tests/utility/isSpecialPurpose.js
@@ -0,0 +1,105 @@
+/*
+ * @flow strict
+ * Copyright (C) 2014 MetaBrainz Foundation
+ *
+ * This file is part of MusicBrainz, the open internet music database,
+ * and is licensed under the GPL version 2, or (at your option) any
+ * later version: http://www.gnu.org/licenses/gpl-2.0.txt
+ */
+
+import test from 'tape';
+
+import {
+  ANON_ARTIST_GID,
+  ANON_ARTIST_ID,
+  DARTIST_ID,
+  DATA_ARTIST_GID,
+  DATA_ARTIST_ID,
+  DIALOGUE_ARTIST_GID,
+  DIALOGUE_ARTIST_ID,
+  NO_ARTIST_GID,
+  NO_ARTIST_ID,
+  TRAD_ARTIST_GID,
+  TRAD_ARTIST_ID,
+  UNKNOWN_ARTIST_GID,
+  UNKNOWN_ARTIST_ID,
+  VARTIST_GID,
+  VARTIST_ID,
+} from '../../common/constants.js';
+import {
+  createArtistObject,
+} from '../../common/entity2.js';
+import isSpecialPurpose from '../../common/utility/isSpecialPurpose.js';
+
+test('isSpecialPurpose', function (t) {
+  t.plan(17);
+
+  t.ok(
+    isSpecialPurpose(createArtistObject({id: DARTIST_ID})),
+    'The id for Deleted Artist is detected as a special purpose artist ID',
+  );
+  t.ok(
+    isSpecialPurpose(createArtistObject({id: VARTIST_ID})),
+    'The id for Deleted Artist is detected as a special purpose artist ID',
+  );
+  t.ok(
+    isSpecialPurpose(createArtistObject({gid: VARTIST_GID})),
+    'The MBID for Various Artists is detected as a special purpose artist MBID',
+  );
+  t.ok(
+    isSpecialPurpose(createArtistObject({id: ANON_ARTIST_ID})),
+    'The id for [anonymous] is detected as a special purpose artist ID',
+  );
+  t.ok(
+    isSpecialPurpose(createArtistObject({gid: ANON_ARTIST_GID})),
+    'The MBID for [anonymous] is detected as a special purpose artist MBID',
+  );
+  t.ok(
+    isSpecialPurpose(createArtistObject({id: DATA_ARTIST_ID})),
+    'The id for [data] is detected as a special purpose artist ID',
+  );
+  t.ok(
+    isSpecialPurpose(createArtistObject({gid: DATA_ARTIST_GID})),
+    'The MBID for [data] is detected as a special purpose artist MBID',
+  );
+  t.ok(
+    isSpecialPurpose(createArtistObject({id: DIALOGUE_ARTIST_ID})),
+    'The id for [dialogue] is detected as a special purpose artist ID',
+  );
+  t.ok(
+    isSpecialPurpose(createArtistObject({gid: DIALOGUE_ARTIST_GID})),
+    'The MBID for [dialogue] is detected as a special purpose artist MBID',
+  );
+  t.ok(
+    isSpecialPurpose(createArtistObject({id: NO_ARTIST_ID})),
+    'The id for [no artist] is detected as a special purpose artist ID',
+  );
+  t.ok(
+    isSpecialPurpose(createArtistObject({gid: NO_ARTIST_GID})),
+    'The MBID for [no artist] is detected as a special purpose artist MBID',
+  );
+  t.ok(
+    isSpecialPurpose(createArtistObject({id: TRAD_ARTIST_ID})),
+    'The id for [traditional] is detected as a special purpose artist ID',
+  );
+  t.ok(
+    isSpecialPurpose(createArtistObject({gid: TRAD_ARTIST_GID})),
+    'The MBID for [traditional] is detected as a special purpose artist MBID',
+  );
+  t.ok(
+    isSpecialPurpose(createArtistObject({id: UNKNOWN_ARTIST_ID})),
+    'The id for [unknown] is detected as a special purpose artist ID',
+  );
+  t.ok(
+    isSpecialPurpose(createArtistObject({gid: UNKNOWN_ARTIST_GID})),
+    'The MBID for [unknown] is detected as a special purpose artist MBID',
+  );
+  t.ok(
+    !isSpecialPurpose(createArtistObject({id: 5})),
+    'A random artist id is not detected as special purpose',
+  );
+  t.ok(
+    !isSpecialPurpose(createArtistObject({gid: '7527f6c2-d762-4b88-b5e2-9244f1e34c46'})),
+    'A random artist MBID is not detected as special purpose',
+  );
+});
