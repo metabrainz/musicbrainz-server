b0f694db64f50f6d55558c60ada39f7c00a1e98e
Nicolás Tamargo <reosarevok@metabrainz.org>
2025-09-23 12:44:43 +0200

    MBS-14157: Search for URLs containing string
    This allows using a ~ regex search to find edits related to a
    URL containing a specific string. ^ and $ can be used to find
    starting or ending strings.

--- lib/MusicBrainz/Server/EditSearch/Predicate/URL.pm
+++ lib/MusicBrainz/Server/EditSearch/Predicate/URL.pm
@@ -1,6 +1,7 @@
 package MusicBrainz::Server::EditSearch::Predicate::URL;
 use Moose;
 use namespace::autoclean;
+use MusicBrainz::Server::Data::Utils qw( non_empty );
 use MusicBrainz::Server::Validation qw( is_valid_url );
 
 with 'MusicBrainz::Server::EditSearch::Predicate';
@@ -14,21 +15,28 @@ sub operator_cardinality_map {
     return (
         '=' => undef,
         '!=' => undef,
+        '~' => undef,
+        '!~' => undef,
     );
 }
 
 sub valid {
     my $self = shift;
     my @args = @{ $self->sql_arguments };
-    return @args && is_valid_url($args[0]);
+    if ($self->operator eq '=' || $self->operator eq '!=') {
+        return @args && is_valid_url($args[0]);
+    }
+    return @args && non_empty($args[0]);
 }
 
 sub combine_with_query {
     my ($self, $query) = @_;
 
+    my $negation = ($self->operator eq '!=' || $self->operator eq '!~') ? 'NOT ' : '';
+    my $operator = ($self->operator eq '~' || $self->operator eq '!~') ? '~' : '=';
     $query->add_where([
-        ($self->operator eq '!=' ? 'NOT ' : '') .
-        'EXISTS (SELECT 1 FROM edit_url JOIN url ON edit_url.url = url.id WHERE edit = edit.id AND url.url = ?)', $self->sql_arguments,
+        $negation .
+        "EXISTS (SELECT 1 FROM edit_url JOIN url ON edit_url.url = url.id WHERE edit = edit.id AND url.url $operator ?)", $self->sql_arguments,
     ]);
 }
 
