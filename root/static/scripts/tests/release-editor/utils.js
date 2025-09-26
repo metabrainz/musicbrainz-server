eff2da1426b0f1fbd217baa6b757a5bdca51ceb1
Nicolás Tamargo <reosarevok@metabrainz.org>
2025-09-26 17:10:23 +0200

    Move unformatTrackLength to its own file
    I'll next use it as part of a new isInvalidLength JS check.

--- root/static/scripts/tests/release-editor/utils.js
+++ root/static/scripts/tests/release-editor/utils.js
@@ -10,38 +10,6 @@ import test from 'tape';
 
 import utils from '../../release-editor/utils.js';
 
-test('unformatTrackLength', function (t) {
-  t.plan(7);
-
-  const seconds = 1000;
-  const minutes = 60 * seconds;
-  const hours = 60 * minutes;
-
-  t.equal(
-    utils.unformatTrackLength('?:??'),
-    null,
-    'MBS-5086: unformatTrackLength(?:??) should be null',
-  );
-  t.equal(utils.unformatTrackLength('23 ms'), 23, 'unformatTrackLength');
-  t.equal(utils.unformatTrackLength('00:23'), 23 * seconds, 'unformatTrackLength');
-  t.equal(
-    utils.unformatTrackLength(':57'),
-    57 * seconds,
-    'MBS-3352: Handle the case of ":57"',
-  );
-  t.equal(utils.unformatTrackLength('59:00'), 59 * minutes, 'unformatTrackLength');
-  t.equal(
-    utils.unformatTrackLength('01:00:00'),
-    60 * minutes,
-    'unformatTrackLength',
-  );
-  t.equal(
-    utils.unformatTrackLength('14:15:16'),
-    (14 * hours) + (15 * minutes) + (16 * seconds),
-    'unformatTrackLength',
-  );
-});
-
 test('calculateDiscID', function (t) {
   t.plan(2);
 
