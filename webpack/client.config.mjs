e0e6fce28d526bbc3a19bbe41c8bb09835cc87a4
Nicolás Tamargo <reosarevok@metabrainz.org>
2025-10-08 16:05:15 +0200

    MBS-12761: Move ISRC validation and changing to React state
    This modifies FormRowTextList to make it properly usable inside
    a larger form; rather than have its own state, it accepts
    dispatch and state props. Additionally, I'm moving currentTextValues
    outside the state since it is a constant, and just passing it as
    a prop. This was in the state for ease of use in the reducer,
    but it can just be passed by the dispatch as needed.
    
    For uses where the FormRow still needs its own state (because
    it's just a standalone component used in a TT form) I added
    StandaloneFormRowTextlist, to which the old local state has moved.
    
    I added isValidIsrc for JS-level ISRC validation; this combines
    is_valid_isrc and format_isrc from Server::Validation since we don't
    want to reject ISRCs that are valid once formatted by the system.
    I expect it'd be more confusing for the user to automatically change
    the format of the ISRC on the form itself so I'm letting it happen
    at the Perl level as earlier.

--- webpack/client.config.mjs
+++ webpack/client.config.mjs
@@ -84,6 +84,7 @@ const entries = [
   'edit/components/HydratedDateRangeFieldset',
   'edit/components/NewNotesAlertCheckbox',
   'edit/components/ReleaseMergeStrategy',
+  'edit/components/StandaloneFormRowTextList',
   'edit/ExampleRelationships',
   'edit/MB/reltypeslist',
   'event/components/EventEditForm',
