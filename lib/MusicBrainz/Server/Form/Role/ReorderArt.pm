6f9396bc04a262de003185db7c764c1e35699336
Nicolás Tamargo <reosarevok@metabrainz.org>
2025-10-15 17:47:35 +0200

    MBS-14151 (II): Check if artwork has changed before reorder
    When a reorder cover art edit form is open, it's possible that
    some images are added or removed in the background
    (for example, a remove edit applies, or an add edit is
    entered or cancelled). This can lead to the reorder form being
    misleading and it currently can end up entering an edit that
    should not exist and makes no sense.
    
    This checks that the old and new reordered images have the
    same IDs (and are there in the same amount) and rejects the
    edit being entered otherwise. The form will just reload
    with the new set of images, so the error is more of an
    explanation of why that happened.

--- lib/MusicBrainz/Server/Form/Role/ReorderArt.pm
+++ lib/MusicBrainz/Server/Form/Role/ReorderArt.pm
@@ -2,7 +2,9 @@ package MusicBrainz::Server::Form::Role::ReorderArt;
 use strict;
 use warnings;
 
+use Data::Compare;
 use HTML::FormHandler::Moose::Role;
+use MusicBrainz::Server::Translation qw( l );
 
 with 'MusicBrainz::Server::Form::Role::Edit';
 
@@ -11,6 +13,24 @@ sub edit_field_names { qw( artwork ) }
 has_field 'artwork' => ( type => 'Repeatable' );
 has_field 'artwork.id' => ( type => '+MusicBrainz::Server::Form::Field::Integer' );
 has_field 'artwork.position' => ( type => '+MusicBrainz::Server::Form::Field::Integer' );
+
+after validate => sub {
+    my $self = shift;
+
+    my $old_artwork = $self->init_object->{artwork};
+
+    my @old_ids = sort { $a<=>$b } map { $_->{id} } @$old_artwork;
+    my @new_ids = sort { $a<=>$b } map { $_->{id} } @{ $self->field('artwork')->value };
+
+    my $are_ids_same = Compare( \@old_ids, \@new_ids );
+
+    if (!$are_ids_same) {
+        $self->field('artwork')->add_error(l(
+            'The edit could not be applied because the images have changed. ' .
+            'Please try again.',
+        ));
+    }
+};
 
 1;
 
