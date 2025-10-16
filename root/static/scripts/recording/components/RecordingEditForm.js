b544601b3c42531d24e079051c411701d2bcb293
Nicolás Tamargo <reosarevok@metabrainz.org>
2025-10-16 16:39:43 +0200

    Show external links bubble on focus

--- root/static/scripts/recording/components/RecordingEditForm.js
+++ root/static/scripts/recording/components/RecordingEditForm.js
@@ -354,6 +354,10 @@ component RecordingEditForm(
     dispatch({bubble: 'name', type: 'toggle-bubble'});
   }
 
+  function handleExternalLinksFocus() {
+    dispatch({bubble: 'external-links', type: 'toggle-bubble'});
+  }
+
   const hasErrors = hasSubfieldErrors(state.form) ||
                     !isArtistCreditStateComplete(state.artistCredit.names);
 
@@ -383,7 +387,7 @@ component RecordingEditForm(
   const commentFieldRef = React.useRef<HTMLDivElement | null>(null);
   const lengthFieldRef = React.useRef<HTMLDivElement | null>(null);
   const isrcFieldRef = React.useRef<HTMLDivElement | null>(null);
-  const externalLinksFieldRef = React.useRef<HTMLDivElement | null>(null);
+  const externalLinksTableRef = React.useRef<HTMLTableElement | null>(null);
 
   return (
     <form
@@ -484,8 +488,10 @@ component RecordingEditForm(
           <legend>{l('External links')}</legend>
           <ExternalLinksEditor
             isNewEntity={!state.recording.id}
+            onFocus={handleExternalLinksFocus}
             ref={externalLinksEditorRef}
             sourceData={state.recording}
+            tableRef={externalLinksTableRef}
           />
         </fieldset>
 
@@ -621,10 +627,9 @@ component RecordingEditForm(
             </p>
           </Bubble>
         ) : null}
-        {/* TODO: Make this show */}
         {state.shownBubble === 'external-links' ? (
           <Bubble
-            controlRef={externalLinksFieldRef}
+            controlRef={externalLinksTableRef}
             id="external-link-bubble"
           >
             <p>
