a20cae9323c5d33816d8d53b786accdedd68400f
Nicolás Tamargo <reosarevok@metabrainz.org>
2025-10-16 17:25:12 +0200

    Ensure no accidental writes on nameStateCtx
    We don't intend to change nameStateCtx after assigning it
    to nameState in any of these forms, so it is safer to use final()
    to make sure it's not writable anymore.

--- root/static/scripts/event/components/EventEditForm.js
+++ root/static/scripts/event/components/EventEditForm.js
@@ -138,7 +138,7 @@ function reducer(state: StateT, action: ActionT): StateT {
       });
       runNameReducer(nameStateCtx, action);
 
-      const nameState = nameStateCtx.read();
+      const nameState = nameStateCtx.final();
       newStateCtx
         .update('form', 'field', 'name', (nameFieldCtx) => {
           nameFieldCtx.set(nameState.field);
--- root/static/scripts/recording/components/RecordingEditForm.js
+++ root/static/scripts/recording/components/RecordingEditForm.js
@@ -243,7 +243,7 @@ function reducer(state: StateT, action: ActionT): StateT {
       });
       runNameReducer(nameStateCtx, action);
 
-      const nameState = nameStateCtx.read();
+      const nameState = nameStateCtx.final();
       newStateCtx
         .update('form', 'field', 'name', (nameFieldCtx) => {
           nameFieldCtx.set(nameState.field);
