c4ef15d7f1901c44fde05109fd9f1a10d280c340
Michael Wiencek <mwtuea@gmail.com>
2025-12-09 20:08:20 -0600

    Remove email verification
    Account creation is moving to MetaBrainz.

--- t/lib/t/MusicBrainz/Server/Controller/Account.pm
+++ t/lib/t/MusicBrainz/Server/Controller/Account.pm
@@ -5,29 +5,8 @@ use warnings;
 use Test::Routine;
 use Test::More;
 use MusicBrainz::Server::Constants qw( $SPAMMER_FLAG );
-use MusicBrainz::Server::Test qw( html_ok );
 
 with 't::Mechanize', 't::Context';
-
-test 'Updated email address is escaped in flash message' => sub {
-    my $test = shift;
-    my $mech = $test->mech;
-
-    MusicBrainz::Server::Test->prepare_test_database($test->c);
-
-    $mech->get_ok('/login');
-    $mech->submit_form(with_fields => {username => 'new_editor', password => 'password'});
-
-    $mech->get_ok('/account/edit');
-    $mech->submit_form(with_fields => { 'profile.email' => '"&&&"@example.com' });
-
-    ok($mech->success);
-    html_ok($mech->content);
-    $mech->content_contains(
-        'We have sent you a verification email to ' .
-        '<code>&quot;&amp;&amp;&amp;&quot;@example.com</code>.',
-    );
-};
 
 test 'MBS-14082: Spam accounts are logged out' => sub {
     my $test = shift;
--- t/lib/t/MusicBrainz/Server/Data/Editor.pm
+++ t/lib/t/MusicBrainz/Server/Data/Editor.pm
@@ -122,7 +122,7 @@ test 'Creating a new editor' => sub {
     );
 };
 
-test 'find_by_email and is_email_used_elsewhere' => sub {
+test 'find_by_email' => sub {
     my $test = shift;
     MusicBrainz::Server::Test->prepare_test_database($test->c, '+editor');
     my $editor_data = MusicBrainz::Server::Data::Editor->new(c => $test->c);
@@ -132,8 +132,6 @@ test 'find_by_email and is_email_used_elsewhere' => sub {
         name => 'new_editor_2',
         password => 'password',
     });
-    # For testing is_email_used_elsewhere
-    my $future_editor_id = $new_editor_2->id + 1;
 
     note('We set an email for the new editor with update_email');
     $editor_data->update_email($new_editor_2, 'editor@example.com');
@@ -147,22 +145,6 @@ test 'find_by_email and is_email_used_elsewhere' => sub {
     is(scalar(@editors), 1, 'An editor was found searching with all caps');
     is($editors[0]->id, $new_editor_2->id, 'The right editor was found');
 
-    note('We check is_email_used_elsewhere shows the email as being in use');
-    ok(
-        $editor_data->is_email_used_elsewhere(
-            'editor@example.com',
-            $future_editor_id,
-        ),
-        'The exact email is shown to be in use if another editor wants it',
-    );
-    ok(
-        $editor_data->is_email_used_elsewhere(
-            'EDITOR@EXAMPLE.COM',
-            $future_editor_id,
-        ),
-        'The email is shown to be in use even if searching with all caps',
-    );
-
     note('We set an all caps email for the new editor with update_email');
     $editor_data->update_email($new_editor_2, 'EDITOR@EXAMPLE.COM');
 
@@ -174,22 +156,6 @@ test 'find_by_email and is_email_used_elsewhere' => sub {
     @editors = $editor_data->find_by_email('editor@example.com');
     is(scalar(@editors), 1, 'An editor was found searching with normal caps');
     is($editors[0]->id, $new_editor_2->id, 'The right editor was found');
-
-    note('We check is_email_used_elsewhere shows the email as being in use');
-    ok(
-        $editor_data->is_email_used_elsewhere(
-            'EDITOR@EXAMPLE.COM',
-            $future_editor_id,
-        ),
-        'The exact email is shown to be in use if another editor wants it',
-    );
-    ok(
-        $editor_data->is_email_used_elsewhere(
-            'editor@example.com',
-            $future_editor_id,
-        ),
-        'The email is shown to be in use even if searching with normal caps',
-    );
 };
 
 test 'Getting/loading existing editors' => sub {
