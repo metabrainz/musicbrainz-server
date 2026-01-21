a4ba0625eb78ba5fee123dd8e93145979e73ee16
Nicolás Tamargo <reosarevok@metabrainz.org>
2026-01-21 11:56:20 +0100

    MBS-14092 (I): Add support for series of series (SQL)

---
+++ admin/sql/updates/20260121-mbs-14092.sql
@@ -0,0 +1,67 @@
+\set ON_ERROR_STOP 1
+BEGIN;
+
+\set SERIES_PART_OF_SERIES_ID '1301'
+
+-- generate_uuid_v3('6ba7b8119dad11d180b400c04fd430c8', 'http://musicbrainz.org/linktype/series/series/part_of')
+\set SERIES_PART_OF_SERIES_GID '''8da75c99-46ff-373c-9d31-276ca8fa8cc3'''
+
+-- id from link_attribute_type where name = 'number'
+\set LINK_ATTRIBUTE_TYPE_NUMBER_ID '788'
+
+-----------------------
+-- CREATE NEW VIEWS  --
+-----------------------
+
+CREATE OR REPLACE VIEW series_series AS
+    SELECT entity0 AS series_part,
+           entity1 AS series,
+           lss.id AS relationship,
+           link_order,
+           lss.link,
+           COALESCE(text_value, '') AS text_value
+    FROM l_series_series lss
+    JOIN series s ON s.id = lss.entity1
+    JOIN link l ON l.id = lss.link
+    JOIN link_type lt ON (lt.id = l.link_type AND lt.gid = '8da75c99-46ff-373c-9d31-276ca8fa8cc3')
+    LEFT OUTER JOIN link_attribute_text_value latv ON (latv.attribute_type = 788 AND latv.link = l.id)
+    ORDER BY series, link_order;
+
+-------------------------
+-- INSERT INITIAL DATA --
+-------------------------
+
+-- Part-of-series rel
+
+INSERT INTO link_type (id, gid, entity_type0, entity_type1, entity0_cardinality,
+                       entity1_cardinality, name, description, link_phrase,
+                       reverse_link_phrase, long_link_phrase) VALUES
+    (
+        :SERIES_PART_OF_SERIES_ID,
+        :SERIES_PART_OF_SERIES_GID,
+        'series', 'series', 0, 0, 'part of',
+        'Indicates that the series is part of a series.',
+        'part of', 'has parts', 'is a part of'
+    );
+
+INSERT INTO link_type_attribute_type (link_type, attribute_type, min, max) VALUES
+    (
+        :SERIES_PART_OF_SERIES_ID,
+        :LINK_ATTRIBUTE_TYPE_NUMBER_ID,
+        0,
+        1
+    );
+
+INSERT INTO orderable_link_type (link_type, direction) VALUES
+    (:SERIES_PART_OF_SERIES_ID, 2);
+
+ALTER TABLE series_type DROP CONSTRAINT IF EXISTS allowed_series_entity_type;
+
+INSERT INTO series_type (id, name, entity_type, parent, child_order, description, gid) VALUES
+    (16, 'Series series', 'series', NULL, 6, 'A series of series.', generate_uuid_v3('6ba7b8119dad11d180b400c04fd430c8', 'series_type16')),
+    (17, 'Series award', 'series', 16, 0, 'A series of series (such as podcasts or festivals) honoured by the same award.', generate_uuid_v3('6ba7b8119dad11d180b400c04fd430c8', 'series_type17'));
+
+\unset SERIES_PART_OF_SERIES_GID
+\unset LINK_ATTRIBUTE_TYPE_NUMBER_ID
+
+COMMIT;
