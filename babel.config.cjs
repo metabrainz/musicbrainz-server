metabrainz/musicbrainz-server

commit 81d820b486de80700534060368eab530b3184b7b

Replace sucrase-node with custom babel-node loader
As a reminder, bin/sucrase-node is used to execute JavaScript files that may
contain Flow syntax in Node.js.

Sucrase has worked great for us, but it's not a good long-term solution. For
example, Flow has been adding new syntax features (components, pattern
matching) that Sucrase hasn't kept up with. (The last release was in December
2023.)

This creates a new module loader that passes the source files through Babel.
Thankfully, compile_resources.sh doesn't seem to be noticeably slower, though
it was already very slow.

I've tested the new loader in Node versions 20.19.4 and 24.5.0.

babel.config.cjs
@@ -43,6 +43,9 @@ module.exports = function (api) {
   const presets = [
     ['@babel/preset-env', {
       corejs: 3.45,
+      modules: api.caller(caller => caller && caller.name === 'babel-node-loader')
+        ? false
+        : 'auto',
       targets: api.caller(caller => caller && caller.target === 'node')
         ? NODE_TARGETS
         : BROWSER_TARGETS[browserTarget],

root/utility/babel-node/hooks.mjs
@@ -1,0 +1,74 @@
+/* eslint-disable import/prefer-default-export */
+
+import {transformAsync} from '@babel/core';
+import {readFile} from 'node:fs/promises';
+import path from 'node:path';
+import {fileURLToPath} from 'node:url';
+
+const MB_SERVER_ROOT = path.resolve(
+  path.dirname(fileURLToPath(import.meta.url)),
+  '../../../',
+);
+
+const loadResultCache = new Map();
+
+export async function load(url, context, nextLoad) {
+  if (!url.startsWith('file:')) {
+    return nextLoad(url, context);
+  }
+
+  const loadResult = loadResultCache.get(url);
+  if (loadResult) {
+    return loadResult;
+  }
+
+  const filename = fileURLToPath(url);
+  const extension = path.extname(filename).toLowerCase();
+  let format = context.format;
+
+  switch (extension) {
+    case '.cjs': {
+      format = 'commonjs';
+      break;
+    }
+    case '.mjs': {
+      format = 'module';
+      break;
+    }
+    case '.js': {
+      format = context.format ?? 'commonjs';
+      break;
+    }
+    default: {
+      return nextLoad(url, context);
+    }
+  }
+  if (format !== 'module' && format !== 'commonjs') {
+    throw new Error('Could not determine a module format for ' + url);
+  }
+
+  const source = await readFile(filename, 'utf8');
+  const transformResult = await transformAsync(source, {
+    caller: {
+      format,
+      name: 'babel-node-loader',
+      supportsStaticESM: true,
+      supportsTopLevelAwait: true,
+    },
+    cwd: process.cwd(),
+    filename,
+    root: MB_SERVER_ROOT,
+  });
+  // transformResult is `null` if the file was ignored
+  if (!transformResult) {
+    return nextLoad(url, context);
+  }
+
+  const result = {
+    format,
+    shortCircuit: true,
+    source: transformResult.code,
+  };
+  loadResultCache.set(url, result);
+  return result;
+}

webpack/exec.mjs
@@ -1,4 +1,4 @@
-#!./bin/sucrase-node
+#!./bin/babel-node
 /*
  * Copyright (C) 2023 MetaBrainz Foundation
  *

bin/babel-node
@@ -3,6 +3,5 @@
 MB_SERVER_ROOT=$(cd "$(dirname "${BASH_SOURCE[0]}")/../" && pwd)
 
 exec node \
-    --experimental-loader "$MB_SERVER_ROOT"/root/utility/sucraseLoader.mjs \
-    --no-warnings \
+    --import "$MB_SERVER_ROOT"/root/utility/babel-node/register.mjs \
     "$@"

docker/musicbrainz-tests/run_js_perl_and_pgtap_tests.sh
@@ -25,7 +25,7 @@ echo OK
 
 sv_start_if_down chrome postgresql redis
 
-sudo -E -H -u musicbrainz carton exec -- ./bin/sucrase-node \
+sudo -E -H -u musicbrainz carton exec -- ./bin/babel-node \
     t/web.js \
     | tee >(./node_modules/.bin/tap-junit > ./junit_output/js_web.xml) \
     | ./node_modules/.bin/tap-difflet

eslint.config.mjs
@@ -35,6 +35,7 @@ export default [
       'root/static/scripts/common/DBDefs.mjs',
       'root/static/scripts/supported-browser-check.js',
       'root/static/scripts/tests/typeInfo.js',
+      'root/utility/babel-node/register.mjs',
       't/selenium.js',
       'babel.config.cjs',
       'eslint.config.mjs',

root/utility/babel-node/register.mjs
@@ -1,0 +1,3 @@
+import {register} from 'node:module';
+
+await register(new URL('./hooks.mjs', import.meta.url));

root/utility/sucraseLoader.mjs
@@ -1,89 +1,0 @@
-/*
- * MIT License
- *
- * Copyright (c) 2020 Geoffrey Booth and contributors
- * Copyright (c) 2022 MetaBrainz Foundation
- *
- * Permission is hereby granted, free of charge, to any person obtaining a
- * copy of this software and associated documentation files (the "Software"),
- * to deal in the Software without restriction, including without limitation
- * the rights to use, copy, modify, merge, publish, distribute, sublicense,
- * and/or sell copies of the Software, and to permit persons to whom the
- * Software is furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
- * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
- * DEALINGS IN THE SOFTWARE.
- */
-
-import {promises as fsPromises} from 'fs';
-import {dirname, extname, resolve as resolvePath} from 'path';
-import {fileURLToPath} from 'url';
-
-import {transform as sucraseTransform} from 'sucrase';
-
-const sucraseOptions = Object.freeze({
-  disableESTransforms: true,
-  transforms: ['jsx', 'flow'],
-});
-
-export function runSucraseTransform(rawSource) {
-  return sucraseTransform(rawSource.toString(), sucraseOptions).code;
-}
-
-export async function load(url, context, defaultLoad) {
-  if (await isModule(url)) {
-    const {source: rawSource} = await defaultLoad(url, {format: 'module'});
-    return {
-      format: 'module',
-      source: runSucraseTransform(rawSource),
-    };
-  }
-  return defaultLoad(url, context, defaultLoad);
-}
-
-export function globalPreload() {
-  // Sucrase doesn't implement an automatic React runtime.
-  return `\
-const {createRequire} = getBuiltin('module');
-const {cwd} = getBuiltin('process');
-const require = createRequire(cwd() + '/<preload>');
-globalThis.React = require('react');\
-`;
-}
-
-export async function isModule(url) {
-  const ext = extname(url);
-  if (ext === '.cjs') {
-    return false;
-  }
-  if (ext === '.mjs') {
-    return true;
-  }
-  const isFilePath = ext !== '';
-  const dir = isFilePath
-    ? dirname(fileURLToPath(url))
-    : url;
-  const packagePath = resolvePath(dir, 'package.json');
-  const type = await fsPromises.readFile(packagePath, {encoding: 'utf8'})
-    .then((filestring) => JSON.parse(filestring).type)
-    .catch(function (err) {
-      if (err?.code !== 'ENOENT') {
-        console.error(err);
-      }
-    });
-  if (type === 'module') {
-    return true;
-  }
-  if (dir.length > 1) {
-    return isModule(resolvePath(dir, '..'));
-  }
-  return false;
-}

commit b6b542d3bc838360aed0570e74775a01f6553385

Fix legacy browser bundles
81d820b486de80700534060368eab530b3184b7b broke running compile_resources.sh
with `BROWSER_TARGET=legacy`, because it inadvertently affected which
node_modules are run through Babel when using babel-node. If we're targeting
node, then `BROWSER_TARGET` shouldn't have any effect whatsoever.

This commit makes the Babel 'ignore' config parameterized on the target
('node' or 'web') and excludes all of node_modules in the former case.
A diff of the server.js output with `BROWSER_TARGET=production` shows
that only some Flow type comments in mutate-cow and weight-balanced-tree
are preserved now, which is fine.

babel.config.cjs
@@ -40,13 +40,22 @@ module.exports = function (api) {
   api.cache.using(() => process.env.NODE_ENV);
   api.cache.using(() => browserTarget);
 
+  /*
+   * The target is set by Webpack (either 'node' or 'web'), or in the case of
+   * babel-node, in root/utility/babel-node/hooks.mjs (hardcoded to 'node').
+   *
+   * If we're targeting 'web', then `BROWSER_TARGETS` specifies which browser
+   * versions we're targeting.
+   */
+  const target = api.caller(caller => caller ? caller.target : null);
+
   const presets = [
     ['@babel/preset-env', {
       corejs: 3.45,
       modules: api.caller(caller => caller && caller.name === 'babel-node-loader')
         ? false
         : 'auto',
-      targets: api.caller(caller => caller && caller.target === 'node')
+      targets: target === 'node'
         ? NODE_TARGETS
         : BROWSER_TARGETS[browserTarget],
       useBuiltIns: 'usage',
@@ -72,7 +81,7 @@ module.exports = function (api) {
   }
 
   return {
-    ignore,
+    ignore: ignore(target),
     plugins,
     presets,
     sourceType: 'unambiguous',

root/utility/babel-node/hooks.mjs
@@ -54,6 +54,7 @@ export async function load(url, context, nextLoad) {
       name: 'babel-node-loader',
       supportsStaticESM: true,
       supportsTopLevelAwait: true,
+      target: 'node',
     },
     cwd: process.cwd(),
     filename,

webpack/babel-ignored.cjs
@@ -2,7 +2,7 @@
 
 // Note: This file is CommonJS because ESLint doesn't support async parsers.
 
-module.exports = [
+module.exports = (target) => [
   /*
    * The modules in the negative-lookahead assertion (?!...) are exceptions
    * to the node_modules exclusion. These most likely use language features
@@ -12,9 +12,14 @@ module.exports = [
    * except for core-js.
    */
   ...(
-    process.env.BROWSER_TARGET === 'legacy'
-      ? [/node_modules\/core-js/]
-      : [/node_modules\/(?!@babel\/runtime|@floating-ui|jed|mutate-cow|punycode|react|weight-balanced-tree)/]
+    target === 'web' ? (
+      process.env.BROWSER_TARGET === 'legacy'
+        ? [/node_modules\/core-js/]
+        : [/node_modules\/(?!@babel\/runtime|@floating-ui|jed|mutate-cow|punycode|react|weight-balanced-tree)/]
+    ) : (
+      /* target === 'node', ignore all of node_modules */
+      [/node_modules/]
+    )
   ),
   /root\/static\/scripts\/tests\/typeInfo\.js/,
   /root\/static\/build\/jed-[A-z0-9_-]+?\.source\.js$/,

webpack/client.config.mjs
@@ -302,7 +302,7 @@ export default {
 
   mode: WEBPACK_MODE,
 
-  module: moduleConfig,
+  module: moduleConfig('web'),
 
   name: 'client-bundles',
 

webpack/exec.mjs
@@ -36,7 +36,7 @@ const compiler = webpack({
 
   mode: 'production',
 
-  module: moduleConfig,
+  module: moduleConfig('node'),
 
   optimization: {
     minimize: false,

webpack/moduleConfig.mjs
@@ -19,12 +19,12 @@ if (PRODUCTION_MODE) {
   ];
 }
 
-export default {
+const moduleConfig = (target) => ({
   noParse: /^(jquery|knockout)$/,
 
   rules: [
     {
-      exclude: ignore,
+      exclude: ignore(target),
       test: /\.m?js$/,
       use: {
         loader: 'babel-loader',
@@ -76,4 +76,6 @@ export default {
       ],
     },
   ],
-};
+});
+
+export default moduleConfig;

webpack/server.config.mjs
@@ -63,7 +63,7 @@ export default {
 
   mode: WEBPACK_MODE,
 
-  module: moduleConfig,
+  module: moduleConfig('node'),
 
   name: 'server-bundle',
 

webpack/tests.config.mjs
@@ -36,7 +36,7 @@ const webTestsConfig = {
 
   mode: 'development',
 
-  module: moduleConfig,
+  module: moduleConfig('web'),
 
   name: 'web-test-bundles',
 