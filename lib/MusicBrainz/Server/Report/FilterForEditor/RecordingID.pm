9259a110b930de4a80803356794175ffe7406a24
zeyad-hassan-1 <studying.zezo@gmail.com>
2026-03-05 00:15:48 +0200

    MBS-9615: Fix subscription filter in recordings same name report
    Show all duplicate members of a group, not only subscribed entities.
    Previously the filter hid the duplicates defeating the report's purpose.

--- lib/MusicBrainz/Server/Report/FilterForEditor/RecordingID.pm
+++ lib/MusicBrainz/Server/Report/FilterForEditor/RecordingID.pm
@@ -9,9 +9,18 @@ sub filter_sql {
 
     return (
         'JOIN recording ON recording.id = recording_id
-         JOIN artist_credit_name ON recording.artist_credit = artist_credit_name.artist_credit
-         JOIN editor_subscribe_artist esa ON esa.artist = artist_credit_name.artist
-         WHERE esa.editor = ?',
+         JOIN artist_credit_name acn ON recording.artist_credit = acn.artist_credit
+         JOIN artist a ON acn.artist = a.id
+         JOIN (
+             SELECT a2.name AS artist_name, r2.name AS recording_name
+             FROM recording r2
+             JOIN artist_credit_name acn2 ON r2.artist_credit = acn2.artist_credit
+             JOIN artist a2 ON acn2.artist = a2.id
+             JOIN editor_subscribe_artist esa ON esa.artist = a2.id
+             WHERE esa.editor = ?
+             GROUP BY a2.name, r2.name
+         ) AS subscribed_items ON a.name = subscribed_items.artist_name
+                               AND recording.name = subscribed_items.recording_name',
         $editor_id,
     );
 }
