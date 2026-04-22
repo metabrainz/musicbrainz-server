d68ea35ef19c02a7865e72a306c38f47f40b797c
Michael Wiencek <mwtuea@gmail.com>
2026-04-22 16:36:55 -0500

    Fix method of adding duplicate release labels
    This can be reproduced by submitting the same release label twice via
    /ws/js/edit/create.
    
    The issue involves `throw_if_release_label_is_duplicate` using the same cached
    release labels on the same `$release` object.

--- t/lib/t/MusicBrainz/Server/Edit/Release/AddReleaseLabel.pm
+++ t/lib/t/MusicBrainz/Server/Edit/Release/AddReleaseLabel.pm
@@ -106,13 +106,35 @@ test 'Prevents initializing an edit with a duplicate label/catalog number pair' 
 
     MusicBrainz::Server::Test->prepare_test_database($c, '+edit_release_label');
 
+    my $release = $c->model('Release')->get_by_id(1);
+    my $label = $c->model('Label')->get_by_id(2);
+
     like exception {
         $c->model('Edit')->create(
             edit_type => $EDIT_RELEASE_ADDRELEASELABEL,
             editor_id => 1,
-            release => $c->model('Release')->get_by_id(1),
-            label => $c->model('Label')->get_by_id(2),
+            release => $release,
+            label => $label,
             catalog_number => 'ABC-123',
+        );
+    }, qr/The label and catalog number in this edit already exist on the release\./;
+
+    # Run another test using new release labels, and with the same `$release`
+    # object being used to initialize both edits.
+    $c->model('Edit')->create(
+        edit_type => $EDIT_RELEASE_ADDRELEASELABEL,
+        editor_id => 1,
+        release => $release,
+        label => $label,
+        catalog_number => 'ABC-456',
+    );
+    like exception {
+        $c->model('Edit')->create(
+            edit_type => $EDIT_RELEASE_ADDRELEASELABEL,
+            editor_id => 1,
+            release => $release,
+            label => $label,
+            catalog_number => 'ABC-456',
         );
     }, qr/The label and catalog number in this edit already exist on the release\./;
 };
