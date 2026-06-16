3900ace4491a6b39bf7e843fdf20eaf46afc052e
Michael Wiencek <mwtuea@gmail.com>
2026-06-16 09:58:24 -0500

    Remove `oauth_access_token` check from `Authentication::Store`
    This module is only used for password logins.

--- lib/MusicBrainz/Server/Authentication/Store.pm
+++ lib/MusicBrainz/Server/Authentication/Store.pm
@@ -5,9 +5,6 @@ use warnings;
 
 use DateTime;
 use MusicBrainz::Server::Authentication::User;
-use MusicBrainz::Server::Authentication::Utils qw(
-    find_oauth_access_token
-);
 
 sub new
 {
@@ -18,17 +15,8 @@ sub new
 sub find_user
 {
     my ($self, $authinfo, $c) = @_;
-    my $editor;
 
-    if (exists $authinfo->{oauth_access_token}) {
-        my $token = find_oauth_access_token($c, $authinfo->{oauth_access_token});
-        if (defined $token) {
-            $editor = $c->model('Editor')->get_by_id($token->editor_id);
-        }
-    }
-    else {
-        $editor = $c->model('Editor')->get_by_name($authinfo->{username});
-    }
+    my $editor = $c->model('Editor')->get_by_name($authinfo->{username});
 
     if (defined $editor && $editor->password) {
         $c->model('Editor')->load_preferences($editor);
