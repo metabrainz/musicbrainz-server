e78e5cb67ad16aea96d4e2b2c1cca5f8181c5bf4
Michael Wiencek <mwtuea@gmail.com>
2026-06-16 10:58:02 -0500

    Factor out `find_metabrainz_oauth_access_token` utility

--- lib/MusicBrainz/Server/Authentication/Utils.pm
+++ lib/MusicBrainz/Server/Authentication/Utils.pm
@@ -13,43 +13,55 @@ use MusicBrainz::Server::Entity::EditorOAuthToken;
 use MusicBrainz::Server::Constants qw( :access_scope );
 
 our @EXPORT_OK = qw(
+    find_metabrainz_oauth_access_token
     find_oauth_access_token
 );
 
-sub find_oauth_access_token {
-    my ($c, $oauth_access_token) = @_;
+sub find_metabrainz_oauth_access_token {
+    my ($c, $access_token) = @_;
 
-    if ($oauth_access_token =~ /^meba_/) {
-        my $ctx = $c->model('MB')->context;
-        my $introspect_url = DBDefs->METABRAINZ_URL . '/oauth2/introspect';
-        my $res = $ctx->lwp->request(
-            POST $introspect_url,
-            {
-                client_id => DBDefs->METABRAINZ_OAUTH_CLIENT_ID,
-                client_secret => DBDefs->METABRAINZ_OAUTH_CLIENT_SECRET,
-                token => $oauth_access_token,
-            },
-        );
-        if ($res->code == HTTP_OK) {
-            my $res_content = $c->json_utf8->decode($res->content);
-            if ($res_content->{active}) {
-                my $scope = 0;
-                for my $scope_name (@{ $res_content->{scope} }) {
-                    $scope |= $ACCESS_SCOPE_BY_NAME{$scope_name};
-                }
-                return MusicBrainz::Server::Entity::EditorOAuthToken->new(
-                    access_token => $oauth_access_token,
-                    editor_id => $res_content->{sub},
-                    expire_time => DateTime->from_epoch($res_content->{expires_at}),
-                    granted => DateTime->from_epoch($res_content->{issued_at}),
-                    scope => $scope,
-                );
+    my $ctx = $c->model('MB')->context;
+    my $introspect_url = DBDefs->METABRAINZ_URL . '/oauth2/introspect';
+    my $res = $ctx->lwp->request(
+        POST $introspect_url,
+        {
+            client_id => DBDefs->METABRAINZ_OAUTH_CLIENT_ID,
+            client_secret => DBDefs->METABRAINZ_OAUTH_CLIENT_SECRET,
+            token => $access_token,
+        },
+    );
+    if ($res->code == HTTP_OK) {
+        my $res_content = $c->json_utf8->decode($res->content);
+        if ($res_content->{active}) {
+            my $scope = 0;
+            for my $scope_name (@{ $res_content->{scope} }) {
+                $scope |= $ACCESS_SCOPE_BY_NAME{$scope_name};
             }
+            return MusicBrainz::Server::Entity::EditorOAuthToken->new(
+                access_token => $access_token,
+                editor_id => $res_content->{sub},
+                expire_time => DateTime->from_epoch($res_content->{expires_at}),
+                granted => DateTime->from_epoch($res_content->{issued_at}),
+                scope => $scope,
+            );
         }
-    } else {
-        return $c->model('EditorOAuthToken')->get_by_access_token($oauth_access_token);
     }
     return;
+}
+
+sub find_musicbrainz_oauth_access_token {
+    my ($c, $access_token) = @_;
+
+    return $c->model('EditorOAuthToken')->get_by_access_token($access_token);
+}
+
+sub find_oauth_access_token {
+    my ($c, $access_token) = @_;
+
+    if ($access_token =~ /^meba_/) {
+        return find_metabrainz_oauth_access_token($c, $access_token);
+    }
+    return find_musicbrainz_oauth_access_token($c, $access_token);
 }
 
 1;
