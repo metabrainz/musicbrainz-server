fba5447ae841b0a47f3dfc5a167eeccb4ff48bfe
Michael Wiencek <mwtuea@gmail.com>
2026-08-31 14:19:31 -0500

    Add flow-typed/environments/LICENSE and fix headers
    The files as synced from the flow-typed command did not contain any copyright
    header. The ones I copied from
    https://github.com/facebook/flow/tree/main/evals/flow-typed/environment in the
    previous commit do. This adds their license to the same directory.

--- flow-typed/environments/geometry.js
+++ flow-typed/environments/geometry.js
@@ -2,10 +2,7 @@
  * Copyright (c) Meta Platforms, Inc. and affiliates.
  *
  * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- *
- * @noformat
- * @oncall flow
+ * LICENSE file in the same directory of this source tree.
  */
 
 // https://www.w3.org/TR/geometry-1/
