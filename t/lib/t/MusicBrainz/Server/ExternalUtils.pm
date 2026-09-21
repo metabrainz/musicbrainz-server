459be48199a94d69c5157eb87525248d013d506f
yvanzo <yvanzo@metabrainz.org>
2026-09-21 11:21:22 +0200

    Forward optional headers in fetch helper
    Extend `get_chunked_with_retry` to accept an optional list of HTTP
    header key/value pairs and pass them through to `LWP::UserAgent::get`.
    
    The existing two-argument callers (e.g. `Data::WikiDoc`) are unaffected.
    
    An LLM was used to check test coverage and write the initial version of
    the added tests, which I thoroughly reviewed and modified afterwards.
    
    The next commit uses this to attach `X-MB-*` headers to search requests
    that `Data::Search::external_search` dispatches to Solr, for MBS-14470.

--- t/lib/t/MusicBrainz/Server/ExternalUtils.pm
+++ t/lib/t/MusicBrainz/Server/ExternalUtils.pm
@@ -9,6 +9,53 @@ use HTTP::Response;
 use HTTP::Status qw( :constants );
 use MusicBrainz::Server::ExternalUtils qw( get_chunked_with_retry );
 
+# A minimal mock user agent that records the arguments passed to get().
+{
+    package t::MusicBrainz::Server::ExternalUtils::MockUA;
+    use HTTP::Response;
+    use HTTP::Status qw( :constants );
+
+    sub new { return bless { calls => [] }, shift }
+
+    sub get {
+        my ($self, $url, @headers) = @_;
+        push @{ $self->{calls} }, { url => $url, headers => \@headers };
+        my $response = HTTP::Response->new;
+        $response->code(HTTP_OK);
+        $response->content('ok');
+        return $response;
+    }
+}
+
+test 'get_chunked_with_retry works with no headers' => sub {
+    my $ua = t::MusicBrainz::Server::ExternalUtils::MockUA->new;
+
+    my $response = get_chunked_with_retry($ua, 'https://search.example.com/path');
+
+    ok($response->is_success, 'request succeeds');
+    is_deeply($ua->{calls}[0]{headers}, [],
+        'no extra headers passed when none supplied');
+};
+
+test 'get_chunked_with_retry forwards headers to the user agent' => sub {
+    my $ua = t::MusicBrainz::Server::ExternalUtils::MockUA->new;
+
+    my $response = get_chunked_with_retry(
+        $ua,
+        'https://search.example.com/path',
+        'X-MB-Foo' => 'Bar',
+    );
+
+    ok($response->is_success, 'request succeeds');
+    is(scalar @{ $ua->{calls} }, 1, 'user agent called once');
+
+    my $call = $ua->{calls}[0];
+    is($call->{url}, 'https://search.example.com/path',
+        'URL is forwarded');
+    is_deeply($call->{headers}, ['X-MB-Foo', 'Bar'],
+        'headers are forwarded to the user agent');
+};
+
 # A mock user agent that simulates a truncated chunked response
 # (`X-Died` header with `read timeout`) on its two first calls,
 # then succeeds. Tracks the attempt count itself, should be 3.
