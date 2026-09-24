deff73a54c2da7a4d531364f96490eee63db020e
Lioncat6 <95449321+Lioncat6@users.noreply.github.com>
2026-09-24 17:37:47 -0500

    Validate subvert relationship types with limited_link_type_combinations

--- root/static/scripts/tests/Control/URLCleanup.js
+++ root/static/scripts/tests/Control/URLCleanup.js
@@ -6235,35 +6235,60 @@ limited_link_type_combinations: ['downloadpurchase', 'mailorder'],
   {
                      input_url: 'https://subvert.fm/draconium?foo=bar',
              input_entity_type: 'artist',
-    expected_relationship_type: 'downloadpurchase',
+limited_link_type_combinations: [
+                                  ['downloadpurchase', 'streamingfree'],
+                                  ['downloadfree', 'streamingfree'],
+                                  'downloadfree',
+                                  'downloadpurchase'
+                                ],
             expected_clean_url: 'https://www.subvert.fm/draconium',
        only_valid_entity_types: ['artist', 'label'],
   },
   {
                      input_url: 'https://www.subvert.fm/nullpolitik#',
              input_entity_type: 'artist',
-    expected_relationship_type: 'streamingfree',
+limited_link_type_combinations: [
+                                  ['downloadpurchase', 'streamingfree'],
+                                  ['downloadfree', 'streamingfree'],
+                                  'downloadfree',
+                                  'downloadpurchase'
+                                ],
             expected_clean_url: 'https://www.subvert.fm/nullpolitik',
        only_valid_entity_types: ['artist', 'label'],
   },
   {
                      input_url: 'https://www.subvert.fm/nullpolitik/nullpolitik#foobar',
              input_entity_type: 'release',
-    expected_relationship_type: 'downloadfree',
+limited_link_type_combinations: [
+                                  ['downloadpurchase', 'streamingfree'],
+                                  ['downloadfree', 'streamingfree'],
+                                  'downloadfree',
+                                  'downloadpurchase'
+                                ],
             expected_clean_url: 'https://www.subvert.fm/nullpolitik/nullpolitik',
        only_valid_entity_types: ['release'],
   },
   {
                      input_url: 'https://subvert.fm/nullpolitik/tracks/himmel',
              input_entity_type: 'release',
-    expected_relationship_type: 'downloadpurchase',
+limited_link_type_combinations: [
+                                  ['downloadpurchase', 'streamingfree'],
+                                  ['downloadfree', 'streamingfree'],
+                                  'downloadfree',
+                                  'downloadpurchase'
+                                ],
             expected_clean_url: 'https://www.subvert.fm/nullpolitik/tracks/himmel',
        only_valid_entity_types: ['release', 'recording'],
   },
   {
                      input_url: 'https://www.subvert.fm/draconium/tracks/legends',
              input_entity_type: 'recording',
-    expected_relationship_type: 'streamingfree',
+limited_link_type_combinations: [
+                                  ['downloadpurchase', 'streamingfree'],
+                                  ['downloadfree', 'streamingfree'],
+                                  'downloadfree',
+                                  'downloadpurchase'
+                                ],
             expected_clean_url: 'https://www.subvert.fm/draconium/tracks/legends',
        only_valid_entity_types: ['release', 'recording'],
   },
